57de59bddcc28cb1f96fa1ad0b2e9de4ac734310
Chef Nomi <defichefnomi@gmail.com>
2020-09-04 20:50:35 +0700

    Remove pro tip

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -24,11 +24,6 @@ const Home: React.FC = () => {
         <Balances />
       </Container>
       <Spacer size="lg" />
-      <StyledInfo>
-        🏆<b>Pro Tip</b>: SUSHI-ETH UNI-V2 LP token pool yields TWICE more token
-        rewards per block.
-      </StyledInfo>
-      <Spacer size="lg" />
       <div
         style={{
           margin: '0 auto',
