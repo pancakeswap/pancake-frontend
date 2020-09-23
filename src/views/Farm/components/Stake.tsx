14f3d96e335f8a8a0482aef2efe39ababf846b21
pancake <pancakeswap1@protonmail.com>
2020-09-24 02:51:54 +0800

    change mining icon

--- src/views/Farm/components/Stake.tsx
+++ src/views/Farm/components/Stake.tsx
@@ -89,7 +89,7 @@ const Stake: React.FC<StakeProps> = ({ lpContract, pid, tokenName }) => {
       <CardContent>
         <StyledCardContentInner>
           <StyledCardHeader>
-            <CardIcon>🧑🏿‍🚒</CardIcon>
+            <CardIcon>⛏🐰</CardIcon>
             <Value value={getBalanceNumber(stakedBalance)} />
             <Label text={`${tokenName} Tokens Staked`} />
           </StyledCardHeader>
