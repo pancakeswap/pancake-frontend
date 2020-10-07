4b1471be5808ae3e19a34ef209c05e17c0d407ca
RichardPK <r.phillips.kerr@gmail.com>
2020-10-07 21:01:18 +0100

    Hooked up providers and API info for crowdin

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import { Credentials, StringTranslations } from '@crowdin/crowdin-api-client'
 import { lightTheme, darkTheme } from './theme'
 import { UseWalletProvider } from 'use-wallet'
 import DisclaimerModal from './components/DisclaimerModal'
@@ -12,6 +13,10 @@ import ModalsProvider from './contexts/Modals'
 import TransactionProvider from './contexts/Transactions'
 import SushiProvider from './contexts/SushiProvider'
 import BscProvider from './contexts/BscProvider'
+import { EN } from './constants/localisation/languageCodes'
+import { allLanguages } from './constants/localisation/languageCodes'
+import { LanguageContext } from './contexts/Localisation/languageContext'
+import { TranslationsContext } from './contexts/Localisation/translationsContext'
 import useModal from './hooks/useModal'
 import useTheme from './hooks/useTheme'
 import Farms from './views/Farms'
@@ -28,6 +33,17 @@ import Web3ReactManager from './components/Web3ReactManager'
 const App: React.FC = () => {
   const [isDark, setIsDark] = useTheme()
   const [mobileMenu, setMobileMenu] = useState(false)
+  const [selectedLanguage, setSelectedLanguage] = useState<any>(undefined)
+  const [translatedLanguage, setTranslatedLanguage] = useState<any>(undefined)
+  const [translations, setTranslations] = useState<Array<any>>([])
+
+  const apiKey = `${process.env.REACT_APP_CROWDIN_APIKEY}`
+  const projectId = parseInt(`${process.env.REACT_APP_CROWDIN_PROJECTID}`)
+  const credentials: Credentials = {
+    token: apiKey,
+  }
+
+  const stringTranslationsApi = new StringTranslations(credentials)
 
   const handleDismissMobileMenu = useCallback(() => {
     setMobileMenu(false)
@@ -37,46 +53,99 @@ const App: React.FC = () => {
     setMobileMenu(true)
   }, [setMobileMenu])
 
+  const getStoredLang = (storedLangCode: string) => {
+    return allLanguages.filter((language) => {
+      return language.code === storedLangCode
+    })[0]
+  }
+
+  useEffect(() => {
+    const storedLangCode = localStorage.getItem('pancakeSwapLanguage')
+    if (storedLangCode) {
+      const storedLang = getStoredLang(storedLangCode)
+      setSelectedLanguage(storedLang)
+    } else {
+      setSelectedLanguage(EN)
+    }
+  }, [])
+
+  const fetchTranslationsForSelectedLanguage = async () => {
+    stringTranslationsApi
+      .listLanguageTranslations(
+        projectId,
+        selectedLanguage.code,
+        undefined,
+        undefined,
+        200,
+      )
+      .then((translationApiResponse) =>
+        setTranslations(translationApiResponse.data),
+      )
+      .then(() => setTranslatedLanguage(selectedLanguage))
+      .catch((error) => console.error(error))
+  }
+
+  useEffect(() => {
+    if (selectedLanguage) {
+      fetchTranslationsForSelectedLanguage()
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedLanguage])
+
   return (
     <Providers isDark={isDark}>
-      <Router>
-        <GlobalStyle />
-        <TopBar
-          isDark={isDark}
-          toogleTheme={setIsDark}
-          onPresentMobileMenu={handlePresentMobileMenu}
-        />
-        <MobileMenu onDismiss={handleDismissMobileMenu} visible={mobileMenu} />
-        <Web3ReactManager>
-          <Switch>
-            <Route path="/" exact>
-              <Home />
-            </Route>
-            <Route path="/farms">
-              <Farms removed={false}/>
-            </Route>
-            <Route path="/staking">
-              <Stake />
-            </Route>
-            <Route path="/syrup">
-              <Syrup />
-            </Route>
-            <Route path="/lottery">
-              <Lottery />
-            </Route>
-            <Route path="/voting">
-              <Voting />
-            </Route>
-            <Route path="/vision">
-              <Vision />
-            </Route>
-            <Route path="/removed">
-              <Farms removed={true}/>
-            </Route>
-          </Switch>
-        </Web3ReactManager>
-      </Router>
-      <Disclaimer />
+      <LanguageContext.Provider
+        value={{
+          selectedLanguage,
+          setSelectedLanguage,
+          translatedLanguage,
+          setTranslatedLanguage,
+        }}
+      >
+        <TranslationsContext.Provider value={{ translations, setTranslations }}>
+          <Router>
+            <GlobalStyle />
+            <TopBar
+              isDark={isDark}
+              toogleTheme={setIsDark}
+              onPresentMobileMenu={handlePresentMobileMenu}
+            />
+            <MobileMenu
+              onDismiss={handleDismissMobileMenu}
+              visible={mobileMenu}
+            />
+            <Web3ReactManager>
+              <Switch>
+                <Route path="/" exact>
+                  <Home />
+                </Route>
+                <Route path="/farms">
+                  <Farms removed={false} />
+                </Route>
+                <Route path="/staking">
+                  <Stake />
+                </Route>
+                <Route path="/syrup">
+                  <Syrup />
+                </Route>
+                <Route path="/lottery">
+                  <Lottery />
+                </Route>
+                <Route path="/voting">
+                  <Voting />
+                </Route>
+                <Route path="/vision">
+                  <Vision />
+                </Route>
+                <Route path="/removed">
+                  <Farms removed={true} />
+                </Route>
+              </Switch>
+            </Web3ReactManager>
+          </Router>
+          <Disclaimer />
+        </TranslationsContext.Provider>
+      </LanguageContext.Provider>
     </Providers>
   )
 }
@@ -87,7 +156,7 @@ const Providers: React.FC<{ isDark: boolean }> = ({ isDark, children }) => {
       <UseWalletProvider
         chainId={56}
         connectors={{
-          walletconnect: { rpcUrl: 'https://bsc-dataseed.binance.org' }
+          walletconnect: { rpcUrl: 'https://bsc-dataseed.binance.org' },
         }}
       >
         <BscProvider>
