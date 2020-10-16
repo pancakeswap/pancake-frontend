dec081ece1e5d977aecfd8bf4c36694d7b9741c0
pancake-cat <71744512+pancake-cat@users.noreply.github.com>
2020-10-16 18:50:56 +0800

    fix: add theme cache (#18)

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -2,22 +2,35 @@ import { useState, useCallback, useEffect } from 'react'
 import mediaObserve from '../utils/mediaObserve'
 
 type ReturnType = [boolean, (isDark: boolean) => void]
+const CACHE_KEY = 'IS_DARK'
 
 function useTheme(): ReturnType {
-  const [isDark, setIsDark] = useState(false)
+  const [matchDarkMode, setMatchDarkMode] = useState(false)
+  const [userDarkMode, setUserDarkMode] = useState(() => {
+    let cache
+    try {
+      cache = JSON.parse(localStorage.getItem(CACHE_KEY))
+    } catch(error){}
+    return cache
+  })
   const toogleTheme = useCallback(rs => {
-    setIsDark(rs)
-  }, [setIsDark])
+    try {
+      localStorage.setItem(CACHE_KEY, JSON.stringify(rs))
+    } catch(error){}
+    setUserDarkMode(rs)
+  }, [setUserDarkMode])
 
   useEffect(() => {
     const unSubscribe = mediaObserve.subscribe(rs => {
-      toogleTheme(rs.isDark)
+      setMatchDarkMode(rs.isDark)
     });
 
     return unSubscribe;
-  }, [toogleTheme])
+  }, [setMatchDarkMode])
 
-  return [isDark, toogleTheme]
+  const rtMode = userDarkMode === null ? matchDarkMode : userDarkMode
+
+  return [rtMode, toogleTheme]
 }
 
 export default useTheme
