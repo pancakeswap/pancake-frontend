394295da5e1fe464bc06b9fe69599671eedf06fd
RabbitDoge <72658581+RabbitDoge@users.noreply.github.com>
2020-10-29 11:33:33 +0900

    Integration fixes (#47)
    * fix: Card alignment on farm
    
    * fix: Global style definition

--- src/style/Global.tsx
+++ src/style/Global.tsx
@@ -1,7 +1,14 @@
 import { createGlobalStyle } from 'styled-components'
+import { PancakeTheme } from '@pancakeswap-libs/uikit/dist/theme/types'
+
+declare module 'styled-components' {
+  /* eslint-disable @typescript-eslint/no-empty-interface */
+  export interface DefaultTheme extends PancakeTheme {}
+}
 
 const GlobalStyle = createGlobalStyle`
   body {
+    background-color: ${({ theme }) => theme.colors.background};
     @media (max-width: 500px) {
       height: 100vh;
     }
