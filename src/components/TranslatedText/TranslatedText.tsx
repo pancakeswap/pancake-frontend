6855c7fd61dd32a8ddfd09710f701bedd83733f7
hachiojidev <hachiojidev@protonmail.com>
2020-10-29 18:33:39 +0900

    fix: TranslateString component

--- src/components/TranslatedText/TranslatedText.tsx
+++ src/components/TranslatedText/TranslatedText.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { TranslateString } from '../../utils/translateTextHelpers'
+import useI18n from 'hooks/useI18n'
 
 export interface TranslatedTextProps {
   translationId: number
@@ -7,6 +7,7 @@ export interface TranslatedTextProps {
 }
 
 const TranslatedText = ({ translationId, children }: TranslatedTextProps) => {
+  const TranslateString = useI18n()
   return <>{TranslateString(translationId, children)}</>
 }
 
