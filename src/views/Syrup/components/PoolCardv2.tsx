d7809316bff09d89f4d12fe0dda6212329671052
hachiojidev <hachiojidev@protonmail.com>
2020-10-30 16:09:16 +0900

    fix: Card active state when unstaked

--- src/views/Syrup/components/PoolCardv2.tsx
+++ src/views/Syrup/components/PoolCardv2.tsx
@@ -92,7 +92,7 @@ const PoolCardv2: React.FC<HarvestProps> = ({
   }, [cakePrice, harvest, tokenPerBlock, tokenPrice, totalStaked])
 
   const isUnstaked =
-    account && !allowance.toNumber() && stakedBalance.toNumber() === 0
+    account && allowance.toNumber() && stakedBalance.toNumber() > 0
 
   // TODO - Remove this when pool removed
   const isOldCTXPool = sousId === CTXOLD
