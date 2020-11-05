97fba1e42aa8f1d15c771ce2cb93e05ab432ebfe
RabbitDoge <72658581+RabbitDoge@users.noreply.github.com>
2020-11-05 22:23:50 +0900

    feat: Integrate new menu

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,72 @@
+import React, { useContext, useCallback, useMemo } from 'react'
+import { Nav } from '@pancakeswap-libs/uikit'
+import { useWallet } from 'use-wallet'
+import { allLanguages } from 'constants/localisation/languageCodes'
+import { LanguageContext } from 'contexts/Localisation/languageContext'
+import useTheme from 'hooks/useTheme'
+
+interface ConnectCallbackType {
+  key: 'metamask' | 'trustwallet' | 'mathwallet' | 'tokenpocket' | 'walletconnect'
+  callback: () => void
+}
+
+const Menu = () => {
+  const { account, connect, reset } = useWallet()
+  const { selectedLanguage, setSelectedLanguage } = useContext(LanguageContext)
+  const { isDark, toggleTheme } = useTheme()
+
+  const login = useCallback(
+    (connectorId) => {
+      connect(connectorId)
+      window.localStorage.setItem('accountStatus', '1')
+    },
+    [connect],
+  )
+
+  const logout = useCallback(() => {
+    window.localStorage.removeItem('accountStatus')
+    reset()
+    return null
+  }, [reset])
+
+  const connectCallbacks: ConnectCallbackType[] = useMemo(
+    () => [
+      {
+        key: 'metamask',
+        callback: () => login('injected'),
+      },
+      {
+        key: 'trustwallet',
+        callback: () => login('injected'),
+      },
+      {
+        key: 'mathwallet',
+        callback: () => login('injected'),
+      },
+      {
+        key: 'tokenpocket',
+        callback: () => login('injected'),
+      },
+      {
+        key: 'walletconnect',
+        callback: () => login('walletconnect'),
+      },
+    ],
+    [login],
+  )
+
+  return (
+    <Nav
+      account={account}
+      connectCallbacks={connectCallbacks}
+      logout={logout}
+      isDark={isDark}
+      toggleTheme={toggleTheme}
+      currentLang={selectedLanguage && selectedLanguage.code}
+      langs={allLanguages}
+      setLang={setSelectedLanguage}
+    />
+  )
+}
+
+export default Menu
---
+++ src/contexts/Localisation/languageContext.tsx
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from 'react'
+import { StringTranslations } from '@crowdin/crowdin-api-client'
+import { TranslationsContext } from 'contexts/Localisation/translationsContext'
+import { allLanguages } from 'constants/localisation/languageCodes'
+import { EN } from 'constants/localisation/languageCodes'
+
+const CACHE_KEY = 'pancakeSwapLanguage'
+
+export interface LangType {
+  code: string
+  language: string
+}
+
+export interface LanguageState {
+  selectedLanguage: LangType
+  setSelectedLanguage: (langObject: LangType) => void
+  translatedLanguage: LangType
+  setTranslatedLanguage: React.Dispatch<React.SetStateAction<LangType>>
+}
+
+const LanguageContext = React.createContext({
+  selectedLanguage: { code: '', language: '' },
+  setSelectedLanguage: () => {},
+  translatedLanguage: { code: '', language: '' },
+  setTranslatedLanguage: () => {},
+} as LanguageState)
+
+const fileId = 8
+const projectId = parseInt(process.env.REACT_APP_CROWDIN_PROJECTID)
+const stringTranslationsApi = new StringTranslations({
+  token: process.env.REACT_APP_CROWDIN_APIKEY,
+})
+
+const fetchTranslationsForSelectedLanguage = (selectedLanguage) => {
+  return stringTranslationsApi.listLanguageTranslations(projectId, selectedLanguage.code, undefined, fileId, 200)
+}
+
+const LanguageContextProvider = ({ children }) => {
+  const [selectedLanguage, setSelectedLanguage] = useState<any>(undefined)
+  const [translatedLanguage, setTranslatedLanguage] = useState<any>(undefined)
+  const [translations, setTranslations] = useState<Array<any>>([])
+
+  const getStoredLang = (storedLangCode: string) => {
+    return allLanguages.filter((language) => {
+      return language.code === storedLangCode
+    })[0]
+  }
+
+  useEffect(() => {
+    const storedLangCode = localStorage.getItem(CACHE_KEY)
+    if (storedLangCode) {
+      const storedLang = getStoredLang(storedLangCode)
+      setSelectedLanguage(storedLang)
+    } else {
+      setSelectedLanguage(EN)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (selectedLanguage) {
+      fetchTranslationsForSelectedLanguage(selectedLanguage)
+        .then((translationApiResponse) => {
+          if (translationApiResponse.data.length < 1) {
+            setTranslations(['error'])
+          } else {
+            console.log(translationApiResponse)
+            setTranslations(translationApiResponse.data)
+          }
+        })
+        .then(() => setTranslatedLanguage(selectedLanguage))
+        .catch((error) => {
+          setTranslations(['error'])
+        })
+    }
+  }, [selectedLanguage, setTranslations])
+
+  const handleLanguageSelect = (langObject: LangType) => {
+    setSelectedLanguage(langObject)
+    localStorage.setItem(CACHE_KEY, langObject.code)
+  }
+
+  return (
+    <LanguageContext.Provider
+      value={{ selectedLanguage, setSelectedLanguage: handleLanguageSelect, translatedLanguage, setTranslatedLanguage }}
+    >
+      <TranslationsContext.Provider value={{ translations, setTranslations }}>{children}</TranslationsContext.Provider>
+    </LanguageContext.Provider>
+  )
+}
+
+export { LanguageContext, LanguageContextProvider }
