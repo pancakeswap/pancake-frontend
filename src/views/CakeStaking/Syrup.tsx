117d5c654dc03ffa46f525bf69f9dda0dbf52a4f
hachiojidev <hachiojidev@protonmail.com>
2020-11-09 12:08:52 +0900

    fix: Missing code

--- src/views/CakeStaking/Syrup.tsx
+++ src/views/CakeStaking/Syrup.tsx
@@ -1,18 +1,40 @@
 /* eslint-disable jsx-a11y/accessible-emoji */
-import React, { useMemo } from 'react'
+import React, { useEffect, useMemo } from 'react'
 import BigNumber from 'bignumber.js'
 import styled from 'styled-components'
-import orderBy from 'lodash/orderBy'
 import { useWallet } from 'use-wallet'
 import { provider } from 'web3-core'
 import { getContract } from 'utils/erc20'
 import useSushi from 'hooks/useSushi'
 import useI18n from 'hooks/useI18n'
 import useAllStakedValue from 'hooks/useAllStakedValue'
+import { useTokenBalance2 } from 'hooks/useTokenBalance'
 import { getPools } from 'sushi/utils'
+
 import PoolCardv2 from './components/PoolCardv2'
 import Coming from './components/Coming'
 import SyrupWarning from './components/SyrupWarning'
+import { sousChefTeam } from 'sushi/lib/constants'
+
+const CAKE_ADDRESS = '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82'
+const COMMUNITY_ADDR = {
+  STAX: {
+    lp: '0x7cd05f8b960ba071fdf69c750c0e5a57c8366500',
+    token: '0x0Da6Ed8B13214Ff28e9Ca979Dd37439e8a88F6c4',
+  },
+  NAR: {
+    lp: '0x745c4fd226e169d6da959283275a8e0ecdd7f312',
+    token: '0xa1303e6199b319a891b79685f0537d289af1fc83',
+  },
+  NYA: {
+    lp: '0x2730bf486d658838464a4ef077880998d944252d',
+    token: '0xbfa0841f7a90c4ce6643f651756ee340991f99d5',
+  },
+  bROOBEE: {
+    lp: '0x970858016C963b780E06f7DCfdEf8e809919BcE8',
+    token: '0xe64f5cb844946c1f102bd25bbd87a5ab4ae89fbe',
+  },
+}
 
 interface SyrupRowProps {
   syrupAddress: string
@@ -41,11 +63,31 @@ const SyrupRow: React.FC<SyrupRowProps> = ({
     return getContract(ethereum as provider, '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82')
   }, [ethereum])
 
+  // /!\ Dirty fix
+  // The community LP are all against CAKE instead of BNB. Thus, the usual function for price computation didn't work.
+  // This quick fix aim to properly compute the price of CAKE pools in order to get the correct APY.
+  // This fix will need to be cleaned, by using config files instead of the COMMUNITY_ADDR,
+  // and factorise the price computation logic.
+
+  const cakeBalanceOnLP = useTokenBalance2(CAKE_ADDRESS, COMMUNITY_ADDR[tokenName]?.lp)
+  const tokenBalanceOnLP = useTokenBalance2(COMMUNITY_ADDR[tokenName]?.token, COMMUNITY_ADDR[tokenName]?.lp)
+
+  const price = (() => {
+    if (isCommunity) {
+      if (cakeBalanceOnLP === 0 || tokenBalanceOnLP === 0) return new BigNumber(0)
+      const tokenBalanceOnLP_BN = new BigNumber(tokenBalanceOnLP)
+      const cakeBalanceOnLP_BN = new BigNumber(cakeBalanceOnLP)
+      const ratio = cakeBalanceOnLP_BN.div(tokenBalanceOnLP_BN)
+      return ratio.times(cakePrice)
+    }
+    return tokenPrice
+  })()
+
   return (
     <PoolCardv2
       syrup={syrup}
       cakePrice={cakePrice}
-      tokenPrice={tokenPrice}
+      tokenPrice={price}
       tokenPerBlock={tokenPerBlock}
       {...{ sousId, tokenName, projectLink, harvest, isCommunity }}
     />
@@ -56,7 +98,8 @@ const Farm: React.FC = () => {
   const sushi = useSushi()
   const TranslateString = useI18n()
   const stakedValue = useAllStakedValue()
-  const pools = getPools(sushi)
+  const pools = getPools(sushi) || sousChefTeam
+
   const renderPools = useMemo(() => {
     const stakedValueObj = stakedValue.reduce(
       (a, b) => ({
@@ -66,32 +109,21 @@ const Farm: React.FC = () => {
       {},
     )
 
-    const formattedPools = pools.map((pool) => ({
-      ...pool,
-      cakePrice: stakedValueObj['CAKE']?.tokenPriceInWeth || new BigNumber(0),
-      tokenPrice: stakedValueObj[pool.tokenName]?.tokenPriceInWeth || new BigNumber(0),
-    }))
-
-    return orderBy(formattedPools, ['sortOrder', 'isFinished', 'isCommunity'], ['asc', 'asc', 'asc'])
+    return pools.map((pool) => {
+      const cakePrice = stakedValueObj['CAKE']?.tokenPriceInWeth || new BigNumber(0)
+      const tokenPrice = stakedValueObj[pool.tokenName]?.tokenPriceInWeth || new BigNumber(0)
+      return {
+        ...pool,
+        cakePrice,
+        tokenPrice,
+      }
+    })
   }, [stakedValue, pools])
 
-  // Separate active pools from finished pools so we can inject the callout
-  const { openPools, finishedPools } = renderPools.reduce(
-    (accum, pool) => {
-      if (pool.isFinished) {
-        return {
-          ...accum,
-          finishedPools: [...accum.finishedPools, pool],
-        }
-      }
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
 
-      return {
-        ...accum,
-        openPools: [...accum.openPools, pool],
-      }
-    },
-    { openPools: [], finishedPools: [] },
-  )
   return (
     <Page>
       <SyrupWarning />
@@ -109,13 +141,10 @@ const Farm: React.FC = () => {
         </div>
       </Hero>
       <Pools>
-        {openPools.map((pool) => (
-          <SyrupRow key={pool.sousId} {...pool} />
+        {renderPools.map((pool) => (
+          <SyrupRow key={pool.sousId} stakedValue={stakedValue} {...pool} />
         ))}
         <Coming />
-        {finishedPools.map((pool) => (
-          <SyrupRow key={pool.sousId} {...pool} />
-        ))}
       </Pools>
     </Page>
   )
@@ -131,30 +160,25 @@ const Hero = styled.div`
   margin-right: auto;
   max-width: 250px;
   padding: 48px 0;
-
   h1 {
     font-size: 64px;
     color: ${({ theme }) => theme.colors.secondary2};
     line-height: 1.1;
     margin: 0 0 32px 0;
   }
-
   ul {
     margin: 0;
     padding: 0;
     list-style-type: none;
     font-size: 16px;
-
     li {
       margin-bottom: 4px;
     }
   }
-
   img {
     height: auto;
     max-width: 100%;
   }
-
   @media (min-width: 576px) {
     grid-template-columns: 1fr 1fr;
     margin: 0;
@@ -169,12 +193,10 @@ const Page = styled.div`
   padding-bottom: 48px;
   padding-left: 16px;
   padding-right: 16px;
-
   @media (min-width: 576px) {
     padding-left: 24px;
     padding-right: 24px;
   }
-
   @media (min-width: 968px) {
     padding-left: 32px;
     padding-right: 32px;
@@ -186,25 +208,20 @@ const Pools = styled.div`
   display: grid;
   grid-template-columns: repeat(8, 1fr);
   grid-gap: 16px;
-
   @media (min-width: 576px) {
     grid-template-columns: repeat(8, 1fr);
     grid-gap: 24px;
   }
-
   @media (min-width: 852px) {
     grid-template-columns: repeat(12, 1fr);
     grid-gap: 24px;
   }
-
   @media (min-width: 968px) {
     grid-template-columns: repeat(12, 1fr);
     grid-gap: 32px;
   }
-
   & > div {
     grid-column: 2 / 8;
-
     @media (min-width: 576px) {
       grid-column: span 4;
     }
