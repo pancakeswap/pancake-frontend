983dd015dfc474a5970948d25afe6a3ecf9c3ade
RabbitDoge <72658581+RabbitDoge@users.noreply.github.com>
2020-11-11 13:22:37 +0900

    fix: Lottery timer

--- src/views/Lottery/components/time.tsx
+++ src/views/Lottery/components/time.tsx
@@ -8,25 +8,31 @@ interface TimePros {
   state: boolean
 }
 
-const Time: React.FC<TimePros> = ({ state }) => {
-  const [currentTime, setCurrentTime] = React.useState(
-    Date.parse(new Date()) / 1000,
-  )
-  const TranslateString = useI18n()
+const getDeadlineTime = (state, currentTime): string => {
+  if (state === LotteryStates.BUY_TICKETS_OPEN) {
+    const endTime = (parseInt(currentTime / 21600) + 1) * 21600 + 7200
+    const minutes = ((endTime - currentTime) % 3600) / 60
+    const hours = (((endTime - currentTime) % (3600 * 24)) / 3600) % 6
+    return `${parseInt(hours)}h, ${parseInt(minutes)}m`
+  }
   const endTime = (parseInt(currentTime / 3600) + 1) * 3600
   const minutes = ((endTime - currentTime) % 3600) / 60
   const hours = ((endTime - currentTime) % (3600 * 24)) / 3600
+  return `${parseInt(hours)}h, ${parseInt(minutes)}m`
+}
 
-  const stateDeadlineTime = `${parseInt(hours)}h, ${parseInt(minutes)}m`
+const Time: React.FC<TimePros> = ({ state }) => {
+  const [currentTime, setCurrentTime] = React.useState(Date.parse(new Date()) / 1000)
+  const TranslateString = useI18n()
+
+  const stateDeadlineTime = getDeadlineTime(state, currentTime)
 
   const tick = () => {
     setCurrentTime(currentTime + 1)
   }
 
   React.useEffect(() => {
-    // 执行定时
     let timerID = setInterval(() => tick(), 1000)
-    // 卸载组件时进行清理
     return () => clearInterval(timerID)
   })
 
@@ -35,18 +41,14 @@ const Time: React.FC<TimePros> = ({ state }) => {
       {state === LotteryStates.BUY_TICKETS_OPEN && (
         <div>
           <Title style={{ marginTop: '2em' }}>⏳</Title>
-          <Title>
-            {TranslateString(434, 'Approx. time until lottery draw')}
-          </Title>
+          <Title>{TranslateString(434, 'Approx. time until lottery draw')}</Title>
           <Title2>{stateDeadlineTime}</Title2>
         </div>
       )}
       {state === LotteryStates.WINNERS_ANNOUNCED && (
         <div>
           <Title style={{ marginTop: '2em' }}>⏳</Title>
-          <Title>
-            {TranslateString(999, 'Approx. time before next lottery start')}
-          </Title>
+          <Title>{TranslateString(999, 'Approx. time before next lottery start')}</Title>
           <Title2>{stateDeadlineTime}</Title2>
         </div>
       )}
