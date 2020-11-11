189021e16fc9cf2273553359058e40fdf779fcc0
hachiojidev <72719739+hachiojidev@users.noreply.github.com>
2020-11-11 10:00:34 +0900

    fix: Dashboard claim amount formatting (#84)

--- src/views/Home/components/CakeWinnings.tsx
+++ src/views/Home/components/CakeWinnings.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { useTotalClaim } from 'hooks/useTickets'
+import { getBalanceNumber } from 'utils/formatBalance'
 import CardValue from 'components/Card/CardValue'
 
 const CakeWinnings = () => {
   const { claimAmount } = useTotalClaim()
-  return <CardValue value={claimAmount.toNumber()} />
+  return <CardValue value={getBalanceNumber(claimAmount)} />
 }
 
 export default CakeWinnings
