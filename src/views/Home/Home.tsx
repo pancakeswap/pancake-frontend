6d5bfd8070d773802f328c6bbe76206951942dc4
RabbitDoge <72658581+RabbitDoge@users.noreply.github.com>
2020-11-16 17:38:23 +0900

    feat: Add Kanit font

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -67,13 +67,8 @@ const Home: React.FC = () => {
     <Page>
       <Hero>
         <div>
-          <Title as="h1">{TranslateString(999, 'Pancake Swap')}</Title>
-          <Subtitle>
-            {TranslateString(
-              999,
-              'The #1 AMM and yield farm on Binance Smart Chain.',
-            )}
-          </Subtitle>
+          <Title as="h1">{TranslateString(999, 'PancakeSwap')}</Title>
+          <Subtitle>{TranslateString(999, 'The #1 AMM and yield farm on Binance Smart Chain.')}</Subtitle>
         </div>
       </Hero>
       <Container>
