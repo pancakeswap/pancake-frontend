bb5f07e146da1e9fb5744d6603e822c3b8ffeeb4
RabbitDoge <72658581+RabbitDoge@users.noreply.github.com>
2020-11-16 18:28:28 +0900

    fix(hook): Clean the auth hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { useWallet } from 'use-wallet'
 import { ResetCSS, Footer } from '@pancakeswap-libs/uikit'
 import GlobalStyle from './style/Global'
 import Menu from './components/Menu'
@@ -11,53 +12,54 @@ import Lottery from './views/Lottery'
 import Voting from './views/Voting'
 import Syrup2 from './views/CakeStaking'
 import ComingSoon from './views/Ifo/ComingSoon'
-import Providers from './Providers'
-import Web3ReactManager from './components/Web3ReactManager'
 
 const App: React.FC = () => {
+  const { account, connect } = useWallet()
+  useEffect(() => {
+    if (!account && window.localStorage.getItem('accountStatus')) {
+      connect('injected')
+    }
+  }, [account, connect])
+
   return (
-    <Providers>
-      <Router>
-        <ResetCSS />
-        <GlobalStyle />
-        <Menu />
-        <Web3ReactManager>
-          <Switch>
-            <Route path="/" exact>
-              <Home />
-            </Route>
-            <Route exact path="/farms">
-              <Farms removed={false} />
-            </Route>
-            <Route path="/farms/:farmId">
-              <Farm />
-            </Route>
-            <Route path="/staking">
-              <Stake />
-            </Route>
-            <Route path="/syrup">
-              <Syrup2 />
-            </Route>
-            <Route path="/lottery">
-              <Lottery />
-            </Route>
-            <Route path="/voting">
-              <Voting />
-            </Route>
-            <Route path="/staking2">
-              <Syrup2 />
-            </Route>
-            <Route path="/removed">
-              <Farms removed />
-            </Route>
-            <Route path="/ifo">
-              <ComingSoon />
-            </Route>
-          </Switch>
-        </Web3ReactManager>
-      </Router>
+    <Router>
+      <ResetCSS />
+      <GlobalStyle />
+      <Menu />
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route exact path="/farms">
+          <Farms removed={false} />
+        </Route>
+        <Route path="/farms/:farmId">
+          <Farm />
+        </Route>
+        <Route path="/staking">
+          <Stake />
+        </Route>
+        <Route path="/syrup">
+          <Syrup2 />
+        </Route>
+        <Route path="/lottery">
+          <Lottery />
+        </Route>
+        <Route path="/voting">
+          <Voting />
+        </Route>
+        <Route path="/staking2">
+          <Syrup2 />
+        </Route>
+        <Route path="/removed">
+          <Farms removed />
+        </Route>
+        <Route path="/ifo">
+          <ComingSoon />
+        </Route>
+      </Switch>
       <Footer />
-    </Providers>
+    </Router>
   )
 }
 
