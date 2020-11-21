c2f2ca59d8a1a5ff82fc4bd7e75fd0c002358c42
Richard Phillips-Kerr <r.phillips.kerr@gmail.com>
2020-11-21 21:59:00 +0000

    style(lottery-prizes-card): Additional styling of total prizes card

--- src/views/Lottery/components/TotalPrizesCard.tsx
+++ src/views/Lottery/components/TotalPrizesCard.tsx
@@ -8,8 +8,9 @@ import useFarmsWithBalance from 'hooks/useFarmsWithBalance'
 import UnlockButton from 'components/UnlockButton'
 
 const CardHeading = styled.div`
+  position: relative;
   display: flex;
-  margin-bottom: 24px;
+  padding-bottom: 24px;
 `
 
 const IconWrapper = styled.div`
@@ -25,16 +26,35 @@ const PrizeCountWrapper = styled.div`
   flex-direction: column;
 `
 
+const Divider = styled.div`
+  position: absolute;
+  bottom: 0;
+  background-color: ${({ theme }) => theme.colors.borderColor};
+  height: 1px;
+  width: 100%;
+`
+
+const PrizeGrid = styled.div`
+  padding-top: 24px;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(4, 1fr);
+`
+
+const RightAlignedText = styled(Text)`
+  text-align: right;
+`
+
+const RightAlignedHeading = styled(Heading)`
+  text-align: right;
+`
+
 const Block = styled.div`
   margin-bottom: 16px;
 `
 
 const Value = styled.div`
   margin-bottom: 8px;
-`
-
-const Actions = styled.div`
-  margin-top: 24px;
 `
 
 const FarmedStakingCard = () => {
@@ -54,12 +74,22 @@ const FarmedStakingCard = () => {
             </Text>
             <Heading size="lg">100,000 {TranslateString(0, 'CAKE')}</Heading>
           </PrizeCountWrapper>
+          <Divider />
         </CardHeading>
-        <Block>
-          <Value>200</Value>
-          <Text>{TranslateString(999, 'CAKE to Harvest')}</Text>
-        </Block>
-        <Actions>{account ? <span>Account</span> : <UnlockButton fullWidth />}</Actions>
+        <PrizeGrid>
+          <Text fontSize="14px" color="textSubtle">
+            No. Matched
+          </Text>
+          <RightAlignedText fontSize="14px" color="textSubtle">
+            Win Chance
+          </RightAlignedText>
+          <RightAlignedText fontSize="14px" color="textSubtle">
+            Prize Pot
+          </RightAlignedText>
+          <Heading size="md">4</Heading>
+          <RightAlignedHeading size="md">0.001%</RightAlignedHeading>
+          <RightAlignedHeading size="md">60,000</RightAlignedHeading>
+        </PrizeGrid>
       </CardBody>
     </Card>
   )
