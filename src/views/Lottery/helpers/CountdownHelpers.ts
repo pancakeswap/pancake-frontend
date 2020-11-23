ed08e01e75d1610595dc1297c34513fa70800bd0
Richard Phillips-Kerr <r.phillips.kerr@gmail.com>
2020-11-23 12:11:35 +0000

    fix(countdown-helpers): Fix step-getting logic

--- src/views/Lottery/helpers/CountdownHelpers.ts
+++ src/views/Lottery/helpers/CountdownHelpers.ts
@@ -3,6 +3,9 @@ const getMinutes = (msTimeValue) => Math.floor((msTimeValue % 3600000) / 60000)
 const getHours = (msTimeValue) => Math.floor((msTimeValue % (3600000 * 24)) / 3600000)
 const hoursAndMinutesString = (hours, minutes) => `${parseInt(hours)}h, ${parseInt(minutes)}m`
 const getNextTicketSaleTime = (currentTime) => (parseInt(currentTime / 3600000) + 1) * 3600000
+// lottery is every 6 hrs (21600000 ms)
+// so they are at 00:00, 06:00, 12:00, 18:00
+// break the current time into chunks of 6hrs (/ 21600000), add one more 6hr unit, multiply by 6hrs to get it back to current timex
 const getNextLotteryDrawTime = (currentTime) => (parseInt(currentTime / 21600000) + 1) * 21600000
 
 export const getTicketSaleTime = (currentTime): string => {
@@ -14,9 +17,6 @@ export const getTicketSaleTime = (currentTime): string => {
 }
 
 export const getLotteryDrawTime = (currentTime): string => {
-  // lottery is every 6 hrs (21600000 ms)
-  // so they are at 00:00, 06:00, 12:00, 18:00
-  // break the current time into chunks of 6hrs (/ 21600000), add one more 6hr unit, multiply by 6hrs to get it back to current timex
   const nextLotteryDrawTime = getNextLotteryDrawTime(currentTime)
   const timeUntilLotteryDraw = nextLotteryDrawTime - currentTime
   const minutes = getMinutes(timeUntilLotteryDraw)
@@ -24,16 +24,11 @@ export const getLotteryDrawTime = (currentTime): string => {
   return hoursAndMinutesString(hours, minutes)
 }
 
-export const getTicketSaleStep = (currentTime) => {
-  const msBetweenLotteries = 21600000
-  const endTime = getNextTicketSaleTime(currentTime)
-  const msUntilTicketSale = endTime - currentTime
-  return (msUntilTicketSale / msBetweenLotteries) * 100
-}
+export const getTicketSaleStep = () => (3600000 / 21600000) * 100
 
 export const getLotteryDrawStep = (currentTime) => {
   const msBetweenLotteries = 21600000
   const endTime = getNextLotteryDrawTime(currentTime)
   const msUntilLotteryDraw = endTime - currentTime
-  return (msUntilLotteryDraw / msBetweenLotteries) * 100
+  return msBetweenLotteries / msUntilLotteryDraw - 1
 }
