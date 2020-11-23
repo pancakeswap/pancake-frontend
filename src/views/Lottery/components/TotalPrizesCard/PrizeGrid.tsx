c22d2d7852db8121c99ce51fdb580a2928e3ed5f
Richard Phillips-Kerr <r.phillips.kerr@gmail.com>
2020-11-23 12:46:42 +0000

    style(prize-grid): Fixing styles for grid

--- src/views/Lottery/components/TotalPrizesCard/PrizeGrid.tsx
+++ src/views/Lottery/components/TotalPrizesCard/PrizeGrid.tsx
@@ -6,7 +6,7 @@ const Grid = styled.div`
   padding-top: 24px;
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: repeat(4, 1fr);
+  grid-template-rows: repeat(4, auto);
 `
 
 const RightAlignedText = styled(Text)`
@@ -17,8 +17,8 @@ const RightAlignedHeading = styled(Heading)`
   text-align: right;
 `
 
-const GridItem = styled.div`
-  margin-bottom: 10px;
+const GridItem = styled.div<{ marginBottom?: string }>`
+  margin-bottom: ${(props) => (props.marginBottom ? props.marginBottom : '10px')};
 `
 
 const PrizeGrid = () => {
@@ -39,6 +39,7 @@ const PrizeGrid = () => {
           Prize Pot
         </RightAlignedText>
       </GridItem>
+      {/* 4 matches row */}
       <GridItem>
         <Heading size="md">4</Heading>
       </GridItem>
@@ -48,18 +49,33 @@ const PrizeGrid = () => {
       <GridItem>
         <RightAlignedHeading size="md">60,000</RightAlignedHeading>
       </GridItem>
+      {/* 3 matches row */}
       <GridItem>
         <Text bold>3</Text>
       </GridItem>
       <GridItem>
-        <RightAlignedText fontSize="14px" color="textSubtle">
-          0.36%
-        </RightAlignedText>
+        <RightAlignedText>0.36%</RightAlignedText>
       </GridItem>
       <GridItem>
-        <RightAlignedText fontSize="14px" color="textSubtle">
-          Prize Pot
-        </RightAlignedText>
+        <RightAlignedText>38,000</RightAlignedText>
+      </GridItem>
+      {/* 2 matches row */}
+      <GridItem marginBottom="20px">
+        <Text>2</Text>
+      </GridItem>
+      <GridItem marginBottom="20px">
+        <RightAlignedText>4.86%</RightAlignedText>
+      </GridItem>
+      <GridItem marginBottom="20px">
+        <RightAlignedText>1,000</RightAlignedText>
+      </GridItem>
+      {/* Burn row */}
+      <GridItem marginBottom="0">
+        <Text>To burn:</Text>
+      </GridItem>
+      <GridItem marginBottom="0" />
+      <GridItem marginBottom="0">
+        <RightAlignedText>1,000</RightAlignedText>
       </GridItem>
     </Grid>
   )
