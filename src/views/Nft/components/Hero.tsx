4792caea107e99cd443145a2cc0a2a2671d6d6b3
hachiojidev <hachiojidev@protonmail.com>
2020-11-26 03:48:12 +0900

    feat(nft): Status cards

--- src/views/Nft/components/Hero.tsx
+++ src/views/Nft/components/Hero.tsx
@@ -1,43 +1,45 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Heading, Text } from '@pancakeswap-libs/uikit'
+import { Button, Heading, Text } from '@pancakeswap-libs/uikit'
 import Container from 'components/layout/Container'
 import useI18n from 'hooks/useI18n'
 
 const StyledHero = styled.div`
+  border-bottom: 1px solid ${({ theme }) => theme.colors.textSubtle};
+  padding-bottom: 32px;
   padding-top: 32px;
 `
+
+const Cta = styled.div`
+  align-items: center;
+  display: flex;
+
+  & > a + a {
+    margin-left: 16px;
+  }
+`
+
 const Hero = () => {
   const TranslateString = useI18n()
 
   return (
-    <StyledHero>
-      <Container>
+    <Container>
+      <StyledHero>
         <Heading as="h1" size="xxl" color="secondary" mb="24px">
           {TranslateString(999, 'NFTs')}
         </Heading>
-        <Heading as="h2" size="lg" color="secondary" mb="24px">
-          {TranslateString(999, 'Claim an NFT, then trade it in for CAKE or HODL it!')}
+        <Heading as="h2" size="lg" color="secondary" mb="16px">
+          {TranslateString(999, 'Trade in for CAKE, or keep for your collection!')}
         </Heading>
-        <Text mb="16px">
-          {TranslateString(
-            999,
-            'If you’ve been randomly selected for a chance to win an NFT, you’ll be able to claim one NFT below.',
-          )}
-        </Text>
-        <Text mb="16px">
-          {TranslateString(999, 'Trade it in: Burn your NFT and receive CAKE')}
-          <br />
-          {TranslateString(999, 'HODL it: Just keep your NFT for fun and collection')}
-        </Text>
-        <Text mb="16px">
-          {TranslateString(
-            999,
-            'But choose quickly! NFTs can only be traded in until the time stated on the card. Only a few wallets get a chance to claim an NFT for this series, so if you’ve been selected, choose wisely!',
-          )}
-        </Text>
-      </Container>
-    </StyledHero>
+        <Text mb="24px">{TranslateString(999, 'Register your interest in winning an NFT below.')}</Text>
+        <Cta>
+          <Button as="a">{TranslateString(999, 'Register for a chance to win')}</Button>
+          <Button as="a" variant="secondary">
+            {TranslateString(999, 'Learn more')}
+          </Button>
+        </Cta>
+      </StyledHero>
+    </Container>
   )
 }
 
--- src/views/Nft/index.tsx
+++ src/views/Nft/index.tsx
@@ -1,68 +1,34 @@
 import React from 'react'
-import styled from 'styled-components'
-import { BaseLayout, Heading, Text } from '@pancakeswap-libs/uikit'
-import useI18n from 'hooks/useI18n'
 import Page from 'components/layout/Page'
 import Container from 'components/layout/Container'
 import Hero from './components/Hero'
-import Menu from './components/Menu'
-import NftCard from './components/NftCard'
-import InfoRow from './components/InfoRow'
-import NftLink from './components/NftLink'
-
-const Grid = styled(BaseLayout)`
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-gap: 32px;
-  margin: 0 auto 32px;
-
-  ${({ theme }) => theme.mediaQueries.sm} {
-    grid-template-columns: repeat(3, 1fr);
-  }
-`
-
-const Header = styled.div`
-  align-items: center;
-  display: flex;
-  margin-bottom: 32px;
-
-  & > div {
-    flex: 1;
-  }
-`
+import HowItWorks from './components/HowItWorks'
+import PleaseWaitCard from './components/PleaseWaitCard'
+import NoNftsToClaimCard from './components/NoNftsToClaimCard'
+import YouWonCard from './components/YouWonCard'
+import NftInWalletCard from './components/NftInWalletCard'
 
 const Nft = () => {
-  const seriesCount = 1
-  const TranslateString = useI18n()
-
   return (
     <Page>
       <Hero />
-      <Container>
-        <Menu />
-        <Header>
-          <div>
-            <Heading size="xl">{TranslateString(999, `Series ${seriesCount}`)}</Heading>
+      <div style={{ paddingBottom: '32px', paddingTop: '32px' }}>
+        <Container>
+          <div style={{ marginBottom: '16px' }}>
+            <PleaseWaitCard />
           </div>
-          <div>
-            <InfoRow>
-              <Text>{TranslateString(999, 'Total NFTs in series')}:</Text>
-              <Text>
-                <strong>500</strong>
-              </Text>
-            </InfoRow>
-            <InfoRow>
-              <Text>{TranslateString(999, 'Time left to trade in NFTs')}:</Text>
-              <NftLink href="https://pancakeswap.finance">100 blocks left</NftLink>
-            </InfoRow>
+          <div style={{ marginBottom: '16px' }}>
+            <NoNftsToClaimCard />
           </div>
-        </Header>
-        <Grid>
-          <div>
-            <NftCard />
+          <div style={{ marginBottom: '16px' }}>
+            <YouWonCard />
           </div>
-        </Grid>
-      </Container>
+          <div style={{ marginBottom: '16px' }}>
+            <NftInWalletCard />
+          </div>
+        </Container>
+      </div>
+      <HowItWorks />
     </Page>
   )
 }
