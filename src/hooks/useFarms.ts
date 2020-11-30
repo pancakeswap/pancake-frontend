a5d98dcd8cb117b2bb02b3b4e76a655078b9ca10
RabbitDoge <72658581+RabbitDoge@users.noreply.github.com>
2020-11-30 14:26:18 +0900

    chore: Remove farm provider

--- src/hooks/useFarms.ts
+++ src/hooks/useFarms.ts
@@ -1,9 +1,10 @@
-import { useContext } from 'react'
-import { Context as FarmsContext } from '../contexts/Farms'
+import useSushi from 'hooks/useSushi'
+import { getFarms } from 'sushi/utils'
+import { Farm } from 'types/farms'
 
-const useFarms = () => {
-  const { farms } = useContext(FarmsContext)
-  return [farms]
+const useFarms = (): Farm[] => {
+  const sushi = useSushi()
+  return getFarms(sushi)
 }
 
 export default useFarms
