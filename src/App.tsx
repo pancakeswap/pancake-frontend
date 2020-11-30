cc323a036992b2e41c06263f8bdf875eb94af992
hachiojidev <hachiojidev@protonmail.com>
2020-12-01 03:05:47 +0900

    feat: 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import Stake from './views/Stake'
 import Lottery from './views/Lottery'
 import Pools from './views/Pools'
 import Ifo from './views/Ifo'
+import NotFound from './views/NotFound'
 
 const App: React.FC = () => {
   const { account, connect } = useWallet()
@@ -50,6 +51,7 @@ const App: React.FC = () => {
         <Route path="/ifo">
           <Ifo />
         </Route>
+        <Route component={NotFound} />
       </Switch>
       <Footer />
     </Router>
