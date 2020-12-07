49b33bb46bb5bdc085799f0c09fdbeb30c276c5e
RabbitDoge <72658581+RabbitDoge@users.noreply.github.com>
2020-12-07 12:08:03 +0900

    chore: Remove unused abi

--- src/utils/erc20.ts
+++ src/utils/erc20.ts
@@ -2,11 +2,11 @@ import Web3 from 'web3'
 import { provider as ProviderType } from 'web3-core'
 import { Contract } from 'web3-eth-contract'
 import { AbiItem } from 'web3-utils'
-import ERC20ABI from '../config/abi/ERC20.json'
+import erc20 from 'sushi/lib/abi/erc20.json'
 
 export const getContract = (provider: ProviderType, address: string) => {
   const web3 = new Web3(provider)
-  const contract = new web3.eth.Contract((ERC20ABI.abi as unknown) as AbiItem, address)
+  const contract = new web3.eth.Contract((erc20 as unknown) as AbiItem, address)
   return contract
 }
 
