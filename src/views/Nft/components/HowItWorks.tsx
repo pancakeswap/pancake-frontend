23fa501204d52fccecc7f2bc5444008098e171ae
hachiojidev <hachiojidev@protonmail.com>
2020-12-07 17:43:07 +0900

    chore: Use icon from UI Kit

--- src/views/Nft/components/HowItWorks.tsx
+++ src/views/Nft/components/HowItWorks.tsx
@@ -1,7 +1,7 @@
 // @ts-nocheck
 import React from 'react'
 import styled from 'styled-components'
-import { Button, Heading, Text as UIKitText } from '@pancakeswap-libs/uikit'
+import { Button, Heading, Text as UIKitText, ArrowForwardIcon } from '@pancakeswap-libs/uikit'
 import useI18n from 'hooks/useI18n'
 import Container from 'components/layout/Container'
 
@@ -29,16 +29,6 @@ const Row = styled.div`
   margin-bottom: 16px;
 `
 
-// TODO: use the one from the UI Kit
-const ArrowForward = () => (
-  <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path
-      d="M5 13.0022H16.17L11.29 17.8822C10.9 18.2722 10.9 18.9122 11.29 19.3022C11.68 19.6922 12.31 19.6922 12.7 19.3022L19.29 12.7122C19.68 12.3222 19.68 11.6922 19.29 11.3022L12.71 4.70217C12.32 4.31217 11.69 4.31217 11.3 4.70217C10.91 5.09217 10.91 5.72217 11.3 6.11217L16.17 11.0022H5C4.45 11.0022 4 11.4522 4 12.0022C4 12.5522 4.45 13.0022 5 13.0022Z"
-      fill="#7645D9"
-    />
-  </svg>
-)
-
 const HowItWorks = () => {
   const TranslateString = useI18n()
 
@@ -51,7 +41,7 @@ const HowItWorks = () => {
           </Heading>
           <Row>
             <Icon>
-              <ArrowForward />
+              <ArrowForwardIcon />
             </Icon>
             <Text>
               {TranslateString(
@@ -62,7 +52,7 @@ const HowItWorks = () => {
           </Row>
           <Row>
             <Icon>
-              <ArrowForward />
+              <ArrowForwardIcon />
             </Icon>
             <Text>
               {TranslateString(999, 'If you’re not selected, you won’t be able to claim. Better luck next time!')}
@@ -70,7 +60,7 @@ const HowItWorks = () => {
           </Row>
           <Row>
             <Icon>
-              <ArrowForward />
+              <ArrowForwardIcon />
             </Icon>
             <Text>
               {TranslateString(
@@ -86,7 +76,7 @@ const HowItWorks = () => {
           </Heading>
           <Row>
             <Icon>
-              <ArrowForward />
+              <ArrowForwardIcon />
             </Icon>
             <Text>{TranslateString(999, 'Winners are selected at random! Good luck!')}</Text>
           </Row>
