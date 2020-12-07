afb9fc6b978792aa8554b9cf244b9ee213b934cd
Richard Phillips-Kerr <r.phillips.kerr@gmail.com>
2020-12-07 20:56:25 +0000

    chore(lottery): Base setup for PastLotteryDraws components

---
+++ src/views/Lottery/components/PastLotteryRoundViewer/PastLotterySearcher.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import useI18n from 'hooks/useI18n'
+
+const PastLotterySearcher = () => {
+  const TranslateString = useI18n()
+
+  return <span>Search area</span>
+}
+
+export default PastLotterySearcher
