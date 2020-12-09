8f78d12a39f2c38af61e235faabdda6d188bcbf9
hachiojidev <hachiojidev@protonmail.com>
2020-12-09 13:08:07 +0900

    fix(nft): Multiple instances of contract addresses

--- src/views/Nft/utils/contracts.ts
+++ src/views/Nft/utils/contracts.ts
@@ -3,9 +3,7 @@ import { getContract } from 'utils/web3'
 import { ContractOptions } from 'web3-eth-contract'
 import rabbitmintingfarm from 'sushi/lib/abi/rabbitmintingfarm.json'
 import pancakeRabbits from 'sushi/lib/abi/pancakeRabbits.json'
-
-export const RABBIT_MINTING_FARM_ADDRESS = '0xE4E3AFeDA68C740D49273b35d406AE1582d6b49A'
-export const PANCAKE_RABBITS_ADDRESS = '0xA286716A5eB2d3F890E0c917Dc7c2e23d3BA70a5'
+import { RABBIT_MINTING_FARM_ADDRESS, PANCAKE_RABBITS_ADDRESS } from 'sushi/lib/constants/nfts'
 
 export const getRabbitMintingContract = (contractOptions?: ContractOptions) => {
   const rabbitMintingFarmAbi = (rabbitmintingfarm as unknown) as AbiItem
