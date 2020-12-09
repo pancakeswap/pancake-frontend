64edf7a2094faa049788d0580b99880f62bf67d9
RabbitDoge <72658581+RabbitDoge@users.noreply.github.com>
2020-12-09 18:25:50 +0900

    Farms calls (#186)
    * chore: Clean the farm components
    
    * perf: Batch Farm page calls
    
    * fix: Use CardBody and remove relative paths

--- src/views/Farm/Farm.tsx
+++ src/views/Farm/Farm.tsx
@@ -1,4 +1,5 @@
-import React, { useMemo } from 'react'
+import React, { useMemo, useState, useEffect } from 'react'
+import BigNumber from 'bignumber.js'
 import { useParams } from 'react-router-dom'
 import styled from 'styled-components'
 import { useWallet } from 'use-wallet'
@@ -6,18 +7,86 @@ import { provider } from 'web3-core'
 import { getContract } from 'utils/erc20'
 import useI18n from 'hooks/useI18n'
 import Page from 'components/layout/Page'
+import multicall from 'utils/multicall'
 import { useFarmLP } from 'contexts/DataContext'
+import erc20ABI from 'sushi/lib/abi/erc20.json'
+import masterchefABI from 'sushi/lib/abi/masterchef.json'
+import contractsAdresses from 'sushi/lib/constants/contracts'
 import Harvest from './components/Harvest'
 import Stake from './components/Stake'
 import DualFarmDisclaimer from './components/DualFarmDisclaimer'
 
+interface UserFarm {
+  allowance?: BigNumber
+  tokenBalance: BigNumber
+  stakedBalance: BigNumber
+  earnings: BigNumber
+}
+
+const useUserFarm = (lpContractAddress, pid): UserFarm => {
+  const [state, setState] = useState({
+    allowance: null,
+    tokenBalance: new BigNumber(0),
+    stakedBalance: new BigNumber(0),
+    earnings: new BigNumber(0),
+  })
+  const { account } = useWallet()
+  useEffect(() => {
+    const fetch = async () => {
+      const masterChefAdress = contractsAdresses.masterChef[process.env.REACT_APP_CHAIN_ID]
+      const [allowance, tokenBalance] = await multicall(erc20ABI, [
+        // Allowance
+        {
+          address: lpContractAddress,
+          name: 'allowance',
+          params: [account, masterChefAdress],
+        },
+        // LP token balance on user account
+        {
+          address: lpContractAddress,
+          name: 'balanceOf',
+          params: [account],
+        },
+      ])
+      const [userInfo, earnings] = await multicall(masterchefABI, [
+        // stakedBalance
+        {
+          address: masterChefAdress,
+          name: 'userInfo',
+          params: [pid, account],
+        },
+        // earnings
+        {
+          address: masterChefAdress,
+          name: 'pendingCake',
+          params: [pid, account],
+        },
+      ])
+
+      setState({
+        allowance: new BigNumber(allowance),
+        tokenBalance: new BigNumber(tokenBalance),
+        // eslint-disable-next-line no-underscore-dangle
+        stakedBalance: new BigNumber(new BigNumber(userInfo[0]._hex).toNumber()),
+        earnings: new BigNumber(earnings),
+      })
+    }
+    if (account && lpContractAddress) {
+      fetch()
+    }
+  }, [account, lpContractAddress, pid])
+
+  return state
+}
+
 const Farm: React.FC = () => {
   const TranslateString = useI18n()
   const { ethereum } = useWallet()
-  const { farmId } = useParams<{ farmId?: string }>()
+  const { lpSymbol } = useParams<{ lpSymbol?: string }>()
 
-  const { pid, lpSymbol, lpAddresses, tokenSymbol, dual } = useFarmLP(farmId)
+  const { pid, lpAddresses, tokenSymbol, dual } = useFarmLP(lpSymbol)
   const lpAddress = lpAddresses[process.env.REACT_APP_CHAIN_ID]
+  const { allowance, tokenBalance, stakedBalance, earnings } = useUserFarm(lpAddress, pid)
 
   const lpContract = useMemo(() => {
     return getContract(ethereum as provider, lpAddress)
@@ -32,8 +101,15 @@ const Farm: React.FC = () => {
       </Header>
       <StyledFarm>
         <Grid>
-          <Harvest pid={pid} />
-          <Stake lpContract={lpContract} pid={pid} tokenName={lpSymbol.toUpperCase()} />
+          <Harvest pid={pid} earnings={earnings} />
+          <Stake
+            lpContract={lpContract}
+            pid={pid}
+            tokenName={lpSymbol.toUpperCase()}
+            allowance={allowance}
+            tokenBalance={tokenBalance}
+            stakedBalance={stakedBalance}
+          />
         </Grid>
         {dual ? (
           <DualFarmDisclaimer tokenName={tokenSymbol} endBlock={dual.endBlock} />
