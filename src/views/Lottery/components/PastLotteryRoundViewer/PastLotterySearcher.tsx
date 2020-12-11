8620f584c3ff49a731ca57b7e2ad34e9ac56341c
RichardPK <r.phillips.kerr@gmail.com>
2020-12-11 15:24:53 +0000

    style(pasty-lottery-searcher): Button and input for lottery search

--- src/views/Lottery/components/PastLotteryRoundViewer/PastLotterySearcher.tsx
+++ src/views/Lottery/components/PastLotteryRoundViewer/PastLotterySearcher.tsx
@@ -1,18 +1,48 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
-import { Text } from '@pancakeswap-libs/uikit'
+import { Text, Input, Button } from '@pancakeswap-libs/uikit'
 import useI18n from 'hooks/useI18n'
+
+/* eslint-disable no-debugger */
 
 const Wrapper = styled.div`
   margin-bottom: 24px;
 `
 
+const SearchWrapper = styled.div`
+  position: relative;
+`
+
+const StyledButton = styled(Button)`
+  position: absolute;
+  right: 8px;
+  top: 50%;
+  transform: translate(0%, -50%);
+`
+
 const PastLotterySearcher = () => {
   const TranslateString = useI18n()
+  const [inputNumber, setInputNumber] = useState(200)
+
+  const onSubmit = () => {
+    console.log(inputNumber)
+  }
 
   return (
     <Wrapper>
       <Text>Select lottery number:</Text>
+      <SearchWrapper>
+        <Input
+          value={inputNumber}
+          type="number"
+          onChange={(event) => {
+            setInputNumber(parseInt(event.currentTarget.value))
+          }}
+        />
+        <StyledButton size="sm" onClick={onSubmit}>
+          Search
+        </StyledButton>
+      </SearchWrapper>
     </Wrapper>
   )
 }
