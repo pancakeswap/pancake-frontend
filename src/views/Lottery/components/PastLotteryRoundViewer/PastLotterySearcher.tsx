410a89fcb0a073e02ab26d0e81e6c253cf2fc23a
RichardPK <r.phillips.kerr@gmail.com>
2020-12-11 12:46:22 +0000

    style(pasty-lottery-searcher): V initial styles

--- src/views/Lottery/components/PastLotteryRoundViewer/PastLotterySearcher.tsx
+++ src/views/Lottery/components/PastLotteryRoundViewer/PastLotterySearcher.tsx
@@ -1,11 +1,20 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
+import { Text } from '@pancakeswap-libs/uikit'
 import useI18n from 'hooks/useI18n'
+
+const Wrapper = styled.div`
+  margin-bottom: 24px;
+`
 
 const PastLotterySearcher = () => {
   const TranslateString = useI18n()
 
-  return <span>Search area</span>
+  return (
+    <Wrapper>
+      <Text>Select lottery number:</Text>
+    </Wrapper>
+  )
 }
 
 export default PastLotterySearcher
