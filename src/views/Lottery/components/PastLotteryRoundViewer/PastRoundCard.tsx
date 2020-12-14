875d57256d2740994078b1d92961fc56873c60f5
RichardPK <r.phillips.kerr@gmail.com>
2020-12-14 19:50:52 +0000

    feat(past-round-card): Create PastLotteryActions card

--- src/views/Lottery/components/PastLotteryRoundViewer/PastRoundCard.tsx
+++ src/views/Lottery/components/PastLotteryRoundViewer/PastRoundCard.tsx
@@ -3,12 +3,17 @@ import styled from 'styled-components'
 import { Heading, Card, CardBody, CardFooter, Text, PancakeRoundIcon, TicketRound } from '@pancakeswap-libs/uikit'
 import useI18n from 'hooks/useI18n'
 import LotteryCardHeading from '../LotteryCardHeading'
+import PastLotteryActions from './PastLotteryActions'
 
 const CardHeading = styled.div`
   position: relative;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+`
+
+const StyledHeading = styled(Heading)`
+  margin-bottom: 24px;
 `
 
 const TopLotteryCardHeading = styled(LotteryCardHeading)`
@@ -22,16 +27,18 @@ const PastRoundCard = () => {
     <Card>
       <CardBody>
         <CardHeading>
-          <TopLotteryCardHeading valueToDisplay="1, 2, 3, 4" Icon={TicketRound}>
-            Winning numbers
+          <StyledHeading size="md">Round #51</StyledHeading>
+          <TopLotteryCardHeading valueToDisplay={TranslateString(999, '1, 2, 3, 4')} Icon={TicketRound}>
+            {TranslateString(999, 'Winning numbers')}
           </TopLotteryCardHeading>
-          <LotteryCardHeading valueToDisplay="100,000 CAKE" Icon={PancakeRoundIcon}>
-            Total prizes
+          <LotteryCardHeading valueToDisplay={TranslateString(999, '100,000 CAKE')} Icon={PancakeRoundIcon}>
+            {TranslateString(999, 'Total prizes')}
           </LotteryCardHeading>
         </CardHeading>
       </CardBody>
       <CardFooter>
         <span>Grid</span>
+        <PastLotteryActions />
       </CardFooter>
     </Card>
   )
