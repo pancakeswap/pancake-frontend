0d60365b0705520e9efecf89cc5eeb606909fb13
RichardPK <r.phillips.kerr@gmail.com>
2020-12-14 00:41:30 +0000

    Fix winnings balance display & progress timer for morning SGT timezone (#201)
    * fix(prizes-won-content): Return balance to 2 decimal places
    
    * fix(countdown-helpers): Further tweak to nextLotteryIsTomorrowLogic

--- src/views/Lottery/helpers/CountdownHelpers.ts
+++ src/views/Lottery/helpers/CountdownHelpers.ts
@@ -20,7 +20,7 @@ const getNextLotteryDrawTime = (currentMillis) => {
   const nextLotteryHour = getClosestLotteryHour(currentHour)
   // next lottery is tomorrow if the next lottery is at 2am UTC...
   // ...and current time is between 02:00am & 23:59pm UTC
-  const nextLotteryIsTomorrow = nextLotteryHour === 2 && currentHour >= 1 && currentHour <= 23
+  const nextLotteryIsTomorrow = nextLotteryHour === 2 && currentHour >= 2 && currentHour <= 23
   let millisTimeOfNextDraw
 
   if (nextLotteryIsTomorrow) {
