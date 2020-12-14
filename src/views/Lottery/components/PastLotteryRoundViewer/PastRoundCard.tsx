530a2644c9b442fc3fbb67aec9675f4d59790307
RichardPK <r.phillips.kerr@gmail.com>
2020-12-14 19:12:46 +0000

    feat(past-round-card): Create reusable LotteryCardHeading component

--- src/views/Lottery/components/PastLotteryRoundViewer/PastRoundCard.tsx
+++ src/views/Lottery/components/PastLotteryRoundViewer/PastRoundCard.tsx
@@ -2,11 +2,17 @@ import React, { useState } from 'react'
 import styled from 'styled-components'
 import { Heading, Card, CardBody, CardFooter, Text, PancakeRoundIcon, TicketRound } from '@pancakeswap-libs/uikit'
 import useI18n from 'hooks/useI18n'
+import LotteryCardHeading from '../LotteryCardHeading'
 
 const CardHeading = styled.div`
   position: relative;
   display: flex;
+  flex-direction: column;
   justify-content: space-between;
+`
+
+const TopLotteryCardHeading = styled(LotteryCardHeading)`
+  margin-bottom: 24px;
 `
 
 const PastRoundCard = () => {
@@ -16,7 +22,12 @@ const PastRoundCard = () => {
     <Card>
       <CardBody>
         <CardHeading>
-          <span>Round</span>
+          <TopLotteryCardHeading valueToDisplay="1, 2, 3, 4" Icon={TicketRound}>
+            Winning numbers
+          </TopLotteryCardHeading>
+          <LotteryCardHeading valueToDisplay="100,000 CAKE" Icon={PancakeRoundIcon}>
+            Total prizes
+          </LotteryCardHeading>
         </CardHeading>
       </CardBody>
       <CardFooter>
