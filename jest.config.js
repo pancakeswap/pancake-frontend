4eaa1114d304333a6081d1e38ee041a524212e3c
hachiojidev <72719739+hachiojidev@users.noreply.github.com>
2020-12-16 11:04:13 +0900

    Feature/remove colorbox (#94)
    * build: Removed color box
    
    * fix: Add Jest ignore patterns

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
 module.exports = {
   preset: "ts-jest",
+  testPathIgnorePatterns: ["/node_modules/", "/dist/", "/.storybook/"],
 };
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ export * from "./components/Button";
 export { default as ButtonMenu } from "./components/ButtonMenu";
 export { default as ButtonMenuItem } from "./components/ButtonMenu/ButtonMenuItem";
 export { default as Checkbox } from "./components/Checkbox";
-export { default as ColorBox } from "./components/ColorBox";
 export { default as Flex } from "./components/Flex";
 export { default as Dropdown } from "./components/Dropdown";
 export { default as Heading } from "./components/Heading";
