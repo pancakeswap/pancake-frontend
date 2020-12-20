c1f4743c05ee57146077cc0e50928af060b6e8a0
emmaodia <odia.emma@gmail.com>
2020-12-20 15:29:59 +0100

    feat: Build Earn APY CTA card with navigation to farms

--- src/views/Home/components/EarnAPYCard.tsx
+++ src/views/Home/components/EarnAPYCard.tsx
@@ -1,19 +1,15 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Heading, Card, CardBody, Svg } from '@pancakeswap-libs/uikit'
-import { useWallet } from 'use-wallet'
+import { Heading, Card, CardBody, ArrowForwardIcon } from '@pancakeswap-libs/uikit'
+import { NavLink } from 'react-router-dom'
 import useI18n from 'hooks/useI18n'
-import { useAllReward } from 'hooks/useReward'
-import useFarmsWithBalance from 'hooks/useFarmsWithBalance'
-import UnlockButton from 'components/UnlockButton'
-import CakeHarvestBalance from './CakeHarvestBalance'
-import CakeWalletBalance from './CakeWalletBalance'
 
 const StyledFarmStakingCard = styled(Card)`
   margin-left: auto;
   margin-right: auto;
   max-width: 344px;
   width: 100%;
+  
 
   ${({ theme }) => theme.mediaQueries.lg} {
     margin: 0;
@@ -23,7 +19,10 @@ const StyledFarmStakingCard = styled(Card)`
 const CardMidContent = styled(Heading).attrs({ size: 'xl' })`
   margin-bottom: 0px;
 `
-
+const Row = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`
 
 const FarmedStakingCard = () => {
     const TranslateString = useI18n() 
@@ -35,8 +34,13 @@ const FarmedStakingCard = () => {
         <div>Earn up to</div>
         <CardMidContent>{TranslateString(999, '293% APY')}</CardMidContent>
         <div>in Farms</div>
-        <Svg />
+        <NavLink exact activeClassName="active" to="/farms">
+          <Row> 
+            <ArrowForwardIcon />
+          </Row>
+        </NavLink>
       </CardBody>
+      
     </StyledFarmStakingCard>
   )
 }
