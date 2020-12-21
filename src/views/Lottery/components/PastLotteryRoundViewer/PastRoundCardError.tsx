248e269a8b99a6522c44953a2495d5d447ca51c2
RichardPK <r.phillips.kerr@gmail.com>
2020-12-21 19:54:43 +0000

    style(past-round-car-error): Use p for pixel values

--- src/views/Lottery/components/PastLotteryRoundViewer/PastRoundCardError.tsx
+++ src/views/Lottery/components/PastLotteryRoundViewer/PastRoundCardError.tsx
@@ -2,12 +2,8 @@ import React from 'react'
 import styled from 'styled-components'
 import { Text } from '@pancakeswap-libs/uikit'
 
-const StyledText = styled(Text)`
-  padding: ${(props) => props.theme.spacing[4]}px;
-`
-
 const PastRoundCardError = ({ error }) => {
-  return <StyledText>{error.message}</StyledText>
+  return <Text p="24px">{error.message}</Text>
 }
 
 export default PastRoundCardError
