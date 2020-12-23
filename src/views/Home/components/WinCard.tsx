4850601657ab4077decdad6d37529d06436d90df
emmaodia <odia.emma@gmail.com>
2020-12-23 15:11:49 +0100

    feat: Win Lottery, cake value parsed to Card Component

--- src/views/Home/components/WinCard.tsx
+++ src/views/Home/components/WinCard.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import styled from 'styled-components'
 import { Heading, Card, CardBody, ArrowForwardIcon } from '@pancakeswap-libs/uikit'
 import { NavLink } from 'react-router-dom'
+import { getBalanceNumber } from 'utils/formatBalance'
+import { useTotalRewards } from 'hooks/useTickets'
 import useI18n from 'hooks/useI18n'
 
 const StyledFarmStakingCard = styled(Card)`
@@ -25,13 +27,14 @@ const Row = styled.div`
 
 const FarmedStakingCard = () => {
     const TranslateString = useI18n() 
-   
+    const lotteryPrizeAmount = +getBalanceNumber(useTotalRewards()).toFixed(0)
+    const lotteryPrizeWithCommaSeparators = lotteryPrizeAmount.toLocaleString()
 
   return (
     <StyledFarmStakingCard>
       <CardBody>
         <div>Win up to</div>
-        <CardMidContent>{TranslateString(999, '293% APY')}</CardMidContent>
+        <CardMidContent>{lotteryPrizeWithCommaSeparators} {TranslateString(999, 'CAKE')}</CardMidContent>
         <div>in Lottery</div>
         <NavLink exact activeClassName="active" to="/lottery">
           <Row> 
