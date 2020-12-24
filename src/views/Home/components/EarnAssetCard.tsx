160ed91bc37bf609a916dae523351e936ec4b595
emmaodia <odia.emma@gmail.com>
2020-12-24 14:34:15 +0100

    feat: List Pool Assets idone. Completed UI Styling

--- src/views/Home/components/EarnAssetCard.tsx
+++ src/views/Home/components/EarnAssetCard.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import styled from 'styled-components'
 import { Heading, Card, CardBody, ArrowForwardIcon } from '@pancakeswap-libs/uikit'
 import { NavLink } from 'react-router-dom'
-import useI18n from 'hooks/useI18n'
 import { poolsConfig } from 'sushi/lib/constants'
 
 const StyledFarmStakingCard = styled(Card)`
@@ -12,44 +11,48 @@ const StyledFarmStakingCard = styled(Card)`
   max-width: 344px;
   width: 100%;
   
-
   ${({ theme }) => theme.mediaQueries.lg} {
     margin: 0;
     max-width: none;
   }
 `
+
+const CardTitle = styled(Heading)`
+  display: flex;
+  margin-bottom: 0px;
+  color: #191326;
+`
+
 const CardMidContent = styled(Heading).attrs({ size: 'xl' })`
   display: flex;
   margin-bottom: 0px;
   color: white;
   font-size: 35px
 `
+const CardFooter = styled(Heading)`
+  display: flex;
+  color: #191326;
+`
 const Row = styled.div`
   display: flex;
   justify-content: flex-end;
   color: #1FC7D4, 100%
 `
-const CardAsset = styled.div`
-  display: flex;
-`
 
 const FarmedStakingCard = () => {
-    const TranslateString = useI18n() 
-const check = poolsConfig.slice(0, 4)
-console.log(`here: ${check.slice(0, 4).map(token => token.tokenName)}`)
-
-const seee = check.map(token => token.tokenName)
+  
+  const check = poolsConfig.slice(3, 7)
+    
+  const result = check.map(token => 
+    `${token.tokenName}, `
+  ) 
 
   return (
     <StyledFarmStakingCard>
       <CardBody>
-        <div>Earn</div>
-        <CardMidContent>{
-          check.map(token => 
-            `${token.tokenName}, `
-          ) 
-        }</CardMidContent>
-        <div>in Pools</div>
+        <CardTitle>Earn</CardTitle>
+        <CardMidContent>{result}</CardMidContent>
+        <CardFooter>in Pools</CardFooter>
         <NavLink exact activeClassName="active" to="/syrup">
           <Row> 
             <ArrowForwardIcon />
