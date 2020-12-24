8538a9cd716a3a77df88f4fdd7841fdae6c5a20d
emmaodia <odia.emma@gmail.com>
2020-12-24 12:04:10 +0100

    feat: WIP - Earn Assets CTA

--- src/views/Home/components/EarnAssetCard.tsx
+++ src/views/Home/components/EarnAssetCard.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import { Heading, Card, CardBody, ArrowForwardIcon } from '@pancakeswap-libs/uikit'
 import { NavLink } from 'react-router-dom'
 import useI18n from 'hooks/useI18n'
+import { poolsConfig } from 'sushi/lib/constants'
 
 const StyledFarmStakingCard = styled(Card)`
   background: linear-gradient(#53DEE9, #7645D9);
@@ -18,23 +19,36 @@ const StyledFarmStakingCard = styled(Card)`
   }
 `
 const CardMidContent = styled(Heading).attrs({ size: 'xl' })`
+  display: flex;
   margin-bottom: 0px;
-  color: white
+  color: white;
+  font-size: 35px
 `
 const Row = styled.div`
   display: flex;
   justify-content: flex-end;
   color: #1FC7D4, 100%
 `
+const CardAsset = styled.div`
+  display: flex;
+`
 
 const FarmedStakingCard = () => {
     const TranslateString = useI18n() 
+const check = poolsConfig.slice(0, 4)
+console.log(`here: ${check.slice(0, 4).map(token => token.tokenName)}`)
+
+const seee = check.map(token => token.tokenName)
 
   return (
     <StyledFarmStakingCard>
       <CardBody>
         <div>Earn</div>
-        <CardMidContent>{TranslateString(999, '293% APY')}</CardMidContent>
+        <CardMidContent>{
+          check.map(token => 
+            `${token.tokenName}, `
+          ) 
+        }</CardMidContent>
         <div>in Pools</div>
         <NavLink exact activeClassName="active" to="/syrup">
           <Row> 
