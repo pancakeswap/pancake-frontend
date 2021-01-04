6f0d3f0f9af4a363ff27a2e72de002f09ca64bc4
hachiojidev <hachiojidev@protonmail.com>
2021-01-04 12:10:46 +0900

    feat(lottery): Update menu to use ButtonMenu

--- src/views/Lottery/Lottery.tsx
+++ src/views/Lottery/Lottery.tsx
@@ -1,37 +1,43 @@
 import React, { useState, useEffect } from 'react'
-import { Switch } from 'react-router-dom'
+import styled from 'styled-components'
+import { ButtonMenu, ButtonMenuItem } from '@pancakeswap-libs/uikit'
+import { useWallet } from 'use-wallet'
+import PastLotteryDataContext from 'contexts/PastLotteryDataContext'
+import { getLotteryContract, getLotteryIssueIndex } from 'sushi/lotteryUtils'
+import useI18n from 'hooks/useI18n'
+import useSushi from 'hooks/useSushi'
 import Page from 'components/layout/Page'
 import Container from 'components/layout/Container'
-import PastLotteryDataContext from 'contexts/PastLotteryDataContext'
-import useSushi from 'hooks/useSushi'
-import { useWallet } from 'use-wallet'
-import { getLotteryContract, getLotteryIssueIndex } from 'sushi/lotteryUtils'
 import Hero from './components/Hero'
 import Divider from './components/Divider'
-import LotteryPageToggle from './components/LotteryPageToggle'
 import NextDrawPage from './NextDrawPage'
 import PastDrawsPage from './PastDrawsPage'
+
+const Wrapper = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 32px;
+`
 
 const Lottery: React.FC = () => {
   const sushi = useSushi()
   const lotteryContract = getLotteryContract(sushi)
   const { account } = useWallet()
-
-  const [nextDrawActive, setNextDrawActive] = useState(true)
+  const TranslateString = useI18n()
+  const [activeIndex, setActiveIndex] = useState(0)
   const [historyData, setHistoryData] = useState([])
   const [historyError, setHistoryError] = useState(false)
   const [mostRecentLotteryNumber, setMostRecentLotteryNumber] = useState(1)
 
   useEffect(() => {
-    const getHistoryChartData = () => {
-      fetch(`https://api.pancakeswap.com/api/lotteryHistory`)
-        .then((response) => response.json())
-        .then((data) => setHistoryData(data))
-        .catch(() => {
-          setHistoryError(true)
-        })
-    }
-    getHistoryChartData()
+    fetch(`https://api.pancakeswap.com/api/lotteryHistory`)
+      .then((response) => response.json())
+      .then((data) => setHistoryData(data))
+      .catch(() => {
+        setHistoryError(true)
+      })
   }, [])
 
   useEffect(() => {
@@ -46,19 +52,28 @@ const Lottery: React.FC = () => {
     }
   }, [account, lotteryContract, sushi])
 
+  const handleClick = (index) => {
+    setActiveIndex(index)
+  }
+
   return (
-    <Switch>
+    <>
       <Page>
         <Hero />
         <Container>
-          <LotteryPageToggle nextDrawActive={nextDrawActive} setNextDrawActive={setNextDrawActive} />
+          <Wrapper>
+            <ButtonMenu activeIndex={activeIndex} onClick={handleClick} size="sm">
+              <ButtonMenuItem>{TranslateString(999, 'Next draw')}</ButtonMenuItem>
+              <ButtonMenuItem>{TranslateString(999, 'Past draws')}</ButtonMenuItem>
+            </ButtonMenu>
+          </Wrapper>
           <Divider />
           <PastLotteryDataContext.Provider value={{ historyError, historyData, mostRecentLotteryNumber }}>
-            {nextDrawActive ? <NextDrawPage /> : <PastDrawsPage />}
+            {activeIndex === 0 ? <NextDrawPage /> : <PastDrawsPage />}
           </PastLotteryDataContext.Provider>
         </Container>
       </Page>
-    </Switch>
+    </>
   )
 }
 
