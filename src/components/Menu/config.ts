d772d61bbacfdb7f8725333c3ff82b18f28b249a
RabbitDoge <72658581+RabbitDoge@users.noreply.github.com>
2021-01-07 10:26:13 +0900

    feat: New menu integration (#263)

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,94 @@
+import { MenuEntry } from '@pancakeswap-libs/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: '/',
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    items: [
+      {
+        label: 'Exchange',
+        href: 'https://exchange.pancakeswap.finance',
+      },
+      {
+        label: 'Liquidity',
+        href: 'https://exchange.pancakeswap.finance/#/pool',
+      },
+    ],
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: '/farms',
+  },
+  {
+    label: 'Pools',
+    icon: 'PoolIcon',
+    href: '/syrup',
+  },
+  {
+    label: 'Lottery',
+    icon: 'TicketIcon',
+    href: '/lottery',
+  },
+  {
+    label: 'NFT',
+    icon: 'NftIcon',
+    href: '/nft',
+  },
+  {
+    label: 'Info',
+    icon: 'InfoIcon',
+    items: [
+      {
+        label: 'Overview',
+        href: 'https://pancakeswap.info',
+      },
+      {
+        label: 'Tokens',
+        href: 'https://pancakeswap.info/tokens',
+      },
+      {
+        label: 'Pairs',
+        href: 'https://pancakeswap.info/pairs',
+      },
+      {
+        label: 'Accounts',
+        href: 'https://pancakeswap.info/accounts',
+      },
+    ],
+  },
+  {
+    label: 'IFO',
+    icon: 'IfoIcon',
+    href: '/ifo',
+  },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: 'Voting',
+        href: 'https://voting.pancakeswap.finance',
+      },
+      {
+        label: 'Github',
+        href: 'https://github.com/pancakeswap',
+      },
+      {
+        label: 'Docs',
+        href: 'https://docs.pancakeswap.finance',
+      },
+      {
+        label: 'Blog',
+        href: 'https://pancakeswap.medium.com',
+      },
+    ],
+  },
+]
+
+export default config
