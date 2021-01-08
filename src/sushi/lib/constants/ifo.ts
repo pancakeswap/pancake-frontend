7d5dc2584dcb30f6e7ee353061931126b3b09f53
Chef-Chungus <71833681+Chef-Chungus@users.noreply.github.com>
2021-01-08 09:54:19 +0900

    Helmet ifo announcement (#267)
    * helmet-ifo-announcement
    
    * add ifo images
    
    * update address
    
    * update url scheme

--- src/sushi/lib/constants/ifo.ts
+++ src/sushi/lib/constants/ifo.ts
@@ -42,7 +42,7 @@ const ifos: Ifo[] = [
   {
     id: 'tenet',
     address: '0xB922aA19A2603A07C6C9ED6c236618C9bac51f06',
-    isActive: true,
+    isActive: false,
     name: 'Tenet (TEN)',
     subTitle: 'Cross-chain Automated Market Maker',
     description:
@@ -57,6 +57,25 @@ const ifos: Ifo[] = [
     currencyAddress: '0xA527a61703D82139F8a06Bc30097cC9CAA2df5A6',
     tokenDecimals: 18,
     releaseBlockNumber: 3483883,
+  },
+  {
+    id: 'helmet',
+    address: '0xa32509d760ee47Eb2Be96D338b5d69B5FBa4eFEB',
+    isActive: true,
+    name: 'Helmet.insure (Helmet)',
+    subTitle: 'Crypto-assets insurance trading platform',
+    description:
+      'Helmet is a peer-to-peer insurance protocol written with options trading logic, which allows anyone to create any insurance policy easily in the market. Helmet aims to enable DeFi users to be protected against the risk of price fluctuations by providing price-shield insurance for BSC assets. They provide suppliers with a range of insurance types to sell, giving a variety of hedging strategies to the market.',
+    launchDate: 'Jan. 13',
+    launchTime: '5PM SGT',
+    saleAmount: '10,000,000 Helmet',
+    raiseAmount: '$1,000,000',
+    cakeToBurn: '$500,000',
+    projectSiteUrl: 'https://www.helmet.insure/',
+    currency: 'CAKE-BNB LP',
+    currencyAddress: '0xA527a61703D82139F8a06Bc30097cC9CAA2df5A6',
+    tokenDecimals: 18,
+    releaseBlockNumber: 3771926,
   },
 ]
 
