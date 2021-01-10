de40c7c4d1f932d1f705fb1338dea59219c288b2
RichardPK <r.phillips.kerr@gmail.com>
2021-01-10 16:11:41 +0000

    feat(details-section): Added OpenNew icon to Farm card details

--- src/views/Farms/components/FarmCard/DetailsSection.tsx
+++ src/views/Farms/components/FarmCard/DetailsSection.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import useI18n from 'hooks/useI18n'
 import styled from 'styled-components'
-import { Text, Flex, Link } from '@pancakeswap-libs/uikit'
+import { Text, Flex, Link, OpenNewIcon } from '@pancakeswap-libs/uikit'
 
 export interface ExpandableSectionProps {
   bscScanAddress?: string
@@ -14,6 +14,21 @@ const Wrapper = styled.div`
   margin-top: 24px;
 `
 
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  font-weight: normal;
+  color: ${({ theme }) => theme.colors.text};
+  display: flex;
+  align-items: center;
+
+  svg {
+    padding-left: 4px;
+    height: 18px;
+    width: auto;
+    fill: ${({ theme }) => theme.colors.primary};
+  }
+`
+
 const DetailsSection: React.FC<ExpandableSectionProps> = ({ bscScanAddress, removed, totalValueFormated, lpLabel }) => {
   const TranslateString = useI18n()
 
@@ -21,7 +36,9 @@ const DetailsSection: React.FC<ExpandableSectionProps> = ({ bscScanAddress, remo
     <Wrapper>
       <Flex justifyContent="space-between">
         <Text>{TranslateString(316, 'Stake')}:</Text>
-        <Text>{lpLabel}</Text>
+        <StyledLink external href="https://exchange.pancakeswap.finance/#/pool">
+          {lpLabel} <OpenNewIcon />
+        </StyledLink>
       </Flex>
       {!removed && (
         <Flex justifyContent="space-between">
