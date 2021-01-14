da6dd4468a471cb8835bd1db44bf61beb6ce8591
hachiojidev <hachiojidev@protonmail.com>
2021-01-14 11:04:27 +0900

    feat: Redux alert system

--- src/state/index.ts
+++ src/state/index.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
+import alertsReducer from './alerts'
 import farmsReducer from './farms'
 import poolsReducer from './pools'
 
 export default configureStore({
   devTools: process.env.NODE_ENV !== 'production',
   reducer: {
+    alerts: alertsReducer,
     farms: farmsReducer,
     pools: poolsReducer,
   },
