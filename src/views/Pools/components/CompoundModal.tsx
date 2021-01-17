eb7be8b95792e521c0c707b808103a4258c84df8
Chef_Chungus <cakechefchungus@protonmail.com>
2021-01-17 12:18:26 +0900

    chore:add id for compound button

--- src/views/Pools/components/CompoundModal.tsx
+++ src/views/Pools/components/CompoundModal.tsx
@@ -34,7 +34,7 @@ const CompoundModal: React.FC<DepositModalProps> = ({ earnings, onConfirm, onDis
           {TranslateString(462, 'Cancel')}
         </Button>
         <Button
-          id="compound_cake"
+          id="compound-cake"
           fullWidth
           disabled={pendingTx}
           onClick={async () => {
