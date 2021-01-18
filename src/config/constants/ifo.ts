507c53d056bfa865f5f1a98bf76863316f2656ce
Chef-Chungus <71833681+Chef-Chungus@users.noreply.github.com>
2021-01-18 15:44:01 +0900

    chore: add WSOTE information (#312)
    * chore: add WSOTE config
    
    * chore: add images
    
    * chore: add contract address

--- src/config/constants/ifo.ts
+++ src/config/constants/ifo.ts
@@ -77,6 +77,25 @@ const ifos: Ifo[] = [
     tokenDecimals: 18,
     releaseBlockNumber: 3771926,
   },
+  {
+    id: 'soteria',
+    address: '0x9C21123D94b93361a29B2C2EFB3d5CD8B17e0A9e',
+    isActive: true,
+    name: 'Soteria',
+    subTitle: 'A blockchain based mutual to share the risk of DeFi on Binance Smart Chain',
+    description:
+      'Soteria allows users to buy insurance cover to greatly diminish or reduce the financial risk of hacks. It replaces the idea of a traditional insurance company, because it is wholly owned by the members. The model encourages engagement as members receive economic incentives for participating in Risk Assessment, Claims Assessment and Governance.',
+    launchDate: 'Jan. 21',
+    launchTime: '4PM SGT',
+    saleAmount: '1,500,000 wSOTE',
+    raiseAmount: '$525,00',
+    cakeToBurn: '$212,000',
+    projectSiteUrl: 'https://soteria.finance/#/',
+    currency: 'CAKE-BNB LP',
+    currencyAddress: '0xA527a61703D82139F8a06Bc30097cC9CAA2df5A6',
+    tokenDecimals: 18,
+    releaseBlockNumber: 4086064,
+  },
 ]
 
 export default ifos
