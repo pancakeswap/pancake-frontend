f13b0638c3f198683b165acfa8976a929aab2cd2
RichardPK <r.phillips.kerr@gmail.com>
2021-01-19 18:25:51 +0000

    chore(get-liquidity-url-path-parts): Improve comment

--- src/utils/getLiquidityUrlPathParts.ts
+++ src/utils/getLiquidityUrlPathParts.ts
@@ -1,4 +1,5 @@
 // Constructing the two forward-slash-separated parts of the 'Add Liquidity' URL
+// Each part of the url represents a different side of the LP pair.
 // In the URL, using the quote token 'BNB' is represented by 'ETH'
 const getLiquidityUrlPathParts = ({ quoteTokenAdresses, quoteTokenSymbol, tokenAddresses }) => {
   const chainId = process.env.REACT_APP_CHAIN_ID
