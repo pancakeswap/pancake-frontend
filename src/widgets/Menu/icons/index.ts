4f7406e02ed18d3dd179098bda54bf59af25747d
hachiojidev <72719739+hachiojidev@users.noreply.github.com>
2021-01-20 11:03:29 +0900

    feat(menu): Groups icon (#144)

--- src/widgets/Menu/icons/index.ts
+++ src/widgets/Menu/icons/index.ts
@@ -1,4 +1,5 @@
 export { default as FarmIcon } from "./Farm";
+export { default as GroupsIcon } from "./Groups";
 export { default as HamburgerIcon } from "./Hamburger";
 export { default as HamburgerCloseIcon } from "./HamburgerClose";
 export { default as HomeIcon } from "./Home";
