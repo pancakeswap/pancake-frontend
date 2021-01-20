82ed6bfd9b77788202eaa15f2b760f7ce6c7e5d3
hachiojidev <72719739+hachiojidev@users.noreply.github.com>
2021-01-20 14:21:49 +0900

    feat: Toasts (#323)

--- src/state/index.ts
+++ src/state/index.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import farmsReducer from './farms'
+import toastsReducer from './toasts'
 import poolsReducer from './pools'
 
 export default configureStore({
   devTools: process.env.NODE_ENV !== 'production',
   reducer: {
     farms: farmsReducer,
+    toasts: toastsReducer,
     pools: poolsReducer,
   },
 })
