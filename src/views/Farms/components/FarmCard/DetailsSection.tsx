cb9708c5202b4d14de1301883cbe1d78bdbbd7a9
RichardPK <r.phillips.kerr@gmail.com>
2021-01-20 14:18:06 +0000

    chore(details-section): Remove unused consts

--- src/views/Farms/components/FarmCard/DetailsSection.tsx
+++ src/views/Farms/components/FarmCard/DetailsSection.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import useI18n from 'hooks/useI18n'
 import styled from 'styled-components'
-import { Text, Flex, Link, OpenNewIcon, LinkExternal } from '@pancakeswap-libs/uikit'
+import { Text, Flex, Link, LinkExternal } from '@pancakeswap-libs/uikit'
 import getLiquidityUrlPathParts from 'utils/getLiquidityUrlPathParts'
 
 export interface TokenAddressesObject {
