cc539bc22c9fd50add7f7043d25ba56dd14e8458
hachiojidev <72719739+hachiojidev@users.noreply.github.com>
2021-01-21 11:26:49 +0900

    feat(teams): Profile creation step 1 work (#331)
    * feat(teams): Profile creation step 1 work
    
    * chore(profile): Restructured routes

---
+++ src/views/Profile/ProfileCreation/Mint.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { Card, CardBody, Heading, Text } from '@pancakeswap-libs/uikit'
+import useI18n from 'hooks/useI18n'
+import NftSelectionCard from '../components/NftSelectionCard'
+
+const nft = {
+  name: 'Swapsies',
+  description: 'These bunnies love nothing more than swapping pancakes. Especially on BSC.',
+  originalImage: 'https://gateway.pinata.cloud/ipfs/QmXdHqg3nywpNJWDevJQPtkz93vpfoHcZWQovFz2nmtPf5/swapsies.png',
+  previewImage: 'swapsies-preview.png',
+  blurImage: 'swapsies-blur.png',
+  sortOrder: 999,
+  bunnyId: 0,
+}
+
+const nft1 = {
+  name: 'Drizzle',
+  description: "It's raining syrup on this bunny, but he doesn't seem to mind. Can you blame him?",
+  originalImage: 'https://gateway.pinata.cloud/ipfs/QmXdHqg3nywpNJWDevJQPtkz93vpfoHcZWQovFz2nmtPf5/drizzle.png',
+  previewImage: 'drizzle-preview.png',
+  blurImage: 'drizzle-blur.png',
+  sortOrder: 999,
+  bunnyId: 1,
+}
+
+const Mint = () => {
+  const TranslateString = useI18n()
+
+  return (
+    <>
+      <Text fontSize="20px" color="textSubtle" bold>
+        {TranslateString(999, `Step ${1}`)}
+      </Text>
+      <Heading as="h3" size="xl" mb="24px">
+        {TranslateString(999, 'Get Starter Collectible')}
+      </Heading>
+      <Text as="p">{TranslateString(999, 'Every profile starts by making a “starter” collectible (NFT).')}</Text>
+      <Text as="p">{TranslateString(999, 'This starter will also become your first profile picture.')}</Text>
+      <Text as="p" mb="24px">
+        {TranslateString(999, 'You can change your profile pic later if you get another approved Pancake Collectible.')}
+      </Text>
+      <Card>
+        <CardBody>
+          <Heading as="h4" size="lg" mb="8px">
+            {TranslateString(999, 'Choose your Starter!')}
+          </Heading>
+          <Text as="p" color="textSubtle">
+            {TranslateString(999, 'Choose wisely: you can only ever make one starter collectible!')}
+          </Text>
+          <Text as="p" mb="24px" color="textSubtle">
+            {TranslateString(999, 'Cost: 5 CAKE')}
+          </Text>
+          <NftSelectionCard nft={nft} />
+          <NftSelectionCard nft={nft1} isChecked />
+        </CardBody>
+      </Card>
+    </>
+  )
+}
+
+export default Mint
