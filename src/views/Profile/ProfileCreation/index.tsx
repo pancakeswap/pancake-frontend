d5132f8a2d6a76094d31860db3ac43cc0be0c6c0
hachiojidev <72719739+hachiojidev@users.noreply.github.com>
2021-01-21 11:26:49 +0900

    feat(teams): Profile creation step 1 work (#331)
    * feat(teams): Profile creation step 1 work
    
    * chore(profile): Restructured routes

---
+++ src/views/Profile/ProfileCreation/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Page from 'components/layout/Page'
+import Mint from './Mint'
+import Header from './Header'
+
+const ProfileCreation = () => {
+  return (
+    <Page>
+      <Header />
+      <Mint />
+    </Page>
+  )
+}
+
+export default ProfileCreation
