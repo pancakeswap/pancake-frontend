dac7d76bf1952e7c6be17447719d3eccfefb3422
RichardPK <r.phillips.kerr@gmail.com>
2021-01-24 19:43:29 +0000

    style(apy-modal): Range of elements styled

--- src/views/Farms/components/FarmCard/ApyCalculatorModal.tsx
+++ src/views/Farms/components/FarmCard/ApyCalculatorModal.tsx
@@ -1,17 +1,105 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Modal, Heading } from '@pancakeswap-libs/uikit'
+import { Modal, Text, LinkExternal, Flex } from '@pancakeswap-libs/uikit'
+import useI18n from 'hooks/useI18n'
 
 interface DepositModalProps {
   onDismiss?: () => void
 }
 
-const ApyButton: React.FC<DepositModalProps> = ({ onDismiss }) => {
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(4, auto);
+  margin-bottom: 24px;
+`
+
+const GridItem = styled.div`
+  margin-bottom: '10px';
+`
+
+const Description = styled(Text)`
+  max-width: 320px;
+  margin-bottom: 28px;
+`
+
+const ApyCalculatorModal: React.FC<DepositModalProps> = ({ onDismiss }) => {
+  const TranslateString = useI18n()
+  //   const liquidityUrlPathParts = getLiquidityUrlPathParts({ quoteTokenAdresses, quoteTokenSymbol, tokenAddresses })
+  const liquidityUrlPathParts = 'bleh'
+
   return (
-    <Modal title="bleh" onDismiss={onDismiss}>
-      <Heading>This does nothing</Heading>
+    <Modal title="ROI" onDismiss={onDismiss}>
+      <Grid>
+        <GridItem>
+          <Text fontSize="12px" bold color="textSubtle" textTransform="uppercase" mb="20px">
+            {TranslateString(999, 'Timeframe')}
+          </Text>
+        </GridItem>
+        <GridItem>
+          <Text fontSize="12px" bold color="textSubtle" textTransform="uppercase" mb="20px">
+            {TranslateString(999, 'ROI')}
+          </Text>
+        </GridItem>
+        <GridItem>
+          <Text fontSize="12px" bold color="textSubtle" textTransform="uppercase" mb="20px">
+            {TranslateString(999, 'CAKE per $1000')}
+          </Text>
+        </GridItem>
+        {/* 1 day row */}
+        <GridItem>
+          <Text>1d</Text>
+        </GridItem>
+        <GridItem>
+          <Text>1</Text>
+        </GridItem>
+        <GridItem>
+          <Text>1</Text>
+        </GridItem>
+        {/* 7 day row */}
+        <GridItem>
+          <Text>7d</Text>
+        </GridItem>
+        <GridItem>
+          <Text>7</Text>
+        </GridItem>
+        <GridItem>
+          <Text>7</Text>
+        </GridItem>
+        {/* 30 day row */}
+        <GridItem>
+          <Text>30d</Text>
+        </GridItem>
+        <GridItem>
+          <Text>30</Text>
+        </GridItem>
+        <GridItem>
+          <Text>30</Text>
+        </GridItem>
+        {/* 365 day / APY row */}
+        <GridItem>
+          <Text>365d(APY)</Text>
+        </GridItem>
+        <GridItem>
+          <Text>365</Text>
+        </GridItem>
+        <GridItem>
+          <Text>365</Text>
+        </GridItem>
+      </Grid>
+      <Description fontSize="12px" color="textSubtle">
+        {TranslateString(
+          999,
+          'Calculated based on current rates. Compounding once daily. Rates are estimates provided for your convenience only, and by no means represent guaranteed returns.',
+        )}
+      </Description>
+      <Flex justifyContent="center">
+        <LinkExternal href={`https://exchange.pancakeswap.finance/#/add/${liquidityUrlPathParts}`}>
+          Get BLEH-BNB LP
+        </LinkExternal>
+      </Flex>
     </Modal>
   )
 }
 
-export default ApyButton
+export default ApyCalculatorModal
