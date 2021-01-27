bb053db82efe6f0bb467834cef0d19a202899670
Chef_Chungus <cakechefchungus@protonmail.com>
2021-01-27 12:32:35 +0900

    fix: Issue with string

--- src/views/Farms/components/FarmCard/FarmCard.tsx
+++ src/views/Farms/components/FarmCard/FarmCard.tsx
@@ -134,7 +134,7 @@ const FarmCard: React.FC<FarmCardProps> = ({ farm, removed, cakePrice, bnbPrice,
       />
       {!removed && (
         <Flex justifyContent="space-between">
-          <Text>{TranslateString(352, '736')}:</Text>
+          <Text>{TranslateString(736, 'APR')}:</Text>
           <Text bold>{farm.apy ? `${farmAPY}%` : 'Loading ...'}</Text>
         </Flex>
       )}
