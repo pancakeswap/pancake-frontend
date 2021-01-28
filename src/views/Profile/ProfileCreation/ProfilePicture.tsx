b48f3733e45f33ee76e87b94d1a4f695643e97c8
RabbitDoge <72658581+RabbitDoge@users.noreply.github.com>
2021-01-28 11:39:50 +0900

    fix: Profile creation (#355)

--- src/views/Profile/ProfileCreation/ProfilePicture.tsx
+++ src/views/Profile/ProfileCreation/ProfilePicture.tsx
@@ -50,6 +50,7 @@ const ProfilePicture: React.FC = () => {
             ) : (
               walletNfts.map((walletNft) => (
                 <NftSelectionCard
+                  key={walletNft.bunnyId}
                   nft={walletNft}
                   isChecked={walletNft.bunnyId === bunnyId}
                   onChange={(value: string) => setBunnyId(parseInt(value, 10))}
