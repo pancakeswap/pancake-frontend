45dc8352c248a1ca5395e76630a3c9c05ff5cf2c
RichardPK <r.phillips.kerr@gmail.com>
2021-01-28 03:16:54 +0000

    Farm APY calculator button + modal (#334)
    * style(apy-button): Added apy button comp
    
    * feat(apy-button): Implement onDismiss
    
    * style(apy-modal): Range of elements styled
    
    * feat(apy-modal): Passing props for LP link construction
    
    * feat(apy-modal): Passing props for APY calc
    
    * feat(apy-modal): Rough compounding APY helper
    
    * feat(apy-modal): Refine functions for APY modal
    
    * feat(apy-modal): Move into farm.apy ternary
    
    * feat(apy-modal): Remove farm.apy check within apycalc and other lint tidy
    
    * style(apy-modal): Add calculate icon
    
    * chore(apy-modal): Remove Todo
    
    * chore(apy-modal): Use Address const rather than declaring interface each time
    
    * feat(apy-modal): Use Skeleton comp for loading
    
    * fix(apy-modal): Fix compoundApyHelpers logic for principal
    
    * chore(apy-modal): Add comments to compoundApyHelpers file
    
    * chore(apy-modal): Use object to make compoundApy function call more explicity
    
    * test(apy-modal): Add rounding functionality to helper to remove need for toFixed() string conversion
    
    * fix(apy-modal): Fix ROI calc maths
    
    * chore(apy-modal): Improve naming conventions
    
    * test(apy-modal): Add test for apyModalRoi
    
    * chore(apy-modal): Reorg APY button/APY, revert APY > APR merge change

---
+++ src/views/Farms/components/FarmCard/ApyButton.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import BigNumber from 'bignumber.js'
+import { CalculateIcon, IconButton, useModal } from '@pancakeswap-libs/uikit'
+import { Address } from 'config/constants/types'
+import ApyCalculatorModal from './ApyCalculatorModal'
+
+export interface ApyButtonProps {
+  lpLabel?: string
+  cakePrice?: BigNumber
+  apy?: BigNumber
+  quoteTokenAdresses?: Address
+  quoteTokenSymbol?: string
+  tokenAddresses: Address
+}
+
+const ApyButton: React.FC<ApyButtonProps> = ({
+  lpLabel,
+  quoteTokenAdresses,
+  quoteTokenSymbol,
+  tokenAddresses,
+  cakePrice,
+  apy,
+}) => {
+  const [onPresentApyModal] = useModal(
+    <ApyCalculatorModal
+      lpLabel={lpLabel}
+      quoteTokenAdresses={quoteTokenAdresses}
+      quoteTokenSymbol={quoteTokenSymbol}
+      tokenAddresses={tokenAddresses}
+      cakePrice={cakePrice}
+      apy={apy}
+    />,
+  )
+
+  return (
+    <IconButton onClick={onPresentApyModal} variant="text" size="sm" ml="4px">
+      <CalculateIcon />
+    </IconButton>
+  )
+}
+
+export default ApyButton
