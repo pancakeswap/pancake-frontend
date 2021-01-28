ddf14a510c114ed4eac5d75a84ea736788602d47
Guilherme Felipe do Nascimento Reis <guifeliper@gmail.com>
2021-01-28 19:58:37 -0300

    fix: Changing moment to date-fns

--- src/scripts/CMSSync/index.ts
+++ src/scripts/CMSSync/index.ts
@@ -1,70 +1,70 @@
 import fs from 'fs'
-import moment from 'moment'
+import { parseISO, format } from 'date-fns'
 
+import { Ifo, PoolConfig, FarmConfig } from '../../config/constants/types'
 import api from '../api'
 import settings from './settings'
-import { Ifo, PoolConfig, FarmConfig } from '../../config/constants/types'
 import { SettingsObject } from './types'
 
 const getIfos = (data) => {
-  const ifos: Ifo = data.map((obj) => {
-    const lunchTime = moment(obj.launch_datetime)
+  const ifos: Ifo = data.map((ifo) => {
+    const lunchTime2 = parseISO(ifo.launch_datetime)
     return {
-      id: obj._id,
-      isActive: obj.is_active,
-      address: obj.address,
-      name: obj.name,
-      subTitle: obj?.sub_title,
-      description: obj?.description,
-      launchDate: lunchTime.format('YYYY-MM-DD'),
-      launchTime: lunchTime.format('h:mm:ss'),
-      saleAmount: obj.sale_amount,
-      raiseAmount: obj.raise_amount,
-      cakeToBurn: obj.burn_amount,
-      projectSiteUrl: obj.project_url,
-      currency: obj.currency,
-      currencyAddress: obj.currency_address,
-      tokenDecimals: obj.decimals,
-      releaseBlockNumber: obj.__v,
+      id: ifo._id,
+      isActive: ifo.is_active,
+      address: ifo.address,
+      name: ifo.name,
+      subTitle: ifo?.sub_title,
+      description: ifo?.description,
+      launchDate: format(lunchTime2, 'yyyy-MM-dd'),
+      launchTime: format(lunchTime2, 'HH:mm:ss'),
+      saleAmount: ifo.sale_amount,
+      raiseAmount: ifo.raise_amount,
+      cakeToBurn: ifo.burn_amount,
+      projectSiteUrl: ifo.project_url,
+      currency: ifo.currency,
+      currencyAddress: ifo.currency_address,
+      tokenDecimals: ifo.decimals,
+      releaseBlockNumber: ifo.__v,
     }
   })
   return ifos
 }
 
 const getPools = (data) => {
-  const pools: PoolConfig = data.map((obj) => {
+  const pools: PoolConfig = data.map((pool) => {
     return {
-      sousId: obj._id,
+      sousId: pool._id,
       // image: string,
-      tokenName: obj?.token?.name,
-      stakingTokenName: obj?.quote_token?.name,
-      stakingLimit: obj?.quote_token?.decimals,
-      stakingTokenAddress: obj?.quote_token?.mainnet_address,
-      contractAddress: obj.contract_address,
-      poolCategory: obj.category,
-      projectLink: obj.project_url,
-      tokenPerBlock: obj.token_per_block,
-      sortOrder: obj.ranking,
-      harvest: obj.harvest,
-      isFinished: obj.is_finished,
-      tokenDecimals: obj?.token?.decimals,
+      tokenName: pool?.token?.name,
+      stakingTokenName: pool?.quote_token?.name,
+      stakingLimit: pool?.quote_token?.decimals,
+      stakingTokenAddress: pool?.quote_token?.mainnet_address,
+      contractAddress: pool.contract_address,
+      poolCategory: pool.category,
+      projectLink: pool.project_url,
+      tokenPerBlock: pool.token_per_block,
+      sortOrder: pool.ranking,
+      harvest: pool.harvest,
+      isFinished: pool.is_finished,
+      tokenDecimals: pool?.token?.decimals,
     }
   })
   return pools
 }
 
 const getFarms = (data) => {
-  const farms: FarmConfig = data.map((obj) => {
+  const farms: FarmConfig = data.map((farm) => {
     return {
-      pid: obj.pid,
-      lpSymbol: obj.lp_symbol,
-      lpAddresses: obj.lp_mainnet_address,
-      tokenSymbol: obj?.token?.symbol,
-      tokenAddresses: obj?.token?.mainnet_address,
-      quoteTokenSymbol: obj?.quote_token?.symbol,
-      quoteTokenAdresses: obj?.quote_token?.mainnet_address,
-      multiplier: obj?.multiplier,
-      isCommunity: obj?.is_community,
+      pid: farm.pid,
+      lpSymbol: farm.lp_symbol,
+      lpAddresses: farm.lp_mainnet_address,
+      tokenSymbol: farm?.token?.symbol,
+      tokenAddresses: farm?.token?.mainnet_address,
+      quoteTokenSymbol: farm?.quote_token?.symbol,
+      quoteTokenAdresses: farm?.quote_token?.mainnet_address,
+      multiplier: farm?.multiplier,
+      isCommunity: farm?.is_community,
       // dual: {
       //   rewardPerBlock: obj
       //   earnLabel: obj
@@ -94,9 +94,9 @@ const getFormattedData = (type, data) => {
 }
 
 settings.forEach((item: SettingsObject) => {
-  console.log(` ✅ - ${item.name}`)
+  console.info(` ✅ - ${item.name}`)
   api(item.link).then((res) => {
-    const secondKey = Object.keys(res)[1]
+    const [, secondKey] = Object.keys(res)
     const formattedData = getFormattedData(item.type, res[secondKey])
     fs.writeFileSync(`src/config/constants/${item.name}.json`, JSON.stringify(formattedData, undefined, 2))
   })
