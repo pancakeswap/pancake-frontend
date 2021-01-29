7336cc3e7f50e93383427aa744b0f1480e6a7b9d
ChefKai <74599990+ChefKai@users.noreply.github.com>
2021-01-29 11:04:25 +0100

    chore: Fix typo

--- src/views/Lottery/components/PastLotteryRoundViewer/PastLotteryActions.tsx
+++ src/views/Lottery/components/PastLotteryRoundViewer/PastLotteryActions.tsx
@@ -51,7 +51,7 @@ const TicketCard: React.FC<{ contractLink?: string; lotteryNumber?: number }> = 
         </Button>
       </div>
       <div>
-        <ExternalLinkWrap href={contractLink}>{TranslateString(999, 'View on BSCscan')}</ExternalLinkWrap>
+        <ExternalLinkWrap href={contractLink}>{TranslateString(999, 'View on BscScan')}</ExternalLinkWrap>
       </div>
     </Wrapper>
   )
