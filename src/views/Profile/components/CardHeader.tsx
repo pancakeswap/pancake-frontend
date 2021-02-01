04019454193fd9166fa296c9f019efb7e007954c
hachiojidev <hachiojidev@protonmail.com>
2021-02-01 16:19:35 +0900

    style(profile): Updated profile card header background

--- src/views/Profile/components/CardHeader.tsx
+++ src/views/Profile/components/CardHeader.tsx
@@ -1,8 +1,16 @@
-import styled from 'styled-components'
+import styled, { DefaultTheme } from 'styled-components'
 import { CardHeader as UIKitCardHeader } from '@pancakeswap-libs/uikit'
 
+const getBackground = (theme: DefaultTheme) => {
+  if (theme.isDark) {
+    return 'linear-gradient(139.73deg, #142339 0%, #24243D 47.4%, #37273F 100%)'
+  }
+
+  return 'linear-gradient(139.73deg, #E6FDFF 0%, #EFF4F5 46.87%, #F3EFFF 100%)'
+}
+
 const CardHeader = styled(UIKitCardHeader)`
-  background: linear-gradient(139.73deg, #e6fdff 0%, #eff4f5 46.87%, #f3efff 100%);
+  background: ${({ theme }) => getBackground(theme)};
   position: relative;
 `
 
