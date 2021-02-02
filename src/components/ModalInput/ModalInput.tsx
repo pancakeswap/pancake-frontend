dee854252f2b252a3df8e65ef5eccdc9c5358ae8
RichardPK <r.phillips.kerr@gmail.com>
2021-02-02 17:57:45 +0000

    style(modal-input): Haphazard styling of new FE elements

--- src/components/ModalInput/ModalInput.tsx
+++ src/components/ModalInput/ModalInput.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Button } from '@pancakeswap-libs/uikit'
+import { Text, Button, Input, Flex } from '@pancakeswap-libs/uikit'
 import useI18n from '../../hooks/useI18n'
-import Input, { InputProps } from '../Input'
+import { InputProps } from '../Input'
 
 interface ModalInputProps extends InputProps {
   max: number | string
@@ -10,34 +10,53 @@ interface ModalInputProps extends InputProps {
   onSelectMax?: () => void
 }
 
+const StyledTokenInput = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: ${({ theme }) => theme.colors.input};
+  border-radius: 16px;
+  color: ${({ theme }) => theme.colors.text};
+  padding: 8px 16px;
+  width: 100%;
+`
+
+const StyledInput = styled(Input)`
+  box-shadow: none;
+`
+
 const ModalInput: React.FC<ModalInputProps> = ({ max, symbol, onChange, onSelectMax, value }) => {
   const TranslateString = useI18n()
   return (
     <StyledTokenInput>
-      <StyledMaxText>
-        {max.toLocaleString()} {symbol} {TranslateString(526, 'Available')}
-      </StyledMaxText>
-      <Input
-        endAdornment={
-          <StyledTokenAdornmentWrapper>
-            <StyledTokenSymbol>{symbol}</StyledTokenSymbol>
-            <StyledSpacer />
-            <div>
-              <Button size="sm" onClick={onSelectMax}>
-                {TranslateString(452, 'Max')}
-              </Button>
-            </div>
-          </StyledTokenAdornmentWrapper>
-        }
-        onChange={onChange}
-        placeholder="0"
-        value={value}
-      />
+      <Flex justifyContent="space-between">
+        <Text fontSize="14px">{TranslateString(999, 'Deposit')}</Text>
+        <Text fontSize="14px">
+          {TranslateString(999, 'Balance')}: {max.toLocaleString()}
+        </Text>
+      </Flex>
+      <Flex>
+        <StyledInput
+          // endAdornment={
+          //   <StyledTokenAdornmentWrapper>
+          //     <StyledTokenSymbol>{symbol}</StyledTokenSymbol>
+          //     <StyledSpacer />
+          //     <div>
+
+          //     </div>
+          //   </StyledTokenAdornmentWrapper>
+          // }
+          onChange={onChange}
+          placeholder="0"
+          value={value}
+        />
+        <Button size="sm" onClick={onSelectMax}>
+          {TranslateString(452, 'Max')}
+        </Button>
+        <Text fontSize="16px">{symbol}</Text>
+      </Flex>
     </StyledTokenInput>
   )
 }
-
-const StyledTokenInput = styled.div``
 
 const StyledSpacer = styled.div`
   width: ${(props) => props.theme.spacing[3]}px;
