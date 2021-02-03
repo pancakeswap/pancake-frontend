7a630bb2aa34f0d9541a48b155093b02b38eced6
hachiojidev <72719739+hachiojidev@users.noreply.github.com>
2021-02-03 11:43:52 +0900

    feat(teams): Team overview (#373)
    * feat(teams): Team overview
    
    * feat(nft): Add starter descriptions
    
    * feat(team): Team view
    
    * feat(profile): Shuffle team list

---
+++ src/views/Teams/components/TeamCard.tsx
@@ -0,0 +1,101 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Card, CardBody, CommunityIcon, Heading, PrizeIcon } from '@pancakeswap-libs/uikit'
+import CardHeader from 'views/Profile/components/CardHeader'
+import useI18n from 'hooks/useI18n'
+import { Team } from 'state/types'
+import ComingSoon from 'views/Profile/components/ComingSoon'
+import StatBox from 'views/Profile/components/StatBox'
+
+interface TeamCardProps {
+  team: Team
+}
+
+const Wrapper = styled.div`
+  padding-top: 16px;
+
+  ${({ theme }) => theme.mediaQueries.md} {
+    padding-top: 24px;
+  }
+`
+
+const Avatar = styled.img`
+  border-radius: 50%;
+  height: 64px;
+  margin-top: -12px;
+  width: 64px;
+
+  ${({ theme }) => theme.mediaQueries.md} {
+    height: 128px;
+    margin-top: -24px;
+    width: 128px;
+  }
+`
+
+const AvatarWrap = styled.div`
+  margin-bottom: 8px;
+  text-align: center;
+`
+
+const StyledCard = styled(Card)`
+  overflow: visible;
+`
+
+const StyledCardHeader = styled(CardHeader)`
+  border-radius: 32px 32px 0 0;
+  padding-top: 0;
+  text-align: center;
+`
+
+const TeamName = styled(Heading).attrs({ as: 'h1' })`
+  font-size: 24px;
+
+  ${({ theme }) => theme.mediaQueries.md} {
+    font-size: 40px;
+  }
+`
+
+const StatRow = styled.div`
+  display: grid;
+  grid-gap: 16px;
+  grid-template-columns: 1fr;
+  margin-bottom: 16px;
+
+  ${({ theme }) => theme.mediaQueries.md} {
+    grid-gap: 32px;
+    grid-template-columns: repeat(2, 1fr);
+    margin-bottom: 32px;
+  }
+`
+
+const TeamCard: React.FC<TeamCardProps> = ({ team }) => {
+  const TranslateString = useI18n()
+
+  return (
+    <Wrapper>
+      <StyledCard>
+        <StyledCardHeader>
+          <AvatarWrap>
+            <Avatar src="/images/nfts/onsen-preview.png" alt="team avatar" />
+          </AvatarWrap>
+          <TeamName>{team.name}</TeamName>
+        </StyledCardHeader>
+        <CardBody>
+          <StatRow>
+            <StatBox icon={CommunityIcon} title={team.users} subtitle={TranslateString(999, 'Active Members')} />
+            <StatBox
+              icon={PrizeIcon}
+              title={TranslateString(999, 'Coming Soon')}
+              subtitle={TranslateString(999, 'Team Points')}
+              isDisabled
+            />
+          </StatRow>
+          <Heading>{TranslateString(999, 'Team Achievements')}</Heading>
+          <ComingSoon />
+        </CardBody>
+      </StyledCard>
+    </Wrapper>
+  )
+}
+
+export default TeamCard
--- src/views/Teams/index.tsx
+++ src/views/Teams/index.tsx
@@ -1,27 +1,27 @@
 import React from 'react'
-import { Heading, Text } from '@pancakeswap-libs/uikit'
+import { Heading, Skeleton } from '@pancakeswap-libs/uikit'
+import orderBy from 'lodash/orderBy'
+import times from 'lodash/times'
 import Page from 'components/layout/Page'
-import { useProfile } from 'state/hooks'
 import useI18n from 'hooks/useI18n'
-import NoProfileCard from './components/NoProfileCard'
+import useTeams from 'hooks/useTeams'
+import TeamListCard from './components/TeamListCard'
+import TeamHeader from './components/TeamHeader'
 
 const Teams = () => {
   const TranslateString = useI18n()
-  const { isInitialized, profile } = useProfile()
-  const showProfileCallout = isInitialized && !profile
+  const teams = useTeams()
+  const topTeams = orderBy(teams, ['points', 'name'], ['desc', 'asc'])
 
   return (
     <Page>
-      {showProfileCallout && <NoProfileCard />}
-      <Heading size="xxl" color="secondary">
-        {TranslateString(999, 'Teams & Profiles')}
+      <TeamHeader />
+      <Heading size="xl" mb="32px">
+        {TranslateString(999, 'Teams')}
       </Heading>
-      <Text bold>
-        {TranslateString(
-          999,
-          'Show off your stats and collectibles with your unique profile. Team features will be revealed soon!',
-        )}
-      </Text>
+      {teams
+        ? topTeams.map((team, index) => <TeamListCard key={team.name} rank={index + 1} team={team} />)
+        : times(3).map((n) => <Skeleton key={n} height="182px" mb="8px" />)}
     </Page>
   )
 }
