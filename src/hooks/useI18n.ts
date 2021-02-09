e4be19db9373001a6f4e0ebb0089e94b44ac810d
hachiojidev <72719739+hachiojidev@users.noreply.github.com>
2021-02-09 11:25:24 +0900

    Bugfix/translation (#418)
    * fix(i18n): Dynamic variables
    
    * docs: Updated README
    
    * chore: Added data to translation

--- src/hooks/useI18n.ts
+++ src/hooks/useI18n.ts
@@ -1,17 +1,42 @@
 import { useContext } from 'react'
+import { isEmpty } from 'lodash'
 import { TranslationsContext } from '../contexts/Localisation/translationsContext'
-import { getTranslation } from '../utils/translateTextHelpers'
+
+interface ContextData {
+  [key: string]: number | string
+}
 
 const useI18n = () => {
   const { translations } = useContext(TranslationsContext)
 
-  return (translationId: number, fallback: string) => {
-    if (translations[0] === 'error') {
-      return fallback
+  return (translationId: number, fallback: string, data: ContextData = {}) => {
+    const foundTranslation = translations.find((translation) => {
+      return translation.data.stringId === translationId
+    })
+
+    if (foundTranslation) {
+      const { text } = foundTranslation.data
+      const includesVariable = text.includes('%')
+
+      if (includesVariable) {
+        let interpolatedText = text
+
+        // If dynamic tags are found but no data was passed return the fallback
+        if (isEmpty(data)) {
+          return fallback
+        }
+
+        Object.keys(data).forEach((dataKey) => {
+          const templateKey = new RegExp(`%${dataKey}%`, 'g')
+          interpolatedText = interpolatedText.replace(templateKey, data[dataKey])
+        })
+
+        return interpolatedText
+      }
+
+      return text
     }
-    if (translations.length > 0) {
-      return getTranslation(translations, translationId, fallback)
-    }
+
     return fallback
   }
 }
