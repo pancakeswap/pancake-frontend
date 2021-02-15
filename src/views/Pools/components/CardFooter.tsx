bb2ea2018bc0ad482a8b7fdb286034368cf2f0ef
Chef_Chungus <cakechefchungus@protonmail.com>
2021-02-15 10:33:23 +0900

    chore: fix linting errors

--- src/views/Pools/components/CardFooter.tsx
+++ src/views/Pools/components/CardFooter.tsx
@@ -94,7 +94,7 @@ const CardFooter: React.FC<Props> = ({
           <Tag />
         </FlexFull>
         <StyledDetailsButton onClick={handleClick}>
-          {isOpen ? {TranslateString(1066, 'Hide')} : {TranslateString(658, 'Details')}} <Icon />
+          {isOpen ? TranslateString(1066, 'Hide') : TranslateString(658, 'Details')} <Icon />
         </StyledDetailsButton>
       </Row>
       {isOpen && (
