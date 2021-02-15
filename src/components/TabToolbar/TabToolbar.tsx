2c60107cb2af0351ae0171cbddee324bdb811172
me <me@ame.com>
2021-02-15 10:24:38 +0100

    fix: Styled components first

--- src/components/TabToolbar/TabToolbar.tsx
+++ src/components/TabToolbar/TabToolbar.tsx
@@ -4,49 +4,6 @@ import { useRouteMatch, Link } from 'react-router-dom'
 import { ButtonMenu, ButtonMenuItem, Text, Toggle, Input } from '@pancakeswap-libs/uikit'
 import useI18n from 'hooks/useI18n'
 
-interface TabToolbarProps {
-  account: number | string
-  stakedOnly: boolean
-  setStakedOnly: (boolean) => void
-  filter: string
-  setFilter: (boolean) => void
-}
-
-const TabToolbar: React.FC<TabToolbarProps> = ({ account, stakedOnly, setStakedOnly, filter, setFilter }) => {
-  const { url, isExact } = useRouteMatch()
-  const TranslateString = useI18n()
-
-  return (
-    <Wrapper>
-      <>
-        <ButtonMenu activeIndex={isExact ? 0 : 1} size="sm" variant="subtle">
-          <ButtonMenuItem as={Link} to={`${url}`}>
-            {TranslateString(698, 'Active')}
-          </ButtonMenuItem>
-          <ButtonMenuItem as={Link} to={`${url}/history`}>
-            {TranslateString(700, 'Inactive')}
-          </ButtonMenuItem>
-        </ButtonMenu>
-        {account && (
-          <ToggleWrapper>
-            <Toggle checked={stakedOnly} onChange={() => setStakedOnly(!stakedOnly)} />
-            <Text> {TranslateString(699, 'Staked only')}</Text>
-          </ToggleWrapper>
-        )}
-      </>
-      <StyledInputFilter
-        type="text"
-        scale="md"
-        placeholder={TranslateString(999, 'Filter')}
-        value={filter}
-        onChange={(e) => setFilter(e.target.value)}
-      />
-    </Wrapper>
-  )
-}
-
-export default TabToolbar
-
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -85,3 +42,44 @@ const ToggleWrapper = styled.div`
 const StyledInputFilter = styled(Input)`
   max-width: 300px;
 `
+
+interface TabToolbarProps {
+  account: number | string
+  stakedOnly: boolean
+  setStakedOnly: (boolean) => void
+  filter: string
+  setFilter: (boolean) => void
+}
+
+const TabToolbar: React.FC<TabToolbarProps> = ({ account, stakedOnly, setStakedOnly, filter, setFilter }) => {
+  const { url, isExact } = useRouteMatch()
+  const TranslateString = useI18n()
+
+  return (
+    <Wrapper>
+      <ButtonMenu activeIndex={isExact ? 0 : 1} size="sm" variant="subtle">
+        <ButtonMenuItem as={Link} to={`${url}`}>
+          {TranslateString(698, 'Active')}
+        </ButtonMenuItem>
+        <ButtonMenuItem as={Link} to={`${url}/history`}>
+          {TranslateString(700, 'Inactive')}
+        </ButtonMenuItem>
+      </ButtonMenu>
+      {account && (
+        <ToggleWrapper>
+          <Toggle checked={stakedOnly} onChange={() => setStakedOnly(!stakedOnly)} />
+          <Text> {TranslateString(699, 'Staked only')}</Text>
+        </ToggleWrapper>
+      )}
+      <StyledInputFilter
+        type="text"
+        scale="md"
+        placeholder={TranslateString(999, 'Filter')}
+        value={filter}
+        onChange={(e) => setFilter(e.target.value)}
+      />
+    </Wrapper>
+  )
+}
+
+export default TabToolbar
