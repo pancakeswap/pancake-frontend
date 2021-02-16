c1e4bc71675a3cd04763b5802f51d0706ba552b3
hachiojidev <hachiojidev@protonmail.com>
2021-02-16 17:28:09 +0900

    fix(profile): Check minimum CAKE balance

--- src/hooks/useHasCakeBalance.ts
+++ src/hooks/useHasCakeBalance.ts
@@ -1,10 +1,11 @@
+import BigNumber from 'bignumber.js'
 import { getCakeAddress } from 'utils/addressHelpers'
 import useTokenBalance from './useTokenBalance'
 
 /**
  * A hook to check if a wallet's CAKE balance is at least the amount passed in
  */
-const useHasCakeBalance = (minimumBalance) => {
+const useHasCakeBalance = (minimumBalance: BigNumber) => {
   const cakeBalance = useTokenBalance(getCakeAddress())
   return cakeBalance.gte(minimumBalance)
 }
