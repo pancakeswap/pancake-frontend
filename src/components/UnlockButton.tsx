74c350ae19989ca61fb8b2b1f0593e38e0de2b4b
Kinji <45015575+aliangm@users.noreply.github.com>
2021-02-23 08:12:46 +0700

    feat: Add activate feature

--- src/components/UnlockButton.tsx
+++ src/components/UnlockButton.tsx
@@ -1,12 +1,26 @@
 import React from 'react'
-import { Button, useWalletModal } from '@pancakeswap-libs/uikit'
+import { Button, ConnectorId, useWalletModal } from '@pancakeswap-libs/uikit'
 import { useWeb3React } from '@web3-react/core'
+import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
+
+import { injected, walletconnect, bsc } from 'connectors'
 import useI18n from 'hooks/useI18n'
 
 const UnlockButton = (props) => {
   const TranslateString = useI18n()
-  const { connect, reset } = useWallet()
-  const { onPresentConnectModal } = useWalletModal(connect, reset)
+  const { account, activate, deactivate, connector } = useWeb3React()
+  const handleLogin = (connectorId: ConnectorId) => {
+    if (connectorId === 'walletconnect') {
+      if (connector instanceof WalletConnectConnector && connector.walletConnectProvider?.wc?.uri) {
+        connector.walletConnectProvider = undefined
+      }
+      return activate(walletconnect)
+    }
+
+    return activate(injected)
+  }
+
+  const { onPresentConnectModal } = useWalletModal(handleLogin, deactivate, account as string)
 
   return (
     <Button onClick={onPresentConnectModal} {...props}>
