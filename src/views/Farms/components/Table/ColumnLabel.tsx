61da5c272b0afce3306332c0e1ad256a72481686
CBunnyDev <evedevpwr@gmail.com>
2021-02-23 22:19:54 -0800

    style: Rm unused

--- src/views/Farms/components/Table/ColumnLabel.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-const ColumnLabel = styled.div`
-  font-size: 0.75rem;
-  color: ${(props) => props.theme.colors.textSubtle};
-  text-align: left;
-`
-
-export default ColumnLabel
