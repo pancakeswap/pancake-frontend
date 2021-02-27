1adb94db028709d373862a0c2b3ad74956c9c203
UBC Student <diegoc96@students.cs.ubc.ca>
2021-02-27 09:39:36 -0800

    fix: Document tiltle hook name, removed unnecessary code and used prettier

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { useWallet } from '@binance-chain/bsc-use-wallet'
 import { ResetCSS } from '@pancakeswap-libs/uikit'
 import BigNumber from 'bignumber.js'
 import { useFetchProfile, useFetchPublicData } from 'state/hooks'
-import usePriceUsd from './hooks/usePriceUsd'
+import useGetDocumentTitlePrice from './hooks/useGetDocumentTitlePrice'
 import GlobalStyle from './style/Global'
 import Menu from './components/Menu'
 import ToastListener from './components/ToastListener'
@@ -12,7 +12,6 @@ import PageLoader from './components/PageLoader'
 import Pools from './views/Pools'
 import GlobalCheckBullHiccupClaimStatus from './views/Collectibles/components/GlobalCheckBullHiccupClaimStatus'
 import history from './routerHistory'
-
 
 // Route-based code splitting
 // Only pool is included in the main bundle because of it's the most visited page'
@@ -34,7 +33,7 @@ BigNumber.config({
 
 const App: React.FC = () => {
   const { account, connect } = useWallet()
-  usePriceUsd()
+  useGetDocumentTitlePrice()
   // Monkey patch warn() because of web3 flood
   // To be removed when web3 1.3.5 is released
   useEffect(() => {
