1b3b2ca20024bb5ecc5f70d96473d5fcf483e475
UBC Student <diegoc96@students.cs.ubc.ca>
2021-02-28 22:32:24 -0800

    fix: Document title hook name, renamed hook and used prettier

---
+++ src/hooks/useGetDocumentTitlePrice.ts
@@ -0,0 +1,13 @@
+import { useEffect } from 'react'
+import { usePriceCakeBusd } from 'state/hooks'
+
+const useGetDocumentTitlePrice = () => {
+  const cakePriceUsd = usePriceCakeBusd()
+  useEffect(() => {
+    document.title = `PancakeSwap - $${Number(cakePriceUsd).toLocaleString(undefined, {
+      minimumFractionDigits: 3,
+      maximumFractionDigits: 3,
+    })}`
+  })
+}
+export default useGetDocumentTitlePrice
