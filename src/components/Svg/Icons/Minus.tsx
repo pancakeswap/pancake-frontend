b93c16d9d886df3ac6d05c07589c804f1c76b408
cbunnydev <59729252+cbunnydev@users.noreply.github.com>
2021-03-04 23:00:31 -0800

    Icons updated (#204)
    * fix: Calculate icon updated
    
    * fix: Update add icon
    
    * fix: Minus icon
    
    * test: Tests updated

--- src/components/Svg/Icons/Minus.tsx
+++ src/components/Svg/Icons/Minus.tsx
@@ -4,8 +4,11 @@ import { SvgProps } from "../types";
 
 const Icon: React.FC<SvgProps> = (props) => {
   return (
-    <Svg viewBox="0 0 15 2" {...props}>
-      <path d="M13.2 2L1.20004 2C0.650043 2 0.200043 1.55 0.200043 1C0.200043 0.45 0.650043 0 1.20004 0L13.2 0C13.75 0 14.2 0.45 14.2 1C14.2 1.55 13.75 2 13.2 2Z" />
+    <Svg viewBox="0 0 14 2" {...props}>
+      <path
+        d="M13 2L1 2C0.45 2 0 1.55 0 1C0 0.45 0.45 0 1 0L13 0C13.55 0 14 0.45 14 1C14 1.55 13.55 2 13 2Z"
+        fill="#1FC7D4"
+      />
     </Svg>
   );
 };
