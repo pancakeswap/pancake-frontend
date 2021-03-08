98d69f2bb317a9ba33a24724ce9df03b285730c3
Chef Hachioji <72719739+hachiojidev@users.noreply.github.com>
2021-03-08 15:24:33 +0900

    fix: Disable eager connect for BSC (#579)

--- src/hooks/useEagerConnect.ts
+++ src/hooks/useEagerConnect.ts
@@ -7,7 +7,11 @@ const useEagerConnect = () => {
 
   useEffect(() => {
     const connectorId = window.localStorage.getItem(connectorLocalStorageKey) as ConnectorNames
-    if (connectorId) {
+
+    // Disable eager connect for BSC Wallet. Currently the BSC Wallet extension does not inject BinanceChain
+    // into the Window object in time causing it to throw an error
+    // TODO: Figure out an elegant way to listen for when the BinanceChain object is ready
+    if (connectorId !== ConnectorNames.BSC) {
       login(connectorId)
     }
   }, [login])
