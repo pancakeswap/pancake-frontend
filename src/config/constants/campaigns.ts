52a44cc13d01d8694bf69918ba797f20f0824ba1
Chef-Chungus <71833681+Chef-Chungus@users.noreply.github.com>
2021-03-08 16:04:23 +0900

    Belt IFO announcement (#606)
    * feat(ifo): Add BELT
    
    * chore: Add contract, releaseBlockNumber & subTitle

--- src/config/constants/campaigns.ts
+++ src/config/constants/campaigns.ts
@@ -11,6 +11,12 @@ import { Campaign } from './types'
 
 const campaigns: Campaign[] = [
   {
+    id: '511080000',
+    type: 'ifo',
+    title: 'Belt',
+    badge: 'ifo-belt.svg',
+  },
+  {
     id: '511070000',
     type: 'ifo',
     title: 'Yieldwatch',
--- src/config/constants/ifo.ts
+++ src/config/constants/ifo.ts
@@ -2,9 +2,30 @@ import { Ifo } from './types'
 
 const ifos: Ifo[] = [
   {
+    id: 'belt',
+    address: '0xc9FBedC033a1c479a6AD451ffE463025E92a1d38',
+    isActive: true,
+    name: 'Belt (BELT)',
+    subTitle: 'AMM protocol incorporating multi-strategy yield optimization',
+    description:
+      'Belt.fi is a protocol that allows users to retain the stability of their asset positions and get maximum yields with minimal risk, including automated vault compounding and yield optimization strategies. BELT is the governance and incentivization token of Belt Finance. BELT will be distributed to all types of pools.',
+    launchDate: 'Mar. 10',
+    launchTime: '5PM SGT',
+    saleAmount: '150,000 BELT',
+    raiseAmount: '$3,000,000',
+    cakeToBurn: '$1,500,000',
+    projectSiteUrl: 'https://beta.belt.fi/',
+    currency: 'CAKE-BNB LP',
+    currencyAddress: '0xA527a61703D82139F8a06Bc30097cC9CAA2df5A6',
+    tokenDecimals: 18,
+    tokenSymbol: 'BELT',
+    releaseBlockNumber: 5493919,
+    campaignId: '511080000',
+  },
+  {
     id: 'watch',
     address: '0x55344b55C71Ad8834C397E6e08dF5195cF84fe6d',
-    isActive: true,
+    isActive: false,
     name: 'Yieldwatch (WATCH)',
     subTitle: 'A smart yield farming dashboard on BSC',
     description:
