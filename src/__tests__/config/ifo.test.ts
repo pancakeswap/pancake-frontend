644a853592746351869ee3cd52edb0196518884e
RabbitDoge <72658581+RabbitDoge@users.noreply.github.com>
2021-03-09 19:19:58 +0900

    test: Update messages and fix a bug

--- src/__tests__/config/ifo.test.ts
+++ src/__tests__/config/ifo.test.ts
@@ -6,15 +6,15 @@ describe('Config IFOs', () => {
     const duplicates = ifos.filter((i) => id === i.id)
     expect(duplicates).toHaveLength(1)
   })
-  it.each(ifos.map((ifo) => ifo.address))('IFO #%d has an unique address', (address) => {
+  it.each(ifos.map((ifo) => [ifo.id, ifo.address]))('IFO #%d has an unique address', (id, address) => {
     const duplicates = ifos.filter((i) => address === i.address)
     expect(duplicates).toHaveLength(1)
   })
-  it.each(ifos.map((ifo) => ifo.campaignId))('IFO #%d has an unique campaign ID', (campaignId) => {
+  it.each(ifos.map((ifo) => [ifo.id, ifo.campaignId]))('IFO #%d has an unique campaign ID', (id, campaignId) => {
     const duplicates = ifos.filter((i) => campaignId === i.campaignId)
     expect(duplicates).toHaveLength(1)
   })
-  it.each(ifos.map((ifo) => ifo.campaignId))('IFO #%d has an existing campaign ID', (campaignId) => {
+  it.each(ifos.map((ifo) => [ifo.id, ifo.campaignId]))('IFO #%d has an existing campaign ID', (id, campaignId) => {
     const campaign = campaigns.find((c) => campaignId === c.id)
     expect(campaign).not.toBeUndefined()
   })
--- src/__tests__/config/pools.test.ts
+++ src/__tests__/config/pools.test.ts
@@ -5,8 +5,11 @@ describe('Config pools', () => {
     const duplicates = pools.filter((p) => sousId === p.sousId)
     expect(duplicates).toHaveLength(1)
   })
-  it.each(pools.map((pool) => pool.contractAddress))('Pool #%d has an unique contract address', (contractAddress) => {
-    const duplicates = pools.filter((p) => contractAddress[56] === p.contractAddress[56])
-    expect(duplicates).toHaveLength(1)
-  })
+  it.each(pools.map((pool) => [pool.sousId, pool.contractAddress]))(
+    'Pool #%d has an unique contract address',
+    (sousId, contractAddress) => {
+      const duplicates = pools.filter((p) => contractAddress[56] === p.contractAddress[56])
+      expect(duplicates).toHaveLength(1)
+    },
+  )
 })
