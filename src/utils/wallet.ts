cc5e002b57b37464919099584238d9996dd7aa28
RabbitDoge <72658581+RabbitDoge@users.noreply.github.com>
2021-03-09 16:15:35 +0900

    fix: Add token into wallet helper (#613)
    And clean the helper file.

--- src/utils/wallet.ts
+++ src/utils/wallet.ts
@@ -1,9 +1,13 @@
+// Set of helper functions to facilitate wallet setup
+
 import { nodes } from './getRpcUrl'
 
+/**
+ * Prompt the user to add BSC as a network on Metamask, or switch to BSC if the wallet is on a different network
+ * @returns {boolean} true if the setup succeeded, false otherwise
+ */
 export const setupNetwork = async () => {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  const provider = window.ethereum
+  const provider = (window as WindowChain).ethereum
   if (provider) {
     const chainId = parseInt(process.env.REACT_APP_CHAIN_ID, 10)
     try {
@@ -34,4 +38,32 @@ export const setupNetwork = async () => {
   }
 }
 
-export default null
+/**
+ * Prompt the user to add a custom token to metamask
+ * @param tokenAddress
+ * @param tokenSymbol
+ * @param tokenDecimals
+ * @param tokenImage
+ * @returns {boolean} true if the token has been added, false otherwise
+ */
+export const registerToken = async (
+  tokenAddress: string,
+  tokenSymbol: string,
+  tokenDecimals: number,
+  tokenImage: string,
+) => {
+  const tokenAdded = await (window as WindowChain).ethereum.request({
+    method: 'wallet_watchAsset',
+    params: {
+      type: 'ERC20',
+      options: {
+        address: tokenAddress,
+        symbol: tokenSymbol,
+        decimals: tokenDecimals,
+        image: tokenImage,
+      },
+    },
+  })
+
+  return tokenAdded
+}
