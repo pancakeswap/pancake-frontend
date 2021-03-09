f4b185e8437988fe7ecb86923b6d605b8ae5ecf0
hachiojidev <hachiojidev@protonmail.com>
2021-03-09 10:51:09 +0900

    fix: Skip login if no connector

--- src/hooks/useEagerConnect.ts
+++ src/hooks/useEagerConnect.ts
@@ -11,7 +11,7 @@ const useEagerConnect = () => {
     // Disable eager connect for BSC Wallet. Currently the BSC Wallet extension does not inject BinanceChain
     // into the Window object in time causing it to throw an error
     // TODO: Figure out an elegant way to listen for when the BinanceChain object is ready
-    if (connectorId !== ConnectorNames.BSC) {
+    if (connectorId && connectorId !== ConnectorNames.BSC) {
       login(connectorId)
     }
   }, [login])
