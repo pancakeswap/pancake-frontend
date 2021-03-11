9cba96e517b028f7e2a689fda76c66620f0fac0a
RabbitDoge <72658581+RabbitDoge@users.noreply.github.com>
2021-03-11 11:09:02 +0900

    fix: Network name when add on metamask

--- src/utils/wallet.ts
+++ src/utils/wallet.ts
@@ -16,10 +16,10 @@ export const setupNetwork = async () => {
         params: [
           {
             chainId: `0x${chainId.toString(16)}`,
-            chainName: 'Binance Smart Chain',
+            chainName: 'Binance Smart Chain Mainnet',
             nativeCurrency: {
               name: 'BNB',
-              symbol: 'BNB',
+              symbol: 'bnb',
               decimals: 18,
             },
             rpcUrls: nodes,
