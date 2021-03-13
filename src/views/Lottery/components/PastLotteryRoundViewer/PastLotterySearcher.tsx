6098e3b9a857c5ce4bb87113e645a517f885c077
memoyil <2213635+memoyil@users.noreply.github.com>
2021-03-13 17:13:37 +0100

    feat: Change to dropdown menu

--- src/views/Lottery/components/PastLotteryRoundViewer/PastLotterySearcher.tsx
+++ src/views/Lottery/components/PastLotteryRoundViewer/PastLotterySearcher.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styled from 'styled-components'
-import { Text, Input, Button } from '@pancakeswap-libs/uikit'
+import { Text } from '@pancakeswap-libs/uikit'
 import useI18n from 'hooks/useI18n'
+import RoundSelect, { RoundOptionProps } from '../Select/RoundSelect'
 
 interface PastLotterySearcherProps {
   initialLotteryNumber: number
@@ -12,56 +13,28 @@ const Wrapper = styled.div`
   margin-bottom: 24px;
 `
 
-const SearchWrapper = styled.div`
-  position: relative;
-`
-
-const ButtonWrapper = styled.div`
-  position: absolute;
-  right: 8px;
-  top: 50%;
-  transform: translate(0%, -50%);
-  width: auto;
-`
-
 const PastLotterySearcher: React.FC<PastLotterySearcherProps> = ({ initialLotteryNumber, onSubmit }) => {
-  const [lotteryNumber, setLotteryNumber] = useState(initialLotteryNumber)
-  const [isError, setIsError] = useState(false)
   const TranslateString = useI18n()
 
-  const handleSubmit = (evt: React.FormEvent<HTMLFormElement>) => {
-    evt.preventDefault()
-    onSubmit(lotteryNumber)
-  }
+  const rounds = Array.from(Array(initialLotteryNumber)).map((_, i) => {
+      return {
+        label: `Round ${i + 1}`,
+        value: i + 1,
+      }
+    },
+  ).reverse()
 
-  const handleChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
-    const value = parseInt(evt.currentTarget.value, 10)
-
-    // The max value will always be the initialLotterNumber which equals
-    // the latest lottery round
-    setIsError(value > initialLotteryNumber)
-    setLotteryNumber(value)
+  const handleSortOptionChange = (option: RoundOptionProps): void => {
+    onSubmit(option.value)
   }
 
   return (
     <Wrapper>
-      <Text>{TranslateString(742, 'Select lottery number:')}</Text>
-      <form onSubmit={handleSubmit}>
-        <SearchWrapper>
-          <Input
-            value={lotteryNumber}
-            type="number"
-            isWarning={isError}
-            max={initialLotteryNumber}
-            onChange={handleChange}
-          />
-          <ButtonWrapper>
-            <Button type="submit" scale="sm" disabled={isError}>
-              {TranslateString(744, 'Search')}
-            </Button>
-          </ButtonWrapper>
-        </SearchWrapper>
-      </form>
+      <Text>{TranslateString(742, 'Select lottery round:')}</Text>
+      <RoundSelect
+        options={rounds}
+        onChange={handleSortOptionChange}
+      />
     </Wrapper>
   )
 }
