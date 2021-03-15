c3993c2866b0eea79d51baf325e40fd9086ff896
Mehmet Yıldırım <2213635+memoyil@users.noreply.github.com>
2021-03-15 03:01:10 +0100

    fix: Wallet connect qr code problems and make error messages human readable (#649)
    * fix: Wallet connect qr code problems and make error messages human readable
    
    * fix: Format

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,6 +1,15 @@
 import { useCallback } from 'react'
 import { useWeb3React, UnsupportedChainIdError } from '@web3-react/core'
-import { ConnectorNames } from '@pancakeswap-libs/uikit'
+import { NoBscProviderError } from '@binance-chain/bsc-connector'
+import {
+  NoEthereumProviderError,
+  UserRejectedRequestError as UserRejectedRequestErrorInjected,
+} from '@web3-react/injected-connector'
+import {
+  UserRejectedRequestError as UserRejectedRequestErrorWalletConnect,
+  WalletConnectConnector,
+} from '@web3-react/walletconnect-connector'
+import { ConnectorNames, connectorLocalStorageKey } from '@pancakeswap-libs/uikit'
 import { useToast } from 'state/hooks'
 import { connectorsByName } from 'utils/web3React'
 import { setupNetwork } from 'utils/wallet'
@@ -19,7 +28,21 @@ const useAuth = () => {
             activate(connector)
           }
         } else {
-          toastError(error.name, error.message)
+          window.localStorage.removeItem(connectorLocalStorageKey)
+          if (error instanceof NoEthereumProviderError || error instanceof NoBscProviderError) {
+            toastError('Provider Error', 'No provider was found')
+          } else if (
+            error instanceof UserRejectedRequestErrorInjected ||
+            error instanceof UserRejectedRequestErrorWalletConnect
+          ) {
+            if (connector instanceof WalletConnectConnector) {
+              const walletConnector = connector as WalletConnectConnector
+              walletConnector.walletConnectProvider = null
+            }
+            toastError('Authorization Error', 'Please authorize to access your account')
+          } else {
+            toastError(error.name, error.message)
+          }
         }
       })
     } else {
