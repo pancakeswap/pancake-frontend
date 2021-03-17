98b82cd34c572a8e75d250ebf52681f9931d6b45
ChefHutch <chef.hutch.pcs@protonmail.com>
2021-03-17 12:18:32 +0000

    feat(trading-competition): Page furniture and routing

---
+++ src/views/TradingCompetition/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Page from 'components/layout/Page'
+
+const TradingCompetition = () => {
+  return (
+    <Page>
+      <span>Hey</span>
+    </Page>
+  )
+}
+
+export default TradingCompetition
