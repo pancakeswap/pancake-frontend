084f0a3f11e2f938a1f9f2040bc978b64aa17e48
memoyil <2213635+memoyil@users.noreply.github.com>
2021-03-19 12:34:56 +0100

    fix: Remove null type

--- src/utils/getLiquidityUrlPathParts.ts
+++ src/utils/getLiquidityUrlPathParts.ts
@@ -5,8 +5,8 @@ import { getWbnbAddress } from './addressHelpers'
 const getLiquidityUrlPathParts = ({ quoteTokenAddress, tokenAddress }) => {
   const chainId = process.env.REACT_APP_CHAIN_ID
   const wBNBAddressString = getWbnbAddress()
-  const quoteTokenAddressString: string | null = quoteTokenAddress ? quoteTokenAddress[chainId] : null
-  const tokenAddressString: string | null = tokenAddress ? tokenAddress[chainId] : null
+  const quoteTokenAddressString: string = quoteTokenAddress ? quoteTokenAddress[chainId] : null
+  const tokenAddressString: string = tokenAddress ? tokenAddress[chainId] : null
   const firstPart =
     !quoteTokenAddressString || quoteTokenAddressString === wBNBAddressString ? 'BNB' : quoteTokenAddressString
   const secondPart = !tokenAddressString || tokenAddressString === wBNBAddressString ? 'BNB' : tokenAddressString
