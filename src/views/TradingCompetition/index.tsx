5e5313215e67ceb5d13427300833a667e87779e5
ChefHutch <chef.hutch.pcs@protonmail.com>
2021-03-19 16:23:50 +0000

    chore(your-score): Removing YourScore components prior to merging core components into root

--- src/views/TradingCompetition/index.tsx
+++ src/views/TradingCompetition/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { useWeb3React } from '@web3-react/core'
 import { useProfile } from 'state/hooks'
 import Page from 'components/layout/Page'
-import YourScore from './components/YourScore'
 import RibbonWithImage from './components/RibbonWithImage'
 import Prizes from './svgs/Prizes'
 import Ranks from './svgs/Ranks'
@@ -14,7 +13,6 @@ const TradingCompetition = () => {
 
   return (
     <Page>
-      <YourScore registered={registered} account={account} profile={profile} />
       <RibbonWithImage imageComponent={<Prizes width="175px" />} ribbonDirection="up">
         Prizes
       </RibbonWithImage>
