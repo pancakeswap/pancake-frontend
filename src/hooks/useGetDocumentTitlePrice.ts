e1fdecd4e42f7362c2ec134614d135201f5aae95
memoyil <2213635+memoyil@users.noreply.github.com>
2021-03-20 19:41:07 +0100

    fix: NaN Cake price in webpage title

--- src/hooks/useGetDocumentTitlePrice.ts
+++ src/hooks/useGetDocumentTitlePrice.ts
@@ -1,13 +1,19 @@
 import { useEffect } from 'react'
 import { usePriceCakeBusd } from 'state/hooks'
+import { BigNumber } from 'bignumber.js'
 
 const useGetDocumentTitlePrice = () => {
-  const cakePriceUsd = usePriceCakeBusd()
+  const cakePriceUsdNumber = usePriceCakeBusd().toNumber()
+
+  const cakePriceUsdString = Number.isNaN(cakePriceUsdNumber)
+    ? ''
+    : ` - $${cakePriceUsdNumber.toLocaleString(undefined, {
+        minimumFractionDigits: 3,
+        maximumFractionDigits: 3,
+      })}`
+
   useEffect(() => {
-    document.title = `PancakeSwap - $${Number(cakePriceUsd).toLocaleString(undefined, {
-      minimumFractionDigits: 3,
-      maximumFractionDigits: 3,
-    })}`
+    document.title = `PancakeSwap${cakePriceUsdString}`
   })
 }
 export default useGetDocumentTitlePrice
