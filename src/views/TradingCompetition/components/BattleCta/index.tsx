b05b5cd5e7666a5b57e145786664d3458155e425
ChefHutch <chef.hutch.pcs@protonmail.com>
2021-03-22 11:51:59 +0000

    feat(trading-competition/battle-cta): Basic styling of button comp

--- src/views/TradingCompetition/components/BattleCta/index.tsx
+++ src/views/TradingCompetition/components/BattleCta/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Card, CardBody, Flex } from '@pancakeswap-libs/uikit'
+import { Card, CardBody, Flex, LaurelLeftIcon, LaurelRightIcon, Button } from '@pancakeswap-libs/uikit'
 import { Heading2Text } from '../CompetitionHeadingText'
 
 const StyledCard = styled(Card)`
   display: inline-flex;
+  background: linear-gradient(180deg, #7645d9 0%, #452a7a 100%);
+  svg {
+    z-index: 0;
+  }
+`
+
+const StyledButton = styled(Button)`
+  margin: 16px 20px 0;
 `
 
 const BattleCta = () => {
@@ -13,6 +21,11 @@ const BattleCta = () => {
       <CardBody>
         <Flex flexDirection="column" justifyContent="center" alignItems="center">
           <Heading2Text>Starting Soon</Heading2Text>
+          <Flex>
+            <LaurelLeftIcon />
+            <StyledButton>Sample</StyledButton>
+            <LaurelRightIcon />
+          </Flex>
         </Flex>
       </CardBody>
     </StyledCard>
