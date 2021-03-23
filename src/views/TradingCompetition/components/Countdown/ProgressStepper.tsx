46d8f9aa0b6ca380610acb461e0535bee1f0c676
ChefHutch <chef.hutch.pcs@protonmail.com>
2021-03-23 16:04:25 +0000

    feat(trading-competition/countdown): Adding progress sterpper comp

---
+++ src/views/TradingCompetition/components/Countdown/ProgressStepper.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Flex } from '@pancakeswap-libs/uikit'
+
+const ProgressStepper: React.FC<{ active?: number }> = ({ active }) => {
+  return <Flex>wat</Flex>
+}
+
+export default ProgressStepper
