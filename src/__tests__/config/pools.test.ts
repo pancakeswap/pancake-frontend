2000df13f7cfb0a6c032a3d6f330e5ea962666ad
RabbitDoge <72658581+RabbitDoge@users.noreply.github.com>
2021-03-23 16:40:11 +0900

    test: Onchain pools tests (#721)

--- src/__tests__/config/pools.test.ts
+++ src/__tests__/config/pools.test.ts
@@ -1,4 +1,10 @@
 import pools from 'config/constants/pools'
+import { getSouschefContract } from 'utils/contractHelpers'
+
+// Pool 0 is special (cake pool)
+// Pool 78 is a broken pool, not used, and break the tests
+const idsToRemove = [0, 78]
+const poolsToTest = pools.filter((pool) => !idsToRemove.includes(pool.sousId))
 
 describe('Config pools', () => {
   it.each(pools.map((pool) => pool.sousId))('Pool #%d has an unique sousId', (sousId) => {
@@ -10,6 +16,22 @@ describe('Config pools', () => {
     (sousId, contractAddress) => {
       const duplicates = pools.filter((p) => contractAddress[56] === p.contractAddress[56])
       expect(duplicates).toHaveLength(1)
+    },
+  )
+  it.each(poolsToTest.filter((pool) => pool.earningToken.symbol !== 'BNB'))(
+    'Pool %p has the correct earning token',
+    async (pool) => {
+      const contract = getSouschefContract(pool.sousId)
+      const rewardTokenAddress = await contract.methods.rewardToken().call()
+      expect(rewardTokenAddress.toLowerCase()).toBe(pool.earningToken.address[56].toLowerCase())
+    },
+  )
+  it.each(poolsToTest.filter((pool) => pool.stakingToken.symbol !== 'BNB'))(
+    'Pool %p has the correct staking token',
+    async (pool) => {
+      const contract = getSouschefContract(pool.sousId)
+      const stakingTokenAddress = await contract.methods.syrup().call()
+      expect(stakingTokenAddress.toLowerCase()).toBe(pool.stakingToken.address[56].toLowerCase())
     },
   )
 })
