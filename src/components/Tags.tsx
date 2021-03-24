a76fa1d20fbd725b2d6002598f003f09f1006e1d
memoyil <2213635+memoyil@users.noreply.github.com>
2021-03-24 10:46:48 +0100

    fix: Black icon and texts in dark theme (#682)
    * fix: Black icon and texts in dark theme
    
    * fix: Jsx call
    
    * fix: For all tags
    
    * fix: Add color to verified icon
    
    * fix: Icons
    
    * fix: Format
    
    * fix: Balance color
    
    * fix: Remove styledtext
    
    * fix: Remove useTheme

--- src/components/Tags.tsx
+++ src/components/Tags.tsx
@@ -2,19 +2,19 @@ import React from 'react'
 import { Tag, VerifiedIcon, CommunityIcon, BinanceIcon } from '@pancakeswap-libs/uikit'
 
 const CoreTag = (props) => (
-  <Tag variant="secondary" outline startIcon={<VerifiedIcon />} {...props}>
+  <Tag variant="secondary" outline startIcon={<VerifiedIcon color="secondary" />} {...props}>
     Core
   </Tag>
 )
 
 const CommunityTag = (props) => (
-  <Tag variant="textSubtle" outline startIcon={<CommunityIcon />} {...props}>
+  <Tag variant="textSubtle" outline startIcon={<CommunityIcon color="secondary" />} {...props}>
     Community
   </Tag>
 )
 
 const BinanceTag = (props) => (
-  <Tag variant="binance" outline startIcon={<BinanceIcon />} {...props}>
+  <Tag variant="binance" outline startIcon={<BinanceIcon color="secondary" />} {...props}>
     Binance
   </Tag>
 )
