06e6d41a1dbe25aee3a7d76042b5d79819bf4d3c
memoyil <2213635+memoyil@users.noreply.github.com>
2021-03-25 02:36:29 +0100

    fix: Cake winnings section Locked when no wallet and remove cake prize in dollar when 0 (#728)
    * fix: Cake winnings section Locked and remove cake prize in dollar when 0
    
    * fix: Remove skeleton
    
    * fix: Translation and absolute import
    
    * fix: Order of import

--- src/views/Home/components/LotteryCard.tsx
+++ src/views/Home/components/LotteryCard.tsx
@@ -13,9 +13,9 @@ import BuyModal from 'views/Lottery/components/TicketCard/BuyTicketModal'
 import { useLotteryAllowance } from 'hooks/useAllowance'
 import { useApproval } from 'hooks/useApproval'
 import PurchaseWarningModal from 'views/Lottery/components/TicketCard/PurchaseWarningModal'
+import UnlockButton from 'components/UnlockButton'
 import CakeWinnings from './CakeWinnings'
 import LotteryJackpot from './LotteryJackpot'
-import UnlockButton from '../../../components/UnlockButton'
 
 const StyledLotteryCard = styled(Card)`
   background-image: url('/images/ticket-bg.svg');
@@ -45,7 +45,7 @@ const Actions = styled.div`
   }
 `
 
-const FarmedStakingCard = () => {
+const LotteryCard = () => {
   const { account } = useWeb3React()
   const lotteryHasDrawn = useGetLotteryHasDrawn()
   const [requesteClaim, setRequestedClaim] = useState(false)
@@ -124,4 +124,4 @@ const FarmedStakingCard = () => {
   )
 }
 
-export default FarmedStakingCard
+export default LotteryCard
--- src/views/Home/components/LotteryJackpot.tsx
+++ src/views/Home/components/LotteryJackpot.tsx
@@ -11,7 +11,7 @@ const LotteryJackpot = () => {
   const TranslateString = useI18n()
   const lotteryPrizeAmount = useTotalRewards()
   const balance = getBalanceNumber(lotteryPrizeAmount)
-  const lotteryPrizeAmoutCake = balance.toLocaleString(undefined, {
+  const lotteryPrizeAmountCake = balance.toLocaleString(undefined, {
     maximumFractionDigits: 2,
   })
   const lotteryPrizeAmountBusd = new BigNumber(balance).multipliedBy(usePriceCakeBusd()).toNumber()
@@ -19,9 +19,9 @@ const LotteryJackpot = () => {
   return (
     <>
       <Text bold fontSize="24px" style={{ lineHeight: '1.5' }}>
-        {lotteryPrizeAmoutCake} {TranslateString(999, 'CAKE')}
+        {TranslateString(999, `${lotteryPrizeAmountCake} CAKE`, { amount: lotteryPrizeAmountCake })}
       </Text>
-      <CardBusdValue value={lotteryPrizeAmountBusd} />
+      {lotteryPrizeAmountBusd !== 0 && <CardBusdValue value={lotteryPrizeAmountBusd} />}
     </>
   )
 }
