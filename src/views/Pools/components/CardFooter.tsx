de369e76f57ee65353dc57283bc1140d8e22c947
Chef Chungus <71833681+Chef-Chungus@users.noreply.github.com>
2021-03-25 18:57:48 +0900

    fix: Wrong TranslationID (#745)

--- src/views/Pools/components/CardFooter.tsx
+++ src/views/Pools/components/CardFooter.tsx
@@ -131,7 +131,7 @@ const CardFooter: React.FC<Props> = ({
           {blocksUntilStart > 0 && (
             <Row mb="4px">
               <FlexFull>
-                <Label>{TranslateString(410, 'Start')}:</Label>
+                <Label>{TranslateString(1212, 'Start')}:</Label>
               </FlexFull>
               <Balance fontSize="14px" isDisabled={isFinished} value={blocksUntilStart} decimals={0} />
             </Row>
