6aab674e7f304439cc1f2fe3754aa8f697cc3efd
Chef Hachioji <72719739+hachiojidev@users.noreply.github.com>
2021-03-31 13:27:16 +0900

    feat(uikit): Icons (#40)
    - Medal: Bronze, Gold, Purple, Silver, Teal
    - Fixed checkmark icon
    - Update Won icon

--- packages/pancake-uikit/src/components/Svg/index.stories.tsx
+++ packages/pancake-uikit/src/components/Svg/index.stories.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Box from "../Box/Box";
 import Flex from "../Box/Flex";
 import Text from "../Text/Text";
 import Svg from "./Svg";
@@ -36,15 +37,29 @@ const components = context.keys().reduce((accum, path) => {
 
 export const Icons: React.FC = () => {
   return (
-    <Flex justifyContent="start" alignItems="center" flexWrap="wrap" style={{ maxWidth: "800px" }}>
+    <Flex justifyContent="start" alignItems="center" flexWrap="wrap">
       {Object.keys(components).map((file) => {
         const Icon = components[file].default;
         return (
-          <Flex key={file} flexDirection="column" alignItems="center" py="4px" px="8px" mb="32px">
-            <Icon size="32px" />
-            <Text color="textSubtle" fontSize="12px">
-              {file}
-            </Text>
+          <Flex
+            key={file}
+            flexDirection="column"
+            alignItems="center"
+            width="128px"
+            style={{ border: "1px solid #eee" }}
+            justifyContent="center"
+            py="8px"
+            m="4px"
+          >
+            <Flex alignItems="center" justifyContent="center" style={{ flex: 1 }} height="100%">
+              <Icon width="48px" />
+              <Icon width="24px" color="secondary" ml="4px" />
+            </Flex>
+            <Box py="4px">
+              <Text color="textSubtle" fontSize="14px">
+                {file}
+              </Text>
+            </Box>
           </Flex>
         );
       })}
