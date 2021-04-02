2b7b7dd0955178597c7c01add5789b8987188127
memoyil <2213635+memoyil@users.noreply.github.com>
2021-04-02 20:19:39 +0200

    fix: Add lottery progress translations

--- src/views/Lottery/components/LotteryProgress.tsx
+++ src/views/Lottery/components/LotteryProgress.tsx
@@ -49,13 +49,13 @@ const LotteryProgress = () => {
           {lotteryHasDrawn ? timeUntilTicketSale : timeUntilLotteryDraw}
         </StyledPrimaryText>
         <Text fontSize="20px" bold color="invertedContrast">
-          {lotteryHasDrawn ? TranslateString(0, 'Until ticket sale') : TranslateString(0, 'Until lottery draw')}
+          {lotteryHasDrawn ? TranslateString(434, 'Until ticket sale') : TranslateString(492, 'Until lottery draw')}
         </Text>
       </TopTextWrapper>
       {lotteryHasDrawn && (
         <BottomTextWrapper>
           <Text color="invertedContrast">
-            {timeUntilLotteryDraw} {TranslateString(0, 'Until lottery draw')}
+            {timeUntilLotteryDraw} {TranslateString(492, 'Until lottery draw')}
           </Text>
         </BottomTextWrapper>
       )}
