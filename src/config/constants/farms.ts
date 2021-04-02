c79adf1f4d66321ae30889a904c7d109b7af9b59
Chef Chungus <71833681+Chef-Chungus@users.noreply.github.com>
2021-04-02 15:28:49 +0900

    feat(farm/pool): DEGO (#771)
    Note: dego-cake.svg already exists

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -23,6 +23,16 @@ const farms: FarmConfig[] = [
     quoteToken: tokens.wbnb,
   },
   {
+    pid: 114,
+    lpSymbol: 'DEGO-BNB LP',
+    lpAddresses: {
+      97: '',
+      56: '0x17F0b998B81cE75074a7CDAdAe6D63Da3cb23572',
+    },
+    token: tokens.dego,
+    quoteToken: tokens.wbnb,
+  },
+  {
     pid: 113,
     lpSymbol: 'GUM-BNB LP',
     lpAddresses: {
