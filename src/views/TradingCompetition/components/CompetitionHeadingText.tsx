60af58a2bba77d5c9bed7cf20a5f1e20bdcbacb5
memoyil <2213635+memoyil@users.noreply.github.com>
2021-04-08 22:18:46 +0200

    fix: Remove color

--- src/views/TradingCompetition/components/CompetitionHeadingText.tsx
+++ src/views/TradingCompetition/components/CompetitionHeadingText.tsx
@@ -22,7 +22,6 @@ const H2SizeStyles = (theme: DefaultTheme) => `
 `
 
 const sharedStyles = (props) => `
-  color: #ffffff;
   background:  ${props.background ? props.background : 'linear-gradient(#7645d9 0%, #452a7a 100%)'};
   background-clip: text;
   -webkit-background-clip: text;
