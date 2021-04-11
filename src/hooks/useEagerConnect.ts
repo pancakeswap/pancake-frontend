34c785010007bc7ad1d01c8f730f27773f17415b
Toms Burgmanis <toms.burgmanis@gmail.com>
2021-04-11 23:27:34 +0200

    fix(auth): Eager connect not working with bsc extension

--- src/hooks/useEagerConnect.ts
+++ src/hooks/useEagerConnect.ts
@@ -2,16 +2,38 @@ import { useEffect } from 'react'
 import { connectorLocalStorageKey, ConnectorNames } from '@pancakeswap-libs/uikit'
 import useAuth from 'hooks/useAuth'
 
+const _binanceChainListener = async () =>
+  new Promise<void>((resolve) =>
+    Object.defineProperty(window, 'BinanceChain', {
+      get() {
+        return this.bsc
+      },
+      set(bsc) {
+        this.bsc = bsc
+
+        resolve()
+      },
+    }),
+  )
+
 const useEagerConnect = () => {
   const { login } = useAuth()
 
   useEffect(() => {
     const connectorId = window.localStorage.getItem(connectorLocalStorageKey) as ConnectorNames
 
-    // Disable eager connect for BSC Wallet. Currently the BSC Wallet extension does not inject BinanceChain
-    // into the Window object in time causing it to throw an error
-    // TODO: Figure out an elegant way to listen for when the BinanceChain object is ready
-    if (connectorId && connectorId !== ConnectorNames.BSC) {
+    if (connectorId) {
+      const isConnectorBinanceChain = connectorId === ConnectorNames.BSC
+      const isBinanceChainDefined = Reflect.has(window, 'BinanceChain')
+
+      // Currently BSC extension doesn't always inject in time.
+      // We must check to see if it exists, and if not, wait for it before proceeding.
+      if (isConnectorBinanceChain && !isBinanceChainDefined) {
+        _binanceChainListener().then(() => login(connectorId))
+
+        return
+      }
+
       login(connectorId)
     }
   }, [login])
