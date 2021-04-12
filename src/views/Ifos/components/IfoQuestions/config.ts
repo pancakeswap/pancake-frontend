4cc51255b629232ddbeee632abcca3ba0859a57c
Chef Hachioji <72719739+hachiojidev@users.noreply.github.com>
2021-04-12 14:09:42 +0900

    feat(ifo): Details FAQ (#2)
    * feat(ifo): Details FAQ
    
    * chore: Move details to config file
    
    * refactor: Config description to array

---
+++ src/views/Ifos/components/IfoQuestions/config.ts
@@ -0,0 +1,26 @@
+const config = [
+  {
+    title: { id: 999, fallback: "What's the difference between a Basic Sale and Unlimited Sale" },
+    description: [
+      {
+        id: 999,
+        fallback: 'TBD',
+      },
+    ],
+  },
+  {
+    title: { id: 999, fallback: 'TBD' },
+    description: [
+      {
+        id: 999,
+        fallback: 'TBD',
+      },
+      {
+        id: 999,
+        fallback: 'TBD',
+      },
+    ],
+  },
+]
+
+export default config
