14d7dd9f11203ce283bae511f73441d5c1b168bb
hachiojidev <hachiojidev@protonmail.com>
2021-04-12 10:34:32 +0900

    chore: Use existing hook

--- src/views/Predictions/hooks/useOnNextRound.ts
+++ src/views/Predictions/hooks/useOnNextRound.ts
@@ -1,4 +1,5 @@
-import { useEffect, useRef } from 'react'
+import usePreviousValue from 'hooks/usePreviousValue'
+import { useEffect } from 'react'
 import { useAppDispatch } from 'state'
 import { useGetCurrentEpoch, useGetSortedRounds } from 'state/hooks'
 import useSwiper from './useSwiper'
@@ -6,19 +7,17 @@ import useSwiper from './useSwiper'
 const useOnNextRound = () => {
   const currentEpoch = useGetCurrentEpoch()
   const rounds = useGetSortedRounds()
-  const previousEpoch = useRef(currentEpoch)
+  const previousEpoch = usePreviousValue(currentEpoch)
   const { swiper } = useSwiper()
   const dispatch = useAppDispatch()
 
   useEffect(() => {
-    if (currentEpoch !== previousEpoch.current) {
+    if (swiper && currentEpoch !== previousEpoch) {
       const currentEpochIndex = rounds.findIndex((round) => round.epoch === currentEpoch)
 
       // Slide to the current LIVE round which is always the one before the current round
       swiper.slideTo(currentEpochIndex - 1)
       swiper.update()
-
-      previousEpoch.current = currentEpoch
     }
   }, [previousEpoch, currentEpoch, rounds, swiper, dispatch])
 }
