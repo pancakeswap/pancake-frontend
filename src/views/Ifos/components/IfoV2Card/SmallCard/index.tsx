4b06c47453e407b851399f59cf9e828238dae480
RabbitDoge <72658581+RabbitDoge@users.noreply.github.com>
2021-04-13 19:18:06 +0900

    feat: Add the tooltip on the small card

--- src/views/Ifos/components/IfoV2Card/SmallCard/index.tsx
+++ src/views/Ifos/components/IfoV2Card/SmallCard/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card, CardBody, CardHeader, Text } from '@pancakeswap-libs/uikit'
+import { Card, CardBody, CardHeader, Text, useTooltip, HelpIcon, Flex } from '@pancakeswap-libs/uikit'
 import { Ifo } from 'config/constants/types'
 import { useProfile } from 'state/hooks'
 import { PublicIfoData, WalletIfoData, PoolIds } from 'hooks/ifo/v2/types'
@@ -19,6 +19,7 @@ interface CardConfig {
     title: string
     variant: 'blue' | 'violet'
     distribution: number
+    tooltip: string
   }
 }
 
@@ -28,50 +29,61 @@ const cardConfig: CardConfig = {
     title: 'Basic Sale',
     variant: 'blue',
     distribution: 0.3,
+    tooltip: 'Every person can only commit a limited amount, but may expect a higher return per token committed.',
   },
   [PoolIds.poolUnlimited]: {
     title: 'Unlimited Sale',
     variant: 'violet',
     distribution: 0.7,
+    tooltip: 'No limits on the amount you can commit. Additional fee applies when claiming.',
   },
 }
 
 const SmallCard: React.FC<IfoCardProps> = ({ ifo, publicIfoData, walletIfoData, poolId }) => {
-  const { hasProfile, isLoading: isProfileLoading } = useProfile()
   const config = cardConfig[poolId]
+  const { hasProfile, isLoading: isProfileLoading } = useProfile()
+  const { targetRef, tooltip, tooltipVisible } = useTooltip(config.tooltip, 'bottom')
   const publicPoolCharacteristics = publicIfoData[poolId]
   const userPoolCharacteristics = walletIfoData[poolId]
 
   const isLoading = isProfileLoading || publicIfoData.status === 'idle'
 
   return (
-    <Card>
-      <CardHeader variant={config.variant}>
-        <Text bold fontSize="20px">
-          {config.title}
-        </Text>
-      </CardHeader>
-      <CardBody>
-        <IfoCardTokens
-          ifo={ifo}
-          distribution={config.distribution}
-          status={publicIfoData.status}
-          publicPoolCharacteristics={publicPoolCharacteristics}
-          userPoolCharacteristics={userPoolCharacteristics}
-          hasProfile={hasProfile}
-          isLoading={isLoading}
-        />
-        <IfoCardActions
-          currency={ifo.currency}
-          publicIfoData={publicIfoData}
-          walletIfoData={walletIfoData}
-          poolId={poolId}
-          hasProfile={hasProfile}
-          isLoading={isLoading}
-        />
-        <IfoCardDetails poolId={poolId} ifo={ifo} publicIfoData={publicIfoData} />
-      </CardBody>
-    </Card>
+    <>
+      {tooltipVisible && tooltip}
+      <Card>
+        <CardHeader variant={config.variant}>
+          <Flex justifyContent="space-between" alignItems="center">
+            <Text bold fontSize="20px">
+              {config.title}
+            </Text>
+            <div ref={targetRef}>
+              <HelpIcon />
+            </div>
+          </Flex>
+        </CardHeader>
+        <CardBody>
+          <IfoCardTokens
+            ifo={ifo}
+            distribution={config.distribution}
+            status={publicIfoData.status}
+            publicPoolCharacteristics={publicPoolCharacteristics}
+            userPoolCharacteristics={userPoolCharacteristics}
+            hasProfile={hasProfile}
+            isLoading={isLoading}
+          />
+          <IfoCardActions
+            currency={ifo.currency}
+            publicIfoData={publicIfoData}
+            walletIfoData={walletIfoData}
+            poolId={poolId}
+            hasProfile={hasProfile}
+            isLoading={isLoading}
+          />
+          <IfoCardDetails poolId={poolId} ifo={ifo} publicIfoData={publicIfoData} />
+        </CardBody>
+      </Card>
+    </>
   )
 }
 
