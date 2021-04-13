b695287a9bc5b7503d5cff3af3bcc01e762692a0
Chef-Chungus <71833681+Chef-Chungus@users.noreply.github.com>
2021-04-13 22:04:06 +0900

    feat(pool/farm): PERL

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -17,6 +17,19 @@ const pools: PoolConfig[] = [
     isFinished: false,
   },
   {
+    sousId: 103,
+    stakingToken: tokens.cake,
+    earningToken: tokens.perl,
+    contractAddress: {
+      97: '',
+      56: '0xa8d32b31ECB5142f067548Bf0424389eE98FaF26',
+    },
+    poolCategory: PoolCategory.CORE,
+    harvest: true,
+    sortOrder: 999,
+    tokenPerBlock: '1.1574',
+  },
+  {
     sousId: 102,
     stakingToken: tokens.cake,
     earningToken: tokens.tlm,
