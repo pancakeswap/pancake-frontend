1902212c6fde1e5824e71954f7c5d6b4e86e0340
hachiojidev <hachiojidev@protonmail.com>
2021-04-16 10:44:30 +0900

    test: Fix typo

--- src/__tests__/config/nft.test.ts
+++ src/__tests__/config/nft.test.ts
@@ -1,7 +1,7 @@
 import nfts from 'config/constants/nfts'
 
 describe('Config NFTs', () => {
-  it.each(nfts.map((nft) => nft.identifier))('NFT #%d has an unique identifier', (identifier) => {
+  it.each(nfts.map((nft) => nft.identifier))('NFT #%d has a unique identifier', (identifier) => {
     const duplicates = nfts.filter((n) => identifier === n.identifier)
     expect(duplicates).toHaveLength(1)
   })
