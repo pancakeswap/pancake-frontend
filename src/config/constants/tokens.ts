313bc917132ce8161ce04b1c357a58cf3ea2b84e
memoyil <2213635+memoyil@users.noreply.github.com>
2021-04-16 04:26:37 +0200

    fix: Security warning when inspect code (#907)

--- src/config/constants/tokens.ts
+++ src/config/constants/tokens.ts
@@ -487,7 +487,7 @@ const tokens = {
       97: '',
     },
     decimals: 18,
-    projectLink: 'http://stablexswap.com/',
+    projectLink: 'https://stablexswap.com/',
   },
   nar: {
     symbol: 'NAR',
@@ -685,7 +685,7 @@ const tokens = {
       97: '',
     },
     decimals: 18,
-    projectLink: 'http://www.makerdao.com/',
+    projectLink: 'https://www.makerdao.com/',
   },
   ada: {
     symbol: 'ADA',
@@ -793,7 +793,7 @@ const tokens = {
       97: '0xE02dF9e3e622DeBdD69fb838bB799E3F168902c5',
     },
     decimals: 18,
-    projectLink: 'http://bch.info/',
+    projectLink: 'https://bch.info/',
   },
   yfi: {
     symbol: 'YFI',
