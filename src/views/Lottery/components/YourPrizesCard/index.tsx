4e7eec3e7389b1c3b1141b199399c87293b9ce04
memoyil <2213635+memoyil@users.noreply.github.com>
2021-04-16 04:12:55 +0200

    fix: Missing margin lottery won card (#870)
    * fix: Missing margin lottery won card
    
    * fix: Remove props
    
    * fix: Remove props

--- src/views/Lottery/components/YourPrizesCard/index.tsx
+++ src/views/Lottery/components/YourPrizesCard/index.tsx
@@ -7,21 +7,22 @@ import PrizesWonContent from './PrizesWonContent'
 import NoPrizesContent from './NoPrizesContent'
 
 const StyledCard = styled(Card)`
+  margin-top: 16px;
+
+  ${({ theme }) => theme.mediaQueries.sm} {
+    margin-top: 24px;
+  }
+
+  ${({ theme }) => theme.mediaQueries.lg} {
+    margin-top: 32px;
+  }
+
   ${(props) =>
     props.isDisabled
-      ? `  
-        margin-top: 16px;
+      ? `
         background-color: unset;
         box-shadow: unset;
         border: 1px solid ${props.theme.colors.textDisabled};
-
-        ${props.theme.mediaQueries.sm} {
-          margin-top: 24px;
-        }
-
-        ${props.theme.mediaQueries.lg} {
-          margin-top: 32px;
-        }
         `
       : ``}
 `
