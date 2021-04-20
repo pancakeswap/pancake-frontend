d97a79f19372a0165694a247ee7688715e2cc03e
Chef Hutch <79279477+ChefHutch@users.noreply.github.com>
2021-04-20 03:15:30 +0100

    feat(apy-modal): Enable ApyModal to handle any token, price and custom link labels (#941)
    * fix(apy-modal): Fix merge conflicts
    
    * chore(apy-modal): Delete APR Row component from pool

--- src/utils/compoundApyHelpers.ts
+++ src/utils/compoundApyHelpers.ts
@@ -1,18 +1,16 @@
 const roundToTwoDp = (number) => Math.round(number * 100) / 100
 
-export const cakeEarnedPerThousandDollarsCompounding = ({ numberOfDays, farmApr, cakePrice }) => {
+export const tokenEarnedPerThousandDollarsCompounding = ({ numberOfDays, farmApr, tokenPrice }) => {
   // Everything here is worked out relative to a year, with the asset compounding daily
   const timesCompounded = 365
-  //   We use decimal values rather than % in the math for both APY and the number of days being calculates as a proportion of the year
+  // We use decimal values rather than % in the math for both APY and the number of days being calculates as a proportion of the year
   const aprAsDecimal = farmApr / 100
   const daysAsDecimalOfYear = numberOfDays / timesCompounded
-  //   Calculate the starting CAKE balance with a dollar balance of $1000.
-  const principal = 1000 / cakePrice
-
+  // Calculate the starting TOKEN balance with a dollar balance of $1000.
+  const principal = 1000 / tokenPrice
   // This is a translation of the typical mathematical compounding APY formula. Details here: https://www.calculatorsoup.com/calculators/financial/compound-interest-calculator.php
   const finalAmount = principal * (1 + aprAsDecimal / timesCompounded) ** (timesCompounded * daysAsDecimalOfYear)
-
-  // To get the cake earned, deduct the amount after compounding (finalAmount) from the starting CAKE balance (principal)
+  // To get the TOKEN amount earned, deduct the amount after compounding (finalAmount) from the starting TOKEN balance (principal)
   const interestEarned = finalAmount - principal
   return roundToTwoDp(interestEarned)
 }
