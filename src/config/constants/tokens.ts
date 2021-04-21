fa6602525aa9249758dd7b9dc5fa889ccce9a204
Chef-Chungus <71833681+Chef-Chungus@users.noreply.github.com>
2021-04-21 19:03:32 +0900

    fix: XED ticker

--- src/config/constants/tokens.ts
+++ src/config/constants/tokens.ts
@@ -1138,7 +1138,7 @@ const tokens = {
     projectLink: 'https://hakka.finance/',
   },
   xed: {
-    symbol: 'xed',
+    symbol: 'XED',
     address: {
       56: '0x5621b5a3f4a8008c4ccdd1b942b121c8b1944f1f',
       97: '',
