2fc2e26aa0658a9369a372779b0145eb46a9ff40
hachiojidev <hachiojidev@protonmail.com>
2021-04-21 19:00:46 +0900

    fix: Use web3 with account

--- src/hooks/useTokenBalance.ts
+++ src/hooks/useTokenBalance.ts
@@ -67,10 +67,10 @@ export const useGetBnbBalance = () => {
   const [balance, setBalance] = useState(new BigNumber(0))
   const { account } = useWeb3React()
   const { lastUpdated, setLastUpdated } = useLastUpdated()
+  const web3 = useWeb3()
 
   useEffect(() => {
     const fetchBalance = async () => {
-      const web3 = getWeb3NoAccount()
       const walletBalance = await web3.eth.getBalance(account)
       setBalance(new BigNumber(walletBalance))
     }
@@ -78,7 +78,7 @@ export const useGetBnbBalance = () => {
     if (account) {
       fetchBalance()
     }
-  }, [account, lastUpdated, setBalance])
+  }, [account, web3, lastUpdated, setBalance])
 
   return { balance, refresh: setLastUpdated }
 }
