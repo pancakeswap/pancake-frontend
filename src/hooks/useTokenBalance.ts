40d5e51f84e8f6f0aff02c02185ee4874a302288
Chef Hachioji <72719739+hachiojidev@users.noreply.github.com>
2021-04-22 09:31:06 +0900

    feat: Get bnb balance hook (#961)
    * feat: Get bnb balance hook
    
    * chore: Fix lint
    
    * refactor: Use last updated instead of refresh
    
    * fix: Use web3 with account
    
    * chore: Remove unused import

--- src/hooks/useTokenBalance.ts
+++ src/hooks/useTokenBalance.ts
@@ -4,6 +4,7 @@ import { useWeb3React } from '@web3-react/core'
 import { getBep20Contract, getCakeContract } from 'utils/contractHelpers'
 import useWeb3 from './useWeb3'
 import useRefresh from './useRefresh'
+import useLastUpdated from './useLastUpdated'
 
 const useTokenBalance = (tokenAddress: string) => {
   const [balance, setBalance] = useState(new BigNumber(0))
@@ -59,6 +60,26 @@ export const useBurnedBalance = (tokenAddress: string) => {
   }, [web3, tokenAddress, slowRefresh])
 
   return balance
+}
+
+export const useGetBnbBalance = () => {
+  const [balance, setBalance] = useState(new BigNumber(0))
+  const { account } = useWeb3React()
+  const { lastUpdated, setLastUpdated } = useLastUpdated()
+  const web3 = useWeb3()
+
+  useEffect(() => {
+    const fetchBalance = async () => {
+      const walletBalance = await web3.eth.getBalance(account)
+      setBalance(new BigNumber(walletBalance))
+    }
+
+    if (account) {
+      fetchBalance()
+    }
+  }, [account, web3, lastUpdated, setBalance])
+
+  return { balance, refresh: setLastUpdated }
 }
 
 export default useTokenBalance
