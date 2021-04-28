941b759511c32636ca03eb289e6ac05aa43485be
hachiojidev <hachiojidev@protonmail.com>
2021-04-28 15:51:56 +0900

    style: Update gradient

--- src/views/Predictions/components/Container.tsx
+++ src/views/Predictions/components/Container.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 const Container = styled.div`
-  background: linear-gradient(180deg, #cbd7ef 0%, #9a9fd0 100%);
+  background: ${({ theme }) => theme.colors.gradients.violetAlt};
   height: calc(100vh - 64px);
   min-height: calc(100vh - 64px);
   overflow: hidden;
