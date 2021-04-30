28b2470103644827bff7173aec8f63b8bf59b692
ChefHutch <chef.hutch.pcs@protonmail.com>
2021-04-30 14:40:00 +0100

    test(apy-calculator): Update tests for compoundApyHelpers function

--- src/__tests__/utils/compoundApyHelpers.test.ts
+++ src/__tests__/utils/compoundApyHelpers.test.ts
@@ -1,14 +1,16 @@
 import { tokenEarnedPerThousandDollarsCompounding, getRoi } from 'utils/compoundApyHelpers'
 
 it.each([
-  [{ numberOfDays: 1, farmApr: 365, tokenPrice: 1 }, 10],
-  [{ numberOfDays: 7, farmApr: 20, tokenPrice: 0.8 }, 4.8],
-  [{ numberOfDays: 40, farmApr: 212.21, tokenPrice: 1.2 }, 217.48],
-  [{ numberOfDays: 330, farmApr: 45.12, tokenPrice: 5 }, 100.67],
-  [{ numberOfDays: 365, farmApr: 100, tokenPrice: 0.2 }, 8572.84],
-  [{ numberOfDays: 365, farmApr: 20, tokenPrice: 1 }, 221.34],
-])('calculate cake earned with values %o', ({ numberOfDays, farmApr, tokenPrice }, expected) => {
-  expect(tokenEarnedPerThousandDollarsCompounding({ numberOfDays, farmApr, tokenPrice })).toEqual(expected)
+  [{ numberOfDays: 1, farmApr: 365, tokenPrice: 1, performanceFee: 20 }, 8],
+  [{ numberOfDays: 7, farmApr: 20, tokenPrice: 0.8, performanceFee: 1 }, 4.75],
+  [{ numberOfDays: 40, farmApr: 212.21, tokenPrice: 1.2, performanceFee: 2 }, 212.63],
+  [{ numberOfDays: 330, farmApr: 45.12, tokenPrice: 5, performanceFee: 5 }, 94.6],
+  [{ numberOfDays: 365, farmApr: 100, tokenPrice: 0.2, performanceFee: 0 }, 8572.84],
+  [{ numberOfDays: 365, farmApr: 20, tokenPrice: 1, performanceFee: 0 }, 221.34],
+])('calculate cake earned with values %o', ({ numberOfDays, farmApr, tokenPrice, performanceFee }, expected) => {
+  expect(tokenEarnedPerThousandDollarsCompounding({ numberOfDays, farmApr, tokenPrice, performanceFee })).toEqual(
+    expected,
+  )
 })
 
 it.each([
