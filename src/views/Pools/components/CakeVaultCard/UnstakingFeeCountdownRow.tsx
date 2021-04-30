bec23a0b7e9674e015d88ddcbd780d6ca5ac14af
hachiojidev <hachiojidev@protonmail.com>
2021-04-30 14:01:35 +0900

    fix(pool): Include check of wallet address when showing staking fee message

--- src/views/Pools/components/CakeVaultCard/UnstakingFeeCountdownRow.tsx
+++ src/views/Pools/components/CakeVaultCard/UnstakingFeeCountdownRow.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Flex, Text } from '@pancakeswap-libs/uikit'
+import { useWeb3React } from '@web3-react/core'
 import useI18n from 'hooks/useI18n'
 import useWithdrawalFeeTimer from 'hooks/cakeVault/useWithdrawalFeeTimer'
 import WithdrawalFeeTimer from './WithdrawalFeeTimer'
@@ -22,8 +23,11 @@ const UnstakingFeeCountdownRow: React.FC<UnstakingFeeCountdownRowProps> = ({
     parseInt(lastDepositedTime, 10),
     parseInt(withdrawalFeePeriod, 10),
   )
+  // TODO: @HUTCH please rename the "account" prop above to something else. We use "account" everywhere in
+  // the app to refer to the connected wallet address
+  const { account: walletAddress } = useWeb3React()
 
-  const shouldShowTimer = account && lastDepositedTime && hasPerformanceFee
+  const shouldShowTimer = account && lastDepositedTime && hasPerformanceFee && walletAddress
 
   return (
     <Flex alignItems="center" justifyContent="space-between">
