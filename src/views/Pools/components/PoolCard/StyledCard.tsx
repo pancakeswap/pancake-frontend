72cc9921265f795ebf2380c7e3577eff18b762a7
memoyil <2213635+memoyil@users.noreply.github.com>
2021-04-30 13:32:15 +0200

    fix: (pools) Remove redundant box shadow in styled card

--- src/views/Pools/components/PoolCard/StyledCard.tsx
+++ src/views/Pools/components/PoolCard/StyledCard.tsx
@@ -5,7 +5,6 @@ const StyledCard = styled(Card)<{ isStaking?: boolean; isFinished?: boolean }>`
   max-width: 352px;
   margin: 0 8px 24px;
   background: ${(props) => props.theme.card.background};
-  box-shadow: 0px 2px 12px -8px rgba(25, 19, 38, 0.1), 0px 1px 1px rgba(25, 19, 38, 0.05);
   border-radius: 32px;
   display: flex;
   color: ${({ isFinished, theme }) => theme.colors[isFinished ? 'textDisabled' : 'secondary']};
