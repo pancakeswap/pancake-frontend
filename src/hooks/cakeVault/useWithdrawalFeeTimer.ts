ada5f25aaa6f9f92bf09222ef8ccb2ab401f4d65
Chef Hutch <79279477+ChefHutch@users.noreply.github.com>
2021-05-11 08:42:35 +0100

    feat(withdrawal-fee-timer): Make vault timer update per second that passes (#1156)
    * feat(withdrawal-fee-timer): Make vault timer update per second that passes
    
    * chore(withdrawal-fee-timer): Move setting interval into same effect as setting the seconds remaining

--- src/hooks/cakeVault/useWithdrawalFeeTimer.ts
+++ src/hooks/cakeVault/useWithdrawalFeeTimer.ts
@@ -3,17 +3,22 @@ import { useEffect, useState } from 'react'
 const useWithdrawalFeeTimer = (lastDepositedTime: number, withdrawalFeePeriod = 259200) => {
   const [secondsRemaining, setSecondsRemaining] = useState(null)
   const [hasUnstakingFee, setHasUnstakingFee] = useState(false)
+  const [currentSeconds, setCurrentSeconds] = useState(Math.floor(Date.now() / 1000))
 
   useEffect(() => {
-    const threeDaysFromDeposit = lastDepositedTime + withdrawalFeePeriod
-    const now = Math.floor(Date.now() / 1000)
-    const secondsRemainingCalc = threeDaysFromDeposit - now
+    const feeEndTime = lastDepositedTime + withdrawalFeePeriod
+    const secondsRemainingCalc = feeEndTime - currentSeconds
     const doesUnstakingFeeApply = secondsRemainingCalc > 0
     if (doesUnstakingFeeApply) {
       setSecondsRemaining(secondsRemainingCalc)
       setHasUnstakingFee(true)
     }
-  }, [lastDepositedTime, withdrawalFeePeriod, setSecondsRemaining])
+    const tick = () => {
+      setCurrentSeconds((prevSeconds) => prevSeconds + 1)
+    }
+    const timerInterval = setInterval(() => tick(), 1000)
+    return () => clearInterval(timerInterval)
+  }, [lastDepositedTime, withdrawalFeePeriod, setSecondsRemaining, currentSeconds])
 
   return { hasUnstakingFee, secondsRemaining }
 }
