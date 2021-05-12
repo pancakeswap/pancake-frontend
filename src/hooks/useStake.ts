a534e65ddc430a05e616b6bb9ff89a89c7f758cb
hachiojidev <hachiojidev@protonmail.com>
2021-05-12 13:37:30 +0900

    feat(pools): Pool staking limit hook

--- src/hooks/useStake.ts
+++ src/hooks/useStake.ts
@@ -1,9 +1,9 @@
-import { useCallback } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { useWeb3React } from '@web3-react/core'
 import { useAppDispatch } from 'state'
 import { fetchFarmUserDataAsync, updateUserStakedBalance, updateUserBalance } from 'state/actions'
 import { stake, sousStake, sousStakeBnb } from 'utils/callHelpers'
-import { useMasterchef, useSousChef } from './useContract'
+import { useMasterchef, useSousChef, useSousChefV2 } from './useContract'
 
 const useStake = (pid: number) => {
   const dispatch = useAppDispatch()
@@ -22,7 +22,7 @@ const useStake = (pid: number) => {
   return { onStake: handleStake }
 }
 
-export const useSousStake = (sousId, isUsingBnb = false) => {
+export const useSousStake = (sousId: number, isUsingBnb = false) => {
   const dispatch = useAppDispatch()
   const { account } = useWeb3React()
   const masterChefContract = useMasterchef()
@@ -37,13 +37,34 @@ export const useSousStake = (sousId, isUsingBnb = false) => {
       } else {
         await sousStake(sousChefContract, amount, decimals, account)
       }
-      dispatch(updateUserStakedBalance(sousId, account))
-      dispatch(updateUserBalance(sousId, account))
+      dispatch(updateUserStakedBalance(sousId.toString(), account))
+      dispatch(updateUserBalance(sousId.toString(), account))
     },
     [account, dispatch, isUsingBnb, masterChefContract, sousChefContract, sousId],
   )
 
   return { onStake: handleStake }
+}
+
+export const usePoolStakingLimit = (sousId: number) => {
+  const [poolLimit, setPoolLimit] = useState(0)
+  const sousChefV2Contract = useSousChefV2(sousId)
+
+  useEffect(() => {
+    const fetchPoolLimit = async () => {
+      try {
+        const limit = await sousChefV2Contract.methods.poolLimitPerUser().call()
+        setPoolLimit(parseInt(limit, 10))
+      } catch (error) {
+        // V1 pools do not contain "poolLimitPerUser" and is not supported
+        setPoolLimit(0)
+      }
+    }
+
+    fetchPoolLimit()
+  }, [setPoolLimit, sousChefV2Contract])
+
+  return poolLimit
 }
 
 export default useStake
