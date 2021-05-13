e703006bd8f5666ec9a06a2e31eb7f0efdcf25bb
hachiojidev <hachiojidev@protonmail.com>
2021-05-13 16:15:37 +0900

    test: Pool helpers

--- src/state/pools/helpers.ts
+++ src/state/pools/helpers.ts
@@ -1,15 +1,22 @@
 import BigNumber from 'bignumber.js'
 import { Pool } from 'state/types'
 
-export const transformUserData = (userData: Pool['userData']) => {
-  return userData
-    ? {
-        allowance: new BigNumber(userData.allowance),
-        stakingTokenBalance: new BigNumber(userData.stakingTokenBalance),
-        stakedBalance: new BigNumber(userData.stakedBalance),
-        pendingReward: new BigNumber(userData.pendingReward),
-      }
-    : userData
+type UserData =
+  | Pool['userData']
+  | {
+      allowance: number | string
+      stakingTokenBalance: number | string
+      stakedBalance: number | string
+      pendingReward: number | string
+    }
+
+export const transformUserData = (userData: UserData) => {
+  return {
+    allowance: new BigNumber(userData.allowance || 0),
+    stakingTokenBalance: new BigNumber(userData.stakingTokenBalance || 0),
+    stakedBalance: new BigNumber(userData.stakedBalance || 0),
+    pendingReward: new BigNumber(userData.pendingReward || 0),
+  }
 }
 
 export const transformPool = (pool: Pool): Pool => {
