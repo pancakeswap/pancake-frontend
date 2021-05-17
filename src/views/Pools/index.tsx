1d6ceb59bd24c192ba380db16a3704215c8df13b
memoyil <2213635+memoyil@users.noreply.github.com>
2021-05-17 07:03:21 +0200

    feat: (pools) Add infinite scroll and lazy loading pool cards (#1211)

--- src/views/Pools/index.tsx
+++ src/views/Pools/index.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
 import { Route, useRouteMatch } from 'react-router-dom'
 import BigNumber from 'bignumber.js'
 import { useWeb3React } from '@web3-react/core'
@@ -16,6 +16,8 @@ import CakeVaultCard from './components/CakeVaultCard'
 import PoolTabButtons from './components/PoolTabButtons'
 import BountyCard from './components/BountyCard'
 
+const NUMBER_OF_POOLS_VISIBLE = 12
+
 const Pools: React.FC = () => {
   const { path } = useRouteMatch()
   const { t } = useTranslation()
@@ -23,6 +25,9 @@ const Pools: React.FC = () => {
   const pools = usePools(account)
   const { currentBlock } = useBlock()
   const [stakedOnly, setStakedOnly] = usePersistState(false, 'pancake_pool_staked')
+  const [numberOfPoolsVisible, setNumberOfPoolsVisible] = useState(NUMBER_OF_POOLS_VISIBLE)
+  const [observerIsSet, setObserverIsSet] = useState(false)
+  const loadMoreRef = useRef<HTMLDivElement>(null)
 
   const [finishedPools, openPools] = useMemo(
     () => partition(pools, (pool) => pool.isFinished || currentBlock > pool.endBlock),
@@ -40,6 +45,24 @@ const Pools: React.FC = () => {
 
   // This pool is passed explicitly to the cake vault
   const cakePoolData = useMemo(() => openPools.find((pool) => pool.sousId === 0), [openPools])
+
+  useEffect(() => {
+    const showMorePools = (entries) => {
+      const [entry] = entries
+      if (entry.isIntersecting) {
+        setNumberOfPoolsVisible((poolsCurrentlyVisible) => poolsCurrentlyVisible + NUMBER_OF_POOLS_VISIBLE)
+      }
+    }
+
+    if (!observerIsSet) {
+      const loadMoreObserver = new IntersectionObserver(showMorePools, {
+        rootMargin: '0px',
+        threshold: 1,
+      })
+      loadMoreObserver.observe(loadMoreRef.current)
+      setObserverIsSet(true)
+    }
+  }, [observerIsSet])
 
   return (
     <>
@@ -72,24 +95,25 @@ const Pools: React.FC = () => {
             <>
               <CakeVaultCard pool={cakePoolData} showStakedOnly={stakedOnly} />
               {stakedOnly
-                ? orderBy(stakedOnlyOpenPools, ['sortOrder']).map((pool) => (
-                    <PoolCard key={pool.sousId} pool={pool} account={account} />
-                  ))
-                : orderBy(openPools, ['sortOrder']).map((pool) => (
-                    <PoolCard key={pool.sousId} pool={pool} account={account} />
-                  ))}
+                ? orderBy(stakedOnlyOpenPools, ['sortOrder'])
+                    .slice(0, numberOfPoolsVisible)
+                    .map((pool) => <PoolCard key={pool.sousId} pool={pool} account={account} />)
+                : orderBy(openPools, ['sortOrder'])
+                    .slice(0, numberOfPoolsVisible)
+                    .map((pool) => <PoolCard key={pool.sousId} pool={pool} account={account} />)}
             </>
           </Route>
           <Route path={`${path}/history`}>
             {stakedOnly
-              ? orderBy(stakedOnlyFinishedPools, ['sortOrder']).map((pool) => (
-                  <PoolCard key={pool.sousId} pool={pool} account={account} />
-                ))
-              : orderBy(finishedPools, ['sortOrder']).map((pool) => (
-                  <PoolCard key={pool.sousId} pool={pool} account={account} />
-                ))}
+              ? orderBy(stakedOnlyFinishedPools, ['sortOrder'])
+                  .slice(0, numberOfPoolsVisible)
+                  .map((pool) => <PoolCard key={pool.sousId} pool={pool} account={account} />)
+              : orderBy(finishedPools, ['sortOrder'])
+                  .slice(0, numberOfPoolsVisible)
+                  .map((pool) => <PoolCard key={pool.sousId} pool={pool} account={account} />)}
           </Route>
         </FlexLayout>
+        <div ref={loadMoreRef} />
         <Image
           mx="auto"
           mt="12px"
