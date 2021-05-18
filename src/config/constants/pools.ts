32f6b437c9dd98257b893b820763249a2b9a54b0
Chef Chungus <71833681+Chef-Chungus@users.noreply.github.com>
2021-05-18 17:41:08 +0900

    feat(farm/pool): UBXT - 1800 (#1233)
    * feat(farm/pool): UBXT
    
    * fix: Sous ID

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -17,6 +17,20 @@ const pools: PoolConfig[] = [
     isFinished: false,
   },
   {
+    sousId: 148,
+    stakingToken: tokens.cake,
+    earningToken: tokens.ubxt,
+    contractAddress: {
+      97: '',
+      56: '0x9b4bac2d8f69853aa29cb45478c77fc54532ac22',
+    },
+    poolCategory: PoolCategory.CORE,
+    harvest: true,
+    sortOrder: 999,
+    tokenPerBlock: '2.4074',
+    isFinished: false,
+  },
+  {
     sousId: 147,
     stakingToken: tokens.cake,
     earningToken: tokens.btr,
