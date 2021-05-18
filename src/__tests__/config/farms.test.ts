44789383fb9ae225f3c1b31d40267f4cc6d9eb91
hachiojidev <hachiojidev@protonmail.com>
2021-05-18 18:55:27 +0900

    test: Added test to check new farms' factory address

--- src/__tests__/config/farms.test.ts
+++ src/__tests__/config/farms.test.ts
@@ -1,18 +1,21 @@
 import farms from 'config/constants/farms'
+import { Farm } from 'state/types'
 import { getBep20Contract, getLpContract } from 'utils/contractHelpers'
 
-const farmsToTest = farms.filter((farm) => farm.pid !== 0)
+const farmsToTest: [number, Farm][] = farms.filter((farm) => farm.pid !== 0).map((farm) => [farm.pid, farm])
 
 describe('Config farms', () => {
-  it.each(farms.map((farm) => farm.pid))('Farm #%d has an unique pid', (pid) => {
+  it.each(farmsToTest)('Farm #%d has an unique pid', (pid) => {
     const duplicates = farms.filter((f) => pid === f.pid)
     expect(duplicates).toHaveLength(1)
   })
-  it.each(farms.map((farm) => [farm.pid, farm.lpAddresses]))('Farm #%d has an unique address', (pid, lpAddresses) => {
-    const duplicates = farms.filter((f) => lpAddresses[56] === f.lpAddresses[56])
+
+  it.each(farmsToTest)('Farm #%d has an unique address', (pid, farm) => {
+    const duplicates = farms.filter((f) => farm.lpAddresses[56] === f.lpAddresses[56])
     expect(duplicates).toHaveLength(1)
   })
-  it.each(farmsToTest)('Farm %p has the correct token addresses', async (farm) => {
+
+  it.each(farmsToTest)('Farm %d has the correct token addresses', async (pid, farm) => {
     const tokenAddress = farm.token.address[56]
     const quoteTokenAddress = farm.quoteToken.address[56]
     const lpContract = getLpContract(farm.lpAddresses[56])
@@ -27,7 +30,8 @@ describe('Config farms', () => {
       token1Address === tokenAddress.toLowerCase() || token1Address === quoteTokenAddress.toLowerCase(),
     ).toBeTruthy()
   })
-  it.each(farmsToTest)('Farm %p has non 0 tokens amount', async (farm) => {
+
+  it.each(farmsToTest)('Farm %d has non 0 tokens amount', async (pid, farm) => {
     const tokenContract = getBep20Contract(farm.token.address[56])
     const quoteTokenContract = getBep20Contract(farm.quoteToken.address[56])
 
@@ -36,5 +40,16 @@ describe('Config farms', () => {
 
     expect(parseInt(tokenAmount, 10)).toBeGreaterThan(0)
     expect(parseInt(quoteTokenAmount, 10)).toBeGreaterThan(0)
+  })
+
+  // The first pid using the new factory
+  const START_PID = 365
+  const FACTORY_ADDRESS = '0xca143ce32fe78f1f7019d7d551a6402fc5350c73'
+  const newFarmsToTest = farmsToTest.filter((farmSet) => farmSet[0] >= START_PID)
+
+  it.each(newFarmsToTest)('farm %d is using correct factory address', async (pid, farm) => {
+    const lpContract = getLpContract(farm.lpAddresses[56])
+    const factory = await lpContract.methods.factory().call()
+    expect(factory.toLowerCase()).toEqual(FACTORY_ADDRESS)
   })
 })
