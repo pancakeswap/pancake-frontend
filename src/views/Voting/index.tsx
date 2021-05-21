9ffa94a13d5156f09646f502f900410400cf9e3e
hachiojidev <hachiojidev@protonmail.com>
2021-05-21 16:20:55 +0900

    style(voting): Voting page layout

--- src/views/Voting/index.tsx
+++ src/views/Voting/index.tsx
@@ -1,8 +1,19 @@
 import React, { useEffect } from 'react'
+import { Flex } from '@pancakeswap/uikit'
+import styled from 'styled-components'
 import Hero from './components/Hero'
 import Footer from './components/Footer'
 import { Proposals } from './components/Proposals'
 import { getProposals } from './helpers'
+
+const Chrome = styled.div`
+  flex: none;
+`
+
+const Content = styled.div`
+  flex: 1;
+  height: 100%;
+`
 
 const Voting = () => {
   useEffect(() => {
@@ -10,11 +21,17 @@ const Voting = () => {
   }, [])
 
   return (
-    <>
-      <Hero />
-      <Proposals />
-      <Footer />
-    </>
+    <Flex flexDirection="column" minHeight="calc(100vh - 64px)">
+      <Chrome>
+        <Hero />
+      </Chrome>
+      <Content>
+        <Proposals />
+      </Content>
+      <Chrome>
+        <Footer />
+      </Chrome>
+    </Flex>
   )
 }
 
