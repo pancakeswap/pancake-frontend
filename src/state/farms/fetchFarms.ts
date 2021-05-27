b1abed496a889941ad228609f035e437d703714a
hachiojidev <hachiojidev@protonmail.com>
2021-05-27 12:18:29 +0900

    refactor(farms): Fetch farm helper

--- src/state/farms/fetchFarms.ts
+++ src/state/farms/fetchFarms.ts
@@ -1,16 +1,11 @@
 import { FarmConfig } from 'config/constants/types'
-import fetchPublicFarmData from './fetchPublicFarmData'
+import fetchFarm from './fetchFarm'
 
 const fetchFarms = async (farmsToFetch: FarmConfig[]) => {
   const data = await Promise.all(
     farmsToFetch.map(async (farmConfig) => {
-      const { pid, lpAddresses, token, quoteToken } = farmConfig
-      const publicFarmData = await fetchPublicFarmData(pid, lpAddresses, token, quoteToken)
-
-      return {
-        ...farmConfig,
-        ...publicFarmData,
-      }
+      const farm = await fetchFarm(farmConfig)
+      return farm
     }),
   )
   return data
