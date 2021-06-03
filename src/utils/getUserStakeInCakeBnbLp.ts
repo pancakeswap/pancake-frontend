d0bbeef67531ef3cc89cfbc1f8c6b31ad08a64e4
hachiojidev <hachiojidev@protonmail.com>
2021-06-03 10:31:22 +0900

    feat: Utility to get user cake balance in cake-bnb lp

---
+++ src/utils/getUserStakeInCakeBnbLp.ts
@@ -0,0 +1,42 @@
+import { Pair, TokenAmount, Token } from '@pancakeswap-libs/sdk-v2'
+import { getLpContract, getMasterchefContract } from 'utils/contractHelpers'
+import { BIG_ZERO } from 'utils/bigNumber'
+import farms from 'config/constants/farms'
+import { getAddress, getCakeAddress } from 'utils/addressHelpers'
+import tokens from 'config/constants/tokens'
+
+const chainId = parseInt(process.env.REACT_APP_CHAIN_ID, 10)
+const cakeBnbPid = 251
+const cakeBnbFarm = farms.find((farm) => farm.pid === cakeBnbPid)
+
+const CAKE_TOKEN = new Token(chainId, getCakeAddress(), 18)
+const WBNB_TOKEN = new Token(chainId, tokens.wbnb.address[chainId], 18)
+const CAKE_BNB_TOKEN = new Token(chainId, getAddress(cakeBnbFarm.lpAddresses), 18)
+
+const getUserStakeInCakeBnbLp = async (account: string, block?: number) => {
+  try {
+    const masterContract = getMasterchefContract()
+    const cakeBnbContract = getLpContract(getAddress(cakeBnbFarm.lpAddresses))
+    const totalSupplyLP = await cakeBnbContract.methods.totalSupply().call(undefined, block)
+    const reservesLP = await cakeBnbContract.methods.getReserves().call(undefined, block)
+    const cakeBnbBalance = await masterContract.methods.userInfo(cakeBnbPid, account).call(undefined, block)
+
+    const pair: Pair = new Pair(
+      new TokenAmount(CAKE_TOKEN, reservesLP._reserve0.toString()),
+      new TokenAmount(WBNB_TOKEN, reservesLP._reserve1.toString()),
+    )
+    const cakeLPBalance = pair.getLiquidityValue(
+      pair.token0,
+      new TokenAmount(CAKE_BNB_TOKEN, totalSupplyLP.toString()),
+      new TokenAmount(CAKE_BNB_TOKEN, cakeBnbBalance.amount.toString()),
+      false,
+    )
+
+    return cakeLPBalance.toSignificant(18)
+  } catch (error) {
+    console.error(`CAKE-BNB LP error: ${error}`)
+    return BIG_ZERO
+  }
+}
+
+export default getUserStakeInCakeBnbLp
