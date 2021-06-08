f2a73243c1646826a918d4b4d0e7a80656de67c1
memoyil <2213635+memoyil@users.noreply.github.com>
2021-06-08 12:12:03 +0200

    fix: (useAuth) Translations not applied to error messages when language changes

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -23,38 +23,40 @@ const useAuth = () => {
   const { activate, deactivate } = useWeb3React()
   const { toastError } = useToast()
 
-  const login = useCallback((connectorID: ConnectorNames) => {
-    const connector = connectorsByName[connectorID]
-    if (connector) {
-      activate(connector, async (error: Error) => {
-        if (error instanceof UnsupportedChainIdError) {
-          const hasSetup = await setupNetwork()
-          if (hasSetup) {
-            activate(connector)
-          }
-        } else {
-          window.localStorage.removeItem(connectorLocalStorageKey)
-          if (error instanceof NoEthereumProviderError || error instanceof NoBscProviderError) {
-            toastError(t('Provider Error'), t('No provider was found'))
-          } else if (
-            error instanceof UserRejectedRequestErrorInjected ||
-            error instanceof UserRejectedRequestErrorWalletConnect
-          ) {
-            if (connector instanceof WalletConnectConnector) {
-              const walletConnector = connector as WalletConnectConnector
-              walletConnector.walletConnectProvider = null
+  const login = useCallback(
+    (connectorID: ConnectorNames) => {
+      const connector = connectorsByName[connectorID]
+      if (connector) {
+        activate(connector, async (error: Error) => {
+          if (error instanceof UnsupportedChainIdError) {
+            const hasSetup = await setupNetwork()
+            if (hasSetup) {
+              activate(connector)
             }
-            toastError(t('Authorization Error'), t('Please authorize to access your account'))
           } else {
-            toastError(error.name, error.message)
+            window.localStorage.removeItem(connectorLocalStorageKey)
+            if (error instanceof NoEthereumProviderError || error instanceof NoBscProviderError) {
+              toastError(t('Provider Error'), t('No provider was found'))
+            } else if (
+              error instanceof UserRejectedRequestErrorInjected ||
+              error instanceof UserRejectedRequestErrorWalletConnect
+            ) {
+              if (connector instanceof WalletConnectConnector) {
+                const walletConnector = connector as WalletConnectConnector
+                walletConnector.walletConnectProvider = null
+              }
+              toastError(t('Authorization Error'), t('Please authorize to access your account'))
+            } else {
+              toastError(error.name, error.message)
+            }
           }
-        }
-      })
-    } else {
-      toastError(t('Unable to find connector'), t('The connector config is wrong'))
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+        })
+      } else {
+        toastError(t('Unable to find connector'), t('The connector config is wrong'))
+      }
+    },
+    [t, activate, toastError],
+  )
 
   const logout = useCallback(() => {
     dispatch(profileClear())
