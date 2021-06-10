39f0a68bd7f6ff50f3b465463f3c30186b05441e
ChefHutch <chef.hutch.pcs@protonmail.com>
2021-06-10 10:42:03 +0100

    fix(contract-helpers): Add getGasPrice helper and chainid consts to set gas based on chainid

--- src/utils/addressHelpers.ts
+++ src/utils/addressHelpers.ts
@@ -1,11 +1,11 @@
+import { MAINNET_CHAIN_ID } from 'config'
 import addresses from 'config/constants/contracts'
 import tokens from 'config/constants/tokens'
 import { Address } from 'config/constants/types'
 
 export const getAddress = (address: Address): string => {
-  const mainNetChainId = 56
   const chainId = process.env.REACT_APP_CHAIN_ID
-  return address[chainId] ? address[chainId] : address[mainNetChainId]
+  return address[chainId] ? address[chainId] : address[MAINNET_CHAIN_ID]
 }
 
 export const getCakeAddress = () => {
--- src/utils/contractHelpers.ts
+++ src/utils/contractHelpers.ts
@@ -50,15 +50,25 @@ import easterNftAbi from 'config/abi/easterNft.json'
 import cakeVaultAbi from 'config/abi/cakeVault.json'
 import predictionsAbi from 'config/abi/predictions.json'
 import chainlinkOracleAbi from 'config/abi/chainlinkOracle.json'
-import { DEFAULT_GAS_PRICE } from 'config'
+import { DEFAULT_GAS_PRICE, TESTNET_CHAIN_ID } from 'config'
 import { getSettings, getGasPriceInWei } from './settings'
+
+export const getGasPrice = (account?: string) => {
+  const chainId = process.env.REACT_APP_CHAIN_ID
+  if (chainId === TESTNET_CHAIN_ID) {
+    return 10
+  }
+  if (account) {
+    return getSettings(account).gasPrice
+  }
+  return DEFAULT_GAS_PRICE
+}
 
 const getContract = (abi: any, address: string, web3?: Web3, account?: string) => {
   const _web3 = web3 ?? web3NoAccount
-  const gasPrice = account ? getSettings(account).gasPrice : DEFAULT_GAS_PRICE
 
   return new _web3.eth.Contract(abi as unknown as AbiItem, address, {
-    gasPrice: getGasPriceInWei(gasPrice).toString(),
+    gasPrice: getGasPriceInWei(getGasPrice(account)).toString(),
   })
 }
 
