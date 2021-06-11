bfbe506e63ef7a243d7425d07fb59c9da9d5a06b
memoyil <2213635+memoyil@users.noreply.github.com>
2021-06-11 06:57:54 +0200

    feat: (connect) Change how to connect link with more accurate one (#152)

--- packages/pancake-uikit/src/widgets/WalletModal/ConnectModal.tsx
+++ packages/pancake-uikit/src/widgets/WalletModal/ConnectModal.tsx
@@ -30,7 +30,7 @@ const ConnectModal: React.FC<Props> = ({ login, onDismiss = () => null }) => (
         mb={index < config.length - 1 ? "8px" : "0"}
       />
     ))}
-    <HelpLink href="https://docs.pancakeswap.finance/help/faq#how-do-i-connect-my-wallet-to-pancakeswap" external>
+    <HelpLink href="https://docs.pancakeswap.finance/get-started/connection-guide" external>
       <HelpIcon color="primary" mr="6px" />
       Learn how to connect
     </HelpLink>
