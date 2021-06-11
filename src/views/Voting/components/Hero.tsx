5c3e9f9095ab0bdc7e12fb59836e8eca0d1926b9
hachiojidev <hachiojidev@protonmail.com>
2021-06-11 16:15:30 +0900

    style: Add hero image

--- src/views/Voting/components/Hero.tsx
+++ src/views/Voting/components/Hero.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Button, Heading, ProposalIcon } from '@pancakeswap/uikit'
+import { Box, Button, Flex, Heading, ProposalIcon, Image } from '@pancakeswap/uikit'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import { useTranslation } from 'contexts/Localization'
@@ -11,23 +11,38 @@ const StyledHero = styled(Box)`
   padding-top: 32px;
 `
 
+const DesktopImage = styled(Image)`
+  display: none;
+
+  ${({ theme }) => theme.mediaQueries.lg} {
+    display: block;
+  }
+`
+
 const Hero = () => {
   const { t } = useTranslation()
 
   return (
     <StyledHero>
       <Container>
-        <Box>
-          <Heading as="h1" scale="xxl" color="secondary" mb="16px">
-            {t('Voting')}
-          </Heading>
-          <Heading as="h3" scale="lg" mb="16px">
-            {t('Have your say in the future of the PancakeSwap Ecosystem')}
-          </Heading>
-          <Button startIcon={<ProposalIcon color="currentColor" width="24px" />} as={Link} to="/voting/proposal/create">
-            {t('Make a Proposal')}
-          </Button>
-        </Box>
+        <Flex justifyContent="space-between">
+          <Box>
+            <Heading as="h1" scale="xxl" color="secondary" mb="16px">
+              {t('Voting')}
+            </Heading>
+            <Heading as="h3" scale="lg" mb="16px">
+              {t('Have your say in the future of the PancakeSwap Ecosystem')}
+            </Heading>
+            <Button
+              startIcon={<ProposalIcon color="currentColor" width="24px" />}
+              as={Link}
+              to="/voting/proposal/create"
+            >
+              {t('Make a Proposal')}
+            </Button>
+          </Box>
+          <DesktopImage src="/images/voting/voting-presents.png" width={349} height={176} />
+        </Flex>
       </Container>
     </StyledHero>
   )
--- src/views/Voting/components/Proposals/TabMenu.tsx
+++ src/views/Voting/components/Proposals/TabMenu.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
-import { TabMenu as UIKitTabMenu, Tab } from '@pancakeswap/uikit'
+import { TabMenu as UIKitTabMenu, Tab, Flex, VerifiedIcon, CommunityIcon } from '@pancakeswap/uikit'
+import { useTranslation } from 'contexts/Localization'
 import { ProposalType } from '../../types'
 
 interface TabMenuProps {
@@ -37,6 +38,7 @@ const getTypeFromIndex = (index: number) => {
 }
 
 const TabMenu: React.FC<TabMenuProps> = ({ proposalType, onTypeChange }) => {
+  const { t } = useTranslation()
   const handleItemClick = (index: number) => {
     onTypeChange(getTypeFromIndex(index))
   }
@@ -44,8 +46,19 @@ const TabMenu: React.FC<TabMenuProps> = ({ proposalType, onTypeChange }) => {
   return (
     <StyledTabMenu>
       <UIKitTabMenu activeIndex={getIndexFromType(proposalType)} onItemClick={handleItemClick}>
-        <Tab>Core</Tab>
-        <Tab>Community</Tab>
+        <Tab>
+          <Flex alignItems="center">
+            <VerifiedIcon color="currentColor" mr="4px" />
+            {t('Core')}
+          </Flex>
+        </Tab>
+        <Tab>
+          {' '}
+          <Flex alignItems="center">
+            <CommunityIcon color="currentColor" mr="4px" />
+            {t('Community')}
+          </Flex>
+        </Tab>
         <Tab>All</Tab>
       </UIKitTabMenu>
     </StyledTabMenu>
