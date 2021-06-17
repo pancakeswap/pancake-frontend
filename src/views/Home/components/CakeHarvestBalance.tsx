96e8e5b85e0a5555032ae8e30340b0a4d0df4e39
memoyil <2213635+memoyil@users.noreply.github.com>
2021-06-17 04:41:48 +0200

    feat: (farmstakingcard) Remove duplicate farm balance requests (#1513)

--- src/views/Home/components/CakeHarvestBalance.tsx
+++ src/views/Home/components/CakeHarvestBalance.tsx
@@ -3,7 +3,7 @@ import { Text } from '@pancakeswap/uikit'
 import { useWeb3React } from '@web3-react/core'
 import BigNumber from 'bignumber.js'
 import { useTranslation } from 'contexts/Localization'
-import useAllEarnings from 'hooks/useAllEarnings'
+import { FarmWithBalance } from 'hooks/useFarmsWithBalance'
 import { usePriceCakeBusd } from 'state/hooks'
 import styled from 'styled-components'
 import { DEFAULT_TOKEN_DECIMAL } from 'config'
@@ -14,12 +14,15 @@ const Block = styled.div`
   margin-bottom: 24px;
 `
 
-const CakeHarvestBalance = () => {
+interface CakeHarvestBalanceProps {
+  farmsWithBalance: FarmWithBalance[]
+}
+
+const CakeHarvestBalance: React.FC<CakeHarvestBalanceProps> = ({ farmsWithBalance }) => {
   const { t } = useTranslation()
   const { account } = useWeb3React()
-  const allEarnings = useAllEarnings()
-  const earningsSum = allEarnings.reduce((accum, earning) => {
-    const earningNumber = new BigNumber(earning)
+  const earningsSum = farmsWithBalance.reduce((accum, earning) => {
+    const earningNumber = new BigNumber(earning.balance)
     if (earningNumber.eq(0)) {
       return accum
     }
--- src/views/Home/components/FarmStakingCard.tsx
+++ src/views/Home/components/FarmStakingCard.tsx
@@ -6,6 +6,7 @@ import { useWeb3React } from '@web3-react/core'
 import { useTranslation } from 'contexts/Localization'
 import useFarmsWithBalance from 'hooks/useFarmsWithBalance'
 import { useMasterchef } from 'hooks/useContract'
+import useToast from 'hooks/useToast'
 import UnlockButton from 'components/UnlockButton'
 import CakeHarvestBalance from './CakeHarvestBalance'
 import CakeWalletBalance from './CakeWalletBalance'
@@ -38,9 +39,10 @@ const FarmedStakingCard = () => {
   const [pendingTx, setPendingTx] = useState(false)
   const { account } = useWeb3React()
   const { t } = useTranslation()
+  const { toastError } = useToast()
   const farmsWithBalance = useFarmsWithBalance()
   const masterChefContract = useMasterchef()
-  const balancesWithValue = farmsWithBalance.filter((balanceType) => balanceType.balance.toNumber() > 0)
+  const balancesWithValue = farmsWithBalance.filter((balanceType) => balanceType.balance.gt(0))
 
   const harvestAllFarms = useCallback(async () => {
     setPendingTx(true)
@@ -50,11 +52,11 @@ const FarmedStakingCard = () => {
         // eslint-disable-next-line no-await-in-loop
         await harvest(masterChefContract, farmWithBalance.pid, account)
       } catch (error) {
-        // TODO: find a way to handle when the user rejects transaction or it fails
+        toastError(t('Error'), error?.message)
       }
     }
     setPendingTx(false)
-  }, [account, balancesWithValue, masterChefContract])
+  }, [account, balancesWithValue, masterChefContract, toastError, t])
 
   return (
     <StyledFarmStakingCard>
@@ -65,7 +67,7 @@ const FarmedStakingCard = () => {
         <CardImage src="/images/cake.svg" alt="cake logo" width={64} height={64} />
         <Block>
           <Label>{t('CAKE to Harvest')}:</Label>
-          <CakeHarvestBalance />
+          <CakeHarvestBalance farmsWithBalance={balancesWithValue} />
         </Block>
         <Block>
           <Label>{t('CAKE in Wallet')}:</Label>
