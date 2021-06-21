f82b8ede97f0c6c9f3c97f841cd31eaf8598766f
RabbitDoge <72658581+RabbitDoge@users.noreply.github.com>
2021-06-21 16:26:43 +0900

    chore: Migrate multicall

--- src/utils/multicall.ts
+++ src/utils/multicall.ts
@@ -1,13 +1,5 @@
-import Web3 from 'web3'
-import { Interface } from '@ethersproject/abi'
-import getRpcUrl from 'utils/getRpcUrl'
-import MultiCallAbi from 'config/abi/Multicall.json'
-import { getMulticallAddress } from 'utils/addressHelpers'
-
-const RPC_URL = getRpcUrl()
-
-const httpProvider = new Web3.providers.HttpProvider(RPC_URL, { timeout: 10000 })
-const web3NoAccount = new Web3(httpProvider)
+import { ethers } from 'ethers'
+import { getMulticallContract } from 'utils/contractHelpers'
 
 interface Call {
   address: string // Address of the contract
@@ -16,20 +8,17 @@ interface Call {
 }
 
 interface MulticallOptions {
-  web3?: Web3
-  blockNumber?: number
   requireSuccess?: boolean
 }
 
-const multicall = async (abi: any[], calls: Call[], options: MulticallOptions = {}) => {
-  const { web3, blockNumber } = options
+const multicall = async (abi: any[], calls: Call[]) => {
   try {
-    const provider = web3 || web3NoAccount
-    const multi = new provider.eth.Contract(MultiCallAbi as any, getMulticallAddress())
-    const itf = new Interface(abi)
+    const multi = getMulticallContract()
+    const itf = new ethers.utils.Interface(abi)
 
     const calldata = calls.map((call) => [call.address.toLowerCase(), itf.encodeFunctionData(call.name, call.params)])
-    const { returnData } = await multi.methods.aggregate(calldata).call(undefined, blockNumber)
+    const { returnData } = await multi.aggregate(calldata)
+
     const res = returnData.map((call, i) => itf.decodeFunctionResult(calls[i].name, call))
 
     return res
@@ -44,16 +33,17 @@ const multicall = async (abi: any[], calls: Call[], options: MulticallOptions = 
  * 1. If "requireSuccess" is false multicall will not bail out if one of the calls fails
  * 2. The return inclues a boolean whether the call was successful e.g. [wasSuccessfull, callResult]
  */
-export const multicallv2 = async (abi: any[], calls: Call[], options: MulticallOptions = {}): Promise<any> => {
-  const { web3, requireSuccess, blockNumber } = options
-  const provider = web3 || web3NoAccount
-  const multi = new provider.eth.Contract(MultiCallAbi as any, getMulticallAddress())
-  const itf = new Interface(abi)
+export const multicallv2 = async (
+  abi: any[],
+  calls: Call[],
+  options: MulticallOptions = { requireSuccess: true },
+): Promise<any> => {
+  const { requireSuccess } = options
+  const multi = getMulticallContract()
+  const itf = new ethers.utils.Interface(abi)
 
   const calldata = calls.map((call) => [call.address.toLowerCase(), itf.encodeFunctionData(call.name, call.params)])
-  const returnData = await multi.methods
-    .tryAggregate(requireSuccess === undefined ? true : requireSuccess, calldata)
-    .call(undefined, blockNumber)
+  const returnData = await multi.tryAggregate(requireSuccess, calldata)
   const res = returnData.map((call, i) => {
     const [result, data] = call
     return result ? itf.decodeFunctionResult(calls[i].name, data) : null
