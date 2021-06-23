27ef3b81eb4f56f64ef9eed2608fbabcae8f8e73
Chef Hachioji <72719739+hachiojidev@users.noreply.github.com>
2021-06-23 18:07:14 +0900

    chore: Add id to prediction CTA (#1558)

--- src/views/Home/components/PredictionPromotionCard.tsx
+++ src/views/Home/components/PredictionPromotionCard.tsx
@@ -124,7 +124,7 @@ const PredictionPromotionCard = () => {
         <Text color="textSubtle" mb="24px">
           *{t('Beta Version')}
         </Text>
-        <Link href="https://pancakeswap.finance/prediction">
+        <Link href="https://pancakeswap.finance/prediction" id="homepage-prediction-cta">
           <Button px={['14px', null, null, null, '20px']}>
             <Flex alignItems="center">
               <Text color="white" bold fontSize="16px">
