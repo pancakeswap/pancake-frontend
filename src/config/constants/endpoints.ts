a9d6e9481ca748d9a2b0ee66b8924a6489dcfb83
ChefHutch <chef.hutch.pcs@protonmail.com>
2021-06-24 08:20:32 +0100

    chore(endpoints): Move subgraph URL out of config

--- src/config/constants/endpoints.ts
+++ src/config/constants/endpoints.ts
@@ -1,4 +1,4 @@
-import { BASE_SUBGRAPH_URL } from 'config'
+const BASE_SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/pancakeswap/'
 
 export const GRAPH_API_PROFILE = `${BASE_SUBGRAPH_URL}/profile`
 export const GRAPH_API_PREDICTION = `${BASE_SUBGRAPH_URL}/prediction`
--- src/config/index.ts
+++ src/config/index.ts
@@ -24,7 +24,6 @@ export const BASE_EXCHANGE_URL = 'https://exchange.pancakeswap.finance'
 export const BASE_ADD_LIQUIDITY_URL = `${BASE_EXCHANGE_URL}/#/add`
 export const BASE_LIQUIDITY_POOL_URL = `${BASE_EXCHANGE_URL}/#/pool`
 export const BASE_BSC_SCAN_URL = 'https://bscscan.com'
-export const BASE_SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/pancakeswap'
 export const LOTTERY_MAX_NUMBER_OF_TICKETS = 50
 export const LOTTERY_TICKET_PRICE = 1
 export const DEFAULT_TOKEN_DECIMAL = BIG_TEN.pow(18)
