d00f045c6d258b3d789d8a6abedca464011a64cc
memoyil <2213635+memoyil@users.noreply.github.com>
2021-06-26 15:13:28 +0200

    fix: (Aprcalculatormodal) No space between modal bullet and text in bullet list

--- src/components/ApyCalculatorModal/index.tsx
+++ src/components/ApyCalculatorModal/index.tsx
@@ -31,6 +31,20 @@ const GridHeaderItem = styled.div`
 `
 
 const BulletList = styled.ul`
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+
+  li {
+    margin: 0;
+    padding: 0;
+  }
+
+  li::before {
+    content: '•';
+    margin-right: 4px;
+  }
+
   li::marker {
     font-size: 12px;
     color: ${({ theme }) => theme.colors.textSubtle};
@@ -187,24 +201,24 @@ const ApyCalculatorModal: React.FC<ApyCalculatorModalProps> = ({
         <Box mb="28px" maxWidth="280px" p="4px">
           <BulletList>
             <li>
-              <Text ml="-8px" fontSize="12px" textAlign="center" color="textSubtle" display="inline">
+              <Text fontSize="12px" textAlign="center" color="textSubtle" display="inline">
                 {t('Calculated based on current rates.')}
               </Text>
             </li>
             <li>
-              <Text ml="-8px" fontSize="12px" textAlign="center" color="textSubtle" display="inline">
+              <Text fontSize="12px" textAlign="center" color="textSubtle" display="inline">
                 {t('Compounding %freq%x daily.', { freq: compoundFrequency.toLocaleString() })}
               </Text>
             </li>
             {isFarm && (
               <li>
-                <Text ml="-8px" fontSize="12px" textAlign="center" color="textSubtle" display="inline">
+                <Text fontSize="12px" textAlign="center" color="textSubtle" display="inline">
                   {t('LP rewards: 0.17% trading fees, distributed proportionally among LP token holders.')}
                 </Text>
               </li>
             )}
             <li>
-              <Text ml="-8px" fontSize="12px" textAlign="center" color="textSubtle" display="inline">
+              <Text fontSize="12px" textAlign="center" color="textSubtle" display="inline">
                 {t(
                   'All figures are estimates provided for your convenience only, and by no means represent guaranteed returns.',
                 )}
@@ -212,7 +226,7 @@ const ApyCalculatorModal: React.FC<ApyCalculatorModalProps> = ({
             </li>
             {performanceFee > 0 && (
               <li>
-                <Text mt="14px" ml="-8px" fontSize="12px" textAlign="center" color="textSubtle" display="inline">
+                <Text mt="14px" fontSize="12px" textAlign="center" color="textSubtle" display="inline">
                   {t('All estimated rates take into account this pool’s %fee%% performance fee', {
                     fee: performanceFee,
                   })}
