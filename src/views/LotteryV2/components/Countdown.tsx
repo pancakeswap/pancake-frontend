c828a52bc0d04458a75b81dfa13520787700491e
ChefHutch <chef.hutch.pcs@protonmail.com>
2021-06-28 12:18:10 +0100

    feat(lottery-v2): Use Lottery-specific timer rather than shared comp

--- src/views/LotteryV2/components/Countdown.tsx
+++ src/views/LotteryV2/components/Countdown.tsx
@@ -1,20 +1,13 @@
 import React from 'react'
-import styled from 'styled-components'
 import { Flex, Heading } from '@pancakeswap/uikit'
-import Timer from 'components/Timer'
 import getTimePeriods from 'utils/getTimePeriods'
+import Timer from './Timer'
 
 interface CountdownProps {
   secondsRemaining: number
   preCountdownText?: string
   postCountdownText?: string
 }
-
-const TimerTextComponent = styled(Heading)`
-  background: ${({ theme }) => theme.colors.gradients.gold};
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-`
 
 const Countdown: React.FC<CountdownProps> = ({ secondsRemaining, preCountdownText, postCountdownText }) => {
   const { days, hours, minutes } = getTimePeriods(secondsRemaining)
@@ -30,18 +23,8 @@ const Countdown: React.FC<CountdownProps> = ({ secondsRemaining, preCountdownTex
         minutes={minutes + 1} // We don't show seconds - so values from 0 - 59s should be shown as 1 min
         hours={hours}
         days={days}
-        HeadingTextComponent={({ children }) => (
-          <TimerTextComponent mb="-4px" scale="xl" mr="4px">
-            {children}
-          </TimerTextComponent>
-        )}
-        BodyTextComponent={({ children }) => <TimerTextComponent mr="8px">{children}</TimerTextComponent>}
       />
-      {postCountdownText && (
-        <Heading ml="4px" color="#ffff">
-          {postCountdownText}
-        </Heading>
-      )}
+      {postCountdownText && <Heading color="#ffff">{postCountdownText}</Heading>}
     </Flex>
   )
 }
---
+++ src/views/LotteryV2/components/Timer.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Flex, Heading } from '@pancakeswap/uikit'
+import { useTranslation } from 'contexts/Localization'
+
+export interface TimerProps {
+  minutes?: number
+  hours?: number
+  days?: number
+}
+
+const StyledTimerFlex = styled(Flex)<{ showTooltip?: boolean }>`
+  ${({ theme, showTooltip }) => (showTooltip ? ` border-bottom: 1px dashed ${theme.colors.textSubtle};` : ``)}
+  div:last-of-type {
+    margin-right: 0;
+  }
+`
+
+const StyledTimerText = styled(Heading)`
+  background: ${({ theme }) => theme.colors.gradients.gold};
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`
+
+const Wrapper: React.FC<TimerProps> = ({ minutes, hours, days }) => {
+  const { t } = useTranslation()
+
+  return (
+    <StyledTimerFlex alignItems="flex-end">
+      {Boolean(days) && (
+        <>
+          <StyledTimerText mb="-4px" scale="xl" mr="4px">
+            {days}
+          </StyledTimerText>
+          <StyledTimerText mr="12px">{t('d')}</StyledTimerText>
+        </>
+      )}
+      {Boolean(hours) && (
+        <>
+          <StyledTimerText mb="-4px" scale="xl" mr="4px">
+            {hours}
+          </StyledTimerText>
+          <StyledTimerText mr="12px">{t('h')}</StyledTimerText>
+        </>
+      )}
+      {Boolean(minutes) && (
+        <>
+          <StyledTimerText mb="-4px" scale="xl" mr="4px">
+            {minutes}
+          </StyledTimerText>
+          <StyledTimerText mr="12px">{t('m')}</StyledTimerText>
+        </>
+      )}
+    </StyledTimerFlex>
+  )
+}
+
+export default Wrapper
