b08107cad03a5ceab025e7ce258e56d264ef5fce
RabbitDoge <72658581+RabbitDoge@users.noreply.github.com>
2021-06-28 14:37:44 +0900

    chore: Clean public folder

--- src/views/Farms/Farms.tsx
+++ src/views/Farms/Farms.tsx
@@ -399,7 +399,7 @@ const Farms: React.FC = () => {
         </ControlContainer>
         {renderContent()}
         <div ref={loadMoreRef} />
-        <StyledImage src="/images/3dpan.png" alt="Pancake illustration" width={120} height={103} />
+        <StyledImage src="/images/decorations/3dpan.png" alt="Pancake illustration" width={120} height={103} />
       </Page>
     </>
   )
--- src/views/Pools/index.tsx
+++ src/views/Pools/index.tsx
@@ -283,7 +283,7 @@ const Pools: React.FC = () => {
         <Image
           mx="auto"
           mt="12px"
-          src="/images/3d-syrup-bunnies.png"
+          src="/images/decorations/3d-syrup-bunnies.png"
           alt="Pancake illustration"
           width={192}
           height={184.5}
