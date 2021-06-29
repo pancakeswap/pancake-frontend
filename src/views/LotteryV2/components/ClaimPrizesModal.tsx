128b90b7e2e4fd9b3fa4791abf7b17820907028c
ChefHutch <chef.hutch.pcs@protonmail.com>
2021-06-29 21:43:03 +0100

    feat(lottery-v2): Add confetti

--- src/views/LotteryV2/components/ClaimPrizesModal.tsx
+++ src/views/LotteryV2/components/ClaimPrizesModal.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
 import { Modal } from '@pancakeswap/uikit'
 import { useWeb3React } from '@web3-react/core'
 import { useTranslation } from 'contexts/Localization'
+import { delay } from 'lodash'
+import confetti from 'canvas-confetti'
 import useTheme from 'hooks/useTheme'
 import { LotteryTicketClaimData } from 'config/constants/types'
 import { useAppDispatch } from 'state'
@@ -17,6 +19,20 @@ const StyledModal = styled(Modal)`
   }
 `
 
+const showConfetti = () => {
+  confetti({
+    resize: true,
+    particleCount: 200,
+    startVelocity: 30,
+    gravity: 0.5,
+    spread: 350,
+    origin: {
+      x: 0.5,
+      y: 0.3,
+    },
+  })
+}
+
 interface ClaimPrizesModalModalProps {
   roundsToClaim: LotteryTicketClaimData[]
   onDismiss?: () => void
@@ -27,6 +43,10 @@ const ClaimPrizesModal: React.FC<ClaimPrizesModalModalProps> = ({ onDismiss, rou
   const { theme } = useTheme()
   const { account } = useWeb3React()
   const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    delay(showConfetti, 100)
+  }, [])
 
   return (
     <StyledModal
