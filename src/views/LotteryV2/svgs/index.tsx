52c158d587a419adb53753103ca60cca5afd265f
ChefHutch <chef.hutch.pcs@protonmail.com>
2021-06-30 18:48:00 +0100

    feat(lottery-v2): Data fetch and winning number comp for LotteryHistoryCardBody

--- src/views/LotteryV2/svgs/index.tsx
+++ src/views/LotteryV2/svgs/index.tsx
@@ -1,2 +1,3 @@
 export { default as TicketPurchaseCard } from './TicketPurchaseCard'
 export { default as WhiteBunny } from './WhiteBunny'
+export { PinkBall, LilacBall, TealBall, AquaBall, GreenBall, YellowBall } from './Balls'
