32df90b2ae22a444e39bfb2ccb078060d6130828
ChefHutch <chef.hutch.pcs@protonmail.com>
2021-06-30 08:10:02 +0100

    chore(ui-kit): Update version and fix name changes

--- src/views/Profile/components/Header.tsx
+++ src/views/Profile/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Flex, Heading, useModal, Won } from '@pancakeswap/uikit'
+import { Button, Flex, Heading, useModal, PresentWonIcon } from '@pancakeswap/uikit'
 import { useProfile } from 'state/hooks'
 import { useTranslation } from 'contexts/Localization'
 import ClaimNftAndCakeModal, { useCanClaim } from './ClaimGiftModal'
@@ -30,7 +30,7 @@ const ProfileHeader = () => {
           {hasProfile && <Button onClick={onEditProfileModal}>{t('Edit Profile')}</Button>}
         </div>
         {canClaim && (
-          <Button variant="tertiary" onClick={onPresentClaimGiftModal} startIcon={<Won />}>
+          <Button variant="tertiary" onClick={onPresentClaimGiftModal} startIcon={<PresentWonIcon />}>
             {t('You’ve got a gift to claim!')}
           </Button>
         )}
