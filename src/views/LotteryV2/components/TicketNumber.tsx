8d7acc7a67be1f6e319b03358d49481370c2429d
ChefHutch <chef.hutch.pcs@protonmail.com>
2021-07-02 13:23:48 +0100

    feat(lottery-v2): Winner sorting and styling for HistoricTicketsView tickets

--- src/views/LotteryV2/components/TicketNumber.tsx
+++ src/views/LotteryV2/components/TicketNumber.tsx
@@ -6,6 +6,7 @@ import styled from 'styled-components'
 import { parseRetreivedNumber } from '../helpers'
 
 const StyledNumberWrapper = styled(Flex)`
+  position: absolute;
   padding: 4px 16px;
   border: 1px solid ${({ theme }) => theme.colors.cardBorder};
   border-radius: ${({ theme }) => theme.radii.default};
@@ -13,20 +14,36 @@ const StyledNumberWrapper = styled(Flex)`
   justify-content: space-between;
 `
 
+const RewardHighlighter = styled.div<{ numberMatches: number }>`
+  position: absolute;
+  border: 2px red;
+`
+
 interface TicketNumberProps extends LotteryTicket {
   localId?: number
+  rewardBracket?: number
 }
 
-const TicketNumber: React.FC<TicketNumberProps> = ({ localId, id, number }) => {
+const TicketNumber: React.FC<TicketNumberProps> = ({ localId, id, number, rewardBracket }) => {
+  const { t } = useTranslation()
   const reversedNumber = parseRetreivedNumber(number)
   const numberAsArray = reversedNumber.split('')
+  const numberMatches = rewardBracket + 1
 
   return (
     <Flex flexDirection="column" mb="12px">
-      <Text fontSize="12px" color="textSubtle">
-        #{localId || id}
-      </Text>
+      <Flex justifyContent="space-between">
+        <Text fontSize="12px" color="textSubtle">
+          #{localId || id}
+        </Text>
+        {numberMatches && (
+          <Text fontSize="12px">
+            {t('Matched first')} {numberMatches}
+          </Text>
+        )}
+      </Flex>
       <StyledNumberWrapper>
+        {numberMatches && <RewardHighlighter numberMatches={numberMatches} />}
         {numberAsArray.map((digit) => (
           <Text fontSize="16px">{digit}</Text>
         ))}
--- src/views/LotteryV2/components/ViewTicketsModal/HistoricTicketsInner.tsx
+++ src/views/LotteryV2/components/ViewTicketsModal/HistoricTicketsInner.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Box, Text, Flex, Button, Skeleton } from '@pancakeswap/uikit'
+import { Box, Text, Flex, Button, Skeleton, Ticket, PresentWonIcon } from '@pancakeswap/uikit'
 import styled from 'styled-components'
 import { useWeb3React } from '@web3-react/core'
 import { LotteryTicket } from 'config/constants/types'
@@ -11,24 +11,59 @@ import { useTranslation } from 'contexts/Localization'
 import useTheme from 'hooks/useTheme'
 import WinningNumbers from '../WinningNumbers'
 import { processLotteryResponse } from '../../helpers'
+import TicketNumber from '../TicketNumber'
 
 const ScrollBox = styled(Box)`
-  /* max-height: 300px; */
+  max-height: 300px;
   overflow-y: scroll;
   margin-top: 24px;
 `
 
+const TicketSkeleton = () => {
+  return (
+    <>
+      <Skeleton width="32px" height="12px" mt="2px" mb="4px" />
+      <Skeleton width="100%" height="34px" mb="12px" />
+    </>
+  )
+}
+
 const HistoricTicketsInner: React.FC<{ roundId: string }> = ({ roundId }) => {
   const [lotteryInfo, setLotteryInfo] = useState<LotteryRound>(null)
   const [allUserTickets, setAllUserTickets] = useState<LotteryTicket[]>(null)
-  const [userWinningTickets, setUserWinningTickets] =
-    useState<{ allWinningTickets: LotteryTicket[]; ticketsWithUnclaimedRewards: LotteryTicket[] }>(null)
+  const [userWinningTickets, setUserWinningTickets] = useState<{
+    allWinningTickets: LotteryTicket[]
+    ticketsWithUnclaimedRewards: LotteryTicket[]
+    isFetched: boolean
+  }>({ allWinningTickets: null, ticketsWithUnclaimedRewards: null, isFetched: false })
   const { t } = useTranslation()
   const { theme } = useTheme()
   const { account } = useWeb3React()
   const userLotteryRoundData = useGetUserLotteryGraphRoundById(roundId)
 
   useEffect(() => {
+    const addWinningTicketInfoToAllTickets = (
+      _allTickets: LotteryTicket[],
+      _allWinningTickets: LotteryTicket[],
+    ): LotteryTicket[] => {
+      const allTicketsWithWinningTickets = _allTickets.map((ticket) => {
+        const winningTicketEquivalent = _allWinningTickets.find((winningTicket) => winningTicket.id === ticket.id)
+        if (winningTicketEquivalent) {
+          return winningTicketEquivalent
+        }
+        return ticket
+      })
+      return allTicketsWithWinningTickets
+    }
+
+    const sortTicketsByWinningBracket = (tickets) => {
+      return tickets.sort((ticketA, ticketB) => {
+        const rewardBracket1 = ticketA.rewardBracket === undefined ? 0 : ticketA.rewardBracket + 1
+        const rewardBracket2 = ticketB.rewardBracket === undefined ? 0 : ticketB.rewardBracket + 1
+        return rewardBracket2 - rewardBracket1
+      })
+    }
+
     const fetchData = async () => {
       const userTickets = await fetchTickets(account, roundId, userLotteryRoundData)
       const lotteryData = await fetchLottery(roundId)
@@ -38,13 +73,27 @@ const HistoricTicketsInner: React.FC<{ roundId: string }> = ({ roundId }) => {
         userTickets,
         finalNumber: processedLotteryData.finalNumber.toString(),
       })
+
       setUserWinningTickets({
+        isFetched: true,
         allWinningTickets: winningTickets?.allWinningTickets,
         ticketsWithUnclaimedRewards: winningTickets?.ticketsWithUnclaimedRewards,
       })
       setLotteryInfo(processedLotteryData)
-      setAllUserTickets(userTickets)
+
+      // If the user has some winning tickets - modify the userTickets response to include that data
+      if (winningTickets?.allWinningTickets) {
+        const allTicketsWithWinningTicketInfo = addWinningTicketInfoToAllTickets(
+          userTickets,
+          winningTickets.allWinningTickets,
+        )
+        const ticketsSortedByWinners = sortTicketsByWinningBracket(allTicketsWithWinningTicketInfo)
+        setAllUserTickets(ticketsSortedByWinners)
+      } else {
+        setAllUserTickets(userTickets)
+      }
     }
+
     fetchData()
   }, [roundId, account, userLotteryRoundData])
 
@@ -65,21 +114,50 @@ const HistoricTicketsInner: React.FC<{ roundId: string }> = ({ roundId }) => {
           {t('Your tickets')}
         </Text>
         <Flex mb="8px" justifyContent="space-between">
+          <Flex>
+            <Ticket width="24px" height="24px" mr="8px" />
+            <Text bold color="text">
+              {t('Total tickets')}:
+            </Text>
+          </Flex>
           <Text bold color="text">
-            {t('Total tickets')}
-          </Text>
-          <Text bold color="text">
-            {allUserTickets && allUserTickets.length}
-          </Text>
-        </Flex>
-        <Flex mb="8px" justifyContent="space-between">
-          <Text bold color="text">
-            {t('Winning tickets')}
-          </Text>
-          <Text bold color="text">
-            {userWinningTickets && userWinningTickets?.allWinningTickets?.length}
+            {allUserTickets ? allUserTickets.length : <Skeleton width="56px" height="24px" />}
           </Text>
         </Flex>
+        <Flex mb="24px" justifyContent="space-between">
+          <Flex>
+            <PresentWonIcon width="24px" height="24px" mr="8px" />
+            <Text bold color="text">
+              {t('Winning tickets')}:
+            </Text>
+          </Flex>
+          <Text bold color="text">
+            {userWinningTickets.isFetched ? (
+              userWinningTickets?.allWinningTickets?.length || '0'
+            ) : (
+              <Skeleton width="40px" height="24px" />
+            )}
+          </Text>
+        </Flex>
+        {allUserTickets ? (
+          allUserTickets.map((ticket) => {
+            return (
+              <TicketNumber
+                key={ticket.id}
+                id={ticket.id}
+                number={ticket.number}
+                rewardBracket={ticket.rewardBracket}
+              />
+            )
+          })
+        ) : (
+          <>
+            <TicketSkeleton />
+            <TicketSkeleton />
+            <TicketSkeleton />
+            <TicketSkeleton />
+          </>
+        )}
       </ScrollBox>
       <Flex borderTop={`1px solid ${theme.colors.cardBorder}`} alignItems="center" justifyContent="center">
         <Button mt="24px" width="100%">
