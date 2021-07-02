0e8dee28cfc8e626453cba54d07354f46aa80902
hachiojidev <hachiojidev@protonmail.com>
2021-07-02 10:48:24 +0900

    fix: Wallet Connect message signing

--- src/utils/web3React.ts
+++ src/utils/web3React.ts
@@ -46,5 +46,15 @@ export const signMessage = async (
     return signature
   }
 
-  return provider.getSigner().signMessage(message)
+  /**
+   * Wallet Connect does not sign the message correctly unless you use their method
+   * @see https://github.com/WalletConnect/walletconnect-monorepo/issues/462
+   */
+  if (provider.provider?.wc) {
+    const wcMessage = ethers.utils.hexlify(ethers.utils.toUtf8Bytes(message))
+    const signature = await provider.provider?.wc.signPersonalMessage([wcMessage, account.toLowerCase()])
+    return signature
+  }
+
+  return provider.getSigner(account.toLowerCase()).signMessage(message)
 }
