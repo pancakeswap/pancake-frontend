6651339992c500c22b6e60baaaef4458d91456af
RabbitDoge <72658581+RabbitDoge@users.noreply.github.com>
2021-07-05 11:22:50 +0900

    build: Update deps (#1633)

--- src/utils/addressHelpers.ts
+++ src/utils/addressHelpers.ts
@@ -1,4 +1,4 @@
-import { ChainId } from '@pancakeswap-libs/sdk'
+import { ChainId } from '@pancakeswap/sdk'
 import addresses from 'config/constants/contracts'
 import tokens from 'config/constants/tokens'
 import { Address } from 'config/constants/types'
