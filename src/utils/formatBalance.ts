feacc82274c5e0820f2ea8f9d2859fc39f6a9426
hachiojidev <hachiojidev@protonmail.com>
2021-07-05 09:31:53 +0900

    refactor: Format big number

--- src/utils/formatBalance.ts
+++ src/utils/formatBalance.ts
@@ -1,5 +1,5 @@
 import BigNumber from 'bignumber.js'
-import { ethers } from 'ethers'
+import { ethers, FixedNumber } from 'ethers'
 import { formatUnits } from 'ethers/lib/utils'
 import { BIG_TEN } from './bigNumber'
 
@@ -38,19 +38,6 @@ export const formatNumber = (number: number, minPrecision = 2, maxPrecision = 2)
  * Note: does NOT round
  */
 export const formatBigNumber = (number: ethers.BigNumber, displayDecimals = 18, decimals = 18) => {
-  const balance = formatUnits(number, decimals)
-  const [leftSide, rightSide] = balance.split('.')
-
-  let charCount = displayDecimals
-
-  if (leftSide.length > 0) {
-    charCount += leftSide.length
-  }
-
-  // Add 1 for the decimal point
-  if (rightSide.length > 0) {
-    charCount += 1
-  }
-
-  return balance.substr(0, charCount <= balance.length ? charCount : balance.length)
+  const remainder = number.mod(ethers.BigNumber.from(10).pow(decimals - displayDecimals))
+  return formatUnits(number.sub(remainder), decimals)
 }
