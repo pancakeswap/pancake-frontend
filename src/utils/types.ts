0322417aac3ce2e5e61d674656ed816f6f6bcf69
hachiojidev <hachiojidev@protonmail.com>
2021-07-05 16:43:22 +0900

    refactor: Separate method interface so it can be exported

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,19 +2,21 @@ import ethers, { Contract, ContractFunction } from 'ethers'
 
 export type MultiCallResponse<T> = T | null
 
+export interface PredictionsRounds {
+  epoch: ethers.BigNumber
+  startBlock: ethers.BigNumber
+  lockBlock: ethers.BigNumber
+  endBlock: ethers.BigNumber
+  lockPrice: ethers.BigNumber
+  closePrice: ethers.BigNumber
+  totalAmount: ethers.BigNumber
+  bullAmount: ethers.BigNumber
+  bearAmount: ethers.BigNumber
+  rewardBaseCalAmount: ethers.BigNumber
+  rewardAmount: ethers.BigNumber
+  oracleCalled: boolean
+}
+
 export interface PredictionsContract extends Contract {
-  rounds: ContractFunction<{
-    epoch: ethers.BigNumber
-    startBlock: ethers.BigNumber
-    lockBlock: ethers.BigNumber
-    endBlock: ethers.BigNumber
-    lockPrice: ethers.BigNumber
-    closePrice: ethers.BigNumber
-    totalAmount: ethers.BigNumber
-    bullAmount: ethers.BigNumber
-    bearAmount: ethers.BigNumber
-    rewardBaseCalAmount: ethers.BigNumber
-    rewardAmount: ethers.BigNumber
-    oracleCalled: boolean
-  }>
+  rounds: ContractFunction<PredictionsRounds>
 }
