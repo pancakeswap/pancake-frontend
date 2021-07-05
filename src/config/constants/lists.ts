48054d6e6a660bc8bc2444f08d20e265852260ad
RabbitDoge <72658581+RabbitDoge@users.noreply.github.com>
2021-07-05 15:56:02 +0900

    chore: Migrate config files (#1642)

---
+++ src/config/constants/lists.ts
@@ -0,0 +1,14 @@
+const PANCAKE_EXTENDED = 'https://tokens.pancakeswap.finance/pancakeswap-extended.json'
+const PANCAKE_TOP100 = 'https://tokens.pancakeswap.finance/pancakeswap-top-100.json'
+
+export const UNSUPPORTED_LIST_URLS: string[] = []
+
+// lower index == higher priority for token import
+export const DEFAULT_LIST_OF_LISTS: string[] = [
+  PANCAKE_TOP100,
+  PANCAKE_EXTENDED,
+  ...UNSUPPORTED_LIST_URLS, // need to load unsupported tokens as well
+]
+
+// default lists to be 'active' aka searched across
+export const DEFAULT_ACTIVE_LIST_URLS: string[] = []
---
+++ src/config/constants/multicall/index.ts
@@ -0,0 +1,9 @@
+import { ChainId } from '@pancakeswap/sdk'
+import MULTICALL_ABI from './abi.json'
+
+const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
+  [ChainId.MAINNET]: '0x1Ee38d535d541c55C9dae27B12edf090C608E6Fb',
+  [ChainId.TESTNET]: '0x301907b5835a2d723Fe3e9E8C5Bc5375d5c1236A',
+}
+
+export { MULTICALL_ABI, MULTICALL_NETWORKS }
