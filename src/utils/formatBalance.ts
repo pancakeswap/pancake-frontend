f069ecfe1338c1830720f0b1df3016eb16219901
Chef Hachioji <72719739+hachiojidev@users.noreply.github.com>
2021-07-06 17:55:30 +0900

    feat: Helper function to format display of ethers' formatUnits (#1634)
    * feat: Helper function to format display of ethers' formatUnits
    
    * refactor: Format big number
    
    * feat: Add helper to format ethers BigNumbers with toFixed
    
    * feat: Helper for fixed numbers
    
    * chore: Remove unused variable

--- src/utils/formatBalance.ts
+++ src/utils/formatBalance.ts
@@ -1,4 +1,6 @@
 import BigNumber from 'bignumber.js'
+import { ethers } from 'ethers'
+import { formatUnits } from 'ethers/lib/utils'
 import { BIG_TEN } from './bigNumber'
 
 /**
@@ -19,8 +21,8 @@ export const getBalanceNumber = (balance: BigNumber, decimals = 18) => {
   return getBalanceAmount(balance, decimals).toNumber()
 }
 
-export const getFullDisplayBalance = (balance: BigNumber, decimals = 18, decimalsToAppear?: number) => {
-  return getBalanceAmount(balance, decimals).toFixed(decimalsToAppear)
+export const getFullDisplayBalance = (balance: BigNumber, decimals = 18, displayDecimals?: number) => {
+  return getBalanceAmount(balance, decimals).toFixed(displayDecimals)
 }
 
 export const formatNumber = (number: number, minPrecision = 2, maxPrecision = 2) => {
@@ -29,4 +31,29 @@ export const formatNumber = (number: number, minPrecision = 2, maxPrecision = 2)
     maximumFractionDigits: maxPrecision,
   }
   return number.toLocaleString(undefined, options)
+}
+
+/**
+ * Method to format the display of wei given an ethers.BigNumber object
+ * Note: does NOT round
+ */
+export const formatBigNumber = (number: ethers.BigNumber, displayDecimals = 18, decimals = 18) => {
+  const remainder = number.mod(ethers.BigNumber.from(10).pow(decimals - displayDecimals))
+  return formatUnits(number.sub(remainder), decimals)
+}
+
+/**
+ * Method to format the display of wei given an ethers.BigNumber object with toFixed
+ * Note: rounds
+ */
+export const formatBigNumberToFixed = (number: ethers.BigNumber, displayDecimals = 18, decimals = 18) => {
+  const formattedString = formatUnits(number, decimals)
+  return (+formattedString).toFixed(displayDecimals)
+}
+
+/**
+ * Formats a FixedNumber like BigNumber
+ */
+export const formatFixedNumber = (number: ethers.FixedNumber, displayDecimals = 18, decimals = 18) => {
+  return formatBigNumber(ethers.BigNumber.from(number), displayDecimals, decimals)
 }
