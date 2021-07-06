27fd7584d7f5638aceb6e939067d885e977ec8ed
memoyil <2213635+memoyil@users.noreply.github.com>
2021-07-06 11:15:03 +0200

    feat: (win) Add prizes info to win card

--- src/views/Home/components/WinCard.tsx
+++ src/views/Home/components/WinCard.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
-import { Heading, Card, CardBody, Flex, ArrowForwardIcon } from '@pancakeswap/uikit'
+import { Heading, Card, CardBody, Flex, ArrowForwardIcon, Skeleton } from '@pancakeswap/uikit'
 import { NavLink } from 'react-router-dom'
 import { useTranslation } from 'contexts/Localization'
+import { useGetCurrentLotteryId, useLottery, usePriceCakeBusd } from 'state/hooks'
+import { getBalanceNumber } from 'utils/formatBalance'
+import Balance from 'components/Balance'
+import useRefresh from 'hooks/useRefresh'
+import useIntersectionObserver from 'hooks/useIntersectionObserver'
+import { useAppDispatch } from 'state'
+import { fetchCurrentLottery, fetchCurrentLotteryId } from 'state/lottery'
 
 const StyledFarmStakingCard = styled(Card)`
   margin-left: auto;
@@ -19,22 +26,61 @@ const StyledFarmStakingCard = styled(Card)`
     opacity: 0.65;
   }
 `
-const CardMidContent = styled(Heading).attrs({ scale: 'xl' })`
-  line-height: 44px;
-`
+
 const WinCard = () => {
   const { t } = useTranslation()
+  const { observerRef, isIntersecting } = useIntersectionObserver()
+  const [loadData, setLoadData] = useState(false)
+  const { slowRefresh } = useRefresh()
+  const dispatch = useAppDispatch()
+  const currentLotteryId = useGetCurrentLotteryId()
+
+  const {
+    currentRound: { amountCollectedInCake },
+  } = useLottery()
+  const cakePriceBusd = usePriceCakeBusd()
+  const prizeInBusd = amountCollectedInCake.times(cakePriceBusd)
+  const prizeTotal = getBalanceNumber(prizeInBusd)
+
+  useEffect(() => {
+    if (isIntersecting) {
+      setLoadData(true)
+    }
+  }, [isIntersecting])
+
+  useEffect(() => {
+    // get current lottery ID, max tickets and historical lottery subgraph data
+    if (loadData) {
+      dispatch(fetchCurrentLotteryId())
+    }
+  }, [dispatch, loadData])
+
+  useEffect(() => {
+    // get public data for current lottery
+    if (currentLotteryId) {
+      dispatch(fetchCurrentLottery({ currentLotteryId }))
+    }
+  }, [dispatch, currentLotteryId, slowRefresh])
 
   return (
     <StyledFarmStakingCard>
       <NavLink exact activeClassName="active" to="/lottery" id="lottery-pot-cta">
         <CardBody>
-          <Heading color="contrast" scale="lg">
+          <Heading color="contrast" scale="lg" mb="2px">
             {t('Lottery')} V2
           </Heading>
-          <CardMidContent color="#7645d9">{t('Coming Soon')}</CardMidContent>
+          {prizeInBusd.isNaN() ? (
+            <>
+              <Skeleton height={60} width={190} />
+              <div ref={observerRef} />
+            </>
+          ) : (
+            <Balance fontSize="40px" color="#7645d9" bold prefix="$" decimals={0} value={prizeTotal} />
+          )}
           <Flex justifyContent="space-between">
-            <Heading color="contrast" scale="lg" />
+            <Heading color="contrast" scale="lg">
+              {t('in prizes!')}
+            </Heading>
             <ArrowForwardIcon mt={30} color="primary" />
           </Flex>
         </CardBody>
