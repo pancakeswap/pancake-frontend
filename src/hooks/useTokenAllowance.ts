63a50888e02f41f7ceac31c007e89d93329e4c4c
Chef Cheems <chefcheems@protonmail.com>
2021-07-06 16:06:25 +0300

    chore: Merge useContractExchange with useContract

--- src/hooks/useTokenAllowance.ts
+++ src/hooks/useTokenAllowance.ts
@@ -1,7 +1,7 @@
 import { Token, TokenAmount } from '@pancakeswap/sdk'
 import { useMemo } from 'react'
 
-import { useTokenContract } from './useContractExchange'
+import { useTokenContract } from './useContract'
 import { useSingleCallResult } from '../state/multicall/hooks'
 
 function useTokenAllowance(token?: Token, owner?: string, spender?: string): TokenAmount | undefined {
