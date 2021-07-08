4485264a19af34456215a752351739ab16564eff
Chef Chungus <71833681+Chef-Chungus@users.noreply.github.com>
2021-07-08 16:58:25 +0900

    chore: Update commenting

--- src/__tests__/config/tokens.test.ts
+++ src/__tests__/config/tokens.test.ts
@@ -6,6 +6,7 @@ import { Token } from 'config/constants/types'
 import multicall from 'utils/multicall'
 
 // remove BNB because it's not a Bep20 token
+// remove ONE because there are two tokens with the symbol ONE (Harmony ONE and BigONE)
 const tokensToTest = omitBy(
   tokens,
   (token) => token.symbol.toLowerCase() === 'bnb' || token.symbol.toLowerCase() === 'one',
