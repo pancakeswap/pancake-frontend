1f13a84f8f2993d179933c065b97a949e4b58d94
hachiojidev <hachiojidev@protonmail.com>
2021-07-08 16:21:57 +0900

    refactor: Sort proposals by newest

--- src/state/voting/helpers.ts
+++ src/state/voting/helpers.ts
@@ -8,7 +8,13 @@ export const getProposals = async (first = 5, skip = 0, state = ProposalState.AC
     SNAPSHOT_API,
     gql`
       query getProposals($first: Int!, $skip: Int!, $state: String!) {
-        proposals(first: $first, skip: $skip, where: { space_in: "cake.eth", state: $state }) {
+        proposals(
+          first: $first
+          skip: $skip
+          orderBy: "end"
+          orderDirection: desc
+          where: { space_in: "cake.eth", state: $state }
+        ) {
           id
           title
           body
