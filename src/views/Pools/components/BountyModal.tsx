d4a3671c695d5adce7e7b45e8bf41bdfa6bb79b9
RabbitDoge <72658581+RabbitDoge@users.noreply.github.com>
2021-07-11 11:20:13 +0900

    chore: Increase gas limit for auto cake pool (#1696)

--- src/views/Pools/components/BountyModal.tsx
+++ src/views/Pools/components/BountyModal.tsx
@@ -57,7 +57,7 @@ const BountyModal: React.FC<BountyModalProps> = ({ onDismiss, TooltipComponent }
   const handleConfirmClick = async () => {
     setPendingTx(true)
     try {
-      const tx = await cakeVaultContract.harvest({ gasLimit: 210000 })
+      const tx = await cakeVaultContract.harvest({ gasLimit: 300000 })
       const receipt = await tx.wait()
       if (receipt.status) {
         toastSuccess(t('Bounty collected!'), t('CAKE bounty has been sent to your wallet.'))
