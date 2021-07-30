b42bcfb6dffb7eb529f87c6c3ed0270460d22810
Amr Hamdy <amrhamdy0027@gmail.com>
2021-07-30 17:56:05 +0200

    cleaned the old pool page

--- src/state/actions.ts
+++ src/state/actions.ts
@@ -1,15 +1,4 @@
 export { fetchFarmsPublicDataAsync, fetchFarmUserDataAsync } from './farms'
-export {
-  fetchPoolsPublicDataAsync,
-  fetchPoolsUserDataAsync,
-  fetchCakeVaultPublicData,
-  fetchCakeVaultUserData,
-  fetchCakeVaultFees,
-  updateUserAllowance,
-  updateUserBalance,
-  updateUserPendingReward,
-  updateUserStakedBalance,
-} from './pools'
 export { profileFetchStart, profileFetchSucceeded, profileFetchFailed } from './profile'
 export { fetchStart, teamFetchSucceeded, fetchFailed, teamsFetchSucceeded } from './teams'
 export { setBlock } from './block'
