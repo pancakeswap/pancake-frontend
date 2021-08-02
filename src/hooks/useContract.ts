5c6efa0363bc6b48e993f935a383e01d74f29f22
Chef Cheems <chefcheems@protonmail.com>
2021-08-02 07:42:10 +0300

    chore: Add comment about useFarmAuctionContract

--- src/hooks/useContract.ts
+++ src/hooks/useContract.ts
@@ -157,6 +157,17 @@ export const useSpecialBunnyPredictionContract = () => {
 
 export const useFarmAuctionContract = () => {
   const { account, library } = useActiveWeb3React()
+  // This hook is slightly different from others
+  // Calls were failing if unconnected user goes to farm auction page
+  // Using library instead of library.getSigner() fixes the problem for unconnected users
+  // However, this fix is not ideal, it currently has following behavior:
+  // - If you visit Farm Auction page coming from some other page there are no errors in console (unconnceted or connected)
+  // - If you go directly to Farm Auction page
+  //   - as unconnected user you don't see any console errors
+  //   - as connected user you see `unknown account #0 (operation="getAddress", code=UNSUPPORTED_OPERATION, ...` errors
+  //     the functionality of the page is not affected, data is loading fine and you can interact with the contract
+  //
+  // Similar behavior was also noticed on Trading Competition page.
   return useMemo(() => getFarmAuctionContract(account ? library.getSigner() : library), [library, account])
 }
 
