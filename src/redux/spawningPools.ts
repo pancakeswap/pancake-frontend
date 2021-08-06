354d295bfd785b65f85d74e81c1acc0db28c52c0
Nnamdi Arimah <nnamdi.arimah@shopify.com>
2021-08-05 22:56:47 -0400

    feat: Started spawningpools

--- src/redux/spawningPools.ts
+++ src/redux/spawningPools.ts
@@ -65,6 +65,66 @@ const spawningPools: SpawningPool[] = [
       nftRevivalTime: 0,
     },
   },
+  {
+    id: 1,
+    name: 'Euler Tools Legendary',
+    subtitle: 'Leonhard',
+    path: 'images/rugZombie/Leonhard.gif',
+    type: 'image',
+    address: {
+      56: '0x83818859688eF175F6AEAFb80Be881Db24A4E50a',
+      97: '0x09804035E6D09fe1d4992F64fE9F69A183572DD3',
+    },
+    endBlock: 12209400,
+    project: {
+      name: 'Gorilla-Fi',
+      description: 'Gorilla-Fi is a comprehensive De-Fi earnings ecosystem that allows anyone with a smartphone to earn passive income.',
+      additionalDetails: [
+        {
+          name: 'Project website',
+          url: 'https://www.gorillafi.com/'
+        },
+        {
+          name: 'Podcast with project founder',
+          url: 'https://www.youtube.com/watch?v=xdwiHSCPSNw',
+        },
+        {
+          name: 'Telegram',
+          url: 'https://t.me/GorillaFi'
+        },
+        {
+          name: 'Medium post',
+          url: 'https://rugzombie.medium.com/first-spawn-gorilla-fi-g-fi-f16a234047f7'
+        }
+      ],
+
+    },
+    withdrawalCooldown: '3 days',
+    nftRevivalTime: '45 days',
+    rewardToken: tokens.gfi,
+    rewardTokenId: 'gorilla-fi',
+    artist: artists.deadtunnelrat,
+    stakingToken: '',
+    pcsVersion: 'v1',
+    liquidityDetails: '',
+    isNew: false,
+    userInfo: {
+      paidUnlockFee: false,
+      tokenWithdrawalDate: 0,
+      nftRevivalDate: 0,
+      amount: BIG_ZERO,
+      pendingReward: BIG_ZERO,
+      zombieAllowance: BIG_ZERO,
+    },
+    poolInfo: {
+      unlockFee: BIG_ZERO,
+      rewardPerBlock: BIG_ZERO,
+      minimumStake: BIG_ZERO,
+      totalZombieStaked: BIG_ZERO,
+      withdrawCooldown: 0,
+      nftRevivalTime: 0,
+    },
+  },
 ]
 
 export default spawningPools
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -17,6 +17,7 @@ import Title from './components/Title'
 import GraveyardCard from './components/GraveyardCard'
 import EnterGravesCard from './components/EnterGravesCard'
 import useEagerConnect from '../../hooks/useEagerConnect'
+import web3 from '../../utils/web3'
 
 const Hero = styled.div`
   align-items: center;
@@ -85,6 +86,20 @@ const Home: React.FC = () => {
     fetch.initialData(account)
   }, [account])
 
+  // const allocPoints = web3.eth.abi.encodeParameter('uint256', '500')
+  // const lpToken = web3.eth.abi.encodeParameter('address', '0x0664C463205356246edF8f1770EA9bC14A648d62').replace('0x','')
+  // const minimumStakingTime = web3.eth.abi.encodeParameter('uint256', '259200').replace('0x','')
+  // const ruggedToken = web3.eth.abi.encodeParameter('address', '0xFbD88E293e2c4263Be3A3dEeEd1939250E114985').replace('0x','')
+  // // const nft = web3.eth.abi.encodeParameter('address', '0xFca723bd09664076c7011BA43ebee05BC38B8E7e').replace('0x','')
+  // const minimumStake = web3.eth.abi.encodeParameter('uint256', '10000000000000000000').replace('0x','')
+  // const unlockFee = web3.eth.abi.encodeParameter('uint256', '5000000000000000000').replace('0x','')
+  // const nftRevivalTime = web3.eth.abi.encodeParameter('uint256', '2592000').replace('0x','')
+  // const withUpdate = web3.eth.abi.encodeParameter('bool', 'true').replace('0x','')
+  // // console.log(`${allocPoints}${lpToken}${minimumStakingTime}${ruggedToken}${nft}${minimumStake}${unlockFee}${nftRevivalTime}${withUpdate}`)
+  // const pid = web3.eth.abi.encodeParameter('uint256', '0')
+  // const nft = web3.eth.abi.encodeParameter('address', '0x6209E17d98ba2089571476940751802AAc4249e8').replace('0x','')
+  //
+  // console.log(`${pid}${nft}`)
   return (
     <>
     <NFTBanner/>
