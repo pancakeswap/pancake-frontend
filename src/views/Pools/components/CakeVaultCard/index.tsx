d30d70455badcc5c786db631a12bbcc76a7c6258
Chef Cheems <chefcheems@protonmail.com>
2021-08-08 19:21:34 +0300

    fix: Pool cards

--- src/views/Pools/components/CakeVaultCard/index.tsx
+++ src/views/Pools/components/CakeVaultCard/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Box, CardBody, Flex, Text, useMatchBreakpoints } from '@pancakeswap/uikit'
+import { Box, CardBody, Flex, Text } from '@pancakeswap/uikit'
 import { useTranslation } from 'contexts/Localization'
 import { useWeb3React } from '@web3-react/core'
 import ConnectWalletButton from 'components/ConnectWalletButton'
@@ -8,7 +8,7 @@ import tokens from 'config/constants/tokens'
 import { useCakeVault } from 'state/pools/hooks'
 import { Pool } from 'state/types'
 import AprRow from '../PoolCard/AprRow'
-import { StyledCard, StyledCardInner } from '../PoolCard/StyledCard'
+import { StyledCard } from '../PoolCard/StyledCard'
 import CardFooter from '../PoolCard/CardFooter'
 import StyledCardHeader from '../PoolCard/StyledCardHeader'
 import VaultCardActions from './VaultCardActions'
@@ -26,7 +26,6 @@ interface CakeVaultProps {
 
 const CakeVaultCard: React.FC<CakeVaultProps> = ({ pool, showStakedOnly }) => {
   const { t } = useTranslation()
-  const { isXl } = useMatchBreakpoints()
   const { account } = useWeb3React()
   const {
     userData: { userShares, isLoading: isVaultUserDataLoading },
@@ -42,37 +41,35 @@ const CakeVaultCard: React.FC<CakeVaultProps> = ({ pool, showStakedOnly }) => {
   }
 
   return (
-    <StyledCard isPromoted={{ isDesktop: isXl }}>
-      <StyledCardInner>
-        <StyledCardHeader
-          isStaking={accountHasSharesStaked}
-          isAutoVault
-          earningToken={tokens.cake}
-          stakingToken={tokens.cake}
-        />
-        <StyledCardBody isLoading={isLoading}>
-          <AprRow pool={pool} performanceFee={performanceFeeAsDecimal} />
-          <Box mt="24px">
-            <RecentCakeProfitRow />
-          </Box>
-          <Box mt="8px">
-            <UnstakingFeeCountdownRow />
-          </Box>
-          <Flex mt="32px" flexDirection="column">
-            {account ? (
-              <VaultCardActions pool={pool} accountHasSharesStaked={accountHasSharesStaked} isLoading={isLoading} />
-            ) : (
-              <>
-                <Text mb="10px" textTransform="uppercase" fontSize="12px" color="textSubtle" bold>
-                  {t('Start earning')}
-                </Text>
-                <ConnectWalletButton />
-              </>
-            )}
-          </Flex>
-        </StyledCardBody>
-        <CardFooter pool={pool} account={account} />
-      </StyledCardInner>
+    <StyledCard isActive>
+      <StyledCardHeader
+        isStaking={accountHasSharesStaked}
+        isAutoVault
+        earningToken={tokens.cake}
+        stakingToken={tokens.cake}
+      />
+      <StyledCardBody isLoading={isLoading}>
+        <AprRow pool={pool} performanceFee={performanceFeeAsDecimal} />
+        <Box mt="24px">
+          <RecentCakeProfitRow />
+        </Box>
+        <Box mt="8px">
+          <UnstakingFeeCountdownRow />
+        </Box>
+        <Flex mt="32px" flexDirection="column">
+          {account ? (
+            <VaultCardActions pool={pool} accountHasSharesStaked={accountHasSharesStaked} isLoading={isLoading} />
+          ) : (
+            <>
+              <Text mb="10px" textTransform="uppercase" fontSize="12px" color="textSubtle" bold>
+                {t('Start earning')}
+              </Text>
+              <ConnectWalletButton />
+            </>
+          )}
+        </Flex>
+      </StyledCardBody>
+      <CardFooter pool={pool} account={account} />
     </StyledCard>
   )
 }
--- src/views/Pools/components/PoolCard/StyledCard.tsx
+++ src/views/Pools/components/PoolCard/StyledCard.tsx
@@ -1,23 +1,7 @@
-import styled, { css, keyframes } from 'styled-components'
-import { Card, Box } from '@pancakeswap/uikit'
+import styled from 'styled-components'
+import { Card } from '@pancakeswap/uikit'
 
-const PromotedGradient = keyframes`
-  0% {
-    background-position: 50% 0%;
-  }
-  50% {
-    background-position: 50% 100%;
-  }
-  100% {
-    background-position: 50% 0%;
-  }
-`
-
-interface PromotedStyleCardProps {
-  isDesktop: boolean
-}
-
-export const StyledCard = styled(Card)<{ isPromoted?: PromotedStyleCardProps; isFinished?: boolean }>`
+export const StyledCard = styled(Card)<{ isFinished?: boolean }>`
   max-width: 352px;
   margin: 0 8px 24px;
   display: flex;
@@ -25,32 +9,10 @@ export const StyledCard = styled(Card)<{ isPromoted?: PromotedStyleCardProps; is
   align-self: baseline;
   position: relative;
   color: ${({ isFinished, theme }) => theme.colors[isFinished ? 'textDisabled' : 'secondary']};
-  box-shadow: 0px 1px 4px rgba(25, 19, 38, 0.15);
-
-  ${({ isPromoted, theme }) =>
-    isPromoted
-      ? css`
-          background: linear-gradient(180deg, ${theme.colors.primaryBright}, ${theme.colors.secondary});
-          padding: 1px 1px 3px 1px;
-          background-size: 400% 400%;
-        `
-      : `background: ${(props) => props.theme.card.background};`}
-
-  ${({ isPromoted }) =>
-    isPromoted &&
-    isPromoted.isDesktop &&
-    css`
-      animation: ${PromotedGradient} 3s ease infinite;
-    `}
 
   ${({ theme }) => theme.mediaQueries.sm} {
     margin: 0 12px 46px;
   }
-`
-
-export const StyledCardInner = styled(Box)`
-  background: ${({ theme }) => theme.card.background};
-  border-radius: ${({ theme }) => theme.radii.card};
 `
 
 export default StyledCard
