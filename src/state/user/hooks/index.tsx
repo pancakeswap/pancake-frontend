59326d1013470d605afaaed95e17028e71f1abef
ChefHutch <chef.hutch.pcs@protonmail.com>
2021-08-10 13:40:49 +0100

    chore(gas-price): Unset testnet value for testing

--- src/state/user/hooks/index.tsx
+++ src/state/user/hooks/index.tsx
@@ -155,9 +155,10 @@ export function useRemoveUserAddedToken(): (chainId: number, address: string) =>
 }
 
 export function useGasPrice(): string {
-  const chainId = process.env.REACT_APP_CHAIN_ID
+  // const chainId = process.env.REACT_APP_CHAIN_ID
   const userGas = useSelector<AppState, AppState['user']['gasPrice']>((state) => state.user.gasPrice)
-  return chainId === ChainId.MAINNET.toString() ? userGas : GAS_PRICE_GWEI.testnet
+  return userGas
+  // return chainId === ChainId.MAINNET.toString() ? userGas : GAS_PRICE_GWEI.testnet
 }
 
 export function useGasPriceManager(): [string, (userGasPrice: string) => void] {
