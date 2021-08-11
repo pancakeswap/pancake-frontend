54d6e9fae639518e40cf7e0c8d31313dbb3a05f2
Chef Cheems <chefcheems@protonmail.com>
2021-08-11 10:06:34 +0300

    fix: Margins for tooltip text paragraphs

--- src/components/RoiCalculatorModal/RoiCalculatorFooter.tsx
+++ src/components/RoiCalculatorModal/RoiCalculatorFooter.tsx
@@ -61,7 +61,7 @@ const RoiCalculatorFooter: React.FC<RoiCalculatorFooterProps> = ({
           'The Multiplier represents the proportion of CAKE rewards each farm receives, as a proportion of the CAKE produced each block.',
         )}
       </Text>
-      <Text>
+      <Text my="24px">
         {t('For example, if a 1x farm received 1 CAKE per block, a 40x farm would receive 40 CAKE per block.')}
       </Text>
       <Text>{t('This amount is already included in all APR calculations for the farm.')}</Text>
