95d787bfe3187b84ea710f40e66b16354282e3d5
memoyil <2213635+memoyil@users.noreply.github.com>
2021-08-15 21:40:41 +0200

    fix: Global settings tooltips not visible correctly in mobile

--- src/components/QuestionHelper/index.tsx
+++ src/components/QuestionHelper/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { HelpIcon, useTooltip, Box, BoxProps } from '@pancakeswap/uikit'
+import { HelpIcon, useTooltip, Box, BoxProps, Placement } from '@pancakeswap/uikit'
 import styled from 'styled-components'
 
 interface Props extends BoxProps {
   text: string | React.ReactNode
+  placement?: Placement
 }
 
 const QuestionWrapper = styled.div`
@@ -13,8 +14,8 @@ const QuestionWrapper = styled.div`
   }
 `
 
-const QuestionHelper: React.FC<Props> = ({ text, ...props }) => {
-  const { targetRef, tooltip, tooltipVisible } = useTooltip(text, { placement: 'right-end', trigger: 'hover' })
+const QuestionHelper: React.FC<Props> = ({ text, placement = 'right-end', ...props }) => {
+  const { targetRef, tooltip, tooltipVisible } = useTooltip(text, { placement, trigger: 'hover' })
 
   return (
     <Box {...props}>
