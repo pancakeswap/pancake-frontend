e31fdc653c1251a617254a66194e6d5d2b8b05e6
hachiojidev <hachiojidev@protonmail.com>
2021-08-19 14:48:30 +0900

    style(predictions): Format BNB position

--- src/views/Predictions/components/CollectRoundWinningsModal.tsx
+++ src/views/Predictions/components/CollectRoundWinningsModal.tsx
@@ -67,7 +67,7 @@ const CollectRoundWinningsModal: React.FC<CollectRoundWinningsModalProps> = ({
 
   const handleClick = async () => {
     try {
-      const tx = await callWithGasPrice(predictionsContract, 'claim', [epoch])
+      const tx = await callWithGasPrice(predictionsContract, 'claim', [[epoch]])
       setIsPendingTx(true)
       const receipt = await tx.wait()
 
@@ -92,6 +92,7 @@ const CollectRoundWinningsModal: React.FC<CollectRoundWinningsModalProps> = ({
         </Box>,
       )
     } catch (error) {
+      console.error('Unable to claim winnings', error)
       toastError(
         t('Error'),
         error?.data?.message || t('Please try again. Confirm the transaction and make sure you are paying enough gas!'),
@@ -117,7 +118,7 @@ const CollectRoundWinningsModal: React.FC<CollectRoundWinningsModalProps> = ({
         <Flex alignItems="start" justifyContent="space-between" mb="8px">
           <Text>{t('Your position')}</Text>
           <Box style={{ textAlign: 'right' }}>
-            <Text>{`${betAmount} BNB`}</Text>
+            <Text>{`${betAmountAsFloat.toFixed(4)} BNB`}</Text>
             <Text fontSize="12px" color="textSubtle">
               {`~$${bnbBusdPrice.times(betAmountAsFloat).toFormat(2)}`}
             </Text>
