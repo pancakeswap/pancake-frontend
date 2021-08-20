71a8767d3c2fc1beed5f43e7f198717bfce53658
Chef Cheems <chefcheems@protonmail.com>
2021-08-20 10:19:42 +0300

    chore: Update action names

--- src/state/info/actions.ts
+++ src/state/info/actions.ts
@@ -2,31 +2,37 @@ import { createAction } from '@reduxjs/toolkit'
 import { ChartDayData, Transaction, PoolData, PoolChartEntry, PriceChartEntry } from 'types'
 import { ProtocolData, TokenData, TokenChartEntry } from './types'
 
-export const updateProtocolData = createAction<{ protocolData: ProtocolData }>('protocol/updateProtocolData')
-export const updateProtocolChartData = createAction<{ chartData: ChartDayData[] }>('protocol/updateProtocolChartData')
+export const updateProtocolData = createAction<{ protocolData: ProtocolData }>('info/protocol/updateProtocolData')
+export const updateProtocolChartData = createAction<{ chartData: ChartDayData[] }>(
+  'info/protocol/updateProtocolChartData',
+)
 export const updateProtocolTransactions = createAction<{ transactions: Transaction[] }>(
-  'protocol/updateProtocolTransactions',
+  'info/protocol/updateProtocolTransactions',
 )
 
-export const updatePoolData = createAction<{ pools: PoolData[] }>('pools/updatePoolData')
-export const addPoolKeys = createAction<{ poolAddresses: string[] }>('pool/addPoolKeys')
-export const updatePoolChartData =
-  createAction<{ poolAddress: string; chartData: PoolChartEntry[] }>('pool/updatePoolChartData')
-export const updatePoolTransactions =
-  createAction<{ poolAddress: string; transactions: Transaction[] }>('pool/updatePoolTransactions')
+export const updatePoolData = createAction<{ pools: PoolData[] }>('info/pools/updatePoolData')
+export const addPoolKeys = createAction<{ poolAddresses: string[] }>('info/pools/addPoolKeys')
+export const updatePoolChartData = createAction<{ poolAddress: string; chartData: PoolChartEntry[] }>(
+  'info/pools/updatePoolChartData',
+)
+export const updatePoolTransactions = createAction<{ poolAddress: string; transactions: Transaction[] }>(
+  'info/pools/updatePoolTransactions',
+)
 
-export const updateTokenData = createAction<{ tokens: TokenData[] }>('tokens/updateTokenData')
-export const addTokenKeys = createAction<{ tokenAddresses: string[] }>('tokens/addTokenKeys')
-export const addTokenPoolAddresses =
-  createAction<{ tokenAddress: string; poolAddresses: string[] }>('tokens/addTokenPoolAddresses')
-export const updateTokenChartData =
-  createAction<{ tokenAddress: string; chartData: TokenChartEntry[] }>('tokens/updateTokenChartData')
+export const updateTokenData = createAction<{ tokens: TokenData[] }>('info/tokens/updateTokenData')
+export const addTokenKeys = createAction<{ tokenAddresses: string[] }>('info/tokens/addTokenKeys')
+export const addTokenPoolAddresses = createAction<{ tokenAddress: string; poolAddresses: string[] }>(
+  'info/tokens/addTokenPoolAddresses',
+)
+export const updateTokenChartData = createAction<{ tokenAddress: string; chartData: TokenChartEntry[] }>(
+  'info/tokens/updateTokenChartData',
+)
 export const updateTokenTransactions = createAction<{ tokenAddress: string; transactions: Transaction[] }>(
-  'tokens/updateTokenTransactions',
+  'info/tokens/updateTokenTransactions',
 )
 export const updateTokenPriceData = createAction<{
   tokenAddress: string
   secondsInterval: number
   priceData: PriceChartEntry[] | undefined
   oldestFetchedTimestamp: number
-}>('tokens/updateTokenPriceData')
+}>('info/tokens/updateTokenPriceData')
