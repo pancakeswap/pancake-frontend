d52a8db9cb0fccc66b40e545a01bb2c23f075c11
hruday11 <hrudaykumar11@gmail.com>
2021-08-23 01:23:00 +0530

    fix: Filter for nfts

--- src/views/Collectibles/components/Collectibles/Collectibles.tsx
+++ src/views/Collectibles/components/Collectibles/Collectibles.tsx
@@ -1,19 +1,41 @@
-import React from 'react';
-import styled from 'styled-components'
-import { CardsLayout } from '@rug-zombie-libs/uikit'
+import React, { useEffect, useState } from 'react';
+import { CardsLayout, Heading } from '@rug-zombie-libs/uikit'
 import './Collectibles.Styles.css'
 import nfts from 'redux/nfts';
 import CollectiblesCard from './CollectiblesCard';
 
-const Collectibles:React.FC = () => {
+const Collectibles: React.FC = () => {
+
+  const [rarityArr, setRarityArray] = useState([]);
+
+  const getRarityArray = () => {
+    const rarityData = []
+    for (let i = 0; i < nfts.length; i++) {
+      if (nfts[i].rarity && !rarityData.includes(nfts[i].rarity)) {
+        rarityData.push(nfts[i].rarity);
+      }
+    }
+    return rarityData;
+  }
+
+  useEffect(() => {
+    setRarityArray(getRarityArray());
+  }, [])
   return (
-    <CardsLayout className="collectibles">
-      <>
-        {nfts.map((nft) => (
-          <CollectiblesCard nft={nft} />
-        ))}
-      </>
-    </CardsLayout>
+    <>
+      {rarityArr.map((rarity) => {
+        return <>
+          <Heading className='cardHeader' size="md" textTransform="capitalize" color='text'>
+            {rarity}
+          </Heading>
+          <CardsLayout className="collectibles">
+            {nfts.map((nft) => {
+              return nft.rarity === rarity ? <CollectiblesCard nft={nft} /> : null
+            })}
+          </CardsLayout>
+        </>
+      })}
+    </>
   )
 }
 
--- src/views/Collectibles/components/Collectibles/CollectiblesCard.tsx
+++ src/views/Collectibles/components/Collectibles/CollectiblesCard.tsx
@@ -49,37 +49,37 @@ const CollectiblesCard: React.FC<CollectiblesCardProps> = ({ nft }: Collectibles
   return (
     <div>
       <Card className="card-active">
-      <StyleCardHeader>
-        {type === 'image' ? <img
-          src={path} alt='test'
-          style={{ width: '100%', height: '100%' }} /> :
-          <video width="100%" height="100%" autoPlay>
-            <source src={path} type="video/mp4" />
-          </video>}
-      </StyleCardHeader>
-      <CardBody>
-        <Heading as='h2' fontSize="20px">{name}</Heading>
-      </CardBody>
-      <CardFooter>
-        <StyleDetails>
-          <StyleCursorPointer onClick={toggleOpen}>
-            Details
+        <StyleCardHeader>
+          {type === 'image' ? <img
+            src={path} alt='test'
+            style={{ width: '100%', height: '100%' }} /> :
+            <video width="100%" height="100%" autoPlay>
+              <source src={path} type="video/mp4" />
+            </video>}
+        </StyleCardHeader>
+        <CardBody>
+          <Heading as='h2' fontSize="20px">{name}</Heading>
+        </CardBody>
+        <CardFooter>
+          <StyleDetails>
+            <StyleCursorPointer onClick={toggleOpen}>
+              Details
                   {
-              isOpen ? <ChevronUpIcon color="text" ml="10px" />
-                : <ChevronDownIcon color="text" ml="10px" />
-            }
-          </StyleCursorPointer>
-        </StyleDetails>
-        {
-          isOpen &&
-          <Styleinfo>
-            <p>
-              {name}
-            </p>
-          </Styleinfo>
-        }
-      </CardFooter>
-    </Card>
+                isOpen ? <ChevronUpIcon color="text" ml="10px" />
+                  : <ChevronDownIcon color="text" ml="10px" />
+              }
+            </StyleCursorPointer>
+          </StyleDetails>
+          {
+            isOpen &&
+            <Styleinfo>
+              <p>
+                {name}
+              </p>
+            </Styleinfo>
+          }
+        </CardFooter>
+      </Card>
     </div>
   )
 }
