04ac24adffa2f595cdf91f03be46362fe079fd8a
Chef Cheems <chefcheems@protonmail.com>
2021-08-23 17:29:46 +0300

    chore: Revert back to top 30 pools/tokens

--- src/state/info/queries/tokens/topTokens.ts
+++ src/state/info/queries/tokens/topTokens.ts
@@ -20,7 +20,7 @@ const fetchTopTokens = async (timestamp24hAgo: number): Promise<string[]> => {
     const query = gql`
       query topTokens($blacklist: [String!], $timestamp24hAgo: Int) {
         tokenDayDatas(
-          first: 5
+          first: 30
           where: { dailyTxns_gt: 300, id_not_in: $blacklist, date_gt: $timestamp24hAgo }
           orderBy: dailyVolumeUSD
           orderDirection: desc
