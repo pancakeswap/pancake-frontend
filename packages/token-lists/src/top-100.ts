67d89c63e0630cb20354c35847d76b2b36af2d8e
Chef Kai <74599990+ChefKai@users.noreply.github.com>
2021-08-25 05:12:44 +0200

    fix(token-lists): Top100 sanitization (#251)
    * chore(token-lists): Sanitize Top100 tokens
    
    * chore(token-lists): Generate Top100 Token List
    
    * fix(token-lists): Escape character

--- packages/token-lists/src/top-100.ts
+++ packages/token-lists/src/top-100.ts
@@ -2,6 +2,7 @@ import fs from "fs";
 import path from "path";
 import { request, gql } from "graphql-request";
 import { getAddress } from "@ethersproject/address";
+import slugify from "slugify";
 
 const pathToImages = process.env.CI
   ? path.join(process.env.GITHUB_WORKSPACE, "packages", "token-lists", "lists", "images")
@@ -22,14 +23,26 @@ interface BitqueryEntity {
 
 // Default token list for exchange + manual exclusion of broken BEP-20 token(s)
 const blacklist: string[] = [
+  // List of default tokens to exclude
   "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c", // WBNB
+  "0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82", // CAKE
   "0xe9e7cea3dedca5984780bafc599bd69add087d56", // BUSD
-  "0x2170ed0880ac9a755fd29b2688956bd959f933f8", // ETH
+  "0x55d398326f99059fF775485246999027B3197955", // USDT
   "0x7130d2a12b9bcbfae4f2634d864a1ee1ce3ead9c", // BTCB
-  "0x55d398326f99059ff775485246999027b3197955", // USDT
-  "0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82", // Cake
+  "0x2170ed0880ac9a755fd29b2688956bd959f933f8", // ETH
+  "0xc9849e6fdb743d08faee3e34dd2d1bc69ea11a51", // BUNNY
+  "0xcf6bb5389c92bdda8a3747ddb454cb7a64626c63", // XVS
+  "0x8076c74c5e3f5852037f31ff0093eeb8c8add8d3", // SAFEMOON
+  "0x8f0528ce5ef7b51152a59745befdd91d97091d2f", // ALPACA
+  "0x7083609fce4d1d8dc0c979aab8c869ea2c873402", // DOT
+  "0x4e6415a5727ea08aae4580057187923aec331227", // FINE
+  "0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3", // DAI
+  "0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d", // USDC
+  "0x844fa82f1e54824655470970f7004dd90546bb28", // DOP
 
-  "0x4269e4090ff9dfc99d8846eb0d42e67f01c3ac8b", // BROKEN TOKEN
+  // List of broken tokens
+  "0x4269e4090ff9dfc99d8846eb0d42e67f01c3ac8b",
+  "0xe2e7329499e8ddb1f2b04ee4b35a8d7f6881e4ea",
 ];
 
 /**
@@ -118,8 +131,14 @@ const main = async (): Promise<void> => {
       const checksummedAddress = getAddress(item.baseCurrency.address);
 
       const updatedToken = {
-        name: item.baseCurrency.name,
-        symbol: item.baseCurrency.symbol.toUpperCase(),
+        name: slugify(item.baseCurrency.name, {
+          replacement: " ",
+          remove: /[^\w\s.]/g,
+        }),
+        symbol: slugify(item.baseCurrency.symbol, {
+          replacement: "-",
+          remove: /[^\w\s.]/g,
+        }).toUpperCase(),
         address: checksummedAddress,
         chainId: 56,
         decimals: item.baseCurrency.decimals,
