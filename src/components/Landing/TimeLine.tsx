0a8206c0fa5f2b0d23cbfaf1ee382032d1a293e3
Nnamdi Arimah <nnamdi.arimah@shopify.com>
2021-08-28 19:06:01 -0400

    fix: Buy background

--- src/components/Landing/TimeLine.tsx
+++ src/components/Landing/TimeLine.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import './Landing.Styles.css'
-import { getDrFrankensteinAddress, getZombieAddress } from '../../utils/addressHelpers'
-import get from '../../../scripts/lib/http-get'
+import { Flex, useMatchBreakpoints } from '@rug-zombie-libs/uikit'
 
 const TimeLine = () => {
-  const zmbeAddr = "0x50ba8BF9E34f0F83F96a340387d1d3888BA4B3b5"
-  const topAddress = zmbeAddr.substring(0, zmbeAddr.length / 2)
-  const bottomAddress = zmbeAddr.substring(zmbeAddr.length / 2, zmbeAddr.length)
-  const bscscanLink = "https://bscscan.com/token/0x50ba8bf9e34f0f83f96a340387d1d3888ba4b3b5"
+  const zmbeAddr = '0x50ba8BF9E34f0F83F96a340387d1d3888BA4B3b5'
+  const { isLg, isXl } = useMatchBreakpoints()
+  const isDesktop = isLg || isXl
+
   return (
-       <iframe src="https://app.bogged.finance/swap?tokenIn=BNB&tokenOut=0x50ba8bf9e34f0f83f96a340387d1d3888ba4b3b5&embed=1" height="900px" width="100%"  title="BUY $ZMBE now using BogSwap"
-       style={{margin:10}}/> 	
+    <div id='Timeline' className="bog-buy-background" style={{width: "100%"}}>
+      <Flex justifyContent="center">
+        <iframe
+          src='https://app.bogged.finance/swap?tokenIn=BNB&tokenOut=0x50ba8bf9e34f0f83f96a340387d1d3888ba4b3b5&embed=1'
+          height='900px'  title='BUY $ZMBE now using BogSwap'
+          style={{ margin: 10, minWidth: isDesktop ? '30%' : '80%' }} />
+      </Flex>
+    </div>
   )
 }
 
