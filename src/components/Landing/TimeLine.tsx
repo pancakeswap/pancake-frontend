315829290e8a69b0937806b5650dd4574679e781
Nnamdi Arimah <nnamdi.arimah@shopify.com>
2021-08-28 20:25:00 -0400

    fix: Bogswap mobile

--- src/components/Landing/TimeLine.tsx
+++ src/components/Landing/TimeLine.tsx
@@ -3,16 +3,15 @@ import './Landing.Styles.css'
 import { Flex, useMatchBreakpoints } from '@rug-zombie-libs/uikit'
 
 const TimeLine = () => {
-  const zmbeAddr = '0x50ba8BF9E34f0F83F96a340387d1d3888BA4B3b5'
-  const { isLg, isXl } = useMatchBreakpoints()
-  const isDesktop = isLg || isXl
+  const { isLg, isXl, isMd } = useMatchBreakpoints()
+  const isDesktop = isLg || isXl || isMd
 
   return (
-    <div id='Timeline' className="bog-buy-background" style={{width: "100%"}}>
+    <div id='Timeline' className="bog-buy-background" style={{width: "100%", paddingTop: isDesktop ? "0px" : "80px", height: isDesktop ? '1200px' : '1280px'}}>
       <Flex justifyContent="center">
         <iframe
           src='https://app.bogged.finance/swap?tokenIn=BNB&tokenOut=0x50ba8bf9e34f0f83f96a340387d1d3888ba4b3b5&embed=1'
-          height='900px'  title='BUY $ZMBE now using BogSwap'
+          height="873px"  title='BUY $ZMBE now using BogSwap'
           style={{ margin: 10, minWidth: isDesktop ? '30%' : '80%' }} />
       </Flex>
     </div>
