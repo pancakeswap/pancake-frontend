c3ace29f35f44ae2e7ddb39598828c95fa0b112a
hachiojidev <hachiojidev@protonmail.com>
2021-09-02 13:00:46 +0900

    feat(predictions): Card state for house wins

--- src/views/Predictions/components/PositionTag.tsx
+++ src/views/Predictions/components/PositionTag.tsx
@@ -1,7 +1,17 @@
 import React, { ReactNode } from 'react'
 import styled from 'styled-components'
-import { ArrowUpIcon, ArrowDownIcon, Flex, FlexProps, Text } from '@pancakeswap/uikit'
+import {
+  ArrowUpIcon,
+  ArrowDownIcon,
+  Flex,
+  FlexProps,
+  Text,
+  useTooltip,
+  TooltipText,
+  InfoIcon,
+} from '@pancakeswap/uikit'
 import { BetPosition } from 'state/types'
+import { useTranslation } from 'contexts/Localization'
 
 interface TagProps extends FlexProps {
   bg?: string
@@ -41,8 +51,36 @@ interface PositionTagProps extends FlexProps {
 }
 
 const PositionTag: React.FC<PositionTagProps> = ({ betPosition, children, ...props }) => {
+  const { t } = useTranslation()
   const isUpPosition = betPosition === BetPosition.BULL
   const icon = isUpPosition ? <ArrowUpIcon color="white" /> : <ArrowDownIcon color="white" />
+  const { targetRef, tooltip, tooltipVisible } = useTooltip(
+    <>
+      <Text bold mb="4px">
+        {t('Neither side wins this round')}
+      </Text>
+      <Text>
+        {t(
+          'The Locked Price & Closed Price are exactly the same (within 8 decimals), so neither side wins. All funds entered into UP and DOWN positions will go to the weekly CAKE burn.',
+        )}
+      </Text>
+    </>,
+    { placement: 'right' },
+  )
+
+  if (betPosition === BetPosition.HOUSE) {
+    return (
+      <>
+        {tooltipVisible && tooltip}
+        <TooltipText ref={targetRef} color="secondary" fontWeight="300" textTransform="uppercase">
+          <Flex alignItems="center">
+            {t('To Burn')}
+            <InfoIcon width="16px" ml="4px" color="secondary" />
+          </Flex>
+        </TooltipText>
+      </>
+    )
+  }
 
   return (
     <Tag bg={isUpPosition ? 'success' : 'failure'} startIcon={icon} {...props}>
--- src/views/Predictions/components/RoundResult/RoundResult.tsx
+++ src/views/Predictions/components/RoundResult/RoundResult.tsx
@@ -1,35 +1,19 @@
 import React from 'react'
-import { ethers } from 'ethers'
-import { BoxProps, Flex, Skeleton, Text } from '@pancakeswap/uikit'
-import { BetPosition, NodeRound } from 'state/types'
+import { BoxProps, Text } from '@pancakeswap/uikit'
+import { NodeRound } from 'state/types'
 import { useTranslation } from 'contexts/Localization'
-import { formatUsdv2, getPriceDifference } from '../../helpers'
-import PositionTag from '../PositionTag'
-import { LockPriceRow, PrizePoolRow, RoundResultBox } from './styles'
+import { getRoundPosition } from '../../helpers'
+import { LockPriceRow, PrizePoolRow, RoundPrice, RoundResultBox } from './styles'
 
 interface RoundResultProps extends BoxProps {
   round: NodeRound
   hasFailed?: boolean
 }
 
-const getBetPosition = (closePrice: ethers.BigNumber, lockPrice: ethers.BigNumber) => {
-  if (!closePrice) {
-    return null
-  }
-
-  if (closePrice.eq(lockPrice)) {
-    return BetPosition.HOUSE
-  }
-
-  return closePrice.gt(lockPrice) ? BetPosition.BULL : BetPosition.BEAR
-}
-
 const RoundResult: React.FC<RoundResultProps> = ({ round, hasFailed = false, children, ...props }) => {
   const { lockPrice, closePrice, totalAmount } = round
-  const betPosition = getBetPosition(closePrice, lockPrice)
-  const isPositionUp = betPosition === BetPosition.BULL
+  const betPosition = getRoundPosition(lockPrice, closePrice)
   const { t } = useTranslation()
-  const priceDifference = getPriceDifference(closePrice, lockPrice)
 
   return (
     <RoundResultBox betPosition={betPosition} {...props}>
@@ -41,16 +25,7 @@ const RoundResult: React.FC<RoundResultProps> = ({ round, hasFailed = false, chi
           {t('Canceled')}
         </Text>
       ) : (
-        <Flex alignItems="center" justifyContent="space-between" mb="16px">
-          {closePrice ? (
-            <Text color={isPositionUp ? 'success' : 'failure'} bold fontSize="24px">
-              {formatUsdv2(closePrice)}
-            </Text>
-          ) : (
-            <Skeleton height="34px" my="1px" />
-          )}
-          <PositionTag betPosition={betPosition}>{formatUsdv2(priceDifference)}</PositionTag>
-        </Flex>
+        <RoundPrice lockPrice={lockPrice} closePrice={closePrice} />
       )}
       {lockPrice && <LockPriceRow lockPrice={lockPrice} />}
       <PrizePoolRow totalAmount={totalAmount} />
