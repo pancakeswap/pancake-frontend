8aeff885935f18edd028ba949f3a5624d9c75c82
RabbitDoge <72658581+RabbitDoge@users.noreply.github.com>
2021-09-06 14:18:28 +0900

    feat: Market structure

--- src/config/constants/nfts/index.ts
+++ src/config/constants/nfts/index.ts
@@ -1,8 +1,10 @@
 import pancakeBunnies from './pancakeBunnies'
+import pancakeSquad from './pancakeSquad'
 import { CollectionKey, Nfts } from './types'
 
 const nfts: Nfts = {
   [CollectionKey.PANCAKE]: pancakeBunnies,
+  [CollectionKey.SQUAD]: pancakeSquad,
 }
 
 export default nfts
