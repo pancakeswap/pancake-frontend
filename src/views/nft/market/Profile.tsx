0f7693a718e922b53ea82c2650498ffa0f0a5879
ChefHutch <chef.hutch.pcs@protonmail.com>
2021-09-06 13:56:45 +0100

    style(nft-profile): Header styles

--- src/views/nft/market/Profile.tsx
+++ src/views/nft/market/Profile.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useWeb3React } from '@web3-react/core'
-import { useProfile } from 'hooks/useContract'
 import { useFetchAchievements } from 'state/achievements/hooks'
+import { useProfile } from 'state/profile/hooks'
 import Page from 'components/Layout/Page'
 import Header from './components/BannerHeader'
 
