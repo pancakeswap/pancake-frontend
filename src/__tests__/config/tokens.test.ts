97a6a3095072facbd2649b27b28177d113f2ae44
Chef Chungus <cakechefchungus@protonmail.com>
2021-09-06 16:28:26 +0900

    fix: Formatting

--- src/__tests__/config/tokens.test.ts
+++ src/__tests__/config/tokens.test.ts
@@ -10,7 +10,10 @@ import multicall from 'utils/multicall'
 // remove HERO because there are two tokens with the symbol HERO (StepHero and Hero)
 const tokensToTest = omitBy(
   tokens,
-  (token) => token.symbol.toLowerCase() === 'bnb' || token.symbol.toLowerCase() === 'one' || token.symbol.toLowerCase() === 'hero',
+  (token) =>
+    token.symbol.toLowerCase() === 'bnb' ||
+    token.symbol.toLowerCase() === 'one' ||
+    token.symbol.toLowerCase() === 'hero',
 )
 
 describe('Config tokens', () => {
