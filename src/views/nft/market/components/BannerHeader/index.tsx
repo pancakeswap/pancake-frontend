4676091fc0f1f08dd50a12e6d64e99bbf56acea8
Chef Hutch <79279477+ChefHutch@users.noreply.github.com>
2021-09-07 00:18:15 +0100

    feat: nft Collections & Profile header (#2106)
    * feat(nft-profile): Add basic header comp and bannerimage
    
    * style(nft-profile): Header styles
    
    * feat(nft-profile): Prop typing and Profile Header
    
    * feat(nft-profile): Allow BannerHeader to receive an avatar component rather than rendering an image

---
+++ src/views/nft/market/components/BannerHeader/index.tsx
@@ -0,0 +1,42 @@
+import React, { ReactNode } from 'react'
+import { Flex, Box } from '@pancakeswap/uikit'
+import BannerImage from './BannerImage'
+import CollectionStatsWrapper from './CollectionStatsWrapper'
+
+interface HeaderProps {
+  bannerImage: string
+  bannerAlt?: string
+  Avatar: ReactNode
+  IconButtons: ReactNode
+  TextContent: ReactNode
+  CollectionStats: ReactNode
+}
+
+const Header: React.FC<HeaderProps> = ({
+  bannerImage,
+  bannerAlt,
+  Avatar,
+  IconButtons,
+  TextContent,
+  CollectionStats,
+}) => {
+  return (
+    <Flex flexDirection="column">
+      <Box position="relative">
+        <BannerImage src={bannerImage} alt={bannerAlt} />
+        <Box position="absolute" bottom={-68} left={-4}>
+          <Flex alignItems="flex-end">
+            {Avatar}
+            {IconButtons}
+          </Flex>
+        </Box>
+      </Box>
+      <Flex flexDirection={['column', null, 'row']} justifyContent="space-between" mt={80}>
+        {TextContent}
+        <CollectionStatsWrapper>{CollectionStats}</CollectionStatsWrapper>
+      </Flex>
+    </Flex>
+  )
+}
+
+export default Header
