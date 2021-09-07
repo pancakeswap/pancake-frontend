aaab5920e9eaf6b1119f71649ba25ce1a9bb3fe4
Chef Hachioji <72719739+hachiojidev@users.noreply.github.com>
2021-09-07 16:47:51 +0900

    style: Collectible card (#2112)
    * style: Collectible card
    
    * chore: Put temp data in its own file

---
+++ src/views/nft/components/CollectibleCard/types.ts
@@ -0,0 +1,10 @@
+import { Nft } from 'config/constants/nfts/types'
+
+// Temp
+export interface Collectible {
+  name: string
+  cost: number
+  lowestCost?: number
+  nft: Nft
+  status: 'selling' | 'profile' | 'wallet'
+}
