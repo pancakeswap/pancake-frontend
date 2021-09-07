48d0ecb3c73c63de998ef334e7fd50c7713c09c7
Chef Hachioji <72719739+hachiojidev@users.noreply.github.com>
2021-09-07 16:47:51 +0900

    style: Collectible card (#2112)
    * style: Collectible card
    
    * chore: Put temp data in its own file

--- src/views/nft/market/index.tsx
+++ src/views/nft/market/index.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { Grid } from '@pancakeswap/uikit'
 import { useFetchCollections } from 'state/nftMarket/hooks'
+import NftCard from '../components/CollectibleCard'
+import { example1, example2, example3 } from './tmp'
 
 const Market = () => {
   useFetchCollections()
-  return <div>Market</div>
+
+  return (
+    <Grid
+      p="16px"
+      gridTemplateColumns={['1fr', null, null, 'repeat(3, 256px)']}
+      gridGap="16px"
+      justifyContent={['center', null, null, 'start']}
+    >
+      <div>
+        <NftCard collectible={example1} />
+      </div>
+      <div>
+        <NftCard collectible={example2} />
+      </div>
+      <div>
+        <NftCard collectible={example3} />
+      </div>
+    </Grid>
+  )
 }
 
 export default Market
