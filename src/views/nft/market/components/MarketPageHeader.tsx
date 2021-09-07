291e4b8fb348a32297ef5e3918b75474ce16b39a
Chef Hachioji <72719739+hachiojidev@users.noreply.github.com>
2021-09-07 16:57:00 +0900

    Nft/collectibles page (#2113)
    * style: Collectible card
    
    * chore: Update import
    
    * chore: Move CollectibleCard to market
    
    * feat: Collection pages
    
    * feat: Page title component

---
+++ src/views/nft/market/components/MarketPageHeader.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import PageHeader, { PageHeaderProps } from 'components/PageHeader'
+
+const MarketPageHeader: React.FC<PageHeaderProps> = (props) => (
+  <PageHeader background="linear-gradient(111.68deg, #f2ecf2 0%, #e8f2f6 100%)" pb={80} {...props} />
+)
+
+export default MarketPageHeader
