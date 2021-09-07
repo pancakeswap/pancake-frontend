d9a2e997fbf9f4d9b4fe67248144ecedc5624eb3
Chef Hachioji <72719739+hachiojidev@users.noreply.github.com>
2021-09-07 16:57:00 +0900

    Nft/collectibles page (#2113)
    * style: Collectible card
    
    * chore: Update import
    
    * chore: Move CollectibleCard to market
    
    * feat: Collection pages
    
    * feat: Page title component

---
+++ src/views/nft/market/Collectible/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Page from 'components/Layout/Page'
+import Header from './Header'
+
+const Collectible = () => {
+  return (
+    <>
+      <Header />
+      <Page>Collectible</Page>
+    </>
+  )
+}
+
+export default Collectible
