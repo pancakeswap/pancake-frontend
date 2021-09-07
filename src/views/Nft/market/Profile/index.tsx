cb9418209df0ab0379c91ed99d0a76c3fc6fdf94
ChefHutch <chef.hutch.pcs@protonmail.com>
2021-09-07 10:23:05 +0100

    feat(nft-profile): TabMenu functionality

--- src/views/Nft/market/Profile/index.tsx
+++ src/views/Nft/market/Profile/index.tsx
@@ -1,14 +1,24 @@
 import React from 'react'
+import { useWeb3React } from '@web3-react/core'
+import { useAchievements, useFetchAchievements } from 'state/achievements/hooks'
+import { Box } from '@pancakeswap/uikit'
+import { useProfile } from 'state/profile/hooks'
 import Page from 'components/Layout/Page'
 import MarketPageHeader from '../components/MarketPageHeader'
 import ProfileHeader from './ProfileHeader'
+import TabMenu from './TabMenu'
 
 const NftProfile = () => {
   return (
     <>
       <MarketPageHeader>
         <ProfileHeader />
+
+        <Box position="absolute" bottom="0">
+          <TabMenu />
+        </Box>
       </MarketPageHeader>
+
       <Page>Profile</Page>
     </>
   )
