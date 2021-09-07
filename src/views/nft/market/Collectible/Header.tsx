ed85295a771fc0c71d37c10d03f6771386ce7d2b
hachiojidev <hachiojidev@protonmail.com>
2021-09-07 16:39:36 +0900

    feat: Page title component

--- src/views/nft/market/Collectible/Header.tsx
+++ src/views/nft/market/Collectible/Header.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import { Redirect, useParams } from 'react-router'
 import { useTranslation } from 'contexts/Localization'
 import nfts from 'config/constants/nfts'
-import BannerHeader from '../components/BannerHeader'
+import { formatNumber } from 'utils/formatBalance'
 import MarketPageHeader from '../components/MarketPageHeader'
-import AvatarImage from '../components/BannerHeader/AvatarImage'
+import MarketPageTitle from '../components/MarketPageTitle'
+import StatBox, { StatBoxItem } from '../components/StatBox'
 
 const Header = () => {
   const { t } = useTranslation()
@@ -14,16 +15,28 @@ const Header = () => {
     return <Redirect to="/404" />
   }
 
-  const imgDir = '/images/collections'
-  const collectionName = name.toLowerCase()
+  // const imgDir = '/images/collections'
+  // const collectionName = name.toLowerCase()
+
+  // Temp
+  const items = 10000
+  const owners = 1202
+  const lowest = 0.33
+  const vol = 11929.329
 
   return (
     <MarketPageHeader>
-      <BannerHeader
-        bannerImage={`${imgDir}/${collectionName}.png`}
-        bannerAlt={t('collection banner')}
-        Avatar={<AvatarImage src={`${imgDir}/${collectionName}-avatar.png`} />}
-      />
+      <MarketPageTitle
+        title={name}
+        description={t("PancakeSwap's first official generative NFT collection.. Join the squad.")}
+      >
+        <StatBox>
+          <StatBoxItem title={t('Items')} stat={formatNumber(items, 0, 0)} />
+          <StatBoxItem title={t('Owners')} stat={formatNumber(owners, 0, 0)} />
+          <StatBoxItem title={t('Lowest (BNB)')} stat={formatNumber(lowest, 0, 4)} />
+          <StatBoxItem title={t('Vol. (BNB)')} stat={formatNumber(vol, 0, 4)} />
+        </StatBox>
+      </MarketPageTitle>
     </MarketPageHeader>
   )
 }
