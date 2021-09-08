6ed32537c5970d957b96f830ffd0f26d5d74c708
Chef Hachioji <72719739+hachiojidev@users.noreply.github.com>
2021-09-08 17:23:27 +0900

    Nft/collection list (#2131)
    * fix: Import
    
    * feat(nft-marketplace): Display collection
    
    * refactor(nft-profile): Clean up profile header
    
    * chore: Fix image and add missing translation strings

--- src/views/Nft/market/Collectible/Header.tsx
+++ src/views/Nft/market/Collectible/Header.tsx
@@ -1,22 +1,20 @@
 import React from 'react'
-import { Redirect, useParams } from 'react-router'
+import { Text } from '@pancakeswap/uikit'
+import { Collection } from 'config/constants/nfts/types'
 import { useTranslation } from 'contexts/Localization'
-import nfts from 'config/constants/nfts'
 import { formatNumber } from 'utils/formatBalance'
 import MarketPageHeader from '../components/MarketPageHeader'
 import MarketPageTitle from '../components/MarketPageTitle'
 import StatBox, { StatBoxItem } from '../components/StatBox'
+import BannerHeader from '../components/BannerHeader'
+import AvatarImage from '../components/BannerHeader/AvatarImage'
 
-const Header = () => {
+interface HeaderProps {
+  collection: Collection
+}
+
+const Header: React.FC<HeaderProps> = ({ collection }) => {
   const { t } = useTranslation()
-  const { name } = useParams<{ name: string }>()
-
-  if (!name || !nfts[name]) {
-    return <Redirect to="/404" />
-  }
-
-  // const imgDir = '/images/collections'
-  // const collectionName = name.toLowerCase()
 
   // Temp
   const items = 10000
@@ -26,15 +24,19 @@ const Header = () => {
 
   return (
     <MarketPageHeader>
+      <BannerHeader
+        bannerImage={`/images/collections/${collection.slug}.png`}
+        avatar={<AvatarImage src={`/images/collections/${collection.slug}-avatar.png`} />}
+      />
       <MarketPageTitle
-        title={name}
-        description={t("PancakeSwap's first official generative NFT collection.. Join the squad.")}
+        title={collection.name}
+        description={collection.description ? <Text color="textSubtle">{t(collection.description)}</Text> : null}
       >
         <StatBox>
           <StatBoxItem title={t('Items')} stat={formatNumber(items, 0, 0)} />
           <StatBoxItem title={t('Owners')} stat={formatNumber(owners, 0, 0)} />
-          <StatBoxItem title={t('Lowest (BNB)')} stat={formatNumber(lowest, 0, 4)} />
-          <StatBoxItem title={t('Vol. (BNB)')} stat={formatNumber(vol, 0, 4)} />
+          <StatBoxItem title={t('Lowest (%symbol%)', { symbol: 'BNB' })} stat={formatNumber(lowest, 0, 4)} />
+          <StatBoxItem title={t('Vol. (%symbol%)', { symbol: 'BNB' })} stat={formatNumber(vol, 0, 4)} />
         </StatBox>
       </MarketPageTitle>
     </MarketPageHeader>
--- src/views/Nft/market/Collectible/index.tsx
+++ src/views/Nft/market/Collectible/index.tsx
@@ -1,12 +1,37 @@
 import React from 'react'
+import { Redirect, useParams } from 'react-router'
+import random from 'lodash/random'
+import { Grid } from '@pancakeswap/uikit'
+import nfts from 'config/constants/nfts'
+import { Nft } from 'config/constants/nfts/types'
+import { getCollectionFromSlug } from 'config/constants/nfts/helpers'
 import Page from 'components/Layout/Page'
+import { CollectibleCard } from '../components/CollectibleCard'
 import Header from './Header'
 
+// Tmp
+const randomPrice = random(1, 11, true)
+
 const Collectible = () => {
+  const { slug } = useParams<{ slug: string }>()
+  const { key, collection } = getCollectionFromSlug(slug)
+
+  if (!slug || !collection) {
+    return <Redirect to="/404" />
+  }
+
+  const nftList = nfts[key]
+
   return (
     <>
-      <Header />
-      <Page>Collectible</Page>
+      <Header collection={collection} />
+      <Page>
+        <Grid gridGap="16px" gridTemplateColumns={['1fr', null, null, null, 'repeat(4,1fr)']}>
+          {nftList.map((nft: Nft) => (
+            <CollectibleCard key={nft.name} collectible={{ name: collection.name, cost: randomPrice, nft }} />
+          ))}
+        </Grid>
+      </Page>
     </>
   )
 }
