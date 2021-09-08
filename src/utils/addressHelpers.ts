a5a005bdf62cfd84488d164cbca8e9fd281dc2ee
Chef Hutch <79279477+ChefHutch@users.noreply.github.com>
2021-09-08 01:21:05 +0100

    chore(nft-market-contract): Add contract addresses and hooks for nft market (#2128)

--- src/utils/addressHelpers.ts
+++ src/utils/addressHelpers.ts
@@ -61,3 +61,6 @@ export const getBunnySpecialLotteryAddress = () => {
 export const getFarmAuctionAddress = () => {
   return getAddress(addresses.farmAuction)
 }
+export const getNftMarketAddress = () => {
+  return getAddress(addresses.nftMarket)
+}
--- src/utils/contractHelpers.ts
+++ src/utils/contractHelpers.ts
@@ -25,6 +25,7 @@ import {
   getBunnySpecialPredictionAddress,
   getBunnySpecialLotteryAddress,
   getFarmAuctionAddress,
+  getNftMarketAddress,
 } from 'utils/addressHelpers'
 
 // ABI
@@ -55,6 +56,8 @@ import bunnySpecialCakeVaultAbi from 'config/abi/bunnySpecialCakeVault.json'
 import bunnySpecialPredictionAbi from 'config/abi/bunnySpecialPrediction.json'
 import bunnySpecialLotteryAbi from 'config/abi/bunnySpecialLottery.json'
 import farmAuctionAbi from 'config/abi/farmAuction.json'
+import nftMarketAbi from 'config/abi/nftMarket.json'
+
 import { ChainLinkOracleContract, FarmAuctionContract, PancakeProfileContract, PredictionsContract } from './types'
 
 const getContract = (abi: any, address: string, signer?: ethers.Signer | ethers.providers.Provider) => {
@@ -144,4 +147,7 @@ export const getBunnySpecialLotteryContract = (signer?: ethers.Signer | ethers.p
 }
 export const getFarmAuctionContract = (signer?: ethers.Signer | ethers.providers.Provider) => {
   return getContract(farmAuctionAbi, getFarmAuctionAddress(), signer) as FarmAuctionContract
+}
+export const getNftMarketContract = (signer?: ethers.Signer | ethers.providers.Provider) => {
+  return getContract(nftMarketAbi, getNftMarketAddress(), signer)
 }
