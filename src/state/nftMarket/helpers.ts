fd566aaa8a82fba4aa17e55843df146329b0fa5b
RabbitDoge <72658581+RabbitDoge@users.noreply.github.com>
2021-09-08 17:35:19 +0900

    feat: Fetch nfts (#2134)

--- src/state/nftMarket/helpers.ts
+++ src/state/nftMarket/helpers.ts
@@ -20,10 +20,35 @@ export const getCollections = async (): Promise<any[]> => {
         }
       `,
     )
-
     return res.collections
   } catch (error) {
     console.error('Failed to fetch NFT collections', error)
+    return []
+  }
+}
+
+export const getNftsFromCollection = async (collectionAddress: string): Promise<any[]> => {
+  try {
+    const res = await request(
+      GRAPH_API_NFTMARKET,
+      gql`
+        query getNftsFromCollection($collectionAddress: String!) {
+          collection(id: $collectionAddress) {
+            id
+            NFTs {
+              currentSeller
+              isTradable
+              tokenId
+              metadataUrl
+            }
+          }
+        }
+      `,
+      { collectionAddress: collectionAddress.toLowerCase() },
+    )
+    return res.collection.NFTs
+  } catch (error) {
+    console.error('Failed to fetch NFTs', error)
     return []
   }
 }
