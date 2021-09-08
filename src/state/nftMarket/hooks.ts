afcc197b83e969baec2465f02df09abcea6027da
RabbitDoge <72658581+RabbitDoge@users.noreply.github.com>
2021-09-08 17:35:19 +0900

    feat: Fetch nfts (#2134)

--- src/state/nftMarket/hooks.ts
+++ src/state/nftMarket/hooks.ts
@@ -1,10 +1,11 @@
 import { useEffect } from 'react'
 import { useAppDispatch } from 'state'
-import { fetchCollections } from './reducer'
+import { fetchCollections, fetchNftsFromCollections } from './reducer'
 
 export const useFetchCollections = () => {
   const dispatch = useAppDispatch()
   useEffect(() => {
     dispatch(fetchCollections())
+    dispatch(fetchNftsFromCollections('0x60935f36e4631f73f0f407e68642144e07ac7f5e'))
   }, [dispatch])
 }
