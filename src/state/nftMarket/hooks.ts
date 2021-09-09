0570837de61cda43af71d9cf16eb7d826b7b1190
RabbitDoge <72658581+RabbitDoge@users.noreply.github.com>
2021-09-09 17:24:28 +0900

    Nft state (#2147)
    * chore: Nft state data
    
    * chore: Remove unused code

--- src/state/nftMarket/hooks.ts
+++ src/state/nftMarket/hooks.ts
@@ -1,6 +1,10 @@
 import { useEffect } from 'react'
+import find from 'lodash/find'
+import { useSelector } from 'react-redux'
 import { useAppDispatch } from 'state'
+import slugify from 'utils/slugify'
 import { fetchCollections, fetchNftsFromCollections } from './reducer'
+import { State } from '../types'
 
 export const useFetchCollections = () => {
   const dispatch = useAppDispatch()
@@ -8,4 +12,14 @@ export const useFetchCollections = () => {
     dispatch(fetchCollections())
     dispatch(fetchNftsFromCollections('0x60935f36e4631f73f0f407e68642144e07ac7f5e'))
   }, [dispatch])
+}
+
+export const useCollectionFromSlug = (slug: string) => {
+  const collections = useSelector((state: State) => state.nftMarket.data.collections)
+  return find(collections, (collection) => slugify(collection.name) === slug)
+}
+
+export const useNftsFromCollection = (collectionAddress: string) => {
+  const collections = useSelector((state: State) => state.nftMarket.data.nfts[collectionAddress])
+  return collections
 }
