e989e73bd7dddd9640ddba3b1af2eec1b6a5dedf
hachiojidev <hachiojidev@protonmail.com>
2021-09-10 12:12:30 +0900

    feat(nftmarket): Min max filter

---
+++ src/views/Nft/market/components/Filters/index.ts
@@ -0,0 +1,1 @@
+export { default as PriceFilter } from './MinMaxFilter'
--- src/views/Nft/market/index.tsx
+++ src/views/Nft/market/index.tsx
@@ -1,19 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Box } from '@pancakeswap/uikit'
 import { useFetchCollections } from 'state/nftMarket/hooks'
+import Page from 'components/Layout/Page'
+import { PriceFilter } from './components/Filters'
 
 const Market = () => {
+  const handleApply = (min: number, max: number) => {
+    console.log(min, max)
+  }
+
   useFetchCollections()
 
   return (
-    <Box p="32px">
+    <Page>
       <Link to="/nft/market/collectibles">Collectibles</Link>
       <br />
       <Link to="/nft/market/profile">Profile</Link>
       <br />
       <Link to="/nft/market/item/7">Individual NFT page</Link>
-    </Box>
+      <br />
+      <br />
+      <PriceFilter min={1} max={100} onApply={handleApply} />
+    </Page>
   )
 }
 
