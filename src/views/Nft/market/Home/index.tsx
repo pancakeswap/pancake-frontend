ad457d5a49cdbebd3cf3f5ddf402af92d8a09f80
hachiojidev <hachiojidev@protonmail.com>
2021-09-13 16:31:07 +0900

    feat: Hot collections card

--- src/views/Nft/market/Home/index.tsx
+++ src/views/Nft/market/Home/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Box, Button, ChevronRightIcon, Flex, Grid, Heading, Text } from '@pancakeswap/uikit'
 import { Link } from 'react-router-dom'
+import { useTranslation } from 'contexts/Localization'
 import Page from 'components/Layout/Page'
+import { HotCollectionCard } from '../components/CollectibleCard'
+import { BNBAmountLabel } from '../components/CollectibleCard/styles'
 
 const Home = () => {
+  const { t } = useTranslation()
+
   return (
     <>
       <Page>
@@ -10,7 +16,48 @@ const Home = () => {
         <br />
         <Link to="/nft/market/profile">Profile</Link>
         <br />
-        <Link to="/nft/market/item/7">Individual NFT page</Link>
+        <Box mb="40px">
+          <Link to="/nft/market/item/7">Individual NFT page</Link>
+        </Box>
+        <Flex alignItems="center" justifyContent="space-between" mb="32px">
+          <Heading as="h3" scale="lg">
+            {t('Hot Collections')}
+          </Heading>
+          <Button variant="secondary" scale="sm" endIcon={<ChevronRightIcon color="primary" width="24px" />}>
+            {t('View All')}
+          </Button>
+        </Flex>
+        <Grid gridGap="16px" gridTemplateColumns="repeat(3, 1fr)">
+          <HotCollectionCard
+            bgSrc="/images/collections/pancake-bunnies-banner-sm.png"
+            avatarSrc="/images/collections/pancake-bunnies-avatar.png"
+            collectionName="Pancake Bunnies"
+          >
+            <Flex alignItems="center">
+              <Text fontSize="12px" color="textSubtle">
+                {t('Volume')}
+              </Text>
+              <BNBAmountLabel amount={255.66} />
+            </Flex>
+          </HotCollectionCard>
+          <HotCollectionCard
+            bgSrc="/images/collections/pancake-squad-banner-sm.png"
+            avatarSrc="/images/collections/pancake-squad-avatar.png"
+            collectionName="Pancake Squad"
+          >
+            <Flex alignItems="center">
+              <Text fontSize="12px" color="textSubtle">
+                {t('Volume')}
+              </Text>
+              <BNBAmountLabel amount={43000.02} />
+            </Flex>
+          </HotCollectionCard>
+          <HotCollectionCard bgSrc="/images/collections/no-collection-banner-sm.png" collectionName="Coming Soon">
+            <Text fontSize="12px" color="textSubtle">
+              {t('More Collections are on their way!')}
+            </Text>
+          </HotCollectionCard>
+        </Grid>
       </Page>
     </>
   )
