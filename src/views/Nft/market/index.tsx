8cf184f734b88db317ff4748bcd8fc5698c5b17e
Chef Hachioji <72719739+hachiojidev@users.noreply.github.com>
2021-09-13 12:09:37 +0900

    Nft/filters (#2156)
    * feat(nftmarket): Min max filter
    
    * feat: List filter
    
    * chore: Move filters up to global components
    
    * style: Add filter header
    
    * feat: Add image to filter list item
    
    * chore: Update translations

--- src/views/Nft/market/index.tsx
+++ src/views/Nft/market/index.tsx
@@ -1,19 +1,48 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Box } from '@pancakeswap/uikit'
+import { random } from 'lodash'
 import { useFetchCollections } from 'state/nftMarket/hooks'
+import Page from 'components/Layout/Page'
+import { MinMaxFilter, ListFilter } from 'components/Filters'
+import { Item } from 'components/Filters/ListFilter/styles'
+
+const exampleList: Item[] = [
+  { image: '/images/collections/pancake-bunnies-avatar.png', label: 'Collection Name', count: random(1, 50) },
+  { label: 'Smiley', count: random(1, 50) },
+  { label: 'Big', count: random(1, 50) },
+  { label: 'Teeth', count: random(1, 50) },
+  { label: 'Cigar', count: random(1, 50) },
+  { label: 'Lollipop', count: random(1, 50) },
+  { label: 'Ears', count: random(1, 50) },
+  { label: 'Glasses', count: random(1, 50) },
+  { label: 'Frown', count: random(1, 50) },
+  { label: 'Zombie', count: random(1, 50) },
+  { label: 'Bazooka', count: random(1, 50) },
+]
 
 const Market = () => {
+  const handleApply = (min: number, max: number) => {
+    console.log(min, max)
+  }
+
+  const handleListApply = (items: Item[]) => {
+    console.log(items)
+  }
+
   useFetchCollections()
 
   return (
-    <Box p="32px">
+    <Page>
       <Link to="/nft/market/collectibles">Collectibles</Link>
       <br />
       <Link to="/nft/market/profile">Profile</Link>
       <br />
       <Link to="/nft/market/item/7">Individual NFT page</Link>
-    </Box>
+      <br />
+      <br />
+      <MinMaxFilter title="Price" min={1} max={100} onApply={handleApply} />
+      <ListFilter title="Attribute" items={exampleList} onApply={handleListApply} />
+    </Page>
   )
 }
 
