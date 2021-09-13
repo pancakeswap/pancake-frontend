6dd4c6650e06e086571a2d0abf2a40c86e184e04
Chef Hachioji <72719739+hachiojidev@users.noreply.github.com>
2021-09-13 12:09:37 +0900

    Nft/filters (#2156)
    * feat(nftmarket): Min max filter
    
    * feat: List filter
    
    * chore: Move filters up to global components
    
    * style: Add filter header
    
    * feat: Add image to filter list item
    
    * chore: Update translations

---
+++ src/components/Filters/FilterFooter.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Grid, GridProps } from '@pancakeswap/uikit'
+
+const FilterFooter: React.FC<GridProps> = ({ children, ...props }) => (
+  <Grid
+    gridGap="16px"
+    gridTemplateColumns="repeat(2,1fr)"
+    {...props}
+    px="24px"
+    py="16px"
+    borderTop="1px solid"
+    borderTopColor="cardBorder"
+  >
+    {children}
+  </Grid>
+)
+
+export default FilterFooter
