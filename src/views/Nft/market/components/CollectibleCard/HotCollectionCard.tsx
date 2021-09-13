a872e84092a223f9028c9a537b34efea4b11323e
Chef Hachioji <72719739+hachiojidev@users.noreply.github.com>
2021-09-13 17:17:22 +0900

    feat: Hot collections card (#2171)

---
+++ src/views/Nft/market/components/CollectibleCard/HotCollectionCard.tsx
@@ -0,0 +1,50 @@
+import { CardBody, Flex, Heading, Image, ProfileAvatar } from '@pancakeswap/uikit'
+import React from 'react'
+import styled from 'styled-components'
+import { StyledCollectibleCard } from './styles'
+
+interface HotCollectionCardProps {
+  bgSrc: string
+  avatarSrc?: string
+  collectionName: string
+}
+
+export const CollectionAvatar = styled(ProfileAvatar)`
+  left: 0;
+  position: absolute;
+  top: -32px;
+`
+
+const StyledHotCollectionCard = styled(StyledCollectibleCard)`
+  border-bottom-left-radius: 56px;
+
+  & > div {
+    border-bottom-left-radius: 56px;
+  }
+`
+
+const HotCollectionCard: React.FC<HotCollectionCardProps> = ({ bgSrc, avatarSrc, collectionName, children }) => {
+  return (
+    <StyledHotCollectionCard>
+      <CardBody p="8px">
+        <Image src={bgSrc} height={125} width={375} />
+        <Flex
+          position="relative"
+          height="65px"
+          justifyContent="center"
+          alignItems="flex-end"
+          py="8px"
+          flexDirection="column"
+        >
+          <CollectionAvatar src={avatarSrc} width={96} height={96} />
+          <Heading as="h3" mb={children ? '8px' : '0'}>
+            {collectionName}
+          </Heading>
+          {children}
+        </Flex>
+      </CardBody>
+    </StyledHotCollectionCard>
+  )
+}
+
+export default HotCollectionCard
