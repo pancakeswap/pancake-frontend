9426cd1d8efa6b86304d69c24be0145e91d015fa
memoyil <2213635+memoyil@users.noreply.github.com>
2021-09-14 02:35:45 +0200

    refactor: Remove bignumber in constants (#2161)

--- src/config/index.ts
+++ src/config/index.ts
@@ -21,9 +21,9 @@ export const BASE_BSC_SCAN_URLS = {
 // 9 CAKE per block goes to Yield farms and lottery
 // CAKE_PER_BLOCK in config/index.ts = 40 as we only change the amount sent to the burn pool which is effectively a farm.
 // CAKE/Block in src/views/Home/components/CakeDataRow.tsx = 15 (40 - Amount sent to burn pool)
-export const CAKE_PER_BLOCK = new BigNumber(40)
-export const BLOCKS_PER_YEAR = new BigNumber((60 / BSC_BLOCK_TIME) * 60 * 24 * 365) // 10512000
-export const CAKE_PER_YEAR = CAKE_PER_BLOCK.times(BLOCKS_PER_YEAR)
+export const CAKE_PER_BLOCK = 40
+export const BLOCKS_PER_YEAR = (60 / BSC_BLOCK_TIME) * 60 * 24 * 365 // 10512000
+export const CAKE_PER_YEAR = CAKE_PER_BLOCK * BLOCKS_PER_YEAR
 export const BASE_URL = 'https://pancakeswap.finance'
 export const BASE_ADD_LIQUIDITY_URL = `${BASE_URL}/add`
 export const BASE_BSC_SCAN_URL = BASE_BSC_SCAN_URLS[ChainId.MAINNET]
