08f48c6b8cfc05a4fd1614cba2245ed29a754922
Chef Hachioji <72719739+hachiojidev@users.noreply.github.com>
2021-09-15 14:36:20 +0900

    feat: Filter container (#2187)

--- src/views/Nft/market/Collectibles/index.tsx
+++ src/views/Nft/market/Collectibles/index.tsx
@@ -1,12 +1,42 @@
 import React from 'react'
+import { Box } from '@pancakeswap/uikit'
 import { Link } from 'react-router-dom'
 import Page from 'components/Layout/Page'
+import FilterContainer from '../components/FilterContainer'
+
+const exampleList = [
+  { label: 'Smiley', value: 1 },
+  { label: 'Big', value: 1 },
+  { label: 'Teeth', value: 1 },
+  { label: 'Cigar', value: 1 },
+  { label: 'Lollipop', value: 1 },
+  { label: 'Ears', value: 1 },
+  { label: 'Glasses', value: 1 },
+  { label: 'Frown', value: 1 },
+  { label: 'Zombie', value: 1 },
+  { label: 'Bazooka', value: 1 },
+  { label: 'Smiley', value: 1 },
+  { label: 'Big', value: 1 },
+  { label: 'Teeth', value: 1 },
+  { label: 'Cigar', value: 1 },
+  { label: 'Lollipop', value: 1 },
+  { label: 'Ears', value: 1 },
+  { label: 'Glasses', value: 1 },
+  { label: 'Frown', value: 1 },
+  { label: 'Zombie', value: 1 },
+  { label: 'Bazooka', value: 1 },
+]
 
 const Collectible = () => {
   return (
     <>
       <Page>
         <Link to="/nft/market/collectibles/pancake-bunnies">Pancake Bunnies</Link>
+        <Link to="/nft/market/collectibles/pancake-squad">Pancake Bunnies</Link>
+
+        <Box py="40px">
+          <FilterContainer items={exampleList} onFilterClick={(item) => console.log(item)} />
+        </Box>
       </Page>
     </>
   )
