5a0a1477e6af8baa37d08eacb2edd746d103ed0a
Chef Chungus <71833681+Chef-Chungus@users.noreply.github.com>
2021-09-16 12:08:56 +0900

    chore: Increase load on Google to 50% (#2208)

--- src/utils/getRpcUrl.ts
+++ src/utils/getRpcUrl.ts
@@ -3,7 +3,7 @@ import sample from 'lodash/sample'
 // Array of available nodes to connect to
 export const nodes = [
   process.env.REACT_APP_NODE_1,
-  process.env.REACT_APP_NODE_2,
+  // process.env.REACT_APP_NODE_2,
   // process.env.REACT_APP_NODE_3,
   process.env.REACT_APP_NODE_4,
 ]
