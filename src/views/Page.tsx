e18afff60d2a574e708cd273e814a515b619610c
ChefCorgi <corgi.farmer@gmail.com>
2021-09-16 10:05:43 +0200

    fix(duplicate nav): Remove unused import

--- src/views/Page.tsx
+++ src/views/Page.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import styled from 'styled-components'
 import { Flex } from '@pancakeswap/uikit'
 import Footer from 'components/Menu/Footer'
-import SubNav from 'components/Menu/SubNav'
 import { PageMeta } from 'components/Layout/Page'
 
 const StyledPage = styled.div`
