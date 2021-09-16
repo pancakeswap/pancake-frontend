6ae009cf8eaf5a174b0f986f20439ce8e6bc42d6
Chef Hutch <79279477+ChefHutch@users.noreply.github.com>
2021-09-16 04:13:20 +0100

    feat(nft-profile-activity): User Activity action, graph query and sorting hook (#2181)

--- src/state/nftMarket/hooks.ts
+++ src/state/nftMarket/hooks.ts
@@ -3,9 +3,10 @@ import find from 'lodash/find'
 import { useSelector } from 'react-redux'
 import { useAppDispatch } from 'state'
 import slugify from 'utils/slugify'
+import { ethers } from 'ethers'
 import { fetchCollections, fetchNftsFromCollections } from './reducer'
 import { State } from '../types'
-import { Collection, UserNftsState } from './types'
+import { Collection, UserNftsState, AskOrder, Transaction } from './types'
 
 export const useFetchCollections = () => {
   const dispatch = useAppDispatch()
@@ -35,4 +36,24 @@ export const useGetNFTInitializationState = () => {
 
 export const useUserNfts = (): UserNftsState => {
   return useSelector((state: State) => state.nftMarket.data.user)
+}
+
+export const useUserActivity = (): (Transaction | AskOrder)[] => {
+  const userNftState = useUserNfts()
+  const {
+    activity: { askOrderHistory, buyTradeHistory, sellTradeHistory },
+  } = userNftState
+
+  const allActivity = [...askOrderHistory, ...buyTradeHistory, ...sellTradeHistory]
+  if (allActivity.length > 0) {
+    const sortedByMostRecent = allActivity.sort((activityItem1, activityItem2) => {
+      const timestamp1 = ethers.BigNumber.from(activityItem1.timestamp)
+      const timestamp2 = ethers.BigNumber.from(activityItem2.timestamp)
+      return timestamp2.sub(timestamp1).toNumber()
+    })
+    // TODO: This return is an array of different types (AskOrders & Transactions)
+    // Once the UI requirements are clearer - there could be data transformation of these to return data of one type
+    return sortedByMostRecent
+  }
+  return []
 }
