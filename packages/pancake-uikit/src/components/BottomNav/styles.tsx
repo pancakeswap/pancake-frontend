5d7587739fa09d1745467ffe6807d7ade1dfc42d
Sam Chen <chenxsan@gmail.com>
2021-09-16 15:52:59 +0800

    fix(uikit): BottomNav (#279)

--- packages/pancake-uikit/src/components/BottomNav/styles.tsx
+++ packages/pancake-uikit/src/components/BottomNav/styles.tsx
@@ -6,6 +6,7 @@ const StyledBottomNavItem = styled(Flex)`
   bottom: 0px;
   width: 100%;
   padding: 5px 8px;
+  padding-bottom: env(safe-area-inset-bottom);
   background: ${({ theme }) => theme.colors.backgroundAlt};
   border-top: 1px solid ${({ theme }) => theme.colors.cardBorder};
 `;
