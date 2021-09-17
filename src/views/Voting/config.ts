41762a5011b8ebad7e5be86789cd5af51edf590b
Chef Chungus <71833681+Chef-Chungus@users.noreply.github.com>
2021-09-17 16:46:13 +0900

    chore: Make chungus Master of Voting now (#2232)

--- src/views/Voting/config.ts
+++ src/views/Voting/config.ts
@@ -1,5 +1,5 @@
 export const PROPOSALS_TO_SHOW = 10
-export const ADMIN_ADDRESS = '0x6eaf1b33b8672c5dc40aB8f4bA3A0111723126c7'
+export const ADMIN_ADDRESS = '0x977E0C1005Dff8749F8caC22F4dF0bD5f013D1A7'
 export const IPFS_GATEWAY = 'https://gateway.ipfs.io/ipfs'
 export const SNAPSHOT_VERSION = '0.1.3'
 export const PANCAKE_SPACE = 'cake.eth'
