7206dd8e1cf8bb749bb068ba18e2512ac7527586
ChefCorgi <corgi.farmer@gmail.com>
2021-09-17 19:31:22 +0200

    feat(pancake squad): Add bunny section

---
+++ src/views/PancakeSquad/components/BunniesSection/styles.tsx
@@ -0,0 +1,7 @@
+import { Flex } from '@pancakeswap/uikit'
+import styled from 'styled-components'
+
+export const StyledBunnySectionContainer = styled(Flex)`
+  background: linear-gradient(180deg, #e6feff 0%, #f3efff 100%);
+  margin-top: 40px;
+`
