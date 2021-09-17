14b90da9e30fbb407bdda48962ec44d03c7c311c
Chef Hachioji <72719739+hachiojidev@users.noreply.github.com>
2021-09-17 15:26:05 +0900

    feat(nft-collections): List attributes (#2231)

--- src/views/Nft/market/components/FilterContainer.tsx
+++ src/views/Nft/market/components/FilterContainer.tsx
@@ -1,17 +1,14 @@
-import React, { ReactNode, ReactText, useState } from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import capitalize from 'lodash/capitalize'
 import { Box, Button, ChevronDownIcon, ChevronUpIcon, Flex } from '@pancakeswap/uikit'
 import { useTranslation } from 'contexts/Localization'
-
-interface FilterItem {
-  label: ReactNode
-  value: ReactText
-}
+import { NftAttribute } from 'state/nftMarket/types'
 
 interface FilterContainerProps {
-  maxDisplayFilters?: number
-  items: FilterItem[]
-  onFilterClick: (filterItem: FilterItem) => void
+  maxDisplayAttributes?: number
+  attributes: NftAttribute[]
+  onAttributeClick: (attribute: NftAttribute) => void
 }
 
 const LightOutlineButton = styled(Button).attrs({ variant: 'secondary', scale: 'sm' })`
@@ -26,10 +23,15 @@ const ScrollableContainer = styled.div`
   overflow-x: auto;
 `
 
-const FilterContainer: React.FC<FilterContainerProps> = ({ maxDisplayFilters = 10, items, onFilterClick }) => {
+const FilterContainer: React.FC<FilterContainerProps> = ({
+  maxDisplayAttributes = 10,
+  children,
+  attributes,
+  onAttributeClick,
+}) => {
   const [isExpanded, setIsExpanded] = useState(false)
   const { t } = useTranslation()
-  const visibleFilters = isExpanded ? items : items.slice(0, maxDisplayFilters)
+  const visibleAttributes = isExpanded ? attributes : attributes.slice(0, maxDisplayAttributes)
 
   const toggleExpanded = () => setIsExpanded(!isExpanded)
 
@@ -37,25 +39,28 @@ const FilterContainer: React.FC<FilterContainerProps> = ({ maxDisplayFilters = 1
     <Flex minHeight="48px">
       <ScrollableContainer>
         <Flex alignItems="center" flexWrap={['nowrap', null, 'wrap']}>
-          {visibleFilters.map((item) => {
-            const handleClick = () => onFilterClick(item)
+          {children}
+          {visibleAttributes.map((item) => {
+            const handleClick = () => onAttributeClick(item)
 
             return (
               <Button variant="light" scale="sm" mr="4px" mb="16px" onClick={handleClick}>
-                {item.label}
+                {capitalize(item.traitType)}
               </Button>
             )
           })}
         </Flex>
       </ScrollableContainer>
-      <Box display={['none', null, 'block']}>
-        <LightOutlineButton
-          onClick={toggleExpanded}
-          endIcon={isExpanded ? <ChevronUpIcon color="textSubtle" /> : <ChevronDownIcon color="textSubtle" />}
-        >
-          {isExpanded ? t('Less') : t('More')}
-        </LightOutlineButton>
-      </Box>
+      {attributes.length >= maxDisplayAttributes && (
+        <Box display={['none', null, 'block']}>
+          <LightOutlineButton
+            onClick={toggleExpanded}
+            endIcon={isExpanded ? <ChevronUpIcon color="textSubtle" /> : <ChevronDownIcon color="textSubtle" />}
+          >
+            {isExpanded ? t('Less') : t('More')}
+          </LightOutlineButton>
+        </Box>
+      )}
     </Flex>
   )
 }
