36c62c36c91ca6ce49add9ab6a5ab076cc8504ee
ChefHutch <chef.hutch.pcs@protonmail.com>
2021-09-19 17:17:12 +0100

    feat(user-nfts): Add metadata fetch to fetchUserNfts reducer

--- src/views/Nft/market/Profile/components/UserNfts.tsx
+++ src/views/Nft/market/Profile/components/UserNfts.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import { Grid } from '@pancakeswap/uikit'
-import { useGetCollections, useGetNftMetadata, useUserNfts } from 'state/nftMarket/hooks'
+import { useUserNfts } from 'state/nftMarket/hooks'
 import { NftLocation } from 'state/nftMarket/types'
-import { getAddress } from 'ethers/lib/utils'
 import { CollectibleCard } from '../../components/CollectibleCard'
 import GridPlaceholder from '../../components/GridPlaceholder'
 
 const UserNfts = () => {
-  const { nfts: userNfts } = useUserNfts()
-  const nftMetadata = useGetNftMetadata(userNfts)
-  const collections = useGetCollections()
+  const { nfts } = useUserNfts()
 
   const handleCollectibleClick = (nftLocation: NftLocation) => {
     switch (nftLocation) {
@@ -29,24 +26,25 @@ const UserNfts = () => {
 
   return (
     <>
-      {nftMetadata.length > 0 ? (
+      {nfts.length > 0 ? (
         <Grid
           gridGap="16px"
           gridTemplateColumns={['1fr', 'repeat(2, 1fr)', 'repeat(3, 1fr)', null, 'repeat(4, 1fr)']}
           alignItems="start"
         >
-          {userNfts.map((userNft) => {
-            const nftDataForCard = nftMetadata.find((nft) => nft.tokenId === userNft.tokenId)
-            const checksummedAddress = getAddress(userNft.collection.id)
-            const collectionName = collections[checksummedAddress]?.name || '-'
+          {nfts.map((nft) => {
+            const marketData = nft.tokens[nft.tokenId]
+
             return (
               <CollectibleCard
-                onClick={() => handleCollectibleClick(userNft.nftLocation)}
-                key={nftDataForCard.id}
-                collectionName={collectionName}
-                nft={nftDataForCard}
-                currentAskPrice={userNft.currentAskPrice && userNft.isTradable && parseFloat(userNft.currentAskPrice)}
-                nftLocation={userNft.nftLocation}
+                onClick={() => handleCollectibleClick(marketData.nftLocation)}
+                key={nft.id}
+                collectionName={nft.collectionName}
+                nft={nft}
+                currentAskPrice={
+                  marketData.currentAskPrice && marketData.isTradable && parseFloat(marketData.currentAskPrice)
+                }
+                nftLocation={marketData.nftLocation}
               />
             )
           })}
