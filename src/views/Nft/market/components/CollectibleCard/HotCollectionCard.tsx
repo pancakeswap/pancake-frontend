136145a01e28ea8251e5eba41c586c4ac1a420e1
RabbitDoge <72658581+RabbitDoge@users.noreply.github.com>
2021-09-20 17:31:42 +0900

    feat: Add links on hp collections (#2258)

--- src/views/Nft/market/components/CollectibleCard/HotCollectionCard.tsx
+++ src/views/Nft/market/components/CollectibleCard/HotCollectionCard.tsx
@@ -1,5 +1,6 @@
 import { CardBody, Flex, Heading, Image, ProfileAvatar } from '@pancakeswap/uikit'
 import React from 'react'
+import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import { StyledCollectibleCard } from './styles'
 
@@ -7,6 +8,7 @@ interface HotCollectionCardProps {
   bgSrc: string
   avatarSrc?: string
   collectionName: string
+  url: string
 }
 
 export const CollectionAvatar = styled(ProfileAvatar)`
@@ -23,26 +25,28 @@ const StyledHotCollectionCard = styled(StyledCollectibleCard)`
   }
 `
 
-const HotCollectionCard: React.FC<HotCollectionCardProps> = ({ bgSrc, avatarSrc, collectionName, children }) => {
+const HotCollectionCard: React.FC<HotCollectionCardProps> = ({ bgSrc, avatarSrc, collectionName, url, children }) => {
   return (
     <StyledHotCollectionCard>
-      <CardBody p="8px">
-        <Image src={bgSrc} height={125} width={375} />
-        <Flex
-          position="relative"
-          height="65px"
-          justifyContent="center"
-          alignItems="flex-end"
-          py="8px"
-          flexDirection="column"
-        >
-          <CollectionAvatar src={avatarSrc} width={96} height={96} />
-          <Heading as="h3" mb={children ? '8px' : '0'}>
-            {collectionName}
-          </Heading>
-          {children}
-        </Flex>
-      </CardBody>
+      <Link to={url}>
+        <CardBody p="8px">
+          <Image src={bgSrc} height={125} width={375} />
+          <Flex
+            position="relative"
+            height="65px"
+            justifyContent="center"
+            alignItems="flex-end"
+            py="8px"
+            flexDirection="column"
+          >
+            <CollectionAvatar src={avatarSrc} width={96} height={96} />
+            <Heading as="h3" mb={children ? '8px' : '0'}>
+              {collectionName}
+            </Heading>
+            {children}
+          </Flex>
+        </CardBody>
+      </Link>
     </StyledHotCollectionCard>
   )
 }
