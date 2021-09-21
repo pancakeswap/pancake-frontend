2a5ffee588fb7fe37ac52e6a34cb4cfff6533e63
RabbitDoge <72658581+RabbitDoge@users.noreply.github.com>
2021-09-21 15:38:32 +0900

    feat: Add links on nft HP (#2277)

--- src/views/Nft/market/components/CollectibleCard/HotCollectionCard.tsx
+++ src/views/Nft/market/components/CollectibleCard/HotCollectionCard.tsx
@@ -8,7 +8,7 @@ interface HotCollectionCardProps {
   bgSrc: string
   avatarSrc?: string
   collectionName: string
-  url: string
+  url?: string
 }
 
 export const CollectionAvatar = styled(ProfileAvatar)`
@@ -26,27 +26,29 @@ const StyledHotCollectionCard = styled(StyledCollectibleCard)`
 `
 
 const HotCollectionCard: React.FC<HotCollectionCardProps> = ({ bgSrc, avatarSrc, collectionName, url, children }) => {
+  const renderBody = () => (
+    <CardBody p="8px">
+      <Image src={bgSrc} height={125} width={375} />
+      <Flex
+        position="relative"
+        height="65px"
+        justifyContent="center"
+        alignItems="flex-end"
+        py="8px"
+        flexDirection="column"
+      >
+        <CollectionAvatar src={avatarSrc} width={96} height={96} />
+        <Heading as="h3" mb={children ? '8px' : '0'}>
+          {collectionName}
+        </Heading>
+        {children}
+      </Flex>
+    </CardBody>
+  )
+
   return (
     <StyledHotCollectionCard>
-      <Link to={url}>
-        <CardBody p="8px">
-          <Image src={bgSrc} height={125} width={375} />
-          <Flex
-            position="relative"
-            height="65px"
-            justifyContent="center"
-            alignItems="flex-end"
-            py="8px"
-            flexDirection="column"
-          >
-            <CollectionAvatar src={avatarSrc} width={96} height={96} />
-            <Heading as="h3" mb={children ? '8px' : '0'}>
-              {collectionName}
-            </Heading>
-            {children}
-          </Flex>
-        </CardBody>
-      </Link>
+      {url ? <Link to={url}>{renderBody()}</Link> : <div style={{ cursor: 'default' }}>{renderBody()}</div>}
     </StyledHotCollectionCard>
   )
 }
