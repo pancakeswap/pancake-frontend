0f1eec1dedc9340996602f7f4318f8a027dc7e87
Chef Hutch <79279477+ChefHutch@users.noreply.github.com>
2021-09-21 11:08:31 +0100

    feat(nft-profile): Add actions to cards and split cards by link/action (#2281)

--- src/views/Nft/market/components/ProfileNftModal.tsx
+++ src/views/Nft/market/components/ProfileNftModal.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { InjectedModalProps, Modal, Flex, Text, Button, useModal, Link } from '@pancakeswap/uikit'
+import { InjectedModalProps, Modal, Flex, Text, Button, useModal, Link, Grid } from '@pancakeswap/uikit'
 import { BASE_URL } from 'config'
 import useTheme from 'hooks/useTheme'
 import styled from 'styled-components'
@@ -22,23 +22,23 @@ interface ProfileNftModalProps extends InjectedModalProps {
   nft: NFT
 }
 
-// TODO: nft typing here may need changing in order to give us access to some market data
 const ProfileNftModal: React.FC<ProfileNftModalProps> = ({ nft, onDismiss }) => {
   const [onEditProfileModal] = useModal(<EditProfileModal />, false)
   const { t } = useTranslation()
   const { theme } = useTheme()
 
   return (
-    <StyledModal title="NFT" onDismiss={onDismiss} headerBackground={theme.colors.gradients.cardHeader}>
+    <StyledModal title={t('Details')} onDismiss={onDismiss} headerBackground={theme.colors.gradients.cardHeader}>
       <Flex flexDirection="column" maxWidth="350px">
         <Flex p="16px">
-          <RoundedImage src={nft?.image?.thumbnail} height={68} width={68} mr="16px" />
-          <Flex justifyContent="space-between" alignItems="center">
-            <Text bold>{nft?.name}</Text>
-            <Text color="textSubtle" fontSize="12px">
-              {nft?.collectionName}
+          <RoundedImage src={nft.image.thumbnail} height={68} width={68} mr="16px" />
+          <Grid flex="1" gridTemplateColumns="1fr 1fr" alignItems="center">
+            <Text bold>{nft.name}</Text>
+            <Text fontSize="12px" color="textSubtle" textAlign="right">
+              {nft.collectionName}
             </Text>
-          </Flex>
+            {/* TODO: Add lowestPrice when available */}
+          </Grid>
         </Flex>
         <TextWrapper p="24px 16px" flexDirection="column">
           <Text mb="16px">{t("You're using this NFT as your Pancake Profile picture")}</Text>
