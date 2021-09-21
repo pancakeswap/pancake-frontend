6a8dcaa5648e147430a50837efc911a20470be35
Chef Hutch <79279477+ChefHutch@users.noreply.github.com>
2021-09-21 02:43:51 +0100

    chore(nft-market): Create const for '/nfts' route (#2269)

--- src/views/Nft/market/Collectibles/index.tsx
+++ src/views/Nft/market/Collectibles/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Page from 'components/Layout/Page'
+import { nftsBaseUrl } from 'views/Nft/market'
 
 const Collectible = () => {
   return (
     <>
       <Page>
-        <Link to="/nfts/collections/pancake-bunnies">Pancake Bunnies</Link>
+        <Link to={`${nftsBaseUrl}/collections/pancake-bunnies`}>Pancake Bunnies</Link>
       </Page>
     </>
   )
