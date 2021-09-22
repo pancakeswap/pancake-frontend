a80a62a28b4fd4f8d420a81b43308c54a813f996
0xCorgi <88371449+0xCorgi@users.noreply.github.com>
2021-09-22 12:33:26 +0200

    feat(anniversary): Change modal font size (#2300)

--- src/views/Collectibles/components/AnniversaryAchievementModal.tsx
+++ src/views/Collectibles/components/AnniversaryAchievementModal.tsx
@@ -54,7 +54,7 @@ const AnniversaryAchievementModal: React.FC<AnniversaryModalProps> = ({ onDismis
         <Box>
           <AnniversaryImage src="/images/achievements/1-year.svg" />
         </Box>
-        <Text textAlign="center" bold color="secondary" fontSize="24px" mb="24px">
+        <Text textAlign="center" bold color="secondary" maxWidth="400px" fontSize="16px" mb="24px">
           {t('You won points for joining PancakeSwap during the first year of our journey!')}
         </Text>
         <Button
