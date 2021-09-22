f24a4ed23743a6a6d63d363b0e434de0fc909a32
ChefCorgi <corgi.farmer@gmail.com>
2021-09-22 16:15:30 +0200

    feat(pancake squad): Add artist section

---
+++ src/views/PancakeSquad/components/ArtistSection/styles.tsx
@@ -0,0 +1,30 @@
+import { Flex } from '@pancakeswap/uikit'
+import styled from 'styled-components'
+
+export const StyledArtistSectionContainer = styled(Flex)`
+  position: relative;
+  background: linear-gradient(90deg, #faf9fa 0%, #d7caec 100%);
+`
+
+export const StyledArtistBioContainer = styled(Flex)`
+  position: relative;
+  padding: 48px 36px;
+  border-radius: 56px;
+  background: #ffffff;
+  box-shadow: ${({ theme }) => theme.shadows.level1};
+
+  & > svg {
+    position: absolute;
+    width: 20px;
+    height: 85px;
+    left: calc(50% - 10px);
+    top: -51px;
+    transform: rotate(90deg);
+
+    ${({ theme }) => theme.mediaQueries.md} {
+      left: -19px;
+      top: calc(50% - 42px);
+      transform: none;
+    }
+  }
+`
