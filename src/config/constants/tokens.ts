9d634f96e89004ed0e1576e4361dcc1ec112979f
Chef Chungus <71833681+Chef-Chungus@users.noreply.github.com>
2021-09-22 17:37:51 +0900

    feat(farm/pool): APENFT (#2297)
    * feat(farm/pool): APENFT
    
    * fix: SousID
    
    * fix: Decimals

--- src/config/constants/tokens.ts
+++ src/config/constants/tokens.ts
@@ -32,6 +32,7 @@ export const mainnetTokens = {
     'https://pancakeswap.finance/',
   ),
   tlos: new Token(MAINNET, '0xb6C53431608E626AC81a9776ac3e999c5556717c', 18, 'TLOS', 'Telos', 'https://www.telos.net/'),
+  nft: new Token(MAINNET, '0x1fC9004eC7E5722891f5f38baE7678efCB11d34D', 6, 'NFT', 'APENFT', 'https://apenft.org'),
   stephero: new Token(
     MAINNET,
     '0xE8176d414560cFE1Bf82Fd73B986823B89E4F545',
