fa1e96d29a56c35b359facd26963ef4e03ddba66
Ikko Ashimine <eltociear@gmail.com>
2021-09-23 16:42:22 +0900

    chore: Fix typo in priceHelperLps.ts (#2303)
    absense -> absence

--- src/config/constants/priceHelperLps.ts
+++ src/config/constants/priceHelperLps.ts
@@ -5,7 +5,7 @@ const priceHelperLps: SerializedFarmConfig[] = [
   /**
    * These LPs are just used to help with price calculation for MasterChef LPs (farms.ts).
    * This list is added to the MasterChefLps and passed to fetchFarm. The calls to get contract information about the token/quoteToken in the LP are still made.
-   * The absense of a PID means the masterchef contract calls are skipped for this farm.
+   * The absence of a PID means the masterchef contract calls are skipped for this farm.
    * Prices are then fetched for all farms (masterchef + priceHelperLps).
    * Before storing to redux, farms without a PID are filtered out.
    */
