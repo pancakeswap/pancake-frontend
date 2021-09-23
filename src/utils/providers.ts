904282451dbcee1ee3a143375aa2e55d5378a010
Chef Kai <74599990+ChefKai@users.noreply.github.com>
2021-09-23 10:45:32 +0200

    chore: Migrate to Google node only (#2308)
    * chore: Migrate to Google node only
    
    * chore: Fix unused function
    
    * fix: Hiccup

--- src/utils/providers.ts
+++ src/utils/providers.ts
@@ -1,7 +1,6 @@
 import { ethers } from 'ethers'
-import getRpcUrl from 'utils/getRpcUrl'
 
-const RPC_URL = getRpcUrl()
+const RPC_URL = process.env.REACT_APP_NODE_BSC
 
 export const simpleRpcProvider = new ethers.providers.JsonRpcProvider(RPC_URL)
 
