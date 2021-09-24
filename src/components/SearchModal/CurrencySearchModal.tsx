08d0349c0ce177a2d64b8904fb5e9114ad77a3de
Joydeep Singha <67229156+Jds-23@users.noreply.github.com>
2021-09-24 07:56:37 +0530

    Feature/extra srollbar (#2316)
    * style: Remove extra scrollbar in currency search modal (#2296)
    
    * style: Hide scrollbar by keeping its functionality (#2296)

--- src/components/SearchModal/CurrencySearchModal.tsx
+++ src/components/SearchModal/CurrencySearchModal.tsx
@@ -34,6 +34,12 @@ const StyledModalContainer = styled(ModalContainer)`
 
 const StyledModalBody = styled(ModalBody)`
   padding: 24px;
+  overflow-y: auto;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `
 
 interface CurrencySearchModalProps extends InjectedModalProps {
