3e0248882dd82d3a57f9eb3f6f1b546e0d82c414
Chef Cheems <81824236+Chef-Cheems@users.noreply.github.com>
2021-09-25 21:35:59 +0300

    Revert "feat(lottery claim): Revert lottery claim check (#2322)" (#2335)
    This reverts commit 65320eac6c281728676f1a0faf4231c28e5af0d3.

--- src/views/Collectibles/components/GlobalCheckClaimStatus.tsx
+++ src/views/Collectibles/components/GlobalCheckClaimStatus.tsx
@@ -3,10 +3,6 @@ import { useLocation } from 'react-router-dom'
 import { useModal } from '@pancakeswap/uikit'
 import { useWeb3React } from '@web3-react/core'
 import { useAnniversaryAchievementContract } from 'hooks/useContract'
-import { Nft } from 'config/constants/nfts/types'
-import nfts from 'config/constants/nfts'
-import NftGiveawayModal from './NftGiveawayModal'
-import useBunnySpecialLottery from '../hooks/useBunnySpecialLottery'
 import AnniversaryAchievementModal from './AnniversaryAchievementModal'
 
 interface GlobalCheckClaimStatusProps {
@@ -24,75 +20,30 @@ const GlobalCheckClaimStatus: React.FC<GlobalCheckClaimStatusProps> = ({ exclude
   const [canClaimAnniversaryPoints, setCanClaimAnniversaryPoints] = useState(false)
   const { account } = useWeb3React()
   const { pathname } = useLocation()
-  const [claimableNfts, setClaimableNfts] = useState<Nft[]>([])
-  const [onPresentGiftModal] = useModal(<NftGiveawayModal nfts={claimableNfts} />)
-  const { canClaimBaller, canClaimLottie, canClaimLucky } = useBunnySpecialLottery()
   const { canClaim, claimAnniversaryPoints } = useAnniversaryAchievementContract()
   const [onPresentAnniversaryModal] = useModal(<AnniversaryAchievementModal onClick={claimAnniversaryPoints} />)
 
   // Check claim status
   useEffect(() => {
-    const fetchClaimStatus = async () => {
-      const claimable: Nft[] = []
-
-      const nftConfigMap = {
-        lottie: nfts.pancake.find((nft) => nft.identifier === 'lottie'),
-        lucky: nfts.pancake.find((nft) => nft.identifier === 'lucky'),
-        baller: nfts.pancake.find((nft) => nft.identifier === 'baller'),
-      }
-
-      const { canClaim: isBallerClaimable } = await canClaimBaller()
-      const { canClaim: isLottieClaimable } = await canClaimLottie()
-      const { canClaim: isLuckyClaimable } = await canClaimLucky()
-
-      if (isBallerClaimable) {
-        claimable.push(nftConfigMap.baller)
-      }
-
-      if (isLottieClaimable) {
-        claimable.push(nftConfigMap.lottie)
-      }
-
-      if (isLuckyClaimable) {
-        claimable.push(nftConfigMap.lucky)
-      }
-
-      setClaimableNfts(claimable)
-    }
-
     const fetchClaimAnniversaryStatus = async () => {
       const canClaimAnniversary = await canClaim(account)
       setCanClaimAnniversaryPoints(canClaimAnniversary)
     }
 
     if (account) {
-      fetchClaimStatus()
       fetchClaimAnniversaryStatus()
     }
-  }, [account, canClaim, canClaimBaller, canClaimLottie, canClaimLucky])
+  }, [account, canClaim])
 
   // Check if we need to display the modal
   useEffect(() => {
     const matchesSomeLocations = excludeLocations.some((location) => pathname.includes(location))
 
-    if (claimableNfts.length > 0 && !matchesSomeLocations && !hasDisplayedModal.current) {
-      onPresentGiftModal()
-      hasDisplayedModal.current = true
-    }
-
     if (canClaimAnniversaryPoints && !matchesSomeLocations && !hasDisplayedModal.current) {
       onPresentAnniversaryModal()
       hasDisplayedModal.current = true
     }
-  }, [
-    pathname,
-    excludeLocations,
-    hasDisplayedModal,
-    onPresentAnniversaryModal,
-    onPresentGiftModal,
-    claimableNfts,
-    canClaimAnniversaryPoints,
-  ])
+  }, [pathname, excludeLocations, hasDisplayedModal, onPresentAnniversaryModal, canClaimAnniversaryPoints])
 
   // Reset the check flag when account changes
   useEffect(() => {
