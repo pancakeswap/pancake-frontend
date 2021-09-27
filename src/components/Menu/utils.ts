9bdf7de5665a9f46d7a178ce118f98fcab0bfa69
ChefHutch <chef.hutch.pcs@protonmail.com>
2021-09-27 14:10:31 +0100

    chore(active-menu-items): Simplify logic. Add test

--- src/components/Menu/utils.ts
+++ src/components/Menu/utils.ts
@@ -17,12 +17,9 @@ export const getActiveSubMenuItem = ({ pathname, menuItem }: { pathname: string;
   }
 
   // Pathname includes multiple sub menu item hrefs - find the most specific match
-  const splitIntoParts = activeSubMenuItems.map((subMenuItem) => {
-    return { label: subMenuItem.label, href: subMenuItem.href.split('/') }
-  })
-
-  const mostSpecificMatch = splitIntoParts.sort(
+  const mostSpecificMatch = activeSubMenuItems.sort(
     (subMenuItem1, subMenuItem2) => subMenuItem2.href.length - subMenuItem1.href.length,
   )[0]
-  return { ...mostSpecificMatch, href: mostSpecificMatch.href.join('/') }
+
+  return mostSpecificMatch
 }
