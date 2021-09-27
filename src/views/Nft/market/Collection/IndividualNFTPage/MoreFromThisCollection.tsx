aabd26dcbf2b99f7e80b43cdac76a28fad3c1a65
Chef Cheems <81824236+Chef-Cheems@users.noreply.github.com>
2021-09-27 16:59:00 +0300

    Fix MoreFromThisCollection slider (#2357)
    * fix: Don't show duplicate cards in MoreFromThisCollection
    
    * fix: Update slider controls

--- src/views/Nft/market/Collection/IndividualNFTPage/MoreFromThisCollection.tsx
+++ src/views/Nft/market/Collection/IndividualNFTPage/MoreFromThisCollection.tsx
@@ -1,55 +1,97 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import SwiperCore, { Pagination } from 'swiper'
-import { Box, Text, useMatchBreakpoints } from '@pancakeswap/uikit'
+import SwiperCore from 'swiper'
+import { ArrowBackIcon, ArrowForwardIcon, Box, IconButton, Text, Flex, useMatchBreakpoints } from '@pancakeswap/uikit'
 import { useTranslation } from 'contexts/Localization'
 import { useNftsFromCollection } from 'state/nftMarket/hooks'
+import { isAddress } from 'utils'
 import { pancakeBunniesAddress } from '../../constants'
 import { CollectibleLinkCard } from '../../components/CollectibleCard'
 
 import 'swiper/swiper-bundle.css'
 
-SwiperCore.use([Pagination])
+const INITIAL_SLIDE = 4
+
+const SwiperCircle = styled.div<{ isActive }>`
+  background-color: ${({ theme, isActive }) => (isActive ? theme.colors.secondary : theme.colors.textDisabled)};
+  width: 12px;
+  height: 12px;
+  margin-right: 8px;
+  border-radius: 50%;
+  cursor: pointer;
+`
 
 const StyledSwiper = styled.div`
-  .swiper-wrapper {
-    align-items: center;
-    display: flex;
-  }
-
-  .swiper-pagination-bullet-active {
-    background-color: ${({ theme }) => theme.colors.secondary};
-  }
-
-  ${({ theme }) => theme.mediaQueries.lg} {
-    .swiper-container {
-      padding-bottom: 48px;
-    }
-
-    .swiper-slide {
-      max-height: 377px;
+  ${({ theme }) => theme.mediaQueries.md} {
+    .swiper-wrapper {
+      max-height: 390px;
     }
   }
 `
 
 interface MoreFromThisCollectionProps {
+  collectionAddress: string
   currentTokenName: string
 }
 
-const MoreFromThisCollection: React.FC<MoreFromThisCollectionProps> = ({ currentTokenName }) => {
+const MoreFromThisCollection: React.FC<MoreFromThisCollectionProps> = ({ collectionAddress, currentTokenName }) => {
   const { t } = useTranslation()
-  const { isMobile } = useMatchBreakpoints()
-  const nftList = useNftsFromCollection(pancakeBunniesAddress)
+  const [swiperRef, setSwiperRef] = useState<SwiperCore>(null)
+  const [activeIndex, setActiveIndex] = useState(1)
+  const { isMobile, isMd, isLg } = useMatchBreakpoints()
+  const nftList = useNftsFromCollection(collectionAddress)
 
   if (!nftList) {
     return null
   }
 
-  const nftsToShow = nftList.filter((nft) => nft.name !== currentTokenName).slice(0, 12)
+  let slidesPerView = 4
+  let maxPageIndex = 3
+
+  if (isLg) {
+    slidesPerView = 3
+    maxPageIndex = 4
+  }
+  if (isMd) {
+    slidesPerView = 2
+    maxPageIndex = 6
+  }
+
+  let nftsToShow = nftList.filter((nft) => nft.name !== currentTokenName && nft.marketData.isTradable)
+  if (isAddress(collectionAddress) === pancakeBunniesAddress) {
+    // PancakeBunnies should display 1 card per bunny id
+    nftsToShow = nftsToShow.reduce((nftArray, current) => {
+      const bunnyId = current.attributes[0].value
+      if (!nftArray.find((nft) => nft.attributes[0].value === bunnyId)) {
+        nftArray.push(current)
+      }
+      return nftArray
+    }, [])
+  }
+  nftsToShow = nftsToShow.slice(0, 12)
+
+  const nextSlide = () => {
+    if (activeIndex < maxPageIndex - 1) {
+      setActiveIndex(activeIndex + 1)
+      swiperRef.slideNext()
+    }
+  }
+
+  const previousSlide = () => {
+    if (activeIndex > 0) {
+      setActiveIndex(activeIndex - 1)
+      swiperRef.slidePrev()
+    }
+  }
+
+  const goToSlide = (index: number) => {
+    setActiveIndex(index / slidesPerView)
+    swiperRef.slideTo(index)
+  }
 
   return (
-    <Box pt="56px" pb="32px" mb="52px">
+    <Box pt="56px" mb="52px">
       <Text bold mb="24px">
         {t('More from this collection')}
       </Text>
@@ -66,13 +108,11 @@ const MoreFromThisCollection: React.FC<MoreFromThisCollectionProps> = ({ current
       ) : (
         <StyledSwiper>
           <Swiper
+            onSwiper={setSwiperRef}
             spaceBetween={16}
-            slidesPerView={4}
-            slidesPerGroup={4}
-            initialSlide={4}
-            pagination={{
-              clickable: true,
-            }}
+            slidesPerView={slidesPerView}
+            slidesPerGroup={slidesPerView}
+            initialSlide={INITIAL_SLIDE}
           >
             {nftsToShow.map((nft) => (
               <SwiperSlide key={nft.tokenId}>
@@ -80,6 +120,21 @@ const MoreFromThisCollection: React.FC<MoreFromThisCollectionProps> = ({ current
               </SwiperSlide>
             ))}
           </Swiper>
+          <Flex mt="16px" alignItems="center" justifyContent="center">
+            <IconButton variant="text" onClick={previousSlide}>
+              <ArrowBackIcon />
+            </IconButton>
+            {[...Array(maxPageIndex).keys()].map((index) => (
+              <SwiperCircle
+                key={index}
+                onClick={() => goToSlide(index * slidesPerView)}
+                isActive={activeIndex === index}
+              />
+            ))}
+            <IconButton variant="text" onClick={nextSlide}>
+              <ArrowForwardIcon />
+            </IconButton>
+          </Flex>
         </StyledSwiper>
       )}
     </Box>
