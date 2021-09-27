18b77518a48cfdbc5058b4a0d0e5d6610c833d0f
memoyil <2213635+memoyil@users.noreply.github.com>
2021-09-27 13:26:53 +0200

    fix: (info): Wrong tab bar in info

--- src/components/Menu/utils.ts
+++ src/components/Menu/utils.ts
@@ -1,7 +1,7 @@
 import { ConfigMenuItemsType } from './config/config'
 
 export const getActiveMenuItem = ({ pathname, menuConfig }: { pathname: string; menuConfig: ConfigMenuItemsType[] }) =>
-  menuConfig.find((menuItem) => pathname.includes(menuItem.href) || getActiveSubMenuItem({ menuItem, pathname }))
+  menuConfig.find((menuItem) => pathname.startsWith(menuItem.href) || getActiveSubMenuItem({ menuItem, pathname }))
 
 export const getActiveSubMenuItem = ({ pathname, menuItem }: { pathname: string; menuItem?: ConfigMenuItemsType }) =>
-  menuItem?.items.find((subMenuItem) => pathname.includes(subMenuItem.href))
+  menuItem?.items.find((subMenuItem) => pathname.startsWith(subMenuItem.href))
