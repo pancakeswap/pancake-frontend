6252bd188857495b8c45bbfb436b91c7c448d662
Chef Hutch <79279477+ChefHutch@users.noreply.github.com>
2021-09-27 14:38:34 +0100

    chore(menu-items): Improve getActivesubMenuItem specificity (#2355)
    * chore(menu-items): Improve getActivesubMenuItem specificity
    
    * fix(active-menu-item): Fix tests
    
    * chore(active-menu-items): Use startsWith
    
    * chore(active-menu-items): Simplify logic. Add test

--- src/__tests__/utils/utils.test.ts
+++ src/__tests__/utils/utils.test.ts
@@ -93,4 +93,15 @@ describe('getActiveSubMenuItem', () => {
     // Then
     expect(result).toEqual(undefined)
   })
+
+  it('should return the item with the longest href when multiple items are found', () => {
+    // Given
+    const pathname = '/nfts/collections/0xDf7952B35f24aCF7fC0487D01c8d5690a60DBa07'
+
+    // When
+    const result = getActiveSubMenuItem({ pathname, menuItem: menuConfig(mockT)[3] })
+
+    // Then
+    expect(result).toEqual(menuConfig(mockT)[3].items[1])
+  })
 })
