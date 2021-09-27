eaa0a3213f0b38a9bbb20ad62d7824b6336e20aa
Chef Hutch <79279477+ChefHutch@users.noreply.github.com>
2021-09-27 14:38:34 +0100

    chore(menu-items): Improve getActivesubMenuItem specificity (#2355)
    * chore(menu-items): Improve getActivesubMenuItem specificity
    
    * fix(active-menu-item): Fix tests
    
    * chore(active-menu-items): Use startsWith
    
    * chore(active-menu-items): Simplify logic. Add test

--- src/components/Menu/utils.ts
+++ src/components/Menu/utils.ts
@@ -7,7 +7,7 @@ export const getActiveSubMenuItem = ({ pathname, menuItem }: { pathname: string;
   const activeSubMenuItems = menuItem?.items.filter((subMenuItem) => pathname.startsWith(subMenuItem.href))
 
   // Pathname doesn't include any submenu item href - return undefined
-  if (!activeSubMenuItems || activeSubMenuItems.length === 0) {
+  if (activeSubMenuItems.length === 0) {
     return undefined
   }
 
