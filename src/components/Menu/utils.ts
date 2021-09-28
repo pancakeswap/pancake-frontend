79f987ed299e267cf54ed9bd6aa18689c1543779
memoyil <2213635+memoyil@users.noreply.github.com>
2021-09-28 02:52:11 +0200

    fix: (info): Wrong tab bar in info (#2354)
    * fix: (info): Wrong tab bar in info
    
    * fix: Apply #2355 fixes

--- src/components/Menu/utils.ts
+++ src/components/Menu/utils.ts
@@ -1,7 +1,25 @@
 import { ConfigMenuItemsType } from './config/config'
 
 export const getActiveMenuItem = ({ pathname, menuConfig }: { pathname: string; menuConfig: ConfigMenuItemsType[] }) =>
-  menuConfig.find((menuItem) => pathname.includes(menuItem.href) || getActiveSubMenuItem({ menuItem, pathname }))
+  menuConfig.find((menuItem) => pathname.startsWith(menuItem.href) || getActiveSubMenuItem({ menuItem, pathname }))
 
-export const getActiveSubMenuItem = ({ pathname, menuItem }: { pathname: string; menuItem?: ConfigMenuItemsType }) =>
-  menuItem?.items.find((subMenuItem) => pathname.includes(subMenuItem.href))
+export const getActiveSubMenuItem = ({ pathname, menuItem }: { pathname: string; menuItem?: ConfigMenuItemsType }) => {
+  const activeSubMenuItems = menuItem?.items.filter((subMenuItem) => pathname.startsWith(subMenuItem.href))
+
+  // Pathname doesn't include any submenu item href - return undefined
+  if (!activeSubMenuItems || activeSubMenuItems.length === 0) {
+    return undefined
+  }
+
+  // Pathname includes one sub menu item href - return it
+  if (activeSubMenuItems.length === 1) {
+    return activeSubMenuItems[0]
+  }
+
+  // Pathname includes multiple sub menu item hrefs - find the most specific match
+  const mostSpecificMatch = activeSubMenuItems.sort(
+    (subMenuItem1, subMenuItem2) => subMenuItem2.href.length - subMenuItem1.href.length,
+  )[0]
+
+  return mostSpecificMatch
+}
