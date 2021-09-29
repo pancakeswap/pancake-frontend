9eb8edcd456123e29545d662418db4a5a6fa12e8
ChefCorgi <corgi.farmer@gmail.com>
2021-09-29 10:24:03 +0200

    fix(nft market): Disable scroll top behavior on same page navigation

---
+++ src/components/HashRoute.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Route } from 'react-router'
+
+const HashRoute = ({ component: Component, hash, ...routeProps }) => (
+  <Route {...routeProps} component={({ location, ...props }) => location.hash === hash && <Component {...props} />} />
+)
+
+export default HashRoute
--- src/hooks/useScrollOnRouteChange.ts
+++ src/hooks/useScrollOnRouteChange.ts
@@ -3,14 +3,16 @@ import history from 'routerHistory'
 
 const useScrollOnRouteChange = () => {
   useEffect(() => {
-    const unlisten = history.listen(() => {
-      setTimeout(() => {
-        window.scroll({
-          top: 0,
-          left: 0,
-          behavior: 'smooth',
-        })
-      }, 50)
+    const unlisten = history.listen((args) => {
+      if (args.hash === '') {
+        setTimeout(() => {
+          window.scroll({
+            top: 0,
+            left: 0,
+            behavior: 'smooth',
+          })
+        }, 50)
+      }
     })
 
     return () => unlisten()
