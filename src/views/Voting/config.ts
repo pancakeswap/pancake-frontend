3cf534644c0a3bf2fa57e72d9da271857f9bae8b
Chef Chungus <cakechefchungus@protonmail.com>
2021-09-30 11:24:57 +0900

    chore: Add TARO to voting

--- src/views/Voting/config.ts
+++ src/views/Voting/config.ts
@@ -1,5 +1,12 @@
 export const PROPOSALS_TO_SHOW = 10
-export const ADMINS = ['0x977e0c1005dff8749f8cac22f4df0bd5f013d1a7', '0x6eaf1b33b8672c5dc40ab8f4ba3a0111723126c7']
+// TARO = 0x842B508681eE336E74600974B4623B709477d29D
+// CHUNGUS = 0x977e0c1005dff8749f8cac22f4df0bd5f013d1a7
+// HOPS = 0x6eaf1b33b8672c5dc40ab8f4ba3a0111723126c7
+export const ADMINS = [
+  '0x842B508681eE336E74600974B4623B709477d29D',
+  '0x977e0c1005dff8749f8cac22f4df0bd5f013d1a7',
+  '0x6eaf1b33b8672c5dc40ab8f4ba3a0111723126c7',
+]
 export const IPFS_GATEWAY = 'https://gateway.ipfs.io/ipfs'
 export const SNAPSHOT_VERSION = '0.1.3'
 export const PANCAKE_SPACE = 'cake.eth'
