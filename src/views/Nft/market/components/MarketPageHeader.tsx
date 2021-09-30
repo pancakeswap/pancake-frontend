c1fef5d3bffb7efb5ae7002964170063691d59d5
Chef Cheems <chefcheems@protonmail.com>
2021-09-30 09:04:10 +0300

    fix: Dark theme header on nft market

--- src/views/Nft/market/components/MarketPageHeader.tsx
+++ src/views/Nft/market/components/MarketPageHeader.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import PageHeader, { PageHeaderProps } from 'components/PageHeader'
+import useTheme from 'hooks/useTheme'
 
-const MarketPageHeader: React.FC<PageHeaderProps> = (props) => (
-  <PageHeader background="linear-gradient(111.68deg, #f2ecf2 0%, #e8f2f6 100%)" {...props} />
-)
+const MarketPageHeader: React.FC<PageHeaderProps> = (props) => {
+  const { theme } = useTheme()
+  const background = theme.isDark
+    ? 'linear-gradient(166.77deg, #3B4155 0%, #3A3045 100%)'
+    : 'linear-gradient(111.68deg, #f2ecf2 0%, #e8f2f6 100%)'
+  return <PageHeader background={background} {...props} />
+}
 
 export default MarketPageHeader
