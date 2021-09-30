1df39c5fb43c71308f8bff483380a5653eb06bdc
RabbitDoge <72658581+RabbitDoge@users.noreply.github.com>
2021-09-30 20:11:39 +0900

    chore: Fix alternative RPC url bug (#2415)

--- src/utils/getRpcUrl.ts
+++ src/utils/getRpcUrl.ts
@@ -1,15 +1,14 @@
 import sample from 'lodash/sample'
 
-// Array of available nodes to connect to
-export const nodes = [
-  process.env.REACT_APP_NODE_1,
-  process.env.REACT_APP_NODE_2,
-  process.env.REACT_APP_NODE_3,
-  // process.env.REACT_APP_NODE_4,
-]
+if (!process.env.REACT_APP_NODE_1 || !process.env.REACT_APP_NODE_2 || !process.env.REACT_APP_NODE_3) {
+  throw Error('One base RPC URL is undefined')
+}
 
-if (process.env.NODE_ENV === 'production') {
-  nodes.push(process.env.REACT_APP_NODE_4)
+// Array of available nodes to connect to
+export const nodes = [process.env.REACT_APP_NODE_1, process.env.REACT_APP_NODE_2, process.env.REACT_APP_NODE_3]
+
+if (process.env.NODE_ENV === 'production' && process.env.REACT_APP_NODE_ALT) {
+  nodes.push(process.env.REACT_APP_NODE_ALT)
 }
 
 const getNodeUrl = () => {
