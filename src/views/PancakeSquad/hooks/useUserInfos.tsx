29097bda5fc236828d61a3b8440c8bd5f5211638
ChefCorgi <corgi.farmer@gmail.com>
2021-09-30 18:15:14 +0200

    feat(pancake squad): Check account before calling contract

--- src/views/PancakeSquad/hooks/useUserInfos.tsx
+++ src/views/PancakeSquad/hooks/useUserInfos.tsx
@@ -11,36 +11,38 @@ const useUserInfos = ({ account, lastBlockNumber, setCallback }) => {
         const nftSaleAddress = getNftSaleAddress()
         const pancakeSquadContract = getPancakeSquadContract()
 
-        const calls = [
-          'canClaimForGen0',
-          'numberTicketsForGen0',
-          'numberTicketsUsedForGen0',
-          'viewNumberTicketsOfUser',
-          'ticketsOfUserBySize',
-        ].map((method) => ({
-          address: nftSaleAddress,
-          name: method,
-          params: method === 'ticketsOfUserBySize' ? [account, 0, 600] : [account],
-        }))
+        if (account) {
+          const calls = [
+            'canClaimForGen0',
+            'numberTicketsForGen0',
+            'numberTicketsUsedForGen0',
+            'viewNumberTicketsOfUser',
+            'ticketsOfUserBySize',
+          ].map((method) => ({
+            address: nftSaleAddress,
+            name: method,
+            params: method === 'ticketsOfUserBySize' ? [account, 0, 600] : [account],
+          }))
 
-        const [
-          currentCanClaimForGen0,
-          currentNumberTicketsForGen0,
-          currentNumberTicketsUsedForGen0,
-          currentNumberTicketsOfUser,
-          currentTicketsOfUser,
-        ] = await multicallv2(nftSaleAbi, calls)
+          const [
+            currentCanClaimForGen0,
+            currentNumberTicketsForGen0,
+            currentNumberTicketsUsedForGen0,
+            currentNumberTicketsOfUser,
+            currentTicketsOfUser,
+          ] = await multicallv2(nftSaleAbi, calls)
 
-        const currentNumberTokensOfUser = await pancakeSquadContract.balanceOf(account)
+          const currentNumberTokensOfUser = await pancakeSquadContract.balanceOf(account)
 
-        setCallback({
-          canClaimForGen0: currentCanClaimForGen0[0],
-          numberTicketsForGen0: currentNumberTicketsForGen0[0].toNumber(),
-          numberTicketsUsedForGen0: currentNumberTicketsUsedForGen0[0].toNumber(),
-          numberTicketsOfUser: currentNumberTicketsOfUser[0].toNumber(),
-          ticketsOfUser: currentTicketsOfUser[0][0],
-          numberTokensOfUser: currentNumberTokensOfUser.toNumber(),
-        })
+          setCallback({
+            canClaimForGen0: currentCanClaimForGen0[0],
+            numberTicketsForGen0: currentNumberTicketsForGen0[0].toNumber(),
+            numberTicketsUsedForGen0: currentNumberTicketsUsedForGen0[0].toNumber(),
+            numberTicketsOfUser: currentNumberTicketsOfUser[0].toNumber(),
+            ticketsOfUser: currentTicketsOfUser[0][0],
+            numberTokensOfUser: currentNumberTokensOfUser.toNumber(),
+          })
+        }
       } catch (e) {
         console.error(e)
       }
