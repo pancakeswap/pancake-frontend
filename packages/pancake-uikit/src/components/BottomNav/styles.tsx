8beb5ccd6581b4804f7f6bf1edae16d3278206cb
0xCorgi <88371449+0xCorgi@users.noreply.github.com>
2021-10-01 14:20:50 +0200

    feat(bottom nav): Fix ios bottom nav issue on specifics browsers (#293)

--- packages/pancake-uikit/src/components/BottomNav/styles.tsx
+++ packages/pancake-uikit/src/components/BottomNav/styles.tsx
@@ -1,14 +1,17 @@
 import styled from "styled-components";
 import { Flex } from "../Box";
 
-const StyledBottomNavItem = styled(Flex)`
+const StyledBottomNav = styled(Flex)`
   position: fixed;
   bottom: 0px;
   width: 100%;
   padding: 5px 8px;
-  padding-bottom: env(safe-area-inset-bottom);
   background: ${({ theme }) => theme.colors.backgroundAlt};
   border-top: 1px solid ${({ theme }) => theme.colors.cardBorder};
+  padding-bottom: env(safe-area-inset-bottom);
+  html[data-useragent*="TokenPocket_iOS"] & {
+    padding-bottom: 45px;
+  }
 `;
 
-export default StyledBottomNavItem;
+export default StyledBottomNav;
