9d5ad821ea5b9f3e07e285cafb20448b3678f020
CryptoWingnut <ninjawingnutcrypto@gmail.com>
2021-09-30 22:23:37 -0300

    feat: Tweaked the padding and size of the bog swap widget

--- src/components/Landing/TimeLine.tsx
+++ src/components/Landing/TimeLine.tsx
@@ -7,11 +7,11 @@ const TimeLine = () => {
   const isDesktop = isLg || isXl || isMd
 
   return (
-    <div id='Timeline' style={{width: "100%", paddingTop: isDesktop ? "0px" : "80px", height: isDesktop ? '1200px' : '1320px'}}>
+    <div id='Timeline' style={{width: "100%", paddingTop: isDesktop ? "30px" : "80px", height: isDesktop ? '1000px' : '1000px' }}>
       <Flex justifyContent="center">
         <iframe
           src='https://app.bogged.finance/swap?tokenIn=BNB&tokenOut=0x50ba8bf9e34f0f83f96a340387d1d3888ba4b3b5&embed=1'
-          height={isDesktop ? '873px' : '941px'}  title='BUY $ZMBE now using BogSwap'
+          height={isDesktop ? '941px' : '941px'}  title='BUY $ZMBE now using BogSwap'
           style={{ margin: 10, minWidth: isDesktop ? '30%' : '80%' }} />
       </Flex>
     </div>
