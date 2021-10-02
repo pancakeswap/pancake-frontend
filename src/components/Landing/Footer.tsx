a753934c818268408297f1b65581f790d29b86a8
CryptoWingnut <ninjawingnutcrypto@gmail.com>
2021-10-01 23:38:35 -0300

    feat: Tweaked landing page changes for mobile

--- src/components/Landing/Footer.tsx
+++ src/components/Landing/Footer.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Landing.Styles.css'
+import { useMatchBreakpoints } from '@rug-zombie-libs/uikit'
+
 import NomicsLogo from "../../images/Nomics_Logo.svg"
 import TelegramLogo from "../../images/TG_Logo.svg";
 import CMCLogo from "../../images/CMC_Logo.svg";
@@ -12,48 +14,59 @@ import TwitterLogo from "../../images/Twitter_Logo.svg";
 import YouTubeLogo from "../../images/Youtube_Logo.svg";
 
 const Footer = () => {
+  const { isLg, isXl, isMd } = useMatchBreakpoints()
+  const isDesktop = isLg || isXl || isMd  
+
   return (
     <div className="footer-wrapper">      
       <div className="copyrights_wrap-1">
         <div className="footer_section-2">
           <div className="footer_logo_wrap">
-            <a href="/" className="brand_link w-inline-block">
+            <a href="/" >
               <img src="https://storage.googleapis.com/rug-zombie/RugZombieBanner.png" width="200" sizes="200px" alt="" className="footer_logo" />
             </a>
           </div>
           <div className="social_links_wrapper navbar_sm">
-            <a href="mailto:info@rugzombie.io" rel="noreferrer" target="_blank" className="social_link w-inline-block">
-              <img src={EmailLogo} width="27" sizes="30px" alt="" className="image-3" />
-            </a>
-            <a href="https://coinmarketcap.com/currencies/rugzombie/" rel="noreferrer" target="_blank" className="social_link w-inline-block">
-              <img src={CMCLogo} width="27" sizes="30px" alt="" className="image-3" />
-            </a>
-            <a href="https://www.coingecko.com/en/coins/rugzombie" rel="noreferrer" target="_blank" className="social_link w-inline-block">
-              <img src={CGLogo} width="27" sizes="30px" alt="" className="image-3" />
-            </a>
-            <a href="https://rugzombie.medium.com/" rel="noreferrer" target="_blank" className="social_link w-inline-block">
-              <img src={MediumLogo} width="27" sizes="30px" alt="" className="image-3" />
-            </a>
-            <br/>
-            <a href="https://www.publish0x.com/@RugZombie-Undertaker" rel="noreferrer" target="_blank" className="social_link w-inline-block">
-              <img src={Publish0xLogo} width="27" sizes="30px" alt="" className="image-3" />
-            </a>
-            <a href="https://www.reddit.com/user/rugzombie" rel="noreferrer" target="_blank" className="social_link w-inline-block">
-              <img src={RedditLogo} width="27" sizes="30px" alt="" className="image-3" />
-            </a>
-            <a href="https://t.me/rugzombie" rel="noreferrer" target="_blank" className="social_link w-inline-block">
-              <img src={TelegramLogo} width="27" sizes="30px" alt="" className="image-3" />
-            </a>
-            <a href="https://twitter.com/rugzombie" rel="noreferrer" target="_blank" className="social_link w-inline-block">
-              <img src={TwitterLogo} width="27" sizes="30px" alt="" className="image-3" />
-            </a>
-            <a href="https://www.youtube.com/channel/UCJv0OdCDThvuJ_0lruZrm_w" rel="noreferrer" target="_blank" className="social_link w-inline-block">
-              <img src={YouTubeLogo} width="27" sizes="30px" alt="" className="image-3" />
-            </a>
-            <a href="https://nomics.com/assets/zmbe-zombie-token" rel="noreferrer" target="_blank" className="social_link w-inline-block">
-              <img src={NomicsLogo} width="45" sizes="30px" alt="" className="image-3" />
-            </a>
-          </div>
+            <div className="social_links_wrapper navbar_sm">
+              <a href="mailto:info@rugzombie.io" rel="noreferrer" target="_blank" className="social_link w-inline-block">
+                <img src={EmailLogo} width="27" sizes="30px" alt="" className="image-3" />
+              </a>
+              <a href="https://coinmarketcap.com/currencies/rugzombie/" rel="noreferrer" target="_blank" className="social_link w-inline-block">
+                <img src={CMCLogo} width="27" sizes="30px" alt="" className="image-3" />
+              </a>
+              <a href="https://www.coingecko.com/en/coins/rugzombie" rel="noreferrer" target="_blank" className="social_link w-inline-block">
+                <img src={CGLogo} width="27" sizes="30px" alt="" className="image-3" />
+              </a>
+              <a href="https://rugzombie.medium.com/" rel="noreferrer" target="_blank" className="social_link w-inline-block">
+                <img src={MediumLogo} width="27" sizes="30px" alt="" className="image-3" />
+              </a>
+            </div>
+            <div className="social_links_wrapper navbar_sm">  
+              <a href="https://www.publish0x.com/@RugZombie-Undertaker" rel="noreferrer" target="_blank" className="social_link w-inline-block">
+                <img src={Publish0xLogo} width="27" sizes="30px" alt="" className="image-3" />
+              </a>
+              <a href="https://www.reddit.com/user/rugzombie" rel="noreferrer" target="_blank" className="social_link w-inline-block">
+                <img src={RedditLogo} width="27" sizes="30px" alt="" className="image-3" />
+              </a>
+              <a href="https://t.me/rugzombie" rel="noreferrer" target="_blank" className="social_link w-inline-block">
+                <img src={TelegramLogo} width="27" sizes="30px" alt="" className="image-3" />
+              </a>
+              <a href="https://twitter.com/rugzombie" rel="noreferrer" target="_blank" className="social_link w-inline-block">
+                <img src={TwitterLogo} width="27" sizes="30px" alt="" className="image-3" />
+              </a>
+            </div>
+            <div className="social_links_wrapper navbar_sm">
+              {!isDesktop && <a href="/" rel="noreferrer" target="_blank" className="social_link w-inline-block">
+                <img src="" width="27" sizes="30px" alt="" className="image-3" />
+              </a>}
+              <a href="https://www.youtube.com/channel/UCJv0OdCDThvuJ_0lruZrm_w" rel="noreferrer" target="_blank" className="social_link w-inline-block">
+                <img src={YouTubeLogo} width="27" sizes="30px" alt="" className="image-3" />
+              </a>
+              <a href="https://nomics.com/assets/zmbe-zombie-token" rel="noreferrer" target="_blank" className="social_link w-inline-block">
+                <img src={NomicsLogo} width="33" sizes="30px" alt="" className="image-3" />
+              </a>
+            </div>
+          </div>                
         </div>
       </div>
     </div>
--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -4,7 +4,7 @@ import Header from './Header';
 import TimeLine from './TimeLine';
 import Tokenomics from './Tokenomics';
 import Footer from './Footer';
-import { APESWAP_EXCHANGE_URL, BASE_EXCHANGE_URL } from '../../config'
+import { APESWAP_EXCHANGE_URL } from '../../config'
 import tokens from '../../config/constants/tokens'
 import { getAddress } from '../../utils/addressHelpers'
 import { routes } from "../../routes"
@@ -24,13 +24,11 @@ const Landing: React.FC<LandingProps> = (props: LandingProps) =>{
   return (
     <>
     <Header />
-    <br /><br /><br /><br /><br />
+    <br /><br /><br /><br />
     <div className="section hero-section">
       <div className="container w-container">
         <img  src='/images/rugZombie/loader.gif' width="350" alt="" className="image" />
           <h1 className="heading">Bringing your rugged tokens<span className="text-span"> back from the dead</span></h1>
-          {/* <p className="max_width_400">Rug zombie is a frictionless, yield-generating contract that allows you to seek shelter amidst the chaos of the market.<br />
-          </p> */}
       </div>
       <div className="_40_percent_block">
         <div tabIndex={0} role="button" onClick={handleClick} onKeyDown ={handleClick} className="button-2 w-button">
@@ -52,17 +50,17 @@ const Landing: React.FC<LandingProps> = (props: LandingProps) =>{
           Spawning Pool Application
         </a>
       </div>
-      <div className="_40_percent_block-copy">
+      <div className="_40_percent_block">
         <a href="https://dex.guru/token/0x50ba8bf9e34f0f83f96a340387d1d3888ba4b3b5-bsc" rel="noreferrer" target="_blank" className="button-2 w-button">
           View Chart
         </a>
       </div>
-      <div className="_40_percent_block-copy">
+      <div className="_40_percent_block">
         <a href="https://app.gitbook.com/@rugzombie/s/docs/tokenomics" rel="noreferrer" target="_blank" className="button-2 w-button">
           View token info
         </a>
       </div>
-      <div className="_40_percent_block-copy">
+      <div className="_40_percent_block">
         <a href="https://t.me/rugzombie" rel="noreferrer" target="_blank" className="button-2 w-button">
           Join our telegram
         </a>
