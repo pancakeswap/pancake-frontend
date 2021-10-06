2fc686887e5b4c6b45621e255f4a257473bcdd75
hachiojidev <hachiojidev@protonmail.com>
2021-10-06 15:39:17 +0900

    feat(market): NFT Filter state (wip)

--- src/views/Nft/market/Collection/Items/Filters.tsx
+++ src/views/Nft/market/Collection/Items/Filters.tsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { Flex } from '@pancakeswap/uikit'
 import capitalize from 'lodash/capitalize'
+import { useAppDispatch } from 'state'
 import { Collection, NftAttribute } from 'state/nftMarket/types'
 import { getNftsFromCollectionApi } from 'state/nftMarket/helpers'
-import { ListFilter } from 'components/Filters'
+import { Item, ListFilter } from 'components/Filters'
+import { useGetNftFilters } from 'state/nftMarket/hooks'
+import { addAttributeFilter, removeAttributeFilter } from 'state/nftMarket/reducer'
 
 interface FiltersProps {
   collection: Collection
@@ -11,6 +14,10 @@ interface FiltersProps {
 
 const Filters: React.FC<FiltersProps> = ({ collection }) => {
   const [attributeDistribution, setAttributeDistribution] = useState({})
+  const { address } = collection
+  const dispatch = useAppDispatch()
+
+  const nftFilters = useGetNftFilters(address)
   const attrsByType: Record<string, NftAttribute[]> = collection?.attributes?.reduce(
     (accum, attr) => ({
       ...accum,
@@ -19,10 +26,9 @@ const Filters: React.FC<FiltersProps> = ({ collection }) => {
     {},
   )
   const uniqueTraitTypes = attrsByType ? Object.keys(attrsByType) : []
-  const { address } = collection
 
-  const handleApplyFilter = (items) => {
-    console.log(items)
+  const handleApply = ({ attr }: Item) => {
+    dispatch(addAttributeFilter({ collectionAddress: address, attr }))
   }
 
   useEffect(() => {
@@ -40,12 +46,38 @@ const Filters: React.FC<FiltersProps> = ({ collection }) => {
     <Flex alignItems="center" flexWrap="wrap">
       {uniqueTraitTypes.map((traitType) => {
         const attrs = attrsByType[traitType]
-        const items = attrs.map((attr) => ({
+        const items: Item[] = attrs.map((attr) => ({
           label: capitalize(attr.value as string),
           count: attributeDistribution[traitType] ? attributeDistribution[traitType][attr.value] : undefined,
+          attr,
         }))
 
-        return <ListFilter key={traitType} title={capitalize(traitType)} items={items} onApply={handleApplyFilter} />
+        // If the attribute has already been selected get it from the current filter list and create an item
+        const selectedAttr = nftFilters.attributes[traitType]
+        const selectedItem = selectedAttr
+          ? {
+              label: capitalize(selectedAttr.value as string),
+              count: attributeDistribution[traitType]
+                ? attributeDistribution[traitType][selectedAttr.value]
+                : undefined,
+              attr: selectedAttr,
+            }
+          : undefined
+
+        const handleClear = () => {
+          dispatch(removeAttributeFilter({ collectionAddress: address, traitType }))
+        }
+
+        return (
+          <ListFilter
+            key={traitType}
+            title={capitalize(traitType)}
+            selectedItem={selectedItem}
+            items={items}
+            onApply={handleApply}
+            onClear={handleClear}
+          />
+        )
       })}
     </Flex>
   )
