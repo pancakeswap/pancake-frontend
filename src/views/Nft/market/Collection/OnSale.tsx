f45a1a8267de5a6645676e0aee4316c9da0f9a77
Chef Hutch <79279477+ChefHutch@users.noreply.github.com>
2021-10-07 14:31:12 +0100

    feat(nft-market): Infinite loading for squad on sale & recent listing filter (#2485)
    * feat(nft-market): Infinite scroll for squad
    
    * feat(nft-market): Add 'recently listed' to on sale nfts
    
    * feat(nft-market): Modify loading button behaviour

--- src/views/Nft/market/Collection/OnSale.tsx
+++ src/views/Nft/market/Collection/OnSale.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router'
-import { Box, Flex, Grid, Text } from '@pancakeswap/uikit'
+import { Box, Flex, Grid, Text, Button, AutoRenewIcon } from '@pancakeswap/uikit'
+import { uniqBy } from 'lodash'
 import { NftToken, TokenMarketData } from 'state/nftMarket/types'
 import { useGetCollection } from 'state/nftMarket/hooks'
 import { getNftApi, getNftsMarketData } from 'state/nftMarket/helpers'
@@ -11,16 +12,27 @@ import Header from './Header'
 import GridPlaceholder from '../components/GridPlaceholder'
 import { CollectibleLinkCard } from '../components/CollectibleCard'
 
+const REQUEST_SIZE = 100
+
+interface QueryParams {
+  orderDirection: 'asc' | 'desc'
+  orderBy: 'currentAskPrice' | 'updatedAt'
+}
+
 const OnSale = () => {
   const { collectionAddress } = useParams<{ collectionAddress: string }>()
   const [nfts, setNfts] = useState<NftToken[]>([])
-  const [sortBy, setSortBy] = useState<'asc' | 'desc'>('asc')
+  const [queryParams, setQueryParams] = useState<QueryParams>({ orderDirection: 'asc', orderBy: 'currentAskPrice' })
+  const [isFetching, setIsFetching] = useState(false)
+  const [skip, setSkip] = useState(0)
   const { t } = useTranslation()
   const collection = useGetCollection(collectionAddress)
+  const { numberTokensListed } = collection
 
   const sortByItems = [
-    { label: t('Lowest price'), value: 'asc' },
-    { label: t('Highest price'), value: 'desc' },
+    { label: t('Lowest price'), value: { orderDirection: 'asc', orderBy: 'currentAskPrice' } },
+    { label: t('Highest price'), value: { orderDirection: 'desc', orderBy: 'currentAskPrice' } },
+    { label: t('Recently listed'), value: { orderDirection: 'desc', orderBy: 'updatedAt' } },
   ]
 
   useEffect(() => {
@@ -35,62 +47,89 @@ const OnSale = () => {
           marketData: marketData[i],
         }
       })
-      setNfts(responsesWithMarketData)
+      setIsFetching(false)
+      setNfts((prevState) => {
+        const combinedNfts = [...prevState, ...responsesWithMarketData]
+        return uniqBy(combinedNfts, 'tokenId')
+      })
     }
 
     const fetchMarketData = async () => {
       const subgraphRes = await getNftsMarketData(
         { collection: collectionAddress.toLowerCase(), isTradable: true },
-        100,
-        'currentAskPrice',
-        sortBy,
+        REQUEST_SIZE,
+        queryParams.orderBy,
+        queryParams.orderDirection,
+        skip,
       )
       fetchApiData(subgraphRes)
     }
 
+    setIsFetching(true)
     fetchMarketData()
-  }, [collectionAddress, sortBy])
+  }, [collectionAddress, queryParams, skip])
 
   const handleChange = (event: OptionProps) => {
     setNfts([])
-    setSortBy(event.value)
+    const { value } = event
+    setQueryParams({ orderDirection: value.orderDirection, orderBy: value.orderBy })
+  }
+
+  const handleLoadMore = () => {
+    setSkip(skip + REQUEST_SIZE)
   }
 
   return (
     <>
       <Header collection={collection} />
       <Page>
-        <Flex alignItems="center" justifyContent={['flex-start', null, null, 'flex-end']} mb="24px">
-          <Box minWidth="165px">
-            <Text fontSize="12px" textTransform="uppercase" color="textSubtle" fontWeight={600} mb="4px">
-              {t('Sort By')}
-            </Text>
-            <Select options={sortByItems} onOptionChange={handleChange} />
-          </Box>
-        </Flex>
-        {!nfts.length ? (
-          <GridPlaceholder />
-        ) : (
-          <Grid
-            gridGap="16px"
-            gridTemplateColumns={['1fr', null, 'repeat(3, 1fr)', null, 'repeat(4, 1fr)']}
-            alignItems="start"
-          >
-            {nfts.map((nft) => {
-              const currentAskPriceAsNumber = nft.marketData?.currentAskPrice
-                ? parseFloat(nft.marketData.currentAskPrice)
-                : 0
+        <Box position="relative">
+          <Flex alignItems="center" justifyContent={['flex-start', null, null, 'flex-end']} mb="24px">
+            <Box minWidth="165px">
+              <Text fontSize="12px" textTransform="uppercase" color="textSubtle" fontWeight={600} mb="4px">
+                {t('Sort By')}
+              </Text>
+              <Select options={sortByItems} onOptionChange={handleChange} />
+            </Box>
+          </Flex>
+          {!nfts.length ? (
+            <GridPlaceholder />
+          ) : (
+            <>
+              <Grid
+                gridGap="16px"
+                gridTemplateColumns={['1fr', null, 'repeat(3, 1fr)', null, 'repeat(4, 1fr)']}
+                alignItems="start"
+              >
+                {nfts.map((nft) => {
+                  const currentAskPriceAsNumber = nft.marketData?.currentAskPrice
+                    ? parseFloat(nft.marketData.currentAskPrice)
+                    : 0
 
-              return (
-                <CollectibleLinkCard
-                  key={`${nft.tokenId}-${nft.collectionName}`}
-                  nft={nft}
-                  currentAskPrice={currentAskPriceAsNumber}
-                />
-              )
-            })}
-          </Grid>
-        )}
+                  return (
+                    <CollectibleLinkCard
+                      key={`${nft.tokenId}-${nft.collectionName}`}
+                      nft={nft}
+                      currentAskPrice={currentAskPriceAsNumber}
+                    />
+                  )
+                })}
+              </Grid>
+            </>
+          )}
+
+          <Flex mt="60px" mb="12px" justifyContent="center">
+            {Number(numberTokensListed) > nfts?.length && (
+              <Button
+                onClick={handleLoadMore}
+                scale="sm"
+                endIcon={isFetching ? <AutoRenewIcon spin color="currentColor" /> : undefined}
+              >
+                {isFetching ? t('Loading') : t('Load more')}
+              </Button>
+            )}
+          </Flex>
+        </Box>
       </Page>
     </>
   )
