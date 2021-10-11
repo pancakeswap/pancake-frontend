6c61755ec7545c713ca6c872f00f915e9bef3017
hachiojidev <hachiojidev@protonmail.com>
2021-10-11 14:20:33 +0900

    feat(market): Filter when click

--- src/views/Nft/market/Collection/Items/Filters.tsx
+++ src/views/Nft/market/Collection/Items/Filters.tsx
@@ -3,10 +3,8 @@ import styled from 'styled-components'
 import { Flex } from '@pancakeswap/uikit'
 import capitalize from 'lodash/capitalize'
 import isEmpty from 'lodash/isEmpty'
-import { useAppDispatch } from 'state'
 import { useGetNftFilters } from 'state/nftMarket/hooks'
 import { Collection, NftAttribute } from 'state/nftMarket/types'
-import { filterNftsFromCollection, removeAttributeFilter } from 'state/nftMarket/reducer'
 import { Item, ListFilter } from 'views/Nft/market/components/Filters'
 import useGetCollectionDistribution from '../../hooks/useGetCollectionDistribution'
 import ClearAllButton from './ClearAllButton'
@@ -32,7 +30,6 @@ const ScrollableFlexContainer = styled(Flex)`
 const Filters: React.FC<FiltersProps> = ({ collection }) => {
   const { address } = collection
   const { data } = useGetCollectionDistribution(address)
-  const dispatch = useAppDispatch()
 
   const nftFilters = useGetNftFilters()
   const attrsByType: Record<string, NftAttribute[]> = collection?.attributes?.reduce(
@@ -44,15 +41,6 @@ const Filters: React.FC<FiltersProps> = ({ collection }) => {
   )
   const uniqueTraitTypes = attrsByType ? Object.keys(attrsByType) : []
 
-  const handleApply = ({ attr }: Item) => {
-    dispatch(
-      filterNftsFromCollection({
-        collectionAddress: address,
-        nftFilters: { ...nftFilters, [attr.traitType]: attr },
-      }),
-    )
-  }
-
   return (
     <ScrollableFlexContainer>
       {uniqueTraitTypes.map((traitType) => {
@@ -63,29 +51,13 @@ const Filters: React.FC<FiltersProps> = ({ collection }) => {
           attr,
         }))
 
-        // If the attribute has already been selected get it from the current filter list and create an item
-        const selectedAttr = nftFilters ? nftFilters[traitType] : null
-        const selectedItem = selectedAttr
-          ? {
-              label: capitalize(selectedAttr.value as string),
-              count: data && data[traitType] ? data[traitType][selectedAttr.value] : undefined,
-              attr: selectedAttr,
-            }
-          : undefined
-
-        const handleClear = () => {
-          dispatch(removeAttributeFilter(traitType))
-        }
-
         return (
           <ListFilter
             key={traitType}
             title={capitalize(traitType)}
-            selectedItem={selectedItem}
+            traitType={traitType}
             items={items}
             collectionAddress={address}
-            onApply={handleApply}
-            onClear={handleClear}
           />
         )
       })}
