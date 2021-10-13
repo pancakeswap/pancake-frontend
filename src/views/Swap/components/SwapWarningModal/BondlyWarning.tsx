0de57cb2d7016bc8dfe9ab24c177d6aa37cee1e1
memoyil <2213635+memoyil@users.noreply.github.com>
2021-10-13 16:28:58 +0200

    fix: (translations): Fix typos in translations json

--- src/views/Swap/components/SwapWarningModal/BondlyWarning.tsx
+++ src/views/Swap/components/SwapWarningModal/BondlyWarning.tsx
@@ -5,7 +5,7 @@ import { useTranslation } from 'contexts/Localization'
 const BondlyWarning = () => {
   const { t } = useTranslation()
 
-  return <Text>{t('Warning: BONDLY has been compromised. Please remove liqudity until further notice.')}</Text>
+  return <Text>{t('Warning: BONDLY has been compromised. Please remove liquidity until further notice.')}</Text>
 }
 
 export default BondlyWarning
