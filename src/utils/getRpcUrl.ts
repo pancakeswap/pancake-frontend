001b06f7f10c98ebe79bde91103789b0a2878a23
Chef Cheems <chefcheems@protonmail.com>
2021-10-13 09:43:23 +0100

    feat: Fully switch to custom node

--- src/utils/getRpcUrl.ts
+++ src/utils/getRpcUrl.ts
@@ -1,17 +1,21 @@
 import sample from 'lodash/sample'
 
-if (!process.env.REACT_APP_NODE_1 || !process.env.REACT_APP_NODE_2 || !process.env.REACT_APP_NODE_3) {
+if (
+  process.env.NODE_ENV !== 'production' &&
+  (!process.env.REACT_APP_NODE_1 || !process.env.REACT_APP_NODE_2 || !process.env.REACT_APP_NODE_3)
+) {
   throw Error('One base RPC URL is undefined')
 }
 
 // Array of available nodes to connect to
 export const nodes = [process.env.REACT_APP_NODE_1, process.env.REACT_APP_NODE_2, process.env.REACT_APP_NODE_3]
 
-if (process.env.NODE_ENV === 'production' && process.env.REACT_APP_NODE_ALT) {
-  nodes.push(process.env.REACT_APP_NODE_ALT)
-}
-
 const getNodeUrl = () => {
+  // Use custom node if available (both for development and production)
+  // However on the testnet it wouldn't work, so if on testnet - comment out the REACT_APP_NODE_PRODUCTION from env file
+  if (process.env.REACT_APP_NODE_PRODUCTION) {
+    return process.env.REACT_APP_NODE_PRODUCTION
+  }
   return sample(nodes)
 }
 
