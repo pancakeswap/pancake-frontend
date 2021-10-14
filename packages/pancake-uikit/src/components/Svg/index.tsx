4d4ecd303c603a6388f10ea9419d27561b26901c
0xCorgi <88371449+0xCorgi@users.noreply.github.com>
2021-10-14 13:02:53 +0200

    feat(icons): Add expand and shrink icons (#299)

--- packages/pancake-uikit/src/components/Svg/index.tsx
+++ packages/pancake-uikit/src/components/Svg/index.tsx
@@ -140,6 +140,8 @@ export { default as TicketIcon } from "./Icons/Ticket";
 export { default as EarnFillIcon } from "./Icons/EarnFill";
 export { default as TrophyIcon } from "./Icons/Trophy";
 export { default as TrophyFillIcon } from "./Icons/TrophyFill";
+export { default as ExpandIcon } from "./Icons/Expand";
+export { default as ShrinkIcon } from "./Icons/Shrink";
 
 export { default as Svg } from "./Svg";
 export type { SvgProps } from "./types";
