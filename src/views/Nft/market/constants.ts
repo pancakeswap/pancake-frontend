7d31098424632b6caa576c538531fdceee8ba1ae
RabbitDoge <72658581+RabbitDoge@users.noreply.github.com>
2021-10-14 12:49:14 +0900

    chore: Remove unused code

--- src/views/Nft/market/constants.ts
+++ src/views/Nft/market/constants.ts
@@ -3,11 +3,5 @@ import { CollectionKey } from 'config/constants/nftsCollections/types'
 import { getAddress } from 'utils/addressHelpers'
 
 export const nftsBaseUrl = '/nfts'
-
-// @TODO This will be removed when more collections are added
 export const pancakeBunniesAddress = getAddress(collections[CollectionKey.PANCAKE].address)
-
 export const pancakeSquadAddress = getAddress(collections[CollectionKey.SQUAD].address)
-
-// @TODO Remove after the V1
-export const TMP_SEE_ALL_LINK = `${nftsBaseUrl}/collections/${pancakeBunniesAddress}`
