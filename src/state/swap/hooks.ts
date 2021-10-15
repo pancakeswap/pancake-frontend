d4071eff723b7361745ce90f4ed5b5f25bdc9ff3
lion cho <chowin21@gmail.com>
2021-10-15 18:24:40 +0900

    fix(router): Fix add new token address

--- src/state/swap/hooks.ts
+++ src/state/swap/hooks.ts
@@ -88,8 +88,8 @@ export function tryParseAmount(value?: string, currency?: Currency): CurrencyAmo
 
 const BAD_RECIPIENT_ADDRESSES: string[] = [
   '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f', // v2 factory
-  '0xf164fC0Ec4E93095b804a4795bBe1e041497b92a', // v2 router 01
-  '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D', // v2 router 02
+  '', // v2 router 01
+  '0x10ED43C718714eb63d5aA57B78B54704E256024E', // v2 router 02
 ]
 
 /**
