8fa7e936488cd82adba0371f1880caa4e4a087f4
ChefCorgi <corgi.farmer@gmail.com>
2021-10-18 14:09:10 +0200

    feat(charts): Update responsive

--- src/components/App/AppBody.tsx
+++ src/components/App/AppBody.tsx
@@ -2,29 +2,15 @@ import React from 'react'
 import styled from 'styled-components'
 import { Card } from '@pancakeswap/uikit'
 
-export const BodyWrapper = styled(Card)<{ $fitContent?: boolean; $maxWidth?: string }>`
+export const BodyWrapper = styled(Card)`
   border-radius: 24px;
-  max-width: ${({ $maxWidth }) => $maxWidth || '436px'};
   width: 100%;
   z-index: 1;
-  ${({ $fitContent }) => ($fitContent ? 'height: fit-content;' : '')}
 `
 
 /**
  * The styled container element that wraps the content of most pages and the tabs.
  */
-export default function AppBody({
-  children,
-  fitContent = false,
-  maxWidth,
-}: {
-  children: React.ReactNode
-  fitContent?: boolean
-  maxWidth?: string
-}) {
-  return (
-    <BodyWrapper $fitContent={fitContent} $maxWidth={maxWidth}>
-      {children}
-    </BodyWrapper>
-  )
+export default function AppBody({ children }: { children: React.ReactNode }) {
+  return <BodyWrapper>{children}</BodyWrapper>
 }
