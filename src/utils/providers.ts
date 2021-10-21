748f6a00f6021bc130c5dfca48015b97b813a17f
memoyil <2213635+memoyil@users.noreply.github.com>
2021-10-21 17:49:22 +0200

    fix: (provider): Requesting Chain Id everytime before making request to node

--- src/utils/providers.ts
+++ src/utils/providers.ts
@@ -3,6 +3,6 @@ import getRpcUrl from 'utils/getRpcUrl'
 
 const RPC_URL = getRpcUrl()
 
-export const simpleRpcProvider = new ethers.providers.JsonRpcProvider(RPC_URL)
+export const simpleRpcProvider = new ethers.providers.StaticJsonRpcProvider(RPC_URL)
 
 export default null
