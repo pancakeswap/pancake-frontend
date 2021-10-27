cbf8582d302ac07ad4e718f43c27329395ccdc65
Chef Cheems <chefcheems@protonmail.com>
2021-10-27 12:03:22 +0100

    chore: Remove soon tag from ifo

--- src/components/Menu/config/config.ts
+++ src/components/Menu/config/config.ts
@@ -79,10 +79,6 @@ const config: (t: ContextApi['t']) => ConfigMenuItemsType[] = (t) => [
       {
         label: t('IFO'),
         href: '/ifo',
-        status: {
-          text: t('Soon'),
-          color: 'warning',
-        },
       },
       {
         label: t('Voting'),
