473832f371ed9e5200dd4e42735079c768a1461e
Chef JoJo <94336009+chef-jojo@users.noreply.github.com>
2021-11-16 12:26:50 +0800

    feat: Porto IFO banner

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Flex } from '@pancakeswap/uikit'
 import PageSection from 'components/PageSection'
 import { useWeb3React } from '@web3-react/core'
 import useTheme from 'hooks/useTheme'
@@ -15,6 +16,7 @@ import Footer from './components/Footer'
 import CakeDataRow from './components/CakeDataRow'
 import { WedgeTopLeft, InnerWedgeWrapper, OuterWedgeWrapper, WedgeTopRight } from './components/WedgeSvgs'
 import UserBanner from './components/UserBanner'
+import IFOBanner from './components/Banners/IFOBanner'
 
 const StyledHeroSection = styled(PageSection)`
   padding-top: 16px;
@@ -64,6 +66,13 @@ const Home: React.FC = () => {
             <UserBanner />
           </UserBannerWrapper>
         )}
+        <Flex
+          pt={[account ? '220px' : '0', null, null, account ? '76px' : '0']}
+          mt={[account ? '0' : '-16px', null, null, account ? '0' : '-48px']}
+          pb="24px"
+        >
+          <IFOBanner />
+        </Flex>
         <Hero />
       </StyledHeroSection>
       <PageSection
