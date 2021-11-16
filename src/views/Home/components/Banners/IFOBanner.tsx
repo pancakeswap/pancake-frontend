05dbec96dbddc0033700246c5f8f3a2033149fab
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2021-11-16 13:13:30 +0800

    feat: Porto IFO banner (#2676)
    * feat: Porto IFO banner
    
    * chore: Add translation

---
+++ src/views/Home/components/Banners/IFOBanner.tsx
@@ -0,0 +1,116 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Text, Flex, Button, ArrowForwardIcon, Heading } from '@pancakeswap/uikit'
+import { Link } from 'react-router-dom'
+import { useTranslation } from 'contexts/Localization'
+
+const StyledSubheading = styled(Heading)`
+  background: -webkit-linear-gradient(#ffd800, #eb8c00);
+  font-size: 20px;
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  -webkit-text-stroke: 1px rgba(0, 0, 0, 0.5);
+  ${({ theme }) => theme.mediaQueries.xs} {
+    font-size: 24px;
+  }
+  ${({ theme }) => theme.mediaQueries.sm} {
+    -webkit-text-stroke: unset;
+  }
+  margin-bottom: 8px;
+`
+
+const StyledHeading = styled(Heading)`
+  color: #ffffff;
+  background: -webkit-linear-gradient(#7645d9 0%, #452a7a 100%);
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-stroke: 6px transparent;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  margin-bottom: 16px;
+`
+
+const Wrapper = styled.div`
+  border-radius: 32px;
+  width: 100%;
+  background-image: linear-gradient(#7645d9, #452a7a);
+  max-height: max-content;
+  overflow: hidden;
+`
+
+const Inner = styled(Flex)`
+  position: relative;
+  padding: 24px;
+  flex-direction: row;
+  justify-content: space-between;
+  max-height: 220px;
+`
+
+const LeftWrapper = styled(Flex)`
+  z-index: 1;
+  width: 100%;
+  flex-direction: column;
+  justify-content: center;
+
+  ${({ theme }) => theme.mediaQueries.md} {
+    padding-top: 40px;
+    padding-bottom: 40px;
+  }
+`
+
+const RightWrapper = styled.div`
+  position: absolute;
+  right: -17px;
+  opacity: 0.9;
+  transform: translate(0, -50%);
+  top: 50%;
+
+  & img {
+    height: 100%;
+    width: 174px;
+  }
+
+  ${({ theme }) => theme.mediaQueries.sm} {
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    right: 24px;
+    top: 0;
+    transform: unset;
+    opacity: 1;
+
+    & img {
+      height: 130%;
+      width: unset;
+    }
+  }
+`
+
+const IFOBanner = () => {
+  const { t } = useTranslation()
+
+  return (
+    <Wrapper>
+      <Inner>
+        <LeftWrapper>
+          <StyledSubheading>{t('Soon')}</StyledSubheading>
+          <StyledHeading scale="xl">PORTO IFO</StyledHeading>
+          <Link to="/ifo">
+            <Button>
+              <Text color="invertedContrast" bold fontSize="16px" mr="4px">
+                {t('Go to IFO')}
+              </Text>
+              <ArrowForwardIcon color="invertedContrast" />
+            </Button>
+          </Link>
+        </LeftWrapper>
+        <RightWrapper>
+          <img src="/images/decorations/3d-ifo-porto.png" alt="IFO PORTO" />
+        </RightWrapper>
+      </Inner>
+    </Wrapper>
+  )
+}
+
+export default IFOBanner
