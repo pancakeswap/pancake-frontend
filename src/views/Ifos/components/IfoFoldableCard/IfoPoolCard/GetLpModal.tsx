0dd652dc0b86673e08b3b100ec10338c4ea950c5
memoyil <2213635+memoyil@users.noreply.github.com>
2021-11-18 09:20:40 +0100

    refactor: Use relative path for exchange links (#2655)

--- src/views/Ifos/components/IfoFoldableCard/IfoPoolCard/GetLpModal.tsx
+++ src/views/Ifos/components/IfoFoldableCard/IfoPoolCard/GetLpModal.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Modal, ModalBody, Text, Image, Button, Link, OpenNewIcon } from '@pancakeswap/uikit'
 import { Token } from '@pancakeswap/sdk'
-import { BASE_URL } from 'config'
 import { useTranslation } from 'contexts/Localization'
 
 interface Props {
@@ -26,7 +25,7 @@ const GetLpModal: React.FC<Partial<Props>> = ({ currency, onDismiss }) => {
         <Button
           as={Link}
           external
-          href={`${BASE_URL}/swap?outputCurrency=0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82`}
+          href="/swap?outputCurrency=0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82"
           endIcon={<OpenNewIcon color="white" />}
           minWidth="100%" // Bypass the width="fit-content" on Links
         >
