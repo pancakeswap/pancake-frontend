958b3db1a64734a6f1224736e257db08c7c1007b
Chef JoJo <94336009+chef-jojo@users.noreply.github.com>
2021-11-25 11:44:15 +0800

    fix(ifo): Commit token image

--- src/config/constants/ifo.ts
+++ src/config/constants/ifo.ts
@@ -3,7 +3,7 @@ import tokens from './tokens'
 import farms from './farms'
 import { Ifo } from './types'
 
-const cakeBnbLpToken = new Token(ChainId.MAINNET, farms[1].lpAddresses[ChainId.MAINNET], 18, farms[1].lpSymbol)
+export const cakeBnbLpToken = new Token(ChainId.MAINNET, farms[1].lpAddresses[ChainId.MAINNET], 18, farms[1].lpSymbol)
 
 const ifos: Ifo[] = [
   {
