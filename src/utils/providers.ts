434c1afe2412c7de7f115b5064f9e8cf65a28284
Chef JoJo <94336009+chef-jojo@users.noreply.github.com>
2021-11-30 19:13:38 +0800

    fix: Try use both static and batch provider

--- src/utils/providers.ts
+++ src/utils/providers.ts
@@ -1,8 +1,34 @@
-import { ethers } from 'ethers'
+import { ethers, logger } from 'ethers'
+import { Logger } from '@ethersproject/logger'
+import { Network } from '@ethersproject/networks'
+import { defineReadOnly } from '@ethersproject/properties'
 import getRpcUrl from 'utils/getRpcUrl'
 
 const RPC_URL = getRpcUrl()
 
-export const simpleRpcProvider = new ethers.providers.JsonRpcBatchProvider(RPC_URL)
+class JsonRpcProvider extends ethers.providers.JsonRpcBatchProvider implements ethers.providers.StaticJsonRpcProvider {
+  // from ethers.providers.StaticJsonRpcProvider
+  async detectNetwork(): Promise<Network> {
+    let network = this.network
+    if (network == null) {
+      network = await super.detectNetwork()
+
+      if (!network) {
+        logger.throwError('no network detected', Logger.errors.UNKNOWN_ERROR, {})
+      }
+
+      // If still not set, set it
+      if (this._network == null) {
+        // A static network does not support "any"
+        defineReadOnly(this, '_network', network)
+
+        this.emit('network', network, null)
+      }
+    }
+    return network
+  }
+}
+
+export const simpleRpcProvider = new JsonRpcProvider(RPC_URL)
 
 export default null
