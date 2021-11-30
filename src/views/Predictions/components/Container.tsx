9e8c1f48eb5b653c40592b20910603f5fb5b5120
memoyil <2213635+memoyil@users.noreply.github.com>
2021-11-30 12:30:30 +0100

    fix: (prediction): Charts button not visible at first sight on desktop

--- src/views/Predictions/components/Container.tsx
+++ src/views/Predictions/components/Container.tsx
@@ -2,8 +2,7 @@ import styled from 'styled-components'
 
 const Container = styled.div`
   background: ${({ theme }) => theme.colors.gradients.violetAlt};
-  height: calc(100vh - 64px);
-  min-height: calc(100vh - 64px);
+  height: calc(100vh - 100px);
   overflow: hidden;
   position: relative;
 `
