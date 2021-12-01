2c4c7f37e8e49f79cf13137b7d59dafbfe5fdaba
Chef Cheems <chefcheems@protonmail.com>
2021-12-01 08:34:41 +0200

    fix: Banner text

--- src/views/Home/components/Banners/IFOBanner.tsx
+++ src/views/Home/components/Banners/IFOBanner.tsx
@@ -94,7 +94,7 @@ const IFOBanner = () => {
     <Wrapper>
       <Inner>
         <LeftWrapper>
-          <StyledSubheading>{t('Live')}</StyledSubheading>
+          <StyledSubheading>{t('Starting Soon')}</StyledSubheading>
           <StyledHeading scale="xl">SANTOS IFO</StyledHeading>
           <Link to="/ifo">
             <Button>
