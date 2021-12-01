6ba9c45045a52ef1311976c921ea22f18de92dc8
only <>
2021-12-01 16:58:43 +0800

    fix(Voting): Remove useless code

--- src/components/ReactMarkdown/styles.tsx
+++ src/components/ReactMarkdown/styles.tsx
@@ -56,7 +56,6 @@ const markdownComponents: Partial<NormalComponents & SpecialComponents> = {
   p: (props) => {
     return <Text as="p" my="16px" {...props} />
   },
-  // table: Table,
   table: ({ node, ...props }) => {
     return (
       <TableBox>
