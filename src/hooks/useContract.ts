9f9f3790630c4337c2458d8078b5697bf889e0c4
memoyil <2213635+memoyil@users.noreply.github.com>
2021-12-03 22:59:20 +0100

    fix: (contract): Use simple rpc provider when signer not needed

--- src/hooks/useContract.ts
+++ src/hooks/useContract.ts
@@ -219,7 +219,12 @@ function useContract(address: string | undefined, ABI: any, withSignerIfPossible
   return useMemo(() => {
     if (!address || !ABI || !library) return null
     try {
-      return getContract(address, ABI, library, withSignerIfPossible && account ? account : undefined)
+      return getContract(
+        address,
+        ABI,
+        withSignerIfPossible ? library : null,
+        withSignerIfPossible && account ? account : undefined,
+      )
     } catch (error) {
       console.error('Failed to get contract', error)
       return null
