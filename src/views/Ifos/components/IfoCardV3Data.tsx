d0979d5f21e3216142aea8d4c9da0e60e97f3ec9
Chef JoJo <94336009+chef-jojo@users.noreply.github.com>
2021-12-13 17:56:54 +0800

    fix(ifo): V3 hooks for past ifo

--- src/views/Ifos/components/IfoCardV3Data.tsx
+++ src/views/Ifos/components/IfoCardV3Data.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import useGetPublicIfoV2Data from 'views/Ifos/hooks/v2/useGetPublicIfoData'
-import useGetWalletIfoV2Data from 'views/Ifos/hooks/v2/useGetWalletIfoData'
+import useGetWalletIfoV3Data from 'views/Ifos/hooks/v3/useGetWalletIfoData'
 import { Ifo } from 'config/constants/types'
 import IfoFoldableCard from './IfoFoldableCard'
 
@@ -11,7 +11,7 @@ interface Props {
 const IfoCardV3Data: React.FC<Props> = ({ ifo }) => {
   // TODO: v3 contract
   const publicIfoData = useGetPublicIfoV2Data(ifo)
-  const walletIfoData = useGetWalletIfoV2Data(ifo)
+  const walletIfoData = useGetWalletIfoV3Data(ifo)
 
   return <IfoFoldableCard ifo={ifo} publicIfoData={publicIfoData} walletIfoData={walletIfoData} />
 }
