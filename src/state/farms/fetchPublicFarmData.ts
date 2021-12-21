027167ba7cacb184f910381949f2b2f12887dbc9
memoyil <2213635+memoyil@users.noreply.github.com>
2021-12-21 10:19:43 +0100

    refactor: (farms): Refactor public farm data fetch

--- src/state/farms/fetchPublicFarmData.ts
+++ src/state/farms/fetchPublicFarmData.ts
@@ -3,8 +3,9 @@ import masterchefABI from 'config/abi/masterchef.json'
 import erc20 from 'config/abi/erc20.json'
 import { getAddress, getMasterChefAddress } from 'utils/addressHelpers'
 import { BIG_TEN, BIG_ZERO } from 'utils/bigNumber'
-import multicall from 'utils/multicall'
+import multicall, { multicallv2 } from 'utils/multicall'
 import { SerializedFarm, SerializedBigNumber } from '../types'
+import { SerializedFarmConfig } from '../../config/constants/types'
 
 type PublicFarmData = {
   tokenAmountTotal: SerializedBigNumber
@@ -13,6 +14,63 @@ type PublicFarmData = {
   tokenPriceVsQuote: SerializedBigNumber
   poolWeight: SerializedBigNumber
   multiplier: string
+}
+
+const fetchFarmCalls = (farm: SerializedFarm) => {
+  const { lpAddresses, token, quoteToken } = farm
+  const lpAddress = getAddress(lpAddresses)
+  return [
+    // Balance of token in the LP contract
+    {
+      address: token.address,
+      name: 'balanceOf',
+      params: [lpAddress],
+    },
+    // Balance of quote token on LP contract
+    {
+      address: quoteToken.address,
+      name: 'balanceOf',
+      params: [lpAddress],
+    },
+    // Balance of LP tokens in the master chef contract
+    {
+      address: lpAddress,
+      name: 'balanceOf',
+      params: [getMasterChefAddress()],
+    },
+    // Total supply of LP tokens
+    {
+      address: lpAddress,
+      name: 'totalSupply',
+    },
+    // Token decimals
+    {
+      address: token.address,
+      name: 'decimals',
+    },
+    // Quote token decimals
+    {
+      address: quoteToken.address,
+      name: 'decimals',
+    },
+  ]
+}
+
+export const fetchPublicFarmsData = async (farms: SerializedFarmConfig[]): Promise<any[]> => {
+  const farmCalls = farms.flatMap((farm) => fetchFarmCalls(farm))
+  const farmMultiCallResult = await multicallv2(erc20, farmCalls)
+  return farmMultiCallResult.reduce((resultArray, item, index) => {
+    const chunkIndex = Math.floor(index / 6)
+
+    if (!resultArray[chunkIndex]) {
+      // eslint-disable-next-line no-param-reassign
+      resultArray[chunkIndex] = [] // start a new chunk
+    }
+
+    resultArray[chunkIndex].push(item)
+
+    return resultArray
+  }, [])
 }
 
 const fetchFarm = async (farm: SerializedFarm): Promise<PublicFarmData> => {
