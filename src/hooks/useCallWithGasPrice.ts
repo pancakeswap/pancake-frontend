f0ba6ed93ca9c3ba8f428f98d4b53b51324971fb
memoyil <2213635+memoyil@users.noreply.github.com>
2021-12-27 16:46:16 +0100

    chore: Fix docs in useCallWithGasPrice

--- src/hooks/useCallWithGasPrice.ts
+++ src/hooks/useCallWithGasPrice.ts
@@ -4,17 +4,17 @@ import { useGasPrice } from 'state/user/hooks'
 import { get } from 'lodash'
 import * as Sentry from '@sentry/react'
 
-/**
- * Perform a contract call with a gas price returned from useGasPrice
- * @param contract Used to perform the call
- * @param methodName The name of the method called
- * @param methodArgs An array of arguments to pass to the method
- * @param overrides An overrides object to pass to the method. gasPrice passed in here will take priority over the price returned by useGasPrice
- * @returns https://docs.ethers.io/v5/api/providers/types/#providers-TransactionReceipt
- */
 export function useCallWithGasPrice() {
   const gasPrice = useGasPrice()
 
+  /**
+   * Perform a contract call with a gas price returned from useGasPrice
+   * @param contract Used to perform the call
+   * @param methodName The name of the method called
+   * @param methodArgs An array of arguments to pass to the method
+   * @param overrides An overrides object to pass to the method. gasPrice passed in here will take priority over the price returned by useGasPrice
+   * @returns https://docs.ethers.io/v5/api/providers/types/#providers-TransactionReceipt
+   */
   const callWithGasPrice = useCallback(
     async (
       contract: Contract,
