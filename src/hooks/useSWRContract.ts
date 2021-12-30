3b59a8901fdf0077921d8ffb39941488f6e599e5
Chef JoJo <94336009+chef-jojo@users.noreply.github.com>
2021-12-30 17:40:42 +0800

    chore: Use callStatic types

--- src/hooks/useSWRContract.ts
+++ src/hooks/useSWRContract.ts
@@ -38,12 +38,12 @@ export const fetchStatusMiddleware: Middleware = (useSWRNext) => {
 }
 
 type MaybeContract<C extends Contract = Contract> = C | null | undefined
-type ContractMethodName<C extends Contract = Contract> = keyof C['functions'] & string
+type ContractMethodName<C extends Contract = Contract> = keyof C['callStatic'] & string
 
 type ContractMethodParams<
   C extends Contract = Contract,
   N extends ContractMethodName<C> = ContractMethodName<C>,
-> = Parameters<C['functions'][N]>
+> = Parameters<C['callStatic'][N]>
 
 type UseSWRContractArrayKey<C extends Contract = Contract, N extends ContractMethodName<C> = any> =
   | [MaybeContract<C>, N, ContractMethodParams<C, N>]
