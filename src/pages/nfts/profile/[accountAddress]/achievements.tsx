e0566bd89505139ab88a10863fe9ace2cb3d93af
Chef JoJo <94336009+chef-jojo@users.noreply.github.com>
2022-01-04 18:25:25 +0800

    fix: Use profile and redux hook for connected profile

--- src/pages/nfts/profile/[accountAddress]/achievements.tsx
+++ src/pages/nfts/profile/[accountAddress]/achievements.tsx
@@ -1,21 +1,39 @@
 import { useRouter } from 'next/router'
+import { useWeb3React } from '@web3-react/core'
 import React from 'react'
-import { useAchievementsForAddress, useProfileForAddress } from 'state/profile/hooks'
+import { useAchievementsForAddress, useProfileForAddress, useProfile } from 'state/profile/hooks'
+import { useAchievements } from 'state/achievements/hooks'
 import { NftMarketLayout } from 'views/Nft/market/Layout'
 import NftProfile from 'views/Nft/market/Profile'
 import Achievements from 'views/Nft/market/Profile/components/Achievements'
+import { AchievementFetchStatus } from 'state/types'
 
 const NftProfileAchievementsPage = () => {
+  const { account } = useWeb3React()
+  const accountAddress = useRouter().query.accountAddress as string
+  const isConnectedProfile = account?.toLowerCase() === accountAddress?.toLowerCase()
+
+  return <>{isConnectedProfile ? <ConnectedAchievement /> : <UnConnectedAchievement />}</>
+}
+
+const ConnectedAchievement = () => {
+  const { profile } = useProfile()
+  const { achievements, achievementFetchStatus } = useAchievements()
+  return (
+    <Achievements
+      achievements={achievements}
+      isLoading={achievementFetchStatus !== AchievementFetchStatus.FETCHED}
+      points={profile?.points}
+    />
+  )
+}
+
+const UnConnectedAchievement = () => {
   const accountAddress = useRouter().query.accountAddress as string
   const { profile: profileHookState } = useProfileForAddress(accountAddress)
   const { profile } = profileHookState || {}
   const { achievements, isFetching: isAchievementFetching } = useAchievementsForAddress(accountAddress)
-
-  return (
-    <>
-      <Achievements achievements={achievements} isLoading={isAchievementFetching} points={profile?.points} />
-    </>
-  )
+  return <Achievements achievements={achievements} isLoading={isAchievementFetching} points={profile?.points} />
 }
 
 NftProfileAchievementsPage.getLayout = (page) => {
