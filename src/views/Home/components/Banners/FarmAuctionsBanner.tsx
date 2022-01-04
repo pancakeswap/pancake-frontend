2a544ebbaefae05c955ccc32fe774a193acc58bb
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-01-04 20:18:44 +0800

    feat: Add whitelist auction 12 (#2979)
    * feat: Add whitelist auction 12
    
    * fix: Update project url
    
    * chore: Update HE quote token

--- src/views/Home/components/Banners/FarmAuctionsBanner.tsx
+++ src/views/Home/components/Banners/FarmAuctionsBanner.tsx
@@ -88,7 +88,7 @@ const FarmAuctionsBanner = () => {
     <Wrapper>
       <Inner>
         <LeftWrapper>
-          <StyledSubheading>{t('%num% Contenders...', { num: 15 })}</StyledSubheading>
+          <StyledSubheading>{t('%num% Contenders...', { num: 16 })}</StyledSubheading>
           <StyledHeading scale="xl">{t('%num% Winners', { num: 3 })}</StyledHeading>
           <Link to="/farms/auction">
             <Button>
