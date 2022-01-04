436328cecc543571c2d75eef1a92332194ff6701
Chef JoJo <94336009+chef-jojo@users.noreply.github.com>
2022-01-04 23:24:29 +0800

    fix: Test

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,6 @@ const customJestConfig = {
   testPathIgnorePatterns: ['<rootDir>/cypress/', '<rootDir>/src/__tests__/config'],
   moduleDirectories: ['node_modules', 'src'],
   testTimeout: 20000,
-  testEnvironment: 'jsdom',
 }
 
 // createJestConfig is exported in this way to ensure that next/jest can load the Next.js configuration, which is async
