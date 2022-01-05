446bdc9cf1fde7b218041ca0747c1271d74779d3
Chef JoJo <94336009+chef-jojo@users.noreply.github.com>
2022-01-05 19:46:18 +0800

    feat: List auction farm 12

--- src/config/constants/tokens.ts
+++ src/config/constants/tokens.ts
@@ -1902,6 +1902,30 @@ export const mainnetTokens = defineTokens({
     'GoldMiner',
     'https://goldminer.games/',
   ),
+  aog: new Token(
+    MAINNET,
+    '0x40C8225329Bd3e28A043B029E0D07a5344d2C27C',
+    18,
+    'AOG',
+    'AgeOfGods',
+    'https://ageofgods.net/',
+  ),
+  eightPay: new Token(
+    MAINNET,
+    '0xfeea0bdd3d07eb6fe305938878c0cadbfa169042',
+    18,
+    '8PAY',
+    '8PAY Network',
+    'https://8pay.network/',
+  ),
+  bath: new Token(
+    MAINNET,
+    '0x0bc89aa98Ad94E6798Ec822d0814d934cCD0c0cE',
+    18,
+    'BATH',
+    'Battle Hero',
+    'https://battlehero.io/',
+  ),
 } as const)
 
 export const testnetTokens = defineTokens({
