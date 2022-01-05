bba5d4d11102f7c931854607cf88d7807396dbb2
memoyil <2213635+memoyil@users.noreply.github.com>
2022-01-05 14:44:13 +0100

    refactor: (nft constant): Rename collection to pancakecollection

--- src/views/Nft/market/constants.ts
+++ src/views/Nft/market/constants.ts
@@ -1,7 +1,7 @@
-import collections from 'config/constants/nftsCollections'
-import { CollectionKey } from 'config/constants/nftsCollections/types'
+import { PancakeCollectionKey } from 'config/constants/nftsCollections/types'
+import pancakeCollections from 'config/constants/nftsCollections'
 import { getAddress } from 'utils/addressHelpers'
 
 export const nftsBaseUrl = '/nfts'
-export const pancakeBunniesAddress = getAddress(collections[CollectionKey.PANCAKE].address)
-export const pancakeSquadAddress = getAddress(collections[CollectionKey.SQUAD].address)
+export const pancakeBunniesAddress = getAddress(pancakeCollections[PancakeCollectionKey.PANCAKE].address)
+export const pancakeSquadAddress = getAddress(pancakeCollections[PancakeCollectionKey.SQUAD].address)
