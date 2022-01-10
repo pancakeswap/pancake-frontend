8f9cae8691863951dd09141bd3c7c2d37bf795c7
Chef JoJo <94336009+chef-jojo@users.noreply.github.com>
2022-01-10 11:40:38 +0800

    ci: Add serve start

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 declare namespace Cypress {
-  interface Chainable<Subject> {
+  interface Chainable {
     /**
      * shorthand command for `data-test="<dataTestAttribute>"`
      * @example cy.getBySel('my-element')
