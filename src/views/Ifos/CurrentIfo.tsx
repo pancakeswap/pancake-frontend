11b9929689e9f700bc5fe7295e32c47c2459ca07
chefilip <philip@pancakeswap.com>
2022-01-10 17:18:04 +0700

    chore(ifo): Set how-to section fullwidth

--- src/views/Ifos/CurrentIfo.tsx
+++ src/views/Ifos/CurrentIfo.tsx
@@ -1,12 +1,20 @@
 import { ifosConfig } from 'config/constants'
 import React from 'react'
+import styled from 'styled-components'
+import { Box } from '@pancakeswap/uikit'
+
 import useGetPublicIfoV2Data from 'views/Ifos/hooks/v2/useGetPublicIfoData'
 import useGetWalletIfoV3Data from 'views/Ifos/hooks/v3/useGetWalletIfoData'
+import Container from 'components/Layout/Container'
 import { IfoCurrentCard } from './components/IfoFoldableCard'
 import IfoLayout, { IfoLayoutWrapper } from './components/IfoLayout'
 import IfoPoolVaultCard from './components/IfoPoolVaultCard'
 import IfoQuestions from './components/IfoQuestions'
 import IfoSteps from './components/IfoSteps'
+
+const IfoStepBackground = styled(Box)`
+  background: ${({ theme }) => theme.colors.gradients.bubblegum};
+`
 
 /**
  * Note: currently there should be only 1 active IFO at a time
@@ -19,12 +27,20 @@ const Ifo = () => {
 
   return (
     <IfoLayout id="current-ifo" py={['24px', '24px', '40px']}>
-      <IfoLayoutWrapper>
-        <IfoPoolVaultCard />
-        <IfoCurrentCard ifo={activeIfo} publicIfoData={publicIfoData} walletIfoData={walletIfoData} />
-      </IfoLayoutWrapper>
-      <IfoSteps isLive={publicIfoData.status === 'live'} ifo={activeIfo} walletIfoData={walletIfoData} />
-      <IfoQuestions />
+      <Container>
+        <IfoLayoutWrapper>
+          <IfoPoolVaultCard />
+          <IfoCurrentCard ifo={activeIfo} publicIfoData={publicIfoData} walletIfoData={walletIfoData} />
+        </IfoLayoutWrapper>
+      </Container>
+      <IfoStepBackground>
+        <Container>
+          <IfoSteps isLive={publicIfoData.status === 'live'} ifo={activeIfo} walletIfoData={walletIfoData} />
+        </Container>
+      </IfoStepBackground>
+      <Container>
+        <IfoQuestions />
+      </Container>
     </IfoLayout>
   )
 }
