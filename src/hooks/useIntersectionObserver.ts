a75585d8bc7561c7f831a9e8f5ca7fbcaf1aaeb8
Chef Cheems <81824236+Chef-Cheems@users.noreply.github.com>
2022-01-16 13:51:29 +0200

    chore: Switch places for hook declaration

--- src/hooks/useIntersectionObserver.ts
+++ src/hooks/useIntersectionObserver.ts
@@ -1,8 +1,8 @@
 import { useEffect, useRef, useState } from 'react'
 
 const useIntersectionObserver = () => {
-  const [observerRef] = useState(() => (element) => setObserverRefElement(element))
   const [observerRefElement, setObserverRefElement] = useState(null)
+  const [observerRef] = useState(() => (element) => setObserverRefElement(element))
   const intersectionObserverRef = useRef<IntersectionObserver>(null)
   const [observerIsSet, setObserverIsSet] = useState(false)
   const [isIntersecting, setIsIntersecting] = useState(false)
