323cf9a0b279202b7bac45b231fda0ffabef4b08
Agustin Kassis <webmaster@masize.com>
2022-01-17 21:49:38 -0300

    home refactored

--- src/views/Home/components/MetricsSection/index.tsx
+++ src/views/Home/components/MetricsSection/index.tsx
@@ -1,28 +1,25 @@
 import React from 'react'
-import { Heading, Flex, Text, Skeleton, ChartIcon, CommunityIcon, SwapIcon } from 'peronio-uikit'
+import { Heading, Flex, Text, CommunityIcon, SwapIcon } from 'peronio-uikit'
 import { useTranslation } from 'contexts/Localization'
-import { useGetStats } from 'hooks/api'
+// import { useGetStats } from 'hooks/api'
 import useTheme from 'hooks/useTheme'
-import { formatLocalisedCompactNumber } from 'utils/formatBalance'
+// import { formatLocalisedCompactNumber } from 'utils/formatBalance'
 import IconCard, { IconCardData } from '../IconCard'
 import StatCardContent from './StatCardContent'
 import GradientLogo from '../GradientLogoSvg'
 
 // Values fetched from bitQuery effective 6/9/21
-const txCount = 30841921
-const addressCount = 2751624
+// const txCount = 30841921
+// const addressCount = 2751624
 
 const Stats = () => {
   const { t } = useTranslation()
-  const data = useGetStats()
+  // const data = useGetStats()
   const { theme } = useTheme()
 
-  const tvlString = data ? formatLocalisedCompactNumber(data.tvl) : '-'
-  const trades = formatLocalisedCompactNumber(txCount)
-  const users = formatLocalisedCompactNumber(addressCount)
-
-  const tvlText = t('And those users are now entrusting the platform with over $%tvl% in funds.', { tvl: tvlString })
-  const [entrusting, inFunds] = tvlText.split(tvlString)
+  // const tvlString = data ? formatLocalisedCompactNumber(data.tvl) : '-'
+  // const trades = formatLocalisedCompactNumber(txCount)
+  // const users = formatLocalisedCompactNumber(addressCount)
 
   const UsersCardData: IconCardData = {
     icon: <CommunityIcon color="secondary" width="36px" />,
@@ -32,54 +29,52 @@ const Stats = () => {
     icon: <SwapIcon color="primary" width="36px" />,
   }
 
-  const StakedCardData: IconCardData = {
-    icon: <ChartIcon color="failure" width="36px" />,
-  }
+  // const StakedCardData: IconCardData = {
+  //   icon: <ChartIcon color="failure" width="36px" />,
+  // }
 
   return (
     <Flex justifyContent="center" alignItems="center" flexDirection="column">
       <GradientLogo height="48px" width="48px" mb="24px" />
       <Heading textAlign="center" scale="xl">
-        {t('Used by millions.')}
+        {t('The currency of the people.')}
       </Heading>
       <Heading textAlign="center" scale="xl" mb="32px">
-        {t('Trusted with billions.')}
+        {t('Managed by the people.')}
       </Heading>
       <Text textAlign="center" color="textSubtle">
-        {t('PancakeSwap has the most users of any decentralized platform, ever.')}
+        {t('Defi platform to operate with Peronio')}
       </Text>
       <Flex flexWrap="wrap">
         <Text display="inline" textAlign="center" color="textSubtle" mb="20px">
-          {entrusting}
-          <>{data ? <>{tvlString}</> : <Skeleton display="inline-block" height={16} width={70} mt="2px" />}</>
-          {inFunds}
+          {t('100% National without state')}
         </Text>
       </Flex>
 
       <Text textAlign="center" color="textSubtle" bold mb="32px">
-        {t('Will you join them?')}
+        {t('Starting point:')}
       </Text>
 
       <Flex flexDirection={['column', null, null, 'row']}>
         <IconCard {...UsersCardData} mr={[null, null, null, '16px']} mb={['16px', null, null, '0']}>
           <StatCardContent
-            headingText={t('%users% users', { users })}
-            bodyText={t('in the last 30 days')}
+            headingText={t('USD 1 = PE 250')}
+            bodyText={t('initial emission')}
             highlightColor={theme.colors.secondary}
           />
         </IconCard>
         <IconCard {...TradesCardData} mr={[null, null, null, '16px']} mb={['16px', null, null, '0']}>
           <StatCardContent
-            headingText={t('%trades% trades', { trades })}
-            bodyText={t('made in the last 30 days')}
+            headingText={t('PE 1 = ARS 0.80')}
+            bodyText={t('gave the peso a handicap')}
             highlightColor={theme.colors.primary}
           />
         </IconCard>
-        <IconCard {...StakedCardData}>
+        <IconCard {...TradesCardData} mr={[null, null, null, '16px']} mb={['16px', null, null, '0']}>
           <StatCardContent
-            headingText={t('$%tvl% staked', { tvl: tvlString })}
-            bodyText={t('Total Value Locked')}
-            highlightColor={theme.colors.failure}
+            headingText={t('15% Interest')}
+            bodyText={t('Provided by AAVE')}
+            highlightColor={theme.colors.primary}
           />
         </IconCard>
       </Flex>
