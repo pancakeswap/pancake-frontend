1c8b8c95fb15be48e749a3fdd912fb3248777fdf
Chef JoJo <94336009+chef-jojo@users.noreply.github.com>
2022-01-21 11:05:40 +0800

    test: Fix farm test sometimes fail

--- cypress/integration/farms/farms.test.ts
+++ cypress/integration/farms/farms.test.ts
@@ -1,17 +1,12 @@
 describe('Farms Page', () => {
-  beforeEach(() => {
-    cy.visit('/farms')
-  })
-
   it('loads live farms', () => {
+    cy.visit('/farms')
     cy.get('#farms-table').should('be.visible')
   })
 
   it('loads finished farms', () => {
-    cy.get('#finished-farms-button').click({ force: true })
+    cy.visit('/farms/history')
     cy.get('#staked-only-farms').click({ force: true })
-    // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(30000)
     cy.get('#farms-table').should('be.visible')
   })
 })
