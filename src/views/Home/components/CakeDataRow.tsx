7377f29386a73fc61348dabe11d9e7923e2c8884
memoyil <2213635+memoyil@users.noreply.github.com>
2022-01-24 03:41:37 +0100

    perf: (home): Make request only when visible for cake token data component (#3083)

--- src/views/Home/components/CakeDataRow.tsx
+++ src/views/Home/components/CakeDataRow.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
-import { useTotalSupply, useBurnedBalance } from 'hooks/useTokenBalance'
 import { getBalanceNumber, formatLocalisedCompactNumber } from 'utils/formatBalance'
+import { multicallv2 } from 'utils/multicall'
 import { usePriceCakeBusd } from 'state/farms/hooks'
 import { Flex, Text, Heading, Skeleton } from '@pancakeswap/uikit'
 import { useTranslation } from 'contexts/Localization'
 import Balance from 'components/Balance'
+import BigNumber from 'bignumber.js'
 import tokens from 'config/constants/tokens'
+import cakeAbi from 'config/abi/cake.json'
+import useIntersectionObserver from 'hooks/useIntersectionObserver'
+import { useSlowRefreshEffect } from 'hooks/useRefreshEffect'
 
 const StyledColumn = styled(Flex)<{ noMobileBorder?: boolean }>`
   flex-direction: column;
@@ -45,12 +49,41 @@ const emissionsPerBlock = 14.25
 
 const CakeDataRow = () => {
   const { t } = useTranslation()
-  const totalSupply = useTotalSupply()
-  const burnedBalance = getBalanceNumber(useBurnedBalance(tokens.cake.address))
-  const cakeSupply = totalSupply ? getBalanceNumber(totalSupply) - burnedBalance : 0
+  const { observerRef, isIntersecting } = useIntersectionObserver()
+  const [loadData, setLoadData] = useState(false)
+  const [totalSupply, setTotalSupply] = useState(0)
+  const [burnedBalance, setBurnedBalance] = useState(0)
+  const cakeSupply = totalSupply ? totalSupply - burnedBalance : 0
   const cakePriceBusd = usePriceCakeBusd()
   const mcap = cakePriceBusd.times(cakeSupply)
   const mcapString = formatLocalisedCompactNumber(mcap.toNumber())
+
+  useEffect(() => {
+    if (isIntersecting) {
+      setLoadData(true)
+    }
+  }, [isIntersecting])
+
+  useSlowRefreshEffect(() => {
+    const fetchTokenData = async () => {
+      const totalSupplyCall = { address: tokens.cake.address, name: 'totalSupply' }
+      const burnedTokenCall = {
+        address: tokens.cake.address,
+        name: 'balanceOf',
+        params: ['0x000000000000000000000000000000000000dEaD'],
+      }
+      const tokenDataResultRaw = await multicallv2(cakeAbi, [totalSupplyCall, burnedTokenCall], {
+        requireSuccess: false,
+      })
+      const tokenDataResult = tokenDataResultRaw.flat()
+      setTotalSupply(tokenDataResult[0] ? getBalanceNumber(new BigNumber(tokenDataResult[0].toString())) : 0)
+      setBurnedBalance(tokenDataResult[1] ? getBalanceNumber(new BigNumber(tokenDataResult[1].toString())) : 0)
+    }
+
+    if (loadData) {
+      fetchTokenData()
+    }
+  }, [loadData])
 
   return (
     <Grid>
@@ -59,7 +92,10 @@ const CakeDataRow = () => {
         {cakeSupply ? (
           <Balance decimals={0} lineHeight="1.1" fontSize="24px" bold value={cakeSupply} />
         ) : (
-          <Skeleton height={24} width={126} my="4px" />
+          <>
+            <div ref={observerRef} />
+            <Skeleton height={24} width={126} my="4px" />
+          </>
         )}
       </Flex>
       <StyledColumn>
