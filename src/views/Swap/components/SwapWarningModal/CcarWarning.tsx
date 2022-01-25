3fd5af6a69e17dc11ceac866fbe639c17af1a632
chefilip <philip@pancakeswap.com>
2022-01-25 10:03:06 +0700

    chore: Update text

--- src/views/Swap/components/SwapWarningModal/CcarWarning.tsx
+++ src/views/Swap/components/SwapWarningModal/CcarWarning.tsx
@@ -8,8 +8,16 @@ const CcarWarning = () => {
   return (
     <>
       <Text>
+        {t('Crypto Cars (CCAR) has been migrated to')}{' '}
+        <Link
+          style={{ display: 'inline' }}
+          external
+          href="https://bscscan.com/token/0x322e5015Cc464Ada7f99dE7131CE494dE1834396"
+        >
+          {t('a new contract address.')}
+        </Link>{' '}
         {t(
-          'Crypto Cars (CCAR) has been migrated to a new contract. Trading on the old contract may result in the complete loss of your assets. For more information please refer to',
+          'Trading on the old address may result in the complete loss of your assets. For more information please refer to',
         )}{' '}
         <Link style={{ display: 'inline' }} external href="https://t.me/Crypto_Cars_Official/465037">
           {t('the announcement.')}
