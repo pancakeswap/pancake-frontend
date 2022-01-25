8c4a4d0abc7d8690f1813b35c7991c9a1b241170
Chef JoJo <94336009+chef-jojo@users.noreply.github.com>
2022-01-26 00:42:50 +0800

    feat(pool): Add user requirement

--- src/views/Pools/components/PoolCard/CardActions/index.tsx
+++ src/views/Pools/components/PoolCard/CardActions/index.tsx
@@ -9,6 +9,8 @@ import { DeserializedPool } from 'state/types'
 import ApprovalAction from './ApprovalAction'
 import StakeActions from './StakeActions'
 import HarvestActions from './HarvestActions'
+import { useProfileRequirement } from 'views/Pools/hooks/useProfileRequirement'
+import { ProfileRequirementWarning } from '../../ProfileRequirementWarning'
 
 const InlineText = styled(Text)`
   display: inline;
@@ -20,7 +22,8 @@ interface CardActionsProps {
 }
 
 const CardActions: React.FC<CardActionsProps> = ({ pool, stakedBalance }) => {
-  const { sousId, stakingToken, earningToken, harvest, poolCategory, userData, earningTokenPrice } = pool
+  const { sousId, stakingToken, earningToken, harvest, poolCategory, userData, earningTokenPrice, profileRequirement } =
+    pool
   // Pools using native BNB behave differently than pools using a token
   const isBnbPool = poolCategory === PoolCategory.BINANCE
   const { t } = useTranslation()
@@ -30,6 +33,8 @@ const CardActions: React.FC<CardActionsProps> = ({ pool, stakedBalance }) => {
   const needsApproval = !allowance.gt(0) && !isBnbPool
   const isStaked = stakedBalance.gt(0)
   const isLoading = !userData
+
+  const { notMeetRequired, notMeetThreshold } = useProfileRequirement(profileRequirement)
 
   return (
     <Flex flexDirection="column">
@@ -62,7 +67,9 @@ const CardActions: React.FC<CardActionsProps> = ({ pool, stakedBalance }) => {
             {isStaked ? t('Staked') : `${stakingToken.symbol}`}
           </InlineText>
         </Box>
-        {needsApproval ? (
+        {notMeetRequired || notMeetThreshold ? (
+          <ProfileRequirementWarning profileRequirement={profileRequirement} />
+        ) : needsApproval ? (
           <ApprovalAction pool={pool} isLoading={isLoading} />
         ) : (
           <StakeActions
