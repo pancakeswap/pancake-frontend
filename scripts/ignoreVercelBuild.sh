eaf105e57de13788d039b2864ba77dd6d737fde3
Chef JoJo <94336009+chef-jojo@users.noreply.github.com>
2022-01-27 11:15:44 +0800

    ci: Update ignore script

--- scripts/ignoreVercelBuild.sh
+++ scripts/ignoreVercelBuild.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-APP=app
+APP=${APP:-app}
 
 # Determine version of Nx installed
 NX_VERSION=$(node -e "console.log(require('./package.json').devDependencies['@nrwl/workspace'])")
 TS_VERSION=$(node -e "console.log(require('./package.json').devDependencies['typescript'])")
 
 # Install @nrwl/workspace in order to run the affected command
-npm install -D @nrwl/workspace@$NX_VERSION --prefer-offline
-npm install -D typescript@$TS_VERSION --prefer-offline
+yanr -D @nrwl/workspace@$NX_VERSION --prefer-offline
+yarn -D typescript@$TS_VERSION --prefer-offline
 
 # Run the affected command, comparing latest commit to the one before that
 npx nx affected:apps --plain --base HEAD~1 --head HEAD | grep $APP -q
