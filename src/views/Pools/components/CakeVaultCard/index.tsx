70dbb0f53107f4a8aad78a5110c4be90261fa17e
Agustin Kassis <webmaster@masize.com>
2022-02-08 17:05:15 -0300

    external twitter link

--- src/views/Pools/components/CakeVaultCard/index.tsx
+++ src/views/Pools/components/CakeVaultCard/index.tsx
@@ -51,7 +51,7 @@ export const CreditCalcBlock = () => {
             'The latest credit calculation period has ended. After the coming IFO, credits will be reset and the calculation will resume.',
           )}
         </Text>
-        <LinkExternal href="https://twitter.com/pancakeswap">
+        <LinkExternal href="https://twitter.com/peronioar">
           {t('Follow us on Twitter to catch the latest news about the coming IFO.')}
         </LinkExternal>
       </>
