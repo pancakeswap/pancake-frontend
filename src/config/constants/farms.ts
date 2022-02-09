0898aa924c79f2301f84f8d47655c5c84e82c10b
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-02-09 13:26:23 +0800

    fix: Token image case (#3202)
    * fix: Token image case
    
    * fix: Token image case 2

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -13,7 +13,7 @@ const farms: SerializedFarmConfig[] = [
     lpSymbol: 'CAKE',
     lpAddresses: {
       97: '0x9C21123D94b93361a29B2C2EFB3d5CD8B17e0A9e',
-      56: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
+      56: '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82',
     },
     token: serializedTokens.syrup,
     quoteToken: serializedTokens.wbnb,
