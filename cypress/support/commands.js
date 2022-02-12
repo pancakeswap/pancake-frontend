764913556b7b0a6a615022ea2f0c53220adb938a
Chef JoJo <94336009+chef-jojo@users.noreply.github.com>
2022-02-12 13:41:55 +0800

    chore: Debug

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -102,7 +102,7 @@ Cypress.Commands.add('getBySel', (selector, ...args) => {
 
 Cypress.Commands.overwrite('log', (subject, message) => cy.task('log', message))
 
-/* eslint-disable */
-Cypress.on('window:before:load', (win) => {
-  win.sfHeader = Cypress.env('SF_HEADER')
-})
+// /* eslint-disable */
+// Cypress.on('window:before:load', (win) => {
+//   win.sfHeader = Cypress.env('SF_HEADER')
+// })
