cdf3221b61205e55f712109882a7643698e82ac7
Chef JoJo <94336009+chef-jojo@users.noreply.github.com>
2022-02-14 13:21:16 +0800

    perf(swap): Remove lazy load loader

--- src/views/Swap/components/Chart/BasicChart.tsx
+++ src/views/Swap/components/Chart/BasicChart.tsx
@@ -4,14 +4,12 @@ import { useState, memo } from 'react'
 import { useFetchPairPrices } from 'state/swap/hooks'
 import dynamic from 'next/dynamic'
 import { PairDataTimeWindowEnum } from 'state/swap/types'
-import { LineChartLoader } from 'views/Info/components/ChartLoaders'
 import NoChartAvailable from './NoChartAvailable'
 import PairPriceDisplay from '../../../../components/PairPriceDisplay'
 import { getTimeWindowChange } from './utils'
 
 const SwapLineChart = dynamic(() => import('./SwapLineChart'), {
   ssr: false,
-  loading: () => <LineChartLoader />,
 })
 
 const BasicChart = ({
