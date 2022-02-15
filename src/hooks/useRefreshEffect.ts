c723b5468fd5d30169b2c94d44c10472725bad4a
memoyil <2213635+memoyil@users.noreply.github.com>
2022-02-15 17:27:16 +0100

    fix: Destructure deps array for useMemo to not be called for same list

--- src/hooks/useRefreshEffect.ts
+++ src/hooks/useRefreshEffect.ts
@@ -1,12 +1,23 @@
 import { FAST_INTERVAL, SLOW_INTERVAL } from 'config/constants'
 import { DependencyList, EffectCallback, useEffect, useMemo } from 'react'
 import useSWR from 'swr'
+import { EMPTY_ARRAY } from 'utils/constantObjects'
 
 type BlockEffectCallback = (blockNumber: number) => ReturnType<EffectCallback>
 
+const useDepsMemo = (blockNumber: number, deps?: DependencyList) => {
+  const depsMemo = useMemo(
+    () => [blockNumber, ...(deps || EMPTY_ARRAY)],
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [blockNumber, ...(deps || EMPTY_ARRAY)],
+  )
+
+  return depsMemo
+}
+
 export function useFastRefreshEffect(effect: BlockEffectCallback, deps?: DependencyList) {
   const { data = 0 } = useSWR([FAST_INTERVAL, 'blockNumber'])
-  const depsMemo = useMemo(() => [data, ...(deps || [])], [data, deps])
+  const depsMemo = useDepsMemo(data, deps)
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(effect.bind(null, data), depsMemo)
@@ -14,7 +25,7 @@ export function useFastRefreshEffect(effect: BlockEffectCallback, deps?: Depende
 
 export function useSlowRefreshEffect(effect: BlockEffectCallback, deps?: DependencyList) {
   const { data = 0 } = useSWR([SLOW_INTERVAL, 'blockNumber'])
-  const depsMemo = useMemo(() => [data, ...(deps || [])], [data, deps])
+  const depsMemo = useDepsMemo(data, deps)
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(effect.bind(null, data), depsMemo)
