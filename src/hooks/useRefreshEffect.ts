479cd412f759cb9c23c73229ab87f51ff9afddce
memoyil <2213635+memoyil@users.noreply.github.com>
2022-02-15 17:48:35 +0100

    fix: Remove useDepsMemo

--- src/hooks/useRefreshEffect.ts
+++ src/hooks/useRefreshEffect.ts
@@ -1,32 +1,20 @@
 import { FAST_INTERVAL, SLOW_INTERVAL } from 'config/constants'
-import { DependencyList, EffectCallback, useEffect, useMemo } from 'react'
+import { DependencyList, EffectCallback, useEffect } from 'react'
 import useSWR from 'swr'
 import { EMPTY_ARRAY } from 'utils/constantObjects'
 
 type BlockEffectCallback = (blockNumber: number) => ReturnType<EffectCallback>
 
-const useDepsMemo = (blockNumber: number, deps?: DependencyList) => {
-  const depsMemo = useMemo(
-    () => [blockNumber, ...(deps || EMPTY_ARRAY)],
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [blockNumber, ...(deps || EMPTY_ARRAY)],
-  )
-
-  return depsMemo
-}
-
 export function useFastRefreshEffect(effect: BlockEffectCallback, deps?: DependencyList) {
   const { data = 0 } = useSWR([FAST_INTERVAL, 'blockNumber'])
-  const depsMemo = useDepsMemo(data, deps)
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(effect.bind(null, data), depsMemo)
+  useEffect(effect.bind(null, data), [data, ...(deps || EMPTY_ARRAY)])
 }
 
 export function useSlowRefreshEffect(effect: BlockEffectCallback, deps?: DependencyList) {
   const { data = 0 } = useSWR([SLOW_INTERVAL, 'blockNumber'])
-  const depsMemo = useDepsMemo(data, deps)
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(effect.bind(null, data), depsMemo)
+  useEffect(effect.bind(null, data), [data, ...(deps || EMPTY_ARRAY)])
 }
