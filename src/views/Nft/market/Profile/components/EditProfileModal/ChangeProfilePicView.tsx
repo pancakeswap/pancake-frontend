caaf19f830db12e7182e46da1bbe5efee5ef2315
memoyil <2213635+memoyil@users.noreply.github.com>
2022-02-17 00:08:12 +0100

    fix: (profile): No nfts inside wallet doesn't show warning in change profile modal

--- src/views/Nft/market/Profile/components/EditProfileModal/ChangeProfilePicView.tsx
+++ src/views/Nft/market/Profile/components/EditProfileModal/ChangeProfilePicView.tsx
@@ -1,5 +1,5 @@
 import { useState, useMemo } from 'react'
-import { Button, Box, InjectedModalProps, Text } from '@pancakeswap/uikit'
+import { Button, Box, InjectedModalProps, Text, Skeleton } from '@pancakeswap/uikit'
 import { useWeb3React } from '@web3-react/core'
 import { useProfile } from 'state/profile/hooks'
 import { useTranslation } from 'contexts/Localization'
@@ -28,7 +28,7 @@ const ChangeProfilePicPage: React.FC<ChangeProfilePicPageProps> = ({ onDismiss, 
   const { t } = useTranslation()
   const { account, library } = useWeb3React()
   const { isLoading: isProfileLoading, profile, refresh: refreshProfile } = useProfile()
-  const { nfts } = useNftsForAddress(account, profile, isProfileLoading)
+  const { nfts, isLoading } = useNftsForAddress(account, profile, isProfileLoading)
   const profileContract = useProfileContract()
   const { toastSuccess } = useToast()
   const { callWithGasPrice } = useCallWithGasPrice()
@@ -75,30 +75,33 @@ const ChangeProfilePicPage: React.FC<ChangeProfilePicPageProps> = ({ onDismiss, 
       <Text as="p" color="textSubtle" mb="24px">
         {t('Choose a new Collectible to use as your profile pic.')}
       </Text>
-      <Box maxHeight="300px" overflowY="scroll">
-        {nftsInWallet.map((walletNft) => {
-          const handleChange = () => {
-            setSelectedNft({
-              tokenId: walletNft.tokenId,
-              collectionAddress: walletNft.collectionAddress,
-            })
-          }
-          return (
-            <SelectionCard
-              name="profilePicture"
-              key={`${walletNft.collectionAddress}#${walletNft.tokenId}`}
-              value={walletNft.tokenId}
-              image={walletNft.image.thumbnail}
-              isChecked={walletNft.tokenId === selectedNft.tokenId}
-              onChange={handleChange}
-              disabled={isApproving || isConfirming || isConfirmed}
-            >
-              <Text bold>{walletNft.name}</Text>
-            </SelectionCard>
-          )
-        })}
-      </Box>
-      {nfts.length === 0 && (
+      {isLoading ? (
+        <Skeleton width="100%" height="80px" mb="16px" />
+      ) : nftsInWallet.length > 0 ? (
+        <Box maxHeight="300px" overflowY="scroll">
+          {nftsInWallet.map((walletNft) => {
+            const handleChange = () => {
+              setSelectedNft({
+                tokenId: walletNft.tokenId,
+                collectionAddress: walletNft.collectionAddress,
+              })
+            }
+            return (
+              <SelectionCard
+                name="profilePicture"
+                key={`${walletNft.collectionAddress}#${walletNft.tokenId}`}
+                value={walletNft.tokenId}
+                image={walletNft.image.thumbnail}
+                isChecked={walletNft.tokenId === selectedNft.tokenId}
+                onChange={handleChange}
+                disabled={isApproving || isConfirming || isConfirmed}
+              >
+                <Text bold>{walletNft.name}</Text>
+              </SelectionCard>
+            )
+          })}
+        </Box>
+      ) : (
         <>
           <Text as="p" color="textSubtle" mb="16px">
             {t('Sorry! You don’t have any eligible Collectibles in your wallet to use!')}
