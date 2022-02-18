fa46d8323cede73b85d55843d74cf92c386a802e
Hsuan Lee <BOCZeratul@gmail.com>
2022-02-18 14:09:10 +0800

    feat: Add Blocto to wallet options (#343)

--- packages/pancake-uikit/src/widgets/WalletModal/config.tsx
+++ packages/pancake-uikit/src/widgets/WalletModal/config.tsx
@@ -6,6 +6,7 @@ import TokenPocket from "../../components/Svg/Icons/TokenPocket";
 import BinanceChain from "../../components/Svg/Icons/BinanceChain";
 import SafePal from "../../components/Svg/Icons/SafePal";
 import Coin98 from "../../components/Svg/Icons/Coin98";
+import Blocto from "../../components/Svg/Icons/Blocto";
 
 import { Config, ConnectorNames } from "./types";
 
@@ -57,6 +58,12 @@ const connectors: Config[] = [
     title: "Coin98",
     icon: Coin98,
     connectorId: ConnectorNames.Injected,
+    priority: 999,
+  },
+  {
+    title: "Blocto",
+    icon: Blocto,
+    connectorId: ConnectorNames.Blocto,
     priority: 999,
   },
 ];
--- packages/pancake-uikit/src/widgets/WalletModal/types.ts
+++ packages/pancake-uikit/src/widgets/WalletModal/types.ts
@@ -5,6 +5,7 @@ export enum ConnectorNames {
   Injected = "injected",
   WalletConnect = "walletconnect",
   BSC = "bsc",
+  Blocto = "blocto",
 }
 
 export type Login = (connectorId: ConnectorNames) => void;
