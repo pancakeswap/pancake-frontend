8dedf0d31b70076af7816bfd78ec99b6df100f42
chefilip <philip@pancakeswap.com>
2022-02-18 14:00:23 +0700

    chore: Allow sentry send errors on preview link

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -13,6 +13,7 @@ const isUserRejected = (err) => {
 
 Sentry.init({
   dsn: SENTRY_DSN || 'https://ed98e16b9d704c22bef92d24bdd5f3b7@o1092725.ingest.sentry.io/6111410',
+  allowUrls: process.env.NODE_ENV === 'production' ? [/https:\/\/pancakeswap\.finance/] : [],
   integrations: [
     new Sentry.Integrations.Breadcrumbs({
       console: process.env.NODE_ENV === 'production',
