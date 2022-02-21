c98b0584fdb807d44b9095c7d53850fddf089bb9
Chef JoJo <94336009+chef-jojo@users.noreply.github.com>
2022-02-21 13:12:20 +0800

    feat(swap): Error message and handle

---
+++ src/hooks/useSwapCallArguments.ts
@@ -0,0 +1,64 @@
+import { Contract } from '@ethersproject/contracts'
+import { JSBI, Percent, Router, SwapParameters, Trade, TradeType } from '@pancakeswap/sdk'
+import useActiveWeb3React from 'hooks/useActiveWeb3React'
+import { useMemo } from 'react'
+import { BIPS_BASE, INITIAL_ALLOWED_SLIPPAGE } from '../config/constants'
+import { getRouterContract } from '../utils'
+import useENS from './ENS/useENS'
+import useTransactionDeadline from './useTransactionDeadline'
+
+interface SwapCall {
+  contract: Contract
+  parameters: SwapParameters
+}
+
+/**
+ * Returns the swap calls that can be used to make the trade
+ * @param trade trade to execute
+ * @param allowedSlippage user allowed slippage
+ * @param recipientAddressOrName
+ */
+export function useSwapCallArguments(
+  trade: Trade | undefined, // trade to execute, required
+  allowedSlippage: number = INITIAL_ALLOWED_SLIPPAGE, // in bips
+  recipientAddressOrName: string | null, // the ENS name or address of the recipient of the trade, or null if swap should be returned to sender
+): SwapCall[] {
+  const { account, chainId, library } = useActiveWeb3React()
+
+  const { address: recipientAddress } = useENS(recipientAddressOrName)
+  const recipient = recipientAddressOrName === null ? account : recipientAddress
+  const deadline = useTransactionDeadline()
+
+  return useMemo(() => {
+    if (!trade || !recipient || !library || !account || !chainId || !deadline) return []
+
+    const contract = getRouterContract(chainId, library, account)
+    if (!contract) {
+      return []
+    }
+
+    const swapMethods = []
+
+    swapMethods.push(
+      Router.swapCallParameters(trade, {
+        feeOnTransfer: false,
+        allowedSlippage: new Percent(JSBI.BigInt(allowedSlippage), BIPS_BASE),
+        recipient,
+        deadline: deadline.toNumber(),
+      }),
+    )
+
+    if (trade.tradeType === TradeType.EXACT_INPUT) {
+      swapMethods.push(
+        Router.swapCallParameters(trade, {
+          feeOnTransfer: true,
+          allowedSlippage: new Percent(JSBI.BigInt(allowedSlippage), BIPS_BASE),
+          recipient,
+          deadline: deadline.toNumber(),
+        }),
+      )
+    }
+
+    return swapMethods.map((parameters) => ({ parameters, contract }))
+  }, [account, allowedSlippage, chainId, deadline, library, recipient, trade])
+}
