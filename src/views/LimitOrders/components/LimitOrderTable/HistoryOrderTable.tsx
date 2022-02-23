c24978303f0e2f1cd70dcedb87405bccdb2497bb
Chef Cheems <chefcheems@protonmail.com>
2022-02-23 08:47:13 +0200

    fix: History page pagination

--- src/views/LimitOrders/components/LimitOrderTable/HistoryOrderTable.tsx
+++ src/views/LimitOrders/components/LimitOrderTable/HistoryOrderTable.tsx
@@ -7,22 +7,43 @@ import { LimitOrderTableProps } from './types'
 import { useTranslation } from 'contexts/Localization'
 import HeaderCellStyle from './HeaderCellStyle'
 import FullRow from './FullRow'
+import { useMemo, useState } from 'react'
+
+const ORDERS_PER_PAGE = 5
 
 const HistoryOrderTable: React.FC<LimitOrderTableProps> = ({ isChartDisplayed, orders }) => {
   const { isTablet } = useMatchBreakpoints()
   const { t } = useTranslation()
+  const [page, setPage] = useState(1)
   const compactMode = !isChartDisplayed || isTablet
+
+  const maxPage = useMemo(() => {
+    if (orders?.length) {
+      return Math.ceil(orders?.length / ORDERS_PER_PAGE)
+    }
+    return 1
+  }, [orders?.length])
 
   if (!orders?.length) {
     return <NoOrderTable />
   }
+
+  const onPageNext = () => {
+    setPage((currentPage) => (currentPage === maxPage ? currentPage : currentPage + 1))
+  }
+
+  const onPagePrev = () => {
+    setPage((currentPage) => (currentPage === 1 ? currentPage : currentPage - 1))
+  }
+
+  const currentPageOrders = orders.slice(ORDERS_PER_PAGE * (page - 1), ORDERS_PER_PAGE * page)
 
   return (
     <>
       <Table>
         {compactMode ? (
           <tbody>
-            {orders.map((order) => (
+            {currentPageOrders.map((order) => (
               <tr key={order.id}>
                 <Td>
                   <CompactRow order={order} />
@@ -53,7 +74,7 @@ const HistoryOrderTable: React.FC<LimitOrderTableProps> = ({ isChartDisplayed, o
           </>
         )}
       </Table>
-      <Navigation currentPage={1} maxPage={1} onPagePrev={() => {}} onPageNext={() => {}} />
+      <Navigation currentPage={page} maxPage={maxPage} onPagePrev={onPagePrev} onPageNext={onPageNext} />
     </>
   )
 }
