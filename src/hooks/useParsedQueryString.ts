14928297fea77c6f19271816abf0a9d7cd53238e
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-02-25 10:47:11 +0800

    feat(swap): Error message and handle (#3318)
    Handle swap error message fix PAN-222
    Remove useParsedQueryString fix PAN-227
    Handle getLiquidityMinted Error fix PAN-220

--- src/hooks/useParsedQueryString.ts
+++
@@ -1,7 +0,0 @@
-import { useRouter } from 'next/router'
-import { ParsedUrlQuery } from 'querystring'
-
-export default function useParsedQueryString(): ParsedUrlQuery {
-  const { query } = useRouter()
-  return query
-}
