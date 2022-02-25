5320eb628ce2f445c8a9f00e53d5d8644811a244
Chef JoJo <94336009+chef-jojo@users.noreply.github.com>
2022-02-25 12:47:39 +0800

    perf(nft): Add Infinate scroll to profile nft

--- src/views/Nft/market/Profile/components/UnconnectedProfileNfts.tsx
+++ src/views/Nft/market/Profile/components/UnconnectedProfileNfts.tsx
@@ -1,4 +1,6 @@
-import { Grid, Text, Flex } from '@pancakeswap/uikit'
+import { Grid, Text, Flex, Spinner } from '@pancakeswap/uikit'
+import { useEffect, useState } from 'react'
+import useIntersectionObserver from 'hooks/useIntersectionObserver'
 import { NftToken } from 'state/nftMarket/types'
 import { useTranslation } from 'contexts/Localization'
 import { CollectibleLinkCard } from '../../components/CollectibleCard'
@@ -7,6 +9,17 @@ import NoNftsImage from '../../components/Activity/NoNftsImage'
 
 const UserNfts: React.FC<{ nfts: NftToken[]; isLoading: boolean }> = ({ nfts, isLoading }) => {
   const { t } = useTranslation()
+  const [showMaxItems, setMaxShowItems] = useState(100)
+
+  const { isIntersecting, observerRef } = useIntersectionObserver()
+
+  const isNotLastPage = nfts.length > showMaxItems
+
+  useEffect(() => {
+    if (isIntersecting) {
+      setMaxShowItems((s) => s + 100)
+    }
+  }, [isIntersecting])
 
   return (
     <>
@@ -20,25 +33,32 @@ const UserNfts: React.FC<{ nfts: NftToken[]; isLoading: boolean }> = ({ nfts, is
         </Flex>
       ) : // User has NFTs and data has been fetched
       nfts.length > 0 ? (
-        <Grid
-          gridGap="16px"
-          gridTemplateColumns={['1fr', 'repeat(2, 1fr)', 'repeat(3, 1fr)', null, 'repeat(4, 1fr)']}
-          alignItems="start"
-        >
-          {nfts.map((nft) => {
-            const { marketData } = nft
+        <>
+          <Grid
+            gridGap="16px"
+            gridTemplateColumns={['1fr', 'repeat(2, 1fr)', 'repeat(3, 1fr)', null, 'repeat(4, 1fr)']}
+            alignItems="start"
+          >
+            {nfts.slice(0, showMaxItems).map((nft) => {
+              const { marketData } = nft
 
-            return (
-              <CollectibleLinkCard
-                key={`${nft.tokenId}-${nft.collectionName}`}
-                nft={nft}
-                currentAskPrice={
-                  marketData?.currentAskPrice && marketData?.isTradable && parseFloat(marketData.currentAskPrice)
-                }
-              />
-            )
-          })}
-        </Grid>
+              return (
+                <CollectibleLinkCard
+                  key={`${nft.tokenId}-${nft.collectionName}`}
+                  nft={nft}
+                  currentAskPrice={
+                    marketData?.currentAskPrice && marketData?.isTradable && parseFloat(marketData.currentAskPrice)
+                  }
+                />
+              )
+            })}
+          </Grid>
+          {isNotLastPage && (
+            <Flex mt="60px" mb="12px" justifyContent="center" ref={observerRef}>
+              <Spinner />
+            </Flex>
+          )}
+        </>
       ) : (
         // User NFT data hasn't been fetched
         <GridPlaceholder />
