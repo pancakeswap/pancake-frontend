4ed9f73d1c344d4db9a87fb3164fa3bd763f595e
Chef Cheems <chefcheems@protonmail.com>
2022-02-28 08:01:27 +0200

    fix: Optional argument

--- src/views/Swap/components/Chart/styles.tsx
+++ src/views/Swap/components/Chart/styles.tsx
@@ -1,7 +1,11 @@
 import { Box } from '@pancakeswap/uikit'
 import styled from 'styled-components'
 
-export const StyledPriceChart = styled(Box)<{ $isDark: boolean; $isExpanded: boolean; $isFullWidthContainer: boolean }>`
+export const StyledPriceChart = styled(Box)<{
+  $isDark: boolean
+  $isExpanded: boolean
+  $isFullWidthContainer?: boolean
+}>`
   border: none;
   border-radius: 32px;
   width: 100%;
