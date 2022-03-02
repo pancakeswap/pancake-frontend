1f76a16d1d7b81e1dc379188f0195731b6bae97e
memoyil <2213635+memoyil@users.noreply.github.com>
2022-03-02 14:37:08 +0100

    fix: (nft): Missing items in Collection listing (#3115)

--- src/views/Nft/market/Collection/Items/CollectionNfts.tsx
+++ src/views/Nft/market/Collection/Items/CollectionNfts.tsx
@@ -1,168 +1,75 @@
-import { useEffect, useState } from 'react'
-import uniqBy from 'lodash/uniqBy'
-import { AutoRenewIcon, Button, Flex, Grid, Text } from '@pancakeswap/uikit'
-import { useAppDispatch } from 'state'
-import {
-  useGetNftFilterLoadingState,
-  useGetNftOrdering,
-  useGetNftShowOnlyOnSale,
-  useNftsFromCollection,
-} from 'state/nftMarket/hooks'
-import { Collection, NftToken, TokenMarketData } from 'state/nftMarket/types'
-import { fetchNftsFromCollections } from 'state/nftMarket/reducer'
-import { getNftApi, getNftsMarketData } from 'state/nftMarket/helpers'
+import { useCallback } from 'react'
+import { BunnyPlaceholderIcon, AutoRenewIcon, Button, Flex, Grid, Text } from '@pancakeswap/uikit'
+import { Collection } from 'state/nftMarket/types'
 import { useTranslation } from 'contexts/Localization'
-import { FetchStatus } from 'config/constants/types'
 import GridPlaceholder from '../../components/GridPlaceholder'
 import { CollectibleLinkCard } from '../../components/CollectibleCard'
-import { REQUEST_SIZE } from '../config'
+import { useCollectionNfts } from '../../hooks/useCollectionNfts'
 
 interface CollectionNftsProps {
   collection: Collection
 }
 
 const CollectionNfts: React.FC<CollectionNftsProps> = ({ collection }) => {
-  const { totalSupply, numberTokensListed, address: collectionAddress } = collection
-  const [page, setPage] = useState(1)
-  const [skip, setSkip] = useState(0)
-  const [nfts, setNfts] = useState<NftToken[]>([])
-  const [isFetchingFilteredNfts, setIsFetchingFilteredNfts] = useState(false)
+  const { address: collectionAddress } = collection
   const { t } = useTranslation()
-  const collectionNfts = useNftsFromCollection(collectionAddress)
-  const nftFilterLoadingState = useGetNftFilterLoadingState(collectionAddress)
-  const dispatch = useAppDispatch()
+  const { nfts, isFetchingNfts, page, setPage, resultSize, isLastPage } = useCollectionNfts(collectionAddress)
 
-  const showOnlyNftsOnSale = useGetNftShowOnlyOnSale(collectionAddress)
-  const { field: orderField, direction: orderDirection } = useGetNftOrdering(collectionAddress)
-  const isFetching = orderField === 'tokenId' ? nftFilterLoadingState === FetchStatus.Fetching : isFetchingFilteredNfts
+  const handleLoadMore = useCallback(() => {
+    setPage(page + 1)
+  }, [setPage, page])
 
-  const handleLoadMore = () => {
-    if (orderField === 'tokenId') {
-      setPage((prevPage) => prevPage + 1)
-    }
-    setSkip(skip + REQUEST_SIZE)
-  }
-
-  useEffect(() => {
-    if (orderField === 'tokenId') {
-      setPage(1)
-    }
-  }, [orderField])
-
-  useEffect(() => {
-    setNfts([])
-    setSkip(0)
-  }, [orderField, orderDirection])
-
-  useEffect(() => {
-    const fetchApiData = async (marketData: TokenMarketData[]) => {
-      const apiRequestPromises = marketData.map((marketNft) => getNftApi(collectionAddress, marketNft.tokenId))
-      const apiResponses = await Promise.all(apiRequestPromises)
-      const responsesWithMarketData = apiResponses.reduce((acc, apiNft, i) => {
-        if (apiNft) {
-          acc.push({
-            ...apiNft,
-            collectionAddress,
-            collectionName: apiNft.collection.name,
-            marketData: marketData[i],
-          })
-        }
-        return acc
-      }, [])
-      setIsFetchingFilteredNfts(false)
-      setNfts((prevState) => {
-        const combinedNfts = [...prevState, ...responsesWithMarketData]
-        return uniqBy(combinedNfts, 'tokenId')
-      })
-    }
-
-    const fetchMarketData = async () => {
-      const subgraphRes = await getNftsMarketData(
-        { collection: collectionAddress.toLowerCase(), isTradable: true },
-        REQUEST_SIZE,
-        orderField,
-        orderDirection,
-        skip,
-      )
-      fetchApiData(subgraphRes)
-    }
-
-    if (orderField !== 'tokenId') {
-      // Query by tokenId is handled in useEffect below since we in this case
-      // we need to show all NFTs, even those that never been on sale (i.e. they are not in subgraph)
-      setIsFetchingFilteredNfts(true)
-      fetchMarketData()
-    }
-  }, [orderField, orderDirection, skip, collectionAddress])
-
-  useEffect(() => {
-    if (orderField === 'tokenId') {
-      dispatch(
-        fetchNftsFromCollections({
-          collectionAddress,
-          page,
-          size: REQUEST_SIZE,
-        }),
-      )
-    }
-  }, [page, collectionAddress, dispatch, orderField])
-
-  const nftsToShow =
-    orderField === 'tokenId'
-      ? collectionNfts?.filter((nft) => {
-          if (showOnlyNftsOnSale) {
-            return nft.marketData?.isTradable
-          }
-          return true
-        })
-      : nfts
-
-  if (!nftsToShow || nftsToShow?.length === 0) {
+  if ((!nfts || nfts?.length === 0) && isFetchingNfts) {
     return <GridPlaceholder />
   }
 
-  const isNotLastPage =
-    showOnlyNftsOnSale || orderField !== 'tokenId'
-      ? nftsToShow?.length < Number(numberTokensListed)
-      : nftsToShow?.length < Number(totalSupply)
-
-  const resultsAmount = showOnlyNftsOnSale || orderField !== 'tokenId' ? numberTokensListed : totalSupply
-
   return (
     <>
-      <Flex p="16px">
-        <Text bold>
-          {resultsAmount} {t('Results')}
-        </Text>
-      </Flex>
-      <Grid
-        gridGap="16px"
-        gridTemplateColumns={['1fr', null, 'repeat(3, 1fr)', null, 'repeat(4, 1fr)']}
-        alignItems="start"
-      >
-        {nftsToShow.map((nft) => {
-          const currentAskPriceAsNumber = nft.marketData && parseFloat(nft.marketData.currentAskPrice)
-
-          return (
-            <CollectibleLinkCard
-              key={nft.tokenId}
-              nft={nft}
-              currentAskPrice={currentAskPriceAsNumber > 0 ? currentAskPriceAsNumber : undefined}
-            />
-          )
-        })}
-      </Grid>
-      <Flex mt="60px" mb="12px" justifyContent="center">
-        {isNotLastPage && (
-          <Button
-            onClick={handleLoadMore}
-            scale="sm"
-            endIcon={isFetching ? <AutoRenewIcon spin color="currentColor" /> : undefined}
+      {resultSize && (
+        <Flex p="16px">
+          <Text bold>
+            {resultSize} {t('Results')}
+          </Text>
+        </Flex>
+      )}
+      {nfts.length > 0 ? (
+        <>
+          <Grid
+            gridGap="16px"
+            gridTemplateColumns={['1fr', null, 'repeat(3, 1fr)', null, 'repeat(4, 1fr)']}
+            alignItems="start"
           >
-            {isFetching ? t('Loading') : t('Load more')}
-          </Button>
-        )}
-      </Flex>
+            {nfts.map((nft) => {
+              const currentAskPriceAsNumber = nft.marketData && parseFloat(nft.marketData.currentAskPrice)
+
+              return (
+                <CollectibleLinkCard
+                  key={nft.tokenId}
+                  nft={nft}
+                  currentAskPrice={currentAskPriceAsNumber > 0 ? currentAskPriceAsNumber : undefined}
+                />
+              )
+            })}
+          </Grid>
+          <Flex mt="60px" mb="12px" justifyContent="center">
+            {!isLastPage && (
+              <Button
+                onClick={handleLoadMore}
+                scale="sm"
+                disabled={isFetchingNfts}
+                endIcon={isFetchingNfts ? <AutoRenewIcon spin color="currentColor" /> : undefined}
+              >
+                {isFetchingNfts ? t('Loading') : t('Load more')}
+              </Button>
+            )}
+          </Flex>
+        </>
+      ) : (
+        <Flex alignItems="center" py="48px" flexDirection="column">
+          <BunnyPlaceholderIcon width="96px" mb="24px" />
+          <Text fontWeight={600}>{t('No NFTs found')}</Text>
+        </Flex>
+      )}
     </>
   )
 }
