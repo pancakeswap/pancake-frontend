e637b0f3da05487944c510b99162bad0040a4a58
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-03-02 18:27:05 +0800

    chore: Update wallet config

--- packages/uikit/src/widgets/WalletModal/config.tsx
+++ packages/uikit/src/widgets/WalletModal/config.tsx
@@ -19,10 +19,10 @@ const connectors: Config[] = [
     priority: 1,
   },
   {
-    title: "WalletConnect",
-    icon: WalletConnect,
-    connectorId: ConnectorNames.WalletConnect,
-    priority: 2,
+    title: "Binance Wallet",
+    icon: BinanceChain,
+    connectorId: ConnectorNames.BSC,
+    priority: 999,
   },
   {
     title: "Coinbase Wallet",
@@ -37,6 +37,12 @@ const connectors: Config[] = [
     priority: 4,
   },
   {
+    title: "WalletConnect",
+    icon: WalletConnect,
+    connectorId: ConnectorNames.WalletConnect,
+    priority: 5,
+  },
+  {
     title: "MathWallet",
     icon: MathWallet,
     connectorId: ConnectorNames.Injected,
@@ -46,13 +52,6 @@ const connectors: Config[] = [
     title: "TokenPocket",
     icon: TokenPocket,
     connectorId: ConnectorNames.Injected,
-    priority: 999,
-  },
-
-  {
-    title: "BNB Smart Chain",
-    icon: BinanceChain,
-    connectorId: ConnectorNames.BSC,
     priority: 999,
   },
   {
