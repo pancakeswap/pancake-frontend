2e19905c564272c12118f2d8506c2a280843b9df
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2022-03-04 10:18:26 +0800

    feat: IFO Banner

--- src/views/Home/components/Banners/IFOBanner.tsx
+++ src/views/Home/components/Banners/IFOBanner.tsx
@@ -1,66 +1,14 @@
-import { memo } from 'react'
-import styled from 'styled-components'
-import { Text, Flex, Button, ArrowForwardIcon, Heading } from '@pancakeswap/uikit'
-import { useActiveIfoWithBlocks } from 'hooks/useActiveIfoWithBlocks'
+import { ArrowForwardIcon, Button, Text } from '@pancakeswap/uikit'
 import { NextLinkFromReactRouter } from 'components/NextLink'
 import { useTranslation } from 'contexts/Localization'
+import { useActiveIfoWithBlocks } from 'hooks/useActiveIfoWithBlocks'
+import Image from 'next/image'
+import { memo } from 'react'
 import { useCurrentBlock } from 'state/block/hooks'
+import styled from 'styled-components'
 import { getStatus } from '../../../Ifos/hooks/helpers'
-
-const StyledSubheading = styled(Heading)`
-  background: -webkit-linear-gradient(#ffd800, #eb8c00);
-  font-size: 20px;
-  background-clip: text;
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  -webkit-text-stroke: 1px rgba(0, 0, 0, 0.5);
-  ${({ theme }) => theme.mediaQueries.xs} {
-    font-size: 24px;
-  }
-  ${({ theme }) => theme.mediaQueries.sm} {
-    -webkit-text-stroke: unset;
-  }
-  margin-bottom: 8px;
-`
-
-const StyledHeading = styled(Heading)`
-  color: #ffffff;
-  background: -webkit-linear-gradient(#7645d9 0%, #452a7a 100%);
-  background-clip: text;
-  -webkit-background-clip: text;
-  -webkit-text-stroke: 6px transparent;
-  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  text-transform: uppercase;
-  margin-bottom: 16px;
-`
-
-const Wrapper = styled.div`
-  border-radius: 32px;
-  width: 100%;
-  background-image: linear-gradient(#7645d9, #452a7a);
-  max-height: max-content;
-  overflow: hidden;
-`
-
-const Inner = styled(Flex)`
-  position: relative;
-  padding: 24px;
-  flex-direction: row;
-  justify-content: space-between;
-  max-height: 220px;
-`
-
-const LeftWrapper = styled(Flex)`
-  z-index: 1;
-  width: 100%;
-  flex-direction: column;
-  justify-content: center;
-
-  ${({ theme }) => theme.mediaQueries.md} {
-    padding-top: 40px;
-    padding-bottom: 40px;
-  }
-`
+import { IFOImage } from './images'
+import * as S from './Styled'
 
 const RightWrapper = styled.div`
   position: absolute;
@@ -69,25 +17,22 @@ const RightWrapper = styled.div`
   transform: translate(0, -50%);
   top: 50%;
 
-  & img {
+  img {
     height: 100%;
-    width: 174px;
+    width: 500px;
   }
 
   ${({ theme }) => theme.mediaQueries.sm} {
-    position: relative;
+    position: absolute;
     display: flex;
     align-items: center;
     justify-content: flex-end;
     right: 24px;
-    top: 0;
+    bottom: 0;
     transform: unset;
     opacity: 1;
-
-    & img {
-      height: 130%;
-      width: unset;
-    }
+    top: unset;
+    height: 211px;
   }
 `
 const IFOBanner = () => {
@@ -102,11 +47,11 @@ const IFOBanner = () => {
     : null
 
   return isIfoAlive && status ? (
-    <Wrapper>
-      <Inner>
-        <LeftWrapper>
-          <StyledSubheading>{status === 'live' ? t('Live') : t('Soon')}</StyledSubheading>
-          <StyledHeading scale="xl">{activeIfoWithBlocks.id} IFO</StyledHeading>
+    <S.Wrapper>
+      <S.Inner>
+        <S.LeftWrapper>
+          <S.StyledSubheading>{status === 'live' ? t('Live') : t('Soon')}</S.StyledSubheading>
+          <S.StyledHeading scale="xl">{activeIfoWithBlocks.id} IFO</S.StyledHeading>
           <NextLinkFromReactRouter to="/ifo">
             <Button>
               <Text color="invertedContrast" bold fontSize="16px" mr="4px">
@@ -115,20 +60,23 @@ const IFOBanner = () => {
               <ArrowForwardIcon color="invertedContrast" />
             </Button>
           </NextLinkFromReactRouter>
-        </LeftWrapper>
+        </S.LeftWrapper>
         <RightWrapper>
-          <img
-            src={`/images/decorations/3d-ifo-${activeIfoWithBlocks.id}.png`}
+          <Image
+            src={IFOImage}
             alt={`IFO ${activeIfoWithBlocks.id}`}
             onError={(event) => {
               // @ts-ignore
               // eslint-disable-next-line no-param-reassign
               event.target.style.display = 'none'
             }}
+            width={291}
+            height={211}
+            placeholder="blur"
           />
         </RightWrapper>
-      </Inner>
-    </Wrapper>
+      </S.Inner>
+    </S.Wrapper>
   ) : null
 }
 
