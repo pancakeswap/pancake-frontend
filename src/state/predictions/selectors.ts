6250e602e4ed202ff86f1005a398099d2c491e0b
memoyil <2213635+memoyil@users.noreply.github.com>
2022-03-07 14:32:55 +0100

    perf: Use selectors on prediction hooks

---
+++ src/state/predictions/selectors.ts
@@ -0,0 +1,107 @@
+import { BigNumber } from '@ethersproject/bignumber'
+import orderBy from 'lodash/orderBy'
+import { createSelector } from '@reduxjs/toolkit'
+import { State, ReduxNodeRound, NodeRound, ReduxNodeLedger, NodeLedger } from '../types'
+import { parseBigNumberObj } from './helpers'
+
+const selectCurrentEpoch = (state: State) => state.predictions.currentEpoch
+const selectRounds = (state: State) => state.predictions.rounds
+const selectRound = (epoch) => (state: State) => state.predictions.rounds[epoch]
+const selectLedgers = (state: State) => state.predictions.ledgers
+const selectClaimableStatuses = (state: State) => state.predictions.claimableStatuses
+const selectMinBetAmount = (state: State) => state.predictions.minBetAmount
+const selectIntervalSeconds = (state: State) => state.predictions.intervalSeconds
+
+export const makeGetRoundSelector = (epoch: number) =>
+  createSelector([selectRound(epoch)], (round) => parseBigNumberObj<ReduxNodeRound, NodeRound>(round))
+
+export const makeGetBetByEpochSelector = (account: string, epoch: number) =>
+  createSelector([selectLedgers], (bets) => {
+    if (!bets[account]) {
+      return null
+    }
+
+    if (!bets[account][epoch]) {
+      return null
+    }
+
+    return parseBigNumberObj<ReduxNodeLedger, NodeLedger>(bets[account][epoch])
+  })
+
+export const makeGetIsClaimableSelector = (epoch: number) =>
+  createSelector([selectClaimableStatuses], (claimableStatuses) => {
+    return claimableStatuses[epoch] || false
+  })
+
+export const getRoundsByCloseOracleIdSelector = createSelector([selectRounds], (rounds) => {
+  return Object.keys(rounds).reduce((accum, epoch) => {
+    const parsed = parseBigNumberObj<ReduxNodeRound, NodeRound>(rounds[epoch])
+    return {
+      ...accum,
+      [parsed.closeOracleId]: parsed,
+    }
+  }, {}) as { [key: string]: NodeRound }
+})
+
+export const getRoundsSelector = createSelector([selectRounds], (rounds) => {
+  return Object.keys(rounds).reduce((accum, epoch) => {
+    return {
+      ...accum,
+      [epoch]: parseBigNumberObj<ReduxNodeRound, NodeRound>(rounds[epoch]),
+    }
+  }, {}) as { [key: string]: NodeRound }
+})
+
+export const getSortedRoundsSelector = createSelector([selectRounds], (rounds) => {
+  return orderBy(
+    Object.values(
+      Object.keys(rounds).reduce((accum, epoch) => {
+        return {
+          ...accum,
+          [epoch]: parseBigNumberObj<ReduxNodeRound, NodeRound>(rounds[epoch]),
+        }
+      }, {}) as { [key: string]: NodeRound },
+    ),
+    ['epoch'],
+    ['asc'],
+  )
+})
+
+export const getCurrentRoundSelector = createSelector([selectCurrentEpoch, selectRounds], (currentEpoch, rounds) => {
+  const roundData = Object.keys(rounds).reduce((accum, epoch) => {
+    return {
+      ...accum,
+      [epoch]: parseBigNumberObj<ReduxNodeRound, NodeRound>(rounds[epoch]),
+    }
+  }, {}) as { [key: string]: NodeRound }
+
+  return roundData[currentEpoch]
+})
+
+export const getMinBetAmountSelector = createSelector([selectMinBetAmount], (minBetAmount) => {
+  return BigNumber.from(minBetAmount)
+})
+
+export const getCurrentRoundLockTimestampSelector = createSelector(
+  [selectCurrentEpoch, selectRounds, selectIntervalSeconds],
+  (currentEpoch, rounds, intervalSeconds) => {
+    const roundData = Object.keys(rounds).reduce((accum, epoch) => {
+      return {
+        ...accum,
+        [epoch]: parseBigNumberObj<ReduxNodeRound, NodeRound>(rounds[epoch]),
+      }
+    }, {}) as { [key: string]: NodeRound }
+
+    const currentRound = roundData[currentEpoch]
+
+    if (!currentRound) {
+      return undefined
+    }
+
+    if (!currentRound.lockTimestamp) {
+      return currentRound.startTimestamp + intervalSeconds
+    }
+
+    return currentRound.lockTimestamp
+  },
+)
