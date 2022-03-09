86f30f2e318014a0624fca815901ae705ec992a2
kim-pink-ba <kimpinkba@gmail.com>
2022-03-09 18:56:58 +0800

    feat(swap): Remove top-100

---
+++ src/config/constants/lists.bmp.ts
@@ -0,0 +1,12 @@
+const PANCAKE_EXTENDED = 'https://tokens.pancakeswap.finance/pancakeswap-extended.json'
+
+export const UNSUPPORTED_LIST_URLS: string[] = []
+
+// lower index == higher priority for token import
+export const DEFAULT_LIST_OF_LISTS: string[] = [
+  PANCAKE_EXTENDED,
+  ...UNSUPPORTED_LIST_URLS, // need to load unsupported tokens as well
+]
+
+// default lists to be 'active' aka searched across
+export const DEFAULT_ACTIVE_LIST_URLS: string[] = []
