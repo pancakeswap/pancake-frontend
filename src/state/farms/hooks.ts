5b25317551c8c191f84e1a9c32c68f27df261a21
memoyil <2213635+memoyil@users.noreply.github.com>
2022-03-10 07:51:02 +0100

    perf: Use selectors for farms (#3422)
    * perf: Use selectors for farms
    
    * refactor: Use selectFarmByKey

--- src/state/farms/hooks.ts
+++ src/state/farms/hooks.ts
@@ -7,43 +7,17 @@ import { useFastRefreshEffect, useSlowRefreshEffect } from 'hooks/useRefreshEffe
 import { useMemo } from 'react'
 import { useSelector } from 'react-redux'
 import { useAppDispatch } from 'state'
-import { deserializeToken } from 'state/user/hooks/helpers'
-import { BIG_ZERO } from 'utils/bigNumber'
-import { getBalanceAmount } from 'utils/formatBalance'
 import { fetchFarmsPublicDataAsync, fetchFarmUserDataAsync, nonArchivedFarms } from '.'
-import { DeserializedFarm, DeserializedFarmsState, DeserializedFarmUserData, SerializedFarm, State } from '../types'
-
-const deserializeFarmUserData = (farm: SerializedFarm): DeserializedFarmUserData => {
-  return {
-    allowance: farm.userData ? new BigNumber(farm.userData.allowance) : BIG_ZERO,
-    tokenBalance: farm.userData ? new BigNumber(farm.userData.tokenBalance) : BIG_ZERO,
-    stakedBalance: farm.userData ? new BigNumber(farm.userData.stakedBalance) : BIG_ZERO,
-    earnings: farm.userData ? new BigNumber(farm.userData.earnings) : BIG_ZERO,
-  }
-}
-
-const deserializeFarm = (farm: SerializedFarm): DeserializedFarm => {
-  const { lpAddresses, lpSymbol, pid, dual, multiplier, isCommunity, quoteTokenPriceBusd, tokenPriceBusd } = farm
-
-  return {
-    lpAddresses,
-    lpSymbol,
-    pid,
-    dual,
-    multiplier,
-    isCommunity,
-    quoteTokenPriceBusd,
-    tokenPriceBusd,
-    token: deserializeToken(farm.token),
-    quoteToken: deserializeToken(farm.quoteToken),
-    userData: deserializeFarmUserData(farm),
-    tokenAmountTotal: farm.tokenAmountTotal ? new BigNumber(farm.tokenAmountTotal) : BIG_ZERO,
-    lpTotalInQuoteToken: farm.lpTotalInQuoteToken ? new BigNumber(farm.lpTotalInQuoteToken) : BIG_ZERO,
-    lpTotalSupply: farm.lpTotalSupply ? new BigNumber(farm.lpTotalSupply) : BIG_ZERO,
-    tokenPriceVsQuote: farm.tokenPriceVsQuote ? new BigNumber(farm.tokenPriceVsQuote) : BIG_ZERO,
-    poolWeight: farm.poolWeight ? new BigNumber(farm.poolWeight) : BIG_ZERO,
-  }
-}
+import { DeserializedFarm, DeserializedFarmsState, DeserializedFarmUserData, State } from '../types'
+import {
+  farmSelector,
+  farmFromLpSymbolSelector,
+  priceCakeFromPidSelector,
+  makeBusdPriceFromPidSelector,
+  makeUserFarmFromPidSelector,
+  makeLpTokenPriceFromLpSymbolSelector,
+  makeFarmFromPidSelector,
+} from './selectors'
 
 export const usePollFarmsWithUserData = (includeArchive = false) => {
   const dispatch = useAppDispatch()
@@ -76,15 +50,7 @@ export const usePollCoreFarmData = () => {
 }
 
 export const useFarms = (): DeserializedFarmsState => {
-  const farms = useSelector((state: State) => state.farms)
-  const deserializedFarmsData = farms.data.map(deserializeFarm)
-  const { loadArchivedFarmsData, userDataLoaded, poolLength } = farms
-  return {
-    loadArchivedFarmsData,
-    userDataLoaded,
-    data: deserializedFarmsData,
-    poolLength,
-  }
+  return useSelector(farmSelector)
 }
 
 export const useFarmsPoolLength = (): number => {
@@ -92,61 +58,34 @@ export const useFarmsPoolLength = (): number => {
 }
 
 export const useFarmFromPid = (pid: number): DeserializedFarm => {
-  const farm = useSelector((state: State) => state.farms.data.find((f) => f.pid === pid))
-  return deserializeFarm(farm)
+  const farmFromPid = useMemo(() => makeFarmFromPidSelector(pid), [pid])
+  return useSelector(farmFromPid)
 }
 
 export const useFarmFromLpSymbol = (lpSymbol: string): DeserializedFarm => {
-  const farm = useSelector((state: State) => state.farms.data.find((f) => f.lpSymbol === lpSymbol))
-  return deserializeFarm(farm)
+  const farmFromLpSymbol = useMemo(() => farmFromLpSymbolSelector(lpSymbol), [lpSymbol])
+  return useSelector(farmFromLpSymbol)
 }
 
 export const useFarmUser = (pid): DeserializedFarmUserData => {
-  const { userData } = useFarmFromPid(pid)
-  const { allowance, tokenBalance, stakedBalance, earnings } = userData
-  return {
-    allowance,
-    tokenBalance,
-    stakedBalance,
-    earnings,
-  }
+  const farmFromPidUser = useMemo(() => makeUserFarmFromPidSelector(pid), [pid])
+  return useSelector(farmFromPidUser)
 }
 
 // Return the base token price for a farm, from a given pid
 export const useBusdPriceFromPid = (pid: number): BigNumber => {
-  const farm = useFarmFromPid(pid)
-  return farm && new BigNumber(farm.tokenPriceBusd)
+  const busdPriceFromPid = useMemo(() => makeBusdPriceFromPidSelector(pid), [pid])
+  return useSelector(busdPriceFromPid)
 }
 
 export const useLpTokenPrice = (symbol: string) => {
-  const farm = useFarmFromLpSymbol(symbol)
-  const farmTokenPriceInUsd = useBusdPriceFromPid(farm.pid)
-  let lpTokenPrice = BIG_ZERO
-
-  if (farm.lpTotalSupply.gt(0) && farm.lpTotalInQuoteToken.gt(0)) {
-    // Total value of base token in LP
-    const valueOfBaseTokenInFarm = farmTokenPriceInUsd.times(farm.tokenAmountTotal)
-    // Double it to get overall value in LP
-    const overallValueOfAllTokensInFarm = valueOfBaseTokenInFarm.times(2)
-    // Divide total value of all tokens, by the number of LP tokens
-    const totalLpTokens = getBalanceAmount(farm.lpTotalSupply)
-    lpTokenPrice = overallValueOfAllTokensInFarm.div(totalLpTokens)
-  }
-
-  return lpTokenPrice
+  const lpTokenPriceFromLpSymbol = useMemo(() => makeLpTokenPriceFromLpSymbolSelector(symbol), [symbol])
+  return useSelector(lpTokenPriceFromLpSymbol)
 }
 
 /**
  * @@deprecated use the BUSD hook in /hooks
  */
 export const usePriceCakeBusd = (): BigNumber => {
-  const cakeBnbFarm = useFarmFromPid(251)
-
-  const cakePriceBusdAsString = cakeBnbFarm.tokenPriceBusd
-
-  const cakePriceBusd = useMemo(() => {
-    return new BigNumber(cakePriceBusdAsString)
-  }, [cakePriceBusdAsString])
-
-  return cakePriceBusd
+  return useSelector(priceCakeFromPidSelector)
 }
