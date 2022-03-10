24f45d09c67dd38fed1836db6ac1ae7d220826ed
memoyil <2213635+memoyil@users.noreply.github.com>
2022-03-10 14:10:58 +0100

    fix: (limit-orders): Order still visible after executed in open orders tab (#3435)
    * fix: Order still visible after executed in open orders tab
    
    * feat: Add saveorders and removeorders
    
    * refactor: Optimize array exist check
    
    * fix: Handle sync per type/s
    
    * refactor: Use enums
    
    * refactor: Move enum to type

--- src/views/LimitOrders/hooks/useGelatoLimitOrdersHistory.ts
+++ src/views/LimitOrders/hooks/useGelatoLimitOrdersHistory.ts
@@ -4,23 +4,39 @@ import useSWR from 'swr'
 import { SLOW_INTERVAL } from 'config/constants'
 import { useMemo } from 'react'
 
-import { getLSOrders, saveOrder } from 'utils/localStorageOrders'
+import { getLSOrders, saveOrder, removeOrder, saveOrders, hashOrderSet, hashOrder } from 'utils/localStorageOrders'
 import useGelatoLimitOrdersLib from 'hooks/limitOrders/useGelatoLimitOrdersLib'
 
-import { ORDER_CATEGORY } from '../types'
+import { ORDER_CATEGORY, LimitOrderType } from '../types'
 
 function newOrdersFirst(a: Order, b: Order) {
   return Number(b.updatedAt) - Number(a.updatedAt)
 }
 
-function syncOrderToLocalStorage({ chainId, account, orders }) {
-  const ordersLS = getLSOrders(chainId, account)
+function syncOrderToLocalStorage({
+  chainId,
+  account,
+  orders,
+  types,
+}: {
+  chainId: number
+  account: string
+  orders: Order[]
+  types: LimitOrderType[]
+}) {
+  const ordersLS = getLSOrders(chainId, account).filter((order) => types.some((type) => type === order.status))
 
-  orders.forEach((order: Order) => {
-    const orderExists = ordersLS.find((confOrder) => confOrder.id.toLowerCase() === order.id.toLowerCase())
+  const ordersLSHashSet = hashOrderSet(ordersLS)
+  const newOrders = orders.filter((order: Order) => !ordersLSHashSet.has(hashOrder(order)))
+  saveOrders(chainId, account, newOrders)
 
-    if (!orderExists || (orderExists && Number(orderExists.updatedAt) < Number(order.updatedAt))) {
-      saveOrder(chainId, account, order)
+  ordersLS.forEach((confOrder: Order) => {
+    const updatedOrder = orders.find((order) => confOrder.id.toLowerCase() === order.id.toLowerCase())
+
+    if (updatedOrder && Number(confOrder.updatedAt) < Number(updatedOrder.updatedAt)) {
+      saveOrder(chainId, account, updatedOrder)
+    } else if (!updatedOrder) {
+      removeOrder(chainId, account, confOrder)
     }
   })
 }
@@ -42,23 +58,24 @@ const useOpenOrders = (turnOn: boolean): Order[] => {
           orders,
           chainId,
           account,
+          types: [LimitOrderType.OPEN],
         })
       } catch (e) {
         console.error('Error fetching open orders from subgraph', e)
       }
 
-      const openOrdersLS = getLSOrders(chainId, account).filter((order) => order.status === 'open')
+      const openOrdersLS = getLSOrders(chainId, account).filter((order) => order.status === LimitOrderType.OPEN)
 
       const pendingOrdersLS = getLSOrders(chainId, account, true)
 
       return [
         ...openOrdersLS.filter((order: Order) => {
           const orderCancelled = pendingOrdersLS
-            .filter((pendingOrder) => pendingOrder.status === 'cancelled')
+            .filter((pendingOrder) => pendingOrder.status === LimitOrderType.CANCELLED)
             .find((pendingOrder) => pendingOrder.id.toLowerCase() === order.id.toLowerCase())
           return !orderCancelled
         }),
-        ...pendingOrdersLS.filter((order) => order.status === 'open'),
+        ...pendingOrdersLS.filter((order) => order.status === LimitOrderType.OPEN),
       ].sort(newOrdersFirst)
     },
     {
@@ -76,7 +93,7 @@ const useHistoryOrders = (turnOn: boolean): Order[] => {
   const startFetch = turnOn && gelatoLimitOrders && account && chainId
 
   const { data } = useSWR(
-    startFetch ? ['gelato', 'cancelledOrders'] : null,
+    startFetch ? ['gelato', 'cancelledExecutedOrders'] : null,
     async () => {
       try {
         const acc = account.toLowerCase()
@@ -90,14 +107,17 @@ const useHistoryOrders = (turnOn: boolean): Order[] => {
           orders: [...canOrders, ...exeOrders],
           chainId,
           account,
+          types: [LimitOrderType.CANCELLED, LimitOrderType.EXECUTED],
         })
       } catch (e) {
         console.error('Error fetching history orders from subgraph', e)
       }
 
-      const executedOrdersLS = getLSOrders(chainId, account).filter((order) => order.status === 'executed')
+      const executedOrdersLS = getLSOrders(chainId, account).filter((order) => order.status === LimitOrderType.EXECUTED)
 
-      const cancelledOrdersLS = getLSOrders(chainId, account).filter((order) => order.status === 'cancelled')
+      const cancelledOrdersLS = getLSOrders(chainId, account).filter(
+        (order) => order.status === LimitOrderType.CANCELLED,
+      )
 
       const pendingCancelledOrdersLS = getLSOrders(chainId, account, true).filter(
         (order) => order.status === 'cancelled',
