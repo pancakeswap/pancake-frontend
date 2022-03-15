c22d4a58d9fad322420e32f3de5de26744612cd4
Chef Momota <chefmomota@chefmomotas-MacBook-Pro.local>
2022-03-15 18:06:01 +0800

    feat: AprRow import Apr component to avoid duplication

--- src/views/Pools/components/PoolCard/AprRow.tsx
+++ src/views/Pools/components/PoolCard/AprRow.tsx
@@ -1,47 +1,19 @@
-import styled from 'styled-components'
-import { Text, Flex, TooltipText, IconButton, useModal, CalculateIcon, Skeleton, useTooltip } from '@pancakeswap/uikit'
+import { Flex, TooltipText, useTooltip } from '@pancakeswap/uikit'
 import { useTranslation } from 'contexts/Localization'
-import Balance from 'components/Balance'
-import RoiCalculatorModal from 'components/RoiCalculatorModal'
 import { DeserializedPool } from 'state/types'
 import BigNumber from 'bignumber.js'
-import { BIG_ZERO } from 'utils/bigNumber'
-import { vaultPoolConfig } from 'config/constants/pools'
-import { useCurrentBlock } from 'state/block/hooks'
-import { getPoolBlockInfo } from 'views/Pools/helpers'
-
-const ApyLabelContainer = styled(Flex)`
-  cursor: pointer;
-
-  &:hover {
-    opacity: 0.5;
-  }
-`
+import Apr from 'views/Pools/components/PoolsTable/Apr'
 
 interface AprRowProps {
   pool: DeserializedPool
   stakedBalance: BigNumber
   performanceFee?: number
+  showIcon?: boolean
 }
 
-const AprRow: React.FC<AprRowProps> = ({ pool, stakedBalance, performanceFee = 0 }) => {
+const AprRow: React.FC<AprRowProps> = ({ pool, stakedBalance, performanceFee = 0, showIcon = true }) => {
   const { t } = useTranslation()
-  const currentBlock = useCurrentBlock()
-  const {
-    stakingToken,
-    earningToken,
-    isFinished,
-    apr,
-    rawApr,
-    earningTokenPrice,
-    stakingTokenPrice,
-    userData,
-    vaultKey,
-  } = pool
-
-  const { shouldShowBlockCountdown, hasPoolStarted } = getPoolBlockInfo(pool, currentBlock)
-
-  const stakingTokenBalance = userData?.stakingTokenBalance ? new BigNumber(userData.stakingTokenBalance) : BIG_ZERO
+  const { vaultKey } = pool
 
   const tooltipContent = vaultKey
     ? t('APY includes compounding, APR doesn’t. This pool’s CAKE is compounded automatically, so we show APY.')
@@ -49,55 +21,11 @@ const AprRow: React.FC<AprRowProps> = ({ pool, stakedBalance, performanceFee = 0
 
   const { targetRef, tooltip, tooltipVisible } = useTooltip(tooltipContent, { placement: 'bottom-start' })
 
-  const apyModalLink = stakingToken.address ? `/swap?outputCurrency=${stakingToken.address}` : '/swap'
-
-  const [onPresentApyModal] = useModal(
-    <RoiCalculatorModal
-      earningTokenPrice={earningTokenPrice}
-      stakingTokenPrice={stakingTokenPrice}
-      apr={vaultKey ? rawApr : apr}
-      linkLabel={t('Get %symbol%', { symbol: stakingToken.symbol })}
-      linkHref={apyModalLink}
-      stakingTokenBalance={stakedBalance.plus(stakingTokenBalance)}
-      stakingTokenSymbol={stakingToken.symbol}
-      earningTokenSymbol={earningToken.symbol}
-      autoCompoundFrequency={vaultPoolConfig[vaultKey]?.autoCompoundFrequency ?? 0}
-      performanceFee={performanceFee}
-    />,
-  )
-
-  // eslint-disable-next-line no-restricted-globals
-  const isValidate = apr !== undefined && !isNaN(apr)
-
   return (
     <Flex alignItems="center" justifyContent="space-between">
       {tooltipVisible && tooltip}
       <TooltipText ref={targetRef}>{vaultKey ? `${t('APY')}:` : `${t('APR')}:`}</TooltipText>
-      {isValidate || isFinished ? (
-        <>
-          {hasPoolStarted || !shouldShowBlockCountdown ? (
-            <ApyLabelContainer alignItems="center" onClick={onPresentApyModal}>
-              <Balance
-                fontSize="16px"
-                isDisabled={isFinished}
-                value={isFinished ? 0 : apr}
-                decimals={2}
-                unit="%"
-                onClick={onPresentApyModal}
-              />
-              {!isFinished && (
-                <IconButton variant="text" scale="sm">
-                  <CalculateIcon color="textSubtle" width="18px" />
-                </IconButton>
-              )}
-            </ApyLabelContainer>
-          ) : (
-            <Text>-</Text>
-          )}
-        </>
-      ) : (
-        <Skeleton width="82px" height="32px" />
-      )}
+      <Apr pool={pool} stakedBalance={stakedBalance} performanceFee={performanceFee} showIcon={showIcon} />
     </Flex>
   )
 }
