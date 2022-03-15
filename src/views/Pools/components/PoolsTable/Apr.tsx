af3b671516e5f86ba1718e0238a1e987e111e6b9
Chef Momota <chefmomota@chefmomotas-MacBook-Pro.local>
2022-03-15 16:22:42 +0800

    fix: Pool APR skeleton when pool not yet started

--- src/views/Pools/components/PoolsTable/Apr.tsx
+++ src/views/Pools/components/PoolsTable/Apr.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable no-restricted-globals */
 import styled from 'styled-components'
-import { Flex, useModal, CalculateIcon, Skeleton, FlexProps, Button } from '@pancakeswap/uikit'
+import { Text, Flex, useModal, CalculateIcon, Skeleton, FlexProps, Button } from '@pancakeswap/uikit'
 import RoiCalculatorModal from 'components/RoiCalculatorModal'
 import Balance from 'components/Balance'
 import { DeserializedPool } from 'state/types'
@@ -7,6 +8,8 @@ import { useTranslation } from 'contexts/Localization'
 import BigNumber from 'bignumber.js'
 import { BIG_ZERO } from 'utils/bigNumber'
 import { vaultPoolConfig } from 'config/constants/pools'
+import { useCurrentBlock } from 'state/block/hooks'
+import { getPoolBlockInfo } from 'views/Pools/helpers'
 
 const AprLabelContainer = styled(Flex)`
   &:hover {
@@ -34,6 +37,9 @@ const Apr: React.FC<AprProps> = ({ pool, showIcon, stakedBalance, performanceFee
     vaultKey,
   } = pool
   const { t } = useTranslation()
+  const currentBlock = useCurrentBlock()
+
+  const { shouldShowBlockCountdown, hasPoolStarted } = getPoolBlockInfo(pool, currentBlock)
 
   const stakingTokenBalance = userData?.stakingTokenBalance ? new BigNumber(userData.stakingTokenBalance) : BIG_ZERO
 
@@ -59,22 +65,30 @@ const Apr: React.FC<AprProps> = ({ pool, showIcon, stakedBalance, performanceFee
     onPresentApyModal()
   }
 
+  const isValidate = apr !== undefined && !isNaN(apr)
+
   return (
     <AprLabelContainer alignItems="center" justifyContent="flex-start" {...props}>
-      {apr || isFinished ? (
+      {isValidate || isFinished ? (
         <>
-          <Balance
-            onClick={openRoiModal}
-            fontSize="16px"
-            isDisabled={isFinished}
-            value={isFinished ? 0 : apr}
-            decimals={2}
-            unit="%"
-          />
-          {!isFinished && showIcon && (
-            <Button onClick={openRoiModal} variant="text" width="20px" height="20px" padding="0px" marginLeft="4px">
-              <CalculateIcon color="textSubtle" width="20px" />
-            </Button>
+          {hasPoolStarted || !shouldShowBlockCountdown ? (
+            <>
+              <Balance
+                onClick={openRoiModal}
+                fontSize="16px"
+                isDisabled={isFinished}
+                value={isFinished ? 0 : apr}
+                decimals={2}
+                unit="%"
+              />
+              {!isFinished && showIcon && (
+                <Button onClick={openRoiModal} variant="text" width="20px" height="20px" padding="0px" marginLeft="4px">
+                  <CalculateIcon color="textSubtle" width="20px" />
+                </Button>
+              )}
+            </>
+          ) : (
+            <Text>-</Text>
           )}
         </>
       ) : (
