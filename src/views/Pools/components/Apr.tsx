ca47004eff5c6197821779bd95dded9d154df7c7
ChefMomota <98292246+ChefMomota@users.noreply.github.com>
2022-03-16 08:54:09 +0800

    fix: Pool APR skeleton when pool not yet started (#3493)
    * fix: Pool APR skeleton when pool not yet started
    
    * fix: Pool Card APR skeleton when pool not yet started
    
    * fix: Eslint-disable change to eslint-disable-next-line
    
    * feat: AprRow import Apr component to avoid duplication
    
    * feat: Moved outside of the table
    
    * feat: AutoAprCell
    
    * feat: ActionPanel
    
    * fix: Using Number.isNaN
    
    Co-authored-by: Chef Momota <chefmomota@chefmomotas-MacBook-Pro.local>

--- src/views/Pools/components/Apr.tsx
+++ src/views/Pools/components/Apr.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { Flex, useModal, CalculateIcon, Skeleton, FlexProps, Button } from '@pancakeswap/uikit'
+import { Text, Flex, useModal, CalculateIcon, Skeleton, FlexProps, Button } from '@pancakeswap/uikit'
 import RoiCalculatorModal from 'components/RoiCalculatorModal'
 import Balance from 'components/Balance'
 import { DeserializedPool } from 'state/types'
@@ -7,6 +7,8 @@ import { useTranslation } from 'contexts/Localization'
 import BigNumber from 'bignumber.js'
 import { BIG_ZERO } from 'utils/bigNumber'
 import { vaultPoolConfig } from 'config/constants/pools'
+import { useCurrentBlock } from 'state/block/hooks'
+import { getPoolBlockInfo } from 'views/Pools/helpers'
 
 const AprLabelContainer = styled(Flex)`
   &:hover {
@@ -34,6 +36,9 @@ const Apr: React.FC<AprProps> = ({ pool, showIcon, stakedBalance, performanceFee
     vaultKey,
   } = pool
   const { t } = useTranslation()
+  const currentBlock = useCurrentBlock()
+
+  const { shouldShowBlockCountdown, hasPoolStarted } = getPoolBlockInfo(pool, currentBlock)
 
   const stakingTokenBalance = userData?.stakingTokenBalance ? new BigNumber(userData.stakingTokenBalance) : BIG_ZERO
 
@@ -59,22 +64,30 @@ const Apr: React.FC<AprProps> = ({ pool, showIcon, stakedBalance, performanceFee
     onPresentApyModal()
   }
 
+  const isValidate = apr !== undefined && !Number.isNaN(apr)
+
   return (
     <AprLabelContainer alignItems="center" justifyContent="flex-start" {...props}>
-      {apr || isFinished ? (
+      {isValidate || isFinished ? (
         <>
-          <Balance
-            onClick={openRoiModal}
-            fontSize="16px"
-            isDisabled={isFinished}
-            value={isFinished ? 0 : apr}
-            decimals={2}
-            unit="%"
-          />
-          {!isFinished && showIcon && (
-            <Button onClick={openRoiModal} variant="text" width="20px" height="20px" padding="0px" marginLeft="4px">
-              <CalculateIcon color="textSubtle" width="20px" />
-            </Button>
+          {hasPoolStarted || !shouldShowBlockCountdown ? (
+            <>
+              <Balance
+                onClick={openRoiModal}
+                fontSize="16px"
+                isDisabled={isFinished}
+                value={isFinished ? 0 : apr}
+                decimals={2}
+                unit="%"
+              />
+              {!isFinished && showIcon && (
+                <Button onClick={openRoiModal} variant="text" width="20px" height="20px" padding="0px" marginLeft="4px">
+                  <CalculateIcon color="textSubtle" width="20px" />
+                </Button>
+              )}
+            </>
+          ) : (
+            <Text>-</Text>
           )}
         </>
       ) : (
