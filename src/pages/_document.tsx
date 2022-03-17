9efe018cf30420af0ad42d6834d62d1f06bb909a
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2022-03-17 16:14:00 +0800

    chore: Try derfer next script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -56,7 +56,7 @@ class MyDocument extends Document {
             />
           </noscript>
           <Main />
-          <NextScript />
+          <DeferNextScript />
           <div id="portal-root" />
         </body>
       </Html>
@@ -65,3 +65,66 @@ class MyDocument extends Document {
 }
 
 export default MyDocument
+
+function dedupe<T extends { file: string }>(bundles: T[]): T[] {
+  const files = new Set<string>()
+  const kept: T[] = []
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const bundle of bundles) {
+    // eslint-disable-next-line no-continue
+    if (files.has(bundle.file)) continue
+    files.add(bundle.file)
+    kept.push(bundle)
+  }
+  return kept
+}
+
+type DocumentFiles = {
+  sharedFiles: readonly string[]
+  pageFiles: readonly string[]
+  allFiles: readonly string[]
+}
+
+/**
+ * Custom NextScript to defer loading of unnecessary JS.
+ * Standard behavior is async. Compatible with Next.js 10.0.3
+ */
+class DeferNextScript extends NextScript {
+  getDynamicChunks(files: DocumentFiles) {
+    const { dynamicImports, assetPrefix, isDevelopment, devOnlyCacheBusterQueryString } = this.context
+    // @ts-ignore
+    return dedupe(dynamicImports).map((bundle) => {
+      if (!bundle.file.endsWith('.js') || files.allFiles.includes(bundle.file)) return null
+
+      return (
+        <script
+          defer={!isDevelopment}
+          key={bundle.file}
+          src={`${assetPrefix}/_next/${encodeURI(bundle.file)}${devOnlyCacheBusterQueryString}`}
+          nonce={this.props.nonce}
+          crossOrigin={this.props.crossOrigin || process.env.__NEXT_CROSS_ORIGIN}
+        />
+      )
+    })
+  }
+
+  getScripts(files: DocumentFiles) {
+    const { assetPrefix, buildManifest, isDevelopment, devOnlyCacheBusterQueryString } = this.context
+
+    const normalScripts = files.allFiles.filter((file) => file.endsWith('.js'))
+    const lowPriorityScripts = buildManifest.lowPriorityFiles?.filter((file) => file.endsWith('.js'))
+
+    return [...normalScripts, ...lowPriorityScripts].map((file) => {
+      return (
+        <script
+          key={file}
+          src={`${assetPrefix}/_next/${encodeURI(file)}${devOnlyCacheBusterQueryString}`}
+          nonce={this.props.nonce}
+          defer={!isDevelopment}
+          crossOrigin={this.props.crossOrigin || process.env.__NEXT_CROSS_ORIGIN}
+        />
+      )
+    })
+  }
+}
