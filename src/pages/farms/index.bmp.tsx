3d380365b856962dfcf099f9fef8790f55deaef1
xuzhanhh <xuzhanhh@gmail.com>
2022-03-21 17:12:41 +0800

    feat(Farms): Init Farms Page

---
+++ src/pages/farms/index.bmp.tsx
@@ -0,0 +1,38 @@
+import React, { useContext } from 'react'
+import { FarmsPageLayout, FarmsContext } from 'views/Farms'
+import FarmCard from 'views/Farms/components/FarmCard/FarmCard'
+import { getDisplayApr } from 'views/Farms/Farms'
+import { usePriceCakeBusd } from 'state/farms/hooks'
+import { useWeb3React } from '@web3-react/core'
+
+const FarmsPage = () => {
+  const { account } = useWeb3React()
+  const { chosenFarmsMemoized } = useContext(FarmsContext)
+  const cakePrice = usePriceCakeBusd()
+  return (
+    <view>
+      {chosenFarmsMemoized.map((farm) => (
+        <FarmCard
+          key={farm.pid}
+          farm={farm}
+          displayApr={getDisplayApr(farm.apr, farm.lpRewardsApr)}
+          cakePrice={cakePrice}
+          account={account}
+          removed={false}
+        />
+      ))}
+    </view>
+  )
+}
+
+// FarmsPage.Layout = FarmsPageLayout
+
+const FarmsPageWrapper = () => {
+  return (
+    <FarmsPageLayout>
+      <FarmsPage />
+    </FarmsPageLayout>
+  )
+}
+
+export default FarmsPageWrapper
