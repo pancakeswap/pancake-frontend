de38403bf5939a723f0fef4a1547d76354507ca4
Chef Momota <chefmomota@chefmomotas-MacBook-Pro.local>
2022-03-22 13:18:20 +0800

    feat: Remove yieldwatch link

--- src/views/Pools/components/PoolCard/Modals/NotEnoughTokensModal.tsx
+++ src/views/Pools/components/PoolCard/Modals/NotEnoughTokensModal.tsx
@@ -34,12 +34,6 @@ const NotEnoughTokensModal: React.FC<NotEnoughTokensModalProps> = ({ tokenSymbol
       <Button mt="24px" as="a" external href="/swap">
         {t('Buy')} {tokenSymbol}
       </Button>
-      <StyledLink href="https://yieldwatch.net" external>
-        <Button variant="secondary" mt="8px" width="100%">
-          {t('Locate Assets')}
-          <OpenNewIcon color="primary" ml="4px" />
-        </Button>
-      </StyledLink>
       <Button variant="text" onClick={onDismiss}>
         {t('Close Window')}
       </Button>
