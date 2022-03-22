576c4ddbe2930fc56c0f6f2ebd11a654a19d541e
memoyil <2213635+memoyil@users.noreply.github.com>
2022-03-22 11:20:22 +0100

    chore: Add translation to apply to run an ifo text (#3563)

--- src/views/Ifos/components/IfoContainer.tsx
+++ src/views/Ifos/components/IfoContainer.tsx
@@ -1,5 +1,6 @@
 import { ReactElement } from 'react'
 import styled from 'styled-components'
+import { useTranslation } from 'contexts/Localization'
 import { Box, LinkExternal } from '@pancakeswap/uikit'
 import Container from 'components/Layout/Container'
 import IfoLayout, { IfoLayoutWrapper } from './IfoLayout'
@@ -15,28 +16,32 @@ interface TypeProps {
   ifoSteps: ReactElement
 }
 
-const IfoContainer: React.FC<TypeProps> = ({ ifoSection, ifoSteps }) => (
-  <IfoLayout id="current-ifo" py={['24px', '24px', '40px']}>
-    <Container>
-      <IfoLayoutWrapper>
-        <IfoPoolVaultCard />
-        {ifoSection}
-      </IfoLayoutWrapper>
-    </Container>
-    <IfoStepBackground>
-      <Container>{ifoSteps}</Container>
-    </IfoStepBackground>
-    <Container>
-      <IfoQuestions />
-      <LinkExternal
-        href="https://docs.pancakeswap.finance/contact-us/business-partnerships#ifos-token-sales"
-        mx="auto"
-        mt="16px"
-      >
-        Apply to run an IFO!
-      </LinkExternal>
-    </Container>
-  </IfoLayout>
-)
+const IfoContainer: React.FC<TypeProps> = ({ ifoSection, ifoSteps }) => {
+  const { t } = useTranslation()
+
+  return (
+    <IfoLayout id="current-ifo" py={['24px', '24px', '40px']}>
+      <Container>
+        <IfoLayoutWrapper>
+          <IfoPoolVaultCard />
+          {ifoSection}
+        </IfoLayoutWrapper>
+      </Container>
+      <IfoStepBackground>
+        <Container>{ifoSteps}</Container>
+      </IfoStepBackground>
+      <Container>
+        <IfoQuestions />
+        <LinkExternal
+          href="https://docs.pancakeswap.finance/contact-us/business-partnerships#ifos-token-sales"
+          mx="auto"
+          mt="16px"
+        >
+          {t('Apply to run an IFO!')}
+        </LinkExternal>
+      </Container>
+    </IfoLayout>
+  )
+}
 
 export default IfoContainer
