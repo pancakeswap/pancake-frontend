257a4f1f25cd0432db5411531d066d9b3326374e
kim-pink-ba <kimpinkba@gmail.com>
2022-03-23 12:47:36 +0800

    feat(farm): Add Tooltip

--- src/components/QuestionHelper/index.bmp.tsx
+++ src/components/QuestionHelper/index.bmp.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { IconButton, CloseIcon, HelpIcon, Box, BoxProps, Placement, Overlay, Text } from '@pancakeswap/uikit'
+import { HelpIcon, Box, BoxProps, Placement, Text } from '@pancakeswap/uikit'
 import styled from 'styled-components'
+import Tooltip from '../bmp/Tooltip'
 
 interface Props extends BoxProps {
   text: string | React.ReactNode
@@ -14,40 +15,16 @@ const QuestionWrapper = styled.div`
     opacity: 0.7;
   }
 `
-const OverlayInner = styled.div`
-  background-color: ${({ theme }) => theme.colors.backgroundAlt};
-  position: absolute;
-  top: 50%;
-  width: 80%;
-  border-radius: 10px;
-  margin: auto;
-  padding: 16px;
-  left: 0;
-  right: 0;
-`
-const QuestionHelper: React.FC<Props> = ({ text, placement = 'right-end', size = '16px', ...props }) => {
+const QuestionHelper: React.FC<Props> = ({ text, size = '16px', ...props }) => {
   const [visible, setVisible] = React.useState(false)
   return (
     <Box {...props}>
       <QuestionWrapper onClick={() => setVisible(true)}>
         <HelpIcon color="textSubtle" width={size} />
       </QuestionWrapper>
-      {visible && (
-        <Overlay onClick={() => setVisible(false)} style={{ backgroundColor: 'transparent' }}>
-          <OverlayInner>
-            <Text style={{ marginRight: 20 }}>{text}</Text>
-
-            <IconButton
-              variant="text"
-              style={{ position: 'absolute', top: 0, right: 0 }}
-              onClick={() => setVisible(false)}
-              aria-label="Close the dialog"
-            >
-              <CloseIcon color="text" width="24px" />
-            </IconButton>
-          </OverlayInner>
-        </Overlay>
-      )}
+      <Tooltip visible={visible} onClose={() => setVisible(false)}>
+        <Text style={{ marginRight: 20 }}>{text}</Text>
+      </Tooltip>
     </Box>
   )
 }
---
+++ src/components/bmp/Tooltip.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { CloseIcon, Overlay, Box, IconButton } from '@pancakeswap/uikit'
+import styled from '@binance/mp-styled'
+
+const OverlayInner = styled.div`
+    background-color: ${({ theme }) => theme.colors.backgroundAlt};
+    position: absolute;
+    top: 50%;
+    width: 80%;
+    border-radius: 10px;
+    margin: auto;
+    padding: 16px;
+    left: 0;
+    right: 0;
+`
+const Tooltip = ({ visible, onClose, children }) => {
+    if (visible) {
+        return (
+            <Overlay onClick={onClose} style={{ backgroundColor: 'transparent' }}>
+                <OverlayInner>
+                    <Box style={{ marginRight: 20 }}>{children}</Box>
+
+                    <IconButton
+                        variant="text"
+                        style={{ position: 'absolute', top: 0, right: 0 }}
+                        onClick={onClose}
+                        aria-label="close the dialog"
+                    >
+                        <CloseIcon color="text" width="24px" />
+                    </IconButton>
+                </OverlayInner>
+            </Overlay>
+        )
+    }
+    return null
+}
+export default Tooltip
