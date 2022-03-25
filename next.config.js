d6276ccf6db69e7e5c58eb59e60a114a0687b77d
Philip <97418926+chefilip@users.noreply.github.com>
2022-03-25 13:08:37 -0300

    chore(sentry): Enable Sentry realease (#3584)
    * chore(sentry): Try to enable Sentry realease
    
    * chore: Avoid deploy release on Sentry for development
    
    * chore: Debug Sentry deploy
    
    * chore: Avoid updating release to Sentry when preview

--- next.config.js
+++ next.config.js
@@ -4,19 +4,29 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-
-  silent: true, // Suppresses all logs
-  dryRun: !process.env.SENTRY_AUTH_TOKEN, // Set to true will skip the upload release step
-  ignore: ['node_modules', 'cypress'],
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-}
+const sentryWebpackPluginOptions =
+  process.env.VERCEL_ENV === 'production'
+    ? {
+        // Additional config options for the Sentry Webpack plugin. Keep in mind that
+        // the following options are set automatically, and overriding them is not
+        // recommended:
+        //   release, url, org, project, authToken, configFile, stripPrefix,
+        //   urlPrefix, include, ignore
+        silent: false, // Logging when deploying to check if there is any problem
+        validate: true,
+        dryRun: !process.env.SENTRY_AUTH_TOKEN,
+        // Set to env false will skip deploying release on Sentry except Production
+        // https://github.com/getsentry/sentry-webpack-plugin/blob/master/src/index.js#L522
+        deploy: {
+          env: process.env.VERCEL_ENV,
+        },
+        // For all available options, see:
+        // https://github.com/getsentry/sentry-webpack-plugin#options.
+      }
+    : {
+        silent: true, // Suppresses all logs
+        dryRun: !process.env.SENTRY_AUTH_TOKEN,
+      }
 
 /** @type {import('next').NextConfig} */
 const config = {
