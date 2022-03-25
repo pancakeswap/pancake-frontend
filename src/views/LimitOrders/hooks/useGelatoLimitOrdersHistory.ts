f013ab5383d39a736e57e03b62527977fef1f3d3
memoyil <2213635+memoyil@users.noreply.github.com>
2022-03-25 17:17:13 +0100

    fix: (limit-order): Order table not refreshed after action (#3585)
    * fix: (limit-order): Order table not refreshed after action
    
    * refactor: Move mutate call to limit orders handlers

--- src/views/LimitOrders/hooks/useGelatoLimitOrdersHistory.ts
+++ src/views/LimitOrders/hooks/useGelatoLimitOrdersHistory.ts
@@ -10,6 +10,9 @@ import useGelatoLimitOrdersLib from 'hooks/limitOrders/useGelatoLimitOrdersLib'
 import orderBy from 'lodash/orderBy'
 import { ORDER_CATEGORY, LimitOrderStatus } from '../types'
 
+export const OPEN_ORDERS_SWR_KEY = ['gelato', 'openOrders']
+export const EXECEUTED_CANCELLED_ORDERS_SWR_KEY = ['gelato', 'cancelledExecutedOrders']
+
 function newOrdersFirst(a: Order, b: Order) {
   return Number(b.updatedAt) - Number(a.updatedAt)
 }
@@ -65,7 +68,7 @@ const useOpenOrders = (turnOn: boolean): Order[] => {
   const startFetch = turnOn && gelatoLimitOrders && account && chainId
 
   const { data } = useSWR(
-    startFetch ? ['gelato', 'openOrders'] : null,
+    startFetch ? OPEN_ORDERS_SWR_KEY : null,
     async () => {
       try {
         const orders = await gelatoLimitOrders.getOpenOrders(account.toLowerCase(), false)
@@ -110,7 +113,7 @@ const useHistoryOrders = (turnOn: boolean): Order[] => {
   const startFetch = turnOn && gelatoLimitOrders && account && chainId
 
   const { data } = useSWR(
-    startFetch ? ['gelato', 'cancelledExecutedOrders'] : null,
+    startFetch ? EXECEUTED_CANCELLED_ORDERS_SWR_KEY : null,
     async () => {
       try {
         const acc = account.toLowerCase()
