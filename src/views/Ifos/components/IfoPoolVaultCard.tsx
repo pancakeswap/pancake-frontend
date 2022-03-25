04c6e3dc8a7cd74a304b4697a3a6f09f4c1a7349
Chef Momota <chefmomota@chefmomotas-MacBook-Pro.local>
2022-03-25 13:58:12 +0800

    feat: New IfoPoolVaultCardMobile

--- src/views/Ifos/components/IfoPoolVaultCard.tsx
+++ src/views/Ifos/components/IfoPoolVaultCard.tsx
@@ -5,30 +5,19 @@ import {
   CardHeader,
   ExpandableButton,
   Flex,
-  HelpIcon,
   Text,
   useMatchBreakpoints,
-  useTooltip,
+  Message,
+  Button,
 } from '@pancakeswap/uikit'
 import Balance from 'components/Balance'
-import { CompoundingPoolTag } from 'components/Tags'
 import { TokenPairImage } from 'components/TokenImage'
 import tokens from 'config/constants/tokens'
 import { useTranslation } from 'contexts/Localization'
 import useActiveWeb3React from 'hooks/useActiveWeb3React'
-import { useBUSDCakeAmount } from 'hooks/useBUSDPrice'
 import { useState } from 'react'
-import { VaultKey } from 'state/types'
 import styled from 'styled-components'
-import { getBalanceNumber } from 'utils/formatBalance'
-import CakeVaultCard from 'views/Pools/components/CakeVaultCard'
-import RecentCakeProfitCountdownRow from 'views/Pools/components/CakeVaultCard/RecentCakeProfitRow'
-import UnstakingFeeCountdownRow from 'views/Pools/components/CakeVaultCard/UnstakingFeeCountdownRow'
-import AprRow from 'views/Pools/components/PoolCard/AprRow'
-import ExpandedFooter from 'views/Pools/components/PoolCard/CardFooter/ExpandedFooter'
-import Staked from 'views/Pools/components/PoolsTable/ActionPanel/Stake'
 import { ActionContainer } from 'views/Pools/components/PoolsTable/ActionPanel/styles'
-import { convertSharesToCake } from '../../Pools/helpers'
 
 const StyledCardMobile = styled(Card)`
   max-width: 400px;
@@ -42,11 +31,6 @@ const StyledTokenContent = styled(Flex)`
   }
 `
 
-const StyledCardFooter = styled(Box)`
-  padding: 16px;
-  background-color: ${({ theme }) => theme.colors.dropdown};
-`
-
 const StyledCardBody = styled(CardBody)`
   display: grid;
   padding: 16px;
@@ -58,106 +42,89 @@ const StyledCardBody = styled(CardBody)`
   }
 `
 
-// const IfoPoolVaultCardMobile: React.FC = () => {
-//   const { account } = useActiveWeb3React()
-//   const { t } = useTranslation()
-//   // const credit = useIfoPoolCredit()
-//   // const {
-//   //   fees: { performanceFeeAsDecimal },
-//   //   userData: { userShares, isLoading: userDataLoading },
-//   //   pricePerFullShare,
-//   // } = useIfoPoolVault()
+const IfoPoolVaultCardMobile: React.FC = () => {
+  const { account } = useActiveWeb3React()
+  const { t } = useTranslation()
+  const [isExpanded, setIsExpanded] = useState(false)
 
-//   const { cakeAsBigNumber } = convertSharesToCake(userShares, pricePerFullShare)
-
-//   const [isExpanded, setIsExpanded] = useState(false)
-
-//   const cakeAsNumberBalance = getBalanceNumber(credit)
-//   const stakedDollarValue = useBUSDCakeAmount(cakeAsNumberBalance)
-
-//   const { targetRef, tooltip, tooltipVisible } = useTooltip(
-//     t('Any funds you stake in this pool will be automagically harvested and restaked (compounded) for you.'),
-//     {
-//       placement: 'bottom',
-//     },
-//   )
-//   return (
-//     <StyledCardMobile isActive>
-//       <CardHeader p="16px">
-//         <Flex justifyContent="space-between" alignItems="center">
-//           <StyledTokenContent alignItems="center" flex={1}>
-//             <TokenPairImage width={24} height={24} primaryToken={tokens.cake} secondaryToken={tokens.cake} />
-//             <Box ml="8px">
-//               <Text fontSize="12px" bold color="secondary" textTransform="uppercase">
-//                 {t('Staked')}
-//               </Text>
-//               <Text small bold>
-//                 IFO CAKE
-//               </Text>
-//               <Text color="textSubtle" fontSize="12px">
-//                 {t('Stake')} CAKE
-//               </Text>
-//             </Box>
-//           </StyledTokenContent>
-//           <StyledTokenContent flexDirection="column" flex={1}>
-//             <Text color="textSubtle" fontSize="12px">
-//               {t('IFO Credit')}
-//             </Text>
-//             <Balance small bold decimals={3} value={cakeAsNumberBalance} />
-//             <Balance
-//               value={stakedDollarValue || 0}
-//               fontSize="12px"
-//               color="textSubtle"
-//               decimals={2}
-//               prefix="~"
-//               unit=" USD"
-//             />
-//           </StyledTokenContent>
-//           <ExpandableButton expanded={isExpanded} onClick={() => setIsExpanded((prev) => !prev)} />
-//         </Flex>
-//       </CardHeader>
-//       {isExpanded && (
-//         <>
-//           <StyledCardBody>
-//             <AprRow pool={pool} stakedBalance={cakeAsBigNumber} performanceFee={performanceFeeAsDecimal} />
-//             <ActionContainer>
-//               <IfoVaultCardAvgBalance />
-//             </ActionContainer>
-//             <Staked pool={pool} userDataLoaded={!userDataLoading} />
-//             <ActionContainer>
-//               <Box>
-//                 <RecentCakeProfitCountdownRow pool={pool} />
-//               </Box>
-//               <Box mt="8px">
-//                 <UnstakingFeeCountdownRow vaultKey={VaultKey.IfoPool} />
-//               </Box>
-//             </ActionContainer>
-//           </StyledCardBody>
-//           <StyledCardFooter>
-//             <ExpandedFooter account={account} pool={pool} />
-//             <Flex alignItems="center">
-//               <CompoundingPoolTag />
-//               {tooltipVisible && tooltip}
-//               <Flex ref={targetRef}>
-//                 <HelpIcon ml="4px" width="20px" height="20px" color="textSubtle" />
-//               </Flex>
-//             </Flex>
-//           </StyledCardFooter>
-//         </>
-//       )}
-//     </StyledCardMobile>
-//   )
-// }
+  return (
+    <StyledCardMobile isActive>
+      <CardHeader p="16px">
+        <Flex justifyContent="space-between" alignItems="center">
+          <StyledTokenContent alignItems="center" flex={1}>
+            <TokenPairImage width={24} height={24} primaryToken={tokens.cake} secondaryToken={tokens.cake} />
+            <Box ml="8px">
+              <Text fontSize="12px" bold color="secondary" textTransform="uppercase">
+                {t('Staked')}
+              </Text>
+              <Text small bold>
+                IFO CAKE
+              </Text>
+              <Text color="textSubtle" fontSize="12px">
+                {t('Stake')} CAKE
+              </Text>
+            </Box>
+          </StyledTokenContent>
+          <StyledTokenContent flexDirection="column" flex={1}>
+            <Text color="textSubtle" fontSize="12px">
+              {t('CAKE staked')}
+            </Text>
+            <Balance small bold decimals={3} value={20} />
+          </StyledTokenContent>
+          <ExpandableButton expanded={isExpanded} onClick={() => setIsExpanded((prev) => !prev)} />
+        </Flex>
+      </CardHeader>
+      {isExpanded && (
+        <>
+          <StyledCardBody>
+            <Message variant="warning" mb="20px">
+              <Flex flexDirection="column">
+                <Text mb="16px">
+                  {t(
+                    'This is the old IFO CAKE pool. Check out the brand new CAKE pool to learn how to earn CAKE rewards with higher APY while enjoying other benefits.',
+                  )}
+                </Text>
+                <Flex ml="-34px">
+                  {/* Migrate */}
+                  <Button width="100%">{t('Go to new CAKE pool')}</Button>
+                </Flex>
+              </Flex>
+            </Message>
+            <ActionContainer>
+              <Box>
+                <Flex mb="4px">
+                  <Text fontSize="12px" color="secondary" bold mr="2px">{`${t('CAKE')}`}</Text>
+                  <Text fontSize="12px" color="textSubtle" bold textTransform="uppercase">
+                    {t('Staked')}
+                  </Text>
+                </Flex>
+                <Text fontSize="20px" bold>
+                  1,483.450
+                </Text>
+              </Box>
+              <Box mt="24px">
+                <Flex alignItems="center" justifyContent="space-between">
+                  <Text fontSize="14px">{`${t('Recent CAKE profit')}:`}</Text>
+                  <Text fontSize="14px" color="textDisabled" bold>
+                    0
+                  </Text>
+                </Flex>
+              </Box>
+            </ActionContainer>
+          </StyledCardBody>
+        </>
+      )}
+    </StyledCardMobile>
+  )
+}
 
 const IfoPoolVaultCard = () => {
-  // const { pool } = useIfoWithApr()
-  // const { isMd, isXs, isSm } = useMatchBreakpoints()
-  // const isSmallerThanTablet = isMd || isXs || isSm
-  // if (isSmallerThanTablet) {
-  //   return <IfoPoolVaultCardMobile />
-  // }
+  const { isMd, isXs, isSm } = useMatchBreakpoints()
+  const isSmallerThanTablet = isMd || isXs || isSm
+  if (isSmallerThanTablet) {
+    return <IfoPoolVaultCardMobile />
+  }
 
-  // return <CakeVaultCard pool={pool} showStakedOnly={false} m="auto" />
   return <>New Layout</>
 }
 
