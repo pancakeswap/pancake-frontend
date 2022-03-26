9746e6636638d65c6af77ab9a4570085de729d2a
Agustin Kassis <webmaster@masize.com>
2022-03-26 18:16:00 -0300

    replaced usdt to usdc

--- src/state/swap/constants.ts
+++ src/state/swap/constants.ts
@@ -1,6 +1,6 @@
-import { mainnetTokens } from "config/constants/tokens"
+import { mainnetTokens } from 'config/constants/tokens'
 
 // USDT
-export const DEFAULT_INPUT_CURRENCY = mainnetTokens.usdt.address
+export const DEFAULT_INPUT_CURRENCY = mainnetTokens.usdc.address
 // PE
 export const DEFAULT_OUTPUT_CURRENCY = mainnetTokens.pe.address
