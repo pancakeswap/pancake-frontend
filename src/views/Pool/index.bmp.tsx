0d90939c92e037b492326321766820ba0e864410
xuzhanhh <xuzhanhh@gmail.com>
2022-03-29 17:35:26 +0800

    feat(Liquidity): Add tips

--- src/views/Pool/index.bmp.tsx
+++ src/views/Pool/index.bmp.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
 import styled from 'styled-components'
 import { Pair } from '@pancakeswap/sdk'
 import isEqual from 'lodash/isEqual'
 import { useDidHide, useDidShow } from '@binance/mp-service'
-import { Text, Flex, CardBody, CardFooter, Button, AddIcon, Image } from '@pancakeswap/uikit'
+import { Text, Flex, CardBody, CardFooter, Button, AddIcon, Image, LinkExternal, Card } from '@pancakeswap/uikit'
 import { useTranslation } from 'contexts/Localization'
 import useActiveWeb3React from 'hooks/useActiveWeb3React'
 import ConnectWalletButton from 'components/ConnectWalletButton'
@@ -16,6 +16,8 @@ import { toV2LiquidityToken, useTrackedTokenPairs } from '../../state/user/hooks
 import Dots from '../../components/Loader/Dots'
 import { AppHeader, AppBody } from '../../components/App'
 import noLiquidityImage from '../../../public/images/no-liquidity.png'
+import AddLiquidityTipImage from '../../images/add-liquidity-tip.png'
+import { useTooltip } from 'contexts/bmp/TooltipContext'
 
 const Body = styled(CardBody)`
   display: flex;
@@ -24,6 +26,38 @@ const Body = styled(CardBody)`
   background-color: ${({ theme }) => theme.colors.dropdownDeep};
   min-height: 324px;
 `
+
+export const AddLiquidityTip = (props) => {
+  const { t } = useTranslation()
+
+  const { onPresent } = useTooltip(
+    <view>
+      <Text mb="10px">{t('impermanent-loss-tip-1')}</Text>
+      <LinkExternal
+        as="a"
+        external
+        href="https://blog.bancor.network/beginners-guide-to-getting-rekt-by-impermanent-loss-7c9510cb2f22"
+      >
+        {t('impermanent-loss-tip-2')}
+      </LinkExternal>
+    </view>,
+  )
+  return (
+    <Flex justifyContent="center" {...props}>
+      <Text color="textSubtle" textAlign="center" mb="16px">
+        {t(
+          "By adding liquidity you'll earn 0.17% of all trades on this pair proportional to your share of the pool. Fees are added to the pool, accrue in real time and can be claimed by withdrawing your liquidity.",
+        )}{' '}
+        {t('add-liquidity-tip-1')}{' '}
+        <Text onClick={onPresent} style={{ display: 'inline', textDecoration: 'underline dotted' }}>
+          {' '}
+          {t('add-liquidity-tip-2')}
+        </Text>{' '}
+        {t('add-liquidity-tip-3')}
+      </Text>
+    </Flex>
+  )
+}
 
 let origin
 const TrackedTokenHook = ({ account, setV2IsLoading, setAllV2PairsWithLiquidity }) => {
@@ -76,17 +110,14 @@ function Pool({ v2IsLoading, allV2PairsWithLiquidity }) {
   const { dispatch } = useLiquidity()
   const { account } = useActiveWeb3React()
   const { t } = useTranslation()
-
   const renderBody = () => {
     if (!account) {
       return (
         <view>
-          <Flex justifyContent="center">
-            <Text color="textSubtle" textAlign="center" width="180px" mb="16px">
-              {t('Connect to a wallet to view your liquidity.')}
-            </Text>
+          <Flex justifyContent="center" mb="16px">
+            <Image src={AddLiquidityTipImage} width={106} height={100} />
           </Flex>
-          <ConnectWalletButton width="100%" />
+          <AddLiquidityTip />
         </view>
       )
     }
@@ -108,16 +139,16 @@ function Pool({ v2IsLoading, allV2PairsWithLiquidity }) {
     }
     return (
       <view>
-        <Flex justifyContent="center" mb="16px">
-          <Image src={noLiquidityImage} width={97} height={100} />
-        </Flex>
+        {/* <Flex justifyContent="center" mb="16px"> */}
+        {/*   <Image src={noLiquidityImage} width={97} height={100} /> */}
+        {/* </Flex> */}
         <Text color="textSubtle" textAlign="center">
           {t('No liquidity found.')}
         </Text>
       </view>
     )
   }
-
+  console.log('???', allV2PairsWithLiquidity)
   return (
     <ErrorBoundary name="pool">
       <AppBody>
@@ -129,38 +160,49 @@ function Pool({ v2IsLoading, allV2PairsWithLiquidity }) {
               <Text color="textSubtle" mb="8px">
                 {t("Don't see a pool you joined?")}
               </Text>
-              {/* <Link href="/find"> */}
               <Button
                 id="import-pool-link"
                 variant="secondary"
                 scale="sm"
                 as="a"
+                mb=""
                 onClick={() => {
                   dispatch({ type: 'setPage', page: LiquidityPage.Find })
                 }}
               >
                 {t('Find other LP tokens')}
               </Button>
-              {/* </Link> */}
+              {allV2PairsWithLiquidity?.length === 0 && (
+                <>
+                  <Image src={AddLiquidityTipImage} width={106} height={100} mt="32px" mb="16px" />
+                  <AddLiquidityTip />
+                </>
+              )}
             </Flex>
           )}
         </Body>
         <CardFooter style={{ textAlign: 'center' }}>
-          {/* <Link href="/add"> */}
-          <Button
-            disabled={!account}
-            id="join-pool-button"
-            width="100%"
-            startIcon={<AddIcon color="white" />}
-            onClick={() => {
-              dispatch({ type: 'setPage', page: LiquidityPage.Add })
-            }}
-          >
-            {t('Add Liquidity')}
-          </Button>
-          {/* </Link> */}
+          {account && (
+            <Button
+              disabled={!account}
+              id="join-pool-button"
+              width="100%"
+              startIcon={<AddIcon color="white" />}
+              onClick={() => {
+                dispatch({ type: 'setPage', page: LiquidityPage.Add })
+              }}
+            >
+              {t('Add Liquidity')}
+            </Button>
+          )}
+          {!account && <ConnectWalletButton width="100%" />}
         </CardFooter>
       </AppBody>
+      {allV2PairsWithLiquidity?.length > 0 && (
+        <Card mt="24px">
+          <AddLiquidityTip mt="16px" mx="20px" />
+        </Card>
+      )}
     </ErrorBoundary>
   )
 }
