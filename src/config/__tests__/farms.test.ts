8895e9ec369f3b6f9c1e2077614ed4b08a063604
Chef Momota <chefmomota@chefmomotas-MacBook-Pro.local>
2022-04-18 20:04:46 +0800

    chore: Fix failed test

--- src/config/__tests__/farms.test.ts
+++ src/config/__tests__/farms.test.ts
@@ -36,7 +36,7 @@ describe('Config farms', () => {
   })
 
   // The first pid using the new factory
-  const START_PID = 365
+  const START_PID = 2
   const FACTORY_ADDRESS = '0xca143ce32fe78f1f7019d7d551a6402fc5350c73'
   const newFarmsToTest = farmsToTest.filter((farmSet) => farmSet[0] >= START_PID)
 
