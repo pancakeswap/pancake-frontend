7b8eab71db6e24337d5bf8c5f366228c88452fe5
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-04-19 20:25:53 +0800

    chore: Reorder token list token import (#3706)

--- src/config/constants/lists.ts
+++ src/config/constants/lists.ts
@@ -10,10 +10,10 @@ export const UNSUPPORTED_LIST_URLS: string[] = []
 
 // lower index == higher priority for token import
 export const DEFAULT_LIST_OF_LISTS: string[] = [
+  PANCAKE_EXTENDED,
+  PANCAKE_TOP100,
   CMC,
   COINGECKO,
-  PANCAKE_TOP100,
-  PANCAKE_EXTENDED,
   ...UNSUPPORTED_LIST_URLS, // need to load unsupported tokens as well
 ]
 
