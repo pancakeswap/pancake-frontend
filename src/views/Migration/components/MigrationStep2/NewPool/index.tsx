f4c2aa26f682d2434e62351b861835b8acce90d4
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-04-20 13:47:06 +0800

    perf: Reduce new pool request

--- src/views/Migration/components/MigrationStep2/NewPool/index.tsx
+++ src/views/Migration/components/MigrationStep2/NewPool/index.tsx
@@ -1,13 +1,27 @@
-import React, { useMemo } from 'react'
+import React, { useEffect, useMemo } from 'react'
 import { useWeb3React } from '@web3-react/core'
-import { usePoolsWithVault, usePoolsPageFetch } from 'state/pools/hooks'
+import { usePoolsWithVault } from 'state/pools/hooks'
+import { useFastRefreshEffect } from 'hooks/useRefreshEffect'
+import { useAppDispatch } from 'state'
+import { fetchCakeVaultFees, fetchCakeVaultPublicData, fetchCakeVaultUserData } from 'state/pools'
 import PoolsTable from './PoolTable'
 
 const NewPool: React.FC = () => {
   const { account } = useWeb3React()
   const { pools, userDataLoaded } = usePoolsWithVault()
   const userDataReady: boolean = !account || (!!account && userDataLoaded)
-  usePoolsPageFetch()
+  const dispatch = useAppDispatch()
+
+  useFastRefreshEffect(() => {
+    dispatch(fetchCakeVaultPublicData())
+    if (account) {
+      dispatch(fetchCakeVaultUserData({ account }))
+    }
+  }, [account, dispatch])
+
+  useEffect(() => {
+    dispatch(fetchCakeVaultFees())
+  }, [dispatch])
 
   const stakedOnlyOpenPools = useMemo(
     () => pools.filter((pool) => pool.userData && pool.sousId === 0 && !pool.isFinished),
