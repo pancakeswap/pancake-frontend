244ae18d0cce32700f08a4e1374473fb6f37d81b
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-04-20 13:54:42 +0800

    get user data loading

--- src/views/Migration/components/MigrationStep2/NewPool/index.tsx
+++ src/views/Migration/components/MigrationStep2/NewPool/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useMemo } from 'react'
 import { useWeb3React } from '@web3-react/core'
-import { usePoolsWithVault } from 'state/pools/hooks'
+import { useCakeVault, usePoolsWithVault } from 'state/pools/hooks'
 import { useFastRefreshEffect } from 'hooks/useRefreshEffect'
 import { useAppDispatch } from 'state'
 import { fetchCakeVaultFees, fetchCakeVaultPublicData, fetchCakeVaultUserData } from 'state/pools'
@@ -8,8 +8,16 @@ import PoolsTable from './PoolTable'
 
 const NewPool: React.FC = () => {
   const { account } = useWeb3React()
-  const { pools, userDataLoaded } = usePoolsWithVault()
-  const userDataReady: boolean = !account || (!!account && userDataLoaded)
+  const { pools } = usePoolsWithVault()
+  const cakeVault = useCakeVault()
+
+  const stakedOnlyOpenPools = useMemo(
+    () => pools.filter((pool) => pool.userData && pool.sousId === 0 && !pool.isFinished),
+    [pools],
+  )
+
+  const userDataReady: boolean = !account || (!!account && !cakeVault.userData?.isLoading)
+
   const dispatch = useAppDispatch()
 
   useFastRefreshEffect(() => {
@@ -22,11 +30,6 @@ const NewPool: React.FC = () => {
   useEffect(() => {
     dispatch(fetchCakeVaultFees())
   }, [dispatch])
-
-  const stakedOnlyOpenPools = useMemo(
-    () => pools.filter((pool) => pool.userData && pool.sousId === 0 && !pool.isFinished),
-    [pools],
-  )
 
   return <PoolsTable pools={stakedOnlyOpenPools} account={account} userDataReady={userDataReady} />
 }
