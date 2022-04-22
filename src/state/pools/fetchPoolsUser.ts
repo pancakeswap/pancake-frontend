f4f1c7c9de0f12ae9ac5c1dfd33b6dcde9bc68c0
memoyil <2213635+memoyil@users.noreply.github.com>
2022-04-22 11:06:01 +0200

    refactor: Remove masterchef calls for cake vault user balance (#3722)

--- src/state/pools/fetchPoolsUser.ts
+++ src/state/pools/fetchPoolsUser.ts
@@ -2,7 +2,6 @@ import poolsConfig from 'config/constants/pools'
 import sousChefABI from 'config/abi/sousChef.json'
 import erc20ABI from 'config/abi/erc20.json'
 import multicall from 'utils/multicall'
-import { getMasterchefContract } from 'utils/contractHelpers'
 import { getAddress } from 'utils/addressHelpers'
 import { simpleRpcProvider } from 'utils/providers'
 import BigNumber from 'bignumber.js'
@@ -12,7 +11,6 @@ import BigNumber from 'bignumber.js'
 const nonBnbPools = poolsConfig.filter((pool) => pool.stakingToken.symbol !== 'BNB')
 const bnbPools = poolsConfig.filter((pool) => pool.stakingToken.symbol === 'BNB')
 const nonMasterPools = poolsConfig.filter((pool) => pool.sousId !== 0)
-const masterChefContract = getMasterchefContract()
 
 export const fetchPoolsAllowance = async (account) => {
   const calls = nonBnbPools.map((pool) => ({
@@ -58,18 +56,13 @@ export const fetchUserStakeBalances = async (account) => {
     params: [account],
   }))
   const userInfo = await multicall(sousChefABI, calls)
-  const stakedBalances = nonMasterPools.reduce(
+  return nonMasterPools.reduce(
     (acc, pool, index) => ({
       ...acc,
       [pool.sousId]: new BigNumber(userInfo[index].amount._hex).toJSON(),
     }),
     {},
   )
-
-  // Cake / Cake pool
-  const { amount: masterPoolAmount } = await masterChefContract.userInfo('0', account)
-
-  return { ...stakedBalances, 0: new BigNumber(masterPoolAmount.toString()).toJSON() }
 }
 
 export const fetchUserPendingRewards = async (account) => {
@@ -79,16 +72,11 @@ export const fetchUserPendingRewards = async (account) => {
     params: [account],
   }))
   const res = await multicall(sousChefABI, calls)
-  const pendingRewards = nonMasterPools.reduce(
+  return nonMasterPools.reduce(
     (acc, pool, index) => ({
       ...acc,
       [pool.sousId]: new BigNumber(res[index]).toJSON(),
     }),
     {},
   )
-
-  // Cake / Cake pool
-  const pendingReward = await masterChefContract.pendingCake('0', account)
-
-  return { ...pendingRewards, 0: new BigNumber(pendingReward.toString()).toJSON() }
 }
