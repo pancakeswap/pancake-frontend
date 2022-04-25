feba9040f03f79fbf28aabd7d35136771539c0f7
Philip <97418926+chefilip@users.noreply.github.com>
2022-04-25 10:11:06 -0300

    fix: Update roi in lock modal (#3775)

--- src/views/Pools/components/LockedPool/utils/formatRoi.ts
+++ src/views/Pools/components/LockedPool/utils/formatRoi.ts
@@ -3,12 +3,13 @@ import { formatNumber } from 'utils/formatBalance'
 interface FormatRoiArgs {
   lockedApy: string
   usdValueStaked: number
+  duration: number
 }
 
 export default function formatRoi(roiArgs: FormatRoiArgs): string {
-  const { lockedApy, usdValueStaked } = roiArgs
+  const { lockedApy, usdValueStaked, duration } = roiArgs
 
-  const roi = usdValueStaked * (Number(lockedApy) / 100)
+  const roi = usdValueStaked * (Number(lockedApy) / 100) * (duration / 31449600)
 
   return formatNumber(roi, roi > 10000 ? 0 : 2, roi > 10000 ? 0 : 2)
 }
