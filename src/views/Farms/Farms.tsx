cd58441610bd8c013bdeed9bfba52f002ed654f7
ChefMomota <98292246+ChefMomota@users.noreply.github.com>
2022-04-27 15:46:15 +0800

    feat: Remove migration notification and change Finished text (#3786)
    Before:
    ![Screenshot 2022-04-27 at 2 33 39 PM](https://user-images.githubusercontent.com/98292246/165455990-33001223-cb4b-4040-8b76-b851ccce9dce.png)
    
    After:
    ![Screenshot 2022-04-27 at 2 33 01 PM](https://user-images.githubusercontent.com/98292246/165455999-db5c7547-566f-4b85-8c66-79c53ec1ca0e.png)

--- src/views/Farms/Farms.tsx
+++ src/views/Farms/Farms.tsx
@@ -26,7 +26,6 @@ import ToggleView from 'components/ToggleView/ToggleView'
 import Table from './components/FarmTable/FarmTable'
 import FarmTabButtons from './components/FarmTabButtons'
 import { RowProps } from './components/FarmTable/Row'
-import MigrationSticky from './components/MigrationSticky'
 import { DesktopColumnSchema, FarmWithStakedValue } from './components/types'
 
 const ControlContainer = styled.div`
@@ -105,7 +104,7 @@ const StyledImage = styled(Image)`
 const FinishedTextContainer = styled(Flex)`
   padding-bottom: 32px;
   flex-direction: column;
-  ${({ theme }) => theme.mediaQueries.sm} {
+  ${({ theme }) => theme.mediaQueries.md} {
     flex-direction: row;
   }
 `
@@ -370,7 +369,6 @@ const Farms: React.FC = ({ children }) => {
           </Button>
         </NextLinkFromReactRouter>
       </PageHeader>
-      <MigrationSticky />
       <Page>
         <ControlContainer>
           <ViewControls>
@@ -427,17 +425,25 @@ const Farms: React.FC = ({ children }) => {
         </ControlContainer>
         {isInactive && (
           <FinishedTextContainer>
-            <Text fontSize="20px" color="failure" pr="4px">
+            <Text fontSize={['16px', null, '20px']} color="failure" pr="4px">
               {t("Don't see the farm you are staking?")}
             </Text>
-            <FinishedTextLink
-              external
-              href="https://v1-farms.pancakeswap.finance/farms/history"
-              fontSize="20px"
-              color="failure"
-            >
-              {t('Check out v1 farms')}
-            </FinishedTextLink>
+            <Flex>
+              <FinishedTextLink href="/migration" fontSize={['16px', null, '20px']} color="failure">
+                {t('Go to migration page')}
+              </FinishedTextLink>
+              <Text fontSize={['16px', null, '20px']} color="failure" padding="0px 4px">
+                or
+              </Text>
+              <FinishedTextLink
+                external
+                color="failure"
+                fontSize={['16px', null, '20px']}
+                href="https://v1-farms.pancakeswap.finance/farms/history"
+              >
+                {t('check out v1 farms')}.
+              </FinishedTextLink>
+            </Flex>
           </FinishedTextContainer>
         )}
         {renderContent()}
