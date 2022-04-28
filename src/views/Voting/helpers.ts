26a83309d2daf1ba0b1843e16a305eb25be7cbdf
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-04-28 22:18:24 +0800

    fix(voting): Onchain query out of gas (#3800)

--- src/views/Voting/helpers.ts
+++ src/views/Voting/helpers.ts
@@ -36,7 +36,7 @@ export interface Message {
   sig: string
 }
 
-const STRATEGIES = [{ name: 'cake', params: { symbol: 'CAKE', address: tokens.cake.address, decimals: 18 } }]
+const STRATEGIES = [{ name: 'cake', params: { symbol: 'CAKE', address: tokens.cake.address, decimals: 18, max: 300 } }]
 const NETWORK = '56'
 
 /**
