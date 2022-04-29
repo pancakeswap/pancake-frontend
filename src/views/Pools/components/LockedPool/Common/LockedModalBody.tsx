457827883eb0761e839060ce6f4761d199b2fde8
chefilip <philip@pancakeswap.com>
2022-04-29 20:39:25 -0300

    chore: Update lint

--- src/views/Pools/components/LockedPool/Common/LockedModalBody.tsx
+++ src/views/Pools/components/LockedPool/Common/LockedModalBody.tsx
@@ -1,5 +1,5 @@
 import { useMemo } from 'react'
-import { Button, AutoRenewIcon, Box, Flex, MessageText, Message } from '@pancakeswap/uikit'
+import { Button, AutoRenewIcon, Box, Flex } from '@pancakeswap/uikit'
 import _noop from 'lodash/noop'
 import { useTranslation } from 'contexts/Localization'
 import { MAX_LOCK_DURATION } from 'config/constants/pools'
