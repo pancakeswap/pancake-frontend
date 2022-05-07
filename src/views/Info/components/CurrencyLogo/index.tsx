c7dd41d36aca74c9aaa574b5d4bee9f280e01375
chefmomota <momota@pancakeswap.com>
2022-05-07 15:41:29 +0800

    chore: Change default trustwallet logo url

--- src/views/Info/components/CurrencyLogo/index.tsx
+++ src/views/Info/components/CurrencyLogo/index.tsx
@@ -19,7 +19,7 @@ export const CurrencyLogo: React.FC<{
   const src = useMemo(() => {
     const checksummedAddress = isAddress(address)
     if (checksummedAddress) {
-      return `https://assets.trustwalletapp.com/blockchains/smartchain/assets/${checksummedAddress}/logo.png`
+      return `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/smartchain/assets/${checksummedAddress}/logo.png`
     }
     return null
   }, [address])
