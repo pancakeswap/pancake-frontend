305608d18d02309abf67919de8e05138e25624ae
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2022-05-09 06:49:54 +0800

    chore: Copywritings of rules

--- src/views/TradingCompetition/components/Rules/FAQs.tsx
+++ src/views/TradingCompetition/components/Rules/FAQs.tsx
@@ -35,7 +35,7 @@ const FAQ = () => {
           <FoldableText title={t('Eligible trading pairs')} mt="24px">
             <Text fontSize="14px" color="textSubtle">
               {t(
-                'Only trades on MBOX/BNB, MBOX/BUSD, CAKE/BNB and CAKE/BUSD pairs will be included in volume calculations.',
+                'Only trades on DAR/BNB, CAKE/BNB, and CAKE/BUSD pairs will be included in volume calculations. Please note that the volume from limit orders will not be included.',
               )}
             </Text>
           </FoldableText>
@@ -57,13 +57,13 @@ const FAQ = () => {
             <Text fontSize="14px" color="textSubtle">
               -{' '}
               {t(
-                'Prizes to be distributed in CAKE and MBOX in a distribution of 1:1 and shared by all members of each respective tier.',
+                'Prizes to be distributed in CAKE and DAR in a distribution of 1:5 and shared by all members of each respective tier.',
               )}
             </Text>
             <Text fontSize="14px" color="textSubtle">
               -{' '}
               {t(
-                'The price of token prizes (CAKE and MBOX) in USD will be determined as per their BUSD pair price during the tally period.',
+                'The price of token prizes ($CAKE and $DAR) in USD will be determined as per their BUSD pair price during the tally period.',
               )}
             </Text>
             <Text fontSize="14px" color="textSubtle">
