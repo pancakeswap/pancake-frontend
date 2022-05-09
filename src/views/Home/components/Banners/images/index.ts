fafc195d04011c9014b0986d567ab95a11bb5498
chefmomota <momota@pancakeswap.com>
2022-05-09 17:07:19 +0800

    chore: Show Mod banner

--- src/views/Home/components/Banners/images/index.ts
+++ src/views/Home/components/Banners/images/index.ts
@@ -3,17 +3,19 @@ import IFOImage from './IFO.png'
 import IFOMobileImage from './IFOMobile.png'
 import competitionImage from './competition.png'
 import competitionMobileImage from './competitionMobile.png'
-import mboxImage from './mbox-tc-desktop.png'
-import mboxMobileImage from './mbox-tc-mobile.png'
+import modImage from './mod-tc-desktop.png'
+import modMobileImage from './mod-tc-mobile.png'
 import lotteryMobileImage from './lotteryMobile.png'
+import modWhiteLogo from './mod-white-logo.png'
 
 export {
   lotteryImage,
   IFOImage,
   competitionImage,
-  mboxImage,
-  mboxMobileImage,
+  modImage,
+  modMobileImage,
   lotteryMobileImage,
   competitionMobileImage,
   IFOMobileImage,
+  modWhiteLogo,
 }
