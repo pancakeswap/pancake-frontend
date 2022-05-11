f9c41a59ddd1fca1c2f79c7fdb8e5a4f1cb6ef77
memoyil <2213635+memoyil@users.noreply.github.com>
2022-05-11 18:11:36 +0200

    fix: Remove differenceInDays for more precision

--- src/state/farms/fetchFarmsWithAuctions.ts
+++ src/state/farms/fetchFarmsWithAuctions.ts
@@ -1,6 +1,6 @@
 import { AuctionStatus } from 'config/constants/types'
 import { FARM_AUCTION_HOSTING_IN_DAYS } from 'config/constants'
-import { differenceInDays, add } from 'date-fns'
+import { add } from 'date-fns'
 import { getFarmAuctionContract } from '../../utils/contractHelpers'
 import { AuctionsResponse } from '../../utils/types'
 import { processAuctionData, sortAuctionBidders } from '../../views/FarmAuction/helpers'
@@ -14,7 +14,11 @@ const fetchFarmsWithAuctions = async (
   const auctionData: AuctionsResponse = await farmAuctionContract.auctions(currentAuctionId)
   const currentAuction = await processAuctionData(currentAuctionId.toNumber(), auctionData, currentBlock)
   if (currentAuction.status === AuctionStatus.Closed) {
-    if (differenceInDays(now, currentAuction.endDate) > FARM_AUCTION_HOSTING_IN_DAYS) {
+    if (
+      Math.abs(now - add(currentAuction.endDate, { days: FARM_AUCTION_HOSTING_IN_DAYS }).getTime()) /
+        (24 * 3600 * 1000) >
+      FARM_AUCTION_HOSTING_IN_DAYS
+    ) {
       return { winnerFarms: [], auctionHostingEndDate: null }
     }
     const [auctionBidders] = await farmAuctionContract.viewBidsPerAuction(currentAuctionId, 0, 500)
