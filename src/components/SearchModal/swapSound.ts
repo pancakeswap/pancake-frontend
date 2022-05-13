ad2421ce5d8ddc56728784d77ee338e94d593ee5
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-05-13 17:59:10 +0800

    chore: Update swap sound url

--- src/components/SearchModal/swapSound.ts
+++ src/components/SearchModal/swapSound.ts
@@ -1,7 +1,10 @@
 let swapSound: HTMLAudioElement
+
+const swapSoundURL = 'https://pancake.mypinata.cloud/ipfs/Qmc5kLWfQpQHu96iwre9DWSjtedXqVUgmCvehdcVPWZtc7'
+
 export const getSwapSound = () => {
   if (!swapSound) {
-    swapSound = new Audio('swap.mp3')
+    swapSound = new Audio(swapSoundURL)
   }
   return swapSound
 }
