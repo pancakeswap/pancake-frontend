45628dbe06253ce97e5ffdaefdf5280221052ed9
memoyil <2213635+memoyil@users.noreply.github.com>
2022-05-16 21:36:09 +0200

    refactor: Update method doc

--- src/utils/formatBalance.ts
+++ src/utils/formatBalance.ts
@@ -28,6 +28,7 @@ export const getFullDisplayBalance = (balance: BigNumber, decimals = 18, display
 
 /**
  * Don't use the result to convert back to number.
+ * It uses undefined locale which uses host language as a result.
  * Languages have different decimal separators which results in inconsistency when converting back this result to number.
  */
 export const formatNumber = (number: number, minPrecision = 2, maxPrecision = 2) => {
