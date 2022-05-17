ddc34fbc308f6b1b32cac6523f5788b96cabfd56
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2022-05-17 16:13:06 +0800

    feat: MoD TC UI details (#3941)
    * feat: add avatar for leaderboard
    
    * chore: wording
    
    * chore: mock fake live status
    
    * chore: UI details
    
    * chore: UI adjustment
    
    * chore: remove mock Live
    
    * chore: update translation
    
    * fix: leaderboard competitionId id

--- src/views/TradingCompetition/components/TeamRanks/TopTradersCard/GridItem.tsx
+++ src/views/TradingCompetition/components/TeamRanks/TopTradersCard/GridItem.tsx
@@ -1,8 +1,20 @@
-import { Heading, Text, Flex } from '@pancakeswap/uikit'
+import { Flex, Heading, Text, SkeletonV2 } from '@pancakeswap/uikit'
+import { useProfileForAddress } from 'state/profile/hooks'
 import styled from 'styled-components'
 import truncateHash from 'utils/truncateHash'
-import { LeaderboardDataItem } from '../../../types'
 import { localiseTradingVolume } from '../../../helpers'
+import { LeaderboardDataItem } from '../../../types'
+
+const Avatar = styled.img`
+  margin-right: 4px;
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+
+  ${({ theme }) => theme.mediaQueries.md} {
+    margin-right: 12px;
+  }
+`
 
 const Wrapper = styled.div`
   position: relative;
@@ -77,6 +89,7 @@ const GridItem: React.FC<{
   teamImages: React.ReactNode[]
 }> = ({ traderData = { address: '', volume: 0, teamId: 0, rank: 0 }, teamImages }) => {
   const { address, volume, teamId, rank } = traderData
+  const { profile, isFetching } = useProfileForAddress(address)
 
   return (
     <Wrapper>
@@ -87,6 +100,9 @@ const GridItem: React.FC<{
         <Text bold>${localiseTradingVolume(volume)}</Text>
       </Flex>
       <Flex alignItems="center" justifyContent="flex-start">
+        <SkeletonV2 width="32px" height="32px" mr={['4px', null, '12px']} isDataReady={!isFetching}>
+          <Avatar src={profile?.nft?.image?.thumbnail} />
+        </SkeletonV2>
         <Text color="primary">{truncateHash(address)}</Text>
       </Flex>
       <TeamImageWrapper justifyContent="flex-end">{teamImages[teamId - 1]}</TeamImageWrapper>
