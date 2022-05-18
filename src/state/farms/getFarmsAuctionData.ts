f417f96526015cfbb2d5ed81c3d0e438b8b4e8cb
memoyil <2213635+memoyil@users.noreply.github.com>
2022-05-18 21:57:33 +0200

    Revert "fix: (farm): Add past data"
    This reverts commit 71471c024d65bfa005683f6a19e5650c86d9cb90.

--- src/state/farms/getFarmsAuctionData.ts
+++ src/state/farms/getFarmsAuctionData.ts
@@ -3,10 +3,9 @@ import { CHAIN_ID } from '../../config/constants/networks'
 
 const getFarmsAuctionData = (farms: SerializedFarm[], winnerFarms: string[], auctionHostingEndDate: string) => {
   return farms.map((farm) => {
-    const isAuctionWinnerFarm =
-      winnerFarms.find((winnerFarm) => winnerFarm.toLowerCase() === farm.lpAddresses[CHAIN_ID].toLowerCase()) ??
-      farm.isCommunity
-
+    const isAuctionWinnerFarm = winnerFarms.find(
+      (winnerFarm) => winnerFarm.toLowerCase() === farm.lpAddresses[CHAIN_ID].toLowerCase(),
+    )
     return {
       ...farm,
       ...(isAuctionWinnerFarm && { isCommunity: true, auctionHostingEndDate }),
