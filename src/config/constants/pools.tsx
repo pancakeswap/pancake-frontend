4c20219b1224a33e461c386714881791d6695f5b
memoyil <2213635+memoyil@users.noreply.github.com>
2022-05-19 14:24:04 +0200

    fix: Extend button to not disable if at least 1 week can be extended (#3951)

--- src/config/constants/pools.tsx
+++ src/config/constants/pools.tsx
@@ -9,6 +9,7 @@ const serializedTokens = serializeTokens()
 
 export const MAX_LOCK_DURATION = 31536000
 export const UNLOCK_FREE_DURATION = 604800
+export const ONE_WEEK_DEFAULT = 604800
 export const BOOST_WEIGHT = BigNumber.from('20000000000000')
 export const DURATION_FACTOR = BigNumber.from('31536000')
 
