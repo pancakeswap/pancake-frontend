1a16f0d4a337230543a7a756b31ac7bc1eadb2ff
chefilip <philip@pancakeswap.com>
2022-05-26 10:22:34 -0300

    chore(trading-competition): Mock current status

--- src/views/TradingCompetition/MoDCompetition.tsx
+++ src/views/TradingCompetition/MoDCompetition.tsx
@@ -56,7 +56,7 @@ const MoDCompetition = () => {
   const { isMobile } = useMatchBreakpoints()
   const { isDark, theme } = useTheme()
   const tradingCompetitionContract = useTradingCompetitionContractMoD(false)
-  const [currentPhase, setCurrentPhase] = useState(CompetitionPhases.CLAIM)
+  const [currentPhase, setCurrentPhase] = useState(CompetitionPhases.FINISHED)
   const { registrationSuccessful, claimSuccessful, onRegisterSuccess, onClaimSuccess } = useRegistrationClaimStatus()
   const [userTradingInformation, setUserTradingInformation] = useState<UserTradingInformation>({
     hasRegistered: false,
@@ -101,8 +101,9 @@ const MoDCompetition = () => {
 
   useEffect(() => {
     const fetchCompetitionInfoContract = async () => {
-      const competitionStatus = await tradingCompetitionContract.currentStatus()
-      setCurrentPhase(SmartContractPhases[competitionStatus])
+      // const competitionStatus = await tradingCompetitionContract.currentStatus()
+
+      setCurrentPhase(SmartContractPhases[2])
     }
 
     const fetchUserContract = async () => {
