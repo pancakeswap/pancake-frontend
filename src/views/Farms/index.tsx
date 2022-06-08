1baa3e32bdbd278ceb92ffede7a48ce693bf0c91
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-06-08 12:34:17 +0800

    fix webviewscript not loading

--- src/views/Farms/index.tsx
+++ src/views/Farms/index.tsx
@@ -1,8 +1,18 @@
 import { FC } from 'react'
+import Script from 'next/script'
 import Farms, { FarmsContext } from './Farms'
 
 export const FarmsPageLayout: FC = ({ children }) => {
   return <Farms>{children}</Farms>
+}
+
+export const FarmsMpPageLayout: FC = ({ children }) => {
+  return (
+    <>
+      <Script src="https://public.bnbstatic.com/static/js/mp-webview-sdk/webview-v1.0.0.min.js" id="mp-webview" />
+      <Farms>{children}</Farms>
+    </>
+  )
 }
 
 export { FarmsContext }
