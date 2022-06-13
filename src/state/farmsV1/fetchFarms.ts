e81c7f94cf37ab0aaf4d87c3a0895b947ba33ae4
memoyil <2213635+memoyil@users.noreply.github.com>
2022-06-13 12:40:40 +0200

    refactor: Use memoized function for full decimal multiplier

--- src/state/farmsV1/fetchFarms.ts
+++ src/state/farmsV1/fetchFarms.ts
@@ -1,6 +1,7 @@
 import { SerializedFarmConfig } from 'config/constants/types'
 import BigNumber from 'bignumber.js'
-import { BIG_TEN, BIG_ZERO, BIG_TWO } from '../../utils/bigNumber'
+import { getFullDecimalMultiplier } from 'utils/getFullDecimalMultiplier'
+import { BIG_ZERO, BIG_TWO } from '../../utils/bigNumber'
 import { fetchPublicFarmsData } from './fetchPublicFarmData'
 import { fetchMasterChefData } from './fetchMasterChefData'
 
@@ -18,8 +19,8 @@ const fetchFarms = async (farmsToFetch: SerializedFarmConfig[]) => {
     const lpTokenRatio = new BigNumber(lpTokenBalanceMC).div(new BigNumber(lpTotalSupply))
 
     // Raw amount of token in the LP, including those not staked
-    const tokenAmountTotal = new BigNumber(tokenBalanceLP).div(BIG_TEN.pow(tokenDecimals))
-    const quoteTokenAmountTotal = new BigNumber(quoteTokenBalanceLP).div(BIG_TEN.pow(quoteTokenDecimals))
+    const tokenAmountTotal = new BigNumber(tokenBalanceLP).div(getFullDecimalMultiplier(tokenDecimals))
+    const quoteTokenAmountTotal = new BigNumber(quoteTokenBalanceLP).div(getFullDecimalMultiplier(quoteTokenDecimals))
 
     // Amount of quoteToken in the LP that are staked in the MC
     const quoteTokenAmountMc = quoteTokenAmountTotal.times(lpTokenRatio)
