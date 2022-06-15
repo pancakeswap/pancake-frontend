4f5dbaa42f1cd75be5b8a6239763a98bbd014a9a
memoyil <2213635+memoyil@users.noreply.github.com>
2022-06-15 16:23:25 +0200

    fix: Forward ref warning on button component

--- packages/uikit/src/components/Button/Button.tsx
+++ packages/uikit/src/components/Button/Button.tsx
@@ -1,44 +1,46 @@
-import React, { cloneElement, ElementType, isValidElement } from "react";
+import React, { cloneElement, ElementType, isValidElement, forwardRef, ForwardedRef } from "react";
 import getExternalLinkProps from "../../util/getExternalLinkProps";
 import StyledButton from "./StyledButton";
 import { ButtonProps, scales, variants } from "./types";
 
-const Button = <E extends ElementType = "button">(props: ButtonProps<E>): JSX.Element => {
-  const { startIcon, endIcon, external, className, isLoading, disabled, children, ...rest } = props;
-  const internalProps = external ? getExternalLinkProps() : {};
-  const isDisabled = isLoading || disabled;
-  const classNames = className ? [className] : [];
+const Button = forwardRef(
+  <E extends ElementType = "button">(props: ButtonProps<E>, _: ForwardedRef<any>): JSX.Element => {
+    const { startIcon, endIcon, external, className, isLoading, disabled, children, ...rest } = props;
+    const internalProps = external ? getExternalLinkProps() : {};
+    const isDisabled = isLoading || disabled;
+    const classNames = className ? [className] : [];
 
-  if (isLoading) {
-    classNames.push("pancake-button--loading");
+    if (isLoading) {
+      classNames.push("pancake-button--loading");
+    }
+
+    if (isDisabled && !isLoading) {
+      classNames.push("pancake-button--disabled");
+    }
+
+    return (
+      <StyledButton
+        $isLoading={isLoading}
+        className={classNames.join(" ")}
+        disabled={isDisabled}
+        {...internalProps}
+        {...rest}
+      >
+        <>
+          {isValidElement(startIcon) &&
+            cloneElement(startIcon, {
+              mr: "0.5rem",
+            })}
+          {children}
+          {isValidElement(endIcon) &&
+            cloneElement(endIcon, {
+              ml: "0.5rem",
+            })}
+        </>
+      </StyledButton>
+    );
   }
-
-  if (isDisabled && !isLoading) {
-    classNames.push("pancake-button--disabled");
-  }
-
-  return (
-    <StyledButton
-      $isLoading={isLoading}
-      className={classNames.join(" ")}
-      disabled={isDisabled}
-      {...internalProps}
-      {...rest}
-    >
-      <>
-        {isValidElement(startIcon) &&
-          cloneElement(startIcon, {
-            mr: "0.5rem",
-          })}
-        {children}
-        {isValidElement(endIcon) &&
-          cloneElement(endIcon, {
-            ml: "0.5rem",
-          })}
-      </>
-    </StyledButton>
-  );
-};
+);
 
 Button.defaultProps = {
   isLoading: false,
