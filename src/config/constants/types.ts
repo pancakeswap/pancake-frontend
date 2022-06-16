bc3da973d94c0f39a21f814546b56d4350f41373
memoyil <2213635+memoyil@users.noreply.github.com>
2022-06-16 14:14:16 +0200

    refactor: (pools): Remove redundant sort order

--- src/config/constants/types.ts
+++ src/config/constants/types.ts
@@ -95,7 +95,6 @@ interface PoolConfigBaseProps {
   contractAddress: Address
   poolCategory: PoolCategory
   tokenPerBlock: string
-  sortOrder?: number
   harvest?: boolean
   isFinished?: boolean
   enableEmergencyWithdraw?: boolean
