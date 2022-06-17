33387b7d783c2748fa2aada3b81fb1cf02b0c6b1
kim-pink-ba <kimpinkba@gmail.com>
2022-06-17 14:39:09 +0800

    refactor: Move Navbar to components folder

--- src/views/Farms/index.tsx
+++ src/views/Farms/index.tsx
@@ -2,8 +2,8 @@ import { FC, useState } from 'react'
 import Script from 'next/script'
 import { listenOnBnMessage, useInterceptLink } from 'utils/mpBridge'
 import { useActiveHandle, getAccount } from 'hooks/useEagerConnect.bmp'
+import Navbar from 'components/Navbar.bmp'
 import Farms, { FarmsContext } from './Farms'
-import Navbar from './Navbar.bmp'
 
 export const FarmsPageLayout: FC = ({ children }) => {
   return <Farms>{children}</Farms>
