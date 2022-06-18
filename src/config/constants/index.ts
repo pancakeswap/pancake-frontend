38ff85185d6092a792c2ae3bedc6ac211c21b7cb
memoyil <2213635+memoyil@users.noreply.github.com>
2022-06-18 10:14:56 +0200

    perf: Introduce big_int_ten

--- src/config/constants/index.ts
+++ src/config/constants/index.ts
@@ -71,6 +71,7 @@ export const INITIAL_ALLOWED_SLIPPAGE = 50
 export const DEFAULT_DEADLINE_FROM_NOW = 60 * 20
 
 export const BIG_INT_ZERO = JSBI.BigInt(0)
+export const BIG_INT_TEN = JSBI.BigInt(10)
 
 // one basis point
 export const BIPS_BASE = JSBI.BigInt(10000)
@@ -85,7 +86,7 @@ export const PRICE_IMPACT_WITHOUT_FEE_CONFIRM_MIN: Percent = new Percent(JSBI.Bi
 export const BLOCKED_PRICE_IMPACT_NON_EXPERT: Percent = new Percent(JSBI.BigInt(1500), BIPS_BASE) // 15%
 
 // used to ensure the user doesn't send so much BNB so they end up with <.01
-export const MIN_BNB: JSBI = JSBI.exponentiate(JSBI.BigInt(10), JSBI.BigInt(16)) // .01 BNB
+export const MIN_BNB: JSBI = JSBI.exponentiate(BIG_INT_TEN, JSBI.BigInt(16)) // .01 BNB
 export const BETTER_TRADE_LESS_HOPS_THRESHOLD = new Percent(JSBI.BigInt(50), BIPS_BASE)
 
 export const ZERO_PERCENT = new Percent('0')
