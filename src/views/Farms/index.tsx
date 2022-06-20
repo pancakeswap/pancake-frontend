2fe9250ca92038196a8e5c5b0630c2c3904861cd
kim-pink-ba <100395873+kim-pink-ba@users.noreply.github.com>
2022-06-20 11:20:22 +0800

    fix: Fix miniProgrampaths undefined error (#4106)
    * fix: Fix miniProgrampaths undefined error
    
    * fix: Fix lint warning
    
    * Modify suffix of useEagerConnect.bmp
    
    * fix cannot found FarmsMpPageLayout in _mp/history

--- src/views/Farms/index.tsx
+++ src/views/Farms/index.tsx
@@ -1,8 +1,4 @@
-import { FC, useState } from 'react'
-import Script from 'next/script'
-import { listenOnBnMessage, useInterceptLink } from 'utils/mpBridge'
-import { useActiveHandle, getAccount } from 'hooks/useEagerConnect.bmp'
-import Navbar from 'components/Navbar.bmp'
+import { FC } from 'react'
 import Farms, { FarmsContext } from './Farms'
 
 export const FarmsPageLayout: FC = ({ children }) => {
