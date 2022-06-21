752c839f4387a21aee237b3ca1d19f352cae50d1
chefmomota <momota@pancakeswap.com>
2022-06-21 16:00:12 +0800

    chore: Using balance component

--- src/views/Pottery/components/FinishedRounds/AllHistoryCard/PreviousRoundCardBody.tsx
+++ src/views/Pottery/components/FinishedRounds/AllHistoryCard/PreviousRoundCardBody.tsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
 import { Box, Flex, Text, CardBody, CardRibbon, LinkExternal } from '@pancakeswap/uikit'
+import Balance from 'components/Balance'
 import { useTranslation } from 'contexts/Localization'
+import { usePriceCakeBusd } from 'state/farms/hooks'
 import Divider from 'components/Divider'
 import { getBscScanLink } from 'utils'
 import Winner from './Winner'
@@ -39,6 +41,7 @@ const WinnersContainer = styled(Flex)`
 
 const PreviousRoundCardBody: React.FC = () => {
   const { t } = useTranslation()
+  const cakePriceBusd = usePriceCakeBusd()
 
   return (
     <StyledCardBody>
@@ -67,24 +70,29 @@ const PreviousRoundCardBody: React.FC = () => {
         <Text fontSize="20px" textAlign={['center', 'center', 'left']} lineHeight="110%" bold>
           {t('Prize Pot')}
         </Text>
-        <Text
+        <Balance
+          prefix="~$"
           bold
           color="secondary"
           lineHeight="110%"
           fontSize={['32px', '32px', '40px']}
           textAlign={['center', 'center', 'left']}
-        >
-          ~$2,668,232
-        </Text>
-        <Text fontSize="14px" textAlign={['center', 'center', 'left']} color="textSubtle" mb="18px">
-          212,232 CAKE
-        </Text>
+          decimals={0}
+          value={2668232}
+        />
+        <Balance
+          unit=" CAKE"
+          mb="18px"
+          fontSize="14px"
+          color="textSubtle"
+          textAlign={['center', 'center', 'left']}
+          decimals={0}
+          value={212232}
+        />
         <Flex flexDirection={['column', 'column', 'row']} justifyContent="space-between">
           <Flex alignSelf={['center', 'center', 'flex-end']}>
             <Text fontSize="14px">{t('Total players this round:')}</Text>
-            <Text fontSize="14px" bold ml="4px">
-              2,890
-            </Text>
+            <Balance bold ml="4px" fontSize="14px" decimals={0} value={2890} />
           </Flex>
           <LinkExternal m={['auto', 'auto', '0px']} href={getBscScanLink('todo.userAddress', 'address')}>
             {t('View on BscScan')}
