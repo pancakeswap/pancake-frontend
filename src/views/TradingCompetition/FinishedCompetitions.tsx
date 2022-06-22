5859e80fef6935724cfa21e89a8e5521c41a2801
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2022-06-23 07:47:29 +0800

    chore: finished TC layout

--- src/views/TradingCompetition/FinishedCompetitions.tsx
+++ src/views/TradingCompetition/FinishedCompetitions.tsx
@@ -1,40 +1,17 @@
-import React from 'react'
 import { Flex } from '@pancakeswap/uikit'
-import { useTranslation } from 'contexts/Localization'
-import FanTokenAllBunnies from './pngs/fan-token-all-bunnies.png'
-import MoboxAllBunnies from './pngs/mobox-all-bunnies.png'
-import SubMenu from './components/SubMenu'
+import React from 'react'
 import {
-  MoboxHistoricalBanner,
-  FanTokenHistoricalBanner,
   EasterTradingHistoricalBanner,
+  FanTokenHistoricalBanner,
+  MoboxHistoricalBanner,
 } from './components/HistoricalBanner'
-import FinishedCompetitionBanner from './components/FinishedCompetitionBanner'
+import SubMenu from './components/SubMenu'
 
 const FinishedCompetitions: React.FC = () => {
-  const { t } = useTranslation()
   return (
     <>
       <SubMenu />
-      <Flex flexDirection="column" justifyContent="center" alignItems="center" px="3rem" mb="24px" mt="24px">
-        {/* <FinishedCompetitionBanner
-          title={t('Mobox Competition')}
-          imgSrc={MoboxAllBunnies}
-          background="radial-gradient(329.58% 50% at 50% 50%, #3B2864 0%, #191326 100%)"
-          to="/competition/finished/mobox"
-        />
-        <FinishedCompetitionBanner
-          title={t('Binance Fan token Trading Competition')}
-          imgSrc={FanTokenAllBunnies}
-          background="linear-gradient(#7645d9 0%, #452a7a 100%)"
-          to="/competition/finished/fantoken"
-        />
-        <FinishedCompetitionBanner
-          title={t('Easter Trading Competition')}
-          imgSrc="/images/tc-easter-bunnies.png"
-          background="radial-gradient(77.72% 89.66% at 79.76% 65.74%, #FEDC90 0%, #FFA514 74.5%)"
-          to="/competition/finished/easter"
-        /> */}
+      <Flex flexDirection="column" justifyContent="center" alignItems="center" mb="24px" mt="24px">
         <MoboxHistoricalBanner />
         <FanTokenHistoricalBanner />
         <EasterTradingHistoricalBanner />
