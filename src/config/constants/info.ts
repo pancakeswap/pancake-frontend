8fad0d2f43492f7e00ca1910392fa77f7d2ebbae
memoyil <2213635+memoyil@users.noreply.github.com>
2022-06-23 10:46:04 +0200

    fix: Treasury and buyback fee constants

--- src/config/constants/info.ts
+++ src/config/constants/info.ts
@@ -4,8 +4,8 @@ export const WEEKS_IN_YEAR = 52.1429
 
 export const TOTAL_FEE = 0.0025
 export const LP_HOLDERS_FEE = 0.0017
-export const TREASURY_FEE = 0.0003
-export const BUYBACK_FEE = 0.0005
+export const TREASURY_FEE = 0.000225
+export const BUYBACK_FEE = 0.000575
 
 export const PCS_V2_START = 1619136000 // April 23, 2021, 12:00:00 AM
 export const ONE_DAY_UNIX = 86400 // 24h * 60m * 60s
