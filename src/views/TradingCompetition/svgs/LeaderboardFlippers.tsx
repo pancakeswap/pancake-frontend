5bcc7719b695c459a6057fefcca8874f5443bf37
Ravikash Gupta <98596722+ravikaashg@users.noreply.github.com>
2022-06-23 19:06:49 -0600

    Update LeaderboardFlippers.tsx

--- src/views/TradingCompetition/svgs/LeaderboardFlippers.tsx
+++ src/views/TradingCompetition/svgs/LeaderboardFlippers.tsx
@@ -3,7 +3,7 @@ import { Svg, SvgProps } from '@pancakeswap/uikit'
 const RibbonDownMid: React.FC<SvgProps> = (props) => {
   return (
    <Svg viewBox="0 0 198 199" {...props}>
-      <image width="200" height="200" href="/images/teams/9.png"/>
+      <image width="200" height="200" href="/images/teams/9.svg"/>
     </Svg>
   )
 }
