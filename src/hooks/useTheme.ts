7a256eb51d6a7d088f19a2ccfec07931f5028fd1
Ravikash Gupta <98596722+ravikaashg@users.noreply.github.com>
2022-06-23 22:18:06 -0600

    Update useTheme.ts

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -5,7 +5,7 @@ import { useTheme as useNextTheme } from 'next-themes'
 const useTheme = () => {
   const { resolvedTheme, setTheme } = useNextTheme()
   const theme = useContext(StyledThemeContext)
-  return { isDark: resolvedTheme === 'dark', theme, setTheme }
+  return { isLight: resolvedTheme === 'light', theme, setTheme }
 }
 
 export default useTheme
