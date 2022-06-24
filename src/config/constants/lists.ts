bb255095621a0cadf28f2319a588ebf34428b2d8
Ravikash Gupta <98596722+ravikaashg@users.noreply.github.com>
2022-06-24 11:25:34 -0600

    Update lists.ts

--- src/config/constants/lists.ts
+++ src/config/constants/lists.ts
@@ -1,10 +1,11 @@
 const PANCAKE_EXTENDED = 'https://tokens.pancakeswap.finance/pancakeswap-extended.json'
 const PANCAKE_TOP100 = 'https://tokens.pancakeswap.finance/pancakeswap-top-100.json'
-const COINGECKO = 'https://tokens.pancakeswap.finance/coingecko.json'
-const CMC = 'https://tokens.pancakeswap.finance/cmc.json'
+const COINGECKO = 'https://metaegg.io/coingecko.json'
+const CMC = 'https://metaegg.io/cmc.json'
+const METAEGG_LIST = 'https://metaegg.io/metaegg-list.json'
 
 // List of official tokens list
-export const OFFICIAL_LISTS = [PANCAKE_EXTENDED, PANCAKE_TOP100]
+export const OFFICIAL_LISTS = [METAEGG_LIST, CMC]
 
 export const UNSUPPORTED_LIST_URLS: string[] = []
 export const WARNING_LIST_URLS: string[] = []
@@ -15,6 +16,7 @@ export const DEFAULT_LIST_OF_LISTS: string[] = [
   PANCAKE_TOP100,
   CMC,
   COINGECKO,
+  METAEGG_LIST,
   ...UNSUPPORTED_LIST_URLS, // need to load unsupported tokens as well
   ...WARNING_LIST_URLS,
 ]
