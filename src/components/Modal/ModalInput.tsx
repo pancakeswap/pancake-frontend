eb96b01725e391b295dad492cc19776c0da617e9
ChefMomota <98292246+ChefMomota@users.noreply.github.com>
2022-06-24 13:04:56 +0800

    perf: Modal input css (#4132)

--- src/components/Modal/ModalInput.tsx
+++ src/components/Modal/ModalInput.tsx
@@ -16,20 +16,12 @@ interface ModalInputProps {
   decimals?: number
 }
 
-const getBoxShadow = ({ isWarning = false, theme }) => {
-  if (isWarning) {
-    return theme.shadows.warning
-  }
-
-  return theme.shadows.inset
-}
-
 const StyledTokenInput = styled.div<InputProps>`
   display: flex;
   flex-direction: column;
   background-color: ${({ theme }) => theme.colors.input};
   border-radius: 16px;
-  box-shadow: ${getBoxShadow};
+  box-shadow: ${({ theme, isWarning }) => (isWarning ? theme.colors.warning : theme.shadows.inset)};
   color: ${({ theme }) => theme.colors.text};
   padding: 8px 16px 8px 0;
   width: 100%;
