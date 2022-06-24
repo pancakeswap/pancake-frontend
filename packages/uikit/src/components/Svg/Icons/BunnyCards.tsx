26121ce1b1080a71385536a2c0a7c81059c1cf53
Ravikash Gupta <98596722+ravikaashg@users.noreply.github.com>
2022-06-24 15:37:47 -0600

    Update BunnyCards.tsx

--- packages/uikit/src/components/Svg/Icons/BunnyCards.tsx
+++ packages/uikit/src/components/Svg/Icons/BunnyCards.tsx
@@ -5,7 +5,7 @@ import { SvgProps } from "../types";
 const Icon: React.FC<SvgProps> = (props) => {
   return (
     <Svg viewBox="0 0 198 199" {...props}>
-      <image width="200" height="200" href="/images/metaegg.png"/>
+      <image width="200" height="200" href="/images/ifo-2.png"/>
     </Svg>
    
   );
