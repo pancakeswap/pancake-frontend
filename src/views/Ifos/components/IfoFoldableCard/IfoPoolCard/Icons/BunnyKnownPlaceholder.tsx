5fb5cd94829b05fb85c4400e3c59f739538fb5bf
Ravikash Gupta <98596722+ravikaashg@users.noreply.github.com>
2022-06-24 15:14:18 -0600

    Update BunnyKnownPlaceholder.tsx

--- src/views/Ifos/components/IfoFoldableCard/IfoPoolCard/Icons/BunnyKnownPlaceholder.tsx
+++ src/views/Ifos/components/IfoFoldableCard/IfoPoolCard/Icons/BunnyKnownPlaceholder.tsx
@@ -8,7 +8,7 @@ const BunnyKnownPlaceholder: React.FC<SvgProps> = (props) => {
 
   return (
    <Svg viewBox="0 0 198 199" {...props}>
-      <image width="200" height="200" href="/images/metaegg-logo-200.png"/>
+      <image width="200" height="200" href="/images/ifo-1.png"/>
     </Svg>
   )
 }
