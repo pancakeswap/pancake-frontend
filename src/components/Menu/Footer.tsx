8c62367704c81b63b9eff983b66f11d41db8aaca
Ravikash Gupta <98596722+ravikaashg@users.noreply.github.com>
2022-06-24 12:45:55 -0600

    Update Footer.tsx

--- src/components/Menu/Footer.tsx
+++ src/components/Menu/Footer.tsx
@@ -1,6 +1,6 @@
 import { memo } from 'react'
 import styled from 'styled-components'
-import { ButtonMenu, ButtonMenuItem, LinkExternal, Flex, Svg, Image, Button } from '@pancakeswap/uikit'
+import { LinkExternal, Flex, Svg, Image, Button } from '@pancakeswap/uikit'
 import { useTranslation } from 'contexts/Localization'
 import { EXCHANGE_DOCS_URLS } from 'config/constants'
 
@@ -47,12 +47,7 @@ const Footer: React.FC<{ variant?: FooterVariant; helpUrl?: string }> = ({
   return (
     <Wrapper $isSide={isSide}>
       <Flex flexDirection={isSide ? 'column' : ['column', 'column', 'row']} alignItems="center">
-        <ButtonMenu variant="subtle" scale="sm" activeIndex={0}>
-          <ButtonMenuItem>V2</ButtonMenuItem>
-          <ButtonMenuItem as="a" href="https://v1exchange.pancakeswap.finance/#/">
-            {t('V1 (old)')}
-          </ButtonMenuItem>
-        </ButtonMenu>
+       
         <LinkExternal
           id="ercBridge"
           href="https://docs.binance.org/smart-chain/guides/cross-chain.html"
