dea38c031fabf5a4de9703f384a8dc4b789428da
Ravikash Gupta <98596722+ravikaashg@users.noreply.github.com>
2022-06-24 19:30:14 -0600

    Update ProfileRequirementWarning.tsx

--- src/views/Pools/components/ProfileRequirementWarning.tsx
+++ src/views/Pools/components/ProfileRequirementWarning.tsx
@@ -17,10 +17,10 @@ export function ProfileRequirementWarning({
         <MessageText>
           {notMeetRequired &&
             notMeetThreshold &&
-            t('This pool requires active Pancake Profile and %amount% profile points.', {
+            t('This pool requires active Metaegg Profile and %amount% profile points.', {
               amount: profileRequirement.thresholdPoints.toNumber().toLocaleString(),
             })}
-          {notMeetRequired && !notMeetThreshold && t('This pool requires active Pancake Profile')}
+          {notMeetRequired && !notMeetThreshold && t('This pool requires active Metaegg Profile')}
           {!notMeetRequired &&
             notMeetThreshold &&
             t('This pool requires %amount% profile points.', {
