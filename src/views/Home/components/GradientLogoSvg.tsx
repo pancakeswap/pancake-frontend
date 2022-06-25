79d11a837cb2aa4ce5ea62435ad857ac565ea19f
Ravikash Gupta <98596722+ravikaashg@users.noreply.github.com>
2022-06-24 18:52:21 -0600

    Update GradientLogoSvg.tsx

--- src/views/Home/components/GradientLogoSvg.tsx
+++ src/views/Home/components/GradientLogoSvg.tsx
@@ -4,7 +4,7 @@ const GradientLogo: React.FC<SvgProps> = (props) => {
   return (
     <Svg viewBox="0 0 48 48" {...props}>
      <Svg viewBox="0 0 198 199" {...props}>
-      <image width="240" height="240" href="/images/metaegg.png"/>
+      <image width="240" height="240" href="/images/metaegg-logo-200.png"/>
     </Svg>
     </Svg>
   )
