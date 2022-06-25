e43d1735882b100b31769c117c491f14acc178cf
Ravikash Gupta <98596722+ravikaashg@users.noreply.github.com>
2022-06-24 21:55:36 -0600

    Update hooks.ts

--- src/state/farmsV1/hooks.ts
+++ src/state/farmsV1/hooks.ts
@@ -60,7 +60,7 @@ export const usePollFarmsV1WithUserData = () => {
 
 /**
  * Fetches the "core" farm data used globally
- * 251 = CAKE-BNB LP
+ * 251 = MEGG-BNB LP
  * 252 = BUSD-BNB LP
  */
 export const usePollCoreFarmData = () => {
