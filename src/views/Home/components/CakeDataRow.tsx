886fd75f22217b2effcd3fe7dd165f18e9784fb2
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-06-28 16:35:03 +0800

    chore: Update emission number (#4156)

--- src/views/Home/components/CakeDataRow.tsx
+++ src/views/Home/components/CakeDataRow.tsx
@@ -62,7 +62,7 @@ const Grid = styled.div`
   }
 `
 
-const emissionsPerBlock = 14.25
+const emissionsPerBlock = 13.75
 
 /**
  * User (Planet Finance) built a contract on top of our original manual CAKE pool,
