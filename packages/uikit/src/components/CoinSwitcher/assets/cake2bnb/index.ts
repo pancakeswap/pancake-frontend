0cf15b614fb683c461ef71b3d8e7e7fecefd603e
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2022-06-28 12:56:09 +0800

    chore: sequence assets host by s3

--- packages/uikit/src/components/CoinSwitcher/assets/cake2bnb/index.ts
+++
@@ -1,8 +0,0 @@
-export function importAll(r: any): string[] {
-  return r.keys().map(r);
-}
-
-export const cake2BnbImages = importAll(require.context("./", false, /\.(png|jpe?g|svg)$/)).map(
-  // @ts-ignore
-  (d) => d.default.src
-);
