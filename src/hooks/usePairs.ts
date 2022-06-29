414c8edfa3f9881f56e84631673b99edf3e36200
memoyil <2213635+memoyil@users.noreply.github.com>
2022-06-29 18:10:59 +0200

    perf: Reduce renders in import lp pool finder

--- src/hooks/usePairs.ts
+++ src/hooks/usePairs.ts
@@ -69,5 +69,6 @@ export function usePairs(currencies: [Currency | undefined, Currency | undefined
 }
 
 export function usePair(tokenA?: Currency, tokenB?: Currency): [PairState, Pair | null] {
-  return usePairs([[tokenA, tokenB]])[0]
+  const pairCurrencies = useMemo<[Currency, Currency][]>(() => [[tokenA, tokenB]], [tokenA, tokenB])
+  return usePairs(pairCurrencies)[0]
 }
