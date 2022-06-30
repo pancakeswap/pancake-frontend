bece012851f1ed6b50a96afa1b9b8c8dbb3dfe2a
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2022-06-30 16:31:10 +0800

    chore: add title for zap error

--- src/views/AddLiquidity/components/ZapErrorMessage.tsx
+++ src/views/AddLiquidity/components/ZapErrorMessage.tsx
@@ -1,5 +1,5 @@
 import { memo } from 'react'
-import { Message, MessageText, Button, useModal, Box } from '@pancakeswap/uikit'
+import { Message, MessageText, Button, useModal, Box, Flex } from '@pancakeswap/uikit'
 import { useTranslation } from 'contexts/Localization'
 
 import SettingsModal from '../../../components/Menu/GlobalSettings/SettingsModal'
@@ -11,26 +11,28 @@ export const ZapErrorMessages: React.FC<{ isSingleToken: boolean; onModalDismiss
     return (
       <Box width={['280px', '380px']}>
         <Message variant="warning" mb="16px">
-          <MessageText small>
-            {isSingleToken
-              ? t('Zap does NOT support tokens with fees on transfers. If you experience any issues, disable Zap')
-              : t(
-                  `Zap does NOT support tokens with fees on transfers. Please choose ’Don't convert’ and retry. If you experience any further issues, disable Zap`,
-                )}{' '}
-            <Button
-              scale="sm"
-              variant="text"
-              height="auto"
-              onClick={() => {
-                if (onModalDismiss) onModalDismiss()
-                onPresentSettingsModal()
-              }}
-              p="0"
-            >
-              {t('here.')}
-            </Button>
-            .
-          </MessageText>
+          <Flex flexDirection="column">
+            <MessageText bold>{t('Currently using Zap for liquidity provisions.')}</MessageText>
+            <MessageText small>
+              {isSingleToken
+                ? t('Zap does NOT support tokens with fees on transfers. If you experience any issues, disable Zap')
+                : t(
+                    `Zap does NOT support tokens with fees on transfers. Please choose ’Don't convert’ and retry. If you experience any further issues, disable Zap`,
+                  )}{' '}
+              <Button
+                scale="sm"
+                variant="text"
+                height="auto"
+                onClick={() => {
+                  if (onModalDismiss) onModalDismiss()
+                  onPresentSettingsModal()
+                }}
+                p="0"
+              >
+                {t('here.')}
+              </Button>
+            </MessageText>
+          </Flex>
         </Message>
       </Box>
     )
