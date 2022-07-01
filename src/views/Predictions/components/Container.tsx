d4c5e88c235422ba813c5e0904a8cd4f1756f50e
memoyil <2213635+memoyil@users.noreply.github.com>
2022-07-01 13:16:19 +0200

    fix: (prediction): Charts in small size mobile phones (#4182)

--- src/views/Predictions/components/Container.tsx
+++ src/views/Predictions/components/Container.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 
 const Container = styled.div`
   background: ${({ theme }) => theme.colors.gradients.violetAlt};
-  @media only screen and (max-width: 576px) and (min-height: 740px) {
+  @media only screen and (max-width: 575px) and (min-height: 740px) {
     height: calc(100vh - 150px);
   }
   ${({ theme }) => theme.mediaQueries.sm} {
