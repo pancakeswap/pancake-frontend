68a621b7bcb8a6f131b07ee31089aa6d40f2b537
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2022-07-01 16:59:53 +0800

    chore: swipe down to close

--- packages/uikit/src/widgets/Modal/Modal.tsx
+++ packages/uikit/src/widgets/Modal/Modal.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useTheme } from "styled-components";
 import Heading from "../../components/Heading/Heading";
 import getThemeValue from "../../util/getThemeValue";
 import { ModalBody, ModalHeader, ModalTitle, ModalContainer, ModalCloseButton, ModalBackButton } from "./styles";
 import { ModalProps } from "./types";
+import { useMatchBreakpointsContext } from "../../contexts";
 
 const Modal: React.FC<ModalProps> = ({
   title,
@@ -17,9 +18,23 @@ const Modal: React.FC<ModalProps> = ({
   ...props
 }) => {
   const theme = useTheme();
+  const { isMobile } = useMatchBreakpointsContext();
+  const wrapperRef = useRef<HTMLDivElement>(null);
   return (
     // @ts-ignore
-    <ModalContainer minWidth={minWidth} {...props}>
+    <ModalContainer
+      drag={isMobile ? "y" : false}
+      dragConstraints={{ top: -50, bottom: 100 }}
+      minWidth={minWidth}
+      onDragStart={() => {
+        if (wrapperRef.current) wrapperRef.current.style.animation = "none";
+      }}
+      onDragEnd={(e, info) => {
+        if (info.offset.y > 50 && onDismiss) onDismiss();
+      }}
+      ref={wrapperRef}
+      {...props}
+    >
       <ModalHeader background={getThemeValue(`colors.${headerBackground}`, headerBackground)(theme)}>
         <ModalTitle>
           {onBack && <ModalBackButton onBack={onBack} />}
