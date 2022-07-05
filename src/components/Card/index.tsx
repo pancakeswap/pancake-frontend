56ed42bddd73ba5ee0a76c4ffbf9063df51989a2
memoyil <2213635+memoyil@users.noreply.github.com>
2022-07-05 11:16:53 +0200

    Revert "fix: Border color change"
    This reverts commit b6565c87298e7b52b2974567e887c9335f6e38ed.

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -16,7 +16,7 @@ const Card = styled(Box)<{
 export default Card
 
 export const LightCard = styled(Card)`
-  border: 1px solid ${({ theme }) => theme.colors.cardBorder};
+  border: 1px solid ${({ theme }) => theme.colors.background};
   background-color: ${({ theme }) => theme.colors.backgroundAlt};
 `
 
