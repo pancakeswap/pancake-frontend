130bf8052e2c1e6ca3cca60787997c864f4236f3
chefilip <philip@pancakeswap.com>
2022-07-06 10:48:07 +0700

    chore(nft): Allow user to reclaim after delist

---
+++ src/config/constants/nftsCollections/delist.ts
@@ -0,0 +1,29 @@
+const DELIST_COLLECTIONS = {
+  '0x4D89eBf7b1F66806A4C99Df71D22b7b9efC6bB0b': {
+    active: false,
+    address: '0x4D89eBf7b1F66806A4C99Df71D22b7b9efC6bB0b',
+    avatar: '',
+    banner: {
+      large: '',
+      small: '',
+    },
+    createdAt: '',
+    creatorAddress: '',
+    creatorFee: '',
+    description: '',
+    id: '0x4d89ebf7b1f66806a4c99df71d22b7b9efc6bb0b',
+    name: 'Baked Potatoes',
+    numberTokensListed: '76',
+    owner: '0xCd1B0abBc3E55E91FCC5AEE393525e68478C2952',
+    symbol: 'POT',
+    totalSupply: '',
+    totalTrades: '',
+    totalVolumeBNB: '',
+    tradingFee: '',
+    updatedAt: '',
+    verified: true,
+    whitelistChecker: '',
+  },
+}
+
+export default DELIST_COLLECTIONS
