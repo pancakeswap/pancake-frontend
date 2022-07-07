11d1d69e604d0bc926c6197ed01c472f91fb215d
Jonathan Dyallo <Emeraldusk@gmail.com>
2022-07-07 14:41:21 -0300

    Feat/tooltip calculation (#15)
    * tooltip
    
    * bump version
    
    * fixed typo

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -6,6 +6,8 @@ import { useTranslation } from 'contexts/Localization'
 // import PhishingWarningBanner from 'components/PhishingWarningBanner'
 import useTheme from 'hooks/useTheme'
 // import { usePhishingBannerManager } from 'state/user/hooks'
+import { usePePriceArs, usePePriceUsd } from 'hooks/useBUSDPrice'
+import useARSPrice from 'hooks/useARSPrice'
 import { usePePriceArs } from 'hooks/useBUSDPrice'
 import AddToken from 'utils/addToken'
 import logo from 'assets/pe.png'
@@ -17,7 +19,9 @@ import { footerLinks } from './config/footerConfig'
 
 const Menu = (props) => {
   const { isDark, toggleTheme } = useTheme()
-  const pePriceUsd = usePePriceArs()
+  const pesosBlueAvg = useARSPrice()
+  const pePriceArs = usePePriceArs()
+  const pePriceUsdc = usePePriceUsd()
   const { currentLanguage, setLanguage, t } = useTranslation()
   const { pathname } = useLocation()
   // const [showPhishingWarningBanner] = usePhishingBannerManager()
@@ -35,12 +39,18 @@ const Menu = (props) => {
       currentLang={currentLanguage.code}
       langs={languageList}
       setLang={setLanguage}
-      cakePriceUsd={pePriceUsd}
+      cakePriceUsd={pePriceArs}
       links={config(t)}
       subLinks={activeMenuItem?.hideSubNav ? [] : activeMenuItem?.items}
       footerLinks={footerLinks(t)}
       activeItem={activeMenuItem?.href}
       activeSubItem={activeSubMenuItem?.href}
+      contentTooltip={
+        <>
+          {pesosBlueAvg && <div>1 ARS = ${(1 / pesosBlueAvg).toFixed(5)} USDC</div>}
+          {pePriceArs && <div>1 PE = ${pePriceArs.toFixed(4)} ARS</div>}
+          {pePriceUsdc && <div>1 PE = ${pePriceUsdc.toFixed(5)} USDC</div>}
+        </>
       buyCakeLabel={t('Buy CAKE')}
       onClick={() =>
         AddToken({
