b5542cceff6e9a3e15531ec33905e99efbe14ee0
memoyil <2213635+memoyil@users.noreply.github.com>
2022-07-08 01:40:18 +0200

    perf: Memoize team podium icon component (#4237)

--- src/views/TradingCompetition/components/TeamRanks/Podium/TeamPodiumIcon.tsx
+++ src/views/TradingCompetition/components/TeamRanks/Podium/TeamPodiumIcon.tsx
@@ -1,3 +1,4 @@
+import { memo } from 'react'
 import styled from 'styled-components'
 import { Flex, Image, Skeleton } from '@pancakeswap/uikit'
 import Sticker from '../../Sticker'
@@ -37,20 +38,19 @@ const TeamPodiumIcon: React.FC<PodiumIconProps> = ({ teamId, teamPosition }) => 
     2: { imgSrc: 'fearsome-flippers-lg.png', stickerCol: '#452A7A' },
     3: { imgSrc: 'chaotic-cakers-lg.png', stickerCol: '#FFB237' },
   }
-
-  const imageSize = () => (teamPosition === 1 ? 128 : 113)
+  const imageSize = teamPosition === 1 ? 128 : 113
 
   return (
-    <Wrapper imageSize={imageSize()}>
+    <Wrapper imageSize={imageSize}>
       {!teamId ? (
         <Skeleton variant="circle" width="100%" height="100%" />
       ) : (
         <Sticker backgroundColor={teamData[teamId].stickerCol} borderColor={teamData[teamId].stickerCol}>
-          <Image src={`/images/teams/${teamData[teamId].imgSrc}`} width={imageSize()} height={imageSize()} />
+          <Image src={`/images/teams/${teamData[teamId].imgSrc}`} width={imageSize} height={imageSize} />
         </Sticker>
       )}
     </Wrapper>
   )
 }
 
-export default TeamPodiumIcon
+export default memo(TeamPodiumIcon)
