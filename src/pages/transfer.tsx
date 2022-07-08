7b9e10b5dc327ede5eb368856e7bd22dcae06235
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-07-08 12:14:09 +0800

    add transfer page

--- src/pages/transfer.tsx
+++ src/pages/transfer.tsx
@@ -1,7 +1,10 @@
+import useTheme from 'hooks/useTheme'
 import Script from 'next/script'
 import { useEffect } from 'react'
+import Page from 'views/Page'
 
 function Test() {
+  const { isDark } = useTheme()
   useEffect(() => {
     customElements.whenDefined('stargate-widget').then(() => {
       // setTimeout(() => {
@@ -9,13 +12,20 @@ function Test() {
       // }, 600)
     })
   }, [])
+
   return (
-    <>
+    <Page hideFooterOnDesktop>
       <Script src="https://unpkg.com/@layerzerolabs/stargate-ui@latest/element.js" />
-      <div style={{}}>
-        <stargate-widget theme="dark" />
+      <div style={{ width: 420 }}>
+        {/* @ts-ignore */}
+        <stargate-widget
+          partnerId="105"
+          feeCollector="0xc13b65f7c53Cd6db2EA205a4b574b4a0858720A6"
+          theme={isDark ? 'dark' : 'light'}
+          tenthBps={0.4}
+        />
       </div>
-    </>
+    </Page>
   )
 }
 
