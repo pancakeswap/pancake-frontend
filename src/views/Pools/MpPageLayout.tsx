620626346199d8091c29f888634c8ba9c2026a4b
opar-xu <opar-xu>
2022-07-11 14:36:50 +0800

    feat: Add i18n support

--- src/views/Pools/MpPageLayout.tsx
+++ src/views/Pools/MpPageLayout.tsx
@@ -1,11 +1,12 @@
 import { FC, useEffect } from 'react'
-import { listenOnBnMessage, useInterceptLink } from 'utils/mpBridge'
+import { listenOnBnMessage, useInterceptLink, useInjectI18n } from 'utils/mpBridge'
 import { useActiveHandle, getAccount } from 'hooks/useEagerConnect.bmp'
 import Navbar from 'components/Navbar.bmp'
 
 listenOnBnMessage()
 const PoolsMpPageLayout: FC = ({ children }) => {
   useInterceptLink()
+  const { injected } = useInjectI18n()
   const handleActive = useActiveHandle()
 
   useEffect(() => {
@@ -21,7 +22,7 @@ const PoolsMpPageLayout: FC = ({ children }) => {
   return (
     <>
       <Navbar />
-      {children}
+      {injected && children}
     </>
   )
 }
