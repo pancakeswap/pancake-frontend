bdeae4abcb7e810b4c41c04704b36abb8fa23e4d
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-07-15 13:44:07 +0800

    rm unused

--- packages/uikit/.storybook/main.js
+++ packages/uikit/.storybook/main.js
@@ -12,12 +12,4 @@ module.exports = {
     "themeprovider-storybook/register",
   ],
   core: { builder: "@storybook/builder-vite" },
-  // webpackFinal: async (config) => {
-  //   config.module.rules.push({
-  //     test: /\.mjs$/,
-  //     include: /node_modules/,
-  //     type: "javascript/auto",
-  //   });
-  //   return config;
-  // },
 };
