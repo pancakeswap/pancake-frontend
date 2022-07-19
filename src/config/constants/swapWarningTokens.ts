45866a5425ad535f4195dd20ece0681f4d16f423
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-07-19 14:03:39 +0800

    fix warning token

--- src/config/constants/swapWarningTokens.ts
+++ src/config/constants/swapWarningTokens.ts
@@ -1,9 +1,9 @@
 import { Token } from '@pancakeswap/sdk'
 import { bscTokens } from 'config/constants/tokens'
-import { mainnetWarningTokens } from 'config/constants/warningTokens'
+import { bscWarningTokens } from 'config/constants/warningTokens'
 
 const { bondly, itam, ccar, bttold } = bscTokens
-const { pokemoney, free, safemoon } = mainnetWarningTokens
+const { pokemoney, free, safemoon } = bscWarningTokens
 
 interface WarningTokenList {
   [key: string]: Token
--- src/config/constants/warningTokens.ts
+++ src/config/constants/warningTokens.ts
@@ -1,19 +1,16 @@
 import { ChainId, Token } from '@pancakeswap/sdk'
-import { defineTokens } from './tokens'
 
-const { MAINNET } = ChainId
-
-export const mainnetWarningTokens = defineTokens({
-  pokemoney: new Token(MAINNET, '0x32ff5b4C3B1744F0344D96fA2f87799Ed2805749', 18, 'PMY', 'Pokemoney Coin', ''),
-  free: new Token(MAINNET, '0x880BCe9321c79cAc1D290De6d31DDE722C606165', 8, 'FREE', 'Freedom Protocol Token', ''),
+export const bscWarningTokens = {
+  pokemoney: new Token(ChainId.BSC, '0x32ff5b4C3B1744F0344D96fA2f87799Ed2805749', 18, 'PMY', 'Pokemoney Coin', ''),
+  free: new Token(ChainId.BSC, '0x880BCe9321c79cAc1D290De6d31DDE722C606165', 8, 'FREE', 'Freedom Protocol Token', ''),
   safemoon: new Token(
-    MAINNET,
+    ChainId.BSC,
     '0x8076C74C5e3F5852037F31Ff0093Eeb8c8ADd8D3',
     9,
     'SAFEMOON',
     'Safemoon Token',
     'https://safemoon.net/',
   ),
-} as const)
+}
 
-export const testnetWarningTokens = defineTokens({} as const)
+export const bscTestnetWarningTokens = {}
