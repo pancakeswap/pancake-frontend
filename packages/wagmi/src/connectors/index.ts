c33dd474a147838b632fdc117d3f75f02ae418e8
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-07-19 15:46:01 +0800

    feat: add wagmi package (#4292)
    * feat: add wagmi package
    
    * update export for wagmi
    
    * add mp connector
    
    * update ts config

---
+++ packages/wagmi/src/connectors/index.ts
@@ -0,0 +1,2 @@
+export * from './bscConnector'
+export * from './bnInjectedConnector'
--- src/utils/wallet.ts
+++ src/utils/wallet.ts
@@ -99,3 +99,5 @@ export const registerToken = async (
 
   return tokenAdded
 }
+
+export const canRegisterToken = () => typeof window !== 'undefined' && window?.ethereum?.isMetaMask
