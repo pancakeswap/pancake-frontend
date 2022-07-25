d4232dc97d8136d448a6a2441ddbae48bdb6c4db
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2022-07-25 13:27:22 +0800

    fix: comment part2

--- src/views/Farms/hooks/useUserBoosterStatus.ts
+++ src/views/Farms/hooks/useUserBoosterStatus.ts
@@ -8,13 +8,16 @@ export const useUserBoosterStatus = (account: string) => {
   const { data: MAX_BOOST_POOL, status: maxBoostStatus } = useSWRImmutable('maxBoostFarm', () =>
     farmBoosterContract.MAX_BOOST_POOL(),
   )
-  const { data: activatedPools, status } = useSWR(account && ['activatedBoostFarm', [account]], () =>
-    farmBoosterContract.activedPools(account),
-  )
+  const {
+    data: activatedPools,
+    status,
+    mutate,
+  } = useSWR(account && ['activatedBoostFarm', [account]], () => farmBoosterContract.activedPools(account))
   return {
     maxBoostCounts: MAX_BOOST_POOL?.toNumber() ?? 0,
     activatedPoolsCounts: activatedPools?.length ?? 0,
     remainingCounts: (MAX_BOOST_POOL?.toNumber() ?? 0) - (activatedPools?.length ?? 0),
     isLoading: maxBoostStatus !== FetchStatus.Fetched || status !== FetchStatus.Fetched,
+    refreshActivePools: mutate,
   }
 }
