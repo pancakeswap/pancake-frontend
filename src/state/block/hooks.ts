f3eadff451899b3c2ee9cb2e14848eff643ea3be
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-08-01 14:24:31 +0800

    block number

--- src/state/block/hooks.ts
+++ src/state/block/hooks.ts
@@ -1,20 +1,22 @@
 import { FAST_INTERVAL, SLOW_INTERVAL } from 'config/constants'
-import useSWR, { useSWRConfig } from 'swr'
+// eslint-disable-next-line camelcase
+import useSWR, { useSWRConfig, unstable_serialize } from 'swr'
 import useSWRImmutable from 'swr/immutable'
 import useActiveWeb3React from 'hooks/useActiveWeb3React'
+import { useBlockNumber } from 'wagmi'
 
 const REFRESH_BLOCK_INTERVAL = 6000
 
 export const usePollBlockNumber = () => {
   const { cache, mutate } = useSWRConfig()
-  const { library, chainId } = useActiveWeb3React()
+  const { chainId } = useActiveWeb3React()
+  const { data: blockNumber } = useBlockNumber({ chainId })
 
   const { data } = useSWR(
-    `blockNumber-${chainId}`,
+    ['blockNumber', chainId],
     async () => {
-      const blockNumber = await library.getBlockNumber()
-      if (!cache.get(`initialBlockNumber-${chainId}`)) {
-        mutate(`initialBlockNumber-${chainId}`, blockNumber)
+      if (!cache.get(unstable_serialize(['initialBlockNumber', chainId]))) {
+        mutate(['initialBlockNumber', chainId], blockNumber)
       }
       return blockNumber
     },
@@ -46,12 +48,12 @@ export const usePollBlockNumber = () => {
 
 export const useCurrentBlock = (): number => {
   const { chainId } = useActiveWeb3React()
-  const { data: currentBlock = 0 } = useSWRImmutable(`blockNumber-${chainId}`)
+  const { data: currentBlock = 0 } = useSWRImmutable(['blockNumber', chainId])
   return currentBlock
 }
 
 export const useInitialBlock = (): number => {
   const { chainId } = useActiveWeb3React()
-  const { data: initialBlock = 0 } = useSWRImmutable(`initialBlockNumber-${chainId}`)
+  const { data: initialBlock = 0 } = useSWRImmutable(['initialBlockNumber', chainId])
   return initialBlock
 }
