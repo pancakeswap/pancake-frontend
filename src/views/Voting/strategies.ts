b3844573a3e8c03373a8e816b93c1b7cbef287f8
ChefMomota <98292246+ChefMomota@users.noreply.github.com>
2022-08-02 10:25:31 +0800

    feat: vCake (#4298)
    * refactor: Function getVotingPower
    
    * fix: Type
    
    * chore: Update wording
    
    * feat: Get cakeVaultBalance value
    
    * feat: Add lockedCakeBalance
    
    * feat: Add vCake UI
    
    * feat: Add boosted expired
    
    * chore: Update wording
    
    * fix: Old voting
    
    * perf: Combine to one function
    
    * chore: Revert
    
    * chore: Update wording
    
    * chore: Update wording
    
    * chore: Text caps

--- src/views/Voting/strategies.ts
+++ src/views/Voting/strategies.ts
@@ -30,10 +30,10 @@ export const cakeBalanceStrategy = (version: 'v0' | 'v1') => ({
   },
 })
 
-export const cakeVaultBalanceStrategy = {
+export const cakeVaultBalanceStrategy = (version: 'v0' | 'v1') => ({
   name: 'contract-call',
   params: {
-    address: votePowerAddress.v0,
+    address: votePowerAddress[version],
     decimals: 18,
     methodABI: {
       inputs: [
@@ -55,7 +55,7 @@ export const cakeVaultBalanceStrategy = {
       type: 'function',
     },
   },
-}
+})
 
 export const ifoPoolBalanceStrategy = {
   name: 'contract-call',
@@ -138,7 +138,7 @@ export const cakeBnbLpBalanceStrategy = (version: 'v0' | 'v1') => ({
   },
 })
 
-export function creatPoolsBalanceStrategy(poolAddress, version: 'v0' | 'v1') {
+export function createPoolsBalanceStrategy(poolAddress, version: 'v0' | 'v1') {
   return {
     name: 'contract-call',
     params: {
@@ -198,6 +198,77 @@ export function createTotalStrategy(poolAddress, version: 'v0' | 'v1') {
           {
             internalType: 'uint256',
             name: '',
+            type: 'uint256',
+          },
+        ],
+        stateMutability: 'view',
+        type: 'function',
+      },
+    },
+  }
+}
+
+export function lockedCake(cakeVaultAddress, outputName: 'lockedAmount' | 'lockEndTime') {
+  return {
+    name: 'contract-call',
+    params: {
+      address: cakeVaultAddress,
+      decimals: 0,
+      output: outputName,
+      args: ['%{address}'],
+      methodABI: {
+        inputs: [
+          {
+            internalType: 'address',
+            name: '',
+            type: 'address',
+          },
+        ],
+        name: 'userInfo',
+        outputs: [
+          {
+            internalType: 'uint256',
+            name: 'shares',
+            type: 'uint256',
+          },
+          {
+            internalType: 'uint256',
+            name: 'lastDepositedTime',
+            type: 'uint256',
+          },
+          {
+            internalType: 'uint256',
+            name: 'cakeAtLastUserAction',
+            type: 'uint256',
+          },
+          {
+            internalType: 'uint256',
+            name: 'lastUserActionTime',
+            type: 'uint256',
+          },
+          {
+            internalType: 'uint256',
+            name: 'lockStartTime',
+            type: 'uint256',
+          },
+          {
+            internalType: 'uint256',
+            name: 'lockEndTime',
+            type: 'uint256',
+          },
+          {
+            internalType: 'uint256',
+            name: 'userBoostedShare',
+            type: 'uint256',
+          },
+          {
+            internalType: 'bool',
+            name: 'locked',
+            type: 'bool',
+          },
+          {
+            internalType: 'uint256',
+            name: 'lockedAmount',
             type: 'uint256',
           },
         ],
