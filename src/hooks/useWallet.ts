3de87f88769d362c72d866ed0f56a06331bef4ba
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-08-10 17:49:56 +0800

    hide uninstalled wallet

--- src/hooks/useWallet.ts
+++ src/hooks/useWallet.ts
@@ -10,22 +10,24 @@ export const useWallet = () => {
   const { login } = useAuth()
 
   const finalConnectors = useMemo(() => {
-    return walletConnectors.map((config) => {
-      const found = connectors.find((c) => c.id === config.connectorId)
-      if (!config.installed || !found.ready) {
-        if (config.connectorId === ConnectorNames.MetaMask) {
+    return walletConnectors
+      .map((config) => {
+        const found = connectors.find((c) => c.id === config.connectorId)
+        if (!(config.installed || found.ready)) {
+          if (config.connectorId === ConnectorNames.MetaMask) {
+            return {
+              ...config,
+              connectorId: ConnectorNames.Injected,
+            }
+          }
           return {
             ...config,
-            connectorId: ConnectorNames.Injected,
+            priority: 999,
           }
         }
-        return {
-          ...config,
-          priority: 999,
-        }
-      }
-      return config
-    })
+        return config
+      })
+      .filter((c) => typeof c.installed === 'undefined' || c.installed === true)
   }, [connectors])
 
   const { onPresentConnectModal } = useWalletModal(login, t, finalConnectors)
