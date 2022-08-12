ab52f4763d4b07bc2313d6f604ef8bf294141687
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-08-12 18:24:52 +0800

    fix cypress

--- src/hooks/useEagerConnect.ts
+++ src/hooks/useEagerConnect.ts
@@ -5,17 +5,19 @@ const AUTOCONNECTED_CONNECTOR_IDS = ['safe']
 
 const useEagerConnect = () => {
   const client = useClient()
-  const { connect, connectors } = useConnect()
+  const { connectAsync, connectors } = useConnect()
   useEffect(() => {
     AUTOCONNECTED_CONNECTOR_IDS.forEach((connector) => {
       const connectorInstance = connectors.find((c) => c.id === connector && c.ready)
       if (connectorInstance) {
-        connect({ connector: connectorInstance })
+        connectAsync({ connector: connectorInstance }).catch(() => {
+          client.autoConnect()
+        })
       } else {
         client.autoConnect()
       }
     })
-  }, [client, connect, connectors])
+  }, [client, connectAsync, connectors])
 }
 
 export default useEagerConnect
