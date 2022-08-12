5ccbae0b87551da6a6f0057789e5de0c4fb2ff1f
chefilip <philip@pancakeswap.com>
2022-08-12 11:31:27 +0700

    chore: Update Learn More link in Booster Tooltip

--- src/views/Farms/components/YieldBooster/components/ActionButton.tsx
+++ src/views/Farms/components/YieldBooster/components/ActionButton.tsx
@@ -32,7 +32,10 @@ const BoosterTooltip = () => {
       {t(
         `Boost multiplier is calculated based on the staking conditions from both Farms and fixed-term CAKE syrup pool and will be automatically updated upon user actions.`,
       )}
-      <LinkExternal href="https://docs.pancakeswap.finance/" external>
+      <LinkExternal
+        href="https://docs.pancakeswap.finance/products/yield-farming/bcake/faq#how-are-the-bcake-multipliers-calculated"
+        external
+      >
         {t('Learn More')}
       </LinkExternal>
     </>
