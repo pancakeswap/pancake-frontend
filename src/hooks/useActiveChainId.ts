46db3cd3f389a5bc16c4ef2cad726b9791eba8d9
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-08-17 22:34:55 +0800

    fix: get chain id from query urgently

--- src/hooks/useActiveChainId.ts
+++ src/hooks/useActiveChainId.ts
@@ -2,13 +2,19 @@ import { ChainId } from '@pancakeswap/sdk'
 import { useRouter } from 'next/router'
 import useSWRImmutable from 'swr/immutable'
 import { isChainSupported } from 'utils/wagmi'
-import { useAccount, useNetwork } from 'wagmi'
+import { useNetwork } from 'wagmi'
 
 export function useLocalNetworkChain() {
   const { data: sessionChainId } = useSWRImmutable('session-chain-id')
+  // useRouter is kind of slow, we only get this query chainId once
+  const { data: queryChainId } = useSWRImmutable('query-chain-id', () => {
+    const params = new URL(window.location.href).searchParams
+    return params.get('chainId')
+  })
+
   const { query } = useRouter()
 
-  const chainId = +(sessionChainId || query.chainId)
+  const chainId = +(sessionChainId || query.chainId || queryChainId)
 
   if (isChainSupported(chainId)) {
     return chainId
@@ -19,9 +25,9 @@ export function useLocalNetworkChain() {
 
 export const useActiveChainId = () => {
   const localChainId = useLocalNetworkChain()
-  const { status } = useAccount()
+  const { isValidating } = useSWRImmutable('query-chain-id')
   const { chain } = useNetwork()
-  const chainId =
-    chain?.id ?? localChainId ?? (status !== 'connecting' && status !== 'reconnecting' ? ChainId.BSC : undefined)
+  const chainId = chain?.id ?? localChainId ?? (!isValidating ? ChainId.BSC : undefined)
+
   return chainId
 }
