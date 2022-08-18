b2d62a11440959ee0c1c29b5c1c18fc4632fe859
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-08-18 14:53:29 +0800

    fix types

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,11 +1,3 @@
-import { Ethereum } from '@wagmi/core'
-
 type SerializedBigNumber = string
 
 declare let __NEZHA_BRIDGE__: any
-
-interface Window {
-  ethereum?: Ethereum & {
-    isSafePal?: true
-  }
-}
