ac2275525a31399e8dcdb3f0b55ccfb44528fa62
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-08-19 11:26:23 +0800

    add jotai

--- src/components/CommitButton.tsx
+++ src/components/CommitButton.tsx
@@ -1,16 +1,30 @@
-import { useTranslation } from '@pancakeswap/localization'
 import { Button, ButtonProps } from '@pancakeswap/uikit'
+import { useSetAtom } from 'jotai'
 import { useActiveChainId } from 'hooks/useActiveChainId'
+import { hideWrongNetworkModalAtom } from './NetworkModal'
+import Trans from './Trans'
 
-export const CommitButton = ({ children, ...props }: ButtonProps) => {
+const wrongNetworkProps: ButtonProps = {
+  variant: 'danger',
+  disabled: false,
+  children: <Trans>Wrong Network</Trans>,
+}
+
+export const CommitButton = (props: ButtonProps) => {
   const { isWrongNetwork } = useActiveChainId()
-  const { t } = useTranslation()
+  const setHideWrongNetwork = useSetAtom(hideWrongNetworkModalAtom)
 
-  let childrenOrWrongNetworkText = children
-
-  if (isWrongNetwork) {
-    childrenOrWrongNetworkText = t('Wrong Network')
-  }
-
-  return <Button {...props}>{childrenOrWrongNetworkText}</Button>
+  return (
+    <Button
+      {...props}
+      onClick={(e) => {
+        if (isWrongNetwork) {
+          setHideWrongNetwork(false)
+        } else {
+          props.onClick?.(e)
+        }
+      }}
+      {...(isWrongNetwork && wrongNetworkProps)}
+    />
+  )
 }
--- src/components/NetworkModal/NetworkModal.tsx
+++ src/components/NetworkModal/NetworkModal.tsx
@@ -1,16 +1,19 @@
 import { ModalV2 } from '@pancakeswap/uikit'
 import useActiveWeb3React from 'hooks/useActiveWeb3React'
-import { useMemo, useState } from 'react'
+import { useMemo } from 'react'
 import { useNetwork } from 'wagmi'
+import { atom, useAtom } from 'jotai'
 import { SUPPORT_ONLY_BSC } from 'config/constants/supportChains'
 import { UnsupportedNetworkModal } from './UnsupportedNetworkModal'
 import { WrongNetworkModal } from './WrongNetworkModal'
 import { PageNetworkSupportModal } from './PageNetworkSupportModal'
 
+export const hideWrongNetworkModalAtom = atom(false)
+
 export const NetworkModal = ({ pageSupportedChains = SUPPORT_ONLY_BSC }: { pageSupportedChains?: number[] }) => {
   const { chainId, chain, isWrongNetwork } = useActiveWeb3React()
   const { chains } = useNetwork()
-  const [dismissWrongNetwork, setDismissWrongNetwork] = useState(false)
+  const [dismissWrongNetwork, setDismissWrongNetwork] = useAtom(hideWrongNetworkModalAtom)
 
   const isPageNotSupported = useMemo(
     () => Boolean(pageSupportedChains.length) && !pageSupportedChains.includes(chainId),
