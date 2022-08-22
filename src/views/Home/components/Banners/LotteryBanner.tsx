bb06fb948706fdd2df2f4d1a756111637f064fc3
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2022-08-22 09:57:06 +0800

    chore: banner layout issue (#4526)
    * chore: banner layout issue
    
    * chore: remove magic number
    
    * chore: mobile banner margin top issue
    
    * chore: set line height
    
    * chore: useLayout effect

--- src/views/Home/components/Banners/LotteryBanner.tsx
+++ src/views/Home/components/Banners/LotteryBanner.tsx
@@ -45,6 +45,9 @@ const TimerWrapper = styled.div`
     background-size: 100% 100%;
     padding: 0px 10px 7px;
     display: inline-flex;
+    white-space: nowrap;
+    transform: scale(0.88);
+    transform-origin: top left;
   }
 `
 
@@ -53,6 +56,9 @@ export const StyledSubheading = styled(Heading)`
   color: white;
   ${({ theme }) => theme.mediaQueries.xs} {
     font-size: 24px;
+    &.lottery {
+      font-size: 20px;
+    }
   }
   ${({ theme }) => theme.mediaQueries.sm} {
     -webkit-text-stroke: unset;
@@ -106,7 +112,7 @@ const LotteryBanner = () => {
         <S.LeftWrapper>
           {status === FetchStatus.Fetched && isLotteryLive(data.status) ? (
             <>
-              <StyledSubheading style={{ textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)' }}>
+              <StyledSubheading className="lottery" style={{ textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)' }}>
                 <LotteryPrice />
               </StyledSubheading>
               <TimerWrapper>
