6258be09b21d7a3a9757c01b672c73cc0d42b4c5
chefmomota <momota@pancakeswap.com>
2022-08-25 17:30:06 +0800

    chore: Mock data

--- src/views/Swap/components/AccessRisk/RiskMessage.tsx
+++ src/views/Swap/components/AccessRisk/RiskMessage.tsx
@@ -2,7 +2,7 @@ import { useMemo } from 'react'
 import { Currency } from '@pancakeswap/sdk'
 import { CurrencyLogo } from 'components/Logo'
 import { useTranslation } from '@pancakeswap/localization'
-import { Flex, Message, MessageText, Text } from '@pancakeswap/uikit'
+import { Flex, Message, MessageText, Text, Link } from '@pancakeswap/uikit'
 import { RiskTokenInfo, TOKEN_RISK } from 'views/Swap/hooks/fetchTokenRisk'
 
 interface RiskMessageProps {
@@ -31,14 +31,23 @@ const RiskMessage: React.FC<RiskMessageProps> = ({ currency, riskTokenInfo }) =>
         {t('%riskLevel% Risk', { riskLevel })}
         <Flex mt="4px">
           <CurrencyLogo currency={currency} size="24px" style={{ marginRight: '8px' }} />
-          <Text fontSize="14px" lineHeight="120%">
+          <Flex flexDirection="column">
+            <Text fontSize="14px" lineHeight="120%">
+              Although no obvious risk is found in the address, it is not guaranteed that there is 100% no risk for the
+              address as the project can still fail or the token price can go down under natural market forces.
+            </Text>
+            <Link fontSize="14px" mt="8px" ml="auto" external href="https://www.hashdit.io/en">
+              {t('Learn More')}
+            </Link>
+          </Flex>
+          {/* <Text fontSize="14px" lineHeight="120%">
             <Text fontSize="14px" bold mr="4px" as="span" lineHeight="120%">
               {currency?.symbol}
             </Text>
             {t('is in the average category with %riskLevel% risk', {
               riskLevel: riskLevel.toLowerCase(),
             })}
-          </Text>
+          </Text> */}
         </Flex>
       </MessageText>
     </Message>
