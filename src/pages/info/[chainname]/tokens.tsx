427b92287c46117ad5e541158a40e080d0805065
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2022-08-29 20:13:23 +0800

    chore: add token routing

---
+++ src/pages/info/[chainname]/tokens.tsx
@@ -0,0 +1,10 @@
+import Tokens from 'views/Info/Tokens'
+import { InfoPageLayout } from 'views/Info'
+
+const InfoTokensPage = () => {
+  return <Tokens />
+}
+
+InfoTokensPage.Layout = InfoPageLayout
+
+export default InfoTokensPage
