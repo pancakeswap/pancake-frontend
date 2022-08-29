fb9e3a6fe1b5fc610841b0457ca8453af5eed889
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2022-08-29 19:48:35 +0800

    chore: routing for multichain

---
+++ src/pages/info/[chainname]/index.tsx
@@ -0,0 +1,10 @@
+import { InfoPageLayout } from 'views/Info'
+import Overview from 'views/Info/Overview'
+
+const MultiChainPage = () => {
+  return <Overview />
+}
+
+MultiChainPage.Layout = InfoPageLayout
+
+export default MultiChainPage
