e485cc95570518f7d5eba9962b70f6ff5a23bc51
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-08-30 09:48:38 +0800

    chore: Clean up multicall

--- src/utils/multicall.ts
+++ src/utils/multicall.ts
@@ -1,15 +1,8 @@
-import { Interface } from '@ethersproject/abi'
 import { CallOverrides } from '@ethersproject/contracts'
-import { createMulticall } from '@pancakeswap/multicall'
-import { useMulticallContract } from 'hooks/useContract'
-import { useCallback } from 'react'
+import { createMulticall, Call } from '@pancakeswap/multicall'
 import { provider } from './wagmi'
 
-export interface Call {
-  address: string // Address of the contract
-  name: string // Function name on the contract (example: balanceOf)
-  params?: any[] // Function params
-}
+export type { Call }
 
 export interface MulticallOptions extends CallOverrides {
   requireSuccess?: boolean
@@ -20,30 +13,3 @@ const { multicall, multicallv2 } = createMulticall(provider)
 export default multicall
 
 export { multicallv2 }
-
-export type ReturnUseMultiCall = ReturnType<typeof useMulticall>
-
-export const useMulticall = () => {
-  const multi = useMulticallContract()
-
-  return useCallback(
-    async <T = any>(abi: any[], calls: Call[], options?: MulticallOptions): Promise<T> => {
-      const { requireSuccess = true, ...overrides } = options || {}
-      const itf = new Interface(abi)
-
-      const calldata = calls.map((call) => ({
-        target: call.address.toLowerCase(),
-        callData: itf.encodeFunctionData(call.name, call.params),
-      }))
-
-      const returnData = await multi.tryAggregate(requireSuccess, calldata, overrides)
-      const res = returnData.map((call, i) => {
-        const [result, data] = call
-        return result ? itf.decodeFunctionResult(calls[i].name, data) : null
-      })
-
-      return res as any
-    },
-    [multi],
-  )
-}
