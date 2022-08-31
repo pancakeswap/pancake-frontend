238aff036479468a2599524c908f2f3cf5a7f953
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-08-31 12:37:35 +0800

    fix binace wallet autoconnect and switch network re-entry

--- src/hooks/useSwitchNetwork.ts
+++ src/hooks/useSwitchNetwork.ts
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 import { useTranslation } from '@pancakeswap/localization'
 import { ChainId } from '@pancakeswap/sdk'
 import { useToast } from '@pancakeswap/uikit'
@@ -32,10 +33,12 @@ export function useSwitchNetwork() {
   const { isConnected, connector } = useAccount()
 
   const switchNetworkLocal = useSwitchNetworkLocal()
+  const isLoading = _isLoading || loading
 
   const switchNetworkAsync = useCallback(
     async (chainId: number) => {
       if (isConnected && typeof _switchNetworkAsync === 'function') {
+        if (isLoading) return
         setLoading(true)
         return _switchNetworkAsync(chainId)
           .catch(() => {
@@ -48,7 +51,7 @@ export function useSwitchNetwork() {
         switchNetworkLocal(chainId)
       })
     },
-    [isConnected, _switchNetworkAsync, t, setLoading, toastError, switchNetworkLocal],
+    [isConnected, _switchNetworkAsync, isLoading, setLoading, toastError, t, switchNetworkLocal],
   )
 
   const switchNetwork = useCallback(
@@ -61,7 +64,6 @@ export function useSwitchNetwork() {
     [_switchNetwork, isConnected, switchNetworkLocal],
   )
 
-  const isLoading = _isLoading || loading
   const canSwitch = useMemo(
     () =>
       isConnected
@@ -70,7 +72,7 @@ export function useSwitchNetwork() {
           !(
             typeof window !== 'undefined' &&
             // @ts-ignore // TODO: add type later
-            window.ethereum?.isSafePal
+            (window.ethereum?.isSafePal || window.ethereum?.isTokenPocket || window.ethereum?.isMathWallet)
           )
         : true,
     [_switchNetworkAsync, isConnected, connector],
