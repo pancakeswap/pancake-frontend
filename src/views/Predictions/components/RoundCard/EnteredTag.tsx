1475b4ccb8aac06574b5b0a7d92cb12d1994896c
memoyil <2213635+memoyil@users.noreply.github.com>
2022-08-31 03:01:18 +0200

    fix: (prediction): Claimed amount in tooltip round card

--- src/views/Predictions/components/RoundCard/EnteredTag.tsx
+++ src/views/Predictions/components/RoundCard/EnteredTag.tsx
@@ -1,20 +1,39 @@
+import { useMemo } from 'react'
 import { BigNumber } from '@ethersproject/bignumber'
 import { CheckmarkCircleIcon, CheckmarkCircleFillIcon, Tag, useTooltip } from '@pancakeswap/uikit'
 import { useTranslation } from '@pancakeswap/localization'
 import { useConfig } from 'views/Predictions/context/ConfigProvider'
+import { ethersToBigNumber } from 'utils/bigNumber'
 import { formatBnbv2 } from '../../helpers'
 
 interface EnteredTagProps {
   amount?: BigNumber
   hasClaimed?: boolean
+  multiplier: string
 }
 
-const EnteredTag: React.FC<React.PropsWithChildren<EnteredTagProps>> = ({ amount, hasClaimed = false }) => {
+const EnteredTag: React.FC<React.PropsWithChildren<EnteredTagProps>> = ({ amount, hasClaimed = false, multiplier }) => {
   const { t } = useTranslation()
   const { token, displayedDecimals } = useConfig()
+  const formattedAmount = useMemo(() => {
+    let tokenAmount
+    if (hasClaimed) {
+      if (amount) {
+        const multiplierNumber = parseFloat(multiplier)
+        tokenAmount = BigNumber.from(
+          ethersToBigNumber(amount)
+            .times(Number.isFinite(multiplierNumber) ? multiplierNumber : 1)
+            .toString(),
+        )
+      }
+    } else {
+      tokenAmount = amount
+    }
+    return formatBnbv2(tokenAmount, displayedDecimals)
+  }, [amount, displayedDecimals, hasClaimed, multiplier])
 
   const { targetRef, tooltipVisible, tooltip } = useTooltip(
-    <div style={{ whiteSpace: 'nowrap' }}>{`${formatBnbv2(amount, displayedDecimals)} ${token.symbol}`}</div>,
+    <div style={{ whiteSpace: 'nowrap' }}>{`${formattedAmount} ${token.symbol}`}</div>,
     { placement: 'bottom' },
   )
 
