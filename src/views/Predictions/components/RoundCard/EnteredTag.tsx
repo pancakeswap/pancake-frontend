596dc9dfade8991932b44b3d571fe4662a7a0ce1
memoyil <2213635+memoyil@users.noreply.github.com>
2022-09-01 10:52:13 +0200

    fix(prediction): Claimed amount in tooltip round card

--- src/views/Predictions/components/RoundCard/EnteredTag.tsx
+++ src/views/Predictions/components/RoundCard/EnteredTag.tsx
@@ -1,20 +1,41 @@
+import { useMemo } from 'react'
 import { BigNumber } from '@ethersproject/bignumber'
 import { CheckmarkCircleIcon, CheckmarkCircleFillIcon, Tag, useTooltip } from '@pancakeswap/uikit'
 import { useTranslation } from '@pancakeswap/localization'
+import { ethersToBigNumber } from 'utils/bigNumber'
+import { REWARD_RATE } from 'state/predictions/config'
 import { useConfig } from 'views/Predictions/context/ConfigProvider'
 import { formatBnbv2 } from '../../helpers'
 
 interface EnteredTagProps {
   amount?: BigNumber
   hasClaimed?: boolean
+  multiplier: string
 }
 
-const EnteredTag: React.FC<React.PropsWithChildren<EnteredTagProps>> = ({ amount, hasClaimed = false }) => {
+const EnteredTag: React.FC<React.PropsWithChildren<EnteredTagProps>> = ({ amount, hasClaimed = false, multiplier }) => {
   const { t } = useTranslation()
   const { token, displayedDecimals } = useConfig()
 
+  const formattedAmount = useMemo(() => {
+    let tokenAmount
+    if (hasClaimed) {
+      if (amount) {
+        const multiplierNumber = parseFloat(multiplier)
+        tokenAmount = BigNumber.from(
+          ethersToBigNumber(amount)
+            .times(Number.isFinite(multiplierNumber) ? multiplierNumber * REWARD_RATE : 1)
+            .toFixed(0),
+        )
+      }
+    } else {
+      tokenAmount = amount
+    }
+    return formatBnbv2(tokenAmount, displayedDecimals)
+  }, [amount, displayedDecimals, hasClaimed, multiplier])
+
   const { targetRef, tooltipVisible, tooltip } = useTooltip(
-    <div style={{ whiteSpace: 'nowrap' }}>{`${formatBnbv2(amount, displayedDecimals)} ${token.symbol}`}</div>,
+    <div style={{ whiteSpace: 'nowrap' }}>{`${formattedAmount} ${token.symbol}`}</div>,
     { placement: 'bottom' },
   )
 
