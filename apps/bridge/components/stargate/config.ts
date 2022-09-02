24b4466e1adc377782b58071aba8ee1ab885d662
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-09-02 10:52:36 +0800

    chore(birdge): Update version (#4642)
    * chore(birdge): Update version
    
    * add disconnect button

--- apps/bridge/components/stargate/config.ts
+++ apps/bridge/components/stargate/config.ts
@@ -1,8 +1,8 @@
 import { avalandche, fantomOpera } from '@pancakeswap/wagmi/chains'
 import { arbitrum, mainnet, optimism, polygon } from 'wagmi/chains'
 
-const VERSION = '0.0.25-mainnet.13'
-const SHA384 = 'toBorozqlXJ4GWTlZXE3XunuRQccctvrZ1tJWY+OhsOtKZuF/XO3QxJUUED74xZd'
+const VERSION = '0.0.25-mainnet.18'
+const SHA384 = '5qByPFCg/LiF1jv/KlCHDgn1H8gLLLoYFFSoNSY6kMc0jWTU9NvHVyDmj4unccQ/'
 export const PARTNER_ID = 0x0002
 export const FEE_COLLECTOR = '0x68C7ABB8b1c3D1cE467E28265770F3a7ECF32654'
 export const FEE_TENTH_BPS = '40'
