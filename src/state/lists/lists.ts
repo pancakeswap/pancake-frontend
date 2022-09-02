c115382dbd14aefbb569aee72d5470f61fdc0b93
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-09-03 01:16:23 +0800

    test

--- src/state/lists/lists.ts
+++ src/state/lists/lists.ts
@@ -1,37 +1,50 @@
 import { atomWithStorage } from 'hooks/atomWithStorage'
-import { createStore, del, get, set } from 'idb-keyval'
 import { useReducerAtom } from 'jotai/utils'
+import defaultStorge from 'redux-persist/lib/storage'
+import localForage from 'localforage'
 import listReducer, { initialState, ListsState } from './reducer'
 
 let gotOnce = false
 
-export const listStore = typeof window !== 'undefined' && createStore('lists', 'list-v1')
-function createStorage<T>() {
-  return {
-    // eslint-disable-next-line consistent-return
-    setItem: (key, value) => {
-      if (listStore && gotOnce) {
-        return set(key, value, listStore)
-      }
-    },
-    getItem: (key) =>
-      get<T>(key, listStore).then((value) => {
-        if (value !== undefined) {
-          gotOnce = true
+/**
+ * Persist you redux state using IndexedDB
+ * @param {string} dbName - IndexedDB database name
+ */
+function IndexedDBStorage(dbName, storeName) {
+  if (typeof window !== 'undefined') {
+    const db = localForage.createInstance({
+      name: dbName,
+      storeName,
+    })
+    return {
+      db,
+      getItem: async (key) => {
+        const value = await db.getItem(key)
+        gotOnce = true
+        if (value) {
           return value
         }
-        return undefined
-      }),
-    removeItem: (key) => {
-      return del(key, listStore)
-    },
-    delayInit: true,
+        return initialState
+      },
+      // eslint-disable-next-line consistent-return
+      setItem: (k, v) => {
+        if (gotOnce) {
+          return db.setItem(k, v)
+        }
+      },
+      removeItem: db.removeItem,
+      delayInit: true,
+    }
   }
+  return defaultStorge
 }
 
-const storage = createStorage<ListsState>()
-
-export const listsAtom = atomWithStorage<ListsState>('lists', initialState, storage)
+export const listsAtom = atomWithStorage<ListsState>(
+  'lists',
+  initialState,
+  // @ts-ignore
+  IndexedDBStorage('lists', 'listv1'),
+)
 
 export function useListState() {
   return useReducerAtom(listsAtom, listReducer)
