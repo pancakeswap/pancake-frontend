f956534e0407c6f4535b15206cced6fc994be86f
ChefMomota <98292246+ChefMomota@users.noreply.github.com>
2022-09-02 15:18:36 +0800

    chore: Update AccessRisk wording (#4646)

--- src/views/Swap/index.tsx
+++ src/views/Swap/index.tsx
@@ -99,7 +99,7 @@ const SwitchIconButton = styled(IconButton)`
 `
 
 const CHART_SUPPORT_CHAIN_IDS = [ChainId.BSC]
-const ACCESS_TOKEN_SUPPORT_CHAIN_IDS = [ChainId.BSC]
+export const ACCESS_TOKEN_SUPPORT_CHAIN_IDS = [ChainId.BSC]
 
 const SettingsModalWithCustomDismiss = withCustomOnDismiss(SettingsModal)
 
