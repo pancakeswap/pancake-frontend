5998d9d96bcaecfc675fb4e429ad33f88745d6d4
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2022-09-07 04:04:42 +0800

    chore: redirect info page by chain

--- src/pages/info/index.tsx
+++ src/pages/info/index.tsx
@@ -1,15 +1,21 @@
 import { InfoPageLayout } from 'views/Info'
+import { useEffect } from 'react'
 import Overview from 'views/Info/Overview'
 import { useRouter } from 'next/router'
 import { ChainId } from '@pancakeswap/sdk'
+import { useActiveChainId } from 'hooks/useActiveChainId'
 
 const InfoPage = () => {
   const router = useRouter()
-  const { chainId, chainName } = router.query
-  if (chainId === ChainId.ETHEREUM.toString() && !chainName)
-    router.replace({ pathname: '/info/eth', query: '' }, undefined, { shallow: true })
-  else if (!chainName && chainId === ChainId.BSC.toString())
-    router.replace({ pathname: '/info', query: '' }, undefined, { shallow: true })
+  const { chainId } = useActiveChainId()
+  useEffect(() => {
+    const { chainName } = router.query
+    if (chainId === ChainId.ETHEREUM && !chainName)
+      router.replace({ pathname: '/info/eth', query: '' }, undefined, { shallow: true })
+    else if (!chainName && chainId === ChainId.BSC)
+      router.replace({ pathname: '/info', query: '' }, undefined, { shallow: true })
+  }, [chainId, router])
+
   return <Overview />
 }
 
