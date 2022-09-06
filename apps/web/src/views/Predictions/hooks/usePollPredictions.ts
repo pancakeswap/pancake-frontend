2588ff3f04318f2d3d1149ea73e31c5298bdeb68
memoyil <2213635+memoyil@users.noreply.github.com>
2022-09-06 12:31:37 +0200

    fix: (prediction): Soon cards entry start time

--- apps/web/src/views/Predictions/hooks/usePollPredictions.ts
+++ apps/web/src/views/Predictions/hooks/usePollPredictions.ts
@@ -1,7 +1,7 @@
 import { useAccount } from 'wagmi'
 import { useGetPredictionsStatus } from 'state/predictions/hooks'
 import { fetchPredictionData } from 'state/predictions'
-import { PredictionStatus } from 'state/types'
+import { useInitialBlock } from 'state/block/hooks'
 import useSWR from 'swr'
 import useLocalDispatch from 'contexts/LocalRedux/useLocalDispatch'
 
@@ -10,17 +10,13 @@ const POLL_TIME_IN_SECONDS = 10
 const usePollPredictions = () => {
   const dispatch = useLocalDispatch()
   const { address: account } = useAccount()
-  const status = useGetPredictionsStatus()
+  const initialBlock = useInitialBlock()
 
-  useSWR(
-    status !== PredictionStatus.INITIAL ? ['predictions', account] : null,
-    () => dispatch(fetchPredictionData(account)),
-    {
-      refreshInterval: POLL_TIME_IN_SECONDS * 1000,
-      refreshWhenHidden: true,
-      refreshWhenOffline: true,
-    },
-  )
+  useSWR(initialBlock > 0 ? ['predictions', account] : null, () => dispatch(fetchPredictionData(account)), {
+    refreshInterval: POLL_TIME_IN_SECONDS * 1000,
+    refreshWhenHidden: true,
+    refreshWhenOffline: true,
+  })
 }
 
 export default usePollPredictions
