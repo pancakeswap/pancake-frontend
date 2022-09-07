1a84c6bd57af2f05cdff28599d05cd71bd7d0b2b
memoyil <2213635+memoyil@users.noreply.github.com>
2022-09-07 16:10:00 +0200

    perf: (info): Use swr in useCmcLink hook to fetch url

--- src/views/Info/hooks/useCMCLink.ts
+++ src/views/Info/hooks/useCMCLink.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import useSWRImmutable from 'swr/immutable'
 
 // endpoint to check asset exists and get url to CMC page
 // returns 400 status code if token is not on CMC
@@ -6,26 +6,18 @@ const CMC_ENDPOINT = 'https://3rdparty-apis.coinmarketcap.com/v1/cryptocurrency/
 
 /**
  * Check if asset exists on CMC, if exists
- * return  url, if not return undefined
+ * return url, if not return undefined
  * @param address token address (all lowercase, checksummed are not supported by CMC)
  */
 const useCMCLink = (address: string): string | undefined => {
-  const [cmcPageUrl, setCMCPageUrl] = useState<string | undefined>(undefined)
+  const { data: cmcPageUrl } = useSWRImmutable(address ? ['cmcLink', address] : null, async () => {
+    const response = await fetch(`${CMC_ENDPOINT}${address}`)
 
-  useEffect(() => {
-    const fetchLink = async () => {
-      const result = await fetch(`${CMC_ENDPOINT}${address}`)
-      // if link exists, format the url
-      if (result.status === 200) {
-        result.json().then(({ data }) => {
-          setCMCPageUrl(data.url)
-        })
-      }
+    if (response.ok) {
+      return (await response.json()).data.url
     }
-    if (address) {
-      fetchLink()
-    }
-  }, [address])
+    return undefined
+  })
 
   return cmcPageUrl
 }
