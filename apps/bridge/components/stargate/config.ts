85f9be2139de344adc3ed4a4256df29b710f93a4
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-09-08 00:19:48 +0800

    chore: Bump stargate version (#4680)

--- apps/bridge/components/stargate/config.ts
+++ apps/bridge/components/stargate/config.ts
@@ -1,8 +1,8 @@
 import { avalandche, fantomOpera } from '@pancakeswap/wagmi/chains'
 import { arbitrum, mainnet, optimism, polygon } from 'wagmi/chains'
 
-const VERSION = '0.0.25-mainnet.18'
-const SHA384 = '5qByPFCg/LiF1jv/KlCHDgn1H8gLLLoYFFSoNSY6kMc0jWTU9NvHVyDmj4unccQ/'
+const VERSION = '0.0.25-mainnet.19'
+const SHA384 = 'qQJSEss4ErEoxG1BxX9kq5sOqbBAn475yj7Bz8ZqNxQKDR2tvk693RdhfA0blQEF'
 export const PARTNER_ID = 0x0002
 export const FEE_COLLECTOR = '0x68C7ABB8b1c3D1cE467E28265770F3a7ECF32654'
 export const FEE_TENTH_BPS = '40'
