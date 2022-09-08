cf94ca5dffb10cc7e2e4514c1d2daaa31e06a3cf
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-09-08 11:47:11 +0800

    fix import

--- packages/uikit/src/components/Toast/Toast.tsx
+++ packages/uikit/src/components/Toast/Toast.tsx
@@ -1,7 +1,7 @@
 import { useCallback, useEffect, useRef } from "react";
 import { CSSTransition } from "react-transition-group";
 import styled from "styled-components";
-import { Alert, alertVariants } from "@pancakeswap/uikit";
+import { Alert, alertVariants } from "../Alert";
 import { ToastProps, types } from "./types";
 
 const alertTypeMap = {
