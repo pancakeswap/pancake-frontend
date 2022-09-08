061a882438c7e0668199f45c3dcc56b26294f7b5
chefmomota <momota@pancakeswap.com>
2022-09-08 13:52:41 +0800

    chore: Update tokenPerBlock

--- src/config/constants/pools.tsx
+++ src/config/constants/pools.tsx
@@ -75,7 +75,7 @@ export const livePools: SerializedPoolConfig[] = [
       97: '',
     },
     poolCategory: PoolCategory.CORE,
-    tokenPerBlock: '55.64',
+    tokenPerBlock: '55.6446',
     version: 3,
   },
   {
