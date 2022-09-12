6278a214bbfc1c823158293bab9f20f534fa9e46
memoyil <2213635+memoyil@users.noreply.github.com>
2022-09-12 18:24:54 +0200

    perf: (token-search): Remove tag info from wrappedtokeninfo

--- packages/tokens/src/helpers.ts
+++ packages/tokens/src/helpers.ts
@@ -17,12 +17,9 @@ export interface SerializedWrappedToken extends SerializedToken {
 export class WrappedTokenInfo extends Token {
   public readonly tokenInfo: TokenInfo
 
-  public readonly tags: TagInfo[]
-
-  constructor(tokenInfo: TokenInfo, tags: TagInfo[]) {
+  constructor(tokenInfo: TokenInfo) {
     super(tokenInfo.chainId, tokenInfo.address, tokenInfo.decimals, tokenInfo.symbol, tokenInfo.name)
     this.tokenInfo = tokenInfo
-    this.tags = tags
   }
 
   public get logoURI(): string | undefined {
@@ -40,10 +37,6 @@ export class WrappedTokenInfo extends Token {
       logoURI: this.logoURI,
     }
   }
-}
-type TagDetails = Tags[keyof Tags]
-export interface TagInfo extends TagDetails {
-  id: string
 }
 
 export type TokenAddressMap = Readonly<{
@@ -63,17 +56,14 @@ export const EMPTY_LIST: TokenAddressMap = {
 
 export function deserializeToken(serializedToken: SerializedWrappedToken): Token {
   if (serializedToken.logoURI) {
-    return new WrappedTokenInfo(
-      {
-        chainId: serializedToken.chainId,
-        address: serializedToken.address,
-        decimals: serializedToken.decimals,
-        symbol: serializedToken.symbol || 'Unknown',
-        name: serializedToken.name || 'Unknown',
-        logoURI: serializedToken.logoURI,
-      },
-      [],
-    )
+    return new WrappedTokenInfo({
+      chainId: serializedToken.chainId,
+      address: serializedToken.address,
+      decimals: serializedToken.decimals,
+      symbol: serializedToken.symbol || 'Unknown',
+      name: serializedToken.name || 'Unknown',
+      logoURI: serializedToken.logoURI,
+    })
   }
   return new Token(
     serializedToken.chainId,
