8ac1ead4f4767805c7f9bbdae72ecebdd73ba05d
memoyil <2213635+memoyil@users.noreply.github.com>
2022-09-14 13:54:35 +0200

    refactor: Remove deprecated getGasPrice and use hook and fix gas settings not applied to the transactions when changed (#4740)

--- src/views/Migration/hook/V1/Pool/useUnstakePool.ts
+++ src/views/Migration/hook/V1/Pool/useUnstakePool.ts
@@ -3,10 +3,9 @@ import { useCallback } from 'react'
 import { DEFAULT_GAS_LIMIT, DEFAULT_TOKEN_DECIMAL } from 'config'
 import { parseUnits } from '@ethersproject/units'
 import { useMasterchefV1, useSousChef } from 'hooks/useContract'
-import getGasPrice from 'utils/getGasPrice'
+import { useGasPrice } from 'state/user/hooks'
 
-const sousUnstake = (sousChefContract: any, amount: string, decimals: number) => {
-  const gasPrice = getGasPrice()
+const sousUnstake = (sousChefContract: any, amount: string, decimals: number, gasPrice: string) => {
   const units = parseUnits(amount, decimals)
 
   return sousChefContract.withdraw(units.toString(), {
@@ -14,30 +13,29 @@ const sousUnstake = (sousChefContract: any, amount: string, decimals: number) =>
   })
 }
 
-const sousEmergencyUnstake = (sousChefContract: any) => {
-  const gasPrice = getGasPrice()
+const sousEmergencyUnstake = (sousChefContract: any, gasPrice: string) => {
   return sousChefContract.emergencyWithdraw({ gasPrice })
 }
 
 const useUnstakePool = (sousId: number, enableEmergencyWithdraw = false) => {
   const masterChefV1Contract = useMasterchefV1()
   const sousChefContract = useSousChef(sousId)
+  const gasPrice = useGasPrice()
 
   const handleUnstake = useCallback(
     async (amount: string, decimals: number) => {
       if (sousId === 0) {
-        const gasPrice = getGasPrice()
         const value = new BigNumber(amount).times(DEFAULT_TOKEN_DECIMAL).toString()
         return masterChefV1Contract.leaveStaking(value, { gasLimit: DEFAULT_GAS_LIMIT, gasPrice })
       }
 
       if (enableEmergencyWithdraw) {
-        return sousEmergencyUnstake(sousChefContract)
+        return sousEmergencyUnstake(sousChefContract, gasPrice)
       }
 
-      return sousUnstake(sousChefContract, amount, decimals)
+      return sousUnstake(sousChefContract, amount, decimals, gasPrice)
     },
-    [enableEmergencyWithdraw, masterChefV1Contract, sousChefContract, sousId],
+    [enableEmergencyWithdraw, masterChefV1Contract, sousChefContract, sousId, gasPrice],
   )
 
   return { onUnstake: handleUnstake }
--- src/views/Pools/hooks/useUnstakePool.ts
+++ src/views/Pools/hooks/useUnstakePool.ts
@@ -1,10 +1,9 @@
 import { useCallback } from 'react'
 import { parseUnits } from '@ethersproject/units'
 import { useSousChef } from 'hooks/useContract'
-import getGasPrice from 'utils/getGasPrice'
+import { useGasPrice } from 'state/user/hooks'
 
-const sousUnstake = (sousChefContract: any, amount: string, decimals: number) => {
-  const gasPrice = getGasPrice()
+const sousUnstake = (sousChefContract: any, amount: string, decimals: number, gasPrice: string) => {
   const units = parseUnits(amount, decimals)
 
   return sousChefContract.withdraw(units.toString(), {
@@ -12,23 +11,23 @@ const sousUnstake = (sousChefContract: any, amount: string, decimals: number) =>
   })
 }
 
-const sousEmergencyUnstake = (sousChefContract: any) => {
-  const gasPrice = getGasPrice()
+const sousEmergencyUnstake = (sousChefContract: any, gasPrice: string) => {
   return sousChefContract.emergencyWithdraw({ gasPrice })
 }
 
 const useUnstakePool = (sousId: number, enableEmergencyWithdraw = false) => {
   const sousChefContract = useSousChef(sousId)
+  const gasPrice = useGasPrice()
 
   const handleUnstake = useCallback(
     async (amount: string, decimals: number) => {
       if (enableEmergencyWithdraw) {
-        return sousEmergencyUnstake(sousChefContract)
+        return sousEmergencyUnstake(sousChefContract, gasPrice)
       }
 
-      return sousUnstake(sousChefContract, amount, decimals)
+      return sousUnstake(sousChefContract, amount, decimals, gasPrice)
     },
-    [enableEmergencyWithdraw, sousChefContract],
+    [enableEmergencyWithdraw, sousChefContract, gasPrice],
   )
 
   return { onUnstake: handleUnstake }
