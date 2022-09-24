727179be3bea12a4544c443aa61e3b48e995b142
memoyil <2213635+memoyil@users.noreply.github.com>
2022-09-24 13:25:13 +0200

    feat: (voting): Use only core proposals to put tag on voting link

--- src/components/Menu/hooks/useVotingStatus.ts
+++ src/components/Menu/hooks/useVotingStatus.ts
@@ -2,25 +2,25 @@ import useSWRImmutable from 'swr/immutable'
 import { ProposalState } from 'state/types'
 import request, { gql } from 'graphql-request'
 import { SNAPSHOT_API } from 'config/constants/endpoints'
-import { PANCAKE_SPACE } from 'views/Voting/config'
+import { PANCAKE_SPACE, ADMINS } from 'views/Voting/config'
 
-export const getActiveProposal = async (): Promise<string[]> => {
+export const getActiveCoreProposal = async (): Promise<string[]> => {
   const response = await request(
     SNAPSHOT_API,
     gql`
-      query getProposals($first: Int!, $skip: Int!, $state: String!) {
-        proposals(first: $first, skip: $skip, where: { space_in: "${PANCAKE_SPACE}", state: $state }) {
+      query getProposals($first: Int!, $skip: Int!, $state: String!, $admins: [String]!) {
+        proposals(first: $first, skip: $skip, where: { author_in: $admins, space_in: "${PANCAKE_SPACE}", state: $state }) {
           id
         }
       }
     `,
-    { first: 1, skip: 0, state: ProposalState.ACTIVE },
+    { first: 1, skip: 0, state: ProposalState.ACTIVE, admins: ADMINS },
   )
   return response.proposals
 }
 
 export const useVotingStatus = () => {
-  const { data: proposals = [] } = useSWRImmutable('anyActiveProposals', getActiveProposal)
+  const { data: proposals = [] } = useSWRImmutable('anyActiveCoreProposals', getActiveCoreProposal)
 
   return Boolean(proposals.length)
 }
