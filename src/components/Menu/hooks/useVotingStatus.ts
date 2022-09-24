127a246b5ca86f9c20e053d522fe91f0529b3fa9
memoyil <2213635+memoyil@users.noreply.github.com>
2022-09-24 11:14:48 +0200

    fix: Voting crash and use constant

--- src/components/Menu/hooks/useVotingStatus.ts
+++ src/components/Menu/hooks/useVotingStatus.ts
@@ -2,13 +2,14 @@ import useSWRImmutable from 'swr/immutable'
 import { ProposalState } from 'state/types'
 import request, { gql } from 'graphql-request'
 import { SNAPSHOT_API } from 'config/constants/endpoints'
+import { PANCAKE_SPACE } from 'views/Voting/config'
 
 export const getActiveProposal = async (): Promise<string[]> => {
   const response = await request(
     SNAPSHOT_API,
     gql`
       query getProposals($first: Int!, $skip: Int!, $state: String!) {
-        proposals(first: $first, skip: $skip, where: { space_in: "cakevote.eth", state: $state }) {
+        proposals(first: $first, skip: $skip, where: { space_in: "${PANCAKE_SPACE}", state: $state }) {
           id
         }
       }
@@ -19,7 +20,7 @@ export const getActiveProposal = async (): Promise<string[]> => {
 }
 
 export const useVotingStatus = () => {
-  const { data: proposals = [] } = useSWRImmutable(['proposals', ProposalState.ACTIVE], getActiveProposal)
+  const { data: proposals = [] } = useSWRImmutable('anyActiveProposals', getActiveProposal)
 
   return Boolean(proposals.length)
 }
