087abc805447a19b7e68897d8b5beeb721d08cb1
memoyil <2213635+memoyil@users.noreply.github.com>
2022-09-26 18:43:30 +0200

    feat: Support multichain in transaction updater (#4834)
    * feat: Support multichain in transaction updater
    
    * refactor: Fix tests
    
    * refactor: Remove interval
    
    * refactor: Test name variable
    
    * refactor: Remove duplication
    
    * refactor: Use useProvider hook
    
    * refactor: Remove chainId key from cache
    
    * fix: Pending transactions

--- src/components/Toast/DescriptionWithTx.tsx
+++ src/components/Toast/DescriptionWithTx.tsx
@@ -7,9 +7,14 @@ import truncateHash from '@pancakeswap/utils/truncateHash'
 interface DescriptionWithTxProps {
   description?: string
   txHash?: string
+  txChainId?: number
 }
 
-const DescriptionWithTx: React.FC<React.PropsWithChildren<DescriptionWithTxProps>> = ({ txHash, children }) => {
+const DescriptionWithTx: React.FC<React.PropsWithChildren<DescriptionWithTxProps>> = ({
+  txHash,
+  txChainId,
+  children,
+}) => {
   const { chainId } = useActiveWeb3React()
   const { t } = useTranslation()
 
@@ -17,8 +22,8 @@ const DescriptionWithTx: React.FC<React.PropsWithChildren<DescriptionWithTxProps
     <>
       {typeof children === 'string' ? <Text as="p">{children}</Text> : children}
       {txHash && (
-        <Link external href={getBlockExploreLink(txHash, 'transaction', chainId)}>
-          {t('View on %site%', { site: getBlockExploreName(chainId) })}: {truncateHash(txHash, 8, 0)}
+        <Link external href={getBlockExploreLink(txHash, 'transaction', txChainId || chainId)}>
+          {t('View on %site%', { site: getBlockExploreName(txChainId || chainId) })}: {truncateHash(txHash, 8, 0)}
         </Link>
       )}
     </>
--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,15 @@ import { BLOCKED_ADDRESSES } from './config/constants'
 import ListsUpdater from './state/lists/updater'
 import MulticallUpdater from './state/multicall/updater'
 import TransactionUpdater from './state/transactions/updater'
+import { chains } from './utils/wagmi'
 
 export function Updaters() {
   return (
     <>
       <ListsUpdater />
-      <TransactionUpdater />
+      {chains.map((chain) => (
+        <TransactionUpdater key={`trxUpdater#${chain.id}`} chainId={chain.id} />
+      ))}
       <MulticallUpdater />
     </>
   )
