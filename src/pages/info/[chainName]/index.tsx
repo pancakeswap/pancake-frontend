632b7f7ee0aa084749dca7094206cd4a59b45f1b
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2022-09-27 15:37:28 +0800

    chore: redirect while wallet connected

--- src/pages/info/[chainName]/index.tsx
+++ src/pages/info/[chainName]/index.tsx
@@ -1,7 +1,17 @@
 import { InfoPageLayout } from 'views/Info'
 import Overview from 'views/Info/Overview'
+import { useEffect } from 'react'
+import useActiveWeb3React from 'hooks/useActiveWeb3React'
+import { ChainId } from '@pancakeswap/sdk'
+import { useWeb3React } from '@pancakeswap/wagmi'
 
 const MultiChainPage = () => {
+  const { account } = useWeb3React()
+  const { chainId } = useActiveWeb3React()
+  useEffect(() => {
+    console.log(chainId, '????????')
+    if (account && chainId === ChainId.BSC) window.location.href = '/info'
+  }, [chainId, account])
   return <Overview />
 }
 
--- src/pages/info/index.tsx
+++ src/pages/info/index.tsx
@@ -1,7 +1,16 @@
 import { InfoPageLayout } from 'views/Info'
 import Overview from 'views/Info/Overview'
+import { useEffect } from 'react'
+import { ChainId } from '@pancakeswap/sdk'
+import { useWeb3React } from '@pancakeswap/wagmi'
+import useActiveWeb3React from 'hooks/useActiveWeb3React'
 
 const InfoPage = () => {
+  const { chainId } = useActiveWeb3React()
+  const { account } = useWeb3React()
+  useEffect(() => {
+    if (account && chainId === ChainId.ETHEREUM) window.location.href = '/info/eth'
+  }, [chainId, account])
   return <Overview />
 }
 
