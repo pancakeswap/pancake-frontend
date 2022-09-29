9c89a59c1b37ceb7ef84ad3ef63862418fc8aa47
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2022-09-30 07:02:14 +0800

    chore: auto redirect

--- src/pages/info/[chainName]/index.tsx
+++ src/pages/info/[chainName]/index.tsx
@@ -1,17 +1,7 @@
 import { InfoPageLayout } from 'views/Info'
 import Overview from 'views/Info/Overview'
-import { useEffect } from 'react'
-import useActiveWeb3React from 'hooks/useActiveWeb3React'
-import { ChainId } from '@pancakeswap/sdk'
-import { useWeb3React } from '@pancakeswap/wagmi'
 
 const MultiChainPage = () => {
-  const { account } = useWeb3React()
-  const { chainId } = useActiveWeb3React()
-  useEffect(() => {
-    console.log(chainId, '????????')
-    if (account && chainId === ChainId.BSC) window.location.href = '/info'
-  }, [chainId, account])
   return <Overview />
 }
 
--- src/views/Info/index.tsx
+++ src/views/Info/index.tsx
@@ -1,6 +1,20 @@
+import { ChainId } from '@pancakeswap/sdk'
+import { useWeb3React } from '@pancakeswap/wagmi'
+import useActiveWeb3React from 'hooks/useActiveWeb3React'
+import { useEffect } from 'react'
+import { useGetChainName } from 'state/info/hooks'
 import InfoNav from './components/InfoNav'
 
 export const InfoPageLayout = ({ children }) => {
+  const { account } = useWeb3React()
+  const { chainId } = useActiveWeb3React()
+  const chainName = useGetChainName()
+
+  useEffect(() => {
+    if (account && chainId === ChainId.BSC && window.location.pathname.includes('eth')) window.location.href = '/info'
+    if (account && chainId === ChainId.ETHEREUM && !window.location.pathname.includes('eth'))
+      window.location.href = '/info/eth'
+  }, [chainId, account, chainName])
   return (
     <>
       <InfoNav />
