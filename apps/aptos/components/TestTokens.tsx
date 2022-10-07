fe8b26df0debf55d2a456111bc66856777634e76
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-10-07 14:27:52 +0800

    add cake mint

--- apps/aptos/components/TestTokens.tsx
+++ apps/aptos/components/TestTokens.tsx
@@ -31,12 +31,12 @@ export function TestTokens() {
       .catch((err) => toastError(JSON.stringify({ err })))
   }
 
-  const mint = async (coinType: string) => {
+  const mint = async (coinType: string, module = 'devnet_coins') => {
     if (connector) {
       const pending = await connector.signAndSubmitTransaction({
         type_arguments: [coinType],
         arguments: ['100000000'],
-        function: '0x08c805723ebc0a7fc5b7d3e7b75d567918e806b3461cb9fa21941a9edc0220bf::devnet_coins::mint_to_wallet',
+        function: `0x08c805723ebc0a7fc5b7d3e7b75d567918e806b3461cb9fa21941a9edc0220bf::${module}::mint_to_wallet`,
       })
       toastInfo('Pending', pending.hash)
       try {
@@ -63,6 +63,14 @@ export function TestTokens() {
           </Button>
         )
       })}
+      <Button
+        variant="text"
+        onClick={() =>
+          mint('0x8c805723ebc0a7fc5b7d3e7b75d567918e806b3461cb9fa21941a9edc0220bf::pancake::Cake', 'pancake')
+        }
+      >
+        Cake
+      </Button>
     </AutoColumn>
   )
 }
