c4d5d7773905baea6fe002b79e5c2450650b1456
ChefMomota <98292246+ChefMomota@users.noreply.github.com>
2022-10-12 09:57:58 +0800

    feat: Update farm test (#4935)

--- src/config/__tests__/farms.test.ts
+++ src/config/__tests__/farms.test.ts
@@ -13,17 +13,21 @@ const farmsToTest: [number, SerializedFarm, number][] = farms56
 
 const farms1ToTest: [number, SerializedFarm, number][] = farms1.slice(0, 10).map((farm) => [farm.pid, farm, 1])
 
-describe('Config farms', () => {
-  // TODO: will check if pid not in farms56
-  // it.each([...farmsToTest, ...farms1ToTest])('Farm #%d has an unique pid', (pid) => {
-  //   const duplicates = farms56.filter((f) => pid === f.pid)
-  //   expect(duplicates).toHaveLength(1)
-  // })
+const getDuplicates = (key: 'pid' | 'lpAddress') => {
+  const farms = [...farms56, ...farms1]
+  const keys = farms.map((farm) => farm[key])
+  return keys.filter((data) => keys.indexOf(data) !== keys.lastIndexOf(data))
+}
 
-  it.each([...farmsToTest, ...farms1ToTest])('Farm #%d has an unique address', async (pid, farm) => {
-    const farms = [...farms56, ...farms1]
-    const duplicates = farms.filter((f) => farm.lpAddress === f.lpAddress)
-    expect(duplicates).toHaveLength(1)
+describe('Config farms', () => {
+  it('All farm has an unique pid', () => {
+    const duplicates = getDuplicates('pid')
+    expect(duplicates).toHaveLength(0)
+  })
+
+  it('All farm has an unique address', () => {
+    const duplicates = getDuplicates('lpAddress')
+    expect(duplicates).toHaveLength(0)
   })
 
   it.each([...farmsToTest, ...farms1ToTest])('Farm %d has the correct token addresses', async (pid, farm, chainId) => {
@@ -48,8 +52,8 @@ describe('Config farms', () => {
     expect(farm.token.symbol).not.toEqual(native.symbol)
   })
 
-  // TODO: Add test for ETH
   // The first pid using the new factory
+  // BSC
   const START_PID = 2
   const FACTORY_ADDRESS = '0xca143ce32fe78f1f7019d7d551a6402fc5350c73'
   const newFarmsToTest = farmsToTest.filter((farmSet) => farmSet[0] >= START_PID)
@@ -58,5 +62,16 @@ describe('Config farms', () => {
     const lpContract = getLpContract(farm.lpAddress)
     const factory = await lpContract.factory()
     expect(factory.toLowerCase()).toEqual(FACTORY_ADDRESS)
+  })
+
+  // ETH
+  const ETH_START_PID = 124
+  const ETH_FACTORY_ADDRESS = '0x1097053fd2ea711dad45caccc45eff7548fcb362'
+  const ethNewFarmsToTest = farms1ToTest.filter((farmSet) => farmSet[0] >= ETH_START_PID)
+
+  it.each(ethNewFarmsToTest)('farm %d is using correct factory address', async (pid, farm) => {
+    const lpContract = getLpContract(farm.lpAddress, farm.token.chainId)
+    const factory = await lpContract.factory()
+    expect(factory.toLowerCase()).toEqual(ETH_FACTORY_ADDRESS)
   })
 })
