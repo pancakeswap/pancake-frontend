c236676bef776597d2735b40d7ffdd9217f168c5
PhamPhu19498 <ppphu2912@gmail.com>
2022-10-17 16:48:54 +0700

    remove folder state khong su dung

--- src/state/index.ts
+++ src/state/index.ts
@@ -7,14 +7,12 @@ import burn from './burn/reducer'
 import farmsReducer from './farms'
 import farmsReducerV1 from './farmsV1'
 import { updateVersion } from './global/actions'
-import infoReducer from './info'
 import mint from './mint/reducer'
 import multicall from './multicall/reducer'
 import poolsReducer from './pools'
 import swap from './swap/reducer'
 import transactions from './transactions/reducer'
 import user from './user/reducer'
-import limitOrders from './limitOrders/reducer'
 import globalReducer from './global/reducer'
 
 const PERSISTED_KEYS: string[] = ['user', 'transactions']
@@ -34,8 +32,6 @@ const persistedReducer = persistReducer(
     farms: farmsReducer,
     farmsV1: farmsReducerV1,
     pools: poolsReducer,
-    info: infoReducer,
-    limitOrders,
 
     // Exchange
     user,
