22dbd5fb26fa82a4a24b3d0afdce0cad0ae1b341
PhamPhu19498 <ppphu2912@gmail.com>
2022-10-17 16:46:55 +0700

    remove folder state khong su dung

--- src/components/Menu/hooks/useMenuItemsStatus.ts
+++ src/components/Menu/hooks/useMenuItemsStatus.ts
@@ -3,14 +3,12 @@ import { ChainId } from '@pancakeswap/sdk'
 import { useActiveIfoWithBlocks } from 'hooks/useActiveIfoWithBlocks'
 import { useChainCurrentBlock } from 'state/block/hooks'
 import { PotteryDepositStatus } from 'state/types'
-import { usePotteryStatus } from './usePotteryStatus'
 import { useCompetitionStatus } from './useCompetitionStatus'
 
 export const useMenuItemsStatus = (): Record<string, string> => {
   const currentBlock = useChainCurrentBlock(ChainId.BSC)
   const activeIfo = useActiveIfoWithBlocks()
   const competitionStatus = useCompetitionStatus()
-  const potteryStatus = usePotteryStatus()
 
   const ifoStatus = null
 
@@ -18,9 +16,6 @@ export const useMenuItemsStatus = (): Record<string, string> => {
     return {
       '/competition': competitionStatus,
       '/ifo': ifoStatus === 'coming_soon' ? 'soon' : ifoStatus,
-      ...(potteryStatus === PotteryDepositStatus.BEFORE_LOCK && {
-        '/pottery': 'pot_open',
-      }),
     }
-  }, [competitionStatus, ifoStatus, potteryStatus])
+  }, [competitionStatus, ifoStatus])
 }
