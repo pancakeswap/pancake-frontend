271c60802d5a82ef48c37712d8868886c11da4fa
PhamPhu19498 <ppphu2912@gmail.com>
2022-10-17 16:11:22 +0700

    remove folder view competition

--- src/views/TradingCompetition/pageSectionStyles.tsx
+++
@@ -1,6 +0,0 @@
-export const DARKBG = 'radial-gradient(329.58% 50% at 50% 50%, #3B2864 0%, #191326 100%)'
-export const MIDBLUEBG = 'linear-gradient(180deg, #CBD7EF 0%, #9A9FD0 100%)'
-export const MIDBLUEBG_DARK = 'linear-gradient(180deg, #434575 0%, #66578D 100%)'
-export const LIGHTBLUEBG = 'linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)'
-export const LIGHTBLUEBG_DARK = 'linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)'
-export const TRADINGCOMPETITIONBANNER = '#191335;'
