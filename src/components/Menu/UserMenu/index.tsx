b83e4506522c9433a199f5d6e1339bf2fc441be3
PhamPhu19498 <ppphu2912@gmail.com>
2022-10-17 16:32:11 +0700

    remove folder nft marketplace

--- src/components/Menu/UserMenu/index.tsx
+++ src/components/Menu/UserMenu/index.tsx
@@ -17,7 +17,6 @@ import { useActiveChainId } from 'hooks/useActiveChainId'
 import useAuth from 'hooks/useAuth'
 import NextLink from 'next/link'
 import { useEffect, useState } from 'react'
-import { useProfile } from 'state/profile/hooks'
 import { usePendingTransactions } from 'state/transactions/hooks'
 import { useAccount } from 'wagmi'
 import ProfileUserMenuItem from './ProfileUserMenuItem'
@@ -30,12 +29,9 @@ const UserMenu = () => {
   const { chainId, isWrongNetwork } = useActiveChainId()
   const { logout } = useAuth()
   const { hasPendingTransactions, pendingNumber } = usePendingTransactions()
-  const { isInitialized, isLoading, profile } = useProfile()
   const [onPresentWalletModal] = useModal(<WalletModal initialView={WalletView.WALLET_INFO} />)
   const [onPresentTransactionModal] = useModal(<WalletModal initialView={WalletView.TRANSACTIONS} />)
   const [onPresentWrongNetworkModal] = useModal(<WalletModal initialView={WalletView.WRONG_NETWORK} />)
-  const hasProfile = isInitialized && !!profile
-  const avatarSrc = profile?.nft?.image?.thumbnail
   const [userMenuText, setUserMenuText] = useState<string>('')
   const [userMenuVariable, setUserMenuVariable] = useState<UserMenuVariant>('default')
 
@@ -71,11 +67,6 @@ const UserMenu = () => {
             {t('Your NFTs')}
           </UserMenuItem>
         </NextLink>
-        <ProfileUserMenuItem
-          isLoading={isLoading}
-          hasProfile={hasProfile}
-          disabled={isWrongNetwork || chainId !== ChainId.BSC}
-        />
         <UserMenuDivider />
         <UserMenuItem as="button" onClick={logout}>
           <Flex alignItems="center" justifyContent="space-between" width="100%">
@@ -89,7 +80,7 @@ const UserMenu = () => {
 
   if (account) {
     return (
-      <UIKitUserMenu account={account} avatarSrc={avatarSrc} text={userMenuText} variant={userMenuVariable}>
+      <UIKitUserMenu account={account} text={userMenuText} variant={userMenuVariable}>
         {({ isOpen }) => (isOpen ? <UserMenuItems /> : null)}
       </UIKitUserMenu>
     )
--- src/state/nftMarket/atoms.ts
+++
@@ -1,5 +0,0 @@
-import { atom } from 'jotai'
-
-export const nftMarketFiltersAtom = atom({})
-export const nftMarketActivityFiltersAtom = atom({})
-export const tryVideoNftMediaAtom = atom(true)
