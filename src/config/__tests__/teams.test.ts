8d93df526fe0ed1e224333ffb58ff78b87b80c4f
PhamPhu19498 <ppphu2912@gmail.com>
2022-10-18 15:05:20 +0700

    remove file config khong su dung

--- src/config/__tests__/teams.test.ts
+++
@@ -1,8 +0,0 @@
-import teams from 'config/constants/teams'
-
-describe('Config teams', () => {
-  it.each(teams.map((team) => team.id))('Team #%d has an unique id', (id) => {
-    const duplicates = teams.filter((t) => id === t.id)
-    expect(duplicates).toHaveLength(1)
-  })
-})
