fc81166fa11807fcc5bd7b1f3f4c91265ac67017
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-10-19 19:22:11 +0800

    add testnet bases

--- apps/aptos/config/constants/exchange.ts
+++ apps/aptos/config/constants/exchange.ts
@@ -33,7 +33,21 @@ export const BASES_TO_CHECK_TRADES_AGAINST = {
       'ETH',
     ),
   ],
-  [ChainId.TESTNET]: [APT[ChainId.TESTNET]],
+  [ChainId.TESTNET]: [
+    APT[ChainId.TESTNET],
+    new Coin(
+      ChainId.TESTNET,
+      '0x8c805723ebc0a7fc5b7d3e7b75d567918e806b3461cb9fa21941a9edc0220bf::devnet_coins::DevnetBNB',
+      8,
+      'BNB',
+    ),
+    new Coin(
+      ChainId.TESTNET,
+      '0x8c805723ebc0a7fc5b7d3e7b75d567918e806b3461cb9fa21941a9edc0220bf::devnet_coins::DevnetETH',
+      8,
+      'ETH',
+    ),
+  ],
 }
 
 /**
