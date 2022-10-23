c6ba652ec90b7c715054bb682359faff36655bc3
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2022-10-23 21:39:13 +0800

    chore: aptos banner style for mobile

--- src/views/Home/components/Banners/AptosBanner.tsx
+++ src/views/Home/components/Banners/AptosBanner.tsx
@@ -22,12 +22,16 @@ const AptosTitle = styled.div`
   font-family: 'Kanit';
   font-style: normal;
   font-weight: 600;
-  font-size: 30px;
+  font-size: 23px;
   line-height: 110%;
   color: #ffffff;
   text-shadow: 0px 2px 2px rgba(0, 0, 0, 0.25);
-  margin-bottom: 16px;
-  margin-top: 14px;
+  margin-bottom: 21px;
+  margin-top: 16px;
+
+  @media screen and (max-width: 375px) {
+    font-size: 21px;
+  }
   ${({ theme }) => theme.mediaQueries.sm} {
     font-size: 35px;
     margin-top: 10px;
