637aa82cc93043baecb2443f09d2b8704eb8da32
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-10-28 09:58:47 +0800

    fix: Trust asset address checksum (#5111)

--- apps/web/src/utils/getTokenLogoURL.ts
+++ apps/web/src/utils/getTokenLogoURL.ts
@@ -1,3 +1,5 @@
+import { getAddress } from '@ethersproject/address'
+import memoize from 'lodash/memoize'
 import { ChainId, Token } from '@pancakeswap/sdk'
 
 const mapping = {
@@ -5,11 +7,16 @@ const mapping = {
   [ChainId.ETHEREUM]: 'ethereum',
 }
 
-const getTokenLogoURL = (token?: Token) => {
-  if (token && mapping[token.chainId]) {
-    return `https://assets-cdn.trustwallet.com/blockchains/${mapping[token.chainId]}/assets/${token.address}/logo.png`
-  }
-  return null
-}
+const getTokenLogoURL = memoize(
+  (token?: Token) => {
+    if (token && mapping[token.chainId]) {
+      return `https://assets-cdn.trustwallet.com/blockchains/${mapping[token.chainId]}/assets/${getAddress(
+        token.address,
+      )}/logo.png`
+    }
+    return null
+  },
+  (t) => `${t.chainId}#${t.address}`,
+)
 
 export default getTokenLogoURL
