face2a3d9853e5c1f6dc2187670da2e0cf4deefc
woodywu810 <woodywu810@gmail.com>
2022-10-30 13:51:44 +0800

    fix

--- apps/web/src/views/TradingCompetition/MoDCompetition.tsx
+++ apps/web/src/views/TradingCompetition/MoDCompetition.tsx
@@ -1,4 +1,4 @@
-import { React, useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { useTranslation } from '@pancakeswap/localization'
 import { useProfile } from 'state/profile/hooks'
 import { Flex, Box, useMatchBreakpoints } from '@pancakeswap/uikit'
