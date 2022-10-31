a7e6744bee7875018a1b8f1714ea74e2c203abd9
apt-waffle <113099966+apt-waffle@users.noreply.github.com>
2022-10-31 22:23:45 +1300

    fix: Remove IFO steps

--- apps/aptos/views/Ifos/SoonIfo.tsx
+++ apps/aptos/views/Ifos/SoonIfo.tsx
@@ -1,12 +1,6 @@
 import IfoContainer from './components/IfoContainer'
-import IfoSteps from './components/IfoSteps'
 import ComingSoonSection from './components/ComingSoonSection'
 
-const SoonIfo = () => (
-  <IfoContainer
-    ifoSection={<ComingSoonSection />}
-    ifoSteps={<IfoSteps isLive={false} hasClaimed={false} isCommitted={false} ifoCurrencyAddress="TODO: Aptos" />}
-  />
-)
+const SoonIfo = () => <IfoContainer ifoSection={<ComingSoonSection />} />
 
 export default SoonIfo
