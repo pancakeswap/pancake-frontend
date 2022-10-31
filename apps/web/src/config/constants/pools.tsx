489e4933837227e634dc24d8a5cca99b56c4d9fe
memoyil <2213635+memoyil@users.noreply.github.com>
2022-10-31 16:30:55 +0100

    chore: Move wom to finished pools (#5145)

--- apps/web/src/config/constants/pools.tsx
+++ apps/web/src/config/constants/pools.tsx
@@ -162,6 +162,14 @@ export const livePools: SerializedPoolConfig[] = [
     tokenPerBlock: '55.6446',
     version: 3,
   },
+].map((p) => ({
+  ...p,
+  stakingToken: p.stakingToken.serialize,
+  earningToken: p.earningToken.serialize,
+}))
+
+// known finished pools
+const finishedPools = [
   {
     sousId: 295,
     stakingToken: bscTokens.cake,
@@ -174,14 +182,6 @@ export const livePools: SerializedPoolConfig[] = [
     tokenPerBlock: '3.4722',
     version: 3,
   },
-].map((p) => ({
-  ...p,
-  stakingToken: p.stakingToken.serialize,
-  earningToken: p.earningToken.serialize,
-}))
-
-// known finished pools
-const finishedPools = [
   {
     sousId: 294,
     stakingToken: bscTokens.cake,
