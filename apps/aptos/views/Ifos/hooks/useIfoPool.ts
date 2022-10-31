f233ff7fd5c00e144821a5ad70a06112ff544dfd
apt-waffle <113099966+apt-waffle@users.noreply.github.com>
2022-10-31 23:09:48 +1300

    fix: Remove manual fetch since we poll, fix user and total amount sync issue

--- apps/aptos/views/Ifos/hooks/useIfoPool.ts
+++ apps/aptos/views/Ifos/hooks/useIfoPool.ts
@@ -1,13 +1,14 @@
 /* eslint-disable camelcase */
-import { useTableItem } from '@pancakeswap/awgmi'
+import { useTableItem, UseTableItemConfig } from '@pancakeswap/awgmi'
 import { IfoPoolKey, IFO_RESOURCE_ACCOUNT_TYPE_POOL_STORE } from 'views/Ifos/constants'
 import { RootObject as IFOPool } from 'views/Ifos/generated/IFOPool'
 import { useIfoResources } from './useIfoResources'
 
-export const useIfoPool = () => {
+export const useIfoPool = (params: UseTableItemConfig<IFOPool> = {}) => {
   const resources = useIfoResources()
 
   return useTableItem<IFOPool>({
+    ...params,
     handle: resources.data?.[IFO_RESOURCE_ACCOUNT_TYPE_POOL_STORE]?.data.ifo_pools.handle,
     data: resources.data?.[IFO_RESOURCE_ACCOUNT_TYPE_POOL_STORE]
       ? {
--- apps/aptos/views/Ifos/hooks/v3/useGetWalletIfoData.ts
+++ apps/aptos/views/Ifos/hooks/v3/useGetWalletIfoData.ts
@@ -1,9 +1,11 @@
 /* eslint-disable camelcase */
 import { Contract } from '@ethersproject/contracts'
+import { useAccount } from '@pancakeswap/awgmi'
 import { BIG_ZERO } from '@pancakeswap/utils/bigNumber'
 import BigNumber from 'bignumber.js'
 import { Ifo, PoolIds } from 'config/constants/types'
 import { useState, useCallback } from 'react'
+import { RootObject as IFOPool } from 'views/Ifos/generated/IFOPool'
 import { computeOfferingAndRefundAmount } from 'views/Ifos/utils'
 import { WalletIfoState, WalletIfoData } from '../../types'
 import { useIfoPool } from '../useIfoPool'
@@ -31,6 +33,8 @@ const initialState = {
  * Gets all data from an IFO related to a wallet
  */
 export const useGetWalletIfoData = (_ifo: Ifo): WalletIfoData => {
+  const { account } = useAccount()
+
   const [state, setState] = useState<WalletIfoState>(initialState)
 
   const contract = {} as Contract
@@ -54,36 +58,41 @@ export const useGetWalletIfoData = (_ifo: Ifo): WalletIfoData => {
     }))
   }
 
-  const pool = useIfoPool()
   const userInfo = useIfoUserInfo()
   const vestingCharacteristics = useVestingCharacteristics()
 
-  const fetchIfoData = useCallback(async () => {
-    const { tax_amount: taxAmountInLP, refunding_amount: refundingAmountInLP } =
-      pool.data && userInfo.data
-        ? computeOfferingAndRefundAmount(userInfo.data, pool.data)
+  const handleOnSettled = useCallback(() => {
+    if (!account && state.isInitialized) {
+      setState(initialState)
+    }
+  }, [userInfo, vestingCharacteristics])
+
+  const handleOnSuccess = useCallback(
+    (data: IFOPool) => {
+      const { tax_amount: taxAmountInLP, refunding_amount: refundingAmountInLP } = userInfo.data
+        ? computeOfferingAndRefundAmount(userInfo.data, data)
         : {
             tax_amount: BIG_ZERO,
             refunding_amount: BIG_ZERO,
           }
 
-    setState((prevState) => ({
-      ...prevState,
-      isInitialized: true,
-      poolUnlimited: {
-        ...prevState.poolUnlimited,
-        ...vestingCharacteristics,
-        amountTokenCommittedInLP: userInfo.data ? new BigNumber(userInfo.data.amount) : BIG_ZERO,
-        hasClaimed: userInfo.data?.claimed ?? false,
-        refundingAmountInLP,
-        taxAmountInLP,
-      },
-    }))
-  }, [pool, userInfo, vestingCharacteristics])
+      setState((prevState) => ({
+        ...prevState,
+        isInitialized: true,
+        poolUnlimited: {
+          ...prevState.poolUnlimited,
+          ...vestingCharacteristics,
+          amountTokenCommittedInLP: userInfo.data ? new BigNumber(userInfo.data.amount) : BIG_ZERO,
+          hasClaimed: userInfo.data?.claimed ?? false,
+          refundingAmountInLP,
+          taxAmountInLP,
+        },
+      }))
+    },
+    [userInfo, vestingCharacteristics],
+  )
 
-  const resetIfoData = useCallback(() => {
-    setState({ ...initialState })
-  }, [])
+  useIfoPool({ onSettled: handleOnSettled, onSuccess: handleOnSuccess })
 
-  return { ...state, contract, setPendingTx, setIsClaimed, fetchIfoData, resetIfoData }
+  return { ...state, contract, setPendingTx, setIsClaimed }
 }
