689a37edd0c7332e598c7a31540ce919815c6379
apt-waffle <113099966+apt-waffle@users.noreply.github.com>
2022-10-31 23:35:15 +1300

    fix: Decimal issue when committing via multiplier

--- apps/aptos/components/ConfirmButton.tsx
+++ apps/aptos/components/ConfirmButton.tsx
@@ -2,26 +2,14 @@ import { useTranslation } from '@pancakeswap/localization'
 import { Button as UIKitButton, AutoRenewIcon } from '@pancakeswap/uikit'
 import styled from 'styled-components'
 
-export enum ButtonArrangement {
-  ROW = 'row',
-  SEQUENTIAL = 'sequential',
-}
-
 interface ConfirmButtonProps {
   isConfirming: boolean
   isConfirmDisabled: boolean
   onConfirm: () => void
-  buttonArrangement?: ButtonArrangement
-  confirmLabel?: string
-  confirmId?: string
 }
 
 const Button = styled(UIKitButton)`
   width: 100%;
-
-  ${({ theme }) => theme.mediaQueries.md} {
-    min-width: 160px;
-  }
 `
 
 const spinnerIcon = <AutoRenewIcon spin color="currentColor" />
@@ -30,21 +18,17 @@ export const ConfirmButton: React.FC<React.PropsWithChildren<ConfirmButtonProps>
   isConfirming,
   isConfirmDisabled,
   onConfirm,
-  confirmLabel,
-  confirmId,
 }) => {
   const { t } = useTranslation()
-  const confirmButtonText = confirmLabel ?? t('Confirm')
 
   return (
     <Button
-      id={confirmId}
       onClick={onConfirm}
       disabled={isConfirmDisabled}
       isLoading={isConfirming}
       endIcon={isConfirming ? spinnerIcon : undefined}
     >
-      {isConfirming ? t('Confirming') : confirmButtonText}
+      {isConfirming ? t('Confirming') : t('Confirm')}
     </Button>
   )
 }
