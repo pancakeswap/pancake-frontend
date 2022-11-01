af84443c555b77f9046e587b445d91ca55228eae
memoyil <2213635+memoyil@users.noreply.github.com>
2022-11-01 23:19:34 +0100

    fix: (info): Pools data fetch (#5161)
    * fix: (info): Pools data fetch
    
    * fix: (info): Add chain id to multicall
    
    * fix: (info): Add chain id to token multicall

--- apps/web/src/state/info/queries/types.ts
+++ apps/web/src/state/info/queries/types.ts
@@ -12,7 +12,7 @@ interface PairResponse {
 export interface MintResponse {
   id: string
   timestamp: string
-  pair: PairResponse
+  pair?: PairResponse
   to: string
   amount0: string
   amount1: string
@@ -22,7 +22,7 @@ export interface MintResponse {
 export interface SwapResponse {
   id: string
   timestamp: string
-  pair: PairResponse
+  pair?: PairResponse
   from: string
   amount0In: string
   amount1In: string
@@ -34,7 +34,7 @@ export interface SwapResponse {
 export interface BurnResponse {
   id: string
   timestamp: string
-  pair: PairResponse
+  pair?: PairResponse
   sender: string
   amount0: string
   amount1: string
