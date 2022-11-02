ab85ff05a0d0e48d45806dd58fbb6077e22f24a9
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-11-02 13:07:48 +0800

    Add devnet on development mode

--- apps/aptos/config/chains.ts
+++ apps/aptos/config/chains.ts
@@ -1,6 +1,8 @@
 import { defaultChain } from '@pancakeswap/awgmi'
-import { mainnet, testnet } from '@pancakeswap/awgmi/core'
+import { mainnet, testnet, devnet, Chain } from '@pancakeswap/awgmi/core'
 
 export { defaultChain }
 
-export const chains = [mainnet, testnet]
+export const chains = [mainnet, testnet, process.env.NODE_ENV === 'development' ? devnet : undefined].filter(
+  Boolean,
+) as Chain[]
