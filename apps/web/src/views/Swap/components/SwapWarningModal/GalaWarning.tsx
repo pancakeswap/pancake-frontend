664ca100073db3ff7472e2f0e00266fe36df43a8
chefmomota <momota@pancakeswap.com>
2022-11-04 11:20:16 +0800

    chore: Gala warning text

--- apps/web/src/views/Swap/components/SwapWarningModal/GalaWarning.tsx
+++ apps/web/src/views/Swap/components/SwapWarningModal/GalaWarning.tsx
@@ -8,7 +8,7 @@ const GalaWarning = () => {
     <>
       <Text>{t('Warning: The pGALA token pool is not a valid token trading pair - please stop buying.')}</Text>
       <LinkExternal href="https://twitter.com/pNetworkDeFi/status/1588266897061031936">
-        {t('For more information, please see their')}
+        {t('For more info, please see pGALA’s twitter.')}
       </LinkExternal>
     </>
   )
