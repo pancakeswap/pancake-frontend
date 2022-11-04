b8b6fbe2a14cc513eb6161bfb317093afc0b7c29
chefmomota <momota@pancakeswap.com>
2022-11-04 12:59:30 +0800

    chore: Hide farm router

--- apps/aptos/components/Menu/Menu.tsx
+++ apps/aptos/components/Menu/Menu.tsx
@@ -53,10 +53,13 @@ const config: (t: ContextApi['t']) => ConfigMenuItemsType[] = (t) => [
     icon: EarnIcon,
     fillIcon: EarnFillIcon,
     image: '/images/decorations/pe2.png',
+    disabled: true,
     items: [
       {
         label: t('Farms'),
         href: '/farms',
+        status: { text: t('Soon'), color: 'warning' },
+        disabled: true,
       },
       {
         label: t('Pools'),
