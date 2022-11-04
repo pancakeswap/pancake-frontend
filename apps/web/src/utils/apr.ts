1abcfaec804411e3a2c4f47a14c1585fc757defd
ChefMomota <98292246+ChefMomota@users.noreply.github.com>
2022-11-04 10:10:03 +0800

    fix: Ethereum farm apr (#5192)

--- apps/web/src/utils/apr.ts
+++ apps/web/src/utils/apr.ts
@@ -1,11 +1,15 @@
 import BigNumber from 'bignumber.js'
+import { ChainId } from '@pancakeswap/sdk'
 import { BLOCKS_PER_YEAR } from 'config'
 import lpAprs56 from 'config/constants/lpAprs/56.json'
+import lpAprs1 from 'config/constants/lpAprs/1.json'
 
 const getLpApr = (chainId: number) => {
   switch (chainId) {
-    case 56:
+    case ChainId.BSC:
       return lpAprs56
+    case ChainId.ETHEREUM:
+      return lpAprs1
     default:
       return {}
   }
