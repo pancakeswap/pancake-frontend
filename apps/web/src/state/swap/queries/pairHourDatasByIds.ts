a6eb78cafd03ea93afe9a1c792ca5d05dd6d2387
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2022-11-08 15:40:19 +0800

    chore: fix swap chart issue

--- apps/web/src/state/swap/queries/pairHourDatasByIds.ts
+++ apps/web/src/state/swap/queries/pairHourDatasByIds.ts
@@ -1,7 +1,7 @@
 import { gql } from 'graphql-request'
 
 const pairHourDatasByIds = gql`
-  query pairHourDatasByIds($pairIds: [String]) {
+  query pairHourDatasByIds($pairIds: [ID!]) {
     pairHourDatas(where: { id_in: $pairIds }, orderBy: hourStartUnix, orderDirection: desc) {
       id
       hourStartUnix
