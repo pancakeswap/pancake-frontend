14c074009305ba8127db5ebd20f1e03f727b5eed
chefjackson <116779127+chefjackson@users.noreply.github.com>
2022-11-08 14:18:25 +0800

    fix: uncaught exception when making multicall to non existing address

--- packages/smart-router/evm/onchain/getPairs.ts
+++ packages/smart-router/evm/onchain/getPairs.ts
@@ -27,66 +27,52 @@ interface Options {
 }
 
 export async function getPairs(currencyPairs: CurrencyPair[], { provider, chainId }: Options): Promise<Pair[]> {
-  const p = provider({ chainId })
   const tokens: [Token | undefined, Token | undefined][] = currencyPairs.map(([currencyA, currencyB]) => [
     wrappedCurrency(currencyA, chainId),
     wrappedCurrency(currencyB, chainId),
   ])
 
-  const addressChecks = await Promise.all(
-    tokens.map((tokenPair) => {
-      const addr = getPairAddress(tokenPair)
-      return p.getCode(addr)
-    }),
-  )
-
-  const validTokenPairs = tokens.filter((_, index) => addressChecks[index] !== '0x')
-  const validPairAddresses = validTokenPairs.map(getPairAddress)
+  const pairAddresses = tokens.map(getPairAddress)
 
   const { multicallv2 } = createMulticall(provider)
-  const reserveCalls: Call[] = validPairAddresses.map((address) => ({
+  const reserveCalls: Call[] = pairAddresses.map((address) => ({
     address: address as string,
     name: 'getReserves',
     params: [],
   }))
 
-  try {
-    const results = await multicallv2<PairReserve[]>({
-      abi: IPancakePairABI,
-      calls: reserveCalls,
-      chainId,
-      options: {
-        requireSuccess: false,
-      },
-    })
+  const results = await multicallv2<PairReserve[]>({
+    abi: IPancakePairABI,
+    calls: reserveCalls,
+    chainId,
+    options: {
+      requireSuccess: false,
+    },
+  })
 
-    const resultWithState: [PairState, Pair | null][] = results.map((result, i) => {
-      if (!result) return [PairState.NOT_EXISTS, null]
+  const resultWithState: [PairState, Pair | null][] = results.map((result, i) => {
+    if (!result) return [PairState.NOT_EXISTS, null]
 
-      const tokenA = validTokenPairs[i][0]
-      const tokenB = validTokenPairs[i][1]
+    const tokenA = tokens[i][0]
+    const tokenB = tokens[i][1]
 
-      if (!tokenA || !tokenB || tokenA.equals(tokenB)) return [PairState.INVALID, null]
+    if (!tokenA || !tokenB || tokenA.equals(tokenB)) return [PairState.INVALID, null]
 
-      const [token0, token1] = tokenA.sortsBefore(tokenB) ? [tokenA, tokenB] : [tokenB, tokenA]
+    const [token0, token1] = tokenA.sortsBefore(tokenB) ? [tokenA, tokenB] : [tokenB, tokenA]
 
-      const { reserve0, reserve1 } = result
-      return [
-        PairState.EXISTS,
-        new Pair(
-          CurrencyAmount.fromRawAmount(token0, reserve0.toString()),
-          CurrencyAmount.fromRawAmount(token1, reserve1.toString()),
-        ),
-      ]
-    })
-    const successfulResult: [PairState.EXISTS, Pair][] = resultWithState.filter(
-      (result): result is [PairState.EXISTS, Pair] => Boolean(result[0] === PairState.EXISTS && result[1]),
-    )
-    return successfulResult.map(([, pair]) => pair)
-  } catch (e) {
-    console.error(`Get reserve failed`, e)
-    return []
-  }
+    const { reserve0, reserve1 } = result
+    return [
+      PairState.EXISTS,
+      new Pair(
+        CurrencyAmount.fromRawAmount(token0, reserve0.toString()),
+        CurrencyAmount.fromRawAmount(token1, reserve1.toString()),
+      ),
+    ]
+  })
+  const successfulResult: [PairState.EXISTS, Pair][] = resultWithState.filter(
+    (result): result is [PairState.EXISTS, Pair] => Boolean(result[0] === PairState.EXISTS && result[1]),
+  )
+  return successfulResult.map(([, pair]) => pair)
 }
 
 function getPairAddress([tokenA, tokenB]: [Token | undefined, Token | undefined]): string {
