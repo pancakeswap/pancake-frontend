9c223020a28ca50f992e1ae8ecbc67e82cbbc7c9
chefmomota <momota@pancakeswap.com>
2022-11-08 15:52:17 +0800

    feat: Add mainnet and testnet

--- apps/aptos/config/__tests__/farms.test.ts
+++ apps/aptos/config/__tests__/farms.test.ts
@@ -1,16 +1,23 @@
 import { describe, it, expect } from 'vitest'
 import { ChainId, Pair, Coin } from '@pancakeswap/aptos-swap-sdk'
 import { SerializedFarm } from '@pancakeswap/farms'
+import farms1 from '../constants/farms/1'
 import farms2 from '../constants/farms/2'
 
 // Test only against the last 10 farms, for performance concern
-const farmsToTest: [number, SerializedFarm, number][] = farms2
+const mainnetFarmsToTest: [number, SerializedFarm, number][] = farms1
+  .filter((farm) => farm.pid !== 0 && farm.pid !== null)
+  .slice(0, 10)
+  .map((farm) => [farm.pid, farm, ChainId.MAINNET])
+
+const farms1ToTest: [number, SerializedFarm, number][] = farms2
   .filter((farm) => farm.pid !== 0 && farm.pid !== null)
   .slice(0, 10)
   .map((farm) => [farm.pid, farm, ChainId.TESTNET])
 
 const getDuplicates = (key: 'pid' | 'lpAddress') => {
-  const keys = farms2.map((farm) => farm[key])
+  const farms = [...farms1, ...farms2]
+  const keys = farms.map((farm) => farm[key])
   return keys.filter((data) => keys.indexOf(data) !== keys.lastIndexOf(data))
 }
 
@@ -25,7 +32,7 @@ describe('Config farms', () => {
     expect(duplicates).toHaveLength(0)
   })
 
-  it.each(farmsToTest)('Farm %d has the correct token addresses', async (pid, farm) => {
+  it.each([...mainnetFarmsToTest, ...farms1ToTest])('Farm %d has the correct token addresses', async (pid, farm) => {
     const token = new Coin(farm.token.chainId, farm.token.address, farm.token.decimals, farm.token.symbol)
     const quoteToken = new Coin(
       farm.quoteToken.chainId,
