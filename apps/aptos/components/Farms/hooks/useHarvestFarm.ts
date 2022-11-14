1dcf340aa8d89a71a1424ac1e085a886867e4642
chefilip <philip@pancakeswap.com>
2022-11-14 18:28:34 -0400

    chore: Consolidate transaction simulation and submission

--- apps/aptos/components/Farms/hooks/useHarvestFarm.ts
+++ apps/aptos/components/Farms/hooks/useHarvestFarm.ts
@@ -1,14 +1,14 @@
 import { useCallback } from 'react'
-import { useSendTransaction } from '@pancakeswap/awgmi'
 import { masterchefDeposit } from 'config/constants/contracts/masterchef'
+import useSimulationAndSendTransaction from 'hooks/useSimulationAndSendTransaction'
 
 const useHarvestFarm = (_farmPid: number, tokenType: string) => {
-  const { sendTransactionAsync } = useSendTransaction()
+  const executeTransaction = useSimulationAndSendTransaction()
 
   const handleHarvest = useCallback(async () => {
     const payload = masterchefDeposit([_farmPid.toString(), '0'], [tokenType])
-    return sendTransactionAsync({ payload })
-  }, [_farmPid, sendTransactionAsync, tokenType])
+    return executeTransaction(payload)
+  }, [_farmPid, executeTransaction, tokenType])
 
   return { onReward: handleHarvest }
 }
