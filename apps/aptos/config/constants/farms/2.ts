8fa628ac98d0ac6913b7ad3adec8a24daec058da
chefmomota <momota@pancakeswap.com>
2022-11-14 17:46:33 +0800

    chore: Add new test farm config

--- apps/aptos/config/constants/farms/2.ts
+++ apps/aptos/config/constants/farms/2.ts
@@ -16,6 +16,14 @@ const farms: SerializedFarmConfig[] = [
     quoteToken: testnetTokens.cake,
   },
   {
+    pid: 6,
+    lpSymbol: 'USDC-CAKE LP',
+    lpAddress:
+      '0xc7efb4076dbe143cbcd98cfaaa929ecfc8f299203dfff63b95ccb6bfe19850fa::swap::LPToken<0x8c805723ebc0a7fc5b7d3e7b75d567918e806b3461cb9fa21941a9edc0220bf::devnet_coins::DevnetUSDC, 0x9460030cb9e68bf33ced3e65d48e8c62610acd9f13c1a6197740733d8b72be60::pancake::Cake>',
+    token: testnetTokens.cake,
+    quoteToken: L0_USDC[ChainId.TESTNET],
+  },
+  {
     pid: 5,
     lpSymbol: 'APT-CAKE LP',
     lpAddress:
