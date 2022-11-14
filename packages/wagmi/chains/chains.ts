e8006620bd45ca7055ec96557a2e0304a7d38abe
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-11-14 11:06:11 +0800

    fix(wagmi): Update bsc testnet rpc

--- packages/wagmi/chains/chains.ts
+++ packages/wagmi/chains/chains.ts
@@ -102,8 +102,8 @@ export const bscTest: Chain = {
     symbol: 'tBNB',
   },
   rpcUrls: {
-    public: 'https://data-seed-prebsc-1-s2.binance.org:8545/',
-    default: 'https://data-seed-prebsc-1-s2.binance.org:8545/',
+    public: 'https://data-seed-prebsc-1-s3.binance.org:8545',
+    default: 'https://data-seed-prebsc-1-s3.binance.org:8545',
   },
   blockExplorers: {
     default: { name: 'BscScan', url: 'https://testnet.bscscan.com' },
