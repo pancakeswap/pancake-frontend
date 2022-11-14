8b975ea90dea559ee751dc0b0eb2d90d63ebd9f7
chefmomota <momota@pancakeswap.com>
2022-11-14 12:23:18 +0800

    chore: Use usePriceCakeBusd

--- apps/aptos/components/Farms/components/FarmTable/Actions/HarvestAction.tsx
+++ apps/aptos/components/Farms/components/FarmTable/Actions/HarvestAction.tsx
@@ -6,7 +6,7 @@ import useCatchTxError from 'hooks/useCatchTxError'
 import { BIG_ZERO } from '@pancakeswap/utils/bigNumber'
 import { getBalanceAmount } from '@pancakeswap/utils/formatBalance'
 import BigNumber from 'bignumber.js'
-import { useCakePrice } from 'hooks/useStablePrice'
+import { usePriceCakeBusd } from 'hooks/useStablePrice'
 import { FARM_DEFAULT_DECIMALS } from 'components/Farms/constants'
 import useHarvestFarm from '../../../hooks/useHarvestFarm'
 import { FarmWithStakedValue } from '../../types'
@@ -33,7 +33,7 @@ export const HarvestAction: React.FunctionComponent<React.PropsWithChildren<Harv
   const { toastSuccess } = useToast()
   const { fetchWithCatchTxError, loading: pendingTx } = useCatchTxError()
   const earningsBigNumber = userData?.earnings ? new BigNumber(userData.earnings) : BIG_ZERO
-  const { data: cakePrice } = useCakePrice()
+  const cakePrice = usePriceCakeBusd()
   let earnings = BIG_ZERO
   let earningsBusd = 0
   let displayBalance = userDataReady ? earnings.toFixed(5, BigNumber.ROUND_DOWN) : <Skeleton width={60} />
