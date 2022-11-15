c8b35016a459ab7c29bc52074947d5224bd9fe9a
chefjackson <116779127+chefjackson@users.noreply.github.com>
2022-11-16 06:30:34 +0800

    feat: add util functions for exchanging trade with stable swap (#5313)

---
+++ apps/web/src/views/Swap/SmartSwap/utils/exchange.ts
@@ -0,0 +1,81 @@
+import { Currency, CurrencyAmount, Fraction, JSBI, Percent, TradeType } from '@pancakeswap/sdk'
+import { TradeWithStableSwap, Trade } from '@pancakeswap/smart-router/dist/evm'
+
+import { BIPS_BASE, INPUT_FRACTION_AFTER_FEE, ONE_HUNDRED_PERCENT } from 'config/constants/exchange'
+import { Field } from 'state/swap/actions'
+import { basisPointsToPercent } from 'utils/exchange'
+
+export function calculateSlippageAmount(value: CurrencyAmount<Currency>, slippage: number): [JSBI, JSBI] {
+  if (slippage < 0 || slippage > 10000) {
+    throw Error(`Unexpected slippage value: ${slippage}`)
+  }
+  return [
+    JSBI.divide(JSBI.multiply(value.quotient, JSBI.BigInt(10000 - slippage)), BIPS_BASE),
+    JSBI.divide(JSBI.multiply(value.quotient, JSBI.BigInt(10000 + slippage)), BIPS_BASE),
+  ]
+}
+
+// computes price breakdown for the trade
+export function computeTradePriceBreakdown(trade?: TradeWithStableSwap<Currency, Currency, TradeType> | null): {
+  priceImpactWithoutFee: Percent | undefined
+  realizedLPFee: CurrencyAmount<Currency> | undefined | null
+} {
+  // for each hop in our trade, take away the x*y=k price impact from 0.3% fees
+  // e.g. for 3 tokens/2 hops: 1 - ((1 - .03) * (1-.03))
+  const realizedLPFee = !trade
+    ? undefined
+    : ONE_HUNDRED_PERCENT.subtract(
+        trade.route.pairs.reduce<Fraction>(
+          (currentFee: Fraction): Fraction => currentFee.multiply(INPUT_FRACTION_AFTER_FEE),
+          ONE_HUNDRED_PERCENT,
+        ),
+      )
+
+  // remove lp fees from price impact
+  const priceImpactWithoutFeeFraction =
+    trade && realizedLPFee ? Trade.priceImpact(trade).subtract(realizedLPFee) : undefined
+
+  // the x*y=k impact
+  const priceImpactWithoutFeePercent = priceImpactWithoutFeeFraction
+    ? new Percent(priceImpactWithoutFeeFraction?.numerator, priceImpactWithoutFeeFraction?.denominator)
+    : undefined
+
+  // the amount of the input that accrues to LPs
+  const realizedLPFeeAmount =
+    realizedLPFee &&
+    trade &&
+    CurrencyAmount.fromRawAmount(
+      trade.inputAmount.currency,
+      realizedLPFee.multiply(trade.inputAmount.quotient).quotient,
+    )
+
+  return { priceImpactWithoutFee: priceImpactWithoutFeePercent, realizedLPFee: realizedLPFeeAmount }
+}
+
+// computes the minimum amount out and maximum amount in for a trade given a user specified allowed slippage in bips
+export function computeSlippageAdjustedAmounts(
+  trade: TradeWithStableSwap<Currency, Currency, TradeType> | undefined,
+  allowedSlippage: number,
+): { [field in Field]?: CurrencyAmount<Currency> } {
+  const pct = basisPointsToPercent(allowedSlippage)
+  return {
+    [Field.INPUT]: trade && Trade.maximumAmountIn(trade, pct),
+    [Field.OUTPUT]: trade && Trade.minimumAmountOut(trade, pct),
+  }
+}
+
+export function formatExecutionPrice(
+  trade?: TradeWithStableSwap<Currency, Currency, TradeType>,
+  inverted?: boolean,
+): string {
+  if (!trade) {
+    return ''
+  }
+  return inverted
+    ? `${Trade.executionPrice(trade).invert().toSignificant(6)} ${trade.inputAmount.currency.symbol} / ${
+        trade.outputAmount.currency.symbol
+      }`
+    : `${Trade.executionPrice(trade).toSignificant(6)} ${trade.outputAmount.currency.symbol} / ${
+        trade.inputAmount.currency.symbol
+      }`
+}
