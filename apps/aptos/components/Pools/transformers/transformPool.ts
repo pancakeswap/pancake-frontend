1bab2e2e614f9b41eb50194b1c40c651ad0e5704
chefilip <philip@pancakeswap.com>
2022-11-14 22:24:48 -0400

    chor: Add finished flag

--- apps/aptos/components/Pools/transformers/transformPool.ts
+++ apps/aptos/components/Pools/transformers/transformPool.ts
@@ -21,7 +21,7 @@ const transformPool = (
   chainId,
   prices,
 ): Pool.DeserializedPool<Coin | AptosCoin> | undefined => {
-  const startTime = _get(resource, 'resource.start_timestamp', '0')
+  const startTime = _toNumber(_get(resource, 'data.start_timestamp', '0'))
 
   const startYet = getSecondsLeftFromNow(startTime)
 
@@ -89,8 +89,6 @@ const transformPool = (
     }
   }
 
-  const nowInSeconds = Math.floor(Date.now() / 1000)
-
   const stakingToken = getTokenByAddress({ chainId, address: stakingAddress })
   const earningToken = getTokenByAddress({ chainId, address: earningAddress })
 
@@ -107,6 +105,8 @@ const transformPool = (
       totalStaked: totalStakedToken,
     }) || 0
 
+  const isFinished = getSecondsLeftFromNow(_toNumber(_get(resource, 'data.end_timestamp', '0')))
+
   return {
     // Ignore sousId
     sousId: 0,
@@ -119,7 +119,7 @@ const transformPool = (
     earningTokenPrice,
     stakingTokenPrice,
 
-    isFinished: nowInSeconds > +resource.data.end_timestamp,
+    isFinished: Boolean(isFinished),
     poolCategory: PoolCategory.CORE,
     startBlock: _toNumber(resource.data.start_timestamp),
     tokenPerBlock: resource.data.reward_per_second,
