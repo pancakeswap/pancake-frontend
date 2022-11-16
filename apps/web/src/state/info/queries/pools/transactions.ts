942d63935ead83c0b341706f311ff9b6b263e8b6
memoyil <2213635+memoyil@users.noreply.github.com>
2022-11-16 14:03:27 +0100

    chore: Remove warnings

--- apps/web/src/state/info/queries/pools/transactions.ts
+++ apps/web/src/state/info/queries/pools/transactions.ts
@@ -1,5 +1,5 @@
 import { gql } from 'graphql-request'
-import { mapBurns, mapMints, mapSwaps, getPairTokenMap } from 'state/info/queries/helpers'
+import { mapBurns, mapMints, mapSwaps } from 'state/info/queries/helpers'
 import { BurnResponse, MintResponse, SwapResponse } from 'state/info/queries/types'
 import { Transaction } from 'state/info/types'
 import { MultiChainName, getMultiChainQueryEndPointWithStableSwap } from '../../constant'
