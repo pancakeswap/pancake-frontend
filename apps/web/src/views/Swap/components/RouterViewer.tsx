f223bc1c22f975017abd0dfeb3ce3902f51e8a77
chefjackson <116779127+chefjackson@users.noreply.github.com>
2022-11-17 11:01:07 +0800

    feat: remove dependency of v2 trade in smart swap

--- apps/web/src/views/Swap/components/RouterViewer.tsx
+++ apps/web/src/views/Swap/components/RouterViewer.tsx
@@ -1,4 +1,5 @@
-import { Currency, Pair } from '@pancakeswap/sdk'
+import { Currency } from '@pancakeswap/sdk'
+import { Pair, isStableSwapPair } from '@pancakeswap/smart-router/evm'
 import { Box, Flex } from '@pancakeswap/uikit'
 import { CurrencyLogo, DoubleCurrencyLogo } from 'components/Logo'
 import styled from 'styled-components'
@@ -24,9 +25,9 @@ export const RouterViewer: React.FC<RouterViewerProps> = ({ pairs, inputCurrency
     <RouterBox justifyContent="space-between">
       <CurrencyLogo currency={inputCurrency} />
       {pairs &&
-        pairs.map((d) => (
-          <RouterPoolBox key={`tradingPairIds${d.liquidityToken.address}`}>
-            <DoubleCurrencyLogo currency0={d.token0} currency1={d.token1} />
+        pairs.map((p) => (
+          <RouterPoolBox key={`tradingPairIds${isStableSwapPair(p) ? p.stableSwapAddress : p.liquidityToken.address}`}>
+            <DoubleCurrencyLogo currency0={p.token0} currency1={p.token1} />
           </RouterPoolBox>
         ))}
       <CurrencyLogo currency={outputCurrency} />
