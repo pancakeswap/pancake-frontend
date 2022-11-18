2440d17d69dfc980a7ff1bbf5cf79b359419df6e
ChefMomota <98292246+ChefMomota@users.noreply.github.com>
2022-11-18 12:32:06 +0800

    feat(aptos): Add bridge link (#5364)

--- apps/aptos/components/Layout/ExchangeLayout.tsx
+++ apps/aptos/components/Layout/ExchangeLayout.tsx
@@ -1,10 +1,13 @@
+import { useTranslation } from '@pancakeswap/localization'
 import { Image, Swap } from '@pancakeswap/uikit'
 
 export const ExchangeLayout = ({ children }: React.PropsWithChildren) => {
+  const { t } = useTranslation()
   return (
     <Swap.Page
       helpUrl="https://docs.pancakeswap.finance/get-started-aptos"
-      isBSC={false}
+      externalText={t('Bridge assets to Aptos Chain')}
+      externalLinkUrl="https://docs.pancakeswap.finance/get-started-aptos/aptos-coin-guide"
       helpImage={<Image src="/help.png" width={178} height={243} alt="Aptos help" />}
     >
       {children}
