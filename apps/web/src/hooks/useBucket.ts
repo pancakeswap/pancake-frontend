b5295bb8322179df08a68caee152c0333558ee8c
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2022-11-24 12:06:05 +0800

    chore: remove info ab testing

--- apps/web/src/hooks/useBucket.ts
+++ apps/web/src/hooks/useBucket.ts
@@ -1,9 +1,9 @@
-import { INFO_BUCKETS_COOKIES } from 'config/constants/info'
-import { atom, useAtom } from 'jotai'
-import Cookies from 'js-cookie'
+// import { INFO_BUCKETS_COOKIES } from 'config/constants/info'
+// import { atom, useAtom } from 'jotai'
+// import Cookies from 'js-cookie'
 
-const bucketInfoAtom = atom(() => Cookies.get(INFO_BUCKETS_COOKIES))
+// const bucketInfoAtom = atom(() => Cookies.get(INFO_BUCKETS_COOKIES))
 
-export function useInfoBucket() {
-  return useAtom(bucketInfoAtom)
-}
+// export function useInfoBucket() {
+//   return useAtom(bucketInfoAtom)
+// }
