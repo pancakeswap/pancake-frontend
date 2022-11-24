cf2eab6231ea3caf3696f32d3a90cd185a68e84b
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2022-11-24 12:33:18 +0800

    chore: remove useBucket hook

--- apps/web/src/hooks/useBucket.ts
+++
@@ -1,9 +0,0 @@
-// import { INFO_BUCKETS_COOKIES } from 'config/constants/info'
-// import { atom, useAtom } from 'jotai'
-// import Cookies from 'js-cookie'
-
-// const bucketInfoAtom = atom(() => Cookies.get(INFO_BUCKETS_COOKIES))
-
-// export function useInfoBucket() {
-//   return useAtom(bucketInfoAtom)
-// }
