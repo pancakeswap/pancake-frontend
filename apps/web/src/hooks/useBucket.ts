b5dfe92f9439d7dc6615e635aa24c3672499865a
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2022-11-24 12:35:54 +0800

    chore: useBucket comment

---
+++ apps/web/src/hooks/useBucket.ts
@@ -0,0 +1,11 @@
+// this hook is for ab testing follow the sample code to create the bucket hook
+//
+// import { INFO_BUCKETS_COOKIES } from 'config/constants/info'
+// import { atom, useAtom } from 'jotai'
+// import Cookies from 'js-cookie'
+
+// const bucketInfoAtom = atom(() => Cookies.get(INFO_BUCKETS_COOKIES))
+
+// export function useInfoBucket() {
+//   return useAtom(bucketInfoAtom)
+// }
