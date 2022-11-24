7a741304169fe9b8211ec87ab445e6be05a4a0d1
memoyil <2213635+memoyil@users.noreply.github.com>
2022-11-24 15:04:23 +0100

    perf: Move menu config related to storybook

--- packages/uikit/src/widgets/Menu/index.ts
+++ packages/uikit/src/widgets/Menu/index.ts
@@ -2,7 +2,7 @@ export { default as Menu } from "./Menu";
 export type { MenuItemsType } from "../../components/MenuItems/types";
 export { DropdownMenuItemType } from "../../components/DropdownMenu/types";
 export type { FooterLinkType } from "../../components/Footer/types";
-export { status as menuStatus, links as menuConfig } from "./config";
+export { links as menuConfig } from "./testConfig";
 export type { NavProps, Language } from "./types";
 
 export { default as UserMenu } from "./components/UserMenu";
