04ed3839bff25126fe426acdd1daee040f48f4bb
Ming Yan <ming@space.id>
2022-11-25 11:50:38 +0800

    feat: Add resolving method.

--- apps/web/src/hooks/useSid.ts
+++ apps/web/src/hooks/useSid.ts
@@ -2,11 +2,11 @@ import { ChainId } from '@pancakeswap/sdk'
 import SID, { getSidAddress } from '@siddomains/sidjs'
 import { useActiveChainId } from 'hooks/useActiveChainId'
 import { useCallback } from 'react'
-import { useAccount, useProvider } from 'wagmi'
+import { useProvider } from 'wagmi'
 
 /**
  * Reverse resolves an address and returns the name
- * If bnb name doesn't exist, returns address instead
+ * If bnb name doesn't exist, returns null.
  * @param address active user address
  * @param chainId chain ID
  * @param provider provider
@@ -27,15 +27,51 @@ async function getName(address, chainId, provider) {
 }
 
 /**
- * Returns a promise of get name
- * @returns
+ * Resolves an Space ID name and returns the corresponding address.
+ * If bnb name doesn't exist, returns null.
+ * @param name requesting Space ID name
+ * @param chainId chain ID
+ * @param provider provider
+ * @returns {string}
+ */
+async function getAddress(name, chainId, provider) {
+  if (![ChainId.BSC, ChainId.BSC_TESTNET].includes(chainId)) {
+    return null
+  }
+
+  try {
+    const sid = new SID({ provider, sidAddress: getSidAddress(`${chainId}`) })
+    const address = await sid.name(name).getAddress()
+    if (parseInt(address, 16) === 0) {
+      return null
+    }
+    return address
+  } catch (error) {
+    return null
+  }
+}
+
+/**
+ * Returns promises of SID resolving and reversed resolving.
+ * @returns {Promise}
  */
 export const useSid = () => {
   const { chainId } = useActiveChainId()
   const provider = useProvider({ chainId })
-  const { address } = useAccount()
 
-  return useCallback(async () => {
-    return getName(address, chainId, provider)
-  }, [address, chainId, provider])
+  const getSidName = useCallback(
+    async (address) => {
+      return getName(address, chainId, provider)
+    },
+    [chainId, provider],
+  )
+
+  const getSidAddr = useCallback(
+    async (name) => {
+      return getAddress(name, chainId, provider)
+    },
+    [chainId, provider],
+  )
+
+  return { getSidName, getSidAddr }
 }
