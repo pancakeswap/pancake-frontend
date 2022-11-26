9c2b677dcce1c33c549be86d50dca02fbc30d620
MasterGordon <gologo.x@gmail.com>
2022-11-26 20:56:50 +0100

    updated hero

--- apps/web/src/views/Home/components/Hero.tsx
+++ apps/web/src/views/Home/components/Hero.tsx
@@ -78,15 +78,6 @@ const StarsWrapper = styled.div`
   }
 `
 
-const starsImage: CompositeImageProps = {
-  path: '/images/home/lunar-bunny/',
-  attributes: [
-    { src: 'star-l', alt: '3D Star' },
-    { src: 'star-r', alt: '3D Star' },
-    { src: 'star-top-r', alt: '3D Star' },
-  ],
-}
-
 const Hero = () => {
   const { t } = useTranslation()
   const { account } = useWeb3React()
