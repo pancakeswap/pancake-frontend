64c1011f3c22e34ebecfd549bc57517769149266
chefilip <philip@pancakeswap.com>
2022-11-29 17:50:19 -0400

    chore: Add Fund to raise

--- apps/aptos/views/Ifos/components/IfoVesting/NotTokens.tsx
+++ apps/aptos/views/Ifos/components/IfoVesting/NotTokens.tsx
@@ -1,6 +1,5 @@
 import { useTranslation } from '@pancakeswap/localization'
 import { Flex, Text, BunnyPlaceholderIcon } from '@pancakeswap/uikit'
-import { MessageTextLink } from '../IfoCardStyles'
 
 const NotTokens: React.FC<React.PropsWithChildren> = () => {
   const { t } = useTranslation()
@@ -15,9 +14,6 @@ const NotTokens: React.FC<React.PropsWithChildren> = () => {
         <Text fontSize="14px" color="textSubtle" textAlign="center">
           {t('Participate in our next IFO. and remember to lock your CAKE to increase your allocation!')}
         </Text>
-        <MessageTextLink href="/ifo#ifo-how-to" color="primary" display="inline">
-          {t('How does it work?')} »
-        </MessageTextLink>
       </Flex>
     </Flex>
   )
