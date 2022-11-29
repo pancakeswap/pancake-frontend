9d3e9a46aba15e4d0ec37de59d04d718abb58e4c
chefilip <philip@pancakeswap.com>
2022-11-29 11:32:32 -0400

    chore: Add test ifo

--- apps/aptos/config/constants/ifo.ts
+++ apps/aptos/config/constants/ifo.ts
@@ -3,11 +3,35 @@ import { Ifo } from './types'
 
 export const ifos: Ifo[] = [
   {
+    id: 'usdc',
+    cIFO: false,
+    address:
+      '0xaec55cf0445a1d93fc3eb2b499227aa6199cdad3b83ba2b3907d6129cac2e19a::IFO::IFOPool<0xe0e5ad285cbcdb873b2ee15bb6bcac73d9d763bcb58395e894255eeecf3992cf::pancake::Cake, 0x8c805723ebc0a7fc5b7d3e7b75d567918e806b3461cb9fa21941a9edc0220bf::devnet_coins::DevnetUSDT, 0xb664f557c71de85be5cf91563961c5bb05345ba2b33f361533d2c184577185b7::uints::U1>',
+    isActive: true,
+    name: 'USDC',
+    poolUnlimited: {
+      saleAmount: '9 USDC',
+      raiseAmount: '$123,456',
+      cakeToBurn: '$0',
+      distributionRatio: 0.7,
+    },
+    currency: testnetTokens.cake,
+    token: testnetTokens.usdc,
+    releaseTime: 0,
+    campaignId: '1',
+    articleUrl: '',
+    tokenOfferingPrice: 0.00004939,
+    version: 3.2,
+    twitterUrl: '',
+    description: 'USDC description',
+    vestingTitle: '$USDC - USDC vesting title.',
+  },
+  {
     id: 'btc',
     cIFO: false,
     address:
       '0xaec55cf0445a1d93fc3eb2b499227aa6199cdad3b83ba2b3907d6129cac2e19a::IFO::IFOPool<0xe0e5ad285cbcdb873b2ee15bb6bcac73d9d763bcb58395e894255eeecf3992cf::pancake::Cake, 0x8c805723ebc0a7fc5b7d3e7b75d567918e806b3461cb9fa21941a9edc0220bf::devnet_coins::DevnetBTC, 0xb664f557c71de85be5cf91563961c5bb05345ba2b33f361533d2c184577185b7::uints::U1>',
-    isActive: true,
+    isActive: false,
     name: 'BTC',
     poolUnlimited: {
       saleAmount: '9 BTC',
