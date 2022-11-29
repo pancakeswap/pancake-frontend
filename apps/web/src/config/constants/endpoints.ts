a231f75ddb8095ebaf857de9e4f694c027b18c18
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2022-11-29 17:30:28 +0800

    chore: update LPs APR and update endpoint

--- apps/web/src/config/constants/endpoints.ts
+++ apps/web/src/config/constants/endpoints.ts
@@ -38,7 +38,7 @@ export const ACCESS_RISK_API = 'https://red.alert.pancakeswap.com/red-api'
 export const CELER_API = 'https://api.celerscan.com/scan'
 
 export const INFO_CLIENT_WITH_CHAIN = {
-  [ChainId.BSC]: 'https://pancakeswap.nodereal.io/graphql/',
+  [ChainId.BSC]: INFO_CLIENT,
   [ChainId.ETHEREUM]: INFO_CLIENT_ETH,
 }
 
