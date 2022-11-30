181d540d18d2d2ef84541afa322b2daf990aeeec
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2022-11-30 16:49:18 +0800

    chore: update pid

--- packages/farms/constants/56.ts
+++ packages/farms/constants/56.ts
@@ -51,7 +51,7 @@ const farms: SerializedFarmConfig[] = [
   },
   //    * V3 by order of release (some may be out of PID order due to multiplier boost)
   {
-    pid: 124,
+    pid: 131,
     lpSymbol: 'USDT-BUSD LP',
     lpAddress: '0x36842F8fb99D55477C0Da638aF5ceb6bBf86aA98',
     token: bscTokens.usdt,
@@ -60,7 +60,7 @@ const farms: SerializedFarmConfig[] = [
     infoStableSwapAddress: '0xa680d27f63Fa5E213C502d1B3Ca1EB6a3C1b31D6',
   },
   {
-    pid: 125,
+    pid: 132,
     lpSymbol: 'USDC-BUSD LP',
     lpAddress: '0x1A77C359D0019cD8F4d36b7CDf5a88043D801072',
     token: bscTokens.usdc,
@@ -69,7 +69,7 @@ const farms: SerializedFarmConfig[] = [
     infoStableSwapAddress: '0xa680d27f63Fa5E213C502d1B3Ca1EB6a3C1b31D6',
   },
   {
-    pid: 126,
+    pid: 133,
     lpSymbol: 'USDT-USDC LP',
     lpAddress: '0xee1bcc9F1692E81A281b3a302a4b67890BA4be76',
     token: bscTokens.usdt,
