3f8c6408423c02ff95cf3b96db8944dd17b9b669
chefjackson <116779127+chefjackson@users.noreply.github.com>
2022-12-01 19:51:34 +0800

    chore: update slippage warning threshold

--- apps/web/src/views/AddLiquidity/AddStableLiquidity/components/FormattedSlippage.tsx
+++ apps/web/src/views/AddLiquidity/AddStableLiquidity/components/FormattedSlippage.tsx
@@ -1,7 +1,9 @@
 import { Percent } from '@pancakeswap/sdk'
-import { warningSeverity } from 'utils/exchange'
+
+import CircleLoader from 'components/Loader/CircleLoader'
 import { ONE_BIPS } from 'config/constants/exchange'
 import { ErrorText } from 'views/Swap/components/styleds'
+import { warningSeverity } from '../utils/slippage'
 
 /**
  * Formatted version of price impact text with warning colors
@@ -9,7 +11,7 @@ import { ErrorText } from 'views/Swap/components/styleds'
 export function FormattedSlippage({ slippage }: { slippage?: Percent }) {
   return (
     <ErrorText fontSize="14px" severity={slippage ? warningSeverity(slippage) : 0}>
-      {slippage ? (slippage.lessThan(ONE_BIPS) ? '<0.01%' : `${slippage.toFixed(2)}%`) : '-'}
+      {slippage ? slippage.lessThan(ONE_BIPS) ? '<0.01%' : `${slippage.toFixed(2)}%` : <CircleLoader stroke="white" />}
     </ErrorText>
   )
 }
