dd978abf26ebcc70fc8cd66338619de2f3792a68
Philip <97418926+chefilip@users.noreply.github.com>
2022-12-02 00:08:51 -0400

    chore: Add HAY warning (#5488)

--- apps/web/src/config/constants/swapWarningTokens.ts
+++ apps/web/src/config/constants/swapWarningTokens.ts
@@ -2,7 +2,7 @@ import { Token } from '@pancakeswap/sdk'
 import { bscTokens } from '@pancakeswap/tokens'
 import { bscWarningTokens } from 'config/constants/warningTokens'
 
-const { bondly, itam, ccar, bttold } = bscTokens
+const { bondly, itam, ccar, bttold, hay, abnbc } = bscTokens
 const { pokemoney, free, safemoon, gala } = bscWarningTokens
 
 interface WarningTokenList {
@@ -18,6 +18,8 @@ const SwapWarningTokens = <WarningTokenList>{
   pokemoney,
   free,
   gala,
+  hay,
+  abnbc,
 }
 
 export default SwapWarningTokens
