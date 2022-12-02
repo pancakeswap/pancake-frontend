5093aa6889c9cb2030b7c9d735dca52eab870fce
chefjackson <116779127+chefjackson@users.noreply.github.com>
2022-12-02 14:19:37 +0800

    chore: update warning wording for abnbc

--- apps/web/src/views/Swap/components/SwapWarningModal/ABNBWarning.tsx
+++ apps/web/src/views/Swap/components/SwapWarningModal/ABNBWarning.tsx
@@ -6,7 +6,11 @@ const ABNBWarning = () => {
 
   return (
     <>
-      <Text>{t('Warning: aBNB token from Ankr has been exploited. Please proceed with caution.')}</Text>
+      <Text>
+        {t(
+          'Warning: aBNB token from Ankr has been exploited. Please proceed with caution. We do not recommend users to trade the token.',
+        )}
+      </Text>
       <LinkExternal href="https://twitter.com/ankr/status/1598503332477280256">{t('Learn More')}</LinkExternal>
     </>
   )
