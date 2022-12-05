6ec7aedfc30be905ef69c693a89c588106bfb5b3
chefmomota <momota@pancakeswap.com>
2022-12-05 10:36:10 +0800

    chore: Update SHA384

--- apps/bridge/components/layerZero/config.ts
+++ apps/bridge/components/layerZero/config.ts
@@ -1,4 +1,4 @@
-const SHA384 = 'wlXGUTlyZsP7h+I5uW6GOxYxMCyR33WRFX2L/i94u+Q0z/KPE6tBo5nI+f6Ah/xS'
+const SHA384 = 'DUn1cJmdwmm5jPyrJCRkz3y34p8FSC6V/64SI6NmmvQLlWbXynA/qCac49JISXyn'
 
 export const LAYER_ZERO_JS = {
   src: 'https://unpkg.com/@layerzerolabs/aptos-bridge-widget@latest/element.js',
