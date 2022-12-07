fbc33ed10b2a44dda3813faa7bcbb01cb65094fb
chefmomota <momota@pancakeswap.com>
2022-12-07 15:09:29 +0800

    fix: Sonarcloud

--- apis/proxy-worker/src/index.ts
+++ apis/proxy-worker/src/index.ts
@@ -1,7 +1,7 @@
 import { Router } from 'itty-router'
 import { missing } from 'itty-router-extras'
 
-const ALLOW = /[^\w](pancake\.run)|(localhost:3000)|(pancakeswap.finance)|(pancakeswap.com)$/
+const ALLOW = /[^\w](?:pancake\.run|localhost:3000|pancakeswap\.finance|pancakeswap\.com)$/
 
 function isString(s: any): s is string {
   return typeof s === 'string' || s instanceof String
