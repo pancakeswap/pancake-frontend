b56adc79d4d902636dc34340e5d0205e8cf71195
chefmomota <momota@pancakeswap.com>
2022-12-08 20:07:56 +0800

    feat: Update version6

--- apps/bridge/components/layerZero/config.ts
+++ apps/bridge/components/layerZero/config.ts
@@ -1,5 +1,5 @@
-const VERSION = '0.0.0-alpha.27-mainnet.5'
-const SHA384 = 'om6yi6kWLUPCEJfOaKP0n6un4tdhkLNnht8oI9eBprvF8prdYDRf5P/WX8xG+20n'
+const VERSION = '0.0.0-alpha.27-mainnet.6'
+const SHA384 = '8JoFUw/tYyJCXw4Xgv3YzdEqPpoXzdb5yL2NPqYk+LWaOPJuEk3lU6edz8DBZvZT'
 
 export const LAYER_ZERO_JS = {
   src: `https://unpkg.com/@layerzerolabs/aptos-bridge-widget@${VERSION}/element.js`,
