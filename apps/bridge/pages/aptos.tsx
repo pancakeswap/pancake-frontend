afa845ab9189347c587bf7ed5d71d34bae305271
chefmomota <momota@pancakeswap.com>
2022-12-12 22:44:42 +0800

    style(bridge): Fix css

--- apps/bridge/pages/aptos.tsx
+++ apps/bridge/pages/aptos.tsx
@@ -7,13 +7,13 @@ import { LayerZeroWidget } from 'components/layerZero/LayerZeroWidget'
 import AptosBridgeMessage from 'components/layerZero/AptosBridgeMessage'
 import AptosBridgeFooter from 'components/layerZero/AptosBridgeFooter'
 
-const Page = styled.div`
+const Page = styled(Box)`
   display: flex;
   height: 100%;
-  min-height: calc(100% - 56px);
+  height: calc(100vh - 56px);
   background: ${({ theme }) => theme.colors.gradientBubblegum};
 
-  ${({ theme }) => theme.mediaQueries.md} {
+  ${({ theme }) => theme.mediaQueries.sm} {
     min-height: 1000px;
   }
 `
@@ -35,18 +35,11 @@ const AptosBridge = () => {
       <Script crossOrigin="anonymous" src={LAYER_ZERO_JS.src} integrity={LAYER_ZERO_JS.integrity} />
       <link rel="stylesheet" href="https://unpkg.com/@layerzerolabs/aptos-bridge-widget@latest/element.css" />
       {show && (
-        <Box width={['100%', null, '420px']} height={['100%', '100%', 'fit-content']} m="auto">
-          <Box display={['none', null, 'block']}>
+        <Box width={['100%', null, '420px']} m="auto">
+          <Box mt="24px" display={['none', null, 'block']}>
             <AptosBridgeMessage />
           </Box>
-          <Flex
-            flexDirection="column"
-            bg="backgroundAlt"
-            borderRadius={[0, null, 24]}
-            alignItems="center"
-            m="auto"
-            height="100%"
-          >
+          <Flex flexDirection="column" bg="backgroundAlt" borderRadius={[0, null, 24]} alignItems="center">
             <Box display={['block', null, 'none']}>
               <AptosBridgeMessage />
             </Box>
