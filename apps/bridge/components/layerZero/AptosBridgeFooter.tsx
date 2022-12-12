08d368dc45cc024b18db735725c725af64bf441e
chefmomota <momota@pancakeswap.com>
2022-12-12 16:14:25 +0800

    feat: Add one more Text

--- apps/bridge/components/layerZero/AptosBridgeFooter.tsx
+++ apps/bridge/components/layerZero/AptosBridgeFooter.tsx
@@ -8,6 +8,9 @@ const AptosBridgeFooter = () => {
       <LinkExternal m="20px auto" href="https://docs.pancakeswap.finance/get-started-aptos/aptos-faq#cake-bridging">
         Need Help?
       </LinkExternal>
+      <LinkExternal m="20px auto" href="https://docs.pancakeswap.finance/get-started-aptos/aptos-coin-guide">
+        Don’t see your assets?
+      </LinkExternal>
     </>
   )
 }
