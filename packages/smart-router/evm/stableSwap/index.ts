04d3eb6c3f48a43d07bb095921dbea9bea573479
chefjackson <116779127+chefjackson@users.noreply.github.com>
2022-12-12 17:13:10 +0800

    feat: add lp output estimation with fee deducted

--- packages/smart-router/evm/stableSwap/index.ts
+++ packages/smart-router/evm/stableSwap/index.ts
@@ -1,9 +1,11 @@
 import { getLPOutputWithoutFee } from './getLPOutputWithoutFee'
+import { getLPOutput } from './getLPOutput'
 import { getD } from './amm'
 
 export * from './utils'
 
 export const StableSwap = {
   getLPOutputWithoutFee,
+  getLPOutput,
   getD,
 }
