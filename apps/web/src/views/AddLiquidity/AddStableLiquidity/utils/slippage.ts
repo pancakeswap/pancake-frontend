47d45573c7b00955b7ee230223779d2c32c80d92
chefjackson <116779127+chefjackson@users.noreply.github.com>
2022-12-13 11:11:34 +0800

    chore: lower the allowed slippage

--- apps/web/src/views/AddLiquidity/AddStableLiquidity/utils/slippage.ts
+++ apps/web/src/views/AddLiquidity/AddStableLiquidity/utils/slippage.ts
@@ -2,10 +2,10 @@ import { JSBI, Percent } from '@pancakeswap/sdk'
 
 import { BIPS_BASE } from 'config/constants/exchange'
 
-export const ALLOWED_SLIPPAGE_LOW: Percent = new Percent(JSBI.BigInt(20), BIPS_BASE) // 0.2%
-export const ALLOWED_SLIPPAGE_MEDIUM: Percent = new Percent(JSBI.BigInt(50), BIPS_BASE) // 0.5%
-export const ALLOWED_SLIPPAGE_HIGH: Percent = new Percent(JSBI.BigInt(100), BIPS_BASE) // 1%
-export const BLOCKED_SLIPPAGE_NON_EXPERT: Percent = new Percent(JSBI.BigInt(500), BIPS_BASE) // 5%
+export const ALLOWED_SLIPPAGE_LOW: Percent = new Percent(JSBI.BigInt(9), BIPS_BASE) // 0.09%
+export const ALLOWED_SLIPPAGE_MEDIUM: Percent = new Percent(JSBI.BigInt(12), BIPS_BASE) // 0.12%
+export const ALLOWED_SLIPPAGE_HIGH: Percent = new Percent(JSBI.BigInt(15), BIPS_BASE) // 0.15%
+export const BLOCKED_SLIPPAGE_NON_EXPERT: Percent = new Percent(JSBI.BigInt(20), BIPS_BASE) // 0.2%
 
 export function warningSeverity(slippage?: Percent): 0 | 1 | 2 | 3 | 4 {
   if (!slippage?.lessThan(BLOCKED_SLIPPAGE_NON_EXPERT)) return 4
