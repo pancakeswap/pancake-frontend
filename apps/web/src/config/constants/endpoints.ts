34a8f0d0b062fe5e92d3995255c4fdbf8130ba26
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-12-13 10:21:47 +0800

    chore(web): Switch to prod worker endpoint

--- apps/web/src/config/constants/endpoints.ts
+++ apps/web/src/config/constants/endpoints.ts
@@ -17,7 +17,7 @@ export const GRAPH_API_POTTERY = 'https://api.thegraph.com/subgraphs/name/pancak
  */
 export const GRAPH_API_PREDICTION_V1 = 'https://api.thegraph.com/subgraphs/name/pancakeswap/prediction'
 
-export const INFO_CLIENT = 'https://proxy-worker-dev.pancake-swap.workers.dev/bsc-exchange'
+export const INFO_CLIENT = 'https://proxy-worker.pancake-swap.workers.dev/bsc-exchange'
 
 export const INFO_CLIENT_ETH = 'https://api.thegraph.com/subgraphs/name/pancakeswap/exhange-eth'
 export const BLOCKS_CLIENT = 'https://api.thegraph.com/subgraphs/name/pancakeswap/blocks'
