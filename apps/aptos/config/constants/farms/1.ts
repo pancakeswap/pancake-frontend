b986f65f7f9399170b2e165df1df6aad3f0df9cf
chefmomota <momota@pancakeswap.com>
2022-12-14 17:10:50 +0800

    chore: Update config

--- apps/aptos/config/constants/farms/1.ts
+++ apps/aptos/config/constants/farms/1.ts
@@ -28,8 +28,8 @@ const farms: SerializedFarmConfig[] = [
     lpSymbol: 'APT-whUSDC LP',
     lpAddress:
       '0xc7efb4076dbe143cbcd98cfaaa929ecfc8f299203dfff63b95ccb6bfe19850fa::swap::LPToken<0x1::aptos_coin::AptosCoin, 0x5e156f1207d0ebfa19a9eeff00d62a282278fb8719f4fab3a586a0a2c0fffbea::coin::T>',
-    token: mainnetTokens.wheth,
-    quoteToken: mainnetTokens.whusdc,
+    token: mainnetTokens.whusdc,
+    quoteToken: mainnetTokens.apt,
   },
   {
     pid: 12,
