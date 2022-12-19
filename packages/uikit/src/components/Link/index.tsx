3c32c85fe5d9060d96a1461a03fdd49799e1f78b
Woody <109973128+Woody810@users.noreply.github.com>
2022-12-19 13:10:35 +0800

    fix

--- packages/uikit/src/components/Link/index.tsx
+++ packages/uikit/src/components/Link/index.tsx
@@ -1,3 +1,4 @@
 export { default as Link } from "./Link";
 export { default as LinkExternal } from "./LinkExternal";
+export { default as LinkBscScan } from "./LinkBscScan";
 export type { LinkProps } from "./types";
