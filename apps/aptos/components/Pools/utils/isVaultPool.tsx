fc16da60e0ad366b3c5ee5fa0f981acbf2b4542d
chefilip <philip@pancakeswap.com>
2022-12-20 07:12:12 -0400

    chore: Disable cake pool

--- apps/aptos/components/Pools/utils/isVaultPool.tsx
+++ apps/aptos/components/Pools/utils/isVaultPool.tsx
@@ -1,5 +1,7 @@
-import { CAKE_PID } from 'config/constants'
+// import { CAKE_PID } from 'config/constants'
 
-const isVaultPool = (pool) => pool?.sousId === CAKE_PID
+// const isVaultPool = (pool) => pool?.sousId === CAKE_PID
+
+const isVaultPool = () => false
 
 export default isVaultPool
