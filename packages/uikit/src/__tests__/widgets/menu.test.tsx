d34bad8b9783bd736cc29b5597c1cc21d814cbe6
chefmomota <momota@pancakeswap.com>
2022-12-21 11:27:21 +0800

    fix: Test

--- packages/uikit/src/__tests__/widgets/menu.test.tsx
+++ packages/uikit/src/__tests__/widgets/menu.test.tsx
@@ -45,6 +45,7 @@ it("renders correctly", () => {
         activeItem="Trade"
         activeSubItem="Exchange"
         buyCakeLabel="Buy CAKE"
+        buyCakeLink="https://pancakeswap.finance/swap?outputCurrency=0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82&chainId=56"
       >
         body
       </Menu>
