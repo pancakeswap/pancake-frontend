507c2cb31bcd60b8ae0551c472f9d2d90dc12bf5
memoyil <2213635+memoyil@users.noreply.github.com>
2023-01-02 14:29:19 +0100

    fix: (farms): Show cake balance in boosted proxy address

--- apps/web/src/views/Farms/components/BCakeBoosterCard.tsx
+++ apps/web/src/views/Farms/components/BCakeBoosterCard.tsx
@@ -11,8 +11,11 @@ import {
   Text,
   useTooltip,
   useMatchBreakpoints,
+  MessageText,
+  Message,
 } from '@pancakeswap/uikit'
 import useActiveWeb3React from 'hooks/useActiveWeb3React'
+import useBCakeProxyBalance from 'hooks/useBCakeProxyBalance'
 import ConnectWalletButton from 'components/ConnectWalletButton'
 import { useTranslation } from '@pancakeswap/localization'
 import Image from 'next/image'
@@ -66,6 +69,22 @@ const StyledCardFooter = styled(CardFooter)`
     background-color: ${({ theme }) => theme.colors.cardBorder};
   }
 `
+
+export const BCakeProxyCakeBalanceCard = () => {
+  const { t } = useTranslation()
+  const { bCakeProxyBalance, bCakeProxyDisplayBalance, isLoading } = useBCakeProxyBalance()
+  return !isLoading && bCakeProxyBalance > 0 ? (
+    <Message marginBottom="8px" variant="warning">
+      <MessageText>
+        {t(
+          'There is %amount% CAKE in the proxy booster contract. In order to harvest that amount you should withdraw, deposit or harvest one of the boosted farms.',
+          { amount: bCakeProxyDisplayBalance },
+        )}
+      </MessageText>
+    </Message>
+  ) : null
+}
+
 export const useBCakeTooltipContent = () => {
   const { t } = useTranslation()
   const tooltipContent = (
@@ -114,6 +133,7 @@ export const BCakeBoosterCard = () => {
           </Box>
         </StyledCardBody>
         <StyledCardFooter>
+          <BCakeProxyCakeBalanceCard />
           <CardContent />
         </StyledCardFooter>
       </Card>
