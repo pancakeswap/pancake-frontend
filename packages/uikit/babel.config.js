26e777de390f63e6ba907d835ef77fd42e2f06c8
memoyil <2213635+memoyil@users.noreply.github.com>
2023-01-03 15:37:12 +0100

    chore: Add mini-css-extract-plugin

--- packages/uikit/babel.config.js
+++ packages/uikit/babel.config.js
@@ -1,4 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
   presets: ["@babel/preset-env", ["@babel/preset-react", { runtime: "automatic" }]],
-  plugins: ["styled-components", "@vanilla-extract/babel-plugin"],
+  plugins: ["styled-components", "@vanilla-extract/babel-plugin", new MiniCssExtractPlugin()],
 };
