d9498b5eea26f6a69d7ffcf29b6b619a9a55663e
memoyil <2213635+memoyil@users.noreply.github.com>
2023-01-06 07:38:45 +0100

    chore: (uikit): Remove unused lodash imports (#5792)

--- packages/uikit/src/components/Balance/Balance.tsx
+++ packages/uikit/src/components/Balance/Balance.tsx
@@ -1,10 +1,6 @@
 import { Text, TextProps } from "@pancakeswap/uikit";
 import { useMemo } from "react";
 import CountUp from "react-countup";
-import _toNumber from "lodash/toNumber";
-import _isNaN from "lodash/isNaN";
-import _replace from "lodash/replace";
-import _toString from "lodash/toString";
 
 interface BalanceProps extends TextProps {
   value: number;
