926d6120a5bf2b344993510dd32b2c2159cdd407
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-01-10 18:15:08 +0800

    feat(farm): List SDAO cross chain farming

--- packages/tokens/src/1.ts
+++ packages/tokens/src/1.ts
@@ -1,4 +1,4 @@
-import { ChainId, WETH9 } from '@pancakeswap/sdk'
+import { ChainId, WETH9, ERC20Token } from '@pancakeswap/sdk'
 import { USDC, USDT, WBTC_ETH } from '@pancakeswap/tokens'
 
 export const ethereumTokens = {
@@ -6,4 +6,12 @@ export const ethereumTokens = {
   usdt: USDT[ChainId.ETHEREUM],
   usdc: USDC[ChainId.ETHEREUM],
   wbtc: WBTC_ETH,
+  sdao: new ERC20Token(
+    ChainId.ETHEREUM,
+    '0x993864E43Caa7F7F12953AD6fEb1d1Ca635B875F',
+    18,
+    'SDAO',
+    'Singularity Dao',
+    'https://www.singularitydao.ai/',
+  ),
 }
