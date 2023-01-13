302f89b23be5f19070cce07529438f2fa06aec6d
memoyil <2213635+memoyil@users.noreply.github.com>
2023-01-13 02:22:15 +0100

    refactor: Remove duplicate deserialize farm (#5842)

--- packages/farms/src/const.ts
+++ packages/farms/src/const.ts
@@ -4,6 +4,8 @@ export const FIXED_ZERO = FixedNumber.from(0)
 export const FIXED_ONE = FixedNumber.from(1)
 export const FIXED_TWO = FixedNumber.from(2)
 
+export const FARM_AUCTION_HOSTING_IN_SECONDS = 691200
+
 export const masterChefAddresses = {
   97: '0xB4A466911556e39210a6bB2FaECBB59E4eB7E43d',
   56: '0xa5f8C5Dbd5F286960b9d90548680aE5ebFf07652',
