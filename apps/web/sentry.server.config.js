abddaeabee30e9eacf1bca9daaa0aa7ccadb0acd
Philip <97418926+chefilip@users.noreply.github.com>
2023-01-12 21:05:17 -0400

    chore: Update Sentry ID in vercel build (#5843)
    Edit sentry files to force Vercel update the sentry id in prod

--- apps/web/sentry.server.config.js
+++ apps/web/sentry.server.config.js
@@ -1,3 +1,5 @@
+// commit caacd62252990f61c2fec902663df268a51944f6
+
 // This file configures the initialization of Sentry on the server.
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
