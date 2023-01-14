b546bd347110faff9d77e0c47ffd3f197e0d7f47
Philip <97418926+chefilip@users.noreply.github.com>
2023-01-14 00:53:01 -0400

    chore: Remove sentry release (#5858)

--- apps/web/sentry.client.config.js
+++ apps/web/sentry.client.config.js
@@ -25,7 +25,6 @@ init({
     }),
     new Dedupe(),
   ],
-  release: process.env.VERCEL_GIT_COMMIT_SHA,
   environment: ENV === 'production' ? 'production' : 'development',
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 0,
