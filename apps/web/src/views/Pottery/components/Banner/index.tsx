ad3fb66fbe1bd98bcc131f4d126b592708ae1d2e
chefmomota <momota@pancakeswap.com>
2023-01-17 09:47:39 +0800

    chore: Pottery CNY bunny image

--- apps/web/src/views/Pottery/components/Banner/index.tsx
+++ apps/web/src/views/Pottery/components/Banner/index.tsx
@@ -38,19 +38,18 @@ const Decorations = styled.div`
 `
 
 const BannerBunny = styled.div`
-  width: 316px;
-  height: 340px;
+  width: 302px;
+  height: 347.5px;
   background: url(/images/pottery/banner-bunny-cny.png);
   margin: 63px auto auto auto;
-  background-size: cover;
+  background-size: contain;
   background-repeat: no-repeat;
   background-position: center;
   z-index: 1;
 
   ${({ theme }) => theme.mediaQueries.md} {
-    width: 420px;
-    height: 549px;
-    margin-top: 0;
+    width: 402px;
+    height: 447.5px;
   }
 `
 
