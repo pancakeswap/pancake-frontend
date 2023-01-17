6525f37c60f5dc85272a744d3f6f0794bc66733a
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-01-17 20:18:33 +0800

    chore: open list by default on ETH

--- apps/web/src/hooks/useSwapHotTokenDisplay.ts
+++ apps/web/src/hooks/useSwapHotTokenDisplay.ts
@@ -1,8 +1,12 @@
+import { ChainId } from '@pancakeswap/sdk'
 import { useAtom } from 'jotai'
 import { atomWithStorage } from 'jotai/utils'
+import { useActiveChainId } from './useActiveChainId'
 
 const isSwapHotTokenDisplay = atomWithStorage<boolean>('pcs:isHotTokensDisplay', false)
+const isSwapHotTokenDisplayETH = atomWithStorage<boolean>('pcs:isHotTokensDisplayETH', true)
 
 export const useSwapHotTokenDisplay = () => {
-  return useAtom(isSwapHotTokenDisplay)
+  const { chainId } = useActiveChainId()
+  return useAtom(chainId === ChainId.BSC ? isSwapHotTokenDisplay : isSwapHotTokenDisplayETH)
 }
