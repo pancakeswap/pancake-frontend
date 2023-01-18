c6f9200bfe2048bd3f6e317e6fa85e66662424f3
chefmomota <momota@pancakeswap.com>
2023-01-18 13:38:14 +0800

    perf: CardArticle use NextLink

--- apps/blog/views/Blog/components/Article/CardArticle.tsx
+++ apps/blog/views/Blog/components/Article/CardArticle.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 import { Box, Text, Flex } from '@pancakeswap/uikit'
-import { useRouter } from 'next/router'
 import { ArticleDataType } from 'views/Blog/utils/transformArticle'
+import NextLink from 'next/link'
 
 const StyledBackgroundImage = styled(Box)<{ imgUrl: string }>`
   height: 100%;
@@ -52,51 +52,47 @@ interface CardArticleProps {
 }
 
 const CardArticle: React.FC<React.PropsWithChildren<CardArticleProps>> = ({ article }) => {
-  const router = useRouter()
-
-  const handleClick = () => {
-    router.push(`/blog/article/${article.id}`)
-  }
-
   return (
-    <StyledArticle flexDirection="column" onClick={handleClick}>
-      <Flex>
-        <Box
-          borderRadius={8}
-          mr={['8px', '15px', '20px', '58px']}
-          overflow="hidden"
-          minWidth={['132px', '152px', '192px', '320px']}
-          height={['71px', '91px', '111px', '180px']}
-        >
-          <StyledBackgroundImage imgUrl={article.imgUrl} />
-        </Box>
-        <Flex overflow="hidden" flexDirection="column" width="100%">
-          <Box mb="24px" display={['none', null, null, 'block']}>
-            <StyledTagGroup>
-              {article?.categories?.map((category: string) => (
-                <Text bold key={category} fontSize="12px" color="textSubtle" textTransform="uppercase">
-                  {category}
-                </Text>
-              ))}
-            </StyledTagGroup>
-          </Box>
-          <Text
-            ellipsis
-            bold
-            lineHeight="100%"
-            mb={['8px', '8px', '8px', '24px']}
-            fontSize={['12px', '14px', '16px', '24px']}
+    <StyledArticle flexDirection="column">
+      <NextLink passHref href={`/blog/article/${article.id}`}>
+        <Flex>
+          <Box
+            borderRadius={8}
+            mr={['8px', '15px', '20px', '58px']}
+            overflow="hidden"
+            minWidth={['132px', '152px', '192px', '320px']}
+            height={['71px', '91px', '111px', '180px']}
           >
-            {article.title}
-          </Text>
-          <Text ellipsis mb="24px">
-            {article.description}
-          </Text>
-          <Text mt="auto" textAlign="right" fontSize={['12px', '12px', '14px']} color="textSubtle">
-            {article.createAt}
-          </Text>
+            <StyledBackgroundImage imgUrl={article.imgUrl} />
+          </Box>
+          <Flex overflow="hidden" flexDirection="column" width="100%">
+            <Box mb="24px" display={['none', null, null, 'block']}>
+              <StyledTagGroup>
+                {article?.categories?.map((category: string) => (
+                  <Text bold key={category} fontSize="12px" color="textSubtle" textTransform="uppercase">
+                    {category}
+                  </Text>
+                ))}
+              </StyledTagGroup>
+            </Box>
+            <Text
+              ellipsis
+              bold
+              lineHeight="100%"
+              mb={['8px', '8px', '8px', '24px']}
+              fontSize={['12px', '14px', '16px', '24px']}
+            >
+              {article.title}
+            </Text>
+            <Text ellipsis mb="24px">
+              {article.description}
+            </Text>
+            <Text mt="auto" textAlign="right" fontSize={['12px', '12px', '14px']} color="textSubtle">
+              {article.createAt}
+            </Text>
+          </Flex>
         </Flex>
-      </Flex>
+      </NextLink>
     </StyledArticle>
   )
 }
