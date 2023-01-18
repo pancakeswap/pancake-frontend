f59989f4baab581d2dba990149ce43b4adfd6140
chefmomota <momota@pancakeswap.com>
2023-01-19 00:27:40 +0800

    fix: MenuItemStyle error

--- apps/blog/components/Menu.tsx
+++ apps/blog/components/Menu.tsx
@@ -1,11 +1,11 @@
-import { Box, Flex, Logo, ThemeSwitcher, Link, Button, LangSelector } from '@pancakeswap/uikit'
+import { Box, Flex, Logo, ThemeSwitcher, Text, Link, Button, LangSelector } from '@pancakeswap/uikit'
 import { useTheme as useNextTheme } from 'next-themes'
 import NextLink from 'next/link'
 import styled from 'styled-components'
 import { useTranslation, languageList } from '@pancakeswap/localization'
 import { useTheme } from '@pancakeswap/hooks'
 
-const MenuItemStyle = styled.a<any>`
+const MenuItemStyle = styled(Text)<{ $isActive: boolean }>`
   position: relative;
   display: flex;
   align-items: center;
