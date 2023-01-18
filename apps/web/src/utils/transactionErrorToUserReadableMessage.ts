6529543d94596d52fd4bf2a339c275a12eeec55e
chefjackson <116779127+chefjackson@users.noreply.github.com>
2023-01-18 10:51:48 +0800

    chore: combine all error messages with reasons

--- apps/web/src/utils/transactionErrorToUserReadableMessage.ts
+++ apps/web/src/utils/transactionErrorToUserReadableMessage.ts
@@ -16,10 +16,13 @@ export function transactionErrorToUserReadableMessage(error: any, t: TranslateFu
 
   if (reason?.indexOf('execution reverted: ') === 0) reason = reason.substring('execution reverted: '.length)
 
+  const formatErrorMessage = (message: string) => [message, `(${reason})`].join(' ')
   switch (reason) {
     case 'PancakeRouter: EXPIRED':
-      return t(
-        'The transaction could not be sent because the deadline has passed. Please check that your transaction deadline is not too low.',
+      return formatErrorMessage(
+        t(
+          'The transaction could not be sent because the deadline has passed. Please check that your transaction deadline is not too low.',
+        ),
       )
     case 'PancakeRouter: INSUFFICIENT_OUTPUT_AMOUNT':
     case 'PancakeRouter: EXCESSIVE_INPUT_AMOUNT':
@@ -27,16 +30,17 @@ export function transactionErrorToUserReadableMessage(error: any, t: TranslateFu
     case 'PancakeRouter: INSUFFICIENT_B_AMOUNT':
     case 'swapMulti: incorrect user balance':
     case 'Pancake: K':
-      return [
+      return formatErrorMessage(
         t(
           'This transaction will not succeed either due to price movement or fee on transfer. Try increasing your slippage tolerance.',
         ),
-        `(${reason})`,
-      ].join(' ')
+      )
     case 'TransferHelper: TRANSFER_FROM_FAILED':
-      return t('The input token cannot be transferred. There may be an issue with the input token.')
+      return formatErrorMessage(t('The input token cannot be transferred. There may be an issue with the input token.'))
     case 'Pancake: TRANSFER_FAILED':
-      return t('The output token cannot be transferred. There may be an issue with the output token.')
+      return formatErrorMessage(
+        t('The output token cannot be transferred. There may be an issue with the output token.'),
+      )
     default:
       if (reason?.indexOf('undefined is not an object') !== -1) {
         console.error(error, reason)
