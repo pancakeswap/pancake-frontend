3362b46c39a850ed0a6a6812fa474a39e9a25277
memoyil <2213635+memoyil@users.noreply.github.com>
2023-01-24 01:18:14 +0100

    chore: Bump up swr (#5906)

--- apps/web/src/hooks/useSWRContract.ts
+++ apps/web/src/hooks/useSWRContract.ts
@@ -12,14 +12,15 @@ import useSWR, {
 } from 'swr'
 import { multicallv2, MulticallOptions, Call } from 'utils/multicall'
 import { MaybeContract, ContractMethodName, ContractMethodParams } from 'utils/types'
+import { BlockingData } from 'swr/_internal'
 
 declare module 'swr' {
-  interface SWRResponse<Data = any, Error = any> {
-    data: Data | undefined
+  interface SWRResponse<Data = any, Error = any, Config = any> {
+    data: BlockingData<Data, Config> extends true ? Data : Data | undefined
     error: Error | undefined
     mutate: KeyedMutator<Data>
     isValidating: boolean
-    isLoading: boolean
+    isLoading: BlockingData<Data, Config> extends true ? false : boolean
     // Add global fetchStatus to SWRResponse
     status: FetchStatus
   }
