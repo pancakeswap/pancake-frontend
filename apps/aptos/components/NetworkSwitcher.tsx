f99c51631882694bd93301001092487a2e94a439
chefmomota <momota@pancakeswap.com>
2023-01-26 14:53:43 +0800

    feat: Testnet logo opacity

--- apps/aptos/components/NetworkSwitcher.tsx
+++ apps/aptos/components/NetworkSwitcher.tsx
@@ -3,7 +3,7 @@ import { useIsMounted } from '@pancakeswap/hooks'
 import { useTranslation } from '@pancakeswap/localization'
 import { Box, Text, UserMenu, UserMenuDivider, UserMenuItem } from '@pancakeswap/uikit'
 import { ChainIdName } from 'config/ChainName'
-
+import { ChainId } from '@pancakeswap/aptos-swap-sdk'
 import { APEX_DOMAIN } from 'config'
 import { defaultChain } from 'config/chains'
 import Image from 'next/image'
@@ -59,12 +59,14 @@ export const NetworkSwitcher = () => {
       mr="8px"
       variant="default"
       avatarSrc="https://tokens.pancakeswap.finance/images/symbol/apt.png"
-      avatarClassName={aptosLogoClass}
+      avatarClassName={aptosLogoClass({
+        isProduction: isMounted && chain?.id === ChainId.MAINNET,
+      })}
       placement="bottom"
       text={
         <>
           <Box display={['none', null, null, null, null, 'block']}>
-            Aptos{isMounted && chain?.testnet && chain?.name ? ` ${chain?.name}` : ''}
+            {`Aptos${isMounted && chain?.testnet && chain?.name ? ` ${chain?.name}` : ''}`}
           </Box>
           <Box display={['block', null, null, null, null, 'none']}>APT</Box>
         </>
