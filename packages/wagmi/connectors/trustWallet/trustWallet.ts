fdb4044c518f2e5faa5955a514e695f1711dd46b
george-gkas <georgios.gkasdrogkas@binance.com>
2023-01-26 11:06:14 +0200

    style: Run Prettier

--- packages/wagmi/connectors/trustWallet/trustWallet.ts
+++ packages/wagmi/connectors/trustWallet/trustWallet.ts
@@ -1,13 +1,7 @@
 /* eslint-disable prefer-destructuring */
 /* eslint-disable consistent-return */
 /* eslint-disable class-methods-use-this */
-import {
-  Chain,
-  ConnectorNotFoundError,
-  ResourceUnavailableError,
-  RpcError,
-  UserRejectedRequestError,
-} from 'wagmi'
+import { Chain, ConnectorNotFoundError, ResourceUnavailableError, RpcError, UserRejectedRequestError } from 'wagmi'
 import { InjectedConnector } from 'wagmi/connectors/injected'
 import { Address, Ethereum, getClient } from '@wagmi/core'
 import { getAddress } from '@ethersproject/address'
@@ -30,11 +24,11 @@ export class TrustWalletConnector extends InjectedConnector {
 
   constructor({
     chains: _chains,
-    options: _options
+    options: _options,
   }: {
     chains?: Chain[]
     options?: {
-      shimDisconnect?: boolean,
+      shimDisconnect?: boolean
       shimChainChangedDisconnect?: boolean
     }
   } = {}) {
@@ -66,10 +60,7 @@ export class TrustWalletConnector extends InjectedConnector {
       // Attempt to show wallet select prompt with `wallet_requestPermissions` when
       // `shimDisconnect` is active and account is in disconnected state (flag in storage)
       let account: Address | null = null
-      if (
-        this.options?.shimDisconnect &&
-        !getClient().storage?.getItem(this.shimDisconnectKey)
-      ) {
+      if (this.options?.shimDisconnect && !getClient().storage?.getItem(this.shimDisconnectKey)) {
         account = await this.getAccount().catch(() => null)
         const isConnected = !!account
         if (isConnected)
@@ -83,8 +74,7 @@ export class TrustWalletConnector extends InjectedConnector {
             account = await this.getAccount()
           } catch (error) {
             // Only bubble up error if user rejects request
-            if (this.isUserRejectedRequestError(error))
-              throw new UserRejectedRequestError(error)
+            if (this.isUserRejectedRequestError(error)) throw new UserRejectedRequestError(error)
           }
       }
 
@@ -104,15 +94,12 @@ export class TrustWalletConnector extends InjectedConnector {
         unsupported = this.isChainUnsupported(id)
       }
 
-      if (this.options?.shimDisconnect)
-        getClient().storage?.setItem(this.shimDisconnectKey, true)
+      if (this.options?.shimDisconnect) getClient().storage?.setItem(this.shimDisconnectKey, true)
 
       return { account, chain: { id, unsupported }, provider }
     } catch (error) {
-      if (this.isUserRejectedRequestError(error))
-        throw new UserRejectedRequestError(error)
-      if ((error as RpcError).code === -32002)
-        throw new ResourceUnavailableError(error)
+      if (this.isUserRejectedRequestError(error)) throw new UserRejectedRequestError(error)
+      if ((error as RpcError).code === -32002) throw new ResourceUnavailableError(error)
       throw error
     }
   }
@@ -120,34 +107,33 @@ export class TrustWalletConnector extends InjectedConnector {
   async getProvider() {
     const isTrustWallet = (ethereum: NonNullable<Window['ethereum']>) => {
       // Identify if Trust Wallet injected provider is present.
-      const trustWallet = !!ethereum.isTrust;
+      const trustWallet = !!ethereum.isTrust
 
-      return trustWallet;
-    };
+      return trustWallet
+    }
 
-    const injectedProviderExist =
-      typeof window !== 'undefined' && typeof window.ethereum !== 'undefined';
+    const injectedProviderExist = typeof window !== 'undefined' && typeof window.ethereum !== 'undefined'
 
     // No injected providers exist.
     if (!injectedProviderExist) {
-      return;
+      return
     }
 
     // Trust Wallet was injected into window.ethereum.
     if (isTrustWallet(window.ethereum as NonNullable<Window['ethereum']>)) {
-      return window.ethereum;
+      return window.ethereum
     }
 
     // Trust Wallet provider might be replaced by another
     // injected provider, check the providers array.
     if (window.ethereum?.providers) {
-      return window.ethereum.providers.find(isTrustWallet);
+      return window.ethereum.providers.find(isTrustWallet)
     }
 
     // In some cases injected providers can replace window.ethereum
     // without updating the providers array. In those instances the Trust Wallet
     // can be installed and its provider instance can be retrieved by
     // looking at the global `trustwallet` object.
-    return window.trustwallet;
+    return window.trustwallet
   }
 }
