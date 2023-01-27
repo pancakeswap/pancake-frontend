37c920d2e4a2948a18084ee172fcdc3450d07b2f
chefmomota <momota@pancakeswap.com>
2023-01-27 11:58:59 +0800

    fix: Max button on swap page

--- apps/aptos/utils/maxAmountSpend.ts
+++ apps/aptos/utils/maxAmountSpend.ts
@@ -1,5 +1,6 @@
 import { Currency, CurrencyAmount, JSBI } from '@pancakeswap/aptos-swap-sdk'
 import { BIG_INT_ZERO, MIN_APT } from 'config/constants/exchange'
+import { APTOS_COIN } from '@pancakeswap/awgmi'
 
 /**
  * Given some token amount, return the max that can be spent of it
@@ -8,7 +9,7 @@ import { BIG_INT_ZERO, MIN_APT } from 'config/constants/exchange'
 export function maxAmountSpend(currencyAmount?: CurrencyAmount<Currency>): CurrencyAmount<Currency> | undefined {
   if (!currencyAmount) return undefined
 
-  if (currencyAmount.currency?.isNative) {
+  if (currencyAmount.currency?.isNative || currencyAmount.currency?.address === APTOS_COIN) {
     if (JSBI.greaterThan(currencyAmount.quotient, MIN_APT)) {
       return CurrencyAmount.fromRawAmount(currencyAmount.currency, JSBI.subtract(currencyAmount.quotient, MIN_APT))
     }
