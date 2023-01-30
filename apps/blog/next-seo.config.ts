9d853d3dc8e804160703b1ed083e827f2f2257b9
chefmomota <momota@pancakeswap.com>
2023-01-30 22:26:06 +0800

    chore: Update SEO

--- apps/blog/next-seo.config.ts
+++ apps/blog/next-seo.config.ts
@@ -2,18 +2,18 @@ import { DefaultSeoProps } from 'next-seo'
 
 export const SEO: DefaultSeoProps = {
   titleTemplate: '%s | PancakeSwap',
-  defaultTitle: 'PancakeSwap',
+  defaultTitle: 'Blog | PancakeSwap',
   description:
-    'The most popular AMM DEX on BSC is now on Aptos! Swap your favourite tokens instantly and provide liquidity to start earning from trading fees. Earn CAKE through yield farming, and stake them to earn more tokens, or use them to buy new tokens in initial farm offerings—all on a platform you can trust.',
+    'Cheaper and faster than Uniswap? Discover PancakeSwap, the leading DEX on BNB Smart Chain (BSC) with the best farms in DeFi and a lottery for CAKE.',
   twitter: {
     cardType: 'summary_large_image',
     handle: '@PancakeSwap',
     site: '@PancakeSwap',
   },
   openGraph: {
-    title: '🥞 PancakeSwap - The most popular DeFi exchange on BSC, now on Aptos',
+    title: '🥞 PancakeSwap - A next evolution DeFi exchange on BNB Smart Chain (BSC)',
     description:
-      'The most popular AMM on BSC is now on Aptos! Swap your favourite tokens instantly and provide liquidity to start earning from trading fees. Earn CAKE through yield farming, and stake them to earn more tokens, or use them to buy new tokens in initial farm offerings—all on a platform you can trust.',
-    images: [{ url: 'https://aptos.pancakeswap.finance/images/hero.jpeg' }],
+      'The most popular AMM on BSC! Earn CAKE through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by PancakeSwap), NFTs, and more, on a platform you can trust.',
+    images: [{ url: 'https://pancakeswap.finance/images/hero.png' }],
   },
 }
