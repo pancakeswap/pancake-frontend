10e0a63266948a593915864b469c072f193e10ee
chefmomota <momota@pancakeswap.com>
2023-01-30 21:44:13 +0800

    feat: Add RedditIcon

--- apps/blog/views/Blog/components/Article/SingleArticle/SocialIcon.tsx
+++ apps/blog/views/Blog/components/Article/SingleArticle/SocialIcon.tsx
@@ -1,21 +1,6 @@
 import styled from 'styled-components'
-import { Flex, TwitterIcon, TelegramIcon, Link } from '@pancakeswap/uikit'
+import { Flex, TwitterIcon, TelegramIcon, RedditIcon, Link } from '@pancakeswap/uikit'
 import { useRouter } from 'next/router'
-
-const StyledSocialIcon = styled(Flex)`
-  position: static;
-  top: 0px;
-  right: 0px;
-  height: 100%;
-  padding-top: 0px;
-  flex-direction: row;
-
-  ${({ theme }) => theme.mediaQueries.lg} {
-    position: sticky;
-    padding-top: 20px;
-    flex-direction: column;
-  }
-`
 
 const StyledLink = styled(Link)`
   &:hover {
@@ -25,20 +10,43 @@ const StyledLink = styled(Link)`
   }
 `
 
+const StyledSocialIcon = styled(Flex)`
+  position: static;
+  top: 0px;
+  right: 0px;
+  height: 100%;
+  padding-top: 0px;
+  flex-direction: row;
+
+  ${StyledLink} {
+    margin: 0 28px 0 0;
+  }
+
+  ${({ theme }) => theme.mediaQueries.lg} {
+    position: sticky;
+    padding-top: 20px;
+    flex-direction: column;
+
+    ${StyledLink} {
+      margin: 0 0 28px 0;
+    }
+  }
+`
+
+const BLOG_URL = 'https://blog.pancakeswap.finance'
+
 const SocialIcon = () => {
   const router = useRouter()
   return (
     <StyledSocialIcon>
-      <StyledLink
-        external
-        mb={['0', '0', '0', '0', '28px']}
-        mr={['28px', '28px', '28px', '28px', '0']}
-        href={`https://twitter.com/share?url=https://blog.pancakeswap.finance${router.asPath}`}
-      >
+      <StyledLink external href={`https://twitter.com/share?url=${BLOG_URL}${router.asPath}`}>
         <TwitterIcon width={40} />
       </StyledLink>
-      <StyledLink external href={`https://telegram.me/share/url?url=https://blog.pancakeswap.finance${router.asPath}`}>
+      <StyledLink external href={`https://telegram.me/share/url?url=${BLOG_URL}${router.asPath}`}>
         <TelegramIcon width={40} />
+      </StyledLink>
+      <StyledLink external href={`https://reddit.com/submit?url=${BLOG_URL}${router.asPath}`}>
+        <RedditIcon width={40} />
       </StyledLink>
     </StyledSocialIcon>
   )
