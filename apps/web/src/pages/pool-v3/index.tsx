b571090dea6b3c450f33b0c5185f56ee2692d82c
chefilip <philip@pancakeswap.com>
2023-01-31 16:10:16 -0500

    chore: Update v3 Liquidity UI

--- apps/web/src/pages/pool-v3/index.tsx
+++ apps/web/src/pages/pool-v3/index.tsx
@@ -1,29 +1,120 @@
+import { AddIcon, Button, CardBody, CardFooter, Text, Dots, Card, Flex, Tag } from '@pancakeswap/uikit'
+import NextLink from 'next/link'
+import styled from 'styled-components'
 import { useWeb3React } from '@pancakeswap/wagmi'
+import { AppBody, AppHeader } from 'components/App'
 import { useV3Positions } from 'hooks/v3/useV3Positions'
 import { CHAIN_IDS } from 'utils/wagmi'
 import PositionListItem from 'views/AddLiquidityV3/components/PoolListItem'
+import Page from 'views/Page'
+import { useTranslation } from '@pancakeswap/localization'
+import DoubleCurrencyLogo from 'components/Logo/DoubleLogo'
+import { Bound } from 'views/AddLiquidityV3/form/actions'
+import { formatTickPrice } from 'hooks/v3/utils/formatTickPrice'
+import { Percent } from '@pancakeswap/sdk'
+
+const Body = styled(CardBody)`
+  background-color: ${({ theme }) => theme.colors.dropdownDeep};
+`
 
 export default function PoolListPage() {
   const { account } = useWeb3React()
+  const {
+    t,
+    currentLanguage: { locale },
+  } = useTranslation()
 
   const { positions, loading: positionsLoading } = useV3Positions(account)
 
+  let bodyChildren = null
+
   if (positionsLoading) {
-    return <div>Loading</div>
+    bodyChildren = (
+      <Text color="textSubtle" textAlign="center">
+        <Dots>{t('Loading')}</Dots>
+      </Text>
+    )
+  } else if (positions?.length) {
+    bodyChildren = positions.map((p) => {
+      return (
+        <PositionListItem key={p.tokenId.toString()} positionDetails={p}>
+          {({
+            currencyBase,
+            currencyQuote,
+            removed,
+            outOfRange,
+            priceLower,
+            tickAtLimit,
+            priceUpper,
+            feeAmount,
+            positionSummaryLink,
+          }) => (
+            <Card mb="8px">
+              <NextLink href={positionSummaryLink}>
+                <Flex justifyContent="space-between" p="16px">
+                  <Flex flexDirection="column">
+                    <Flex alignItems="center" mb="4px">
+                      <DoubleCurrencyLogo currency0={currencyQuote} currency1={currencyBase} size={20} />
+                      <Text bold ml="8px">
+                        {!currencyQuote || !currencyBase ? (
+                          <Dots>{t('Loading')}</Dots>
+                        ) : (
+                          `${currencyQuote.symbol}/${currencyBase.symbol}`
+                        )}
+                      </Text>
+                      <Tag ml="8px" variant="secondary" outline>
+                        {new Percent(feeAmount, 1_000_000).toSignificant()}%
+                      </Tag>
+                    </Flex>
+                    <Text fontSize="14px" color="textSubtle">
+                      Min {formatTickPrice(priceLower, tickAtLimit, Bound.LOWER, locale)} / Max:{' '}
+                      {formatTickPrice(priceUpper, tickAtLimit, Bound.UPPER, locale)} {currencyQuote?.symbol} per{' '}
+                      {currencyBase?.symbol}
+                    </Text>
+                  </Flex>
+
+                  {removed ? (
+                    <Tag ml="8px" variant="failure" outline>
+                      Closed
+                    </Tag>
+                  ) : outOfRange ? (
+                    <Tag ml="8px" variant="textSubtle" outline>
+                      Out of range
+                    </Tag>
+                  ) : (
+                    <Tag ml="8px" variant="success" outline>
+                      In range
+                    </Tag>
+                  )}
+                </Flex>
+              </NextLink>
+            </Card>
+          )}
+        </PositionListItem>
+      )
+    })
+  } else {
+    bodyChildren = (
+      <Text color="textSubtle" textAlign="center">
+        {t('No liquidity found.')}
+      </Text>
+    )
   }
 
   return (
-    <ul>
-      {positions?.length
-        ? positions.map((p) => {
-            return (
-              <li>
-                <PositionListItem key={p.tokenId.toString()} positionDetails={p} />
-              </li>
-            )
-          })
-        : 'No position'}
-    </ul>
+    <Page>
+      <AppBody>
+        <AppHeader title={t('Your Liquidity')} subtitle={t('List of your liquidity positions')} />
+        <Body>{bodyChildren}</Body>
+        <CardFooter style={{ textAlign: 'center' }}>
+          <NextLink href="/add" passHref>
+            <Button id="join-pool-button" width="100%" startIcon={<AddIcon color="invertedContrast" />}>
+              {t('Add Liquidity')}
+            </Button>
+          </NextLink>
+        </CardFooter>
+      </AppBody>
+    </Page>
   )
 }
 
--- apps/web/src/views/AddLiquidityV3/components/PoolListItem.tsx
+++ apps/web/src/views/AddLiquidityV3/components/PoolListItem.tsx
@@ -1,41 +1,34 @@
-import { Percent } from '@pancakeswap/sdk'
 import { Position } from '@pancakeswap/v3-sdk'
-
-import { DoubleCurrencyLogo } from 'components/Logo'
 import { useToken } from 'hooks/Tokens'
 import { useMemo } from 'react'
-
-import NextLink from 'next/link'
-
 import getPriceOrderingFromPositionForUI from 'hooks/v3/utils/getPriceOrderingFromPositionForUI'
 import { unwrappedToken } from 'utils/wrappedCurrency'
-import { RowBetween } from '@pancakeswap/uikit'
 import { usePool } from 'hooks/v3/usePools'
 import { PositionDetails } from 'hooks/v3/types'
 import useIsTickAtLimit from 'hooks/v3/useIsTickAtLimit'
-import styled from 'styled-components'
-import { formatTickPrice } from 'hooks/v3/utils/formatTickPrice'
-import { useTranslation } from '@pancakeswap/localization'
-import { Bound } from '../form/actions'
+import { Currency, Price, Token } from '@pancakeswap/sdk'
 
-const PrimaryPositionIdData = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  > * {
-    margin-right: 8px;
+interface PositionListItemDisplayProps {
+  positionSummaryLink: string
+  currencyBase: Currency
+  currencyQuote: Currency
+  removed: boolean
+  outOfRange: boolean
+  priceUpper: Price<Token, Token>
+  tickAtLimit: {
+    LOWER: boolean
+    UPPER: boolean
   }
-`
+  priceLower: Price<Token, Token>
+  feeAmount: number
+}
 
 interface PositionListItemProps {
   positionDetails: PositionDetails
+  children: (displayProps: PositionListItemDisplayProps) => JSX.Element
 }
 
-export default function PositionListItem({ positionDetails }: PositionListItemProps) {
-  const {
-    currentLanguage: { locale },
-  } = useTranslation()
-
+export default function PositionListItem({ positionDetails, children }: PositionListItemProps) {
   const {
     token0: token0Address,
     token1: token1Address,
@@ -76,31 +69,15 @@ export default function PositionListItem({ positionDetails }: PositionListItemPr
 
   const removed = liquidity?.eq(0)
 
-  return (
-    <NextLink href={positionSummaryLink}>
-      <RowBetween>
-        <PrimaryPositionIdData>
-          <DoubleCurrencyLogo currency0={currencyBase} currency1={currencyQuote} size={18} margin />
-          <div>
-            &nbsp;{currencyQuote?.symbol}&nbsp;/&nbsp;{currencyBase?.symbol}
-          </div>
-          &nbsp;
-          {new Percent(feeAmount, 1_000_000).toSignificant()}%
-        </PrimaryPositionIdData>
-        {removed ? 'Removed' : outOfRange ? 'Out of range' : 'In range'}
-      </RowBetween>
-      {priceLower && priceUpper && (
-        <>
-          <div>
-            Min: {formatTickPrice(priceLower, tickAtLimit, Bound.LOWER, locale)} {currencyQuote?.symbol} per
-            {currencyBase?.symbol ?? ''}
-          </div>
-          <div>
-            Max: {formatTickPrice(priceUpper, tickAtLimit, Bound.UPPER, locale)} {currencyQuote?.symbol} per{' '}
-            {currencyBase?.symbol}
-          </div>
-        </>
-      )}
-    </NextLink>
-  )
+  return children({
+    positionSummaryLink,
+    currencyBase,
+    currencyQuote,
+    removed,
+    outOfRange,
+    priceUpper,
+    tickAtLimit,
+    priceLower,
+    feeAmount,
+  })
 }
