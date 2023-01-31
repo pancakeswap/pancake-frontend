646b52d5491a91e967500132632ed4e17429608a
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2023-01-31 18:01:09 +0800

    add missing chains on start

--- apps/aptos/state/user/expertMode.ts
+++ apps/aptos/state/user/expertMode.ts
@@ -2,7 +2,7 @@ import { useAtom, useAtomValue } from 'jotai'
 import { atomWithStorage } from 'jotai/utils'
 
 const userExpertModeAtom = atomWithStorage<boolean>('pcs:expert-mode', false)
-const userExpertModeAcknowledgementAtom = atomWithStorage<boolean>('pcs:expert-mode-acknowledgement', false)
+const userExpertModeAcknowledgementAtom = atomWithStorage<boolean>('pcs:expert-mode-acknowledgement', true)
 
 export function useExpertMode() {
   return useAtom(userExpertModeAtom)
