54782cec9b9b65392ae2b8c4594de08b32cd4e9d
chefilip <philip@pancakeswap.com>
2023-02-02 10:07:52 -0500

    chore: Test IFO testnet

--- apps/aptos/config/constants/ifo.ts
+++ apps/aptos/config/constants/ifo.ts
@@ -1,30 +1,30 @@
 import { getIFOUID, IFO_POOL_STORE_TAG } from 'views/Ifos/constants'
-import { mainnetTokens } from './tokens/1'
+import { testnetTokens } from './tokens/2'
 import { Ifo } from './types'
 
 export const ifos: Ifo[] = [
   {
-    id: 'lzUSDT',
+    id: 'USDC',
     cIFO: false,
-    address: `${IFO_POOL_STORE_TAG}<${mainnetTokens.cake.address}, ${mainnetTokens.lzusdt.address}, ${getIFOUID(1)}>`,
+    address: `${IFO_POOL_STORE_TAG}<${testnetTokens.cake.address}, ${testnetTokens.usdc.address}, ${getIFOUID(1)}>`,
     isActive: true,
-    name: 'lzUSDT',
+    name: 'USDC',
     poolUnlimited: {
-      saleAmount: '10 lzUSDT',
+      saleAmount: '10 USDC',
       raiseAmount: '$10',
       cakeToBurn: '$0',
       distributionRatio: 1,
     },
     campaignId: '1',
-    currency: mainnetTokens.cake,
-    token: mainnetTokens.lzusdt,
+    currency: testnetTokens.cake,
+    token: testnetTokens.usdc,
     releaseTime: 1674921023,
     articleUrl:
       'https://pancakeswap.finance/voting/proposal/0x06598b682d9f33ec5ea0c2acf8eba13dea7c63fa08dd2c4dfd7bc7af16920d51',
     tokenOfferingPrice: 1,
     version: 3.2,
     twitterUrl: '',
-    description: 'lzUSDT description',
-    vestingTitle: '$lzUSDT - lzUSDT vesting title.',
+    description: 'USDC description',
+    vestingTitle: '$USDC - USDC vesting title.',
   },
 ]
