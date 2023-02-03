fb15066f4284cc125de71fddfe396d2e93e02684
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2023-02-03 10:32:31 +0800

    add articles path

--- apps/blog/components/Blog/Article/SingleArticle/SimilarArticles.tsx
+++ apps/blog/components/Blog/Article/SingleArticle/SimilarArticles.tsx
@@ -46,7 +46,7 @@ const SimilarArticles = () => {
             >
               {similarArticles?.map((article) => (
                 <SwiperSlide key={article.id}>
-                  <NextLink passHref href={`/${article.id}`}>
+                  <NextLink passHref href={`/articles/${article.id}`}>
                     <BlogCard
                       margin="auto"
                       padding={['0', '0', '18.5px']}
