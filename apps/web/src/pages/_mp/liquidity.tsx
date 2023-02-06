d3494ce9fab9011f6e00897f49f8256a9d698c8b
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2023-02-06 17:00:49 +0800

    test

--- apps/web/src/pages/_mp/liquidity.tsx
+++ apps/web/src/pages/_mp/liquidity.tsx
@@ -1,7 +1,16 @@
 import { MpPageLayout } from 'components/MpPageLayout'
+import { useRouter } from 'next/router'
 import Liquidity from 'views/Pool'
 
-const MpLiquidityPage = () => <Liquidity />
+const MpLiquidityPage = () => {
+  const { query } = useRouter()
+  return (
+    <>
+      {JSON.stringify(query, null, 2)}
+      <Liquidity />
+    </>
+  )
+}
 
 MpLiquidityPage.Layout = MpPageLayout
 MpLiquidityPage.mp = true
