b31fa51e44a9ed4eb9cc278bdf729b55005a06fe
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2023-02-06 16:39:07 +0800

    liquidity layout

--- apps/web/src/pages/_mp/liquidity.tsx
+++ apps/web/src/pages/_mp/liquidity.tsx
@@ -1,7 +1,9 @@
+import { MpPageLayout } from 'components/MpPageLayout'
 import Liquidity from 'views/Pool'
 
 const MpLiquidityPage = () => <Liquidity />
 
+MpLiquidityPage.Layout = MpPageLayout
 MpLiquidityPage.mp = true
 
 export default MpLiquidityPage
