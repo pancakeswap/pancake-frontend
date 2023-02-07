0f16c02a10d271618f1d4b01a6040bf4f2ee6e5c
chefilip <philip@pancakeswap.com>
2023-02-07 00:55:08 -0500

    chore: Update router address

--- apps/web/src/config/constants/exchange.ts
+++ apps/web/src/config/constants/exchange.ts
@@ -3,11 +3,12 @@ import { BigNumber } from '@ethersproject/bignumber'
 import { bscTokens, bscTestnetTokens, USDC, USDT, BUSD, WBTC_ETH } from '@pancakeswap/tokens'
 import { ChainMap, ChainTokenList } from './types'
 
+// Philip TODO: Change Router address
 export const ROUTER_ADDRESS: ChainMap<string> = {
   [ChainId.ETHEREUM]: '0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45',
   [ChainId.GOERLI]: '0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45',
-  [ChainId.BSC]: '0x10ED43C718714eb63d5aA57B78B54704E256024E',
-  [ChainId.BSC_TESTNET]: '0xD99D1c33F9fC3444f8101754aBC46c52416550D1',
+  [ChainId.BSC]: '0x5853552f6d6b75e003D7d1A6AA02D299640B50b0',
+  [ChainId.BSC_TESTNET]: '0x5853552f6d6b75e003D7d1A6AA02D299640B50b0',
 }
 
 // used to construct intermediary pairs for trading
