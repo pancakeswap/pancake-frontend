79a760e618c946a7113c0b90100fc38ddfe28f72
memoyil <2213635+memoyil@users.noreply.github.com>
2023-02-07 12:43:01 +0100

    refactor: Remove duplicate get token modal

--- packages/uikit/src/widgets/Ifo/index.ts
+++ packages/uikit/src/widgets/Ifo/index.ts
@@ -3,4 +3,5 @@ export { default as IfoPercentageOfTotal } from "./IfoPercentageOfTotal";
 export { default as IfoProgressStepper } from "./IfoProgressStepper";
 export { default as IfoVestingAvailableToClaim } from "./IfoVestingAvailableToClaim";
 export { default as IfoVestingFooter } from "./IfoVestingFooter";
+export { default as IfoGetTokenModal } from "./IfoGetTokenModal";
 export * from "./IfoSkeletons";
