86928692524a8603dfcf123bbe0b53b4f800525f
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2023-02-08 17:15:59 +0800

    lint

--- apis/og-image/components/nft.tsx
+++ apis/og-image/components/nft.tsx
@@ -1,7 +1,9 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @next/next/no-img-element */
 import { getTemplatePath } from 'config'
 
-export function NFTOgImage({ title, image, price }: { title: string; price: string; image: string }) {
+export function NFTOgImage(props: { title: string; price: string; image: string }) {
   return (
     <div
       style={{
