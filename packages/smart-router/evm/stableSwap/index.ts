26dc48f0ac37d212fe984249946ac31036400ff0
chefjackson <116779127+chefjackson@users.noreply.github.com>
2023-02-08 17:45:18 +0800

    feat: add stable & v3 off chain quote providers

--- packages/smart-router/evm/stableSwap/index.ts
+++ packages/smart-router/evm/stableSwap/index.ts
@@ -1,9 +1,11 @@
 import { getLPOutputWithoutFee } from './getLPOutputWithoutFee'
 import { getLPOutput } from './getLPOutput'
-import { getSwapOutput, getSwapOutputWithoutFee } from './getSwapOutput'
+import { getSwapOutput, getSwapOutputWithoutFee, getSwapInput, getSwapInputWithtouFee } from './getSwapOutput'
 import { getD } from './amm'
 
 export const StableSwap = {
+  getSwapInput,
+  getSwapInputWithtouFee,
   getSwapOutput,
   getSwapOutputWithoutFee,
   getLPOutputWithoutFee,
