c80ab4993a92dea560eb0b2eb81b781b17144021
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-02-08 14:52:53 +0800

    chore: fix ConfirmationPendingContent break change

--- apps/web/src/views/Swap/MMLinkPools/components/ConfirmSwapModal.tsx
+++ apps/web/src/views/Swap/MMLinkPools/components/ConfirmSwapModal.tsx
@@ -1,11 +1,16 @@
 import { useTranslation } from '@pancakeswap/localization'
 import { Currency, CurrencyAmount, TradeType } from '@pancakeswap/sdk'
-import { InjectedModalProps, LinkExternal, Text, TransactionErrorContent } from '@pancakeswap/uikit'
+import {
+  ConfirmationPendingContent,
+  InjectedModalProps,
+  LinkExternal,
+  Text,
+  TransactionErrorContent,
+} from '@pancakeswap/uikit'
 import { TransactionSubmittedContent } from 'components/TransactionConfirmationModal'
 import { useActiveChainId } from 'hooks/useActiveChainId'
-import { memo, useCallback } from 'react'
+import { memo, useCallback, useMemo } from 'react'
 import { Field } from 'state/swap/actions'
-import ConfirmationPendingContent from 'views/Swap/components/ConfirmationPendingContent'
 import ConfirmSwapModalContainer from '../../components/ConfirmSwapModalContainer'
 import { TradeWithMM } from '../types'
 import TransactionConfirmSwapContentWithSmartRouter from './TransactionConfirmSwapContent'
@@ -85,7 +90,7 @@ const ConfirmSwapModal: React.FC<React.PropsWithChildren<InjectedModalProps & Co
   isRFQLoading,
 }) => {
   const { chainId } = useActiveChainId()
-
+  const { t } = useTranslation()
   const handleDismiss = useCallback(() => {
     if (customOnDismiss) {
       customOnDismiss()
@@ -130,12 +135,22 @@ const ConfirmSwapModal: React.FC<React.PropsWithChildren<InjectedModalProps & Co
     ],
   )
 
+  // text to show while loading
+  const pendingText = useMemo(() => {
+    return t('Swapping %amountA% %symbolA% for %amountB% %symbolB%', {
+      amountA: trade.inputAmount?.toSignificant(6) ?? '',
+      symbolA: trade.inputAmount?.currency?.symbol ?? '',
+      amountB: trade.outputAmount?.toSignificant(6) ?? '',
+      symbolB: trade.outputAmount?.currency?.symbol ?? '',
+    })
+  }, [t, trade])
+
   if (!chainId) return null
 
   return (
     <ConfirmSwapModalContainer handleDismiss={handleDismiss}>
       {attemptingTxn ? (
-        <ConfirmationPendingContent inputAmount={trade.inputAmount} outputAmount={trade.outputAmount} />
+        <ConfirmationPendingContent pendingText={pendingText} />
       ) : txHash ? (
         <TransactionSubmittedContent
           chainId={chainId}
