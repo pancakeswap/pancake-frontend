36620cb8b6ee2e995e8223f7931339af30b09b20
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-02-11 00:03:19 +0800

    chore: add feature flag for deal display

--- apps/web/src/views/Swap/MMLinkPools/components/MMAndAMMDealDisplay.tsx
+++ apps/web/src/views/Swap/MMLinkPools/components/MMAndAMMDealDisplay.tsx
@@ -1,5 +1,6 @@
 import { Currency, Trade, TradeType } from '@pancakeswap/sdk'
 import { Box, Text } from '@pancakeswap/uikit'
+import { useRouter } from 'next/router'
 
 import { Field } from 'state/swap/actions'
 import { TradeWithMM } from '../types'
@@ -14,33 +15,36 @@ export const MMAndAMMDealDisplay: React.FC<{
 }> = ({ isMMBetter = false, independentField, v2Trade, mmTrade, mmQuoteExpiryRemainingSec }) => {
   const isExactIn = independentField === Field.INPUT
   const dealInANdOut = isExactIn ? '(out)' : '(in)'
-  return (
-    <Box pl="20px" pt="10px">
-      <Text color="textSubtle">
-        type: <Text display="inline-block">{isExactIn ? 'ExactIn' : 'ExactOut'}</Text>
-      </Text>
-      <Text color="textSubtle">
-        <Text display="inline-block" color="transparent">
-          A
+  const { query } = useRouter()
+  if (query['dev-mode'])
+    return (
+      <Box pl="20px" pt="10px">
+        <Text color="textSubtle">
+          type: <Text display="inline-block">{isExactIn ? 'ExactIn' : 'ExactOut'}</Text>
         </Text>
-        MM Deal{dealInANdOut}:{' '}
-        <Text display="inline-block">
-          {(isExactIn ? mmTrade?.outputAmount?.toSignificant(6) : mmTrade?.inputAmount?.toSignificant(6)) ?? 'null'}
+        <Text color="textSubtle">
+          <Text display="inline-block" color="transparent">
+            A
+          </Text>
+          MM Deal{dealInANdOut}:{' '}
+          <Text display="inline-block">
+            {(isExactIn ? mmTrade?.outputAmount?.toSignificant(6) : mmTrade?.inputAmount?.toSignificant(6)) ?? 'null'}
+          </Text>
         </Text>
-      </Text>
-      <Text color="textSubtle">
-        AMM Deal{dealInANdOut}:{' '}
-        <Text display="inline-block">
-          {(isExactIn ? v2Trade?.outputAmount?.toSignificant(6) : v2Trade?.inputAmount?.toSignificant(6)) ?? 'null'}
+        <Text color="textSubtle">
+          AMM Deal{dealInANdOut}:{' '}
+          <Text display="inline-block">
+            {(isExactIn ? v2Trade?.outputAmount?.toSignificant(6) : v2Trade?.inputAmount?.toSignificant(6)) ?? 'null'}
+          </Text>
         </Text>
-      </Text>
-      <Text color="textSubtle">
-        MM win: <Text display="inline-block">{`${isMMBetter}`}</Text>
-      </Text>
-      <Text color="textSubtle">
-        Quote Expiry In:{' '}
-        <Text display="inline-block">{`${mmQuoteExpiryRemainingSec > 0 ? mmQuoteExpiryRemainingSec : '?'}`} SEC</Text>
-      </Text>
-    </Box>
-  )
+        <Text color="textSubtle">
+          MM win: <Text display="inline-block">{`${isMMBetter}`}</Text>
+        </Text>
+        <Text color="textSubtle">
+          Quote Expiry In:{' '}
+          <Text display="inline-block">{`${mmQuoteExpiryRemainingSec > 0 ? mmQuoteExpiryRemainingSec : '?'}`} SEC</Text>
+        </Text>
+      </Box>
+    )
+  return null
 }
