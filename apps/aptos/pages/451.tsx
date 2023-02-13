a3a3650224bfeba7fc3957d0f7aa31b22825b341
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2023-02-13 22:27:57 +0800

    chore(aptos): Geo block (#6080)

---
+++ apps/aptos/pages/451.tsx
@@ -0,0 +1,25 @@
+import { useTranslation } from '@pancakeswap/localization'
+import { Text } from '@pancakeswap/uikit'
+import styled from 'styled-components'
+
+const StyledNotFound = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  height: calc(100vh - 64px);
+  justify-content: center;
+`
+
+const NotSupport = () => {
+  const { t } = useTranslation()
+
+  return (
+    <StyledNotFound>
+      <Text mb="16px">{t('Unavailable for legal reasons')}</Text>
+    </StyledNotFound>
+  )
+}
+
+NotSupport.pure = true
+
+export default NotSupport
