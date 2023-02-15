c1b782426d26936305cfdcf2c040123111f03530
Philip <97418926+chefilip@users.noreply.github.com>
2023-02-15 08:48:33 -0500

    fix: Unstake nonbsc undefined (#6125)
    use gasPrice for nonBSC network

--- apps/web/src/views/Farms/hooks/useUnstakeFarms.ts
+++ apps/web/src/views/Farms/hooks/useUnstakeFarms.ts
@@ -2,12 +2,12 @@ import { useCallback } from 'react'
 import useActiveWeb3React from 'hooks/useActiveWeb3React'
 import { unstakeFarm, nonBscUnstakeFarm } from 'utils/calls'
 import { useMasterchef, useNonBscVault } from 'hooks/useContract'
-import { useGasPrice } from 'state/user/hooks'
 import { useOraclePrice } from 'views/Farms/hooks/useFetchOraclePrice'
+import { useFeeDataWithGasPrice } from 'state/user/hooks'
 
 const useUnstakeFarms = (pid: number, vaultPid?: number) => {
   const { account, chainId } = useActiveWeb3React()
-  const gasPrice = useGasPrice()
+  const { gasPrice } = useFeeDataWithGasPrice()
   const oraclePrice = useOraclePrice(chainId)
   const masterChefContract = useMasterchef()
   const nonBscVaultContract = useNonBscVault()
