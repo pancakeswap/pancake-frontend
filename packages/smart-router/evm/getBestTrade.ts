5ae39442a3b62bb59ca43227150354aaa26c05e3
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2023-02-15 11:08:34 +0800

    fix

--- packages/smart-router/evm/getBestTrade.ts
+++ packages/smart-router/evm/getBestTrade.ts
@@ -5,11 +5,16 @@ import { getBestTradeWithStableSwap } from './getBestTradeWithStableSwap'
 import { stableSwapPairsByChainId } from './getStableSwapPairs'
 import { getStableSwapFee, getStableSwapOutputAmount } from './onchain'
 import { createTradeWithStableSwap, createTradeWithStableSwapFromV2Trade, getFeePercent } from './stableSwap'
-import { BestTradeOptions, RouteType, TradeWithStableSwap } from './types'
+import { BestTradeOptions, RouteType, StableSwapPair, TradeWithStableSwap } from './types'
 
 export const getBestTradeExactIn = createGetBestTrade(TradeType.EXACT_INPUT)
 
 export const getBestTradeExactOut = createGetBestTrade(TradeType.EXACT_OUTPUT)
+
+const isDirectStableSwapTrade = (currencyIn: Currency, currencyOut: Currency, stableSwapPairs: StableSwapPair[]) => {
+  const directStablePair = stableSwapPairs.find((p) => p.involvesToken(currencyIn) && p.involvesToken(currencyOut))
+  return directStablePair
+}
 
 function createGetBestTrade<TTradeType extends TradeType>(tradeType: TTradeType) {
   const getBestTradeFromV2 =
@@ -29,39 +34,52 @@ function createGetBestTrade<TTradeType extends TradeType>(tradeType: TTradeType)
 
     const stableSwapPairs = stableSwapPairsByChainId[chainId] || []
 
+    const directStablePair = isDirectStableSwapTrade(amountIn.currency, output, stableSwapPairs)
+
+    let stableSwapTrade
+
+    if (directStablePair) {
+      const [outputAmount, fees] = await Promise.all([
+        getStableSwapOutputAmount(directStablePair, amountIn, { provider }),
+        getStableSwapFee(directStablePair, amountIn, { provider }),
+      ])
+      const { fee, adminFee } = getFeePercent(amountIn, outputAmount, fees)
+
+      const pairs = [
+        {
+          ...directStablePair,
+          price: new Price({ baseAmount: amountIn, quoteAmount: outputAmount.add(fees.fee) }),
+          fee,
+          adminFee,
+        },
+      ]
+
+      stableSwapTrade = createTradeWithStableSwap({
+        routeType: RouteType.STABLE_SWAP,
+        inputAmount: amountIn,
+        outputAmount,
+        pairs,
+        tradeType: TradeType.EXACT_INPUT,
+      })
+    }
+
     if (!bestTradeV2) {
-      const directStablePair = stableSwapPairs.find(
-        (p) => p.involvesToken(amountIn.currency) && p.involvesToken(output),
-      )
-      if (directStablePair) {
-        const [outputAmount, fees] = await Promise.all([
-          getStableSwapOutputAmount(directStablePair, amountIn, { provider }),
-          getStableSwapFee(directStablePair, amountIn, { provider }),
-        ])
-        const { fee, adminFee } = getFeePercent(amountIn, outputAmount, fees)
-
-        const pairs = [
-          {
-            ...directStablePair,
-            price: new Price({ baseAmount: amountIn, quoteAmount: outputAmount.add(fees.fee) }),
-            fee,
-            adminFee,
-          },
-        ]
-
-        return createTradeWithStableSwap({
-          routeType: RouteType.STABLE_SWAP,
-          inputAmount: amountIn,
-          outputAmount,
-          pairs,
-          tradeType: TradeType.EXACT_INPUT,
-        })
+      if (stableSwapTrade) {
+        return stableSwapTrade
       }
       return null
     }
 
     const bestTradeWithStableSwap = await getBestTradeWithStableSwap(bestTradeV2, stableSwapPairs, { provider })
     const { outputAmount: outputAmountWithStableSwap } = bestTradeWithStableSwap
+
+    if (
+      stableSwapTrade &&
+      stableSwapTrade.outputAmount.greaterThan(outputAmountWithStableSwap) &&
+      stableSwapTrade.outputAmount.greaterThan(bestTradeV2.outputAmount)
+    ) {
+      return stableSwapTrade
+    }
 
     // If stable swap is not as good as best trade got from v2, then use v2
     if (outputAmountWithStableSwap.lessThan(bestTradeV2.outputAmount)) {
