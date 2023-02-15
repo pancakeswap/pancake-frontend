b8203cdbb1ec29f92fa6cb3c3b2addb58f81640a
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2023-02-15 11:32:49 +0800

    remove isSamePath

--- apps/web/src/views/Swap/SmartSwap/hooks/useIsSmartRouterBetter.tsx
+++ apps/web/src/views/Swap/SmartSwap/hooks/useIsSmartRouterBetter.tsx
@@ -16,24 +16,10 @@ export const useIsSmartRouterBetter = ({ trade, v2Trade }: Options) => {
     !trade.inputAmount.currency.equals(v2Trade.inputAmount.currency) ||
     !trade.outputAmount.currency.equals(v2Trade.outputAmount.currency) ||
     // Trade is cached so when changing the input, trade might be outdated
-    (trade.tradeType === v2Trade.tradeType && !trade.inputAmount.equalTo(v2Trade.inputAmount)) ||
-    // Trade should share the same path with v2 trade
-    !isSamePath(trade.route.path, v2Trade.route.path)
+    (trade.tradeType === v2Trade.tradeType && !trade.inputAmount.equalTo(v2Trade.inputAmount))
   ) {
     return false
   }
 
   return trade.route.routeType !== RouteType.V2 && trade.outputAmount.greaterThan(v2Trade.outputAmount)
-}
-
-function isSamePath(one: Currency[], another: Currency[]) {
-  if (one.length !== another.length) {
-    return false
-  }
-  for (let i = 0; i < one.length; i += 1) {
-    if (!one[i].equals(another[i])) {
-      return false
-    }
-  }
-  return true
 }
