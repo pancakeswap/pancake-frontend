7ed74192323341d50f58c0ff1d2c19f749c0a18f
ChefMomota <98292246+ChefMomota@users.noreply.github.com>
2023-02-17 18:46:35 +0800

    style: Fix footer SocialLinks (#6105)
    https://github.com/pancakeswap/pancake-frontend/pull/6102

--- packages/uikit/src/components/Footer/Components/SocialLinks.tsx
+++ packages/uikit/src/components/Footer/Components/SocialLinks.tsx
@@ -4,35 +4,40 @@ import Flex from "../../Box/Flex";
 import Dropdown from "../../Dropdown/Dropdown";
 import Link from "../../Link/Link";
 import { socials } from "../config";
+import useMatchBreakpoints from "../../../contexts/MatchBreakpoints/useMatchBreakpoints";
 
-const SocialLinks: React.FC<React.PropsWithChildren<FlexProps>> = ({ ...props }) => (
-  <Flex {...props} data-theme="dark">
-    {socials.map((social, index) => {
-      const iconProps = {
-        width: "20px",
-        color: "textSubtle",
-        style: { cursor: "pointer" },
-      };
-      const Icon = social.icon;
-      const mr = index < socials.length - 1 ? "24px" : 0;
-      if (social.items) {
+const SocialLinks: React.FC<React.PropsWithChildren<FlexProps>> = ({ ...props }) => {
+  const { isMobile } = useMatchBreakpoints();
+
+  return (
+    <Flex {...props} data-theme="dark">
+      {socials.map((social, index) => {
+        const iconProps = {
+          width: "20px",
+          color: "textSubtle",
+          style: { cursor: "pointer" },
+        };
+        const Icon = social.icon;
+        const mr = index < socials.length - 1 ? (isMobile ? "16px" : "24px") : 0;
+        if (social.items) {
+          return (
+            <Dropdown key={social.label} position="top" target={<Icon {...iconProps} mr={mr} />}>
+              {social.items.map((item) => (
+                <Link external key={item.label} href={item.href} aria-label={item.label} color="textSubtle">
+                  {item.label}
+                </Link>
+              ))}
+            </Dropdown>
+          );
+        }
         return (
-          <Dropdown key={social.label} position="top" target={<Icon {...iconProps} mr={mr} />}>
-            {social.items.map((item) => (
-              <Link external key={item.label} href={item.href} aria-label={item.label} color="textSubtle">
-                {item.label}
-              </Link>
-            ))}
-          </Dropdown>
+          <Link external key={social.label} href={social.href} aria-label={social.label} mr={mr}>
+            <Icon {...iconProps} />
+          </Link>
         );
-      }
-      return (
-        <Link external key={social.label} href={social.href} aria-label={social.label} mr={mr}>
-          <Icon {...iconProps} />
-        </Link>
-      );
-    })}
-  </Flex>
-);
+      })}
+    </Flex>
+  );
+};
 
 export default React.memo(SocialLinks, () => true);
