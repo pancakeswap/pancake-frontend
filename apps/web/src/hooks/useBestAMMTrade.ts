0984e49d5dd5ae9e28d60f24cf2c95269093a194
chefjackson <116779127+chefjackson@users.noreply.github.com>
2023-02-19 16:49:32 +0800

    perf: add debounce to deferred value

--- apps/web/src/hooks/useBestAMMTrade.ts
+++ apps/web/src/hooks/useBestAMMTrade.ts
@@ -2,8 +2,11 @@ import useSWR from 'swr'
 import { useDeferredValue, useMemo } from 'react'
 import { SmartRouter } from '@pancakeswap/smart-router/evm'
 import { CurrencyAmount, TradeType, Currency } from '@pancakeswap/sdk'
+import { useDebounce } from '@pancakeswap/hooks'
 
-import { provider } from 'utils/wagmi'
+// import { provider } from 'utils/wagmi'
+import { useGasPrice } from 'state/user/hooks'
+import { useCurrentBlock } from 'state/block/hooks'
 
 import { useCommonPools } from './useCommonPools'
 
@@ -17,32 +20,55 @@ interface Options {
 }
 
 export function useBestAMMTrade({ amount, baseCurrency, currency, tradeType, maxHops, maxSplits }: Options) {
+  const gasPrice = useGasPrice()
+  const blockNumber = useCurrentBlock()
   const { pools: candidatePools, loading, syncing } = useCommonPools(baseCurrency || amount?.currency, currency)
   const poolProvider = useMemo(() => SmartRouter.createStaticPoolProvider(candidatePools), [candidatePools])
-  const deferQuotient = useDeferredValue(amount?.quotient.toString())
-  const { data: trade, isLoading } = useSWR(
+  const deferQuotientRaw = useDeferredValue(amount?.quotient.toString())
+  const deferQuotient = useDebounce(deferQuotientRaw, 150)
+  const {
+    data: trade,
+    isLoading,
+    isValidating,
+  } = useSWR(
     amount && currency
-      ? [amount.currency.chainId, amount.currency.symbol, currency.symbol, tradeType, deferQuotient, maxHops, maxSplits]
+      ? [
+          amount.currency.chainId,
+          amount.currency.symbol,
+          currency.symbol,
+          tradeType,
+          deferQuotient,
+          maxHops,
+          maxSplits,
+          gasPrice,
+          blockNumber,
+        ]
       : null,
-    async () =>
-      SmartRouter.getBestTrade(amount, currency, tradeType, {
-        gasPriceWei: async () => {
-          const price = await provider({ chainId: amount.currency.chainId }).getGasPrice()
-          return price.toString()
-        },
+    async () => {
+      if (!deferQuotient) {
+        return null
+      }
+      const start = Date.now()
+      const res = await SmartRouter.getBestTrade(amount, currency, tradeType, {
+        // TODO fix on ethereum
+        gasPriceWei: async () => gasPrice || '5',
         maxHops,
         poolProvider,
         // quoteProvider: SmartRouter.createQuoteProvider({ onChainProvider: provider }),
         quoteProvider: SmartRouter.createOffChainQuoteProvider(),
-        blockNumber: () => provider({ chainId: amount.currency.chainId }).getBlockNumber(),
-      }),
+        // blockNumber: () => provider({ chainId: amount.currency.chainId }).getBlockNumber(),
+        blockNumber,
+      })
+      console.log('Getting best trade takes', Date.now() - start, deferQuotient)
+      return res
+    },
     {
       keepPreviousData: true,
     },
   )
   return {
     trade,
-    isLoading: isLoading || loading,
+    isLoading: isLoading || loading || isValidating,
     syncing,
   }
 }
