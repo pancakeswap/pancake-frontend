ccc87eb38ff495c7b95944d22a4c5e7fdba8b3cd
chefmomota <momota@pancakeswap.com>
2023-02-21 13:34:50 +0800

    feat: Update

--- apps/aptos/config/constants/farms/1.ts
+++ apps/aptos/config/constants/farms/1.ts
@@ -42,7 +42,7 @@ const farms: SerializedFarmConfig[] = [
     pid: 16,
     lpSymbol: 'stAPT-whUSDC LP',
     lpAddress:
-      '0xc7efb4076dbe143cbcd98cfaaa929ecfc8f299203dfff63b95ccb6bfe19850fa::swap::LPToken<0x1::aptos_coin::AptosCoin, 0x5e156f1207d0ebfa19a9eeff00d62a282278fb8719f4fab3a586a0a2c0fffbea::coin::T>',
+      '0xc7efb4076dbe143cbcd98cfaaa929ecfc8f299203dfff63b95ccb6bfe19850fa::swap::LPToken<0x84d7aeef42d38a5ffc3ccef853e1b82e4958659d16a7de736a29c55fbbeb0114::staked_aptos_coin::StakedAptosCoin, 0x5e156f1207d0ebfa19a9eeff00d62a282278fb8719f4fab3a586a0a2c0fffbea::coin::T>',
     token: mainnetTokens.whusdc,
     quoteToken: mainnetTokens.stapt,
   },
