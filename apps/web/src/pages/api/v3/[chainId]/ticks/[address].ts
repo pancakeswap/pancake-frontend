7e250f7a1974487f910f0e155d0a839b9b257c8b
chefjackson <116779127+chefjackson@users.noreply.github.com>
2023-02-23 17:32:44 +0800

    feat: get ticks from cached api endpoint

--- apps/web/src/pages/api/v3/[chainId]/ticks/[address].ts
+++ apps/web/src/pages/api/v3/[chainId]/ticks/[address].ts
@@ -1,27 +1,31 @@
 /* eslint-disable no-param-reassign */
-import { gql, request } from 'graphql-request'
+import { gql } from 'graphql-request'
 import { NextApiHandler } from 'next'
 import { z } from 'zod'
+import { ChainId } from '@pancakeswap/sdk'
+
+import { swapClientWithChain } from 'utils/graphql'
 
 const zChainId = z.enum(['56', '1', '5', '97'])
 
 const zAddress = z.string().regex(/^0x[a-fA-F0-9]{40}$/)
 
+const zBlockNumber = z
+  .string()
+  .regex(/^[0-9]+$/)
+  .optional()
+
 const zParams = z.object({
   chainId: zChainId,
   address: zAddress,
+  blockNumber: zBlockNumber,
 })
 
-const SUBGRAPH_URLS = {
-  1: 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3',
-  5: 'https://api.thegraph.com/subgraphs/name/liqwiz/uniswap-v3-goerli',
-}
-
 const cacheTime = {
-  1: 10,
-  5: 10,
-  56: 2,
-  97: 2,
+  [ChainId.ETHEREUM]: 10,
+  [ChainId.GOERLI]: 10,
+  [ChainId.BSC]: 2,
+  [ChainId.BSC_TESTNET]: 2,
 }
 
 // currently can get the total active liquidity for a pool
@@ -33,41 +37,10 @@ const handler: NextApiHandler = async (req, res) => {
     return res.status(400).json(parsed.error)
   }
 
-  const { chainId, address: address_ } = parsed.data
+  const { chainId, address: address_, blockNumber } = parsed.data
 
   const address = address_.toLowerCase()
-
-  const result = []
-  let page = 0
-
-  const maxPage = 3
-
-  // eslint-disable-next-line no-constant-condition
-  while (true) {
-    // eslint-disable-next-line no-await-in-loop
-    const response = await request(
-      SUBGRAPH_URLS[chainId],
-      gql`
-        query AllV3Ticks($poolAddress: String!, $skip: Int!) {
-          ticks(first: 1000, skip: $skip, where: { poolAddress: $poolAddress }, orderBy: tickIdx) {
-            tick: tickIdx
-            liquidityNet
-            price0
-            price1
-          }
-        }
-      `,
-      {
-        poolAddress: address,
-        skip: page * 1000,
-      },
-    )
-    result.push(response.ticks)
-    if (response.ticks.length !== 1000 || page > maxPage) {
-      break
-    }
-    page += 1
-  }
+  const ticks = await getPoolTicks(chainId, address, blockNumber?.toString())
 
   const lastUpdated = new Date().toISOString()
 
@@ -77,9 +50,77 @@ const handler: NextApiHandler = async (req, res) => {
   )
 
   return res.status(200).json({
-    data: result,
+    data: ticks,
     lastUpdated,
+    blockNumber,
   })
+}
+
+async function getPoolTicks(chainId: string, poolAddress: string, blockNumber?: string) {
+  const BATCH_PAGE = 3
+  const PAGE_SIZE = 1000
+  let result: any[] = []
+  let page = 0
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    const pageNums = Array(BATCH_PAGE)
+      .fill(page)
+      .map((p, i) => p + i)
+    // eslint-disable-next-line no-await-in-loop
+    const poolsCollections = await Promise.all(
+      pageNums.map((p) => _getPoolTicksByPage(chainId, poolAddress, p, PAGE_SIZE, blockNumber)),
+    )
+
+    let hasMore = true
+    for (const pools of poolsCollections) {
+      result = [...result, ...pools]
+      if (pools.length !== PAGE_SIZE) {
+        hasMore = false
+      }
+    }
+    if (!hasMore) {
+      break
+    }
+
+    page += BATCH_PAGE
+  }
+  return result
+}
+
+async function _getPoolTicksByPage(
+  chainId: string,
+  poolAddress: string,
+  page: number,
+  pageSize: number,
+  blockNumber?: string,
+) {
+  const client = swapClientWithChain(<ChainId>(<unknown>chainId))
+  if (!client) {
+    return []
+  }
+  const response = await client.request(
+    gql`
+        query AllV3Ticks($poolAddress: String!, $skip: Int!, $pageSize: Int!) {
+          ticks(
+            first: 1000,
+            ${blockNumber ? `block: { number: ${blockNumber} }` : ''}
+            skip: $skip,
+            where: { poolAddress: $poolAddress },
+            orderBy: tickIdx
+          ) {
+        tick: tickIdx
+        liquidityNet
+        liquidityGross
+          }
+        }
+      `,
+    {
+      poolAddress,
+      skip: page * pageSize,
+      pageSize,
+    },
+  )
+  return response.ticks
 }
 
 export default handler
