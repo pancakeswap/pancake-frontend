9bc9d4966dc5cad8fb144f55d3e1a43f3ad951b4
memoyil <2213635+memoyil@users.noreply.github.com>
2023-02-28 05:31:39 +0100

    fix: Use ledger timestamp in ifo progress steps (#6214)

--- apps/aptos/views/Ifos/hooks/vesting/useFetchVestingData.ts
+++ apps/aptos/views/Ifos/hooks/vesting/useFetchVestingData.ts
@@ -1,10 +1,12 @@
 import BigNumber from 'bignumber.js'
 import { useMemo } from 'react'
+import useLedgerTimestamp from 'hooks/useLedgerTimestamp'
 import { PoolIds } from 'config/constants/types'
 import { useFetchUserWalletIfoData } from './useFetchUserWalletIfoData'
 
 const useFetchVestingData = () => {
-  const currentTimeStamp = new Date().getTime()
+  const getNow = useLedgerTimestamp()
+  const currentTimeStamp = getNow()
   const allData = useFetchUserWalletIfoData()
 
   const filterVestingIfos = useMemo(
--- apps/web/src/views/Ifos/components/IfoFoldableCard/IfoPoolCard/IfoVestingCard/index.tsx
+++ apps/web/src/views/Ifos/components/IfoFoldableCard/IfoPoolCard/IfoVestingCard/index.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react'
+import { useCallback, useMemo } from 'react'
 import BigNumber from 'bignumber.js'
 import { Flex, Box, Text, IfoProgressStepper, IfoVestingFooter } from '@pancakeswap/uikit'
 import { useTranslation } from '@pancakeswap/localization'
@@ -48,6 +48,10 @@ const IfoVestingCard: React.FC<React.PropsWithChildren<IfoVestingCardProps>> = (
     [token, amountAlreadyClaimed],
   )
 
+  const getNow = useCallback(() => {
+    return Date.now()
+  }, [])
+
   const releaseRate = useMemo(() => {
     const rate = new BigNumber(userPool?.vestingAmountTotal).div(vestingInformation.duration)
     const rateBalance = getFullDisplayBalance(rate, token.decimals, 5)
@@ -61,6 +65,7 @@ const IfoVestingCard: React.FC<React.PropsWithChildren<IfoVestingCardProps>> = (
           vestingStartTime={publicIfoData.vestingStartTime || 0}
           cliff={vestingInformation?.cliff || 0}
           duration={vestingInformation?.duration || 0}
+          getNow={getNow}
         />
         <TotalPurchased ifo={ifo} poolId={poolId} walletIfoData={walletIfoData} />
         <ReleasedTokenInfo
@@ -90,6 +95,7 @@ const IfoVestingCard: React.FC<React.PropsWithChildren<IfoVestingCardProps>> = (
         duration={vestingInformation?.duration || 0}
         vestingStartTime={vestingStartTime}
         releaseRate={releaseRate}
+        getNow={getNow}
       />
     </Flex>
   )
