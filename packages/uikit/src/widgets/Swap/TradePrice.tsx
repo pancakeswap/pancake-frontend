ba2ee036c7573f5674d4e581b2ed9947a07fa246
Woody810 <woodywu810@gmail.com>
2023-02-28 17:57:37 +0800

    usecallback

--- packages/uikit/src/widgets/Swap/TradePrice.tsx
+++ packages/uikit/src/widgets/Swap/TradePrice.tsx
@@ -1,7 +1,7 @@
 import { Price, Currency } from "@pancakeswap/swap-sdk-core";
 import { SyncAltIcon } from "@pancakeswap/uikit";
 import { AtomBox } from "@pancakeswap/ui/components/AtomBox";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { Text } from "../../components/Text";
 
 interface TradePriceProps {
@@ -10,6 +10,10 @@ interface TradePriceProps {
 
 export function TradePrice({ price }: TradePriceProps) {
   const [showInverted, setShowInverted] = useState<boolean>(false);
+  const toggleShowInverted = useCallback(() => {
+    setShowInverted((prev) => !prev);
+  }, []);
+
   const formattedPrice = showInverted ? price?.toSignificant(6) : price?.invert()?.toSignificant(6);
 
   const show = Boolean(price?.baseCurrency && price?.quoteCurrency);
@@ -19,13 +23,7 @@ export function TradePrice({ price }: TradePriceProps) {
       {show ? (
         <>
           {`1 ${showInverted ? price?.baseCurrency?.symbol : price?.quoteCurrency?.symbol}`}
-          <AtomBox
-            mt="8px"
-            ml="4px"
-            mr="4px"
-            onClick={() => setShowInverted(!showInverted)}
-            style={{ cursor: "pointer" }}
-          >
+          <AtomBox mt="8px" ml="4px" mr="4px" onClick={toggleShowInverted} style={{ cursor: "pointer" }}>
             <SyncAltIcon color="textSubtle" />
           </AtomBox>
           {`${formattedPrice} ${showInverted ? price?.quoteCurrency?.symbol : price?.baseCurrency?.symbol}`}
