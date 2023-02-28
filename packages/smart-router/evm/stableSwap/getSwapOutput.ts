0086d0e017c616e19e9472b3cce77bf701bbd3a4
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2023-02-28 20:32:16 +0800

    fix smart router native checking

--- packages/smart-router/evm/stableSwap/getSwapOutput.ts
+++ packages/smart-router/evm/stableSwap/getSwapOutput.ts
@@ -27,12 +27,12 @@ export function getSwapOutput({
   const balances: JSBI[] = []
   for (const [index, b] of balanceAmounts.entries()) {
     balances.push(b.quotient)
-    if (b.currency.equals(amount.currency)) {
+    if (b.currency.wrapped.equals(amount.currency.wrapped)) {
       i = index
       // eslint-disable-next-line no-continue
       continue
     }
-    if (b.currency.equals(outputCurrency)) {
+    if (b.currency.wrapped.equals(outputCurrency.wrapped)) {
       j = index
       // eslint-disable-next-line no-continue
       continue
