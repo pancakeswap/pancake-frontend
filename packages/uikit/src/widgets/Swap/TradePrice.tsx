5a2dba8e9e567cb524b42d03ba6c27fe6623f102
Woody810 <woodywu810@gmail.com>
2023-02-28 16:16:02 +0800

    feat(Swap): Integrate the toggle price display button into the SyncAltIcon

--- packages/uikit/src/widgets/Swap/TradePrice.tsx
+++ packages/uikit/src/widgets/Swap/TradePrice.tsx
@@ -2,8 +2,6 @@ import { Price, Currency } from "@pancakeswap/swap-sdk-core";
 import { SyncAltIcon } from "@pancakeswap/uikit";
 import { AtomBox } from "@pancakeswap/ui/components/AtomBox";
 import { useState } from "react";
-import { balanceMaxMiniClass } from "./SwapWidget.css";
-import { AutoRenewIcon } from "../../components/Svg";
 import { Text } from "../../components/Text";
 
 interface TradePriceProps {
@@ -21,11 +19,16 @@ export function TradePrice({ price }: TradePriceProps) {
       {show ? (
         <>
           {`1 ${showInverted ? price?.baseCurrency?.symbol : price?.quoteCurrency?.symbol}`}
-          <SyncAltIcon color="textSubtle" ml="4px" mr="4px" />
-          {`${formattedPrice} ${showInverted ? price?.quoteCurrency?.symbol : price?.baseCurrency?.symbol}`}
-          <AtomBox className={balanceMaxMiniClass} onClick={() => setShowInverted(!showInverted)}>
-            <AutoRenewIcon width="14px" />
+          <AtomBox
+            mt="8px"
+            ml="4px"
+            mr="4px"
+            onClick={() => setShowInverted(!showInverted)}
+            style={{ cursor: "pointer" }}
+          >
+            <SyncAltIcon color="textSubtle" />
           </AtomBox>
+          {`${formattedPrice} ${showInverted ? price?.quoteCurrency?.symbol : price?.baseCurrency?.symbol}`}
         </>
       ) : (
         "-"
