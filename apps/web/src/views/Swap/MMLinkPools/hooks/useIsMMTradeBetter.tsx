45bd2967c304f9c8529c4cfb163a57b48e0fbe01
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-03-02 04:44:38 +0800

    feat: compare result with smart router

--- apps/web/src/views/Swap/MMLinkPools/hooks/useIsMMTradeBetter.tsx
+++ apps/web/src/views/Swap/MMLinkPools/hooks/useIsMMTradeBetter.tsx
@@ -31,12 +31,36 @@ export const useIsTradeWithMMBetter = ({
       return false
     }
     if (!v2Trade && !trade && tradeWithMM) return true // v2 and smart router has not liq but MM provide the deal
-    if (!v2Trade?.outputAmount || !v2Trade?.inputAmount) {
-      if (tradeWithMM) return true
+    if (v2Trade && !trade) {
+      // compare with v2 only
+      if (!v2Trade?.outputAmount || !v2Trade?.inputAmount) {
+        if (tradeWithMM) return true
+      }
+      return (
+        (isExactIn && tradeWithMM.outputAmount.greaterThan(v2Trade?.outputAmount ?? ZERO)) || // exactIn
+        (!isExactIn && tradeWithMM.inputAmount.lessThan(v2Trade?.inputAmount ?? ZERO)) // exactOut
+      )
     }
+    if (!v2Trade && trade) {
+      // compare with smart router only
+      if (!trade?.outputAmount || !trade?.inputAmount) {
+        if (tradeWithMM) return true
+      }
+      return (
+        (isExactIn && tradeWithMM.outputAmount.greaterThan(trade?.outputAmount ?? ZERO)) || // exactIn
+        (!isExactIn && tradeWithMM.inputAmount.lessThan(trade?.inputAmount ?? ZERO)) // exactOut
+      )
+    }
+    // compare with smart router and v2 at same time
     return (
-      (isExactIn && tradeWithMM.outputAmount.greaterThan(v2Trade?.outputAmount ?? ZERO)) || // exactIn
-      (!isExactIn && tradeWithMM.inputAmount.lessThan(v2Trade?.inputAmount ?? ZERO)) // exactOut
+      // exactIn
+      (isExactIn &&
+        tradeWithMM.outputAmount.greaterThan(trade?.outputAmount ?? ZERO) &&
+        tradeWithMM.outputAmount.greaterThan(v2Trade?.outputAmount ?? ZERO)) ||
+      // exactOut
+      (!isExactIn &&
+        tradeWithMM.inputAmount.lessThan(trade?.inputAmount ?? ZERO) &&
+        tradeWithMM.inputAmount.lessThan(v2Trade?.inputAmount ?? ZERO))
     )
   }, [trade, v2Trade, tradeWithMM, isExpertMode, independentField])
 }
