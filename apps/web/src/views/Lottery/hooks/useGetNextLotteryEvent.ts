a6a2bf8f0a133a4b1f39974125f4c188817fa41f
Ehsan Fouladi <ehsan.fouladi@gmail.com>
2023-03-02 15:49:39 +0330

    changed endtimes 60 5 5

--- apps/web/src/views/Lottery/hooks/useGetNextLotteryEvent.ts
+++ apps/web/src/views/Lottery/hooks/useGetNextLotteryEvent.ts
@@ -8,9 +8,9 @@ interface LotteryEvent {
   preCountdownText?: string
 }
 
-const vrfRequestTime = 180 // 3 mins
-const secondsBetweenRounds = 300 // 5 mins
-const transactionResolvingBuffer = 30 // Delay countdown by 30s to ensure contract transactions have been calculated and broadcast
+const vrfRequestTime = 5 // 3 mins
+const secondsBetweenRounds = 60 // 5 mins
+const transactionResolvingBuffer = 5 // Delay countdown by 30s to ensure contract transactions have been calculated and broadcast
 
 const useGetNextLotteryEvent = (endTime: number, status: LotteryStatus): LotteryEvent => {
   const { t } = useTranslation()
