62d8ccca5ba3dca6231e26ee5acfa9c67561cc44
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-03-03 16:37:50 +0800

    chore: remove campaign banner legacy code

--- apps/web/src/views/Home/components/Banners/hooks/useMultipleBannerConfig.tsx
+++ apps/web/src/views/Home/components/Banners/hooks/useMultipleBannerConfig.tsx
@@ -4,10 +4,8 @@ import AptosBanner from '../AptosBanner'
 import CompetitionBanner from '../CompetitionBanner'
 import IFOBanner from '../IFOBanner'
 import PerpetualBanner from '../PerpetualBanner'
-import TrustWalletCampaignBanner from '../TrustWalletCampaignBanner'
 import useIsRenderCompetitionBanner from './useIsRenderCompetitionBanner'
 import useIsRenderIfoBanner from './useIsRenderIFOBanner'
-import useIsRenderTrustWalletCampaignBanner from './useIsRenderTrustWalletCampaignBanner'
 
 interface IBannerConfig {
   shouldRender: boolean
@@ -29,11 +27,9 @@ interface IBannerConfig {
 export const useMultipleBannerConfig = () => {
   const isRenderIFOBanner = useIsRenderIfoBanner()
   const isRenderCompetitionBanner = useIsRenderCompetitionBanner()
-  const isRenderTrustWalletCampaignBanner = useIsRenderTrustWalletCampaignBanner()
 
   return useMemo(() => {
     const NO_SHUFFLE_BANNERS: IBannerConfig[] = [
-      { shouldRender: isRenderTrustWalletCampaignBanner, banner: <TrustWalletCampaignBanner /> },
       { shouldRender: true, banner: <AptosBanner /> },
       {
         shouldRender: isRenderIFOBanner,
@@ -54,5 +50,5 @@ export const useMultipleBannerConfig = () => {
     return [...NO_SHUFFLE_BANNERS, ...shuffle(SHUFFLE_BANNERS)]
       .filter((bannerConfig: IBannerConfig) => bannerConfig.shouldRender)
       .map((bannerConfig: IBannerConfig) => bannerConfig.banner)
-  }, [isRenderIFOBanner, isRenderCompetitionBanner, isRenderTrustWalletCampaignBanner])
+  }, [isRenderIFOBanner, isRenderCompetitionBanner])
 }
