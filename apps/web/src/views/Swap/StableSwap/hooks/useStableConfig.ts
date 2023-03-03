13f7c66e8ab7c38d33f79948e2a5d2261e6f19fb
chefjackson <116779127+chefjackson@users.noreply.github.com>
2023-03-03 15:01:03 +0800

    fix: error from merging conflicts

--- apps/web/src/views/Swap/StableSwap/hooks/useStableConfig.ts
+++ apps/web/src/views/Swap/StableSwap/hooks/useStableConfig.ts
@@ -1,18 +1,14 @@
+import { SerializedStableFarmConfig } from '@pancakeswap/farms'
+import { Contract } from '@ethersproject/contracts'
+import { Currency, CurrencyAmount, ERC20Token, Token } from '@pancakeswap/sdk'
+import { createContext, useMemo } from 'react'
+
+import { useTokenBalancesWithLoadingIndicator } from 'state/wallet/hooks'
 import stableSwapInfoABI from 'config/abi/infoStableSwap.json'
 import stableLPABI from 'config/abi/stableLP.json'
 import stableSwapABI from 'config/abi/stableSwap.json'
 import { useContract } from 'hooks/useContract'
 import { useStableSwapPairs } from 'state/swap/useStableSwapPairs'
-
-import { Currency, CurrencyAmount, ERC20Token, Token } from '@pancakeswap/sdk'
-import { useTokenBalancesWithLoadingIndicator } from 'state/wallet/hooks'
-import { createContext, useMemo } from 'react'
-import useSWRImmutable from 'swr/immutable'
-import { getStableConfig } from '@pancakeswap/farms/constants'
-import { deserializeToken } from '@pancakeswap/token-lists'
-import { useActiveChainId } from 'hooks/useActiveChainId'
-import { Contract } from '@ethersproject/contracts'
-import { SerializedStableFarmConfig } from '@pancakeswap/farms/src/types'
 
 interface StableSwapConfigType extends SerializedStableFarmConfig {
   liquidityToken: ERC20Token
@@ -21,23 +17,6 @@ interface StableSwapConfigType extends SerializedStableFarmConfig {
 }
 
 export type StableSwapConfig = Omit<StableSwapConfigType, 'token' | 'quoteToken' | 'lpAddress'>
-
-export function useStableFarms(): StableSwapConfig[] {
-  const { chainId } = useActiveChainId()
-
-  const { data: stableFarms = [] } = useSWRImmutable(chainId && ['stable-farms', chainId], async () => {
-    const farms = await getStableConfig(chainId)
-
-    return farms.map(({ token, quoteToken, lpAddress, ...rest }) => ({
-      ...rest,
-      liquidityToken: new ERC20Token(chainId, lpAddress, 18, 'Stable-LP', 'Pancake StableSwap LPs'),
-      token0: deserializeToken(token),
-      token1: deserializeToken(quoteToken),
-    }))
-  })
-
-  return stableFarms
-}
 
 function useFindStablePair({ tokenA, tokenB }: { tokenA: Currency | undefined; tokenB: Currency | undefined }) {
   const stablePairs = useStableSwapPairs()
@@ -56,23 +35,16 @@ function useFindStablePair({ tokenA, tokenB }: { tokenA: Currency | undefined; t
   )
 }
 
-export interface LPStablePair extends StableSwapConfig {
-  reserve0: CurrencyAmount<Token>
-  reserve1: CurrencyAmount<Token>
-  getLiquidityValue: () => CurrencyAmount<Token>
-}
-
 export function useLPTokensWithBalanceByAccount(account) {
   const lpTokens = useStableSwapPairs()
-  const { chainId } = useActiveChainId()
 
   const [stableBalances] = useTokenBalancesWithLoadingIndicator(
     account ?? undefined,
-    lpTokens.map(({ lpAddress }) => new ERC20Token(chainId, lpAddress, 18, 'Stable-LP', 'Pancake StableSwap LPs')),
+    lpTokens.map(({ liquidityToken }) => liquidityToken),
   )
 
   const lpTokensWithBalance = useMemo(
-    () => lpTokens.filter(({ lpAddress }) => stableBalances[lpAddress]?.greaterThan('0')),
+    () => lpTokens.filter(({ liquidityToken }) => stableBalances[liquidityToken.address]?.greaterThan('0')),
     [lpTokens, stableBalances],
   )
 
@@ -96,7 +68,7 @@ export default function useStableConfig({ tokenA, tokenB }: { tokenA: Currency; 
   const stablePair = useFindStablePair({ tokenA, tokenB })
   const stableSwapContract = useContract(stablePair?.stableSwapAddress, stableSwapABI)
   const stableSwapInfoContract = useContract(stablePair?.infoStableSwapAddress, stableSwapInfoABI)
-  const stableSwapLPContract = useContract(stablePair?.lpAddress, stableLPABI)
+  const stableSwapLPContract = useContract(stablePair?.liquidityToken.address, stableLPABI)
 
   return useMemo(
     () => ({
