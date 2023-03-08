9401a87139ad4386acae69af20691c52a9dd550f
chefilip <philip@pancakeswap.com>
2023-03-08 12:41:08 -0500

    fix(v3-liquidity): Avoid LiquidityFormStore shared between instances

--- apps/web/src/views/AddLiquidityV3/formViews/V3FormView/form/LiquidityFormProvider.tsx
+++ apps/web/src/views/AddLiquidityV3/formViews/V3FormView/form/LiquidityFormProvider.tsx
@@ -1,11 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit'
 import LocalReduxProvider from 'contexts/LocalRedux/Provider'
+import { useEffect, useState } from 'react'
 import reducer from './reducer'
 
-const formStore = configureStore({
-  reducer,
-})
-
 export default function LiquidityFormProvider({ children }) {
-  return <LocalReduxProvider store={formStore}>{children}</LocalReduxProvider>
+  const [formStore, setFormStore] = useState(null)
+
+  useEffect(() => {
+    // NOTE: Set Store when mounted to avoid leaked store to other LiquidityFormProvider instance
+    setFormStore(
+      configureStore({
+        reducer,
+      }),
+    )
+  }, [])
+
+  return formStore ? <LocalReduxProvider store={formStore}>{children}</LocalReduxProvider> : null
 }
