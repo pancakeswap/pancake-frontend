ef5d474cbdaf8dbfed61f9dca682de3647a6320d
chefjackson <116779127+chefjackson@users.noreply.github.com>
2023-03-08 21:07:28 +0800

    chore: update the stable swap address on bsc testnet

--- packages/smart-router/evm/constants/stableSwap/97.ts
+++ packages/smart-router/evm/constants/stableSwap/97.ts
@@ -1,5 +1,5 @@
 import { ChainId, ERC20Token } from '@pancakeswap/sdk'
-// import { bscTestnetTokens } from '@pancakeswap/tokens'
+import { bscTestnetTokens } from '@pancakeswap/tokens'
 
 import { StableSwapPool } from './types'
 
@@ -14,16 +14,10 @@ export const pools: StableSwapPool[] = [
   // },
   {
     lpSymbol: 'USDT-WBNB LP',
-    lpAddress: '0xd1742b5eC6798cEB8C791e0ebbEf606A4946f67E',
+    lpAddress: '0x4c091Dc4418Bd3353A87488535528FD86954da2c',
     token: new ERC20Token(ChainId.BSC_TESTNET, '0x0fB5D7c73FA349A90392f873a4FA1eCf6a3d0a96', 18, 'USDT', 'MOCK Token'), // coins[0]
-    quoteToken: new ERC20Token(
-      ChainId.BSC_TESTNET,
-      '0x931Bf638fC27499506a4C1446e5f905b0EC73C81',
-      18,
-      'WBNB',
-      'Wrapped BNB',
-    ), // coins[1]
-    stableSwapAddress: '0xD38F21E8C575F85Ef3dFeeCfb177666aA62DEcd2',
+    quoteToken: bscTestnetTokens.wbnb, // coins[1]
+    stableSwapAddress: '0xBcd585Ee8B8Ac8de6b0e45dA32Aa31703036b2a1',
     infoStableSwapAddress: '0x0A548d59D04096Bc01206D58C3D63c478e1e06dB',
     stableLpFee: 0.0004,
     stableLpFeeRateOfTotalFee: 0.5,
