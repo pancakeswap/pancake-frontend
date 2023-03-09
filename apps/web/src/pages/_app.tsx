2bc9ffe6dbe1f73f63cdabf7b56325e7af6f2223
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-03-09 14:52:05 +0800

    chore: fix the typo

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -120,7 +120,7 @@ function MyApp(props: AppProps<{ initialReduxState: any }>) {
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','custoMDataLayer','GTM-PP4GC4F');
+})(window,document,'script','customDataLayer','GTM-PP4GC4F');
           `,
         }}
       />
