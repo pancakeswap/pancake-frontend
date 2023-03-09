dcc4758f35eaddc526b952befd8adc140dc963b1
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-03-09 14:51:43 +0800

    chore: add custom dataLayer

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -111,15 +111,19 @@ function MyApp(props: AppProps<{ initialReduxState: any }>) {
           `,
         }}
       />
-      <noscript>
-        <iframe
-          title="customEvent"
-          src="https://www.googletagmanager.com/ns.html?id=GTM-PP4GC4F"
-          height="0"
-          width="0"
-          style={{ display: 'none', visibility: 'hidden' }}
-        />
-      </noscript>
+      <Script
+        strategy="afterInteractive"
+        id="google-tag-2"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','custoMDataLayer','GTM-PP4GC4F');
+          `,
+        }}
+      />
     </>
   )
 }
