ec571ac0a6176c03220787d4534619dc2bd48909
chefjackson <116779127+chefjackson@users.noreply.github.com>
2023-03-10 18:36:44 +0800

    chore: add new stable pair on bsc testnet

--- packages/smart-router/evm/constants/stableSwap/97.ts
+++ packages/smart-router/evm/constants/stableSwap/97.ts
@@ -3,6 +3,14 @@ import { bscTestnetTokens } from '@pancakeswap/tokens'
 
 import { StableSwapPool } from './types'
 
+const mockUSDT = new ERC20Token(
+  ChainId.BSC_TESTNET,
+  '0x0fB5D7c73FA349A90392f873a4FA1eCf6a3d0a96',
+  18,
+  'USDT',
+  'MOCK Token',
+)
+
 export const pools: StableSwapPool[] = [
   // {
   //   lpSymbol: 'USDC-BUSD LP',
@@ -15,9 +23,19 @@ export const pools: StableSwapPool[] = [
   {
     lpSymbol: 'USDT-WBNB LP',
     lpAddress: '0x4c091Dc4418Bd3353A87488535528FD86954da2c',
-    token: new ERC20Token(ChainId.BSC_TESTNET, '0x0fB5D7c73FA349A90392f873a4FA1eCf6a3d0a96', 18, 'USDT', 'MOCK Token'), // coins[0]
+    token: mockUSDT, // coins[0]
     quoteToken: bscTestnetTokens.wbnb, // coins[1]
     stableSwapAddress: '0xBcd585Ee8B8Ac8de6b0e45dA32Aa31703036b2a1',
+    infoStableSwapAddress: '0x0A548d59D04096Bc01206D58C3D63c478e1e06dB',
+    stableLpFee: 0.0004,
+    stableLpFeeRateOfTotalFee: 0.5,
+  },
+  {
+    lpSymbol: 'USDT-BUSD LP',
+    lpAddress: '0x746a7063101E2D3305D1556888ee08193f2B9a07',
+    token: mockUSDT, // coins[0]
+    quoteToken: bscTestnetTokens.busd, // coins[1]
+    stableSwapAddress: '0xE25A1352477f3DB9B3008B31e9b7a07a18f8A9e6',
     infoStableSwapAddress: '0x0A548d59D04096Bc01206D58C3D63c478e1e06dB',
     stableLpFee: 0.0004,
     stableLpFeeRateOfTotalFee: 0.5,
