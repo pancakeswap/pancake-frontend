672150fb857d5a2caa982951aea296ca59219480
chefjackson <116779127+chefjackson@users.noreply.github.com>
2023-03-13 12:10:19 +0800

    fix: build error

--- apps/web/src/views/Farms/components/FarmTable/Row.tsx
+++ apps/web/src/views/Farms/components/FarmTable/Row.tsx
@@ -230,7 +230,7 @@ const Row: React.FunctionComponent<React.PropsWithChildren<RowPropsWithLoading>>
                       <CellInner>
                         <CellLayout label={t(tableSchema[columnIndex].label)}>
                           {createElement(cells[key], { ...props[key], userDataReady })}
-                          {v3PromotionFarms[details.pid] && key === 'farm' && <V3SwapPromotionIcon />}
+                          {v3PromotionFarms[props.details?.pid] && key === 'farm' && <V3SwapPromotionIcon />}
                         </CellLayout>
                       </CellInner>
                     </td>
@@ -249,7 +249,7 @@ const Row: React.FunctionComponent<React.PropsWithChildren<RowPropsWithLoading>>
           <FarmMobileCell colSpan={3}>
             <Flex justifyContent="space-between" alignItems="center">
               <FarmCell {...props.farm} />
-              {v3PromotionFarms[details.pid] && <V3SwapPromotionIcon />}
+              {v3PromotionFarms[props.details?.pid] && <V3SwapPromotionIcon />}
 
               {props.type === 'community' ? (
                 <FarmAuctionTag marginRight="16px" scale="sm" />
