d956e31d3d21c1ef920718668d62ddd34dbb7acd
chefmomota <momota@pancakeswap.com>
2023-03-21 12:30:14 +0800

    feat: Import local json

--- apps/aptos/components/Farms/hooks/useLpRewardsAprs.ts
+++ apps/aptos/components/Farms/hooks/useLpRewardsAprs.ts
@@ -14,6 +14,13 @@ const useLpRewardsAprs = () => {
       const result = await response.json()
       return result
     }
+
+    // Incase API not work, return local lpAprs json.
+    const importLocalLpAprsData = (await import(`../../../config/constants/lpAprs/${chainId}.json`)).default
+    if (importLocalLpAprsData) {
+      return importLocalLpAprsData
+    }
+
     return {}
   })
 
