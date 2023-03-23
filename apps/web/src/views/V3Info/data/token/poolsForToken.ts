89c1f174fd648f51ed42840876f8a4ff00d02533
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-03-23 17:59:22 +0800

    chore: pool for token query

---
+++ apps/web/src/views/V3Info/data/token/poolsForToken.ts
@@ -0,0 +1,62 @@
+import { gql, GraphQLClient } from 'graphql-request'
+
+export const POOLS_FOR_TOKEN = gql`
+  query topPools($address: Bytes!) {
+    asToken0: pools(
+      first: 200
+      orderBy: totalValueLockedUSD
+      orderDirection: desc
+      where: { token0: $address }
+      subgraphError: allow
+    ) {
+      id
+    }
+    asToken1: pools(
+      first: 200
+      orderBy: totalValueLockedUSD
+      orderDirection: desc
+      where: { token1: $address }
+      subgraphError: allow
+    ) {
+      id
+    }
+  }
+`
+
+interface PoolsForTokenResponse {
+  asToken0: {
+    id: string
+  }[]
+  asToken1: {
+    id: string
+  }[]
+}
+
+/**
+ * Fetch top addresses by volume
+ */
+export async function fetchPoolsForToken(
+  address: string,
+  client: GraphQLClient,
+): Promise<{
+  error: boolean
+  addresses: string[] | undefined
+}> {
+  try {
+    const data = await client.request<PoolsForTokenResponse>(POOLS_FOR_TOKEN, {
+      address,
+    })
+
+    const formattedData = data.asToken0.concat(data.asToken1).map((p) => p.id)
+
+    return {
+      error: false,
+      addresses: formattedData,
+    }
+  } catch {
+    return {
+      error: true,
+      addresses: undefined,
+    }
+  }
+}
