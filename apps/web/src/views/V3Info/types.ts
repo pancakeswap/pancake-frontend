84da348bb2de5faa4cce52ffebe60cfea706d344
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-03-24 10:36:26 +0800

    chore: add types for pool data

--- apps/web/src/views/V3Info/types.ts
+++ apps/web/src/views/V3Info/types.ts
@@ -71,3 +71,54 @@ export interface TokenChartEntry {
   volumeUSD: number
   totalValueLockedUSD: number
 }
+
+export type PoolChartEntry = {
+  date: number
+  volumeUSD: number
+  totalValueLockedUSD: number
+  feesUSD: number
+}
+
+export interface PoolData {
+  // basic token info
+  address: string
+  feeTier: number
+
+  token0: {
+    name: string
+    symbol: string
+    address: string
+    decimals: number
+    derivedETH: number
+  }
+
+  token1: {
+    name: string
+    symbol: string
+    address: string
+    decimals: number
+    derivedETH: number
+  }
+
+  // for tick math
+  liquidity: number
+  sqrtPrice: number
+  tick: number
+
+  // volume
+  volumeUSD: number
+  volumeUSDChange: number
+  volumeUSDWeek: number
+
+  // liquidity
+  tvlUSD: number
+  tvlUSDChange: number
+
+  // prices
+  token0Price: number
+  token1Price: number
+
+  // token amounts
+  tvlToken0: number
+  tvlToken1: number
+}
