06cd2cb9e35ae98c826929513606a5773351af6f
chefmomota <momota@pancakeswap.com>
2023-03-24 16:27:24 +0800

    feat: Done Banner

--- apps/web/src/views/TradingReward/components/Banner.tsx
+++ apps/web/src/views/TradingReward/components/Banner.tsx
@@ -1,12 +1,13 @@
 import { Box, Flex, Text, Button } from '@pancakeswap/uikit'
 import styled from 'styled-components'
+import { useTranslation } from '@pancakeswap/localization'
 import { floatingStarsLeft, floatingStarsRight } from 'views/Lottery/components/Hero'
 import Image from 'next/image'
 import bunnyImage from '../../../../public/images/trading-reward/trading-reward-banner-bunny.png'
 
 const Container = styled(Box)`
   padding: 47px 16px 38px 16px;
-  background: linear-gradient(340.33deg, #c1edf0 -11.09%, #eafbf7 32.51%, #ece4fb 96.59%);
+  background: linear-gradient(0deg, rgba(193, 237, 240, 1) 0%, rgba(234, 251, 247, 1) 20%, rgba(236, 228, 251, 1) 100%);
 `
 
 const Decorations = styled(Box)`
@@ -21,74 +22,66 @@ const Decorations = styled(Box)`
     position: absolute;
   }
   & :nth-child(1) {
-    left: 2%;
-    top: 2%;
-    width: 69px;
-    animation: ${floatingStarsLeft} 3s ease-in-out infinite;
+    top: 10%;
+    left: 55%;
+    animation: ${floatingStarsRight} 2.5s ease-in-out infinite;
     ${({ theme }) => theme.mediaQueries.xl} {
-      left: 15%;
-      top: 8%;
-      width: auto;
     }
   }
   & :nth-child(2) {
-    left: 0%;
-    bottom: 55%;
-    width: 126px;
-    animation: ${floatingStarsRight} 3.5s ease-in-out infinite;
-    ${({ theme }) => theme.mediaQueries.xl} {
-      left: 15%;
-      bottom: 5%;
-      width: auto;
-    }
+    top: 50%;
+    right: 22%;
+    animation: ${floatingStarsLeft} 3s ease-in-out infinite;
+    // ${({ theme }) => theme.mediaQueries.xl} {
+    // }
   }
   & :nth-child(3) {
-    right: 0%;
-    top: 0%;
-    width: 71px;
-    animation: ${floatingStarsRight} 2.5s ease-in-out infinite;
-    ${({ theme }) => theme.mediaQueries.xl} {
-      right: 10%;
-      top: 15%;
-      width: auto;
-    }
+    left: 48%;
+    bottom: 15%;
+    animation: ${floatingStarsRight} 3.5s ease-in-out infinite;
+    // ${({ theme }) => theme.mediaQueries.xl} {
+    // }
   }
 }`
 
 const TradingRewardBanner = () => {
+  const { t } = useTranslation()
+
   return (
     <Container position="relative">
       <Decorations>
-        <img src="/images/trading-reward/star1.png" width="155px" height="170px" alt="star1" />
-        <img src="/images/trading-reward/star2.png" width="240px" height="187px" alt="star2" />
-        <img src="/images/trading-reward/star3.png" width="160px" height="124px" alt="star3" />
+        <img src="/images/trading-reward/star1.png" width="43px" height="43px" alt="star1" />
+        <img src="/images/trading-reward/star2.png" width="71px" height="71px" alt="star2" />
+        <img src="/images/trading-reward/star3.png" width="36px" height="36px" alt="star3" />
       </Decorations>
-      <Flex position="relative" zIndex="1">
-        <Box>
+      <Flex
+        position="relative"
+        zIndex="1"
+        width={['100%', '100%', '100%', '1140px']}
+        margin="auto"
+        justifyContent="space-between"
+      >
+        <Flex flexDirection="column" alignSelf="center">
           <Text bold fontSize="60px" color="secondary">
-            Trading Reward
+            {t('Trading Reward')}
           </Text>
           <Text bold fontSize="40px" color="secondary" mb="16px">
             $42,000
             <Text bold fontSize="40px" color="secondary" as="span" ml="4px">
-              in total to be earn!
+              {t('in total to be earn!')}
             </Text>
           </Text>
           <Text maxWidth="404px" bold fontSize="24px" mb="32px" lineHeight="26.4px">
-            Earn CAKE while trading your favourite tokens on PancakeSwap.
+            {t('Earn CAKE while trading your favorite tokens on PancakeSwap.')}
           </Text>
           <Flex>
-            <Button>Start Trading</Button>
+            <Button>{t('Start Trading')}</Button>
             <Button ml="12px" variant="secondary">
-              How to Earn?
+              {t('How to Earn?')}
             </Button>
           </Flex>
-        </Box>
-        <Box
-          m={['auto', 'auto', 'auto', '0']}
-          width={['227px', '257px', '327px', '560px']}
-          height={['306px', '356px', '406px', '639px']}
-        >
+        </Flex>
+        <Box width={['554px']} height={['573px']}>
           <Image src={bunnyImage} alt="banner-image" />
         </Box>
       </Flex>
