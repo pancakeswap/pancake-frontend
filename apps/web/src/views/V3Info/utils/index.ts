dbdcd7cb7e5bec3ca5a8cd281f3a0f47aba59deb
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-03-25 14:09:37 +0800

    chore: add search query

--- apps/web/src/views/V3Info/utils/index.ts
+++ apps/web/src/views/V3Info/utils/index.ts
@@ -1,3 +1,7 @@
 export function notEmpty<TValue>(value: TValue | null | undefined): value is TValue {
   return value !== null && value !== undefined
 }
+
+export function escapeRegExp(string: string): string {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&') // $& means the whole matched string
+}
