64025fde87c76ab642a0dbd51485eae5da310df9
ChefMomota <98292246+ChefMomota@users.noreply.github.com>
2023-03-29 19:42:51 +0800

    feat: Prod cookie domain (#6405)

--- apps/web/src/config/constants/affiliatesProgram.ts
+++ apps/web/src/config/constants/affiliatesProgram.ts
@@ -1,4 +1,8 @@
 const host = () => {
+  if (process.env.VERCEL_ENV === 'production' || process.env.NODE_ENV === 'production') {
+    return '.pancakeswap.finance'
+  }
+
   const key = '.pancake.'
   const hostName = process.env.NEXT_PUBLIC_VERCEL_URL ? process.env.NEXT_PUBLIC_VERCEL_URL.split(key)[1] : ''
   return process.env.NEXT_PUBLIC_VERCEL_URL ? `${key}${hostName}` : 'localhost'
