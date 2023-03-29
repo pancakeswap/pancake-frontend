43ef4c0489c7b4900b4b6a5f801bef480f29666b
chefmomota <momota@pancakeswap.com>
2023-03-29 15:27:43 +0800

    feat: Update LP address

--- packages/farms/constants/56.ts
+++ packages/farms/constants/56.ts
@@ -53,7 +53,7 @@ const farms: SerializedFarmConfig[] = [
   {
     pid: 159,
     lpSymbol: 'RDNT-BNB LP',
-    lpAddress: '0x9Ed9E9aA51670A3210fD6078024c21ec6c1d61d9',
+    lpAddress: '0x346575fC7f07E6994D76199E41D13dC1575322E1',
     token: bscTokens.rdnt,
     quoteToken: bscTokens.wbnb,
   },
