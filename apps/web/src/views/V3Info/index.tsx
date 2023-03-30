913dcd9970a465f4d3d447205c42717c1da94fd8
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-03-30 13:25:04 +0800

    chore: info main page

--- apps/web/src/views/V3Info/index.tsx
+++ apps/web/src/views/V3Info/index.tsx
@@ -1,21 +1,23 @@
-import { AutoColumn, PairDataTimeWindowEnum, Text } from '@pancakeswap/uikit'
-import dynamic from 'next/dynamic'
-import styled from 'styled-components'
 import { ethereumTokens } from '@pancakeswap/tokens'
-// import { useTransformedVolumeData } from 'hooks/chart'
+import { AutoColumn, PairDataTimeWindowEnum, Text, Button, Box, Flex } from '@pancakeswap/uikit'
 import { useActiveChainId } from 'hooks/useActiveChainId'
 import useTheme from 'hooks/useTheme'
 import { usePairTokensPrice } from 'hooks/v3/usePairTokensPrice'
+import dynamic from 'next/dynamic'
 import { useEffect, useMemo, useState } from 'react'
-import { useProtocolChartData } from './hooks'
-
-import { MonoSpace, PageWrapper, ThemedBackgroundGlobal } from './components/shared'
-// import BarChart from './components/BarChart/alt'
-
+import styled from 'styled-components'
+import BarChart from './components/BarChart/alt'
 import LineChart from './components/LineChart/alt'
-import { ResponsiveRow } from './components/Row'
+import Percent from './components/Percent'
+import { ResponsiveRow, RowFixed, RowBetween } from './components/Row'
+import TransactionsTable from './components/TransactionsTable'
+import { MonoSpace, PageWrapper, ThemedBackgroundGlobal } from './components/shared'
+import { useProtocolChartData, useProtocolData, useProtocolTransactionData } from './hooks'
+import { useTransformedVolumeData } from './hooks/chart'
 import { unixToDate } from './utils/date'
 import { formatDollarAmount } from './utils/numbers'
+import { VolumeWindow } from './types'
+import { DarkGreyCard } from './components/Card'
 
 const SwapLineChart = dynamic(() => import('@pancakeswap/uikit/src/components/Chart/PairPriceChart'), {
   ssr: false,
@@ -32,13 +34,14 @@ export default function Home() {
 
   const { theme } = useTheme()
 
-  // const [protocolData] = useProtocolData()
+  const protocolData = useProtocolData()
+  const transactionData = useProtocolTransactionData()
   const { chainId } = useActiveChainId()
 
-  const [, setVolumeHover] = useState<number | undefined>()
+  const [volumeHover, setVolumeHover] = useState<number | undefined>()
   const [liquidityHover, setLiquidityHover] = useState<number | undefined>()
   const [leftLabel, setLeftLabel] = useState<string | undefined>()
-  // const [rightLabel, setRightLabel] = useState<string | undefined>()
+  const [rightLabel, setRightLabel] = useState<string | undefined>()
 
   // Hot fix to remove errors in TVL data while subgraph syncs.
   const chartData = useProtocolChartData()
@@ -53,16 +56,16 @@ export default function Home() {
   const pairTokensPrice = usePairTokensPrice(ethereumTokens.weth, ethereumTokens.usdc, PairDataTimeWindowEnum.YEAR, 1)
 
   // if hover value undefined, reset to current day value
-  // useEffect(() => {
-  //   if (volumeHover === undefined && protocolData) {
-  //     setVolumeHover(protocolData.volumeUSD)
-  //   }
-  // }, [protocolData, volumeHover])
-  // useEffect(() => {
-  //   if (liquidityHover === undefined && protocolData) {
-  //     setLiquidityHover(protocolData.tvlUSD)
-  //   }
-  // }, [liquidityHover, protocolData])
+  useEffect(() => {
+    if (volumeHover === undefined && protocolData) {
+      setVolumeHover(protocolData.volumeUSD)
+    }
+  }, [protocolData, volumeHover])
+  useEffect(() => {
+    if (liquidityHover === undefined && protocolData) {
+      setLiquidityHover(protocolData.tvlUSD)
+    }
+  }, [liquidityHover, protocolData])
 
   const formattedTvlData = useMemo(() => {
     if (chartData) {
@@ -76,22 +79,22 @@ export default function Home() {
     return []
   }, [chartData])
 
-  // const formattedVolumeData = useMemo(() => {
-  //   if (chartData) {
-  //     return chartData.map((day) => {
-  //       return {
-  //         time: unixToDate(day.date),
-  //         value: day.volumeUSD,
-  //       }
-  //     })
-  //   }
-  //   return []
-  // }, [chartData])
+  const formattedVolumeData = useMemo(() => {
+    if (chartData) {
+      return chartData.map((day) => {
+        return {
+          time: unixToDate(day.date),
+          value: day.volumeUSD,
+        }
+      })
+    }
+    return []
+  }, [chartData])
 
-  // const weeklyVolumeData = useTransformedVolumeData(chartData, 'week')
-  // const monthlyVolumeData = useTransformedVolumeData(chartData, 'month')
+  const weeklyVolumeData = useTransformedVolumeData(chartData, 'week')
+  const monthlyVolumeData = useTransformedVolumeData(chartData, 'month')
 
-  // const [volumeWindow, setVolumeWindow] = useState(VolumeWindow.weekly)
+  const [volumeWindow, setVolumeWindow] = useState(VolumeWindow.weekly)
 
   const tvlValue = useMemo(() => {
     return formatDollarAmount(liquidityHover ?? 0, 2, true)
@@ -99,103 +102,119 @@ export default function Home() {
   return (
     <PageWrapper>
       <ThemedBackgroundGlobal backgroundColor={theme.colors.background} />
-      <AutoColumn gap="16px">
-        <Text>PancakeSwap Overview</Text>
-        <ResponsiveRow>
-          <ChartWrapper>
-            <LineChart
-              data={formattedTvlData}
-              height={220}
-              minHeight={332}
-              // color={theme.colors.primary}
-              value={liquidityHover}
-              label={leftLabel}
-              setValue={setLiquidityHover}
-              setLabel={setLeftLabel}
-              topLeft={
-                <AutoColumn gap="4px">
-                  <Text fontSize="16px">TVL</Text>
-                  <Text fontSize="32px">
-                    <MonoSpace>{tvlValue} </MonoSpace>
-                  </Text>
-                  <Text fontSize="12px" height="14px">
-                    {leftLabel ? <MonoSpace>{leftLabel} (UTC)</MonoSpace> : null}
-                  </Text>
-                </AutoColumn>
-              }
-            />
-          </ChartWrapper>
-          <ChartWrapper>
-            <SwapLineChart
-              data={pairTokensPrice}
-              setHoverValue={setHoverValue}
-              setHoverDate={setHoverDate}
-              isChangePositive={false}
-              isChartExpanded={false}
-              timeWindow={PairDataTimeWindowEnum.MONTH}
-              // priceLineData={[
-              //   { title: 'max', price: 100, color: '#31D0AA' },
-              //   { title: 'min', price: 80, color: '#ED4B9E' },
-              //   { title: 'mid', price: 90, color: '#BDC2C4' },
-              // ]}
-            />
-          </ChartWrapper>
-          {/* <ChartWrapper>
-            <BarChart
-              height={220}
-              minHeight={332}
-              data={
-                volumeWindow === VolumeWindow.monthly
-                  ? monthlyVolumeData
-                  : volumeWindow === VolumeWindow.weekly
-                  ? weeklyVolumeData
-                  : formattedVolumeData
-              }
-              color={theme.colors.gradientBlue}
-              setValue={setVolumeHover}
-              setLabel={setRightLabel}
-              value={volumeHover}
-              label={rightLabel}
-              activeWindow={volumeWindow}
-              topRight={
-                <RowFixed style={{ marginLeft: '-40px', marginTop: '8px' }}>
-                  <SmallOptionButton
-                    active={volumeWindow === VolumeWindow.daily}
-                    onClick={() => setVolumeWindow(VolumeWindow.daily)}
-                  >
-                    D
-                  </SmallOptionButton>
-                  <SmallOptionButton
-                    active={volumeWindow === VolumeWindow.weekly}
-                    style={{ marginLeft: '8px' }}
-                    onClick={() => setVolumeWindow(VolumeWindow.weekly)}
-                  >
-                    W
-                  </SmallOptionButton>
-                  <SmallOptionButton
-                    active={volumeWindow === VolumeWindow.monthly}
-                    style={{ marginLeft: '8px' }}
-                    onClick={() => setVolumeWindow(VolumeWindow.monthly)}
-                  >
-                    M
-                  </SmallOptionButton>
+
+      <Text>PancakeSwap Overview</Text>
+      <Flex>
+        <ChartWrapper>
+          <LineChart
+            data={formattedTvlData}
+            height={220}
+            minHeight={332}
+            // color={theme.colors.primary}
+            value={liquidityHover}
+            label={leftLabel}
+            setValue={setLiquidityHover}
+            setLabel={setLeftLabel}
+            topLeft={
+              <AutoColumn gap="4px">
+                <Text fontSize="16px">TVL</Text>
+                <Text fontSize="32px">
+                  <MonoSpace>{tvlValue} </MonoSpace>
+                </Text>
+                <Text fontSize="12px" height="14px">
+                  {leftLabel ? <MonoSpace>{leftLabel} (UTC)</MonoSpace> : null}
+                </Text>
+              </AutoColumn>
+            }
+          />
+        </ChartWrapper>
+        <ChartWrapper>
+          <BarChart
+            height={220}
+            minHeight={332}
+            data={
+              volumeWindow === VolumeWindow.monthly
+                ? monthlyVolumeData
+                : volumeWindow === VolumeWindow.weekly
+                ? weeklyVolumeData
+                : formattedVolumeData
+            }
+            color={theme.colors.primary}
+            setValue={setVolumeHover}
+            setLabel={setRightLabel}
+            value={volumeHover}
+            label={rightLabel}
+            activeWindow={volumeWindow}
+            topRight={
+              <RowFixed style={{ marginLeft: '-40px', marginTop: '8px' }}>
+                <Button onClick={() => setVolumeWindow(VolumeWindow.daily)}>D</Button>
+                <Button style={{ marginLeft: '8px' }} onClick={() => setVolumeWindow(VolumeWindow.weekly)}>
+                  W
+                </Button>
+                <Button style={{ marginLeft: '8px' }} onClick={() => setVolumeWindow(VolumeWindow.monthly)}>
+                  M
+                </Button>
+              </RowFixed>
+            }
+            topLeft={
+              <AutoColumn gap="4px">
+                <Text fontSize="16px">Volume 24H</Text>
+                <Text fontSize="32px">
+                  <MonoSpace> {formatDollarAmount(volumeHover, 2)}</MonoSpace>
+                </Text>
+                <Text fontSize="12px" height="14px">
+                  {rightLabel ? <MonoSpace>{rightLabel} (UTC)</MonoSpace> : null}
+                </Text>
+              </AutoColumn>
+            }
+          />
+        </ChartWrapper>
+      </Flex>
+      <Box>
+        <DarkGreyCard>
+          <RowBetween>
+            <RowFixed>
+              <RowFixed mr="20px">
+                <Text mr="4px">Volume 24H: </Text>
+                <Text mr="4px">{formatDollarAmount(protocolData?.volumeUSD)}</Text>
+                <Percent value={protocolData?.volumeUSDChange} wrap />
+              </RowFixed>
+              <RowFixed mr="20px">
+                <Text mr="4px">Fees 24H: </Text>
+                <Text mr="4px">{formatDollarAmount(protocolData?.feesUSD)}</Text>
+                <Percent value={protocolData?.feeChange} wrap />
+              </RowFixed>
+              <Box>
+                <RowFixed mr="20px">
+                  <Text mr="4px">TVL: </Text>
+                  <Text mr="4px">{formatDollarAmount(protocolData?.tvlUSD)}</Text>
+                  <Percent value={protocolData?.tvlUSDChange} wrap />
                 </RowFixed>
-              }
-              topLeft={
-                <AutoColumn gap="4px">
-                  <Text fontSize="16px">Volume 24H</Text>
-                  <Text fontSize="32px">
-                    <MonoSpace> {formatDollarAmount(volumeHover, 2)}</MonoSpace>
-                  </Text>
-                  <Text fontSize="12px" height="14px">
-                    {rightLabel ? <MonoSpace>{rightLabel} (UTC)</MonoSpace> : null}
-                  </Text>
-                </AutoColumn>
-              }
-            />
-          </ChartWrapper> */}
-        </ResponsiveRow>
-      </AutoColumn>
+              </Box>
+            </RowFixed>
+          </RowBetween>
+        </DarkGreyCard>
+      </Box>
+
+      <Text>Transactions</Text>
+
+      {transactionData ? <TransactionsTable transactions={transactionData} color={theme.colors.primary} /> : null}
+
+      <ChartWrapper>
+        <SwapLineChart
+          data={pairTokensPrice}
+          setHoverValue={setHoverValue}
+          setHoverDate={setHoverDate}
+          isChangePositive={false}
+          isChartExpanded={false}
+          timeWindow={PairDataTimeWindowEnum.MONTH}
+          // priceLineData={[
+          //   { title: 'max', price: 100, color: '#31D0AA' },
+          //   { title: 'min', price: 80, color: '#ED4B9E' },
+          //   { title: 'mid', price: 90, color: '#BDC2C4' },
+          // ]}
+        />
+      </ChartWrapper>
     </PageWrapper>
   )
 }
