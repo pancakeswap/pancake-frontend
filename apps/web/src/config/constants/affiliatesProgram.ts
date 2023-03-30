aa48ee14baffedb53241f2b2de751daa3f4a9c2c
ChefMomota <98292246+ChefMomota@users.noreply.github.com>
2023-03-30 13:02:41 +0800

    feat: Add log (#6416)

--- apps/web/src/config/constants/affiliatesProgram.ts
+++ apps/web/src/config/constants/affiliatesProgram.ts
@@ -1,6 +1,11 @@
 const host = () => {
   const hostName = process.env.NEXT_PUBLIC_VERCEL_URL ?? ''
-
+  // eslint-disable-next-line no-console
+  console.log({
+    NEXT_PUBLIC_VERCEL_URL: process.env.NEXT_PUBLIC_VERCEL_URL,
+    VERCEL_ENV: process.env.VERCEL_ENV,
+    NODE_ENV: process.env.NODE_ENV,
+  })
   if (hostName.includes('.pancake.run')) {
     return '.pancake.run'
   }
