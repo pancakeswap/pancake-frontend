484f2ec7df16ff7871bacc7af539787aee1d3c6a
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-03-30 13:26:01 +0800

    chore: v3 info shared component

--- apps/web/src/views/V3Info/components/shared.tsx
+++ apps/web/src/views/V3Info/components/shared.tsx
@@ -1,5 +1,30 @@
 import styled from 'styled-components'
 
+export const PageButtons = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 0.2em;
+  margin-bottom: 0.5em;
+`
+
+export const Arrow = styled.div<{ faded: boolean }>`
+  color: ${({ theme }) => theme.colors.primary};
+  opacity: ${(props) => (props.faded ? 0.3 : 1)};
+  padding: 0 20px;
+  user-select: none;
+  :hover {
+    cursor: pointer;
+  }
+`
+
+export const Break = styled.div`
+  height: 1px;
+  background-color: ${({ theme }) => theme.colors.backgroundAlt};
+  width: 100%;
+`
+
 export const MonoSpace = styled.span`
   font-variant-numeric: tabular-nums;
 `
