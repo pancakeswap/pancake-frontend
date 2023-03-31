bcd4d7c2a73ccf5eda91b2822561bf366e149f05
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-03-31 13:50:23 +0800

    chore: add token black list

--- apps/web/src/views/V3Info/constants.ts
+++ apps/web/src/views/V3Info/constants.ts
@@ -13,3 +13,13 @@ export const POOL_HIDE: { [key: string]: string[] } = {
   ],
   [ChainId.BSC]: [],
 }
+
+export const TOKEN_HIDE: { [key: string]: string[] } = {
+  [ChainId.ETHEREUM]: [
+    '0xd46ba6d942050d489dbd938a2c909a5d5039a161',
+    '0x7dfb72a2aad08c937706f21421b15bfc34cba9ca',
+    '0x12b32f10a499bf40db334efe04226cca00bf2d9b',
+    '0x160de4468586b6b2f8a92feb0c260fc6cfc743b1',
+  ],
+  [ChainId.BSC]: [],
+}
