f962cf4cf9e5632e9d5ce7c9c563820b971223f0
chefmomota <momota@pancakeswap.com>
2023-04-01 22:23:44 +0800

    feat: Show Affiliate Landing

--- apps/web/src/views/AffiliatesProgram/components/OnBoardingModal/Congratulations.tsx
+++ apps/web/src/views/AffiliatesProgram/components/OnBoardingModal/Congratulations.tsx
@@ -1,9 +1,11 @@
-import { useMemo } from 'react'
-import { Flex, Text, Button, Link } from '@pancakeswap/uikit'
+import { useMemo, useState } from 'react'
+import { Flex, Text, Button, Link, ModalV2 } from '@pancakeswap/uikit'
 import { useTranslation } from '@pancakeswap/localization'
 import { useTheme } from 'styled-components'
 import { useActiveChainId } from 'hooks/useActiveChainId'
 import { getPerpetualUrl } from 'utils/getPerpetualUrl'
+import USCitizenConfirmModal from 'components/Modal/USCitizenConfirmModal'
+import { useUserNotUsCitizenAcknowledgement } from 'hooks/useUserIsUsCitizenAcknowledgement'
 
 const Congratulations = () => {
   const {
@@ -12,31 +14,48 @@ const Congratulations = () => {
   } = useTranslation()
   const { chainId } = useActiveChainId()
   const { isDark } = useTheme()
-
+  const [isOpen, setIsOpen] = useState(false)
+  const [userNotUsCitizenAcknowledgement] = useUserNotUsCitizenAcknowledgement()
   const perpetualUrl = useMemo(() => getPerpetualUrl({ chainId, languageCode: code, isDark }), [chainId, code, isDark])
 
   return (
-    <Flex flexDirection="column" padding={['24px', '24px', '24px', '24px', '80px 24px']}>
-      <Text lineHeight="110%" maxWidth="190px" fontSize={['24px']} bold m="12px 0">
-        {t('Congratulations! You’re all set!')}
-      </Text>
-      <Text color="textSubtle" fontSize="14px" mb="24px">
-        {t('Start trading and enjoy referral discounts!')}
-      </Text>
-      <Link external href="/swap" width="100% !important">
-        <Button width="100%">{t('Start Trading')}</Button>
-      </Link>
-      <Link external href={perpetualUrl} width="100% !important">
-        <Button width="100%" m="8px 0" variant="secondary">
-          {t('Try out our Perpetuals Platform')}
-        </Button>
-      </Link>
-      <Link external href="/" width="100% !important">
-        <Button width="100%" variant="secondary">
-          {t('Learn More')}
-        </Button>
-      </Link>
-    </Flex>
+    <>
+      <ModalV2 style={{ zIndex: 100 }} isOpen={isOpen} closeOnOverlayClick onDismiss={() => setIsOpen(false)}>
+        <USCitizenConfirmModal />
+      </ModalV2>
+      <Flex flexDirection="column" padding={['24px', '24px', '24px', '24px', '80px 24px']}>
+        <Text lineHeight="110%" maxWidth="190px" fontSize={['24px']} bold m="12px 0">
+          {t('Congratulations! You’re all set!')}
+        </Text>
+        <Text color="textSubtle" fontSize="14px" mb="24px">
+          {t('Start trading and enjoy referral discounts!')}
+        </Text>
+        <Link external href="/swap" width="100% !important">
+          <Button width="100%">{t('Start Trading')}</Button>
+        </Link>
+        <Link
+          external
+          href={perpetualUrl}
+          width="100% !important"
+          onClick={(e) => {
+            if (!userNotUsCitizenAcknowledgement) {
+              e.stopPropagation()
+              e.preventDefault()
+              setIsOpen(true)
+            }
+          }}
+        >
+          <Button width="100%" m="8px 0" variant="secondary">
+            {t('Try out our Perpetuals Platform')}
+          </Button>
+        </Link>
+        <Link external href="/" width="100% !important">
+          <Button width="100%" variant="secondary">
+            {t('Learn More')}
+          </Button>
+        </Link>
+      </Flex>
+    </>
   )
 }
 
