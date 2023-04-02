ea1d5a0453f112e3f5a831e2a830f50c35a5e88b
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-04-03 04:19:31 +0800

    chore: v3 info multiChain page

--- apps/web/src/pages/v3Info/[chainName]/index.tsx
+++ apps/web/src/pages/v3Info/[chainName]/index.tsx
@@ -1,5 +1,11 @@
-const ChainNameIndex = () => {
-  return <>V3ChainNameIndex</>
+import { InfoPageLayout } from 'views/V3Info/components/Layout'
+import Overview from 'views/V3Info'
+
+const InfoPage = () => {
+  return <Overview />
 }
 
-export default ChainNameIndex
+InfoPage.Layout = InfoPageLayout
+InfoPage.chains = [] // set all
+
+export default InfoPage
