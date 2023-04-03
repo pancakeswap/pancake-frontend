0e02346c13da49f40fc093d9fa6a9d6079997169
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2023-04-03 19:16:23 +0800

    fix: case

--- apps/web/src/pages/info/v3/pairs/[address].tsx
+++ apps/web/src/pages/info/v3/pairs/[address].tsx
@@ -4,7 +4,7 @@ import Pool from 'views/V3Info/views/PoolPage'
 
 const PoolPage = () => {
   const router = useRouter()
-  return <Pool address={String(router.query.address)} />
+  return <Pool address={String(router.query.address).toLowerCase()} />
 }
 
 PoolPage.Layout = InfoPageLayout
