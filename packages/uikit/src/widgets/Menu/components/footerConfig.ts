f2867854d60df94a4fc18c5ebc9ebabb221d9c45
chefmomota <momota@pancakeswap.com>
2023-04-03 14:21:16 +0800

    feat: Add Terms of service

--- packages/uikit/src/widgets/Menu/components/footerConfig.ts
+++ packages/uikit/src/widgets/Menu/components/footerConfig.ts
@@ -26,6 +26,10 @@ export const footerLinks: (t: ContextApi["t"]) => FooterLinkType[] = (t) => [
         label: t("Litepaper"),
         href: "https://v2litepaper.pancakeswap.finance/",
       },
+      {
+        label: t("Terms Of Service"),
+        href: "/terms-of-service",
+      },
     ],
   },
   {
