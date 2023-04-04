d3862bfb8a61f0ac0b18b44fe31cb1a961882a05
Woody <woodywu810@gmail.com>
2023-04-04 17:52:59 +0800

    feat(Swap): Integrate the toggle price display button into the SyncAltIcon

--- packages/uikit/src/widgets/Swap/TradePrice.tsx
+++ packages/uikit/src/widgets/Swap/TradePrice.tsx
@@ -2,9 +2,8 @@ import { Price, Currency } from "@pancakeswap/swap-sdk-core";
 import { Loading, SyncAltIcon } from "@pancakeswap/uikit";
 import { formatPrice } from "@pancakeswap/utils/formatFractions";
 import { AtomBox } from "@pancakeswap/ui/components/AtomBox";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { iconButtonClass } from "./SwapWidget.css";
-import { AutoRenewIcon } from "../../components/Svg";
 import { Text } from "../../components/Text";
 
 interface TradePriceProps {
@@ -16,6 +15,10 @@ export function TradePrice({ price, loading }: TradePriceProps) {
   const [showInverted, setShowInverted] = useState<boolean>(false);
   const formattedPrice = showInverted ? formatPrice(price, 6) : formatPrice(price?.invert(), 6);
 
+  const toggleShowInverted = useCallback(() => {
+    setShowInverted((prev) => !prev);
+  }, []);
+
   const show = Boolean(price?.baseCurrency && price?.quoteCurrency);
 
   return (
@@ -26,17 +29,16 @@ export function TradePrice({ price, loading }: TradePriceProps) {
       {show ? (
         <>
           {`1 ${showInverted ? price?.baseCurrency?.symbol : price?.quoteCurrency?.symbol}`}
-          <SyncAltIcon width="14px" height="14px" color="textSubtle" ml="4px" mr="4px" />
-          {`${formattedPrice} ${showInverted ? price?.quoteCurrency?.symbol : price?.baseCurrency?.symbol}`}
           {loading ? (
             <AtomBox className={iconButtonClass}>
               <Loading width="12px" height="12px" />
             </AtomBox>
           ) : (
-            <AtomBox role="button" className={iconButtonClass} onClick={() => setShowInverted(!showInverted)}>
-              <AutoRenewIcon width="14px" />
+            <AtomBox mt="8px" ml="4px" mr="4px" onClick={toggleShowInverted} style={{ cursor: "pointer" }}>
+              <SyncAltIcon color="textSubtle" />
             </AtomBox>
           )}
+          {`${formattedPrice} ${showInverted ? price?.quoteCurrency?.symbol : price?.baseCurrency?.symbol}`}
         </>
       ) : (
         "-"
