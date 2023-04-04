63feb90e62a5166bb43cbed337a7e513c5ce13ef
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-04-04 16:33:26 +0800

    chore: migrate to new gtag

--- apps/bridge/pages/_app.tsx
+++ apps/bridge/pages/_app.tsx
@@ -99,7 +99,7 @@ function MyApp({ Component, pageProps }: AppProps) {
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer', '${process.env.NEXT_PUBLIC_GTAG}');
+            })(window,document,'script','dataLayer', '${process.env.NEXT_PUBLIC_NEW_GTAG}');
           `,
         }}
       />
--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -100,7 +100,7 @@ function MyApp(props: AppProps<{ initialReduxState: any }>) {
         </Blocklist>
       </Providers>
       <Analytics />
-      <Script
+      {/* <Script
         strategy="afterInteractive"
         id="google-tag"
         dangerouslySetInnerHTML={{
@@ -112,7 +112,7 @@ function MyApp(props: AppProps<{ initialReduxState: any }>) {
             })(window,document,'script','dataLayer', '${process.env.NEXT_PUBLIC_GTAG}');
           `,
         }}
-      />
+      /> */}
       <Script
         strategy="afterInteractive"
         id="google-tag-new"
@@ -120,9 +120,9 @@ function MyApp(props: AppProps<{ initialReduxState: any }>) {
           __html: `
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='customDataLayer'?'&l='+l:'';j.async=true;j.src=
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','customDataLayer','${process.env.NEXT_PUBLIC_NEW_GTAG}');
+})(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_NEW_GTAG}');
           `,
         }}
       />
