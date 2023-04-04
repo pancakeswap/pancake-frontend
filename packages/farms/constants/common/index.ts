9e78fd99d978f7ed048482c138d1bc90830c6c38
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2023-04-04 14:29:27 +0800

    fix(blog): Build (#6480)

--- packages/farms/constants/common/index.ts
+++ packages/farms/constants/common/index.ts
@@ -1,5 +1,6 @@
 import { ChainId, ERC20Token } from '@pancakeswap/sdk'
 import { bscTestnetTokens, bscTokens, ethereumTokens, goerliTestnetTokens } from '@pancakeswap/tokens'
+import type { CommonPrice } from '../../src/fetchFarmsV3'
 
 export const CAKE_BNB_LP_MAINNET = '0x0eD7e52944161450477ee417DE9Cd3a859b14fD0'
 
@@ -20,7 +21,7 @@ export const priceHelperTokens = {
 } satisfies Record<number, PriceHelper>
 
 // for testing purposes
-export const DEFAULT_COMMON_PRICE = {
+export const DEFAULT_COMMON_PRICE: Record<ChainId, CommonPrice> = {
   [ChainId.ETHEREUM]: {},
   [ChainId.GOERLI]: {
     [goerliTestnetTokens.mockA.address]: '10',
