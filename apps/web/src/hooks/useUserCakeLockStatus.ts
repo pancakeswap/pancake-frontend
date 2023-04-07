9c40f31a81dce007409741836621c305e507009c
memoyil <2213635+memoyil@users.noreply.github.com>
2023-04-07 10:07:16 +0200

    chore: Use pcs rpc for cake lock status (#6388)

--- apps/web/src/hooks/useUserCakeLockStatus.ts
+++ apps/web/src/hooks/useUserCakeLockStatus.ts
@@ -7,14 +7,14 @@ import { useActiveChainId } from './useActiveChainId'
 export const useUserCakeLockStatus = () => {
   const { address: account } = useAccount()
   const { chainId } = useActiveChainId()
-  const cakeVaultContract = useCakeVaultContract()
+  const cakeVaultContract = useCakeVaultContract(false)
 
   const { data: userCakeLockStatus = null } = useSWRImmutable(
     account && chainId === ChainId.BSC ? ['userCakeLockStatus', account] : null,
     async () => {
       const { locked, lockEndTime } = await cakeVaultContract.userInfo(account)
       const lockEndTimeStr = lockEndTime.toString()
-      return locked && (lockEndTimeStr === '0' || new Date() > new Date(parseInt(lockEndTimeStr) * 1000))
+      return locked && (lockEndTimeStr === '0' || Date.now() > parseInt(lockEndTimeStr) * 1000)
     },
   )
   return userCakeLockStatus
