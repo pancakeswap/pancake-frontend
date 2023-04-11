cc26075bfdb7f2f8dd230d1720a958cffbeed082
chefmomota <momota@pancakeswap.com>
2023-04-11 22:43:33 +0800

    feat: Get merkleProofs with api

--- apps/web/src/views/TradingReward/hooks/useClaimAllReward.tsx
+++ apps/web/src/views/TradingReward/hooks/useClaimAllReward.tsx
@@ -1,7 +1,4 @@
 import { useCallback } from 'react'
-import web3 from 'web3'
-import { useAccount } from 'wagmi'
-import { keccak256 } from '@ethersproject/keccak256'
 import BigNumber from 'bignumber.js'
 import { useToast } from '@pancakeswap/uikit'
 import useCatchTxError from 'hooks/useCatchTxError'
@@ -9,29 +6,29 @@ import { useTranslation } from '@pancakeswap/localization'
 import { UserCampaignInfoDetail } from 'views/TradingReward/hooks/useAllUserCampaignInfo'
 import { useTradingRewardContract } from 'hooks/useContract'
 import { ToastDescriptionWithTx } from 'components/Toast'
-import { MerkleTree } from 'merkletreejs'
+import { useActiveChainId } from 'hooks/useActiveChainId'
+import { TRADING_REWARD_API } from 'config/constants/endpoints'
 
 export const useClaimAllReward = (unclaimData: UserCampaignInfoDetail[]) => {
   const { t } = useTranslation()
-  const { address: account } = useAccount()
+  const { chainId } = useActiveChainId()
   const { toastSuccess } = useToast()
   const { fetchWithCatchTxError, loading: isPending } = useCatchTxError()
   const contract = useTradingRewardContract()
 
   const handleClaim = useCallback(async () => {
     const campaignIds = unclaimData.map((i) => i.campaignId)
-    const selfVolumes = unclaimData.map((i) => [Number(new BigNumber(i.totalVolume).div(1e18).toFixed(2))])
+    const selfVolumes = unclaimData.map((i) => [Number(new BigNumber(i.totalVolume.toFixed(2)).div(1e18))])
 
-    const merkleProofs = unclaimData.map((i) => {
-      const volume = new BigNumber(i.totalVolume).div(1e18).toFixed(2)
-      const leaf = Buffer.from(`0x${keccak256(keccak256(web3.utils.encodePacked(account, volume)))}`, 'utf8').toString(
-        'hex',
-      )
-      const tree = new MerkleTree([leaf], keccak256, { sortPairs: true })
-      const hexProof = JSON.stringify(tree.getHexProof(leaf))
-      const merkleProof = JSON.parse(hexProof)
-      return merkleProof
-    })
+    const merkleProofsData = await Promise.all(
+      unclaimData.map(async (i) => {
+        const response = await fetch(`${TRADING_REWARD_API}/hash/chainId/${chainId}/campaignId/${i.campaignId}`)
+        const result = await response.json()
+        return { merkleProof: result.data }
+      }),
+    )
+
+    const merkleProofs = merkleProofsData.map((i) => i.merkleProof)
 
     const receipt = await fetchWithCatchTxError(() => contract.claimRewardMulti(campaignIds, merkleProofs, selfVolumes))
 
@@ -45,7 +42,7 @@ export const useClaimAllReward = (unclaimData: UserCampaignInfoDetail[]) => {
       // dispatch(fetchPotteryUserDataAsync(account))
     }
     return null
-  }, [account, contract, fetchWithCatchTxError, t, toastSuccess, unclaimData])
+  }, [chainId, contract, fetchWithCatchTxError, t, toastSuccess, unclaimData])
 
   return { isPending, handleClaim }
 }
