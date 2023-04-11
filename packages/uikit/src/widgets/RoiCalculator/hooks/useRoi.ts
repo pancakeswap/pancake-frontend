62e119882b2cca72b5318ea8569813202bfc8a5a
chefjackson <116779127+chefjackson@users.noreply.github.com>
2023-04-11 14:37:12 +0800

    fix: liquidity calculation error when switching base token with quote token

--- packages/uikit/src/widgets/RoiCalculator/hooks/useRoi.ts
+++ packages/uikit/src/widgets/RoiCalculator/hooks/useRoi.ts
@@ -1,6 +1,6 @@
 import { Currency, CurrencyAmount, Fraction, JSBI, ONE, Percent, ZERO } from "@pancakeswap/sdk";
 import { FeeAmount, FeeCalculator } from "@pancakeswap/v3-sdk";
-import { formatFraction } from "@pancakeswap/utils/formatFractions";
+import { formatFraction, parseNumberToFraction } from "@pancakeswap/utils/formatFractions";
 import { useMemo } from "react";
 
 import { useRate } from "./useRate";
@@ -18,12 +18,6 @@ interface Params extends Omit<FeeParams, "amount" | "currency"> {
   cakePrice?: number;
 }
 
-const scale = 1_000_000_000_000_000;
-
-const decimalToFraction = (decimal: number) => {
-  return new Fraction(Math.floor(decimal * scale), scale);
-};
-
 export function useRoi({
   amountA,
   amountB,
@@ -38,8 +32,8 @@ export function useRoi({
 }: Params) {
   const fee24h = useFee24h({
     ...rest,
-    amount: amountA,
-    currency: amountB?.currency,
+    amountA,
+    amountB,
   });
   const principal = useMemo(
     () =>
@@ -57,7 +51,7 @@ export function useRoi({
     compoundOn,
     stakeFor,
   });
-  const fee = useMemo(() => decimalToFraction(reward), [reward]);
+  const fee = useMemo(() => parseNumberToFraction(reward, 18), [reward]);
 
   const {
     apr: cakeAprInPercent,
@@ -101,9 +95,9 @@ export function useRoi({
 
 export interface FeeParams {
   // Amount of token user input
-  amount?: CurrencyAmount<Currency>;
+  amountA?: CurrencyAmount<Currency>;
   // Currency of the other token in the pool
-  currency?: Currency;
+  amountB?: CurrencyAmount<Currency>;
   tickLower?: number;
   tickUpper?: number;
   // Average 24h historical trading volume in USD
@@ -122,8 +116,8 @@ export interface FeeParams {
 }
 
 export function useFee24h({
-  amount,
-  currency,
+  amountA,
+  amountB,
   tickLower,
   tickUpper,
   volume24H,
@@ -134,8 +128,8 @@ export function useFee24h({
 }: FeeParams) {
   return useMemo(() => {
     if (
-      !amount ||
-      !currency ||
+      !amountA ||
+      !amountB ||
       typeof tickLower !== "number" ||
       typeof tickUpper !== "number" ||
       !volume24H ||
@@ -145,9 +139,9 @@ export function useFee24h({
     ) {
       return new Fraction(ZERO, ONE);
     }
-    return FeeCalculator.getEstimatedLPFee({
-      amount,
-      currency,
+    return FeeCalculator.getEstimatedLPFeeByAmounts({
+      amountA,
+      amountB,
       tickLower,
       tickUpper,
       volume24H,
@@ -156,5 +150,5 @@ export function useFee24h({
       fee,
       protocolFee,
     });
-  }, [amount, currency, tickLower, tickUpper, volume24H, sqrtRatioX96, mostActiveLiquidity, fee, protocolFee]);
+  }, [amountA, amountB, tickLower, tickUpper, volume24H, sqrtRatioX96, mostActiveLiquidity, fee, protocolFee]);
 }
