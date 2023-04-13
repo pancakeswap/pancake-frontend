6ee8cb83883c4668b5d056af986d7b1f4bd8a8d9
chefmomota <momota@pancakeswap.com>
2023-04-13 13:21:19 +0800

    feat: Blog default language get from localStorage

--- apps/blog/components/Article/ArticleSortSelect.tsx
+++ apps/blog/components/Article/ArticleSortSelect.tsx
@@ -1,4 +1,5 @@
 import { Box, Text, Select, OptionProps } from '@pancakeswap/uikit'
+import { useMemo } from 'react'
 
 interface SortByItem {
   label: string
@@ -7,6 +8,7 @@ interface SortByItem {
 
 interface ArticleSortSelectProps {
   title: string
+  languageOption?: string
   options: SortByItem[]
   setOption: (value: string) => void
 }
@@ -14,18 +16,25 @@ interface ArticleSortSelectProps {
 const ArticleSortSelect: React.FC<React.PropsWithChildren<ArticleSortSelectProps>> = ({
   title,
   options,
+  languageOption,
   setOption,
 }) => {
   const handleChange = (newOption: OptionProps) => {
     setOption(newOption.value)
   }
 
+  const placeHolderText = useMemo(() => options.find((i) => i.value === languageOption), [languageOption, options])
+
   return (
     <Box minWidth="165px">
       <Text fontSize="12px" textTransform="uppercase" color="textSubtle" fontWeight={600} mb="4px">
         {title}
       </Text>
-      <Select options={options} onOptionChange={handleChange} />
+      <Select
+        placeHolderText={languageOption ? placeHolderText?.label : ''}
+        options={options}
+        onOptionChange={handleChange}
+      />
     </Box>
   )
 }
