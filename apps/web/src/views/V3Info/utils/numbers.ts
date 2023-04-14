57ed5b2592d731ab79e04c81f2876d618aeaa0fd
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-04-14 17:44:39 +0800

    chore: transaction table detail improvement

--- apps/web/src/views/V3Info/utils/numbers.ts
+++ apps/web/src/views/V3Info/utils/numbers.ts
@@ -2,7 +2,7 @@ import numbro from 'numbro'
 
 // using a currency library here in case we want to add more in future
 export const formatDollarAmount = (num: number | undefined, digits = 2, round = true) => {
-  if (num === 0) return '$0.00'
+  if (num <= 0) return '$0.00'
   if (!num) return '-'
   if (num < 0.001 && digits <= 3) {
     return '<$0.001'
@@ -24,7 +24,7 @@ export const formatDollarAmount = (num: number | undefined, digits = 2, round = 
 
 // using a currency library here in case we want to add more in future
 export const formatAmount = (num: number | undefined, digits = 2) => {
-  if (num === 0) return '0'
+  if (num <= 0) return '0'
   if (!num) return '-'
   if (num < 0.001) {
     return '<0.001'
