91507b1c3299a1a1d757583735a014ffc635c03c
chefjackson <116779127+chefjackson@users.noreply.github.com>
2023-04-17 18:31:55 +0800

    chore: fix build error

--- apps/web/src/pages/pools/index.tsx
+++ apps/web/src/pages/pools/index.tsx
@@ -1,6 +1,9 @@
 import { SUPPORTED_CHAIN_IDS } from '@pancakeswap/pools'
+
 import Pools from 'views/Pools'
 
-Pools.chains = SUPPORTED_CHAIN_IDS
+const PoolsPage = () => <Pools />
+
+PoolsPage.chains = SUPPORTED_CHAIN_IDS
 
 export default Pools
