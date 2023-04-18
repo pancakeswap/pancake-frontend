d5e8ad5ec9061a0953930e615a14a0671a8debd6
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2023-04-18 14:31:59 +0800

    fix: tsc handle error

--- packages/v3-sdk/tsup.config.ts
+++ packages/v3-sdk/tsup.config.ts
@@ -11,6 +11,11 @@ export default defineConfig((options) => ({
   treeshake: true,
   splitting: true,
   onSuccess: async () => {
-    exec('tsc --emitDeclarationOnly --declaration')
+    exec('tsc --emitDeclarationOnly --declaration', (err, stdout) => {
+      if (err) {
+        console.error(stdout)
+        process.exit(1)
+      }
+    })
   },
 }))
