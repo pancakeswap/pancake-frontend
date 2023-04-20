39d8628ed4ff91982b74910caec09103f61d1f25
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-04-20 19:05:31 +0800

    fix: eth token highlight list

--- apps/web/src/views/Swap/components/HotTokenList/useList.tsx
+++ apps/web/src/views/Swap/components/HotTokenList/useList.tsx
@@ -2,6 +2,7 @@ import { PANCAKE_EXTENDED } from 'config/constants/lists'
 import { useAtomValue } from 'jotai'
 import { useMemo } from 'react'
 import { selectorByUrlsAtom } from 'state/lists/hooks'
+import { multiChainName } from 'state/info/constant'
 import { useTokenDatasSWR, useAllTokenHighLight } from 'state/info/hooks'
 import { useActiveChainId } from 'hooks/useActiveChainId'
 import { ChainId } from '@pancakeswap/sdk'
@@ -19,7 +20,7 @@ export const useTokenHighLightList = () => {
   const { chainId } = useActiveChainId()
   const bscWhiteList = useBSCWhiteList()
   const allTokensFromBSC = useTokenDatasSWR(chainId === ChainId.BSC ? bscWhiteList : [], false)
-  const allTokensFromETH = useAllTokenHighLight()
+  const allTokensFromETH = useAllTokenHighLight(multiChainName[chainId])
 
   return chainId === ChainId.BSC ? allTokensFromBSC : allTokensFromETH
 }
