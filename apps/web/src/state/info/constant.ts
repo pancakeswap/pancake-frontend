f84b50cc9e2cdca13168c79f7c49b662f1be4234
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-04-21 19:07:28 +0800

    chore: fix type

--- apps/web/src/state/info/constant.ts
+++ apps/web/src/state/info/constant.ts
@@ -1,5 +1,6 @@
 import { BLOCKS_CLIENT, BLOCKS_CLIENT_ETH, INFO_CLIENT, INFO_CLIENT_ETH } from 'config/constants/endpoints'
 import { infoClientETH, infoClient, infoStableSwapClient } from 'utils/graphql'
+import { GraphQLClient } from 'graphql-request'
 
 import { ChainId } from '@pancakeswap/sdk'
 import {
@@ -72,7 +73,7 @@ export const multiChainTokenWhiteList = {
   ETH: ETH_TOKEN_WHITELIST,
 }
 
-export const getMultiChainQueryEndPointWithStableSwap = (chainName: MultiChainName) => {
+export const getMultiChainQueryEndPointWithStableSwap = (chainName: MultiChainName): GraphQLClient => {
   const isStableSwap = checkIsStableSwap()
   if (isStableSwap) return infoStableSwapClient
   return multiChainQueryClient[chainName]
