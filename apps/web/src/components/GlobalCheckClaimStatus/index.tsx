019dafbd9f526e30c3cb7c58d3638946ace4cdbb
chefmomota <momota@pancakeswap.com>
2023-04-26 19:44:53 +0800

    feat: Update router

--- apps/web/src/components/GlobalCheckClaimStatus/index.tsx
+++ apps/web/src/components/GlobalCheckClaimStatus/index.tsx
@@ -8,9 +8,9 @@ import useCatchTxError from 'hooks/useCatchTxError'
 import useActiveWeb3React from 'hooks/useActiveWeb3React'
 import { useRouter } from 'next/router'
 import { useEffect, useRef, useState } from 'react'
-import v3WhitelistAddress from 'components/GlobalCheckClaimStatus/constants/v3WhitelistAddress.json'
-import v3ForSC from 'components/GlobalCheckClaimStatus/constants/v3ForSC.json'
-import v3MerkleProofs from 'components/GlobalCheckClaimStatus/constants/v3MerkleProofs.json'
+import v3WhitelistAddress from './constants/v3WhitelistAddress.json'
+import v3ForSC from './constants/v3ForSC.json'
+import v3MerkleProofs from './constants/v3MerkleProofs.json'
 import V3AirdropModal, { WhitelistType } from './V3AirdropModal'
 
 interface GlobalCheckClaimStatusProps {
