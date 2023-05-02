43d59e30952b33cacd4f633eafcc6d62261572dd
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2023-05-02 11:00:56 +0800

    test outputFileTracingIgnores

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -38,6 +38,7 @@ const config = {
   },
   experimental: {
     scrollRestoration: true,
+    outputFileTracingIgnores: ['**node_modules/.pnpm/**'],
   },
   transpilePackages: [
     '@pancakeswap/ui',
