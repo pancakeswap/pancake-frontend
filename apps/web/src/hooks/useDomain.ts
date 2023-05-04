58d3dfe863641282ddbf9fc10a5ffea932221cbd
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-05-04 14:14:16 +0800

    feat: ENS integration

--- apps/web/src/hooks/useDomain.ts
+++ apps/web/src/hooks/useDomain.ts
@@ -1,11 +1,25 @@
+import { ChainId } from '@pancakeswap/sdk'
 import { useSidNameForAddress } from 'hooks/useSid'
 import { useUnsNameForAddress } from 'hooks/useUns'
 import { useMemo } from 'react'
+import { useEnsName } from 'wagmi'
+import useActiveWeb3React from './useActiveWeb3React'
 
-export const useDomainNameForAddress = (address: string, fetchData = true) => {
+export const useDomainNameForAddress = (address: `0x${string}` | string, fetchData = true) => {
+  const { chainId } = useActiveWeb3React()
   const { sidName, isLoading: isSidLoading } = useSidNameForAddress(address, fetchData)
   const { unsName, isLoading: isUnsLoading } = useUnsNameForAddress(address, fetchData && !sidName && !isSidLoading)
+  const { data: ensName, isLoading: isEnsLoading } = useEnsName({
+    address: address as `0x${string}`,
+    chainId,
+    cacheTime: 3_600,
+    enabled: chainId !== ChainId.BSC && chainId !== ChainId.BSC_TESTNET,
+    scopeKey: address,
+  })
   return useMemo(() => {
-    return { domainName: sidName || unsName, isLoading: isSidLoading || (!sidName && isUnsLoading) }
-  }, [sidName, unsName, isSidLoading, isUnsLoading])
+    return {
+      domainName: ensName || sidName || unsName,
+      isLoading: isEnsLoading || (!ensName && isSidLoading) || (!sidName && isUnsLoading),
+    }
+  }, [sidName, unsName, isSidLoading, isUnsLoading, ensName, isEnsLoading])
 }
