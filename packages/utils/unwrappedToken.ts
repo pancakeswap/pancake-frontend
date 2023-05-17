2652f77d45fa5c0196b4a6813c04d47798c7fd0c
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2023-05-17 13:42:21 +0800

    fix pool

--- packages/utils/unwrappedToken.ts
+++ packages/utils/unwrappedToken.ts
@@ -2,6 +2,6 @@ import { Token, Currency } from '@pancakeswap/swap-sdk-core'
 import { WNATIVE, Native } from '@pancakeswap/sdk'
 
 export function unwrappedToken(token: Token): Currency {
-  if (token.equals(WNATIVE[token.chainId])) return Native.onChain(token.chainId)
+  if (token && token.equals(WNATIVE[token.chainId])) return Native.onChain(token.chainId)
   return token
 }
