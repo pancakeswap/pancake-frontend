5d9515b5e6eece06e30c5087b498f9da9bde6a69
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2023-05-17 17:45:24 +0800

    remove

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -50,10 +50,6 @@ const config = {
     outputFileTracingExcludes: {
       '*': ['**@swc+core*', '**/@esbuild**'],
     },
-    swcTraceProfiling: true,
-    turbotrace: {
-      logAll: true,
-    },
   },
   transpilePackages: [
     '@pancakeswap/ui',
