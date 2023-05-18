69c90bea597793a63ae8f60dbf973e6149df5a9d
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2023-05-18 20:05:22 +0800

    use public node for wait transaction

---
+++ apps/web/src/hooks/usePublicNodeWaitForTransaction.ts
@@ -0,0 +1,47 @@
+import { ChainId } from '@gelatonetwork/limit-orders-lib'
+import { CHAINS } from 'config/chains'
+import { TransactionReceipt, createPublicClient, http } from 'viem'
+import { PublicClient } from 'wagmi'
+import { useCallback } from 'react'
+import { WaitForTransactionArgs, waitForTransaction } from 'wagmi/actions'
+import { useActiveChainId } from './useActiveChainId'
+
+const viemClientsPublicNodes = CHAINS.reduce((prev, cur) => {
+  return {
+    ...prev,
+    [cur.id]: createPublicClient({
+      chain: cur,
+      transport: http(cur.rpcUrls.public.http[0], {
+        timeout: 15_000,
+      }),
+      batch: {
+        multicall: {
+          batchSize: 1_024 * 10,
+        },
+      },
+    }),
+  }
+}, {} as Record<ChainId, PublicClient>)
+
+export function usePublicNodeWaitForTransaction() {
+  const { chainId } = useActiveChainId()
+
+  const waitForTransaction_ = useCallback(
+    async (opts: WaitForTransactionArgs): Promise<TransactionReceipt> => {
+      let receipt
+      // our custom node might be late to sync up
+      if (viemClientsPublicNodes[chainId]) {
+        receipt = await viemClientsPublicNodes[chainId].waitForTransactionReceipt(opts)
+      } else {
+        receipt = await waitForTransaction(opts)
+      }
+
+      return receipt
+    },
+    [chainId],
+  )
+
+  return {
+    waitForTransaction: waitForTransaction_,
+  }
+}
