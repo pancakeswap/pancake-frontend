974c7773c96f1d452986f02e0a6272721177d83e
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2023-05-18 14:54:11 +0800

    force bsc on cake emission

--- apps/web/src/views/Home/hooks/useCakeEmissionPerBlock.tsx
+++ apps/web/src/views/Home/hooks/useCakeEmissionPerBlock.tsx
@@ -1,3 +1,4 @@
+import { ChainId } from '@pancakeswap/sdk'
 import BigNumber from 'bignumber.js'
 import { masterChefV2ABI } from 'config/abi/masterchefV2'
 import { getMasterChefV2Address } from 'utils/addressHelpers'
@@ -11,6 +12,7 @@ export const useCakeEmissionPerBlock = (inView?: boolean) => {
   const { data: emissionsPerBlock } = useContractRead({
     abi: masterChefV2ABI,
     address: masterChefAddress,
+    chainId: ChainId.BSC,
     functionName: 'cakePerBlockToBurn',
     enabled: inView,
     select: (d) => {
