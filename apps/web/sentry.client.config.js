ad2874bd9a450de0918b0fafa08362d5e0186c03
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2023-05-19 01:08:05 +0800

    handle rpc error

--- apps/web/sentry.client.config.js
+++ apps/web/sentry.client.config.js
@@ -3,16 +3,35 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import { init, GlobalHandlers, Breadcrumbs, Dedupe } from '@sentry/react'
+import { UserRejectedRequestError, UnknownRpcError } from 'viem'
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 
 const isUserRejected = (err) => {
-  // provider user rejected error code
-  return (
-    typeof err === 'object' &&
-    (('code' in err && (err.code === 4001 || err.code === 'ACTION_REJECTED')) ||
-      ('cause' in err && 'code' in err.cause && err.cause.code === 4001))
-  )
+  if (err instanceof UserRejectedRequestError) {
+    return true
+  }
+  if (err instanceof UnknownRpcError) {
+    // fallback for some wallets that don't follow EIP 1193, trust, safe
+    if (err.details.includes('cancel')) {
+      return true
+    }
+  }
+
+  // fallback for raw rpc error code
+  if (typeof err === 'object') {
+    if (
+      ('code' in err && (err.code === 4001 || err.code === 'ACTION_REJECTED')) ||
+      ('cause' in err && 'code' in err.cause && err.cause.code === 4001)
+    ) {
+      return true
+    }
+
+    if ('cause' in err) {
+      return isUserRejected(err.cause)
+    }
+  }
+  return false
 }
 
 const ENV = process.env.VERCEL_ENV || process.env.NODE_ENV
