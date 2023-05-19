2aa11905bd572c4a89c89a8cd6662831a337e92e
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-05-19 18:10:56 +0800

    chore: remove unused import

--- apps/web/src/views/Home/components/Banners/PancakeProtectorBanner.tsx
+++ apps/web/src/views/Home/components/Banners/PancakeProtectorBanner.tsx
@@ -1,5 +1,5 @@
 import { useTranslation } from '@pancakeswap/localization'
-import { ArrowForwardIcon, Box, Button, Flex, Link, useMatchBreakpoints, Logo } from '@pancakeswap/uikit'
+import { ArrowForwardIcon, Box, Button, Flex, Link, useMatchBreakpoints } from '@pancakeswap/uikit'
 import { ASSET_CDN } from 'config/constants/endpoints'
 import Image from 'next/legacy/image'
 import { memo } from 'react'
