9e159172ffd45b374769ea3b9d3c5407d02dd15b
chefmomota <momota@pancakeswap.com>
2023-05-19 16:07:05 +0800

    feat: Update chainId

--- apps/web/src/views/AffiliatesProgram/hooks/useShowWarningMessage.tsx
+++ apps/web/src/views/AffiliatesProgram/hooks/useShowWarningMessage.tsx
@@ -1,10 +1,11 @@
 import { useMemo } from 'react'
-import { ChainId } from '@pancakeswap/sdk'
+import { useActiveChainId } from 'hooks/useActiveChainId'
 import { V3_SUBGRAPH_URLS } from 'config/constants/endpoints'
 import useSubgraphHealth, { SubgraphStatus } from 'hooks/useSubgraphHealth'
 
 const useShowWarningMessage = () => {
-  const subgraphName = V3_SUBGRAPH_URLS[ChainId.BSC]?.replace('https://api.thegraph.com/subgraphs/name/', '') || ''
+  const { chainId } = useActiveChainId()
+  const subgraphName = V3_SUBGRAPH_URLS[chainId]?.replace('https://api.thegraph.com/subgraphs/name/', '') || ''
   const { status } = useSubgraphHealth(subgraphName)
 
   return useMemo(() => {
