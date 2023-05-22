7dd3f295231d04d2209d6049e1dd64c4f7e91b0f
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2023-05-22 23:39:25 +0800

    config update

--- apps/web/src/utils/viem.server.ts
+++ apps/web/src/utils/viem.server.ts
@@ -2,14 +2,20 @@ import { ChainId } from '@pancakeswap/sdk'
 import { OnChainProvider } from '@pancakeswap/smart-router/evm'
 import { CHAINS } from 'config/chains'
 import { SERVER_NODES } from 'config/nodes'
-import { createPublicClient, http, PublicClient } from 'viem'
+import { createPublicClient, fallback, http, PublicClient } from 'viem'
 
 export const viemServerClients = CHAINS.reduce((prev, cur) => {
   return {
     ...prev,
     [cur.id]: createPublicClient({
       chain: cur,
-      transport: http(SERVER_NODES[cur.id]),
+      transport: fallback(
+        SERVER_NODES[cur.id].map((url) =>
+          http(url, {
+            timeout: 15_000,
+          }),
+        ),
+      ),
       batch: {
         multicall: {
           batchSize: 1024 * 200,
@@ -19,7 +25,6 @@ export const viemServerClients = CHAINS.reduce((prev, cur) => {
   }
 }, {} as Record<ChainId, PublicClient>)
 
-// @ts-ignore
 export const getViemClients: OnChainProvider = ({ chainId }: { chainId?: ChainId }) => {
   return viemServerClients[chainId]
 }
