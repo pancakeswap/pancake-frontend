758b6a3698c062cdf890a1ea8ef28248a032f200
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2023-05-22 23:39:25 +0800

    config update

--- apps/web/src/config/nodes.ts
+++ apps/web/src/config/nodes.ts
@@ -2,11 +2,19 @@ import { ChainId } from '@pancakeswap/sdk'
 import { getNodeRealUrlV2 } from 'utils/nodeReal'
 
 export const SERVER_NODES = {
-  [ChainId.BSC]: process.env.NEXT_PUBLIC_NODE_PRODUCTION,
-  [ChainId.BSC_TESTNET]: 'https://data-seed-prebsc-1-s1.binance.org:8545',
-  [ChainId.ETHEREUM]: getNodeRealUrlV2(ChainId.ETHEREUM, process.env.SERVER_NODE_REAL_API_ETH),
-  [ChainId.GOERLI]: getNodeRealUrlV2(ChainId.GOERLI, process.env.SERVER_NODE_REAL_API_GOERLI),
-} satisfies Record<ChainId, string>
+  [ChainId.BSC]: [
+    process.env.NEXT_PUBLIC_NODE_PRODUCTION,
+    'https://bsc-dataseed1.defibit.io',
+    'https://bsc-dataseed1.binance.org',
+  ].filter(Boolean),
+  [ChainId.BSC_TESTNET]: ['https://data-seed-prebsc-1-s1.binance.org:8545'],
+  [ChainId.ETHEREUM]: [
+    getNodeRealUrlV2(ChainId.ETHEREUM, process.env.SERVER_NODE_REAL_API_ETH),
+    'https://eth.llamarpc.com',
+    'https://cloudflare-eth.com',
+  ],
+  [ChainId.GOERLI]: [getNodeRealUrlV2(ChainId.GOERLI, process.env.SERVER_NODE_REAL_API_GOERLI)],
+} satisfies Record<ChainId, string[]>
 
 export const PUBLIC_NODES = {
   [ChainId.BSC]: [
@@ -15,7 +23,11 @@ export const PUBLIC_NODES = {
     'https://bsc-dataseed1.defibit.io',
     'https://bsc-dataseed1.binance.org',
   ].filter(Boolean),
-  [ChainId.BSC_TESTNET]: 'https://data-seed-prebsc-1-s1.binance.org:8545',
-  [ChainId.ETHEREUM]: getNodeRealUrlV2(ChainId.ETHEREUM, process.env.NEXT_PUBLIC_NODE_REAL_API_ETH),
-  [ChainId.GOERLI]: getNodeRealUrlV2(ChainId.GOERLI, process.env.NEXT_PUBLIC_NODE_REAL_API_GOERLI),
-} satisfies Record<ChainId, string | string[]>
+  [ChainId.BSC_TESTNET]: ['https://data-seed-prebsc-1-s1.binance.org:8545'],
+  [ChainId.ETHEREUM]: [
+    getNodeRealUrlV2(ChainId.ETHEREUM, process.env.NEXT_PUBLIC_NODE_REAL_API_ETH),
+    'https://eth.llamarpc.com',
+    'https://cloudflare-eth.com',
+  ],
+  [ChainId.GOERLI]: [getNodeRealUrlV2(ChainId.GOERLI, process.env.NEXT_PUBLIC_NODE_REAL_API_GOERLI)],
+} satisfies Record<ChainId, string[]>
