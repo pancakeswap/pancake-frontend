ab6bfd4e7aa03982611685235a4d35de999092b5
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-05-24 10:27:44 +0800

    chore: update APR tooltip

--- apps/web/src/views/Farms/components/FarmCard/V3/FarmV3ApyButton.tsx
+++ apps/web/src/views/Farms/components/FarmCard/V3/FarmV3ApyButton.tsx
@@ -214,7 +214,8 @@ function FarmV3ApyButton_({ farm, existingPosition, isPositionStaked, tokenId }:
       </Text>
       <ul>
         <li>
-          {t('Farm APR')}: <b>{positionCakeAprDisplay}%</b>
+          {t('Farm APR')}:
+          <b>{isBoosted ? parseFloat(positionCakeAprDisplay) * userMultiplier : positionCakeAprDisplay}%</b>
         </li>
         <li>
           {t('LP Fee APR')}: <b>{lpAprDisplay}%</b>
