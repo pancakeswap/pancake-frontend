e2b08931e50cbad00eb035d58ef0ab56929bb654
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-05-26 17:39:27 +0800

    chore: update boostMultiplier logic

--- apps/web/src/views/Farms/components/YieldBooster/components/bCakeV3/CardView.tsx
+++ apps/web/src/views/Farms/components/YieldBooster/components/bCakeV3/CardView.tsx
@@ -1,7 +1,8 @@
 import { useTranslation } from '@pancakeswap/localization'
-import { Box, Button, Flex, AutoRenewIcon } from '@pancakeswap/uikit'
+import { AutoRenewIcon, Box, Button, Flex } from '@pancakeswap/uikit'
 import useTheme from 'hooks/useTheme'
-import { useUserBoostedMultiplier } from '../../hooks/bCakeV3/useBCakeV3Info'
+import { useCallback } from 'react'
+import { useUserBoostedMultiplier, useUserPositionInfo } from '../../hooks/bCakeV3/useBCakeV3Info'
 import { useBoosterFarmV3Handlers } from '../../hooks/bCakeV3/useBoostBcakeV3'
 import { BoostStatus, useBoostStatus } from '../../hooks/bCakeV3/useBoostStatus'
 import { StatusView } from './StatusView'
@@ -13,13 +14,27 @@ export const BCakeV3CardView: React.FC<{
 }> = ({ tokenId, pid, isFarmStaking }) => {
   const { t } = useTranslation()
   const { status: boostStatus, updateStatus } = useBoostStatus(pid, tokenId)
-  const { activate, deactivate, isConfirming } = useBoosterFarmV3Handlers(tokenId, updateStatus)
+  const {
+    data: { boostMultiplier },
+    updateUserPositionInfo,
+  } = useUserPositionInfo(tokenId)
 
-  const boostedMultiplier = useUserBoostedMultiplier(tokenId)
+  const onDone = useCallback(() => {
+    updateStatus()
+    updateUserPositionInfo()
+  }, [updateStatus, updateUserPositionInfo])
+
+  const { activate, deactivate, isConfirming } = useBoosterFarmV3Handlers(tokenId, onDone)
+
+  const multiplierBeforeBoosted = useUserBoostedMultiplier(tokenId)
   const { theme } = useTheme()
   return (
     <Flex width="100%" alignItems="center" justifyContent="space-between">
-      <StatusView status={boostStatus} boostedMultiplier={boostedMultiplier} isFarmStaking={isFarmStaking} />
+      <StatusView
+        status={boostStatus}
+        boostedMultiplier={boostStatus === BoostStatus.farmCanBoostButNot ? boostMultiplier : multiplierBeforeBoosted}
+        isFarmStaking={isFarmStaking}
+      />
       <Box>
         {boostStatus === BoostStatus.farmCanBoostButNot && isFarmStaking && (
           <Button
