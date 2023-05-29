8d3538fc257f3d874daeb48a802deb9e31686d38
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-05-30 00:55:54 +0800

    fix: build type issue

--- apps/web/src/views/Predictions/components/ChainlinkChart.tsx
+++ apps/web/src/views/Predictions/components/ChainlinkChart.tsx
@@ -47,7 +47,7 @@ function useChainlinkRoundDataSet() {
           abi: chainlinkOracleABI,
           address: chainlinkOracleAddress,
           functionName: 'getRoundData',
-          args: [(lastRound.data ?? 0n) - BigInt(i)] as const,
+          args: [(lastRound.data ?? 0n) - BigInt(i)],
         } as const),
     ),
     enabled: !!lastRound.data,
