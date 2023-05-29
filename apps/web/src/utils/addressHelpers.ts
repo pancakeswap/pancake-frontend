lnavarroa/simply-auto-swap

chore: BCake v3 contract initialize

apps/web/src/utils/addressHelpers.ts
@@ -98,6 +98,10 @@ export const getBCakeFarmBoosterAddress = () => {
   return getAddress(addresses.bCakeFarmBooster)
 }
 
+export const getBCakeFarmBoosterV3Address = () => {
+  return getAddress(addresses.bCakeFarmBoosterV3)
+}
+
 export const getBCakeFarmBoosterProxyFactoryAddress = () => {
   return getAddress(addresses.bCakeFarmBoosterProxyFactory)
 }

pancakeswap/pancake-frontend

apps/web/src/utils/addressHelpers.ts
@@ -92,6 +92,10 @@ export const getBCakeFarmBoosterAddress = () => {
   return getAddressFromMap(addresses.bCakeFarmBooster)
 }
 
+export const getBCakeFarmBoosterV3Address = () => {
+  return getAddressFromMap(addresses.bCakeFarmBoosterV3)
+}
+
 export const getBCakeFarmBoosterProxyFactoryAddress = () => {
   return getAddressFromMap(addresses.bCakeFarmBoosterProxyFactory)
 }