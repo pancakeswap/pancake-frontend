9a9f6a975e8bacdbce7834e459976955d50f228d
chefjackson <116779127+chefjackson@users.noreply.github.com>
2023-06-02 10:32:58 +0800

    feat: add tvl storage

--- apis/routing/src/pools.ts
+++ apis/routing/src/pools.ts
@@ -1,43 +1,53 @@
-import type { Router } from 'itty-router'
+import type { Router, Request, RouteHandler } from 'itty-router'
 import { missing } from 'itty-router-extras'
 
 export const getPoolsObjectName = (chainId: string | number) => `v3_pools_${chainId}_latest`
 
+export const getPoolsTvlObjectName = (chainId: string | number) => `v3_pools_tvl_${chainId}_latest`
+
+const respondWithCache = async (req: Request, event: any, handler: RouteHandler<Request>) => {
+  const url = new URL(req.url)
+  const cacheKey = new Request(url.toString(), req)
+  const cache = caches.default
+  const response = await cache.match(cacheKey)
+  if (response) {
+    return response
+  }
+
+  const res = await handler(req, event)
+  event.waitUntil(cache.put(cacheKey, res.clone()))
+
+  return res
+}
+
 export function poolsRoute(router: Router) {
-  router.get('/v0/v3-pools/:chainId', async (req, event) => {
-    const url = new URL(req.url)
-    const cacheKey = new Request(url.toString(), req)
-    const cache = caches.default
-    const response = await cache.match(cacheKey)
-    if (response) {
-      console.log(`Cache hit for: ${req.url}.`)
-      return response
-    }
-    console.log(`Response for request url: ${req.url} not present in cache. Fetching and caching request.`)
+  const createRoute = (route: string, getObjectName: (chainId: string) => string) => {
+    router.get(route, async (req, event) => {
+      return respondWithCache(req, event, async () => {
+        const chainId = req.params?.chainId
+        const objectName = chainId && getObjectName(chainId)
+        if (!objectName) {
+          return missing('Not Found')
+        }
 
-    const chainId = req.params?.chainId
-    const objectName = chainId && getPoolsObjectName(chainId)
-    if (!objectName) {
-      return missing('Not Found')
-    }
+        const object = await SUBGRAPH_POOLS.get(objectName)
+        if (object === null) {
+          return missing('Not Found')
+        }
 
-    const object = await SUBGRAPH_POOLS.get(objectName)
-    if (object === null) {
-      return missing('Not Found')
-    }
+        const headers = new Headers()
+        object.writeHttpMetadata(headers)
+        headers.set('etag', object.httpEtag)
 
-    const headers = new Headers()
-    object.writeHttpMetadata(headers)
-    headers.set('etag', object.httpEtag)
+        headers.append('Cache-Control', 's-maxage=86400')
 
-    headers.append('Cache-Control', 's-maxage=86400')
-
-    const res = new Response(object.body, {
-      headers,
+        return new Response(object.body, {
+          headers,
+        })
+      })
     })
+  }
 
-    event.waitUntil(cache.put(cacheKey, res.clone()))
-
-    return res
-  })
+  createRoute('/v0/v3-pools/:chainId', getPoolsObjectName)
+  createRoute('/v0/v3-pools-tvl/:chainId', getPoolsTvlObjectName)
 }
--- apis/routing/src/subgraphPoolBackup.ts
+++ apis/routing/src/subgraphPoolBackup.ts
@@ -2,7 +2,7 @@ import { SmartRouter } from '@pancakeswap/smart-router/evm'
 
 import { v3SubgraphProvider } from './provider'
 import { SUPPORTED_CHAINS } from './constants'
-import { getPoolsObjectName } from './pools'
+import { getPoolsObjectName, getPoolsTvlObjectName } from './pools'
 
 // eslint-disable-next-line consistent-return
 async function handleScheduled(event: ScheduledEvent) {
@@ -10,19 +10,29 @@ async function handleScheduled(event: ScheduledEvent) {
     case '0 0 * * *': {
       for (const chainId of SUPPORTED_CHAINS) {
         try {
-          const objectName = getPoolsObjectName(chainId)
           // eslint-disable-next-line no-await-in-loop
           const pools = await SmartRouter.getAllV3PoolsFromSubgraph({ chainId, provider: v3SubgraphProvider })
           const serializedPools = pools.map((p) => ({
             ...SmartRouter.Transformer.serializePool(p),
             tvlUSD: p.tvlUSD.toString(),
           }))
+          const poolsTvl = pools.map((p) => ({
+            address: p.address,
+            tvlUSD: p.tvlUSD.toString(),
+          }))
           // eslint-disable-next-line no-await-in-loop
-          await SUBGRAPH_POOLS.put(objectName, JSON.stringify(serializedPools), {
-            httpMetadata: {
-              contentType: 'application/json',
-            },
-          })
+          await Promise.all([
+            SUBGRAPH_POOLS.put(getPoolsObjectName(chainId), JSON.stringify(serializedPools), {
+              httpMetadata: {
+                contentType: 'application/json',
+              },
+            }),
+            SUBGRAPH_POOLS.put(getPoolsTvlObjectName(chainId), JSON.stringify(poolsTvl), {
+              httpMetadata: {
+                contentType: 'application/json',
+              },
+            }),
+          ])
         } catch (e) {
           console.error(e)
         }
