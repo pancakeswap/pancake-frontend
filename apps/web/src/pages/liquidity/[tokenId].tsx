adf93764740bbd647b9893fcf56dd41ae37de0da
memoyil <2213635+memoyil@users.noreply.github.com>
2023-06-07 00:49:10 +0200

    fix: (liquidity): Farm hint shown when another account active

--- apps/web/src/pages/liquidity/[tokenId].tsx
+++ apps/web/src/pages/liquidity/[tokenId].tsx
@@ -300,7 +300,7 @@ export default function PoolPage() {
       data: calldata,
       value: hexToBigInt(value),
       account,
-      chain: signer.chain,
+      chain: signer?.chain,
     }
 
     getViemClients({ chainId })
@@ -444,7 +444,7 @@ export default function PoolPage() {
   }
 
   const farmingTips =
-    hasActiveFarm && !isStakedInMCv3 ? (
+    ownsNFT && hasActiveFarm && !isStakedInMCv3 ? (
       <Message variant="primary" mb="2em">
         <Box>
           <Text display="inline" bold mr="0.25em">{`${currencyQuote?.symbol}-${currencyBase?.symbol}`}</Text>
