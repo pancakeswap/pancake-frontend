30308a02bf42a653311902539aab6ef3a8123f08
memoyil <2213635+memoyil@users.noreply.github.com>
2023-06-07 23:23:25 +0200

    fix: Lint

--- apps/web/src/hooks/useTransactionDeadline.ts
+++ apps/web/src/hooks/useTransactionDeadline.ts
@@ -1,11 +1,9 @@
 import { useMemo } from 'react'
-import { useSelector } from 'react-redux'
+import { useUserState } from 'state/user/reducer'
 
 import { L2_CHAIN_IDS } from 'config/chains'
 import { L2_DEADLINE_FROM_NOW } from 'config/constants'
 
-import { AppState } from '../state'
-import { useUserState } from 'state/user/reducer'
 import useCurrentBlockTimestamp from './useCurrentBlockTimestamp'
 import { useActiveChainId } from './useActiveChainId'
 
