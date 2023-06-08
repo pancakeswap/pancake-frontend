a915e5ea798b605926106fd48b1d1c90adb23612
chefilip <philip@pancakeswap.com>
2023-06-08 09:07:51 +0700

    chore: Update text

--- packages/uikit/src/widgets/Menu/components/footerConfig.ts
+++ packages/uikit/src/widgets/Menu/components/footerConfig.ts
@@ -27,7 +27,7 @@ export const footerLinks: (t: ContextApi["t"]) => FooterLinkType[] = (t) => [
         href: "https://v2litepaper.pancakeswap.finance/",
       },
       {
-        label: t("CAKE Emission Project"),
+        label: t("CAKE Emission Projector"),
         href: "https://analytics.pancakeswap.finance/",
       },
       {
