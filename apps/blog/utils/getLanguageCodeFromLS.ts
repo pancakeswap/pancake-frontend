baacfa9ac4954b87867efd7e080d30898dbe8986
chefmomota <momota@pancakeswap.com>
2023-06-14 12:00:03 +0800

    chore: Update

--- apps/blog/utils/getLanguageCodeFromLS.ts
+++ apps/blog/utils/getLanguageCodeFromLS.ts
@@ -3,7 +3,6 @@ export const LS_KEY = 'blog_pancakeswap_language'
 export const getLanguageCodeFromLS = () => {
   try {
     const codeFromStorage = localStorage.getItem(LS_KEY)
-
     return codeFromStorage || 'en'
   } catch {
     return 'en'
