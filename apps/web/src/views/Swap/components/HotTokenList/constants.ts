2aa0e41106dfdf163acfe11159372b93891928a9
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-06-19 05:52:00 +0800

    chore: add promotion token

---
+++ apps/web/src/views/Swap/components/HotTokenList/constants.ts
@@ -0,0 +1,15 @@
+import { TokenHighlightData } from './types'
+
+export const BSC_PROMOTION_TOKENS: TokenHighlightData[] = [
+  {
+    name: 'Arbitrum',
+    symbol: 'ARB',
+    address: '0xa050FFb3eEb8200eEB7F61ce34FF644420FD3522',
+    tvlUSD: 0,
+    tvlUSDChange: 0,
+    priceUSD: 0,
+    priceUSDChange: 0,
+    volumeUSD: 0,
+    decimals: 18,
+  },
+]
