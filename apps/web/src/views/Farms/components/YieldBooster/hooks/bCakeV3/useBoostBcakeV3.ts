de38e12d9da32afa7367592211c794131b5aa25c
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-06-21 00:27:28 +0800

    chore: apply the new gas limit

--- apps/web/src/views/Farms/components/YieldBooster/hooks/bCakeV3/useBoostBcakeV3.ts
+++ apps/web/src/views/Farms/components/YieldBooster/hooks/bCakeV3/useBoostBcakeV3.ts
@@ -1,4 +1,4 @@
-import { BOOSTED_FARM_GAS_LIMIT } from 'config'
+import { BOOSTED_FARM_V3_GAS_LIMIT } from 'config'
 import { useCallWithGasPrice } from 'hooks/useCallWithGasPrice'
 import useCatchTxError from 'hooks/useCatchTxError'
 import { useBCakeFarmBoosterV3Contract } from 'hooks/useContract'
@@ -11,7 +11,7 @@ export const useBoosterFarmV3Handlers = (tokenId: string, onDone: () => void) =>
 
   const activate = useCallback(async () => {
     const receipt = await fetchWithCatchTxError(() => {
-      return callWithGasPrice(farmBoosterV3Contract, 'activate', [tokenId], { gas: BOOSTED_FARM_GAS_LIMIT })
+      return callWithGasPrice(farmBoosterV3Contract, 'activate', [tokenId], { gas: BOOSTED_FARM_V3_GAS_LIMIT })
     })
 
     if (receipt?.status && onDone) {
@@ -21,7 +21,7 @@ export const useBoosterFarmV3Handlers = (tokenId: string, onDone: () => void) =>
 
   const deactivate = useCallback(async () => {
     const receipt = await fetchWithCatchTxError(() => {
-      return callWithGasPrice(farmBoosterV3Contract, 'deactive', [tokenId], { gas: BOOSTED_FARM_GAS_LIMIT })
+      return callWithGasPrice(farmBoosterV3Contract, 'deactive', [tokenId], { gas: BOOSTED_FARM_V3_GAS_LIMIT })
     })
 
     if (receipt?.status && onDone) {
