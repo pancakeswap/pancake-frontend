9569910060deb7bd4a602a08e7bac1ed0b7f7946
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-06-20 14:07:34 +0800

    chore: remove fork mainNet RPC

--- apps/web/src/config/nodes.ts
+++ apps/web/src/config/nodes.ts
@@ -3,10 +3,9 @@ import { getNodeRealUrlV2 } from 'utils/nodeReal'
 
 export const SERVER_NODES = {
   [ChainId.BSC]: [
-    'https://devnet.pancakeswap.ai', // TODO: remove this after test
-    // process.env.NEXT_PUBLIC_NODE_PRODUCTION,
-    // 'https://bsc-dataseed1.defibit.io',
-    // 'https://bsc-dataseed1.binance.org',
+    process.env.NEXT_PUBLIC_NODE_PRODUCTION,
+    'https://bsc-dataseed1.defibit.io',
+    'https://bsc-dataseed1.binance.org',
   ].filter(Boolean),
   [ChainId.BSC_TESTNET]: ['https://data-seed-prebsc-1-s1.binance.org:8545'],
   [ChainId.ETHEREUM]: [
@@ -22,11 +21,10 @@ export const SERVER_NODES = {
 
 export const PUBLIC_NODES = {
   [ChainId.BSC]: [
-    'https://devnet.pancakeswap.ai', // TODO: remove this after test
-    // process.env.NEXT_PUBLIC_NODE_PRODUCTION,
-    // getNodeRealUrlV2(ChainId.BSC, process.env.NEXT_PUBLIC_NODE_REAL_API_ETH),
-    // 'https://bsc-dataseed1.defibit.io',
-    // 'https://bsc-dataseed1.binance.org',
+    process.env.NEXT_PUBLIC_NODE_PRODUCTION,
+    getNodeRealUrlV2(ChainId.BSC, process.env.NEXT_PUBLIC_NODE_REAL_API_ETH),
+    'https://bsc-dataseed1.defibit.io',
+    'https://bsc-dataseed1.binance.org',
   ].filter(Boolean),
   [ChainId.BSC_TESTNET]: ['https://data-seed-prebsc-1-s1.binance.org:8545'],
   [ChainId.ETHEREUM]: [
