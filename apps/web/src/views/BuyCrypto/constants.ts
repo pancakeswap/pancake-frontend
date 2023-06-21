971a08bde40eacd7010273fc9a9ba829d971be62
BingBong <bingbong@pancakseswap.com>
2023-06-21 23:24:14 +0100

    fiat curr

--- apps/web/src/views/BuyCrypto/constants.ts
+++ apps/web/src/views/BuyCrypto/constants.ts
@@ -1,19 +1,5 @@
 export const MOONPAY_UNSUPPORTED_CURRENCY_CODES = ['BNB', 'USDT', 'BUSD']
-export const whiteListedFiatCurrencies = [
-  'USD',
-  'EUR',
-  'GBP',
-  'HKD',
-  'CAD',
-  'AUD',
-  'BRL',
-  'JPY',
-  'KRW',
-  'TWD',
-  'IDR',
-  'VND',
-  'SGD',
-]
+export const whiteListedFiatCurrencies = ['USD', 'EUR', 'GBP', 'HKD', 'CAD', 'AUD', 'BRL', 'JPY', 'KRW', 'VND']
 export const mercuryoWhitelist = ['BNB', 'BUSD']
 
 export const SUPPORTED_MERCURYO_FIAT_CURRENCIES = ['USD', 'EUR', 'GBP', 'HKD', 'CAD', 'AUD', 'BRL', 'JPY', 'KRW', 'VND']
