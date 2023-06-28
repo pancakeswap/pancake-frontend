3c71c40cb6ed3c46cf443aca00d172f6802c5ee4
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-06-28 23:26:10 +0900

    feat: add AMA text for top farm bid

--- apps/web/src/views/FarmAuction/constants.ts
+++ apps/web/src/views/FarmAuction/constants.ts
@@ -6,7 +6,7 @@ export const HARD_CODE_TOP_THREE_AUCTION_DATA = [
   [1, 2],
   [1, 1],
 ]
-// id: [feeTier, multiplier][]
+// id: [feeTier, multiplier, AMACakeUnderLine][]
 export const HARD_CODE_V3_AUCTION_DATA_BY_ID = {
   34: [
     [1, 6],
@@ -14,7 +14,7 @@ export const HARD_CODE_V3_AUCTION_DATA_BY_ID = {
     [1, 1],
   ],
   35: [
-    [0.25, 6],
+    [0.25, 6, 10000],
     [0.25, 2],
     [0.25, 1],
   ],
