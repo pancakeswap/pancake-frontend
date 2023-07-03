9ac51e66e27a9dc6b2634755da6ce103d9ad1096
chefjackson <116779127+chefjackson@users.noreply.github.com>
2023-07-03 17:53:40 +0800

    chore: add test to eth tokens

--- apps/web/src/config/__tests__/tokens.test.ts
+++ apps/web/src/config/__tests__/tokens.test.ts
@@ -1,5 +1,5 @@
-import { ChainId, Token } from '@pancakeswap/sdk'
-import { bscTokens } from '@pancakeswap/tokens'
+import { Token } from '@pancakeswap/sdk'
+import { bscTokens, ethereumTokens } from '@pancakeswap/tokens'
 import { erc20ABI } from 'wagmi'
 import map from 'lodash/map'
 import slice from 'lodash/slice'
@@ -13,7 +13,7 @@ const whitelist = ['deprecated_tusd']
 // remove ONE because there are two tokens with the symbol ONE (Harmony ONE and BigONE)
 // remove HERO because there are two tokens with the symbol HERO (StepHero and Hero)
 // remove aBNBc because the token has been exploited
-const tokensToTest = omitBy(
+const bscTokensToTest = omitBy(
   bscTokens,
   (token) =>
     token.symbol.toLowerCase() === 'bnb' ||
@@ -22,8 +22,12 @@ const tokensToTest = omitBy(
     token.symbol.toLowerCase() === 'abnbc' ||
     token.symbol.toLowerCase() === 'hero',
 )
+const tokenListsToTest = [bscTokensToTest, ethereumTokens]
 
-const tokenTables: [string, Token][] = map(tokensToTest, (token, key) => [key, token])
+const tokenTables: [string, Token][] = tokenListsToTest.reduce(
+  (acc, cur) => [...acc, map(cur, (token, key) => [key, token])],
+  [],
+)
 
 describe.concurrent(
   'Config tokens',
@@ -31,8 +35,8 @@ describe.concurrent(
     it.each(slice(tokenTables, tokenTables.length - 50))(
       'Token %s has the correct key, symbol, and decimal',
       async (key: string, token: Token) => {
-        const bscClient = publicClient({ chainId: ChainId.BSC })
-        const [symbol, decimals] = await bscClient.multicall({
+        const client = publicClient({ chainId: token.chainId })
+        const [symbol, decimals] = await client.multicall({
           contracts: [
             {
               abi: erc20ABI,
