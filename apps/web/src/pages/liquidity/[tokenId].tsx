ebcfefa4e9963e6cde035cbbad44e81cd7441e0c
memoyil <2213635+memoyil@users.noreply.github.com>
2023-07-07 12:50:25 +0200

    fix: Build

--- apps/web/src/pages/liquidity/[tokenId].tsx
+++ apps/web/src/pages/liquidity/[tokenId].tsx
@@ -210,7 +210,7 @@ export default function PoolPage() {
     invert: manuallyInverted,
   })
 
-  const inverted = useMemo(() => (token1 && token1 ? base?.equals(token1) : undefined), [token1, base])
+  const inverted = useMemo(() => (token1 ? base?.equals(token1) : undefined), [token1, base])
   const currencyQuote = inverted ? currency0 : currency1
   const currencyBase = inverted ? currency1 : currency0
 
@@ -362,6 +362,7 @@ export default function PoolPage() {
     functionName: 'ownerOf',
     args: [tokenId],
   }).result
+  const ownsNFT = owner === account || positionDetails?.operator === account
 
   const feeValueUpper = inverted ? feeValue0 : feeValue1
   const feeValueLower = inverted ? feeValue1 : feeValue0
