d371934b957d997a1dbffef22c8e3c0495ce08c2
chefmomota <momota@pancakeswap.com>
2023-07-12 14:40:11 +0800

    feat: Rebase

--- apps/web/src/hooks/useScrollToHash.ts
+++ apps/web/src/hooks/useScrollToHash.ts
@@ -1,25 +1,26 @@
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
-import { useSearchParams } from 'next/navigation'
+import { getHashFromRouter } from 'utils/getHashFromRouter'
 
 const useScrollToHash = (isFetching: boolean, enabled: boolean) => {
   const router = useRouter()
-  const searchParams = useSearchParams()
   const [initialScrollDone, setInitialScrollDone] = useState(false)
 
   useEffect(() => {
     if (enabled && !isFetching && router.isReady && !initialScrollDone) {
-      const hashFromRouter = searchParams.get('to')
+      const hashFromRouter = getHashFromRouter(router)?.[0]
       if (hashFromRouter !== null && hashFromRouter !== '') {
         const elementToScroll = document.getElementById(hashFromRouter)
         if (elementToScroll && window) {
           const scrollAfter = setTimeout(() => {
-            window.scrollTo({
-              top: elementToScroll.offsetTop,
-            })
-            setInitialScrollDone(true)
-          }, 1000)
-
+            const elementToScrollNewPosition = document.getElementById(hashFromRouter)
+            if (elementToScrollNewPosition) {
+              window.scrollTo({
+                top: elementToScrollNewPosition.offsetTop,
+              })
+              setInitialScrollDone(true)
+            }
+          }, 100)
           return () => {
             clearTimeout(scrollAfter)
           }
@@ -27,7 +28,7 @@ const useScrollToHash = (isFetching: boolean, enabled: boolean) => {
       }
     }
     return undefined
-  }, [router, isFetching, initialScrollDone, enabled, searchParams])
+  }, [router, isFetching, initialScrollDone, enabled])
 }
 
 export default useScrollToHash
