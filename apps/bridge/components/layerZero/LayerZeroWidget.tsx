467d0f40a30a36c6c73ab700d50571257ec1501e
chefmomota <momota@pancakeswap.com>
2023-07-14 12:50:54 +0800

    feat: Update css

--- apps/bridge/components/layerZero/LayerZeroWidget.tsx
+++ apps/bridge/components/layerZero/LayerZeroWidget.tsx
@@ -13,10 +13,8 @@ declare global {
 
 export const LayerZeroWidget = ({ theme }: { theme: PancakeTheme }) => {
   useEffect(() => {
-    const themeText = 'light'
-    const themeColor = lightTheme
-    console.log(window.app)
-
+    const themeText = theme.isDark ? 'dark' : 'light'
+    const themeColor = theme.isDark ? darkTheme : lightTheme
     customElements.whenDefined('lz-bridge').then(() => {
       document.body.classList.add(themeText)
       document.querySelector('aptos-bridge-container').setTheme(themeColor)
@@ -42,20 +40,64 @@ export const LayerZeroWidget = ({ theme }: { theme: PancakeTheme }) => {
           background: transparent;
         }
 
-        // .light .css-1qbjrin-LzButton-SelectButtonRoot {
+        // .css-1qbjrin-LzButton-SelectButtonRoot {
         //   background-color: rgb(238, 234, 244) !important;
-        // }
-
-        // .light a {
         //   color: rgb(122, 110, 170) !important;
-        // }
-
-        // .light .css-1h2mwam {
-        //   border: solid 1px transparent;
         // }
 
         .bridge-tracker > div {
           padding: 24px;
+        }
+
+        .css-twekd7,
+        .css-iv85qm {
+          margin-top: 32px !important;
+        }
+
+        .aptos-bridge-container > div > .MuiContainer-root {
+          padding: 0 8px !important;
+        }
+
+        .dark [id^='radix-'] {
+          background-color: #27262c;
+        }
+
+        .light [id^='radix-'] {
+          background-color: #ffffff;
+        }
+
+        .css-11saint rect {
+          fill: #d9d9d9;
+        }
+
+        .css-1ay9vb9 .css-9k6lzc-LzButton,
+        .css-1ay9vb9 .css-1s9mcc4-LzButton {
+          border: 1px solid #1fc7d4;
+          border-radius: 8px;
+          color: #1fc7d4;
+          background: transparent;
+        }
+
+        .dark [data-radix-popper-content-wrapper] {
+          background-color: #ffffff;
+          border-radius: 18px;
+        }
+        .dark [data-radix-popper-content-wrapper] div {
+          color: #280d5f;
+        }
+        .dark [data-radix-popper-content-wrapper] svg {
+          fill: #ffffff;
+        }
+
+        .light [data-radix-popper-content-wrapper] {
+          background-color: black;
+          border-radius: 18px;
+        }
+        .light [data-radix-popper-content-wrapper] div {
+          color: #ffffff;
+        }
+        .light [data-radix-popper-content-wrapper] svg {
+          fill: black;
         }
       `}</style>
       {/* @ts-ignore */}
