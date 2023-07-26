0d8c4b2782460c9933e6f87bddb9aec90e2c95d5
chefmomota <momota@pancakeswap.com>
2023-07-26 13:55:55 +0800

    feat: Only in BSC

--- apps/web/src/views/Pools/hooks/useVCake.tsx
+++ apps/web/src/views/Pools/hooks/useVCake.tsx
@@ -1,4 +1,5 @@
 import useSWR from 'swr'
+import { ChainId } from '@pancakeswap/sdk'
 import useAccountActiveChain from 'hooks/useAccountActiveChain'
 import { useVCakeContract } from 'hooks/useContract'
 
@@ -11,15 +12,18 @@ const useVCake = (): UseVCake => {
   const { account, chainId } = useAccountActiveChain()
   const vCakeContract = useVCakeContract({ chainId })
 
-  const { data, mutate } = useSWR(account && chainId && ['/v-cake-initialization', account, chainId], async () => {
-    try {
-      const initialization = await vCakeContract.read.initialization([account])
-      return initialization
-    } catch (error) {
-      console.error('[ERROR] Fetching vCake initialization', error)
-      return null
-    }
-  })
+  const { data, mutate } = useSWR(
+    account && chainId === ChainId.BSC && ['/v-cake-initialization', account, chainId],
+    async () => {
+      try {
+        const initialization = await vCakeContract.read.initialization([account])
+        return initialization
+      } catch (error) {
+        console.error('[ERROR] Fetching vCake initialization', error)
+        return null
+      }
+    },
+  )
 
   return {
     isInitialization: data,
