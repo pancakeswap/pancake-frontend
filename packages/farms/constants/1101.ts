0ce2340803d7e6fe1daabeede8fe317e99ace9ed
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-07-26 13:00:23 +0800

    chore: list zkevm initial farm

--- packages/farms/constants/1101.ts
+++ packages/farms/constants/1101.ts
@@ -1,5 +1,34 @@
-// import { polygonZkEvmTokens } from '@pancakeswap/tokens'
-// import { FeeAmount } from '@pancakeswap/v3-sdk'
+import { polygonZkEvmTokens } from '@pancakeswap/tokens'
+import { FeeAmount } from '@pancakeswap/v3-sdk'
 import { defineFarmV3Configs } from '../src/defineFarmV3Configs'
 
-export const farmsV3 = defineFarmV3Configs([])
+export const farmsV3 = defineFarmV3Configs([
+  {
+    pid: 1,
+    lpAddress: '0xD43b9dCbB61e6ccFbCFef9f21e1BB5064F1CB33f',
+    token0: polygonZkEvmTokens.weth,
+    token1: polygonZkEvmTokens.usdc,
+    feeAmount: FeeAmount.LOW,
+  },
+  {
+    pid: 2,
+    lpAddress: '0x4A080D9488cE2C8258185d78852275D6d3c2820c',
+    token0: polygonZkEvmTokens.usdt,
+    token1: polygonZkEvmTokens.weth,
+    feeAmount: FeeAmount.LOW,
+  },
+  {
+    pid: 3,
+    lpAddress: '0x4A080D9488cE2C8258185d78852275D6d3c2820c',
+    token0: polygonZkEvmTokens.usdt,
+    token1: polygonZkEvmTokens.usdc,
+    feeAmount: FeeAmount.LOWEST,
+  },
+  {
+    pid: 4,
+    lpAddress: '0xaE30fcdEE41dC9eC265e841D8185d055B87d1B7a',
+    token0: polygonZkEvmTokens.weth,
+    token1: polygonZkEvmTokens.matic,
+    feeAmount: FeeAmount.MEDIUM,
+  },
+])
--- packages/tokens/src/1101.ts
+++ packages/tokens/src/1101.ts
@@ -1,4 +1,4 @@
-import { ChainId, WETH9 } from '@pancakeswap/sdk'
+import { ChainId, WETH9, ERC20Token } from '@pancakeswap/sdk'
 import { USDC, USDT, CAKE } from './common'
 
 export const polygonZkEvmTokens = {
@@ -6,4 +6,12 @@ export const polygonZkEvmTokens = {
   usdc: USDC[ChainId.POLYGON_ZKEVM],
   usdt: USDT[ChainId.POLYGON_ZKEVM],
   cake: CAKE[ChainId.POLYGON_ZKEVM],
+  matic: new ERC20Token(
+    ChainId.POLYGON_ZKEVM,
+    '0xa2036f0538221a77A3937F1379699f44945018d0',
+    18,
+    'MATIC',
+    'Matic Token',
+    'https://polygon.technology/polygon-zkevm',
+  ),
 }
