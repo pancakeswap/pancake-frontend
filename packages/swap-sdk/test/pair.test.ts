760a990233575567f21233eae33f40aa238749d9
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2023-07-28 11:47:00 +0800

    chore: zksync v2 factory

--- packages/swap-sdk/test/pair.test.ts
+++ packages/swap-sdk/test/pair.test.ts
@@ -28,6 +28,10 @@ describe('Pair', () => {
       const WETH = new Token(ChainId.ZKSYNC_TESTNET, '0x02968DB286f24cB18bB5b24903eC8eBFAcf591C0', 18, 'WETH', 'WETH')
       const USDC_ = new Token(ChainId.ZKSYNC_TESTNET, '0x0faF6df7054946141266420b43783387A78d82A9', 18, 'USDC', 'USDC')
       expect(Pair.getAddress(USDC_, WETH)).toEqual('0x6470f17dB9B338dF4955328077ce59b52Fb5A961')
+
+      const WETH_ = new Token(ChainId.ZKSYNC, '0x5AEa5775959fBC2557Cc8789bC1bf90A239D9a91', 18, 'WETH', 'WETH')
+      const USDC__ = new Token(ChainId.ZKSYNC, '0x3355df6D4c9C3035724Fd0e3914dE96A5a83aaf4', 18, 'USDC', 'USDC')
+      expect(Pair.getAddress(USDC__, WETH_)).toEqual('0x63edB9338D81c6D8219856548251bacf2a9b1830')
     })
   })
 
