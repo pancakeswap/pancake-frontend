4d202da4b8b6e728aacec97ac280a77edf23f641
BingBong <bingbong@pancakseswap.com>
2023-07-28 23:01:41 +0100

    test fix

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -80,6 +80,7 @@ function MyApp(props: AppProps<{ initialReduxState: any }>) {
           // eslint-disable-next-line @next/next/no-sync-scripts
           <script src="https://public.bnbstatic.com/static/js/mp-webview-sdk/webview-v1.0.0.min.js" id="mp-webview" />
         )}
+        <meta name="referrer" content="no-referrer" />
       </Head>
       {/* <ClientContextProvider> */}
       <DefaultSeo {...SEO} />
