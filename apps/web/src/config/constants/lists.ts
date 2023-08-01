e522dfc2128e4ca972f9d69a0da92ccbe8a54f70
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-08-01 14:56:10 +0800

    chore: set zksync default list

--- apps/web/src/config/constants/lists.ts
+++ apps/web/src/config/constants/lists.ts
@@ -1,6 +1,7 @@
 export const PANCAKE_EXTENDED = 'https://tokens.pancakeswap.finance/pancakeswap-extended.json'
 export const COINGECKO = 'https://tokens.pancakeswap.finance/coingecko.json'
 export const PANCAKE_ETH_DEFAULT = 'https://tokens.pancakeswap.finance/pancakeswap-eth-default.json'
+export const PANCAKE_ZKSYNC_DEFAULT = 'https://tokens.pancakeswap.finance/pancakeswap-zksync-default.json'
 export const PANCAKE_POLYGON_ZKEVM_DEFAULT = 'https://tokens.pancakeswap.finance/pancakeswap-polygon-zkevm-default.json'
 export const PANCAKE_ETH_MM = 'https://tokens.pancakeswap.finance/pancakeswap-eth-mm.json'
 export const PANCAKE_BSC_MM = 'https://tokens.pancakeswap.finance/pancakeswap-bnb-mm.json'
@@ -10,6 +11,7 @@ export const CMC = 'https://tokens.pancakeswap.finance/cmc.json'
 export const ETH_URLS = [PANCAKE_ETH_DEFAULT, PANCAKE_ETH_MM, COINGECKO_ETH]
 export const BSC_URLS = [PANCAKE_EXTENDED, CMC, COINGECKO, PANCAKE_BSC_MM]
 export const POLYGON_ZKEVM_URLS = [PANCAKE_POLYGON_ZKEVM_DEFAULT]
+export const ZKSYNC_URLS = [PANCAKE_ZKSYNC_DEFAULT]
 
 // List of official tokens list
 export const OFFICIAL_LISTS = [PANCAKE_EXTENDED, PANCAKE_ETH_DEFAULT]
@@ -21,6 +23,7 @@ export const WARNING_LIST_URLS: string[] = []
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   ...BSC_URLS,
   ...ETH_URLS,
+  ...ZKSYNC_URLS,
   ...POLYGON_ZKEVM_URLS,
   ...UNSUPPORTED_LIST_URLS, // need to load unsupported tokens as well
   ...WARNING_LIST_URLS,
@@ -31,5 +34,7 @@ export const DEFAULT_ACTIVE_LIST_URLS: string[] = [
   PANCAKE_EXTENDED,
   PANCAKE_ETH_DEFAULT,
   PANCAKE_ETH_MM,
+  PANCAKE_BSC_MM,
+  PANCAKE_ETH_DEFAULT,
   PANCAKE_POLYGON_ZKEVM_DEFAULT,
 ]
