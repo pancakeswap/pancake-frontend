b544c4f89e1fe4b4049d4d468360ae488066b943
chefmomota <momota@pancakeswap.com>
2023-08-03 17:18:54 +0800

    feat(TradingReward): Add farm config

--- apps/web/src/views/TradingReward/index.tsx
+++ apps/web/src/views/TradingReward/index.tsx
@@ -20,6 +20,7 @@ const TradingReward = () => {
     status: RewardStatus.ALL,
     type: RewardType.CAKE_STAKERS,
   })
+  console.log('allTradingRewardPairData', allTradingRewardPairData)
   const campaignId = allTradingRewardPairData.campaignIds[allTradingRewardPairData.campaignIds.length - 1]
   const { data: campaignInfoData, isFetching: isCampaignInfoFetching } = useCampaignIdInfo({
     campaignId,
