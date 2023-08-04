258f868cc5bf20240c47955dee18efc3b74a3e68
stavinskyi <andrii.stavinskyi@wallchain.xyz>
2023-08-04 18:27:01 +0100

    src: update allowed tokens

--- apps/web/src/config/wallchain.ts
+++ apps/web/src/config/wallchain.ts
@@ -2,4 +2,6 @@ export const WallchainKeys = {
   bsc: process.env.NEXT_PUBLIC_WALLCHAIN_BSC_KEY,
 }
 
-export const WallchainPairs = [['190b589cf9Fb8DDEabBFeae36a813FFb2A702454', 'bb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c']]
+export const WallchainPairs = [
+  ['0x4338665CBB7B2485A8855A139b75D5e34AB0DB94', '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c'],
+]
