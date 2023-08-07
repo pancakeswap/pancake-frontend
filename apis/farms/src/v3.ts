c7b01ec78372db5b1813a9e20a57440a13369163
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2023-08-07 12:26:34 +0800

    add race

--- apis/farms/src/v3.ts
+++ apis/farms/src/v3.ts
@@ -232,28 +232,6 @@ const handler_ = async (req: Request, event: FetchEvent) => {
   }
 
   try {
-    const poolTokens = await V3_SUBGRAPH_CLIENTS[chainId].request(
-      gql`
-        query pool($poolAddress: String!) {
-          pool(id: $poolAddress) {
-            token0 {
-              id
-              decimals
-            }
-            token1 {
-              id
-              decimals
-            }
-          }
-        }
-      `,
-      {
-        poolAddress: address,
-      },
-    )
-
-    const updatedAt = new Date().toISOString()
-
     const [allocPoint, , , , , totalLiquidity] = poolInfo
     const [sqrtPriceX96, tick] = slot0
 
@@ -269,7 +247,7 @@ const handler_ = async (req: Request, event: FetchEvent) => {
             token0: '0',
             token1: '0',
           },
-          updatedAt,
+          updatedAt: new Date().toISOString(),
         },
         {
           headers: {
@@ -279,60 +257,16 @@ const handler_ = async (req: Request, event: FetchEvent) => {
       )
     }
 
-    let allActivePositions: any[] = []
+    const resultTimeout = await Promise.race([
+      timeout(15),
+      fetchLiquidityFromSubgraph(chainId, address, masterChefV3Address, tick, sqrtPriceX96),
+    ])
 
-    // eslint-disable-next-line no-inner-declarations
-    async function fetchPositionByMasterChefId(posId_ = '0') {
-      const resp = await V3_SUBGRAPH_CLIENTS[chainId].request(
-        gql`
-          query tvl($poolAddress: String!, $owner: String!, $posId: String!, $currentTick: String!) {
-            positions(
-              where: { pool: $poolAddress, liquidity_gt: "0", owner: $owner, id_gt: $posId }
-              first: 1000
-              orderBy: id
-              tickLower_: { tickIdx_lte: currentTick }
-              tickUpper_: { tickIdx_gt: currentTick }
-            ) {
-              liquidity
-              id
-              tickUpper {
-                tickIdx
-              }
-              tickLower {
-                tickIdx
-              }
-            }
-          }
-        `,
-        {
-          poolAddress: address,
-          owner: masterChefV3Address.toLowerCase(),
-          currentTick: tick.toString(),
-          posId: posId_,
-        },
-      )
-
-      return resp.positions
+    if (!resultTimeout) {
+      throw new Error('Timeout')
     }
 
-    let posId = '0'
-
-    // eslint-disable-next-line no-constant-condition
-    while (true) {
-      // eslint-disable-next-line no-await-in-loop
-      const pos = await fetchPositionByMasterChefId(posId)
-      allActivePositions = [...allActivePositions, ...pos]
-      if (pos.length < 1000) {
-        break
-      }
-      posId = pos[pos.length - 1].id
-    }
-
-    console.info('fetching farms active liquidity', {
-      address,
-      chainId,
-      allActivePositions: allActivePositions.length,
-    })
+    const { allActivePositions, result } = resultTimeout
 
     if (allActivePositions.length === 0) {
       return json(
@@ -345,7 +279,7 @@ const handler_ = async (req: Request, event: FetchEvent) => {
             token0: '0',
             token1: '0',
           },
-          updatedAt,
+          updatedAt: new Date().toISOString(),
         },
         {
           headers: {
@@ -355,54 +289,8 @@ const handler_ = async (req: Request, event: FetchEvent) => {
       )
     }
 
-    const currentTick = tick
-    const sqrtRatio = sqrtPriceX96
-
-    let totalToken0 = 0n
-    let totalToken1 = 0n
-
-    for (const position of allActivePositions.filter(
-      // double check that the position is within the current tick range
-      (p) => +p.tickLower.tickIdx <= currentTick && +p.tickUpper.tickIdx > currentTick,
-    )) {
-      const token0 = PositionMath.getToken0Amount(
-        currentTick,
-        +position.tickLower.tickIdx,
-        +position.tickUpper.tickIdx,
-        sqrtRatio,
-        BigInt(position.liquidity),
-      )
-
-      const token1 = PositionMath.getToken1Amount(
-        currentTick,
-        +position.tickLower.tickIdx,
-        +position.tickUpper.tickIdx,
-        sqrtRatio,
-        BigInt(position.liquidity),
-      )
-      totalToken0 += token0
-      totalToken1 += token1
-    }
-
-    const curr0 = CurrencyAmount.fromRawAmount(
-      new ERC20Token(+chainId, poolTokens.pool.token0.id, +poolTokens.pool.token0.decimals, '0'),
-      totalToken0.toString(),
-    ).toExact()
-    const curr1 = CurrencyAmount.fromRawAmount(
-      new ERC20Token(+chainId, poolTokens.pool.token1.id, +poolTokens.pool.token1.decimals, '1'),
-      totalToken1.toString(),
-    ).toExact()
-
-    const result = {
-      tvl: {
-        token0: totalToken0.toString(),
-        token1: totalToken1.toString(),
-      },
-      formatted: {
-        token0: curr0,
-        token1: curr1,
-      },
-      updatedAt,
+    if (!result) {
+      throw new Error('No result')
     }
 
     event.waitUntil(FarmKV.saveV3Liquidity(chainId, address, result))
@@ -438,4 +326,156 @@ const handler_ = async (req: Request, event: FetchEvent) => {
 
     return error(500, { error: 'Failed to get active liquidity' })
   }
+}
+
+async function fetchLiquidityFromSubgraph(
+  chainId: keyof typeof V3_SUBGRAPH_CLIENTS,
+  address: string,
+  masterChefV3Address: string,
+  tick: number,
+  sqrtPriceX96: bigint,
+) {
+  const updatedAt = new Date().toISOString()
+  let allActivePositions: any[] = []
+
+  const poolTokens = await V3_SUBGRAPH_CLIENTS[chainId].request(
+    gql`
+      query pool($poolAddress: String!) {
+        pool(id: $poolAddress) {
+          token0 {
+            id
+            decimals
+          }
+          token1 {
+            id
+            decimals
+          }
+        }
+      }
+    `,
+    {
+      poolAddress: address,
+    },
+  )
+
+  // eslint-disable-next-line no-inner-declarations
+  async function fetchPositionByMasterChefId(posId_ = '0') {
+    const resp = await V3_SUBGRAPH_CLIENTS[chainId].request(
+      gql`
+        query tvl($poolAddress: String!, $owner: String!, $posId: String!, $currentTick: String!) {
+          positions(
+            where: { pool: $poolAddress, liquidity_gt: "0", owner: $owner, id_gt: $posId }
+            first: 1000
+            orderBy: id
+            tickLower_: { tickIdx_lte: currentTick }
+            tickUpper_: { tickIdx_gt: currentTick }
+          ) {
+            liquidity
+            id
+            tickUpper {
+              tickIdx
+            }
+            tickLower {
+              tickIdx
+            }
+          }
+        }
+      `,
+      {
+        poolAddress: address,
+        owner: masterChefV3Address.toLowerCase(),
+        currentTick: tick.toString(),
+        posId: posId_,
+      },
+    )
+
+    return resp.positions
+  }
+
+  let posId = '0'
+
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    // eslint-disable-next-line no-await-in-loop
+    const pos = await fetchPositionByMasterChefId(posId)
+    allActivePositions = [...allActivePositions, ...pos]
+    if (pos.length < 1000) {
+      break
+    }
+    posId = pos[pos.length - 1].id
+  }
+
+  console.info('fetching farms active liquidity', {
+    address,
+    chainId,
+    allActivePositions: allActivePositions.length,
+  })
+
+  if (allActivePositions.length === 0) {
+    return {
+      allActivePositions,
+    }
+  }
+
+  const currentTick = tick
+  const sqrtRatio = sqrtPriceX96
+
+  let totalToken0 = 0n
+  let totalToken1 = 0n
+
+  for (const position of allActivePositions.filter(
+    // double check that the position is within the current tick range
+    (p) => +p.tickLower.tickIdx <= currentTick && +p.tickUpper.tickIdx > currentTick,
+  )) {
+    const token0 = PositionMath.getToken0Amount(
+      currentTick,
+      +position.tickLower.tickIdx,
+      +position.tickUpper.tickIdx,
+      sqrtRatio,
+      BigInt(position.liquidity),
+    )
+
+    const token1 = PositionMath.getToken1Amount(
+      currentTick,
+      +position.tickLower.tickIdx,
+      +position.tickUpper.tickIdx,
+      sqrtRatio,
+      BigInt(position.liquidity),
+    )
+    totalToken0 += token0
+    totalToken1 += token1
+  }
+
+  const curr0 = CurrencyAmount.fromRawAmount(
+    new ERC20Token(+chainId, poolTokens.pool.token0.id, +poolTokens.pool.token0.decimals, '0'),
+    totalToken0.toString(),
+  ).toExact()
+  const curr1 = CurrencyAmount.fromRawAmount(
+    new ERC20Token(+chainId, poolTokens.pool.token1.id, +poolTokens.pool.token1.decimals, '1'),
+    totalToken1.toString(),
+  ).toExact()
+
+  const result = {
+    tvl: {
+      token0: totalToken0.toString(),
+      token1: totalToken1.toString(),
+    },
+    formatted: {
+      token0: curr0,
+      token1: curr1,
+    },
+    updatedAt,
+  }
+  return {
+    result,
+    allActivePositions,
+  }
+}
+
+function timeout(seconds: number) {
+  return new Promise<null>((resolve) =>
+    setTimeout(() => {
+      resolve(null)
+    }, seconds * 1_000),
+  )
 }
