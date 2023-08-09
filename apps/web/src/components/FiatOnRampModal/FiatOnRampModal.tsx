784b39665e934ba0b029e066bfd2f5a4b6dd6a36
BingBong <bingbong@pancakseswap.com>
2023-08-09 06:44:16 +0100

    lint

--- apps/web/src/components/FiatOnRampModal/FiatOnRampModal.tsx
+++ apps/web/src/components/FiatOnRampModal/FiatOnRampModal.tsx
@@ -13,7 +13,7 @@ import {
 } from '@pancakeswap/uikit'
 import { LoadingDot } from '@pancakeswap/uikit/src/widgets/Liquidity'
 import { CommitButton } from 'components/CommitButton'
-import { MERCURYO_WIDGET_ID, MOONPAY_SIGN_URL, ONRAMP_API_BASE_URL } from 'config/constants/endpoints'
+import { MOONPAY_SIGN_URL, ONRAMP_API_BASE_URL } from 'config/constants/endpoints'
 import { useActiveChainId } from 'hooks/useActiveChainId'
 import Script from 'next/script'
 import { ReactNode, memo, useCallback, useEffect, useState } from 'react'
