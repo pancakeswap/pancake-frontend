4f6c3cd8747e3df7e9a16d6a26391b89d1b0e3aa
chefjackson <116779127+chefjackson@users.noreply.github.com>
2023-08-10 07:40:34 +0800

    chore: update ifo

--- packages/ifos/src/constants/ifos/1101.ts
+++ packages/ifos/src/constants/ifos/1101.ts
@@ -6,7 +6,7 @@ import { BaseIfoConfig } from '../../types'
 export const ifos: BaseIfoConfig[] = [
   {
     id: '1101-test',
-    address: '0xd978a851ec83e2669c0386098d8c68627f40d686',
+    address: '0x567c8af8c369f9d886453b250fb7c2313fef9be9',
     isActive: true,
     name: 'USDT',
     plannedStartTime: 1691474400,
