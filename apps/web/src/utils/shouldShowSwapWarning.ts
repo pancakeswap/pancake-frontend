20140b1429ad6cf4505be9b869fccdbe97cdd9f3
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2023-08-10 20:16:54 +0800

    fix

--- apps/web/src/utils/shouldShowSwapWarning.ts
+++ apps/web/src/utils/shouldShowSwapWarning.ts
@@ -1,13 +1,13 @@
 import { Token } from '@pancakeswap/sdk'
 import SwapWarningTokens from 'config/constants/swapWarningTokens'
 
-const shouldShowSwapWarning = (chainId: number, swapCurrency: Token) => {
+const shouldShowSwapWarning = (chainId: number, swapCurrency: Token): boolean => {
   if (SwapWarningTokens[chainId]) {
     const swapWarningTokens = Object.values(SwapWarningTokens[chainId])
     return swapWarningTokens.some((warningToken) => warningToken.equals(swapCurrency))
   }
 
-  return []
+  return false
 }
 
 export default shouldShowSwapWarning
