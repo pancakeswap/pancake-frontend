71aa41b9ff97a82beef8804c67c1e20188735135
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-08-16 15:06:28 +0800

    chore: update footer

--- packages/uikit/src/widgets/Menu/components/footerConfig.ts
+++ packages/uikit/src/widgets/Menu/components/footerConfig.ts
@@ -3,24 +3,27 @@ import { FooterLinkType } from "../../../components/Footer/types";
 
 export const footerLinks: (t: ContextApi["t"]) => FooterLinkType[] = (t) => [
   {
-    label: t("About"),
+    label: "Ecosystem",
     items: [
       {
-        label: t("Contact"),
-        href: "https://docs.pancakeswap.finance/contact-us",
-        isHighlighted: true,
+        label: t("Trade"),
+        href: "/swap",
       },
       {
-        label: t("Brand"),
-        href: "https://docs.pancakeswap.finance/brand",
+        label: t("Earn"),
+        href: "/pools",
       },
       {
-        label: t("Blog"),
-        href: "https://blog.pancakeswap.finance/",
+        label: t("Game"),
+        href: "https://protectors.pancakeswap.finance/",
       },
       {
-        label: t("Community"),
-        href: "https://docs.pancakeswap.finance/contact-us/telegram",
+        label: t("NFT"),
+        href: "/nfts",
+      },
+      {
+        label: t("Tokenomics"),
+        href: "/swap",
       },
       {
         label: t("Litepaper"),
@@ -45,19 +48,27 @@ export const footerLinks: (t: ContextApi["t"]) => FooterLinkType[] = (t) => [
     ],
   },
   {
-    label: t("Help"),
+    label: "Business",
     items: [
       {
-        label: t("Customer Support"),
-        href: "https://docs.pancakeswap.finance/contact-us/customer-support",
+        label: t("Farms and Syrup Pools"),
+        href: "/swap",
       },
       {
-        label: t("Troubleshooting"),
-        href: "https://docs.pancakeswap.finance/help/troubleshooting",
+        label: t("IFO"),
+        href: "/swap",
       },
       {
-        label: t("Guides"),
-        href: "https://docs.pancakeswap.finance/get-started",
+        label: t("NFT Marketplace"),
+        href: "/swap",
+      },
+      {
+        label: t("Etherum Expansion"),
+        href: "/swap",
+      },
+      {
+        label: t("Aptos Development"),
+        href: "/swap",
       },
     ],
   },
@@ -65,7 +76,7 @@ export const footerLinks: (t: ContextApi["t"]) => FooterLinkType[] = (t) => [
     label: t("Developers"),
     items: [
       {
-        label: "Github",
+        label: t("Github"),
         href: "https://github.com/pancakeswap",
       },
       {
@@ -76,13 +87,55 @@ export const footerLinks: (t: ContextApi["t"]) => FooterLinkType[] = (t) => [
         label: t("Bug Bounty"),
         href: "https://docs.pancakeswap.finance/code/bug-bounty",
       },
+      // {
+      //   label: t("Audits"),
+      //   href: "https://docs.pancakeswap.finance/help/faq#is-pancakeswap-safe-has-pancakeswap-been-audited",
+      // },
+    ],
+  },
+  {
+    label: t("Support"),
+    items: [
       {
-        label: t("Audits"),
-        href: "https://docs.pancakeswap.finance/help/faq#is-pancakeswap-safe-has-pancakeswap-been-audited",
+        label: t("Contact"),
+        href: "https://docs.pancakeswap.finance/contact-us",
+      },
+      // {
+      //   label: t("Customer Support"),
+      //   href: "https://docs.pancakeswap.finance/contact-us/customer-support",
+      // },
+      {
+        label: t("Troubleshooting"),
+        href: "https://docs.pancakeswap.finance/help/troubleshooting",
+      },
+      {
+        label: t("FAQ"),
+        href: "https://docs.pancakeswap.finance/get-started",
+      },
+    ],
+  },
+  {
+    label: t("About"),
+    items: [
+      {
+        label: t("Terms Of Service"),
+        href: "https://pancakeswap.finance/terms-of-service",
+      },
+      {
+        label: t("Blog"),
+        href: "https://blog.pancakeswap.finance/",
+      },
+      {
+        label: t("Brand Assets"),
+        href: "https://docs.pancakeswap.finance/brand",
       },
       {
         label: t("Careers"),
         href: "https://docs.pancakeswap.finance/hiring/become-a-chef",
+      },
+      {
+        label: t("Community"),
+        href: "https://docs.pancakeswap.finance/contact-us/telegram",
       },
     ],
   },
