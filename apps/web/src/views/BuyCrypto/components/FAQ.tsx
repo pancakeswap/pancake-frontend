37a4e24079227e6764120d86be14ca345bf3de8a
chefmomota <momota@pancakeswap.com>
2023-08-17 19:20:01 +0800

    fix: Text

--- apps/web/src/views/BuyCrypto/components/FAQ.tsx
+++ apps/web/src/views/BuyCrypto/components/FAQ.tsx
@@ -49,7 +49,7 @@ const config = (t) => [
     ),
   },
   {
-    title: t('Why do i recieve different USDC tokens on Arbitrum?'),
+    title: t('Why do i receive different USDC tokens on Arbitrum?'),
     description: t(
       'In the case of Aibitrum, theres a bridged version of USDC known as USDC.e that has been bridged from Ethereum to Arbitrum, and native USDC, known as USDC. Depending on providers, we support both USDC.E and USDC. Please check your wallet balances for both tokens',
     ),
