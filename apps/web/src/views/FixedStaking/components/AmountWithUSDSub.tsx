f81b1b911d9288bb4dd3592db2989151ba84cc53
chefilip <philip@pancakeswap.com>
2023-08-17 15:53:31 -0400

    chore: Update number format

--- apps/web/src/views/FixedStaking/components/AmountWithUSDSub.tsx
+++ apps/web/src/views/FixedStaking/components/AmountWithUSDSub.tsx
@@ -13,7 +13,7 @@ export function AmountWithUSDSub({ amount, shouldStrike }: { shouldStrike?: bool
     undefined,
     <>
       <Text bold mb="-4px">
-        {amount.toSignificant(2)} {amount.currency.symbol}
+        {amount.toSignificant(5)} {amount.currency.symbol}
       </Text>
       <Balance unit=" USD" color="textSubtle" prefix="~$" fontSize="12px" decimals={2} value={formattedUsdAmount} />
     </>,
