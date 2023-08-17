85d9f945732300483be5449d56c0cfc80cb23a5f
chefmomota <momota@pancakeswap.com>
2023-08-17 11:07:23 +0800

    feat: Turn Risk API cache to 1 min

--- apps/web/src/pages/api/risk/[chainId]/[address].ts
+++ apps/web/src/pages/api/risk/[chainId]/[address].ts
@@ -43,7 +43,7 @@ const handler: NextApiHandler = async (req, res) => {
   const json = await response.json()
 
   if (json.data.risk_level >= 0 && json.data.has_result) {
-    res.setHeader('Cache-Control', 's-maxage=86400, max-age=3600')
+    res.setHeader('Cache-Control', 's-maxage=600, max-age=600')
   }
 
   return res.status(response.status).json({
