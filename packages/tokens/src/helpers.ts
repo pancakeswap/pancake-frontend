cdaf891b4d9a06fd6668fea3d3f96f8e3a1c8533
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2023-08-18 10:16:08 +0800

    fix empty list

--- packages/tokens/src/helpers.ts
+++ packages/tokens/src/helpers.ts
@@ -5,7 +5,7 @@ import { TokenAddressMap } from '@pancakeswap/token-lists'
  * An empty result, useful as a default.
  */
 export const EMPTY_LIST = Object.values(ChainId).reduce((accum, chainId) => {
-  return { ...accum, [chainId]: [] }
+  return { ...accum, [chainId]: {} }
 }, {} as TokenAddressMap<ChainId>)
 
 export function serializeTokens(unserializedTokens: any) {
