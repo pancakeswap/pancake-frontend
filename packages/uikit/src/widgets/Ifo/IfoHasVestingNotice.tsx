02c57f670d60760bff9a15cd836d4a90e2612635
chefjackson <116779127+chefjackson@users.noreply.github.com>
2023-08-21 16:49:23 +0800

    fix: text color

--- packages/uikit/src/widgets/Ifo/IfoHasVestingNotice.tsx
+++ packages/uikit/src/widgets/Ifo/IfoHasVestingNotice.tsx
@@ -9,7 +9,7 @@ const IfoHasVestingNotice: React.FC<React.PropsWithChildren<{ url: string }>> = 
     <Box maxWidth="350px">
       <Message variant="primary" mb="16px">
         <Box>
-          <Text fontSize="14px" color="#D67E0A">
+          <Text fontSize="14px">
             {t("This IFO has token vesting. Purchased tokens are released over a period of time.")}
           </Text>
           <IfoMessageTextLink external href={url} color="#1FC7D4" display="inline">
