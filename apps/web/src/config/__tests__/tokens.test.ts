ffecab3a5812ceb6555c493c89a5876645563fef
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-08-24 19:24:59 +0800

    chore: fix token test case

--- apps/web/src/config/__tests__/tokens.test.ts
+++ apps/web/src/config/__tests__/tokens.test.ts
@@ -2,12 +2,11 @@ import { Token } from '@pancakeswap/sdk'
 import { bscTokens, ethereumTokens } from '@pancakeswap/tokens'
 import { erc20ABI } from 'wagmi'
 import map from 'lodash/map'
-import slice from 'lodash/slice'
 import omitBy from 'lodash/omitBy'
 import { describe, it } from 'vitest'
 import { publicClient } from '../../utils/wagmi'
 
-const whitelist = ['deprecated_tusd']
+const whitelist = ['deprecated_tusd', 'deprecated_rpg', 'deprecated_mix']
 
 // remove BNB because it's not a Bep20 token
 // remove ONE because there are two tokens with the symbol ONE (Harmony ONE and BigONE)
@@ -32,31 +31,28 @@ const tokenTables: [string, Token][] = tokenListsToTest.reduce(
 describe.concurrent(
   'Config tokens',
   () => {
-    it.each(slice(tokenTables, tokenTables.length - 50))(
-      'Token %s has the correct key, symbol, and decimal',
-      async (key: string, token: Token) => {
-        const client = publicClient({ chainId: token.chainId })
-        const [symbol, decimals] = await client.multicall({
-          contracts: [
-            {
-              abi: erc20ABI,
-              address: token.address,
-              functionName: 'symbol',
-            },
-            {
-              abi: erc20ABI,
-              address: token.address,
-              functionName: 'decimals',
-            },
-          ],
-          allowFailure: false,
-        })
-        const isWhitelisted = whitelist.includes(key.toLowerCase())
-        if (!isWhitelisted) expect(key.toLowerCase()).toBe(token.symbol.toLowerCase())
-        expect(token.symbol.toLocaleLowerCase()).toBe(symbol.toLocaleLowerCase())
-        expect(token.decimals).toBe(decimals)
-      },
-    )
+    it.each(tokenTables)('Token %s has the correct key, symbol, and decimal', async (key: string, token: Token) => {
+      const client = publicClient({ chainId: token.chainId })
+      const [symbol, decimals] = await client.multicall({
+        contracts: [
+          {
+            abi: erc20ABI,
+            address: token.address,
+            functionName: 'symbol',
+          },
+          {
+            abi: erc20ABI,
+            address: token.address,
+            functionName: 'decimals',
+          },
+        ],
+        allowFailure: false,
+      })
+      const isWhitelisted = whitelist.includes(key.toLowerCase())
+      if (!isWhitelisted) expect(key.toLowerCase()).toBe(token.symbol.toLowerCase())
+      if (!isWhitelisted) expect(token.symbol.toLocaleLowerCase()).toBe(symbol.toLocaleLowerCase())
+      expect(token.decimals).toBe(decimals)
+    })
   },
   {
     timeout: 50_000,
