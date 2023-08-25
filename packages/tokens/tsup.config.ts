d5414f1004c1e6ce73e7c9150af59c7a203d724e
chefjackson <116779127+chefjackson@users.noreply.github.com>
2023-08-25 12:10:12 +0800

    Revert "Revert "chore: Remove invalid dependency" (#7650)"
    This reverts commit bb43870d8dbbbaa296e1487a18ce2dbd9b4ff637.

--- packages/tokens/tsup.config.ts
+++ packages/tokens/tsup.config.ts
@@ -7,6 +7,7 @@ export default defineConfig((options) => {
       index: './src/index.ts',
     },
     format: ['esm', 'cjs'],
+    noExternal: ['@pancakeswap/utils'],
     dts: false,
     clean: !options.watch,
     treeshake: true,
