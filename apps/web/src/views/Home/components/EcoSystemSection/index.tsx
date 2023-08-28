77b5eb9fa53a9d81b93d93f805337266d17b7a8e
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-08-28 12:14:07 +0800

    chore: update purple hover effect

--- apps/web/src/views/Home/components/EcoSystemSection/index.tsx
+++ apps/web/src/views/Home/components/EcoSystemSection/index.tsx
@@ -23,6 +23,21 @@ import gamePottery from '../../images/game-pottery.png'
 import nftMarketplace from '../../images/nft-marketplace.png'
 import GradientLogo from '../GradientLogoSvg'
 
+import tradeSwapPurple from '../../images/trade-swap-purple.png'
+import tradeLiquidityPurple from '../../images/trade-liquidity-purple.png'
+import tradeBridgePurple from '../../images/trade-bridge-purple.png'
+import tradePerpetualPurple from '../../images/trade-perpetual-purple.png'
+import tradeBuyPurple from '../../images/trade-buy-crypto-purple.png'
+import earnFarmPurple from '../../images/earn-farm-purple.png'
+import earnPoolsPurple from '../../images/earn-pools-purple.png'
+import earnLiquidStakingPurple from '../../images/earn-liquidity-staking-purple.png'
+import earnFixedStakingPurple from '../../images/earn-fixed-staking-purple.png'
+import gamePredictionPurple from '../../images/game-prediction-purple.png'
+import gamePancakeProtectorsPurple from '../../images/game-pancake-protectors-purple.png'
+import gameLotteryPurple from '../../images/game-lottery-purple.png'
+import gamePotteryPurple from '../../images/game-pottery-purple.png'
+import nftMarketplacePurple from '../../images/nft-marketplace-purple.png'
+
 export const CardWrapper = styled.div`
   border-radius: 24px;
   background: ${({ theme }) => theme.colors.backgroundAlt};
@@ -39,9 +54,26 @@ export const CardWrapper = styled.div`
   }
 `
 export const ImageBox = styled.div`
+  position: relative;
   transition: filter 0.25s linear;
+  .default {
+    display: none;
+  }
   ${({ theme }) => theme.mediaQueries.xl} {
-    filter: invert(38%) sepia(97%) saturate(433%) hue-rotate(215deg) brightness(83%) contrast(86%);
+    .default {
+      display: block;
+      position: relative;
+      z-index: 1;
+    }
+    .hover {
+      transition: opacity 0.25s ease-in-out;
+      position: absolute;
+      top: 0;
+      left: 0;
+      opacity: 0;
+      z-index: 2;
+    }
+    // filter: invert(38%) sepia(97%) saturate(433%) hue-rotate(215deg) brightness(83%) contrast(86%);
   }
 `
 
@@ -61,7 +93,10 @@ export const ItemWrapper = styled(Flex)<{ $flexBasis: number }>`
       color: ${({ theme }) => theme.colors.primary};
     }
     ${ImageBox} {
-      filter: invert(0%) sepia(0%) saturate(100%) hue-rotate(0deg) brightness(100%) contrast(100%);
+      .hover {
+        opacity: 1;
+      }
+      // filter: invert(0%) sepia(0%) saturate(100%) hue-rotate(0deg) brightness(100%) contrast(100%);
     }
   }
   flex-basis: 50%;
@@ -122,30 +157,35 @@ const useTradeBlockData = () => {
         description: t('Trade crypto instantly across multiple chains'),
         ctaTitle: t('Trade Now'),
         image: tradeSwap,
+        defaultImage: tradeSwapPurple,
       },
       {
         title: t('Liquidity'),
         description: t('Fund liquidity pools, earn trading fees'),
         ctaTitle: t('Add Now'),
         image: tradeLiquidity,
+        defaultImage: tradeLiquidityPurple,
       },
       {
         title: t('Bridge'),
         description: t('Seamlessly transfer assets across chains'),
         ctaTitle: t('Bridge Now'),
         image: tradeBridge,
+        defaultImage: tradeBridgePurple,
       },
       {
         title: t('Perpetual'),
         description: t('Trade endlessly without expiration dates'),
         ctaTitle: t('Trade Now'),
         image: tradePerpetual,
+        defaultImage: tradePerpetualPurple,
       },
       {
         title: t('Buy Crypto'),
         description: t('Buy crypto with your preferred currency and payment method'),
         ctaTitle: t('Buy Now'),
         image: tradeBuy,
+        defaultImage: tradeBuyPurple,
       },
     ]
   }, [t])
@@ -160,24 +200,28 @@ const useEarnBlockData = () => {
         description: t('Stake LP tokens, harvest CAKE'),
         ctaTitle: t('Stake Now'),
         image: earnFarm,
+        defaultImage: earnFarmPurple,
       },
       {
         title: t('Pools'),
         description: t('Stake CAKE, earn various rewards'),
         ctaTitle: t('Stake Now'),
         image: earnPools,
+        defaultImage: earnPoolsPurple,
       },
       {
         title: t('Liquid Staking'),
         description: t('Earn rewards while retaining asset flexibility'),
         ctaTitle: t('Add Now'),
         image: earnLiquidStaking,
+        defaultImage: earnLiquidStakingPurple,
       },
       {
         title: t('Coming Soon'),
         description: t('Fixed staking'),
         ctaTitle: t('Learn More'),
         image: earnFixedStaking,
+        defaultImage: earnFixedStakingPurple,
       },
     ]
   }, [t])
@@ -192,30 +236,35 @@ const useNftGameBlockData = () => {
         description: t('Forecast token prices within minutes'),
         ctaTitle: t('Try Now'),
         image: gamePrediction,
+        defaultImage: gamePredictionPurple,
       },
       {
         title: t('Pancake Protectors'),
         description: t('Immersive PvP & PvE tower-defense GameFi'),
         ctaTitle: t('Play Now'),
         image: gamePancakeProtectors,
+        defaultImage: gamePancakeProtectorsPurple,
       },
       {
         title: t('Lottery'),
         description: t('Enter for a chance to win CAKE prize pools'),
         ctaTitle: t('Try Now'),
         image: gameLottery,
+        defaultImage: gameLotteryPurple,
       },
       {
         title: t('Pottery'),
         description: t('Stake CAKE, acquire pottery tickets, win prizes'),
         ctaTitle: t('Try Now'),
         image: gamePottery,
+        defaultImage: gamePotteryPurple,
       },
       {
         title: t('NFT Marketplace'),
         description: t('Trade unique NFTs on BNB Chain'),
         ctaTitle: t('Trade Now'),
         image: nftMarketplace,
+        defaultImage: nftMarketplacePurple,
       },
     ]
   }, [t])
@@ -225,15 +274,17 @@ const FeatureBox: React.FC<{
   title: string
   description: string
   image: StaticImageData
+  defaultImage: StaticImageData
   width: number
   ctaTitle: string
   className?: string
-}> = ({ title, description, image, ctaTitle, width, className }) => {
+}> = ({ title, description, image, defaultImage, ctaTitle, width, className }) => {
   const { theme } = useTheme()
   return (
     <ItemWrapper className={className} $flexBasis={width}>
       <ImageBox>
-        <Image src={image} width={108} height={108} alt={title} />
+        <Image className="default" src={defaultImage} width={108} height={108} alt={title} />
+        <Image className="hover" src={image} width={108} height={108} alt={title} />
       </ImageBox>
       <Box>
         <Text fontSize="20px" mb="8px" lineHeight="110%" fontWeight={600} color={theme.colors.text}>
@@ -301,6 +352,7 @@ const EcoSystemSection: React.FC = () => {
                   className="type-a"
                   title={item.title}
                   description={item.description}
+                  defaultImage={item.defaultImage}
                   image={item.image}
                   width={100 / tradeBlockData.length}
                   ctaTitle={item.ctaTitle}
@@ -334,6 +386,7 @@ const EcoSystemSection: React.FC = () => {
                   title={item.title}
                   description={item.description}
                   image={item.image}
+                  defaultImage={item.defaultImage}
                   width={100 / tradeBlockData.length}
                   ctaTitle={item.ctaTitle}
                 />
@@ -365,6 +418,7 @@ const EcoSystemSection: React.FC = () => {
                   key={`${item.title}Block`}
                   title={item.title}
                   description={item.description}
+                  defaultImage={item.defaultImage}
                   image={item.image}
                   width={100 / tradeBlockData.length}
                   ctaTitle={item.ctaTitle}
