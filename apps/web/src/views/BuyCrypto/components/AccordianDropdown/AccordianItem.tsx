55d467577e0900f078fab6d0bc01dc8a55cec116
BingBong <bingbong@pancakseswap.com>
2023-09-05 00:18:10 +0100

    added time

--- apps/web/src/views/BuyCrypto/components/AccordianDropdown/AccordianItem.tsx
+++ apps/web/src/views/BuyCrypto/components/AccordianDropdown/AccordianItem.tsx
@@ -59,7 +59,7 @@ function AccordionItem({
   const [visiblity, setVisiblity] = useState(false)
   const [mobileTooltipShow, setMobileTooltipShow] = useState(false)
 
-  const { days, hours, minutes } = getTimePeriods(1681699200)
+  const { days, hours, minutes } = getTimePeriods(1692576000)
   const isActive = () => (multiple ? visiblity : active)
 
   const toogleVisiblity = useCallback(() => {
