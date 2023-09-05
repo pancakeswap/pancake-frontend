49857488531a3b5912d947ca57923ecab6e3a401
BingBong <bingbong@pancakseswap.com>
2023-09-05 18:34:50 +0100

    FIX use main api url now

--- apps/web/src/views/BuyCrypto/hooks/useProviderAvailability.ts
+++ apps/web/src/views/BuyCrypto/hooks/useProviderAvailability.ts
@@ -1,10 +1,11 @@
+import { ONRAMP_API_BASE_URL } from 'config/constants/endpoints'
 import { ONRAMP_PROVIDERS } from '../constants'
 
 export async function fetchProviderAvailabilities(
   payload,
 ): Promise<{ [provider in keyof typeof ONRAMP_PROVIDERS]: boolean }> {
   // Fetch data from endpoint 1
-  const response = await fetch(`http://localhost:8081/fetch-provider-availability`, {
+  const response = await fetch(`${ONRAMP_API_BASE_URL}/fetch-provider-availability`, {
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json',
