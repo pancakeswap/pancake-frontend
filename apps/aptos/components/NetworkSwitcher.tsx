f8d756ce894c9330e65ebf99fb4884d9c4a07ee5
chefmomota <momota@pancakeswap.com>
2023-09-05 11:09:43 +0800

    feat(Aptos): Network Select Add Base

--- apps/aptos/components/NetworkSwitcher.tsx
+++ apps/aptos/components/NetworkSwitcher.tsx
@@ -15,6 +15,7 @@ const evmChains = [
   { id: 1101, name: 'Polygon zkEVM', chainName: 'polygonZkEVM' },
   { id: 42161, name: 'Arbitrum One', chainName: 'arb' },
   { id: 59144, name: 'Linea', chainName: 'linea' },
+  { id: 8453, name: 'Base', chainName: 'base' },
 ]
 
 const NetworkSelect = () => {
