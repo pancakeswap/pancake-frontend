75c71a6aedc259f45b77e80bac805515fdccd2cd
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-09-06 15:31:15 +0800

    chore: sort farm config

--- packages/farms/constants/8453.ts
+++ packages/farms/constants/8453.ts
@@ -4,17 +4,10 @@ import { defineFarmV3Configs } from '../src/defineFarmV3Configs'
 
 export const farmsV3 = defineFarmV3Configs([
   {
-    pid: 1,
-    lpAddress: '0xe58b73fF901325b8b2056B29712C50237242F520',
+    pid: 4,
+    lpAddress: '0x9BFa331679b307Cf358438F45f6413a205FD3EBf',
     token0: baseTokens.weth,
-    token1: baseTokens.usdbc,
-    feeAmount: FeeAmount.LOW,
-  },
-  {
-    pid: 2,
-    lpAddress: '0xc0efC182479319ff258EcA420e2647cD82D3790c',
-    token0: baseTokens.cbETH,
-    token1: baseTokens.weth,
+    token1: baseTokens.dai,
     feeAmount: FeeAmount.LOW,
   },
   {
@@ -25,10 +18,17 @@ export const farmsV3 = defineFarmV3Configs([
     feeAmount: FeeAmount.LOWEST,
   },
   {
-    pid: 4,
-    lpAddress: '0x9BFa331679b307Cf358438F45f6413a205FD3EBf',
+    pid: 2,
+    lpAddress: '0xc0efC182479319ff258EcA420e2647cD82D3790c',
+    token0: baseTokens.cbETH,
+    token1: baseTokens.weth,
+    feeAmount: FeeAmount.LOW,
+  },
+  {
+    pid: 1,
+    lpAddress: '0xe58b73fF901325b8b2056B29712C50237242F520',
     token0: baseTokens.weth,
-    token1: baseTokens.dai,
+    token1: baseTokens.usdbc,
     feeAmount: FeeAmount.LOW,
   },
 ])
