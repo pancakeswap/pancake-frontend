0bad7c9035331d0dd465dfdbff6963d02e340736
chefmomota <momota@pancakeswap.com>
2023-09-07 19:08:10 +0800

    chore: Just for fun

--- apps/web/src/hooks/useMerryChristmas.ts
+++ apps/web/src/hooks/useMerryChristmas.ts
@@ -3,18 +3,19 @@ import { useParticleBurst } from '@pancakeswap/uikit'
 import { useRouter } from 'next/router'
 
 const disableWhenNotChristmas = () => {
-  const today = new Date()
-  const month = today.getMonth() + 1
-  const day = today.getDate()
+  // const today = new Date()
+  // const month = today.getMonth() + 1
+  // const day = today.getDate()
 
-  if (month !== 12) {
-    return true
-  }
+  // if (month !== 12) {
+  //   return true
+  // }
 
-  if (![24, 25].includes(day)) {
-    return true
-  }
+  // if (![24, 25].includes(day)) {
+  //   return true
+  // }
 
+  // return false
   return false
 }
 
