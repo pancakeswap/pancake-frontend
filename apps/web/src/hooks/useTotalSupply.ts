80381faaab1df8d7a69c3055c9cde006632cee21
memoyil <2213635+memoyil@users.noreply.github.com>
2023-09-08 18:38:19 +0200

    perf: Use query with interval in total supply hook

--- apps/web/src/hooks/useTotalSupply.ts
+++ apps/web/src/hooks/useTotalSupply.ts
@@ -1,17 +1,33 @@
 import { useMemo } from 'react'
 import { Currency, CurrencyAmount, Token } from '@pancakeswap/sdk'
-import { useTokenContract } from './useContract'
-import { useSingleCallResult } from '../state/multicall/hooks'
+import { useQuery } from '@tanstack/react-query'
+import { useActiveChainId } from 'hooks/useActiveChainId'
+import { publicClient } from 'utils/wagmi'
+import { erc20ABI } from 'wagmi'
+import { FAST_INTERVAL } from 'config/constants'
 
 // returns undefined if input token is undefined, or fails to get token contract,
 // or contract total supply cannot be fetched
 export function useTotalSupply(token?: Currency): CurrencyAmount<Token> | undefined {
-  const contract = useTokenContract(token?.isToken ? token.address : undefined)
+  const { chainId } = useActiveChainId()
 
-  const totalSupplyStr: string | undefined = useSingleCallResult({
-    contract,
-    functionName: 'totalSupply',
-  })?.result?.toString()
+  const { data: totalSupplyStr } = useQuery(
+    ['totalSupply', chainId, token?.isToken ? token.address : undefined],
+    async () => {
+      const data = await publicClient({ chainId }).readContract({
+        abi: erc20ABI,
+        address: token?.isToken ? token.address : undefined,
+        functionName: 'totalSupply',
+      })
+      return data?.toString()
+    },
+    {
+      refetchInterval: FAST_INTERVAL,
+      retry: true,
+      refetchOnWindowFocus: false,
+      enabled: token?.isToken,
+    },
+  )
 
   return useMemo(
     () => (token?.isToken && totalSupplyStr ? CurrencyAmount.fromRawAmount(token, totalSupplyStr) : undefined),
