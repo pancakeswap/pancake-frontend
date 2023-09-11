8cb7d5feb66c344cea7d6ffa8748d6e6ececc2b2
chefmomota <momota@pancakeswap.com>
2023-09-11 16:24:52 +0800

    chore: Typo

--- apps/web/src/views/Swap/V3Swap/containers/ConfirmSwapModal.tsx
+++ apps/web/src/views/Swap/V3Swap/containers/ConfirmSwapModal.tsx
@@ -255,7 +255,7 @@ export const ConfirmSwapModal = memo<InjectedModalProps & ConfirmSwapModalProps>
     const currencyA = currencyBalances.INPUT?.currency ?? trade?.inputAmount?.currency
     const currencyB = currencyBalances.OUTPUT?.currency ?? trade?.outputAmount?.currency
     const amountA = formatAmount(trade?.inputAmount, 6) ?? ''
-    const amountB = formatAmount(trade?.inputAmount, 6) ?? ''
+    const amountB = formatAmount(trade?.outputAmount, 6) ?? ''
 
     if (confirmModalState === ConfirmModalState.RESETTING_APPROVAL) {
       return <ApproveModalContent title={t('Reset approval on USDT.')} isMM={isMM} />
