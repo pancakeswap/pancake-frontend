444081288b0a5d58b37baef8fdd332cda0694af8
chefmomota <momota@pancakeswap.com>
2023-09-12 19:42:03 +0800

    feat: Add useConfetti

--- apps/web/src/hooks/useThirdYearBirthdayEffect.ts
+++ apps/web/src/hooks/useThirdYearBirthdayEffect.ts
@@ -1,13 +1,80 @@
-import { useEffect } from 'react'
-import { useParticleBurst } from '@pancakeswap/uikit'
+import { useEffect, useCallback } from 'react'
 import { useRouter } from 'next/router'
+import confetti from 'canvas-confetti'
+import debounce from 'lodash/debounce'
+
+const defaultOptions = {
+  debounceDuration: 200,
+}
+
+type Options = {
+  selector?: string
+  debounceDuration?: number
+  disableWhen?: () => boolean
+}
+
+const useConfetti = (options: Options): { initialize: () => void; teardown: () => void } => {
+  const { selector, debounceDuration, disableWhen } = {
+    ...defaultOptions,
+    ...options,
+  }
+
+  const makeListener = useCallback(
+    () =>
+      debounce(
+        () => {
+          const isDisabled = disableWhen && disableWhen()
+
+          if (!isDisabled) {
+            confetti({
+              particleCount: 100,
+              startVelocity: 30,
+              spread: 360,
+              origin: {
+                x: Math.random(),
+                y: Math.random() - 0.2,
+              },
+            })
+          }
+        },
+        debounceDuration,
+        { leading: true },
+      ),
+    [debounceDuration, disableWhen],
+  )
+  const listener = makeListener()
+
+  const initialize = useCallback(() => {
+    if (selector) {
+      document.querySelectorAll<HTMLElement>(selector).forEach((element) => {
+        element.addEventListener('click', listener)
+      })
+    } else {
+      document.addEventListener('click', listener)
+    }
+  }, [selector, listener])
+
+  const teardown = useCallback(() => {
+    if (selector) {
+      document.querySelectorAll<HTMLElement>(selector).forEach((element) => {
+        element.removeEventListener('click', listener)
+      })
+    } else {
+      document.removeEventListener('click', listener)
+    }
+  }, [selector, listener])
+
+  useEffect(() => {
+    initialize()
+    return () => teardown()
+  }, [initialize, teardown])
+
+  return { initialize, teardown }
+}
 
 const useThirdYearBirthdayEffect = () => {
   const { pathname } = useRouter()
-  const { initialize, teardown } = useParticleBurst({
-    imgSrc: '/images/third-year-cake-icon.png',
-    debounceDuration: 1000,
-  })
+  const { initialize, teardown } = useConfetti({ debounceDuration: 1000 })
 
   useEffect(() => {
     initialize()
