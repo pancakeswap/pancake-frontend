ed74556827296ee369e5f1fde10ac4bf05a1fc05
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-09-21 18:36:24 +0800

    chore: footer assets

--- apps/web/src/views/Home/components/Footer.tsx
+++ apps/web/src/views/Home/components/Footer.tsx
@@ -4,8 +4,58 @@ import ConnectWalletButton from 'components/ConnectWalletButton'
 import Container from 'components/Layout/Container'
 import { styled } from 'styled-components'
 import { useAccount } from 'wagmi'
+import Image from 'next/image'
 import CompositeImage from './CompositeImage'
 import SunburstSvg from './SunburstSvg'
+import ctaPancake from '../images/cta-pancake.png'
+import ctaPancakeBig from '../images/cta-pancake-big.png'
+import ctaRock from '../images/cta-rock.png'
+import ctaRock2 from '../images/cta-rock-2.png'
+
+const ImageWrapper = styled.div`
+  z-index: 2;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+  .pancake {
+    position: absolute;
+    width: 120px;
+    top: 20px;
+    left: -40px;
+  }
+  .rock {
+    position: absolute;
+    width: 120px;
+    top: 16px;
+    right: 5px;
+  }
+  .big-pancake {
+    width: 160px;
+    position: absolute;
+    bottom: 10px;
+    right: -60px;
+  }
+  .rock2 {
+    width: 140px;
+    position: absolute;
+    bottom: 10px;
+    left: 20px;
+  }
+`
+
+const ImageBox: React.FC = () => {
+  return (
+    <ImageWrapper>
+      <Image className="pancake" src={ctaPancake} alt="pancake" />
+      <Image className="rock" src={ctaRock} alt="rock" />
+      <Image className="big-pancake" src={ctaPancakeBig} alt="big-pancake" />
+      <Image className="rock2" src={ctaRock2} alt="rock2" />
+    </ImageWrapper>
+  )
+}
 
 const BgWrapper = styled.div`
   overflow: hidden;
@@ -36,75 +86,22 @@ const Wrapper = styled(Flex)`
   overflow: hidden;
 `
 
-const FloatingPancakesWrapper = styled(Container)`
-  overflow: hidden;
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 50%;
-  transform: translateX(-50%);
-  visibility: hidden;
-
-  ${({ theme }) => theme.mediaQueries.md} {
-    visibility: visible;
-  }
-`
-
-const TopLeftImgWrapper = styled(Flex)`
-  position: absolute;
-  left: 0;
-  top: 0;
-`
-
-const BottomRightImgWrapper = styled(Flex)`
-  position: absolute;
-  right: 0;
-  bottom: 0;
-`
-
-const topLeftImage = {
-  path: '/images/home/flying-pancakes/',
-  attributes: [
-    { src: '1-bottom', alt: 'Pancake flying on the bottom' },
-    { src: '1-left', alt: 'Pancake flying on the left' },
-    { src: '1-top', alt: 'Pancake flying on the top' },
-  ],
-}
-
-const bottomRightImage = {
-  path: '/images/home/flying-pancakes/',
-  attributes: [
-    { src: '2-bottom', alt: 'Pancake flying on the bottom' },
-    { src: '2-top', alt: 'Pancake flying on the top' },
-    { src: '2-right', alt: 'Pancake flying on the right' },
-  ],
-}
-
 const Footer = () => {
   const { t } = useTranslation()
   const { address: account } = useAccount()
-  const { isTablet, isDesktop, isMobile } = useMatchBreakpoints()
+  const { isMobile } = useMatchBreakpoints()
 
   return (
     <Box p="48px">
       <BgWrapper>
-        <Flex alignItems="center" justifyContent="center" width="100%" height="100%">
+        <Flex position="relative" zIndex={2} alignItems="center" justifyContent="center" width="100%" height="100%">
           <StyledSunburst />
         </Flex>
+        <ImageBox />
       </BgWrapper>
-      {(isTablet || isDesktop) && (
-        <FloatingPancakesWrapper>
-          <TopLeftImgWrapper>
-            <CompositeImage {...topLeftImage} maxHeight="256px" />
-          </TopLeftImgWrapper>
-          <BottomRightImgWrapper>
-            <CompositeImage {...bottomRightImage} maxHeight="256px" />
-          </BottomRightImgWrapper>
-        </FloatingPancakesWrapper>
-      )}
+
       <Wrapper>
-        <Text mb="24px" fontWeight={600} color="#F4EEFF" fontSize={isMobile ? 32 : 40}>
+        <Text mb="24px" fontWeight={600} color="#F4EEFF" textAlign="center" fontSize={isMobile ? 32 : 40}>
           {t("Join Everyone's Favorite DEX Now!")}
         </Text>
         {!account && <ConnectWalletButton mt="24px" />}
