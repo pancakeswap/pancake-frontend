84a2a249da9b176302e4437f2eac66d8e8392de5
chefmomota <momota@pancakeswap.com>
2023-09-21 10:21:15 +0800

    fix: Conflict

--- apps/web/src/utils/llamaPrice.ts
+++ apps/web/src/utils/llamaPrice.ts
@@ -1,4 +1,6 @@
 import { ChainId } from '@pancakeswap/chains'
+import { CAKE } from '@pancakeswap/tokens'
+import { getCakePriceFromOracle } from 'hooks/useCakePrice'
 
 const CHAIN_MAPPING = {
   [ChainId.ETHEREUM]: 'ethereum',
@@ -16,13 +18,39 @@ export const fetchTokenUSDValue = async (chainId: number, tokenAddresses: string
 
   const list = tokenAddresses.map((address) => `${CHAIN_MAPPING[chainId]}:${address}`).join(',')
 
-  const result: { coins: { [key: string]: { price: string } } } = await fetch(
-    `https://coins.llama.fi/prices/current/${list}`,
-  ).then((res) => res.json())
+  let tokenPriceArray: { coins: { [key: string]: { price: string } } } = {
+    coins: {},
+  }
+
+  await fetch(`https://coins.llama.fi/prices/current/${list}`).then(async (res) => {
+    const data = await res.json()
+    tokenPriceArray = {
+      coins: {
+        ...tokenPriceArray.coins,
+        ...data.coins,
+      },
+    }
+  })
 
   const commonTokenUSDValue = new Map<string, string>()
 
-  Object.entries(result.coins || {}).forEach(([key, value]) => {
+  await Promise.all(
+    tokenAddresses.map(async (address) => {
+      if (address === CAKE[chainId].address) {
+        const cakePrice = parseFloat(await getCakePriceFromOracle())
+        tokenPriceArray = {
+          coins: {
+            ...tokenPriceArray.coins,
+            [`${CHAIN_MAPPING[chainId]}:${address}`]: {
+              price: cakePrice.toString(),
+            },
+          },
+        }
+      }
+    }),
+  )
+
+  Object.entries(tokenPriceArray.coins || {}).forEach(([key, value]) => {
     const [, address] = key.split(':')
     commonTokenUSDValue.set(address, value.price)
   })
