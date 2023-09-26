617a9d9900951bd34632423807a24a4f07bbcddc
BingBong <bingbong@pancakseswap.com>
2023-09-26 08:30:09 +0100

    list arb and base farms

--- packages/tokens/src/8453.ts
+++ packages/tokens/src/8453.ts
@@ -15,4 +15,5 @@ export const baseTokens = {
   ),
   usdbc: new ERC20Token(ChainId.BASE, '0xd9aAEc86B65D86f6A7B5B1b0c42FFA531710b6CA', 6, 'USDbC', 'USD Base Coin'),
   dai: new ERC20Token(ChainId.BASE, '0x50c5725949A6F0c72E6C4a641F24049A917DB0Cb', 18, 'DAI', 'Dai Stablecoin'),
+  tbtc: new ERC20Token(ChainId.BASE, '0x236aa50979D5f3De3Bd1Eeb40E81137F22ab794b', 18, 'tBTC', 'Base tBTC'),
 }
