c3725c9625f9870efeecb7099e8b6bf7061a2d8f
Chef Jerry <jerry@pancakeswap.com>
2023-09-27 15:43:52 +0800

    fix: Domain chainId can only accept number

--- packages/permit2-sdk/src/utils/types.ts
+++ packages/permit2-sdk/src/utils/types.ts
@@ -1,12 +1,10 @@
-import { BigintIsh } from '@pancakeswap/sdk'
-
 export type Bytes = ArrayLike<number>
 type BytesLike = string | Bytes
 
 export interface TypedDataDomain {
   name?: string
   version?: string
-  chainId?: BigintIsh
+  chainId?: number
   verifyingContract?: string
   salt?: BytesLike
 }
