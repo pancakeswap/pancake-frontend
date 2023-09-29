d8c3e1dfb7ea3e37457945903a7177121520d146
chefilip <philip@pancakeswap.com>
2023-09-29 07:51:36 -0400

    chore: Update text

--- apps/web/src/views/FixedStaking/index.tsx
+++ apps/web/src/views/FixedStaking/index.tsx
@@ -70,7 +70,7 @@ const FixedStaking = () => {
         <Flex justifyContent="space-between" flexDirection={['column', null, null, 'row']}>
           <Flex flex="1" flexDirection="column" mr={['8px', 0]}>
             <Heading as="h1" scale="xxl" color="secondary" mb="24px">
-              {t('Fixed Staking')}
+              {t('Simple Staking')}
             </Heading>
             <Heading scale="md" color="text">
               {t('Single-Sided Simple Earn Staking')}
