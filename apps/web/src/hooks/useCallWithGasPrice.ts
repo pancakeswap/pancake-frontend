f4ae0348390876f0d9f5e9986a5197bb02861285
chefjackson <116779127+chefjackson@users.noreply.github.com>
2023-10-09 11:45:10 +0800

    chore: fix missing type

--- apps/web/src/hooks/useCallWithGasPrice.ts
+++ apps/web/src/hooks/useCallWithGasPrice.ts
@@ -11,7 +11,7 @@ import {
   InferFunctionName,
   WriteContractParameters,
 } from 'viem'
-import { EstimateContractGasParameters } from 'viem/dist/types/actions/public/estimateContractGas'
+import type { EstimateContractGasParameters } from 'viem'
 import { useWalletClient } from 'wagmi'
 import { SendTransactionResult } from 'wagmi/actions'
 import { calculateGasMargin } from 'utils'
