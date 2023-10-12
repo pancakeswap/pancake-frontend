83d6f396a819b4e2e77148ef225ca56d57f9bba7
chefmomota <momota@pancakeswap.com>
2023-10-12 12:40:03 +0800

    feat: List new alp pool

--- packages/pools/src/constants/pools/42161.ts
+++ packages/pools/src/constants/pools/42161.ts
@@ -5,6 +5,15 @@ import { PoolCategory, SerializedPool } from '../../types'
 
 export const livePools: SerializedPool[] = [
   {
+    sousId: 2,
+    stakingToken: arbitrumTokens.alp,
+    earningToken: arbitrumTokens.cake,
+    contractAddress: '0x0639c5715EC308E16f089c96C0C109302d76FA81',
+    poolCategory: PoolCategory.CORE,
+    tokenPerSecond: '0.01177',
+    version: 3,
+  },
+  {
     sousId: 1,
     stakingToken: arbitrumTokens.alp,
     earningToken: arbitrumTokens.cake,
