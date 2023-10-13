25051808e3d5a70e1cd789720e79a27404081380
chefmomota <momota@pancakeswap.com>
2023-10-13 10:03:53 +0800

    feat: Add All Article

---
+++ apps/web/src/views/Game/components/Community/CategoriesSelector.tsx
@@ -0,0 +1,38 @@
+import { useMemo } from 'react'
+import { Flex, Button } from '@pancakeswap/uikit'
+import { Categories } from 'views/Home/types'
+import { useTranslation } from '@pancakeswap/localization'
+
+interface CategoriesSelectorProps {
+  selected: number
+  categoriesData: Categories[]
+  childMargin: string
+  setSelected: (value: number) => void
+}
+
+export const CategoriesSelector = ({ selected, categoriesData, childMargin, setSelected }: CategoriesSelectorProps) => {
+  const { t } = useTranslation()
+
+  const allCategories = useMemo(() => {
+    const firstCategories = { id: 0, name: t('All') }
+    return [firstCategories, ...categoriesData]
+  }, [categoriesData, t])
+
+  return (
+    <Flex flexDirection={['row', 'row', 'row', 'row', 'row', 'row', 'column']}>
+      {allCategories?.map((category) => (
+        <Button
+          key={category.id}
+          scale="sm"
+          display="block"
+          width="fit-content"
+          m={childMargin}
+          variant={selected === category.id ? 'subtle' : 'light'}
+          onClick={() => setSelected(category.id)}
+        >
+          {category.name}
+        </Button>
+      ))}
+    </Flex>
+  )
+}
