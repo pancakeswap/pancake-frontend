b776ccd9ea7e677dac2ade287c15775a57135fa6
memoyil <2213635+memoyil@users.noreply.github.com>
2023-10-20 20:12:23 +0200

    perf: (swap): Don't request all token highlights when in bsc

--- apps/web/src/views/Swap/components/HotTokenList/useList.tsx
+++ apps/web/src/views/Swap/components/HotTokenList/useList.tsx
@@ -9,34 +9,33 @@ import { useActiveChainId } from 'hooks/useActiveChainId'
 import { ChainId } from '@pancakeswap/chains'
 import { parseV3TokenData, parseV2TokenData } from './utils'
 
-export const useBSCWhiteList = () => {
+export const useChainWhiteList = (chainId?: number) => {
   const listsByUrl = useAtomValue(selectorByUrlsAtom)
-  const lists = listsByUrl[PANCAKE_EXTENDED]
-  const list = lists?.current
   const whiteList = useMemo(() => {
+    const list = listsByUrl[PANCAKE_EXTENDED]?.current
     return list ? list.tokens.map((t) => t.address.toLowerCase()) : []
-  }, [list])
+  }, [listsByUrl])
+  if (chainId !== ChainId.BSC) return null
   return whiteList
 }
 
-// TODO: refactor -> remove chain specific logic
 export const useTokenHighLightList = () => {
   const { chainId } = useActiveChainId()
-  const bscWhiteList = useBSCWhiteList()
-  const allTokensFromBSC = useTokenDatasSWR(chainId === ChainId.BSC ? bscWhiteList : [], false)
-  const allTokensFromETH = useAllTokenHighLight(multiChainName[chainId])
+  const chainWhiteList = useChainWhiteList(chainId)
+  const allTokensFromWhiteList = useTokenDatasSWR(chainWhiteList || [], false)
+  const allTokensFromChain = useAllTokenHighLight(!chainWhiteList, multiChainName[chainId])
   const tokenAddresses: string[] = useMemo(
-    () => allTokensFromETH?.map(({ address }) => address) || [],
-    [allTokensFromETH],
+    () => allTokensFromChain?.map(({ address }) => address) || [],
+    [allTokensFromChain],
   )
-  const allV3TokensFromV3 = useTokensData(chainId === ChainId.BSC ? bscWhiteList : tokenAddresses, chainId)
+  const allV3TokensFromV3 = useTokensData(chainWhiteList || tokenAddresses, chainId)
 
   const tokens = useMemo(() => {
     return {
-      v2Tokens: (chainId === ChainId.BSC ? allTokensFromBSC : allTokensFromETH)?.map(parseV2TokenData),
+      v2Tokens: (allTokensFromWhiteList || allTokensFromChain)?.map(parseV2TokenData),
       v3Tokens: allV3TokensFromV3?.map(parseV3TokenData) ?? [],
     }
-  }, [allTokensFromBSC, allTokensFromETH, allV3TokensFromV3, chainId])
+  }, [allTokensFromWhiteList, allTokensFromChain, allV3TokensFromV3])
 
   return tokens
 }
