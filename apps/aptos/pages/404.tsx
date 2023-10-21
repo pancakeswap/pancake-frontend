85c7080b91e384ba233d8471179706fa115b8fda
Philip <97418926+chefilip@users.noreply.github.com>
2023-10-21 01:31:10 -0400

    refactor(uikit): remove next-seo (#8181)

--- apps/aptos/pages/404.tsx
+++ apps/aptos/pages/404.tsx
@@ -1,5 +1,10 @@
 import { NotFound } from '@pancakeswap/uikit'
+import { NextSeo } from 'next-seo'
 
-const NotFoundPage = () => <NotFound />
+const NotFoundPage = () => (
+  <NotFound>
+    <NextSeo title="404" />
+  </NotFound>
+)
 
 export default NotFoundPage
