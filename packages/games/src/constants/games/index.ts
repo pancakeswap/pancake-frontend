8e4e74efd8af5ecc7b91f43cd9547de2c98ce337
chefmomota <momota@pancakeswap.com>
2023-10-23 15:31:20 +0800

    feat: Add example data

--- packages/games/src/constants/games/index.ts
+++ packages/games/src/constants/games/index.ts
@@ -1,7 +1,10 @@
 import shuffle from 'lodash/shuffle'
+import { Game } from '../../types'
+// Project
+import { example } from './example'
 
-export const TOP_GAMES_LIST = []
+export const TOP_GAMES_LIST: Game[] = []
 
-export const DEFAULT_GAMES_LIST = []
+export const DEFAULT_GAMES_LIST: Game[] = [example]
 
 export const GAMES_LIST = [...TOP_GAMES_LIST, ...shuffle(DEFAULT_GAMES_LIST)]
