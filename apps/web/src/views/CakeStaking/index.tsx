40f0202b0e331f795f1cc389b3ef697dbe1beee4
jerry <jerry@pancakeswap.com>
2023-11-01 13:58:48 +0800

    feat(ui): staking page header

--- apps/web/src/views/CakeStaking/index.tsx
+++ apps/web/src/views/CakeStaking/index.tsx
@@ -1,5 +1,15 @@
 import { useTranslation } from '@pancakeswap/localization'
-import { Flex, Heading, PageHeader } from '@pancakeswap/uikit'
+import {
+  ArrowForwardIcon,
+  Box,
+  Button,
+  Flex,
+  Heading,
+  NextLinkFromReactRouter,
+  PageHeader,
+  Text,
+} from '@pancakeswap/uikit'
+import { NewCakeStakingCard } from './components/NewCakeStakingCard'
 
 const CakeStaking = () => {
   const { t } = useTranslation()
@@ -11,12 +21,27 @@ const CakeStaking = () => {
             <Heading as="h1" scale="xxl" color="secondary" mb="24px">
               {t('Cake Staking')}
             </Heading>
-            {/* <Heading scale="md" color="text">
-              {t(
-                'Enjoy the benefits of weekly CAKE yield, revenue share, gauges voting, farm yield boosting, participating in IFOs, and so much more!',
-              )}
-            </Heading> */}
+            <Box maxWidth="537px">
+              <Text color="textSubtle">
+                {t(
+                  'Enjoy the benefits of weekly CAKE yield, revenue share, gauges voting, farm yield boosting, participating in IFOs, and so much more!',
+                )}
+              </Text>
+            </Box>
+            {/* @todo: @ChefJerry add link */}
+            <NextLinkFromReactRouter to="/tbd" prefetch={false}>
+              <Button p="0" variant="text">
+                <Text color="primary" bold fontSize="16px" mr="4px">
+                  {t('Get CAKE')}
+                </Text>
+                <ArrowForwardIcon color="primary" />
+              </Button>
+            </NextLinkFromReactRouter>
           </Flex>
+
+          <Box>
+            <NewCakeStakingCard />
+          </Box>
         </Flex>
       </PageHeader>
     </>
