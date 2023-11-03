ac1366eb4cc2038bfe254be22d2ed36fd7f25336
chefmomota <momota@pancakeswap.com>
2023-11-03 12:38:18 +0800

    feat: Update pancake protectors config

--- packages/games/src/constants/games/example.ts
+++ packages/games/src/constants/games/example.ts
@@ -1,25 +1,25 @@
 import { GameType, PostersLayout, PostersItemDataType } from '../../types'
 
-export const example: GameType = {
+export const pancakeProtectors: GameType = {
   id: 'pancake-protectors',
-  projectName: 'Pancake Protector',
+  projectName: 'Mobox',
   title: 'Pancake Protector',
   subTitle: 'Unlock the Power of CAKE and Perks for Pancake Squad and Bunnies Holders',
   description:
-    'Immerse yourself in the exhilarating world of Pancake Protector with this meticulously crafted collection of NFTs. Designed exclusively for the game, these NFTs bestow extraordinary attribute bonuses upon your heroes, elevating their capabilities to unmatched heights. Step into the realm of epic battles and heroic quests as you wield the power of these remarkable artefact. But that’s not all - the Exclusive Fashion NFTs unlock captivating and exclusive visual transformations, ensuring your designated heroes stand out in style. Prepare for an unforgettable adventure and unleash the full potential of these extraordinary NFTs in your quest to become a true champion of Pancake Protector!',
+    'PancakeSwap and Mobox joined forces to launch a tower-defense and PvP game tailored for GameFi players, as well as CAKE, Pancake Squad, and Bunnies holders.',
   publishDate: 1698044972,
-  headerImage: '/images/game/example-header.jpeg',
+  headerImage: 'https://protectors.pancakeswap.finance/assets/pancakeswap-game-landing/header.jpeg',
   headerIconImage: {
-    desktop: '/images/game/example-desktop-header-icon.png',
-    mobile: '/images/game/example-mobile-header-icon.png',
+    desktop: 'https://protectors.pancakeswap.finance/assets/pancakeswap-game-landing/desktop-header-icon.png',
+    mobile: 'https://protectors.pancakeswap.finance/assets/pancakeswap-game-landing/mobile-header-icon.png',
   },
   projectLogo: {
-    lightTheme: 'https://pancakeswap.finance/images/ifos/sable-bg.png',
-    darkTheme: 'https://pancakeswap.finance/images/ifos/sable-bg.png',
+    lightTheme: 'https://protectors.pancakeswap.finance/assets/pancakeswap-game-landing/project-logo-light-theme.png',
+    darkTheme: 'https://protectors.pancakeswap.finance/assets/pancakeswap-game-landing/project-logo-dark-theme.png',
   },
   projectCircleLogo: {
-    lightTheme: 'https://pancakeswap.finance/images/ifos/sable-bg.png',
-    darkTheme: 'https://pancakeswap.finance/images/ifos/sable-bg.png',
+    lightTheme: 'https://protectors.pancakeswap.finance/assets/pancakeswap-game-landing/mobox-circle-logo.png',
+    darkTheme: 'https://protectors.pancakeswap.finance/assets/pancakeswap-game-landing/mobox-circle-logo.png',
   },
   gameLink: 'https://protectors.pancakeswap.finance/',
   posters: {
@@ -27,23 +27,19 @@ export const example: GameType = {
     items: [
       {
         type: PostersItemDataType.Image,
-        image:
-          'https://cdn.akamai.steamstatic.com/steam/apps/578080/ss_4454f310776c626a76baeca2d05fd82bd17c6ee0.600x338.jpg?t=1694608943',
+        image: 'https://protectors.pancakeswap.finance/assets/pancakeswap-game-landing/1.png',
       },
       {
-        type: PostersItemDataType.Video,
-        image: 'https://cdn.akamai.steamstatic.com/steam/apps/256962886/movie.293x165.jpg?t=1691652642',
-        video: 'https://cdn.akamai.steamstatic.com/steam/apps/256962886/movie480_vp9.webm?t=1691652642',
+        type: PostersItemDataType.Image,
+        image: 'https://protectors.pancakeswap.finance/assets/pancakeswap-game-landing/2.jpg',
       },
       {
-        type: PostersItemDataType.Video,
-        image: 'https://cdn.akamai.steamstatic.com/steam/apps/256957737/movie.293x165.jpg?t=1689138251',
-        video: 'https://cdn.akamai.steamstatic.com/steam/apps/256957737/movie480_vp9.webm?t=1689138251',
+        type: PostersItemDataType.Image,
+        image: 'https://protectors.pancakeswap.finance/assets/pancakeswap-game-landing/3.png',
       },
       {
-        type: PostersItemDataType.Video,
-        image:
-          'https://cdn.akamai.steamstatic.com/steam/apps/578080/ss_108e2981889423b057b778cd07ae25ac18406cf1.1920x1080.jpg?t=1694608943',
+        type: PostersItemDataType.Image,
+        image: 'https://protectors.pancakeswap.finance/assets/pancakeswap-game-landing/4.png',
       },
     ],
   },
