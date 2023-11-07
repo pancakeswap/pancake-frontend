37703b48932aa98a1255f15f6b3ccb45681f49c9
chefmomota <momota@pancakeswap.com>
2023-11-07 10:05:23 +0800

    feat: Pancake Protectors always be first

--- packages/games/src/constants/games/index.ts
+++ packages/games/src/constants/games/index.ts
@@ -4,8 +4,8 @@ import { GameType } from '../../types'
 import { pancakeProtectors } from './pancakeProtectors'
 import { binaryX } from './binaryX'
 
-export const TOP_GAMES_LIST: GameType[] = []
+export const TOP_GAMES_LIST: GameType[] = [pancakeProtectors]
 
-export const DEFAULT_GAMES_LIST: GameType[] = [binaryX, pancakeProtectors]
+export const DEFAULT_GAMES_LIST: GameType[] = [binaryX]
 
 export const GAMES_LIST = [...TOP_GAMES_LIST, ...shuffle(DEFAULT_GAMES_LIST)]
