aca4c44f0125673d40d405342b21936d2bf571a5
chefmomota <momota@pancakeswap.com>
2023-11-08 15:27:38 +0800

    feat: Update fetchAlpBoostedPoolApr format

--- apps/web/src/utils/fetchAlpBoostedPoolApr.ts
+++ apps/web/src/utils/fetchAlpBoostedPoolApr.ts
@@ -3,6 +3,8 @@ import { ChainId } from '@pancakeswap/chains'
 import { getViemClients } from 'utils/viem'
 import { CONTRACT_ADDRESS } from './fetchTokenAplPrice'
 
+const WEEK = 7
+const YEAR = 365
 const API_URL = 'https://perp.pancakeswap.finance/bapi/futures/v1/public/future/apx/fee/info?chain=ARB'
 
 export const fetchAlpBoostedPoolApr = async () => {
@@ -45,17 +47,18 @@ export const fetchAlpBoostedPoolApr = async () => {
     })
 
     const totalValueUsd = totalValue
-      .map((i) => new BigNumber(i.valueUsd.toString()).div(10 ** i.decimals).toNumber())
+      .map((i) => new BigNumber(i.valueUsd.toString()).div(1e18).toNumber())
       .reduce((a, b) => a + b, 0)
 
     const response = await fetch(API_URL)
     const result = await response.json()
 
     const { alpFundingFee, alpTradingFee, alpLipFee } = result.data
-    const fee = new BigNumber(alpFundingFee).plus(alpTradingFee).plus(alpLipFee)
-    const feeApr = fee.div(totalValueUsd.toString())
+    const apr = new BigNumber(alpFundingFee).plus(alpTradingFee).plus(alpLipFee)
+    const totalApr = apr.div(totalValueUsd.toString()).div(WEEK)
 
-    return feeApr.toNumber()
+    const apy = totalApr.plus(1).exponentiatedBy(YEAR).minus(1).times(100)
+    return apy.toNumber()
   } catch (error) {
     console.info('Fetch ALP boosted fee error: ', error)
     return 0
