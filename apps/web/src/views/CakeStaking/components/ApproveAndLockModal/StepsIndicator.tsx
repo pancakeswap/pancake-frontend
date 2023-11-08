1c611eb6e1ef83574bc7751854b24859990cfa09
jerry <jerry@pancakeswap.com>
2023-11-08 13:51:44 +0800

    feat(ui): Step Indicator

---
+++ apps/web/src/views/CakeStaking/components/ApproveAndLockModal/StepsIndicator.tsx
@@ -0,0 +1,54 @@
+import { useTranslation } from '@pancakeswap/localization'
+import { Box, Flex, Text } from '@pancakeswap/uikit'
+import styled from 'styled-components'
+import { ApproveAndLockStatus } from 'views/CakeStaking/types'
+
+const StepsContainer = styled(Flex)`
+  width: 100px;
+  height: 8px;
+  border-radius: 4px;
+  margin: 16px auto auto auto;
+  overflow: hidden;
+  background: ${({ theme }) => theme.colors.input};
+`
+
+type StepProps = {
+  active: boolean
+}
+
+const Step = styled('div').withConfig({ shouldForwardProp: (prop) => prop !== 'active' })<StepProps>`
+  height: 100%;
+  background: ${({ theme, active }) => (active ? theme.colors.secondary : theme.colors.input)};
+  flex: 1;
+`
+
+const STEPS = [
+  {
+    status: ApproveAndLockStatus.APPROVING_TOKEN,
+  },
+  {
+    status: ApproveAndLockStatus.APPROVE_PENDING,
+  },
+  {
+    status: ApproveAndLockStatus.PENDING_CONFIRMATION,
+  },
+]
+
+export const StepsIndicator = ({ currentStep }) => {
+  const { t } = useTranslation()
+  return (
+    <Box mt="32px">
+      <Text fontSize="12px" textAlign="center" color="textSubtle">
+        {t('Proceed in your wallet')}
+      </Text>
+
+      <>
+        <StepsContainer>
+          {STEPS.map(({ status }) => {
+            return <Step key={status} active={currentStep === status} />
+          })}
+        </StepsContainer>
+      </>
+    </Box>
+  )
+}
