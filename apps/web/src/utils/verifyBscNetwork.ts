18e376a29ace8284f5e50615e52e7f9ce9526c18
memoyil <2213635+memoyil@users.noreply.github.com>
2023-11-09 16:48:32 +0100

    Update apps/web/src/utils/verifyBscNetwork.ts
    Co-authored-by: chefjackson <116779127+chefjackson@users.noreply.github.com>

--- apps/web/src/utils/verifyBscNetwork.ts
+++ apps/web/src/utils/verifyBscNetwork.ts
@@ -1,5 +1,5 @@
 import { ChainId } from '@pancakeswap/chains'
 
 export const verifyBscNetwork = (chainId?: number) => {
-  return chainId && (chainId === ChainId.BSC || chainId === ChainId.BSC_TESTNET)
+  return Boolean(chainId && (chainId === ChainId.BSC || chainId === ChainId.BSC_TESTNET))
 }
