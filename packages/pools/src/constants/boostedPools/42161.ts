38e187c1f1f758526ab343c79c49a2519e53feb8
chefmomota <momota@pancakeswap.com>
2023-11-09 11:12:35 +0800

    chore: Clean code

--- packages/pools/src/constants/boostedPools/42161.ts
+++ packages/pools/src/constants/boostedPools/42161.ts
@@ -1,6 +1,5 @@
 export const arbBoostedPools = [
   {
     contractAddress: '0x0639c5715EC308E16f089c96C0C109302d76FA81',
-    aprApi: 'https://perp.pancakeswap.finance/bapi/futures/v1/public/future/apx/fee/info?chain=ARB',
   },
 ]
