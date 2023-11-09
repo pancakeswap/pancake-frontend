02b47cab41abd620cbfe411dc89f523c7f9582b0
chefmomota <momota@pancakeswap.com>
2023-11-09 11:10:52 +0800

    chore: Clean code

--- apps/web/src/utils/fetchTokenAplPrice.ts
+++ apps/web/src/utils/fetchTokenAplPrice.ts
@@ -4,7 +4,7 @@ import BigNumber from 'bignumber.js'
 import { getBalanceAmount } from '@pancakeswap/utils/formatBalance'
 
 // Contract will return price with 10^8, although ALP token decimals is 18.
-export const TOKEN_DECIMALS = 8
+const TOKEN_DECIMALS = 8
 export const CONTRACT_ADDRESS = '0xB3879E95a4B8e3eE570c232B19d520821F540E48'
 
 export const fetchTokenAplPrice = async () => {
