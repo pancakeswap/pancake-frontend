b670942b0938d13b3dd8f8838baf3246aec948a5
jerry <jerry@pancakeswap.com>
2023-11-14 16:51:35 +0800

    feat(ui): GaugesTable

---
+++ apps/web/src/views/GaugesVoting/components/GaugesTable/index.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import styled from 'styled-components'
+import { TableHeader } from './TableHeader'
+import { ExpandRow, TableRow } from './TableRow'
+
+const Scrollable = styled.div.withConfig({ shouldForwardProp: (prop) => !['expanded'].includes(prop) })<{
+  expanded: boolean
+}>`
+  overflow-y: auto;
+  height: ${({ expanded }) => (expanded ? 'auto' : '192px')};
+`
+
+export const GaugesTable = () => {
+  const [expanded, setExpanded] = useState(false)
+  return (
+    <>
+      <TableHeader />
+      <Scrollable expanded={expanded}>
+        {new Array(15).fill(0).map((i) => (
+          <TableRow key={i} />
+        ))}
+      </Scrollable>
+      <ExpandRow onCollapse={() => setExpanded(!expanded)} />
+    </>
+  )
+}
---
+++ apps/web/src/views/GaugesVoting/components/GaugesTable/styled.tsx
@@ -0,0 +1,33 @@
+import styled, { css } from 'styled-components'
+
+export const Row = css`
+  border-top: 1px solid ${({ theme }) => theme.colors.cardBorder};
+  border-bottom: 1px solid ${({ theme }) => theme.colors.cardBorder};
+  display: grid;
+  grid-template-columns: 3fr repeat(3, 1fr);
+  padding: 16px 24px 16px 16px;
+
+  & :nth-child(3) {
+    justify-content: center;
+  }
+
+  & :nth-child(4) {
+    text-align: right;
+  }
+`
+
+export const THeader = styled.thead`
+  ${Row}
+`
+
+export const TRow = styled.tr`
+  ${Row}
+
+  &:first-of-type {
+    border-top: none;
+  }
+
+  &:last-of-type {
+    border-bottom: none;
+  }
+`
