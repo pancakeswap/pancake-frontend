87349ce04c1b15d50cf0a0b4e65b36f3ac093532
jerry <jerry@pancakeswap.com>
2023-11-14 12:54:12 +0800

    feat: logo

---
+++ apps/web/src/views/GaugesVoting/components/TripleLogo.tsx
@@ -0,0 +1,48 @@
+import { Token } from '@pancakeswap/sdk'
+import { Box } from '@pancakeswap/uikit'
+import { CurrencyLogo } from 'components/Logo'
+import { ChainLogo } from 'components/Logo/ChainLogo'
+import { useMemo } from 'react'
+import styled from 'styled-components'
+import { safeGetAddress } from 'utils'
+import { Address } from 'viem'
+
+const StyledDualLogo = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: baseline;
+  justify-content: center;
+`
+
+export const TripleLogo: React.FC<{
+  address0?: Address
+  address1?: Address
+  chainId?: number
+}> = ({ address0, address1, chainId }) => {
+  console.debug('debug', {
+    address0: safeGetAddress(address0),
+    address1: safeGetAddress(address1),
+    chainId,
+  })
+  const currency0 = useMemo(() => {
+    return chainId && safeGetAddress(address0) ? new Token(chainId, safeGetAddress(address0)!, 18, '') : undefined
+  }, [address0, chainId])
+  const currency1 = useMemo(() => {
+    return chainId && safeGetAddress(address1) ? new Token(chainId, safeGetAddress(address1)!, 18, '') : undefined
+  }, [address1, chainId])
+  return (
+    <StyledDualLogo>
+      {currency0 ? (
+        <Box mr="-5.62px">
+          <CurrencyLogo currency={currency0} size="36px" />
+        </Box>
+      ) : null}
+      {currency1 ? <CurrencyLogo currency={currency1} size="36px" /> : null}
+      {chainId ? (
+        <Box ml="-9px">
+          <ChainLogo chainId={chainId} />
+        </Box>
+      ) : null}
+    </StyledDualLogo>
+  )
+}
