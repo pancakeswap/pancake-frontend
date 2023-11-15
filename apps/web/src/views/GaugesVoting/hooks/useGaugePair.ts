48817f529693420f586b9fde7a67796cb65e1841
jerry <jerry@pancakeswap.com>
2023-11-16 03:58:42 +0800

    feat(w.i.p): tooltip data

---
+++ apps/web/src/views/GaugesVoting/hooks/useGaugePair.ts
@@ -0,0 +1,59 @@
+import { useQuery } from '@tanstack/react-query'
+import { PoolData as V3PoolData } from 'views/V3Info/types'
+import { fetchPoolDatas } from 'views/V3Info/data/pool/poolData'
+import { v3InfoClients } from 'utils/graphql'
+import { PoolData as V2PoolData } from 'state/info/types'
+import { fetchAllPoolDataWithAddress } from 'state/info/queries/pools/poolData'
+import { MultiChainName, multiChainName } from 'state/info/constant'
+
+const refreshIntervalForInfo = 15000 // 15s
+const QUERY_SETTINGS_IMMUTABLE = {
+  retry: 3,
+  retryDelay: 3000,
+  keepPreviousData: true,
+  refetchOnMount: false,
+  refetchOnReconnect: false,
+  refetchOnWindowFocus: false,
+}
+const QUERY_SETTINGS_WITHOUT_INTERVAL_REFETCH = {
+  retry: 3,
+  retryDelay: 3000,
+}
+const QUERY_SETTINGS_INTERVAL_REFETCH = {
+  refetchInterval: refreshIntervalForInfo,
+  keepPreviousData: true,
+  ...QUERY_SETTINGS_WITHOUT_INTERVAL_REFETCH,
+}
+
+export const useV3PoolData = (address?: string, chainId?: number): V3PoolData | undefined => {
+  const { data } = useQuery(
+    [`v3/info/pool/poolData/${chainId}/${address}`, chainId],
+    () => fetchPoolDatas(v3InfoClients[chainId!], [address!], []),
+    {
+      enabled: Boolean(chainId && address && address !== 'undefined'),
+      ...QUERY_SETTINGS_IMMUTABLE,
+    },
+  )
+
+  if (!address) return undefined
+
+  console.debug('debug poolData v3', data, { chainId, address })
+
+  return data?.data?.[address] ?? undefined
+}
+
+export const useV2PairData = (address?: string, chainId?: number): V2PoolData | undefined => {
+  const { data } = useQuery(
+    // @fixme support stableswap
+    [`info/pool/data/${address}/swap`, chainId],
+    () => fetchAllPoolDataWithAddress([], multiChainName[chainId!] as MultiChainName, [address!]),
+    {
+      enabled: Boolean(chainId) && Boolean(address),
+      ...QUERY_SETTINGS_IMMUTABLE,
+      ...QUERY_SETTINGS_INTERVAL_REFETCH,
+    },
+  )
+  if (!address) return undefined
+  console.debug('debug poolData v2', data)
+  return data?.[address].data
+}
