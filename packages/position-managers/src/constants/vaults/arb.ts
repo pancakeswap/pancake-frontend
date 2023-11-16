2ab01788563d57547ec941076d5a51a814cc609c
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-11-16 18:24:55 +0800

    chore: update adapterAddress

--- packages/position-managers/src/constants/vaults/arb.ts
+++ packages/position-managers/src/constants/vaults/arb.ts
@@ -9,7 +9,7 @@ export const vaults: VaultConfig[] = [
     idByManager: 1,
     name: 'DEFIEDGE',
     address: '0xa9B98C0cc70a7625aCB12B7dec5D278F317d4Cb0',
-    adapterAddress: '0x58bd28D7C1fAD5BE7AFBA968d5E4C1a42f447ffE',
+    adapterAddress: '0x5a3019d6A889be4DE156972CfDe6CA009583c31D',
     currencyA: arbitrumTokens.weth,
     currencyB: arbitrumTokens.arb,
     earningToken: arbitrumTokens.cake,
