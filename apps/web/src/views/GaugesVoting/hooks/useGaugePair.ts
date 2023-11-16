1e1c87b1d5e788767f18bb64cd439404edf6ca5a
jerry <jerry@pancakeswap.com>
2023-11-17 04:22:47 +0800

    feat(hooks): useGaugePair

--- apps/web/src/views/GaugesVoting/hooks/useGaugePair.ts
+++ apps/web/src/views/GaugesVoting/hooks/useGaugePair.ts
@@ -7,6 +7,8 @@ import { fetchAllPoolDataWithAddress } from 'state/info/queries/pools/poolData'
 import { MultiChainName, multiChainName } from 'state/info/constant'
 import { getDeltaTimestamps } from 'utils/getDeltaTimestamps'
 import { useBlockFromTimeStampQuery } from 'views/Info/hooks/useBlocksFromTimestamps'
+import { useMemo } from 'react'
+import { feeTierPercent } from 'views/V3Info/utils'
 
 const QUERY_SETTINGS_IMMUTABLE = {
   retry: 3,
@@ -50,4 +52,35 @@ export const useV2PairData = (address?: string, chainId?: number): V2PoolData | 
   if (!address) return undefined
   console.debug('debug poolData v2', data)
   return data?.[address]?.data
+}
+
+export const useGaugePair = (address?: string, chainId?: number) => {
+  const v2 = useV2PairData(address, chainId)
+  const v3 = useV3PoolData(address, chainId)
+
+  const token0 = useMemo(() => {
+    if (v2) return v2.token0
+    if (v3) return v3.token0
+    return undefined
+  }, [v2, v3])
+
+  const token1 = useMemo(() => {
+    if (v2) return v2.token1
+    if (v3) return v3.token1
+    return undefined
+  }, [v2, v3])
+
+  const pairName = useMemo(() => {
+    return `${token0?.symbol}-${token1?.symbol}`
+  }, [token0?.symbol, token1?.symbol])
+
+  const feeTier = useMemo(() => (v3?.feeTier ? feeTierPercent(v3?.feeTier) : undefined), [v3?.feeTier])
+  return {
+    v2,
+    v3,
+    token0,
+    token1,
+    pairName,
+    feeTier,
+  }
 }
