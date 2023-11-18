bcc989a9cc8391e962cffd0b0ec3171504499bdc
jerry <jerry@pancakeswap.com>
2023-11-19 02:28:10 +0800

    fix: weight unit

--- apps/web/src/views/GaugesVoting/components/ChartLabel.tsx
+++ apps/web/src/views/GaugesVoting/components/ChartLabel.tsx
@@ -1,7 +1,8 @@
 import { useTranslation } from '@pancakeswap/localization'
 import { Percent } from '@pancakeswap/swap-sdk-core'
 import { AutoColumn, Text } from '@pancakeswap/uikit'
-import formatLocalisedCompactNumber from '@pancakeswap/utils/formatBalance'
+import formatLocalisedCompactNumber, { getBalanceNumber } from '@pancakeswap/utils/formatBalance'
+import BN from 'bignumber.js'
 import { useMemo } from 'react'
 import { GaugeVoting } from '../hooks/useGaugesVoting'
 
@@ -13,13 +14,17 @@ export const ChartLabel: React.FC<{
   const percent = useMemo(() => {
     return new Percent(gauge?.weight ?? 0, total || 1).toFixed(2)
   }, [total, gauge])
+  const weight = useMemo(() => {
+    return getBalanceNumber(new BN(gauge?.weight || total))
+  }, [gauge?.weight, total])
+
   return (
     <AutoColumn alignItems="center" justifyContent="center" textAlign="center">
       <Text textTransform="uppercase" fontWeight={600} fontSize={12}>
         {t('total')}
       </Text>
       <Text bold fontSize={16}>
-        {`${formatLocalisedCompactNumber(gauge?.weight || total, true)} veCAKE`}
+        {`${formatLocalisedCompactNumber(weight, true)} veCAKE`}
       </Text>
       {gauge?.weight ? (
         <Text fontSize={14} color="textSubtle">
