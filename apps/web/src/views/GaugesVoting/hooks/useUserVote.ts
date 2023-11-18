b10b8f442b4ea0752fb8ca869e6ffca89d6fe5b6
jerry <jerry@pancakeswap.com>
2023-11-19 02:12:06 +0800

    feat: add vote lock

--- apps/web/src/views/GaugesVoting/hooks/useUserVote.ts
+++ apps/web/src/views/GaugesVoting/hooks/useUserVote.ts
@@ -1,27 +1,50 @@
 import { useQuery } from '@tanstack/react-query'
+import dayjs from 'dayjs'
+import useAccountActiveChain from 'hooks/useAccountActiveChain'
 import { useGaugesVotingContract } from 'hooks/useContract'
 import { Hex } from 'viem'
-import { useAccount } from 'wagmi'
+import { usePublicClient } from 'wagmi'
 import { GaugeVoting } from './useGaugesVoting'
 
 export type VotedSlope = {
   slope: number
   power: number
   end: number
+  lastVoteTime: number
+  voteLocked: boolean
 }
 
 export const useUserVote = (gauge?: GaugeVoting) => {
-  const { address: account } = useAccount()
   const contract = useGaugesVotingContract()
+  const { account, chainId } = useAccountActiveChain()
+  const publicClient = usePublicClient({ chainId })
+
   const { data } = useQuery(
     ['/vecake/userVoteSlopes', contract.address, gauge?.hash, account],
     async (): Promise<VotedSlope> => {
-      const [slope, power, end] = (await contract.read.voteUserSlopes([account!, gauge?.hash as Hex])) ?? 0n
+      const response = await publicClient.multicall({
+        contracts: [
+          {
+            ...contract,
+            functionName: 'voteUserSlopes',
+            args: [account!, gauge?.hash as Hex],
+          },
+          {
+            ...contract,
+            functionName: 'lastUserVote',
+            args: [account!, gauge?.hash as Hex],
+          },
+        ],
+        allowFailure: false,
+      })
+      const [[slope, power, end], lastVoteTime] = response
 
       return {
         slope: Number(slope),
         power: Number(power),
         end: Number(end),
+        lastVoteTime: Number(lastVoteTime),
+        voteLocked: dayjs.unix(Number(lastVoteTime)).add(7, 'day').isBefore(dayjs()),
       }
     },
     {
