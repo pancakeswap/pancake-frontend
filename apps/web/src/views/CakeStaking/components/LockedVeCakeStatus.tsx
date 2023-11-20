b1685ddf12778cc4a14fd4d29328727685c863c4
jerry <jerry@pancakeswap.com>
2023-11-20 22:59:46 +0800

    feat: tooltip

--- apps/web/src/views/CakeStaking/components/LockedVeCakeStatus.tsx
+++ apps/web/src/views/CakeStaking/components/LockedVeCakeStatus.tsx
@@ -28,6 +28,8 @@ import { formatTime } from 'utils/formatTime'
 import { CakeLockStatus } from 'views/CakeStaking/types'
 import { useCakeLockStatus } from '../hooks/useVeCakeUserInfo'
 import { useWriteMigrateCallback } from '../hooks/useContractWrite/useWriteMigrateCallback'
+import { Tooltips } from './Tooltips'
+import { useProxyVeCakeBalance } from '../hooks/useProxyVeCakeBalance'
 
 dayjs.extend(relativeTime)
 
@@ -35,8 +37,26 @@ export const LockedVeCakeStatus: React.FC<{
   status: CakeLockStatus
 }> = ({ status }) => {
   const { balance } = useVeCakeBalance()
+  const { balance: proxyBalance } = useProxyVeCakeBalance()
+  const balanceBN = useMemo(() => getBalanceNumber(balance), [balance])
+  const proxyCake = useMemo(() => getBalanceNumber(proxyBalance), [proxyBalance])
   if (status === CakeLockStatus.NotLocked) return null
 
+  console.debug('debug balance', getBalanceNumber(proxyBalance))
+  const balanceText =
+    balanceBN < 0.01 ? (
+      <UnderlineText fontSize="20px" bold color={balance.eq(0) ? 'failure' : 'secondary'}>
+        {`< 0.01`}
+      </UnderlineText>
+    ) : (
+      <UnderlinedBalance
+        fontSize="20px"
+        bold
+        color={balance.eq(0) ? 'failure' : 'secondary'}
+        value={getBalanceNumber(balance)}
+        decimals={2}
+      />
+    )
   return (
     <Box maxWidth={['100%', '369px']}>
       <Card isActive>
@@ -44,13 +64,11 @@ export const LockedVeCakeStatus: React.FC<{
           <RowBetween>
             <AutoColumn>
               <Heading color="text">My VeCAKE</Heading>
-              <Balance
-                fontSize="20px"
-                bold
-                color={balance.eq(0) ? 'failure' : 'secondary'}
-                value={getBalanceNumber(balance)}
-                decimals={2}
-              />
+              {proxyBalance.gt(0) ? (
+                <Tooltips content={<FromProxyTooltip proxyCake={proxyCake} />}>{balanceText}</Tooltips>
+              ) : (
+                balanceText
+              )}
             </AutoColumn>
             <img srcSet="/images/cake-staking/token-vecake.png 2x" alt="token-vecake" />
           </RowBetween>
@@ -72,17 +90,18 @@ const CUSTOM_WARNING_COLOR = '#D67E0A'
 
 const LockedInfo = () => {
   const { t } = useTranslation()
-  const cakePrice = useCakePrice()
   const migrate = useWriteMigrateCallback()
-  const { cakeLockedAmount, cakeUnlockTime, shouldMigrate, cakePoolLockExpired } = useCakeLockStatus()
-  const cakeLocked = useMemo(() => Number(formatBigInt(cakeLockedAmount, 18)), [cakeLockedAmount])
-  const cakeLockedUsdValue: number = useMemo(() => {
-    return cakePrice.times(cakeLocked).toNumber()
-  }, [cakePrice, cakeLocked])
-
-  const unlocked = cakeUnlockTime > 0 && dayjs().isAfter(dayjs.unix(cakeUnlockTime))
-  const unlockTimeToNow = cakeUnlockTime ? dayjs.unix(cakeUnlockTime).fromNow(true) : ''
-
+  const {
+    cakeUnlockTime,
+    shouldMigrate,
+    nativeCakeLockedAmount,
+    proxyCakeLockedAmount,
+    cakePoolLocked,
+    cakePoolUnlockTime,
+    cakeLocked,
+    cakeLockExpired,
+    cakePoolLockExpired,
+  } = useCakeLockStatus()
   return (
     <FlexGap flexDirection="column" gap="24px" margin={24}>
       {shouldMigrate ? (
@@ -101,28 +120,21 @@ const LockedInfo = () => {
             <Text fontSize={12} color="textSubtle" textTransform="uppercase" bold>
               {t('cake locked')}
             </Text>
-            <Balance value={cakeLocked} decimals={2} fontWeight={600} fontSize={20} />
-            <Balance prefix="~" value={cakeLockedUsdValue} decimals={2} unit="USD" fontSize={12} />
+            <CakeLocked nativeCakeLocked={nativeCakeLockedAmount} proxyCakeLocked={proxyCakeLockedAmount} />
           </AutoColumn>
           <AutoColumn>
             <Text fontSize={12} color="textSubtle" textTransform="uppercase" bold>
               {t('unlocks in')}
             </Text>
-            {unlocked ? (
-              <Text fontWeight={600} fontSize={20} color={CUSTOM_WARNING_COLOR}>
-                {t('Unlocked')}
-              </Text>
-            ) : (
-              <Text fontWeight={600} fontSize={20}>
-                {unlockTimeToNow}
-              </Text>
-            )}
-
-            {cakeUnlockTime ? (
-              <Text fontSize={12} color={unlocked ? CUSTOM_WARNING_COLOR : undefined}>
-                {t('on')} {formatTime(Number(dayjs.unix(cakeUnlockTime)))}
-              </Text>
-            ) : null}
+            <CakeUnlockAt
+              proxyCakeLocked={proxyCakeLockedAmount}
+              nativeLocked={cakeLocked}
+              nativeExpired={cakeLockExpired}
+              proxyLocked={cakePoolLocked}
+              proxyExpired={cakePoolLockExpired}
+              nativeUnlockTime={cakeUnlockTime}
+              proxyUnlockTime={cakePoolUnlockTime}
+            />
           </AutoColumn>
         </RowBetween>
         {shouldMigrate ? (
@@ -152,7 +164,7 @@ const LockedInfo = () => {
           </Link>
         </>
       ) : null}
-      {unlocked ? (
+      {cakeLockExpired ? (
         <Message variant="warning" icon={<InfoFilledIcon color={CUSTOM_WARNING_COLOR} />}>
           <Text as="p" color={CUSTOM_WARNING_COLOR}>
             {t(
@@ -170,7 +182,7 @@ const LockedInfo = () => {
           </Text>
         </Message>
       ) : null}
-      {!unlocked && !shouldMigrate ? (
+      {!cakeLockExpired && !shouldMigrate ? (
         <Flex justifyContent="center">
           <img src="/images/cake-staking/my-cake-bunny.png" alt="my-cake-bunny" width="254px" />
         </Flex>
@@ -178,3 +190,154 @@ const LockedInfo = () => {
     </FlexGap>
   )
 }
+
+const FromProxyTooltip: React.FC<{
+  proxyCake: number
+}> = ({ proxyCake }) => {
+  const { t } = useTranslation()
+
+  return (
+    <>
+      {t('%amount% veCAKE from CAKE Pool migrated position.', { amount: proxyCake })}
+      <Link href="http://@todo">
+        {t('Learn More')} {'>>'}
+      </Link>
+    </>
+  )
+}
+
+const ProxyUnlockTooltip: React.FC<{
+  proxyExpired: boolean
+  proxyCake: number
+  proxyUnlockTime: number
+}> = ({ proxyExpired, proxyCake, proxyUnlockTime }) => {
+  const { t } = useTranslation()
+
+  return (
+    <>
+      {t(
+        proxyExpired
+          ? '%amount% CAKE from CAKE Pool migrated position is already unlocked. Go to the pool page to withdraw these CAKE.'
+          : '%amount% CAKE from CAKE Pool migrated position will unlock on %expiredAt%.',
+        {
+          amount: proxyCake,
+          expiredAt: formatTime(Number(dayjs.unix(proxyUnlockTime))),
+        },
+      )}
+      <Link href="http://@todo">
+        {t('Learn More')} {'>>'}
+      </Link>
+    </>
+  )
+}
+
+export const CakeLocked: React.FC<{
+  nativeCakeLocked: bigint
+  proxyCakeLocked: bigint
+}> = ({ nativeCakeLocked, proxyCakeLocked }) => {
+  const cakePrice = useCakePrice()
+  const nativeCake = useMemo(() => Number(formatBigInt(nativeCakeLocked, 18)), [nativeCakeLocked])
+  const nativeCakeUsdValue: number = useMemo(() => {
+    return cakePrice.times(nativeCake).toNumber()
+  }, [cakePrice, nativeCake])
+  const proxyCake = useMemo(() => Number(formatBigInt(proxyCakeLocked, 18)), [proxyCakeLocked])
+  const totalCake = useMemo(
+    () => Number(formatBigInt(nativeCakeLocked + proxyCakeLocked, 18)),
+    [nativeCakeLocked, proxyCakeLocked],
+  )
+  const totalCakeUsdValue: number = useMemo(() => {
+    return cakePrice.times(totalCake).toNumber()
+  }, [cakePrice, totalCake])
+
+  if (!proxyCakeLocked && nativeCakeLocked) {
+    return (
+      <>
+        <Balance value={nativeCake} decimals={2} fontWeight={600} fontSize={20} />
+        <Balance prefix="~" value={nativeCakeUsdValue} decimals={2} unit="USD" fontSize={12} />
+      </>
+    )
+  }
+
+  return (
+    <>
+      <Tooltips content={<FromProxyTooltip proxyCake={proxyCake} />}>
+        <UnderlinedBalance value={totalCake} decimals={2} fontWeight={600} fontSize={20} />
+      </Tooltips>
+      <Balance prefix="~" value={totalCakeUsdValue} decimals={2} unit="USD" fontSize={12} />
+    </>
+  )
+}
+
+const CakeUnlockAt: React.FC<{
+  proxyCakeLocked: bigint
+  nativeLocked: boolean
+  nativeExpired: boolean
+  proxyLocked: boolean
+  proxyExpired: boolean
+  nativeUnlockTime: number
+  proxyUnlockTime: number
+}> = ({
+  proxyCakeLocked,
+  nativeLocked,
+  nativeExpired,
+  nativeUnlockTime,
+  proxyLocked,
+  proxyExpired,
+  proxyUnlockTime,
+}) => {
+  const { t } = useTranslation()
+  const proxyCake = useMemo(() => Number(formatBigInt(proxyCakeLocked, 18)), [proxyCakeLocked])
+
+  const [unlocked, unlockTime, unlockTimeToNow] = useMemo(() => {
+    if (!nativeLocked && proxyLocked) {
+      return [proxyExpired, proxyUnlockTime, proxyUnlockTime ? dayjs.unix(proxyUnlockTime).fromNow(true) : '']
+    }
+    return [nativeExpired, nativeUnlockTime, nativeUnlockTime ? dayjs.unix(nativeUnlockTime).fromNow(true) : '']
+  }, [nativeExpired, nativeLocked, nativeUnlockTime, proxyExpired, proxyLocked, proxyUnlockTime])
+
+  const TextComp = proxyLocked ? UnderlineText : Text
+
+  const unlockText = (
+    <>
+      {unlocked ? (
+        <TextComp fontWeight={600} fontSize={20} color={CUSTOM_WARNING_COLOR}>
+          {t('Unlocked')}
+        </TextComp>
+      ) : (
+        <TextComp fontWeight={600} fontSize={20}>
+          {unlockTimeToNow}
+        </TextComp>
+      )}
+
+      {unlockTime ? (
+        <Text fontSize={12} color={unlocked ? CUSTOM_WARNING_COLOR : undefined}>
+          {t('on')} {formatTime(Number(dayjs.unix(unlockTime)))}
+        </Text>
+      ) : null}
+    </>
+  )
+
+  if (!proxyLocked) return unlockText
+
+  return (
+    <Tooltips
+      content={
+        <ProxyUnlockTooltip proxyExpired={proxyExpired} proxyCake={proxyCake} proxyUnlockTime={proxyUnlockTime} />
+      }
+    >
+      {unlockText}
+    </Tooltips>
+  )
+}
+
+const UnderlinedBalance = styled(Balance)`
+  text-decoration: underline dotted;
+  text-decoration-color: ${({ theme }) => theme.colors.textSubtle};
+  text-underline-offset: 0.1em;
+`
+
+const UnderlineText = styled(Text)`
+  text-decoration: underline dotted;
+  text-decoration-color: currentColor;
+  text-underline-offset: 0.1em;
+`
