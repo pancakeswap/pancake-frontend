c51e0ebdec998d20a4c4bae35173fc1ba100302d
jerry <jerry@pancakeswap.com>
2023-11-20 16:49:04 +0800

    chore: testnet rpc

--- apps/web/src/config/nodes.ts
+++ apps/web/src/config/nodes.ts
@@ -93,7 +93,7 @@ export const PUBLIC_NODES = {
     'https://bsc-dataseed1.defibit.io',
     'https://bsc-dataseed1.binance.org',
   ].filter(Boolean),
-  [ChainId.BSC_TESTNET]: ['https://data-seed-prebsc-1-s1.binance.org:8545'],
+  [ChainId.BSC_TESTNET]: ['http://178.128.17.220:8545'],
   [ChainId.ETHEREUM]: [
     getNodeRealUrl(ChainId.ETHEREUM, process.env.NEXT_PUBLIC_NODE_REAL_API_ETH) || '',
     process.env.NEXT_PUBLIC_NODIES_ETH || '',
