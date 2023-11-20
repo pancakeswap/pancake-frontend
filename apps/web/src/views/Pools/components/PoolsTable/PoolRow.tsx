bbacd7d6a994eaa2c9314cc661aa78fbe89ac7c9
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-11-20 08:03:34 +0800

    chore: update row

--- apps/web/src/views/Pools/components/PoolsTable/PoolRow.tsx
+++ apps/web/src/views/Pools/components/PoolsTable/PoolRow.tsx
@@ -1,16 +1,18 @@
 import { useMatchBreakpoints } from '@pancakeswap/uikit'
 import { Pool } from '@pancakeswap/widgets-internal'
 import { memo, useCallback, useMemo } from 'react'
-
 import { getBalanceNumber } from '@pancakeswap/utils/formatBalance'
 import { useDeserializedPoolByVaultKey, usePool, useVaultPoolByKey } from 'state/pools/hooks'
 import { VaultKey } from 'state/types'
-
 import { VeCakeBenefitCard } from 'views/CakeStaking/components/SyrupPool/VeCakeCard'
+
 import ActionPanel from './ActionPanel/ActionPanel'
 import AprCell from './Cells/AprCell'
+import AutoAprCell from './Cells/AutoAprCell'
+import AutoEarningsCell from './Cells/AutoEarningsCell'
 import EarningsCell from './Cells/EarningsCell'
 import NameCell from './Cells/NameCell'
+import StakedCell from './Cells/StakedCell'
 import TotalStakedCell from './Cells/TotalStakedCell'
 
 export const VaultPoolRow: React.FC<
@@ -28,22 +30,28 @@ export const VaultPoolRow: React.FC<
     return getBalanceNumber(totalCakeInVault, stakingToken.decimals)
   }, [stakingToken.decimals, totalCakeInVault])
 
+  const vaultData = useVaultPoolByKey(vaultKey)
+
   return (
     <Pool.ExpandRow initialActivity={initialActivity} panel={<ActionPanel account={account} pool={pool} expanded />}>
       <NameCell pool={pool} />
       <Pool.BaseCell style={{ padding: 0, justifyContent: 'center', flexGrow: 1 }}>
-        <VeCakeBenefitCard isTableView />
+        {!account && <VeCakeBenefitCard isTableView />}
       </Pool.BaseCell>
-      {/* {isXLargerScreen && <AutoEarningsCell pool={pool} account={account} />}
-      {isXLargerScreen ? <StakedCell pool={pool} account={account} /> : null}
-      <AutoAprCell pool={pool} />
-      {isLargerScreen && (
-        <TotalStakedCell
-          stakingToken={stakingToken}
-          totalStaked={totalStaked}
-          totalStakedBalance={totalStakedBalance}
-        />
-      )} */}
+      {account && (
+        <>
+          {isXLargerScreen && <AutoEarningsCell pool={pool} account={account} />}
+          {isXLargerScreen ? <StakedCell pool={pool} account={account} /> : null}
+          <AutoAprCell pool={pool} />
+          {isLargerScreen && (
+            <TotalStakedCell
+              stakingToken={stakingToken}
+              totalStaked={totalStaked}
+              totalStakedBalance={totalStakedBalance}
+            />
+          )}
+        </>
+      )}
     </Pool.ExpandRow>
   )
 })
