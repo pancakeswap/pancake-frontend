a5ee20b0470dbecd32fe8391084ac67662d627c5
jerry <jerry@pancakeswap.com>
2023-11-20 17:31:57 +0800

    fix: total ifo sold

---
+++ apps/web/src/views/CakeStaking/hooks/useTotalIFOSold.ts
@@ -0,0 +1,14 @@
+import { getTotalIFOSold } from '@pancakeswap/ifos'
+import { useActiveChainId } from 'hooks/useActiveChainId'
+import { useEffect, useState } from 'react'
+
+export const useTotalIFOSold = () => {
+  const { chainId } = useActiveChainId()
+  const [sold, setSold] = useState(0)
+
+  useEffect(() => {
+    getTotalIFOSold(chainId).then(setSold)
+  }, [chainId])
+
+  return sold
+}
