efd8f4fc47721b7b1637b1992c50cfe261b4cbfb
jerry <jerry@pancakeswap.com>
2023-11-21 18:25:02 +0800

    fix: lock data set calc

--- apps/web/src/views/CakeStaking/components/DataSet/LockCakeDataSet.tsx
+++ apps/web/src/views/CakeStaking/components/DataSet/LockCakeDataSet.tsx
@@ -2,11 +2,13 @@ import { useTranslation } from '@pancakeswap/localization'
 import { TooltipText } from '@pancakeswap/uikit'
 import { getBalanceAmount, getDecimalAmount } from '@pancakeswap/utils/formatBalance'
 import BN from 'bignumber.js'
+import { WEEK } from 'config/constants/veCake'
 import dayjs from 'dayjs'
 import { useVeCakeBalance } from 'hooks/useTokenBalance'
 import { useMemo } from 'react'
 import { useLockCakeData } from 'state/vecake/hooks'
 import { getVeCakeAmount } from 'utils/getVeCakeAmount'
+import { useCurrentBlockTimestamp } from 'views/CakeStaking/hooks/useCurrentBlockTimestamp'
 import { useCakeLockStatus } from 'views/CakeStaking/hooks/useVeCakeUserInfo'
 import { Tooltips } from '../Tooltips'
 import { DataBox, DataHeader, DataRow } from './DataBox'
@@ -16,20 +18,21 @@ export const LockCakeDataSet = () => {
   const { t } = useTranslation()
   const { balance: veCakeBalance } = useVeCakeBalance()
   const { cakeUnlockTime, cakeLockedAmount } = useCakeLockStatus()
-  const { cakeLockAmount, cakeLockWeeks } = useLockCakeData()
+  const { cakeLockAmount } = useLockCakeData()
   const amountInputBN = useMemo(() => getDecimalAmount(new BN(cakeLockAmount || 0)), [cakeLockAmount])
   const amountLockedBN = useMemo(() => getBalanceAmount(new BN(cakeLockedAmount.toString() || '0')), [cakeLockedAmount])
   const amount = useMemo(() => {
     return getBalanceAmount(amountInputBN.plus(amountLockedBN))
   }, [amountInputBN, amountLockedBN])
-  const veCakeAmount = useMemo(
-    () => getBalanceAmount(veCakeBalance).plus(getVeCakeAmount(cakeLockAmount, cakeLockWeeks)),
-    [cakeLockAmount, cakeLockWeeks, veCakeBalance],
-  )
+  const currentTimestamp = useCurrentBlockTimestamp()
+  const veCakeAmount = useMemo(() => {
+    const remainLockWeeks = Math.floor((cakeUnlockTime - currentTimestamp) / WEEK)
+    return getBalanceAmount(veCakeBalance).plus(getVeCakeAmount(cakeLockAmount, remainLockWeeks))
+  }, [cakeLockAmount, cakeUnlockTime, currentTimestamp, veCakeBalance])
 
   const newUnlockTime = useMemo(() => {
-    return formatDate(dayjs.unix(cakeUnlockTime).add(Number(cakeLockWeeks), 'weeks'))
-  }, [cakeLockWeeks, cakeUnlockTime])
+    return formatDate(dayjs.unix(Number(cakeUnlockTime)))
+  }, [cakeUnlockTime])
 
   return (
     <DataBox gap="8px">
