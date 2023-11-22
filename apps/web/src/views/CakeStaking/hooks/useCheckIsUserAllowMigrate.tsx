437729ea9a12ad0d9f434e4654ff08562e8ce160
jerry <jerry@pancakeswap.com>
2023-11-22 18:36:09 +0800

    chore: reset time

--- apps/web/src/views/CakeStaking/hooks/useCheckIsUserAllowMigrate.tsx
+++ apps/web/src/views/CakeStaking/hooks/useCheckIsUserAllowMigrate.tsx
@@ -1,16 +1,18 @@
 import { useMemo } from 'react'
+import { useCurrentBlockTimestamp } from './useCurrentBlockTimestamp'
 
 export const useCheckIsUserAllowMigrate = (lockEndTime?: string) => {
+  const currentTimestamp = useCurrentBlockTimestamp()
   const lockEndTimeAsNumber = useMemo(() => {
     return lockEndTime ? parseInt(lockEndTime) : 0
   }, [lockEndTime])
-  const nextUTCThursdayTimeStamp = getNextUTCThursday()
+  const nextUTCThursdayTimeStamp = getNextUTCThursday(currentTimestamp)
 
   return lockEndTimeAsNumber > nextUTCThursdayTimeStamp
 }
 
-const getNextUTCThursday = () => {
-  const d = new Date()
+const getNextUTCThursday = (timestamp: number) => {
+  const d = new Date(timestamp * 1000)
   d.setUTCDate(d.getUTCDate() + ((4 + 7 - d.getUTCDay()) % 7 || 7))
   d.setUTCHours(0)
   d.setUTCMinutes(0)
