f5b6f6aeeaa1875f2a24c616feada45be80a1965
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2023-11-22 22:03:13 +0800

    chore: Update learn more link

--- apps/web/src/views/CakeStaking/components/SyrupPool/VeCakeMigrateCard.tsx
+++ apps/web/src/views/CakeStaking/components/SyrupPool/VeCakeMigrateCard.tsx
@@ -3,6 +3,7 @@ import { Box, Flex, Text } from '@pancakeswap/uikit'
 import { memo } from 'react'
 import { useCheckIsUserAllowMigrate } from '../../hooks/useCheckIsUserAllowMigrate'
 import { useIsMigratedToVeCake } from '../../hooks/useIsMigratedToVeCake'
+import { LearnMoreLink } from './LearnMoreLink'
 import { VeCakeButton } from './VeCakeButton'
 import { ShineStyledBox } from './VeCakeCard'
 
@@ -18,17 +19,21 @@ export const VeCakeMigrateCard: React.FC<{ isTableView?: boolean; lockEndTime?: 
           <img src="/images/cake-staking/token-vecake.png" alt="token-vecake" width="38px" />
           <Box>
             <Text color="white" bold fontSize={14} pr="20px">
-              {isMigratedToVeCake
-                ? t('Your CAKE pool position has been migrated to veCAKE.')
-                : t(
-                    'All fixed term staking positions must migrate to veCAKE to continue receiving rewards. Learn more',
-                  )}
+              {isMigratedToVeCake ? (
+                t('Your CAKE pool position has been migrated to veCAKE.')
+              ) : (
+                <>
+                  {t('All fixed term staking positions must migrate to veCAKE to continue receiving rewards.')}
+                  <LearnMoreLink />
+                </>
+              )}
             </Text>
             {isMigratedToVeCake && (
               <Text mt="10px" color="white" bold fontSize={14} pr="20px">
                 {t(
-                  'Extending or adding CAKE is not available for migrated positions. You will be able to withdraw CAKE when the lock ends. Learn more',
+                  'Extending or adding CAKE is not available for migrated positions. You will be able to withdraw CAKE when the lock ends.',
                 )}
+                <LearnMoreLink />
               </Text>
             )}
           </Box>
