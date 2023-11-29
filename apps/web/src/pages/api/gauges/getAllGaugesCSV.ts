494ec0fb4790132ee18887af6efe894d95ba7969
Chef Jerry <jerry@pancakeswap.com>
2023-11-29 22:23:36 +0800

    feat: csv api

---
+++ apps/web/src/pages/api/gauges/getAllGaugesCSV.ts
@@ -0,0 +1,36 @@
+import { getAllGauges } from '@pancakeswap/gauges'
+import { NextApiHandler } from 'next'
+
+const MAX_CACHE_SECONDS = 60 * 5
+
+const keys = [
+  'pid',
+  'gid',
+  'pairName',
+  'pairAddress',
+  'hash',
+  'chainId',
+  'weight',
+  'boostMultiplier',
+  'maxVoteCap',
+  'type',
+  'feeTier',
+  'masterChef',
+]
+
+const handler: NextApiHandler = async (req, res) => {
+  const gauges = await getAllGauges()
+
+  res.setHeader('Cache-Control', `max-age=0, s-maxage=${MAX_CACHE_SECONDS}, stale-while-revalidate`)
+  const data = [keys.join(',')] as unknown[]
+  gauges.forEach((gauge) => {
+    const row = [] as unknown[]
+    keys.forEach((key) => {
+      row.push(gauge[key])
+    })
+    data.push(row.join(','))
+  })
+  return res.status(200).write(data.join('\n'))
+}
+
+export default handler
