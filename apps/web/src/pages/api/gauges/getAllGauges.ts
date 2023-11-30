5bffc383e03abb927886ca22faea4f86b9e777eb
Chef Jerry <jerry@pancakeswap.com>
2023-11-30 14:54:33 +0800

    feat(api): support testnet

--- apps/web/src/pages/api/gauges/getAllGauges.ts
+++ apps/web/src/pages/api/gauges/getAllGauges.ts
@@ -5,13 +5,22 @@ import { stringify } from 'viem'
 const MAX_CACHE_SECONDS = 60 * 5
 
 const handler: NextApiHandler = async (req, res) => {
-  const gauges = await getAllGauges()
+  const useTestnet = req.query.testnet
+  try {
+    const gauges = await getAllGauges({
+      testnet: Boolean(useTestnet),
+    })
 
-  res.setHeader('Cache-Control', `max-age=0, s-maxage=${MAX_CACHE_SECONDS}, stale-while-revalidate`)
-  return res.status(200).json({
-    data: JSON.parse(stringify(gauges)),
-    lastUpdated: Number(Date.now()),
-  })
+    res.setHeader('Cache-Control', `max-age=0, s-maxage=${MAX_CACHE_SECONDS}, stale-while-revalidate`)
+    return res.status(200).json({
+      data: JSON.parse(stringify(gauges)),
+      lastUpdated: Number(Date.now()),
+    })
+  } catch (err) {
+    return res.status(500).json({
+      error: err,
+    })
+  }
 }
 
 export default handler
