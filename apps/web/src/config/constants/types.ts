eff33e1cb90f435bb4933c2371429d205b991f41
Chef Jerry <jerry@pancakeswap.com>
2023-11-30 14:54:19 +0800

    chore: remove deprecated config

--- apps/web/src/config/constants/types.ts
+++ apps/web/src/config/constants/types.ts
@@ -2,7 +2,6 @@ import { ChainId } from '@pancakeswap/chains'
 import type { FarmConfigBaseProps, SerializedFarmConfig } from '@pancakeswap/farms'
 import { Currency, CurrencyAmount, Percent, Price, Token, Trade, TradeType } from '@pancakeswap/sdk'
 import { LegacyTradeWithStableSwap as TradeWithStableSwap } from '@pancakeswap/smart-router/legacy-router'
-import { FeeAmount } from '@pancakeswap/v3-sdk'
 import BigNumber from 'bignumber.js'
 import { Address } from 'wagmi'
 
@@ -188,66 +187,3 @@ export enum Bound {
   LOWER = 'LOWER',
   UPPER = 'UPPER',
 }
-
-export enum GaugeType {
-  // @note: this is a hack
-  // on SC StableSwap & V2 are same value,
-  // just use -1 for StableSwap as id on FE
-  StableSwap = -1,
-  V2 = 0,
-  V3 = 1,
-  ALM = 2,
-  VeCakePool = 3,
-  Aptos = 4,
-}
-
-export const GAUGE_TYPE_NAMES: Record<GaugeType, string> = {
-  [GaugeType.StableSwap]: 'StableSwap',
-  [GaugeType.V2]: 'V2',
-  [GaugeType.V3]: 'V3',
-  [GaugeType.ALM]: 'ALM',
-  [GaugeType.VeCakePool]: 'VeCakePool',
-  [GaugeType.Aptos]: 'Aptos',
-}
-
-export interface GaugeBaseConfig {
-  gid: number
-  chainId: ChainId
-  type: GaugeType
-  address: Address
-}
-
-export interface GaugeV2Config extends GaugeBaseConfig {
-  token0Address: Address
-  token1Address: Address
-  type: GaugeType.V2
-  pairName: string
-  feeTier: FeeAmount
-}
-
-export interface GaugeStableSwapConfig extends GaugeBaseConfig {
-  type: GaugeType.StableSwap
-  pairName: string
-  tokenAddresses: Array<Address>
-}
-export interface GaugeV3Config extends GaugeBaseConfig {
-  token0Address: Address
-  token1Address: Address
-  type: GaugeType.V3
-  feeTier: FeeAmount
-  pairName: string
-}
-
-export interface GaugeALMConfig extends GaugeBaseConfig {
-  type: GaugeType.ALM
-  token0Address: Address
-  token1Address: Address
-  pairName: string
-}
-
-export interface GaugeVeCakePoolConfig extends GaugeBaseConfig {
-  type: GaugeType.VeCakePool
-  pairName: string
-}
-
-export type GaugeConfig = GaugeV2Config | GaugeStableSwapConfig | GaugeV3Config | GaugeALMConfig | GaugeVeCakePoolConfig
