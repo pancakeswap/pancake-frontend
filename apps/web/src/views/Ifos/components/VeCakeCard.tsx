129030d642927cc8b8420de0b3a01d90f73f9edc
chefjackson <116779127+chefjackson@users.noreply.github.com>
2023-12-01 15:50:25 +0800

    feat: add tips and nativate button

--- apps/web/src/views/Ifos/components/VeCakeCard.tsx
+++ apps/web/src/views/Ifos/components/VeCakeCard.tsx
@@ -1,4 +1,18 @@
 import { Ifo } from '@pancakeswap/widgets-internal'
+import { Button } from '@pancakeswap/uikit'
+import { useTranslation } from '@pancakeswap/localization'
+import Link from 'next/link'
+import { SpaceProps } from 'styled-system'
+
+function NavigateButton(props: SpaceProps) {
+  const { t } = useTranslation()
+
+  return (
+    <Button width="100%" as={Link} href="/cake-staking" {...props}>
+      {t('Go to CAKE Staking')}
+    </Button>
+  )
+}
 
 export function VeCakeCard() {
   const header = (
@@ -12,6 +26,8 @@ export function VeCakeCard() {
     <Ifo.VeCakeCard header={header}>
       <Ifo.MyVeCake amount={1000000} />
       <Ifo.ICakeInfo mt="1.5rem" snapshot={Date.now() / 1000} />
+      <Ifo.ZeroVeCakeTips mt="1.5rem" />
+      <NavigateButton mt="1.5rem" />
     </Ifo.VeCakeCard>
   )
 }
