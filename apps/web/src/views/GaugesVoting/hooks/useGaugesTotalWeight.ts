679bb5e3519baf78f5c15b39ce4f9a97c191dc96
Chef Jerry <jerry@pancakeswap.com>
2023-12-01 14:34:17 +0800

    refactor: read weight from calc sc

--- apps/web/src/views/GaugesVoting/hooks/useGaugesTotalWeight.ts
+++ apps/web/src/views/GaugesVoting/hooks/useGaugesTotalWeight.ts
@@ -1,14 +1,11 @@
 import { useQuery } from '@tanstack/react-query'
-import { useGaugesVotingContract } from 'hooks/useContract'
-import { useAtomValue } from 'jotai'
-import { gaugesInCapAtom } from 'state/vecake/atoms'
+import { useCalcGaugesVotingContract } from 'hooks/useContract'
 
 export const useGaugesTotalWeight = () => {
-  const gaugesVotingContract = useGaugesVotingContract()
-  const inCap = useAtomValue(gaugesInCapAtom)
+  const gaugesVotingContract = useCalcGaugesVotingContract()
 
-  const { data } = useQuery(['gaugesTotalWeight', gaugesVotingContract.address, inCap], async () => {
-    return gaugesVotingContract.read.getTotalWeight([inCap])
+  const { data } = useQuery(['gaugesTotalWeight', gaugesVotingContract.address], async () => {
+    return gaugesVotingContract.read.getTotalWeight([true])
   })
 
   return data ?? 0n
