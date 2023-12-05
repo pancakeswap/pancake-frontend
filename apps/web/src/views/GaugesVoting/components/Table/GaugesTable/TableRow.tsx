4198a117daa3e3e92dde8eaf6ae16cbcd0e7110c
jerry <jerry@pancakeswap.com>
2023-12-06 00:22:42 +0800

    fix: icons

--- apps/web/src/views/GaugesVoting/components/Table/GaugesTable/TableRow.tsx
+++ apps/web/src/views/GaugesVoting/components/Table/GaugesTable/TableRow.tsx
@@ -2,7 +2,6 @@ import { GAUGE_TYPE_NAMES, Gauge, GaugeType } from '@pancakeswap/gauges'
 import { useTranslation } from '@pancakeswap/localization'
 import { Percent } from '@pancakeswap/sdk'
 import {
-  AddCircleIcon,
   Button,
   CheckmarkCircleFillIcon,
   ChevronDownIcon,
@@ -32,6 +31,24 @@ const SelectButton = styled(Button)`
     background-color: transparent;
   }
 `
+
+const AddIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none">
+      <g clip-path="url(#clip0_7260_24227)">
+        <path
+          d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM16 13H13V16C13 16.55 12.55 17 12 17C11.45 17 11 16.55 11 16V13H8C7.45 13 7 12.55 7 12C7 11.45 7.45 11 8 11H11V8C11 7.45 11.45 7 12 7C12.55 7 13 7.45 13 8V11H16C16.55 11 17 11.45 17 12C17 12.55 16.55 13 16 13Z"
+          fill="#31D0AA"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_7260_24227">
+          <rect width="24" height="24" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  )
+}
 
 export const TableRow: React.FC<{
   data: RowData
@@ -78,10 +95,10 @@ export const TableRow: React.FC<{
                 isHover ? (
                   <CrossIcon color="#ED4B9E" />
                 ) : (
-                  <CheckmarkCircleFillIcon color="disabled" />
+                  <CheckmarkCircleFillIcon color="#BDC2C4" />
                 )
               ) : (
-                <AddCircleIcon color="primary" />
+                <AddIcon />
               )}
             </SelectButton>
           </span>
