ac824555d3eeffc935de94132e5dec763bf3558e
jerry <jerry@pancakeswap.com>
2023-12-07 20:04:45 +0800

    chore: remove forknetwork

--- apps/web/src/config/nodes.ts
+++ apps/web/src/config/nodes.ts
@@ -24,15 +24,14 @@ const ARBITRUM_NODES = [
 ].filter(Boolean)
 
 export const SERVER_NODES = {
-  // [ChainId.BSC]: [
-  //   process.env.NEXT_PUBLIC_NODE_PRODUCTION || '',
-  //   getPoktUrl(ChainId.BSC, process.env.NEXT_PUBLIC_POKT_API_KEY) || '',
-  //   'https://bsc.publicnode.com',
-  //   'https://binance.llamarpc.com',
-  //   'https://bsc-dataseed1.defibit.io',
-  //   'https://bsc-dataseed1.binance.org',
-  // ].filter(Boolean),
-  [ChainId.BSC]: ['https://devnet_1.pancakeswap.ai'],
+  [ChainId.BSC]: [
+    process.env.NEXT_PUBLIC_NODE_PRODUCTION || '',
+    getPoktUrl(ChainId.BSC, process.env.NEXT_PUBLIC_POKT_API_KEY) || '',
+    'https://bsc.publicnode.com',
+    'https://binance.llamarpc.com',
+    'https://bsc-dataseed1.defibit.io',
+    'https://bsc-dataseed1.binance.org',
+  ].filter(Boolean),
   [ChainId.BSC_TESTNET]: ['https://data-seed-prebsc-1-s1.binance.org:8545'],
   [ChainId.ETHEREUM]: [
     getNodeRealUrl(ChainId.ETHEREUM, process.env.SERVER_NODE_REAL_API_ETH) || '',
@@ -84,17 +83,16 @@ export const SERVER_NODES = {
 } satisfies Record<ChainId, readonly string[]>
 
 export const PUBLIC_NODES = {
-  // [ChainId.BSC]: [
-  //   process.env.NEXT_PUBLIC_NODE_PRODUCTION || '',
-  //   getNodeRealUrl(ChainId.BSC, process.env.NEXT_PUBLIC_NODE_REAL_API_ETH) || '',
-  //   process.env.NEXT_PUBLIC_NODIES_BSC || '',
-  //   getPoktUrl(ChainId.BSC, process.env.NEXT_PUBLIC_POKT_API_KEY) || '',
-  //   'https://bsc.publicnode.com',
-  //   'https://binance.llamarpc.com',
-  //   'https://bsc-dataseed1.defibit.io',
-  //   'https://bsc-dataseed1.binance.org',
-  // ].filter(Boolean),
-  [ChainId.BSC]: ['https://devnet_1.pancakeswap.ai'],
+  [ChainId.BSC]: [
+    process.env.NEXT_PUBLIC_NODE_PRODUCTION || '',
+    getNodeRealUrl(ChainId.BSC, process.env.NEXT_PUBLIC_NODE_REAL_API_ETH) || '',
+    process.env.NEXT_PUBLIC_NODIES_BSC || '',
+    getPoktUrl(ChainId.BSC, process.env.NEXT_PUBLIC_POKT_API_KEY) || '',
+    'https://bsc.publicnode.com',
+    'https://binance.llamarpc.com',
+    'https://bsc-dataseed1.defibit.io',
+    'https://bsc-dataseed1.binance.org',
+  ].filter(Boolean),
   [ChainId.BSC_TESTNET]: ['https://data-seed-prebsc-1-s1.binance.org:8545'],
   [ChainId.ETHEREUM]: [
     getNodeRealUrl(ChainId.ETHEREUM, process.env.NEXT_PUBLIC_NODE_REAL_API_ETH) || '',
