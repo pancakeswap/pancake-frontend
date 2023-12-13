01c4a576e2778bfbb65fbe03d6f1e6d4c4ef9a15
chefjackson <116779127+chefjackson@users.noreply.github.com>
2023-12-13 11:22:23 +0800

    feat: binance web3 wallet integration

--- apps/web/src/hooks/useEagerConnect.ts
+++ apps/web/src/hooks/useEagerConnect.ts
@@ -1,6 +1,8 @@
 import { useConfig, useConnect } from 'wagmi'
 import { useEffect } from 'react'
 import { isCyberWallet } from '@cyberlab/cyber-app-sdk'
+import { ChainId } from '@pancakeswap/chains'
+import { isInBinance } from '@binance/w3w-utils'
 
 import { CHAINS } from 'config/chains'
 import { cyberWalletConnector } from 'utils/wagmi'
@@ -12,6 +14,21 @@ const useEagerConnect = () => {
     if (!(typeof window === 'undefined') && window?.parent !== window && isCyberWallet() && cyberWalletConnector) {
       connectAsync({ connector: cyberWalletConnector as any }).catch(() => {
         config.autoConnect()
+      })
+      return
+    }
+
+    if (typeof window !== 'undefined' && isInBinance()) {
+      import('@binance/w3w-wagmi-connector').then(({ getWagmiConnector }) => {
+        const BinanceConnector = getWagmiConnector()
+        const binanceConnector = new BinanceConnector({
+          chains: CHAINS,
+          options: { chainId: ChainId.BSC },
+        })
+
+        connectAsync({ connector: binanceConnector as any }).catch(() => {
+          config.autoConnect()
+        })
       })
       return
     }
