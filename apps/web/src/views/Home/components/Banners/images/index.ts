3e2c0616fb70cc646570ab1c540fbe0f688c28f8
chefmomota <momota@pancakeswap.com>
2023-12-14 13:45:05 +0800

    feat: Remove banner

--- apps/web/src/views/Home/components/Banners/images/index.ts
+++ apps/web/src/views/Home/components/Banners/images/index.ts
@@ -1,200 +1,190 @@
-import lotteryImage from './lottery.png'
+import apolloXCampaignBg from './ApolloXCampaignBg.png'
+import apolloXCampaignBgMobile from './ApolloXCampaignBgMobile.png'
+import apolloXCampaignBunny from './ApolloXCampaignBunny.png'
+import apolloXCampaignBunnyMobile from './ApolloXCampaignBunnyMobile.png'
+import apolloXCampaignTitle from './ApolloXCampaignTitle.png'
+import Aptos from './Aptos.png'
+import AptosXPancakeSwap from './AptosXPancakeSwap.png'
+import ETHBunny from './ETHBunny.png'
 import IFOImage from './IFO.png'
 import IFOMobileImage from './IFOMobile.png'
 import competitionImage from './competition.png'
 import competitionMobileImage from './competitionMobile.png'
-import modImage from './mod-tc-desktop.png'
-import modMobileImage from './mod-tc-mobile.png'
-import lotteryMobileImage from './lotteryMobile.png'
-import modWhiteLogo from './mod-white-logo.png'
-import perpetualImage from './perpetual.png'
-import perpetualMobileImage from './perpetualMobile.png'
-import Aptos from './Aptos.png'
-import AptosXPancakeSwap from './AptosXPancakeSwap.png'
-import trustwalletBg from './trustwalletBg.png'
-import trustwalletBunny from './trustwalletBunny.png'
-import v3AirdropBunny from './v3AirdropBunny.png'
-import v3Board from './v3Board.png'
-import v3BgCoinUp from './v3BgCoinUp.png'
-import v3BgCoinDown from './v3BgCoinDown.png'
-import v3BgBlingNormal from './v3BgBlingNormal.png'
-import v3BgBlingBlur from './v3BgBlingBlur.png'
-import farmV3MigrationBunny from './farmV3MigrationBunny.png'
-import farmV3MigrationMobileBunny from './farmV3MigrationMobileBunny.png'
-import farmV3MigrationBunnyFull from './farmV3MigrationBunnyFull.png'
-import TradingRewardButter from './butter.png'
-import TradingRewardButter2 from './butter-2.png'
-import TradingRewardLoveButter from './love-butter.png'
-import TradingRewardBunny from './trading-reward-bunny.png'
-import TradingRewardBg from './trading-reward-bg.png'
 import ETHXPancakeSwap from './ethXpancakeswap.png'
-import ETHBunny from './ETHBunny.png'
-import v3LaunchBg from './v3LaunchBg.png'
-import v3LaunchBnb from './v3LaunchBnb.png'
-import v3LaunchEth from './v3LaunchEth.png'
-import v3LaunchFlag from './v3LaunchFlag.png'
-import v3LaunchBunny from './v3LaunchBunny.png'
-import v3LaunchBgMobile from './v3LaunchBgMobile.png'
-import v3LaunchBunnyMobile from './v3LaunchBunnyMobile.png'
-import apolloXCampaignBunny from './ApolloXCampaignBunny.png'
-import apolloXCampaignBg from './ApolloXCampaignBg.png'
-import apolloXCampaignTitle from './ApolloXCampaignTitle.png'
-import apolloXCampaignBgMobile from './ApolloXCampaignBgMobile.png'
-import apolloXCampaignBunnyMobile from './ApolloXCampaignBunnyMobile.png'
-import liquidStakingTitle from './liquidStakingTitle.png'
-import liquidStakingTitleMobile from './liquidStakingTitleMobile.png'
+import farmV3MigrationBunny from './farmV3MigrationBunny.png'
+import farmV3MigrationBunnyFull from './farmV3MigrationBunnyFull.png'
+import farmV3MigrationMobileBunny from './farmV3MigrationMobileBunny.png'
 import liquidStakingBunny from './liquidStakingBunny.png'
-import liquidStakingBunnyMobile from './liquidStakingBunnyMobile.png'
 import liquidStakingBunnyBg1 from './liquidStakingBunnyBg1.png'
 import liquidStakingBunnyBg2 from './liquidStakingBunnyBg2.png'
 import liquidStakingBunnyBg2Mobile from './liquidStakingBunnyBg2Mobile.png'
 import liquidStakingBunnyBg3 from './liquidStakingBunnyBg3.png'
 import liquidStakingBunnyBg4 from './liquidStakingBunnyBg4.png'
+import liquidStakingBunnyMobile from './liquidStakingBunnyMobile.png'
+import liquidStakingTitle from './liquidStakingTitle.png'
+import liquidStakingTitleMobile from './liquidStakingTitleMobile.png'
+import lotteryImage from './lottery.png'
+import lotteryMobileImage from './lotteryMobile.png'
+import modImage from './mod-tc-desktop.png'
+import modMobileImage from './mod-tc-mobile.png'
+import modWhiteLogo from './mod-white-logo.png'
+import perpetualImage from './perpetual.png'
+import perpetualMobileImage from './perpetualMobile.png'
+import trustwalletBg from './trustwalletBg.png'
+import trustwalletBunny from './trustwalletBunny.png'
+import v3AirdropBunny from './v3AirdropBunny.png'
+import v3BgBlingBlur from './v3BgBlingBlur.png'
+import v3BgBlingNormal from './v3BgBlingNormal.png'
+import v3BgCoinDown from './v3BgCoinDown.png'
+import v3BgCoinUp from './v3BgCoinUp.png'
+import v3Board from './v3Board.png'
+import v3LaunchBg from './v3LaunchBg.png'
+import v3LaunchBgMobile from './v3LaunchBgMobile.png'
+import v3LaunchBnb from './v3LaunchBnb.png'
+import v3LaunchBunny from './v3LaunchBunny.png'
+import v3LaunchBunnyMobile from './v3LaunchBunnyMobile.png'
+import v3LaunchEth from './v3LaunchEth.png'
+import v3LaunchFlag from './v3LaunchFlag.png'
 
-import galxeTraverseBg from './galxe-traverse-bg.png'
-import galxeTraverseBgMobile from './galxe-traverse-bg-mobile.png'
-import galxeTraverseCloud from './galxe-traverse-cloud.png'
-import galxeTraverseBunny from './galxe-traverse-bunny.png'
-import galxeLogo from './galxe-logo.png'
-import polygonZkBg from './polygon-zk-bg.png'
-import polygonZkBgMobile from './polygon-zk-bg-mobile.png'
-import polygonZkBunny from './polygon-zk-bunny.png'
-import zkSyncBgMobile from './zkSyncBgMobile.png'
-import zkSyncBg from './zkSyncBg.png'
-import zkSyncBunny from './zkSyncBunny.png'
-import eraLogo from './eraLogo.png'
 import arbBg from './arbBg.png'
 import arbBgMobile from './arbBgMobile.png'
 import arbBunny from './arbBunny.png'
 import arbLogo from './arbLogo.png'
+import baseBgMobile from './base-bg-mobile.png'
+import baseBg from './base-bg.png'
+import baseBunny from './base-bunny.png'
+import baseMoon from './base-moon.png'
+import baseTree from './base-tree.png'
+import eraLogo from './eraLogo.png'
+import galxeLogo from './galxe-logo.png'
+import galxeTraverseBgMobile from './galxe-traverse-bg-mobile.png'
+import galxeTraverseBg from './galxe-traverse-bg.png'
+import galxeTraverseBunny from './galxe-traverse-bunny.png'
+import galxeTraverseCloud from './galxe-traverse-cloud.png'
+import gameCube from './game-cube.png'
+import gameDesktopBg from './game-desktop-bg.png'
+import gameDesktopBunny from './game-desktop-bunny.png'
+import gameMobileBunny from './game-mobile-bunny.png'
+import gameMobileText from './game-mobile-text.png'
+import gameText from './game-text.png'
+import lineaBgMobile from './linea-bg-mobile.png'
+import lineaBg from './linea-bg.png'
+import lineaBunnyMobile from './linea-bunny-mobile.png'
+import lineaBunny from './linea-bunny.png'
+import lineaLogo from './linea-logo.png'
+import mercuryoLogo from './mercuryo-logo.png'
 import moonpayBgMobile from './moonpay-bg-mobile.png'
 import moonpayBg from './moonpay-bg.png'
 import moonpayCash from './moonpay-cash.png'
 import moonpayLogo from './moonpay-logo.png'
-import lineaLogo from './linea-logo.png'
-import lineaBg from './linea-bg.png'
-import lineaBgMobile from './linea-bg-mobile.png'
-import lineaBunny from './linea-bunny.png'
-import lineaBunnyMobile from './linea-bunny-mobile.png'
-import baseBg from './base-bg.png'
-import baseBgMobile from './base-bg-mobile.png'
-import baseBunny from './base-bunny.png'
-import baseTree from './base-tree.png'
-import baseMoon from './base-moon.png'
-import mercuryoLogo from './mercuryo-logo.png'
-import opbnbBg from './opbnb-bg.png'
 import opbnbBgMobile from './opbnb-bg-mobile.png'
+import opbnbBg from './opbnb-bg.png'
 import opbnbBunny from './opbnb-bunny.png'
-import gameDesktopBg from './game-desktop-bg.png'
-import gameDesktopBunny from './game-desktop-bunny.png'
-import gameMobileBunny from './game-mobile-bunny.png'
-import gameCube from './game-cube.png'
-import gameText from './game-text.png'
-import gameMobileText from './game-mobile-text.png'
+import polygonZkBgMobile from './polygon-zk-bg-mobile.png'
+import polygonZkBg from './polygon-zk-bg.png'
+import polygonZkBunny from './polygon-zk-bunny.png'
 import vecakeBg from './vecake-bg.png'
-import vecakeMobileBg from './vecake-mobile-bg.png'
 import vecakeDesktopBunny from './vecake-desktop-bunny.png'
+import vecakeMobileBg from './vecake-mobile-bg.png'
 import vecakeMobileBunny from './vecake-mobile-bunny.png'
 import vecakeRuby from './vecake-ruby.png'
 import vecakeTitle from './vecake-title.png'
+import zkSyncBg from './zkSyncBg.png'
+import zkSyncBgMobile from './zkSyncBgMobile.png'
+import zkSyncBunny from './zkSyncBunny.png'
 
 export {
-  lotteryImage,
-  IFOImage,
-  competitionImage,
-  modImage,
-  modMobileImage,
-  lotteryMobileImage,
-  competitionMobileImage,
-  IFOMobileImage,
-  modWhiteLogo,
-  perpetualImage,
-  perpetualMobileImage,
   Aptos,
   AptosXPancakeSwap,
-  trustwalletBg,
-  trustwalletBunny,
-  v3AirdropBunny,
-  v3Board,
-  v3BgCoinUp,
-  v3BgCoinDown,
-  v3BgBlingNormal,
-  v3BgBlingBlur,
-  farmV3MigrationBunny,
-  farmV3MigrationMobileBunny,
-  farmV3MigrationBunnyFull,
-  TradingRewardButter,
-  TradingRewardLoveButter,
-  TradingRewardBunny,
-  TradingRewardBg,
-  TradingRewardButter2,
-  ETHXPancakeSwap,
   ETHBunny,
-  v3LaunchBg,
-  v3LaunchBunny,
-  v3LaunchBgMobile,
-  v3LaunchBunnyMobile,
-  v3LaunchBnb,
-  v3LaunchEth,
-  v3LaunchFlag,
-  apolloXCampaignBunny,
+  ETHXPancakeSwap,
+  IFOImage,
+  IFOMobileImage,
   apolloXCampaignBg,
-  apolloXCampaignTitle,
   apolloXCampaignBgMobile,
+  apolloXCampaignBunny,
   apolloXCampaignBunnyMobile,
-  liquidStakingTitle,
-  liquidStakingTitleMobile,
-  liquidStakingBunny,
-  liquidStakingBunnyMobile,
-  liquidStakingBunnyBg1,
-  liquidStakingBunnyBg2,
-  liquidStakingBunnyBg2Mobile,
-  liquidStakingBunnyBg3,
-  liquidStakingBunnyBg4,
-  galxeTraverseBg,
-  galxeTraverseBgMobile,
-  galxeTraverseCloud,
-  galxeTraverseBunny,
-  galxeLogo,
-  polygonZkBg,
-  polygonZkBgMobile,
-  polygonZkBunny,
-  zkSyncBgMobile,
-  zkSyncBg,
-  zkSyncBunny,
-  eraLogo,
+  apolloXCampaignTitle,
   arbBg,
   arbBgMobile,
   arbBunny,
   arbLogo,
-  moonpayBgMobile,
-  moonpayBg,
-  moonpayCash,
-  moonpayLogo,
-  lineaLogo,
-  lineaBg,
-  lineaBgMobile,
-  lineaBunny,
-  lineaBunnyMobile,
   baseBg,
   baseBgMobile,
   baseBunny,
   baseMoon,
   baseTree,
-  mercuryoLogo,
-  opbnbBg,
-  opbnbBgMobile,
-  opbnbBunny,
+  competitionImage,
+  competitionMobileImage,
+  eraLogo,
+  farmV3MigrationBunny,
+  farmV3MigrationBunnyFull,
+  farmV3MigrationMobileBunny,
+  galxeLogo,
+  galxeTraverseBg,
+  galxeTraverseBgMobile,
+  galxeTraverseBunny,
+  galxeTraverseCloud,
+  gameCube,
   gameDesktopBg,
   gameDesktopBunny,
   gameMobileBunny,
-  gameCube,
-  gameText,
   gameMobileText,
+  gameText,
+  lineaBg,
+  lineaBgMobile,
+  lineaBunny,
+  lineaBunnyMobile,
+  lineaLogo,
+  liquidStakingBunny,
+  liquidStakingBunnyBg1,
+  liquidStakingBunnyBg2,
+  liquidStakingBunnyBg2Mobile,
+  liquidStakingBunnyBg3,
+  liquidStakingBunnyBg4,
+  liquidStakingBunnyMobile,
+  liquidStakingTitle,
+  liquidStakingTitleMobile,
+  lotteryImage,
+  lotteryMobileImage,
+  mercuryoLogo,
+  modImage,
+  modMobileImage,
+  modWhiteLogo,
+  moonpayBg,
+  moonpayBgMobile,
+  moonpayCash,
+  moonpayLogo,
+  opbnbBg,
+  opbnbBgMobile,
+  opbnbBunny,
+  perpetualImage,
+  perpetualMobileImage,
+  polygonZkBg,
+  polygonZkBgMobile,
+  polygonZkBunny,
+  trustwalletBg,
+  trustwalletBunny,
+  v3AirdropBunny,
+  v3BgBlingBlur,
+  v3BgBlingNormal,
+  v3BgCoinDown,
+  v3BgCoinUp,
+  v3Board,
+  v3LaunchBg,
+  v3LaunchBgMobile,
+  v3LaunchBnb,
+  v3LaunchBunny,
+  v3LaunchBunnyMobile,
+  v3LaunchEth,
+  v3LaunchFlag,
   vecakeBg,
-  vecakeMobileBg,
   vecakeDesktopBunny,
+  vecakeMobileBg,
   vecakeMobileBunny,
   vecakeRuby,
   vecakeTitle,
+  zkSyncBg,
+  zkSyncBgMobile,
+  zkSyncBunny,
 }
