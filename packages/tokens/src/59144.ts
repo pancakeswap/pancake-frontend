534271f0bd7cd542f178d779c712773ecea00ea8
chefmomota <momota@pancakeswap.com>
2023-12-18 14:33:49 +0800

    feat: Update project link

--- packages/tokens/src/59144.ts
+++ packages/tokens/src/59144.ts
@@ -5,9 +5,23 @@ import { CAKE, USDC, USDT } from './common'
 export const lineaTokens = {
   weth: WETH9[ChainId.LINEA],
   usdc: USDC[ChainId.LINEA],
-  wbtc: new ERC20Token(ChainId.LINEA, '0x3aAB2285ddcDdaD8edf438C1bAB47e1a9D05a9b4', 8, 'WBTC', 'Wrapped BTC'),
+  wbtc: new ERC20Token(
+    ChainId.LINEA,
+    '0x3aAB2285ddcDdaD8edf438C1bAB47e1a9D05a9b4',
+    8,
+    'WBTC',
+    'Wrapped BTC',
+    'https://bitcoin.org/',
+  ),
   usdt: USDT[ChainId.LINEA],
-  dai: new ERC20Token(ChainId.LINEA, '0x4AF15ec2A0BD43Db75dd04E62FAA3B8EF36b00d5', 18, 'DAI', 'Dai Stablecoin'),
+  dai: new ERC20Token(
+    ChainId.LINEA,
+    '0x4AF15ec2A0BD43Db75dd04E62FAA3B8EF36b00d5',
+    18,
+    'DAI',
+    'Dai Stablecoin',
+    'https://www.makerdao.com/',
+  ),
   cake: CAKE[ChainId.LINEA],
   axlusdc: new ERC20Token(
     ChainId.LINEA,
@@ -15,6 +29,7 @@ export const lineaTokens = {
     6,
     'axlUSDC',
     'Axelar Wrapped USDC',
+    'https://axelarscan.io/assets/',
   ),
   wstETH: new ERC20Token(
     ChainId.LINEA,
