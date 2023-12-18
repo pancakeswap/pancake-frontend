21005a63b55f55fa0111b76e53ea4563a65d197f
chefmomota <momota@pancakeswap.com>
2023-12-18 22:01:12 +0800

    feat: Test

--- apps/web/src/state/pools/fetchAceTokenPrice.ts
+++ apps/web/src/state/pools/fetchAceTokenPrice.ts
@@ -1,25 +1,22 @@
 import { ChainId } from '@pancakeswap/chains'
-import dayjs from 'dayjs'
 import { v3InfoClients } from 'utils/graphql'
-import { ONE_HOUR_SECONDS, SUBGRAPH_START_BLOCK, TimeWindow } from 'views/V3Info/constants'
-import { fetchTokenPriceData } from 'views/V3Info/data/token/priceData'
+import { fetchedTokenDatas } from 'views/V3Info/data/token/tokenData'
 
 export const fetchAceTokenPrice = async (tokenAddress: string) => {
-  const utcCurrentTime = dayjs()
-  const startTimestamp = utcCurrentTime
-    .subtract(1, TimeWindow.WEEK ?? 'day')
-    .startOf('hour')
-    .unix()
+  const result = await fetchedTokenDatas(v3InfoClients[ChainId.BSC], [tokenAddress])
 
-  const result = await fetchTokenPriceData(
-    tokenAddress,
-    ONE_HOUR_SECONDS,
-    startTimestamp,
-    v3InfoClients[ChainId.BSC],
-    'BSC',
-    SUBGRAPH_START_BLOCK[ChainId.BSC],
-  )
+  console.log('result', result?.data)
 
-  console.log('result', result)
-  return result?.data ?? 0
+  // .priceUSD
+
+  // fetchTokenPriceData(
+  //   tokenAddress,
+  //   ONE_HOUR_SECONDS,
+  //   startTimestamp,
+  //   v3InfoClients[ChainId.BSC],
+  //   'BSC',
+  //   SUBGRAPH_START_BLOCK[ChainId.BSC],
+  // )
+
+  return result?.data?.data?.[tokenAddress] ?? 0
 }
