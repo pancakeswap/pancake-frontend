44ad91a15883c4a4e73e6135190ccf2282738020
chefmomota <momota@pancakeswap.com>
2023-12-18 22:44:12 +0800

    feat: Print log

--- apps/web/src/state/pools/fetchAceTokenPrice.ts
+++ apps/web/src/state/pools/fetchAceTokenPrice.ts
@@ -18,5 +18,8 @@ export const fetchAceTokenPrice = async (tokenAddress: string) => {
     blocks?.filter((d) => d.number >= SUBGRAPH_START_BLOCK[ChainId.BSC]),
   )
 
+  console.log('result', result)
+  console.log('tokenAddress', result?.[tokenAddress?.toLowerCase()]?.priceUSD)
+
   return result?.[tokenAddress?.toLowerCase()]?.priceUSD ?? 0
 }
