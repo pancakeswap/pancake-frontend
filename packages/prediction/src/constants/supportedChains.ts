786d2e3e13c1098422faf086673aa01e779a340f
chefmomota <momota@pancakeswap.com>
2023-12-21 15:59:06 +0800

    feat: Remove unsupport chain

--- packages/prediction/src/constants/supportedChains.ts
+++ packages/prediction/src/constants/supportedChains.ts
@@ -1,8 +1,8 @@
 import { ChainId } from '@pancakeswap/chains'
-import { arbitrum, bsc, zkSync } from 'wagmi/chains'
+import { bsc, zkSync } from 'wagmi/chains'
 
-export const SUPPORTED_CHAIN_IDS = [ChainId.BSC, ChainId.ARBITRUM_ONE, ChainId.ZKSYNC] as const
+export const SUPPORTED_CHAIN_IDS = [ChainId.BSC, ChainId.ZKSYNC] as const
 
 export type SupportedChainId = (typeof SUPPORTED_CHAIN_IDS)[number]
 
-export const targetChains = [bsc, zkSync, arbitrum]
+export const targetChains = [bsc, zkSync]
