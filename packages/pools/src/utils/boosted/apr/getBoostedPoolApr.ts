7ce578f51c520c6721df8bfc100a3220203ccf88
ChefMomota <98292246+ChefMomota@users.noreply.github.com>
2023-12-21 14:45:25 +0800

    feat: Add ALP opBNB boosted APR (#8636)
    <!--
    Before opening a pull request, please read the [contributing
    guidelines](https://github.com/pancakeswap/pancake-frontend/blob/develop/CONTRIBUTING.md)
    first
    -->
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    ### Detailed summary
    - The PR focuses on updating the `getBoostedPoolApr` function in the
    `getBoostedPoolApr.ts` file to include the `chainId` parameter when
    calling the `fetchAlpBoostedPoolApr` function.
    - It also adds a new entry to the `opBnbBoostedPools` array in the
    `opBNB.ts` file, which includes the `tooltipsText` property.
    - The `fetchAlpBoostedPoolApr` function in the
    `fetchAlpBoostedPoolApr.ts` file is updated to include the `chainId`
    parameter when making API requests and accessing the contract address.
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your
    question}`
    
    <!-- end pr-codex -->

--- packages/pools/src/utils/boosted/apr/getBoostedPoolApr.ts
+++ packages/pools/src/utils/boosted/apr/getBoostedPoolApr.ts
@@ -1,4 +1,3 @@
-import { ChainId } from '@pancakeswap/chains'
 import { Address, PublicClient } from 'viem'
 import { getBoostedPoolConfig } from '../../../constants/boostedPools'
 import { BoosterType } from '../types'
@@ -17,9 +16,9 @@ export const getBoostedPoolApr = async ({ client, contractAddress, chainId }: Ge
     return 0
   }
 
-  // Arbitrum ALP pools
-  if (pool?.boosterType === BoosterType.ALP && chainId === ChainId.ARBITRUM_ONE) {
-    const result = await fetchAlpBoostedPoolApr(client)
+  // ALP pools
+  if (pool?.boosterType === BoosterType.ALP) {
+    const result = await fetchAlpBoostedPoolApr(client, chainId)
     return result
   }
 
