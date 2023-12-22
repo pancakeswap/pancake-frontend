7cab10256f09b5c8d82d525471f948fc1baf12b8
chefBingbong <133646395+ChefBingbong@users.noreply.github.com>
2023-12-22 12:53:47 +0000

    feat: re-enable buy crypto (#8650)
    <!--
    Before opening a pull request, please read the [contributing
    guidelines](https://github.com/pancakeswap/pancake-frontend/blob/develop/CONTRIBUTING.md)
    first
    -->
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    ### Detailed summary:
    - Removed the `useTooltip`, `ConnectWalletButton`, and `isMobile`
    imports from `GetQuotesButton.tsx`.
    - Removed the disabled state and tooltip for the buy crypto button.
    - Added the `disabled` prop to the `CommitButton` based on the
    `errorText` value.
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your
    question}`
    
    <!-- end pr-codex -->

--- apps/web/src/views/BuyCrypto/components/GetQuotesButton.tsx
+++ apps/web/src/views/BuyCrypto/components/GetQuotesButton.tsx
@@ -1,11 +1,11 @@
+import ConnectWalletButton from 'components/ConnectWalletButton'
+
 import { useTranslation } from '@pancakeswap/localization'
-import { AutoColumn, CircleLoader, Flex, InfoIcon, Text, useTooltip } from '@pancakeswap/uikit'
+import { AutoColumn, CircleLoader, Flex, Text } from '@pancakeswap/uikit'
 import { useMutation } from '@tanstack/react-query'
 import { CommitButton } from 'components/CommitButton'
-import ConnectWalletButton from 'components/ConnectWalletButton'
 import { SetStateAction } from 'jotai'
 import { Dispatch, ReactNode, useCallback } from 'react'
-import { isMobile } from 'react-device-detect'
 import { CryptoFormView } from 'views/BuyCrypto/types'
 import { useAccount } from 'wagmi'
 
@@ -26,24 +26,6 @@ export default function GetQuotesButton({ errorText, setModalView, fetchQuotes }
   })
   const next = useCallback(() => mutate(), [mutate])
 
-  const {
-    tooltip: buyCryptoTooltip,
-    tooltipVisible: buyCryptoTooltipVisible,
-    targetRef: buyCryptoTargetRef,
-  } = useTooltip(
-    <Text as="p">
-      {t(
-        'The buy crypto feature is disabled for the time being but should be back running soon. Sorry for any inconvenience',
-      )}
-    </Text>,
-    {
-      isInPortal: false,
-      placement: isMobile ? 'top' : 'bottom',
-      trigger: isMobile ? 'focus' : 'hover',
-      ...(isMobile && { manualVisible: true }),
-    },
-  )
-
   if (!account) {
     return <ConnectWalletButton width="100%" />
   }
@@ -53,7 +35,6 @@ export default function GetQuotesButton({ errorText, setModalView, fetchQuotes }
     buttonText = errorText
   }
   if (isLoading) {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     buttonText = (
       <>
         <Flex alignItems="center">
@@ -70,17 +51,11 @@ export default function GetQuotesButton({ errorText, setModalView, fetchQuotes }
       <CommitButton
         variant={errorText ? 'danger' : 'primary'}
         onClick={next}
-        disabled
+        disabled={Boolean(errorText)}
         isLoading={isLoading}
         height="55px"
       >
-        <Flex ref={buyCryptoTargetRef}>
-          <Text color="White" fontWeight="bold">
-            {t('Disabled')}
-          </Text>
-          <InfoIcon color="white" paddingX="4px" width="25px" />
-        </Flex>
-        {buyCryptoTooltipVisible && !isMobile && buyCryptoTooltip}
+        {buttonText}
       </CommitButton>
     </AutoColumn>
   )
