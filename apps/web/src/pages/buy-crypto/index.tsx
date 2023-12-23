1f8229cd6b56acd90b7d7d97bee41fb39bb67730
BingBong <bingbong@pancakseswap.com>
2023-12-23 06:47:40 +0000

    debug

--- apps/web/src/pages/buy-crypto/index.tsx
+++ apps/web/src/pages/buy-crypto/index.tsx
@@ -2,23 +2,23 @@ import { ONRAMP_API_BASE_URL } from 'config/constants/endpoints'
 import { SUPPORT_BUY_CRYPTO } from 'config/constants/supportChains'
 import BuyCrypto from 'views/BuyCrypto'
 
-const BuyCryptoPage = ({ userIp }) => {
+const BuyCryptoPage = ({ userIp, error }) => {
+  console.log(userIp, error)
   return <BuyCrypto userIp={userIp} />
 }
 
-export async function getServerSideProps(context) {
+export async function getServerSideProps() {
   try {
     const response = await fetch(`${ONRAMP_API_BASE_URL}/user-ip`)
     const data = await response.json()
-    const userIp = context.req.headers['x-forwarded-for']
-    console.log(context.req.headers['x-forwarded-for'])
+    const userIp = data.ipAddress
 
     return {
-      props: { userIp },
+      props: { userIp, error: undefined },
     }
   } catch (error) {
     return {
-      props: { userIp: null }, // Pass null as the user IP if an error occurs
+      props: { userIp: null, error }, // Pass null as the user IP if an error occurs
     }
   }
 }
