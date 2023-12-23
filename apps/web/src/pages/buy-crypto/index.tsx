b8c9c173087e5ae99c2e6889984f94b7f20d65de
BingBong <bingbong@pancakseswap.com>
2023-12-23 11:02:51 +0000

    try x-forwarded-for

--- apps/web/src/pages/buy-crypto/index.tsx
+++ apps/web/src/pages/buy-crypto/index.tsx
@@ -3,9 +3,22 @@ import BuyCrypto from 'views/BuyCrypto'
 import { fetchUserIp } from 'views/BuyCrypto/hooks/useProviderAvailability'
 import { useQuery } from 'wagmi'
 
-const BuyCryptoPage = () => {
+const BuyCryptoPage = ({ ip }) => {
+  console.log(ip, 'ipp')
   const { data: userIp } = useQuery(['userIp'], () => fetchUserIp(), { initialData: null })
   return <BuyCrypto userIp={userIp} />
+}
+
+export async function getServerSideProps(context) {
+  try {
+    return {
+      props: { ip: context.req.headers['x-forwarded-for'] },
+    }
+  } catch (error) {
+    return {
+      props: { ip: null }, // Pass null as the user IP if an error occurs
+    }
+  }
 }
 
 BuyCryptoPage.chains = SUPPORT_BUY_CRYPTO
