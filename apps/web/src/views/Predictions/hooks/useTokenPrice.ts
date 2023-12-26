e8d92dd9f5b5e4aa1ac4857c1513527221dd3123
chefmomota <momota@pancakeswap.com>
2023-12-26 12:45:36 +0800

    fix: Leaderboard token price

--- apps/web/src/views/Predictions/hooks/useTokenPrice.ts
+++ apps/web/src/views/Predictions/hooks/useTokenPrice.ts
@@ -1,30 +1,9 @@
 import { Currency } from '@pancakeswap/swap-sdk-core'
-import { bscTokens } from '@pancakeswap/tokens'
 import { BIG_ZERO } from '@pancakeswap/utils/bigNumber'
 import BigNumber from 'bignumber.js'
-import { useBNBPrice } from 'hooks/useBNBPrice'
 import { useStablecoinPrice } from 'hooks/useBUSDPrice'
-import { useCakePrice } from 'hooks/useCakePrice'
-import { useMemo } from 'react'
 
 export const useTokenPrice = (token: Currency | undefined, enabled = true): BigNumber => {
-  const enableCake = useMemo(() => enabled && token?.equals(bscTokens.cake), [enabled, token])
-  const enableBnb = useMemo(() => enabled && token?.equals(bscTokens.bnb), [enabled, token])
-  const enableOther = useMemo(() => enabled && !enableCake && !enableBnb, [enabled, enableCake, enableBnb])
-
-  const cakePrice = useCakePrice({ enabled: enableCake })
-  const bnbPrice = useBNBPrice({ enabled: enableBnb })
-  const tokenPrice = useStablecoinPrice(token, { enabled: enableOther })
-
-  if (!token) {
-    return BIG_ZERO
-  }
-
-  if (enableCake) {
-    return cakePrice
-  }
-  if (enableBnb) {
-    return bnbPrice
-  }
+  const tokenPrice = useStablecoinPrice(token, { enabled })
   return tokenPrice ? new BigNumber(tokenPrice.toSignificant(18)) : BIG_ZERO
 }
