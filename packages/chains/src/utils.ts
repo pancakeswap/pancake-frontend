036b8c7ded4477d5be8b8b036c2e69fa53d5be7a
Chef Jerry <jerry@pancakeswap.com>
2023-12-26 16:59:14 +0800

    feat: add istestnet utils

--- packages/chains/src/utils.ts
+++ packages/chains/src/utils.ts
@@ -1,5 +1,5 @@
-import { ChainId } from './chainId'
-import { chainNames, defiLlamaChainNames, chainNameToChainId } from './chainNames'
+import { ChainId, TestnetChainId } from './chainId'
+import { chainNameToChainId, chainNames, defiLlamaChainNames } from './chainNames'
 
 export function getChainName(chainId: ChainId) {
   return chainNames[chainId]
@@ -12,4 +12,8 @@ export function getLlamaChainName(chainId: ChainId) {
 export function getChainIdByChainName(chainName?: string): ChainId | undefined {
   if (!chainName) return undefined
   return chainNameToChainId[chainName] ?? undefined
+}
+
+export function isTestnetChainId(chainId: ChainId) {
+  return Boolean(TestnetChainId[chainId])
 }
--- packages/chains/test/chain.test.ts
+++ packages/chains/test/chain.test.ts
@@ -1,8 +1,13 @@
 import { describe, expect, it } from 'vitest'
-import { ChainId } from '../src'
+import { ChainId, isTestnetChainId } from '../src'
 
 describe('chains', () => {
   it('should be defined', () => {
     expect(ChainId).toBeDefined()
+  })
+
+  it('should return if chainId is testnet or not ', () => {
+    expect(isTestnetChainId(ChainId.ARBITRUM_GOERLI)).toBeTruthy()
+    expect(isTestnetChainId(ChainId.ARBITRUM_ONE)).toBeFalsy()
   })
 })
