44872a958d54a857b7703f97644924b77cc0b91b
memoyil <2213635+memoyil@users.noreply.github.com>
2024-01-10 17:00:25 +0100

    fix: User added tokens

--- apps/web/src/state/user/hooks/useUserAddedTokens.ts
+++ apps/web/src/state/user/hooks/useUserAddedTokens.ts
@@ -1,18 +1,13 @@
 import { useMemo } from 'react'
 import { Token } from '@pancakeswap/sdk'
-import { createSelector } from '@reduxjs/toolkit'
 import { deserializeToken } from '@pancakeswap/token-lists'
-import { useSelector } from 'react-redux'
 import { useActiveChainId } from 'hooks/useActiveChainId'
-import { AppState } from '../../index'
+import { useUserState } from 'state/user/reducer'
 
-const selectUserTokens = ({ user: { tokens } }: AppState) => tokens
-
-export const userAddedTokenSelector = (chainId: number) =>
-  createSelector(selectUserTokens, (serializedTokensMap) =>
-    Object.values(serializedTokensMap?.[chainId] ?? {}).map(deserializeToken),
-  )
 export default function useUserAddedTokens(): Token[] {
   const { chainId } = useActiveChainId()
-  return useSelector(useMemo(() => userAddedTokenSelector(chainId), [chainId]))
+  const [state, dispatch] = useUserState()
+  return useMemo(() => {
+    return Object.values(state?.tokens?.[chainId] ?? {}).map(deserializeToken)
+  }, [state])
 }
