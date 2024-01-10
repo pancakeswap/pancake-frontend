715c1acaa368a57eff03479abd99e0d4f9416644
BingBong <bingbong@pancakseswap.com>
2024-01-10 15:06:08 +0000

    feat: add weth-eth+ gauge

--- packages/gauges/src/constants/config/prod.ts
+++ packages/gauges/src/constants/config/prod.ts
@@ -2279,6 +2279,16 @@ export const CONFIG_PROD: GaugeConfig[] = [
     feeTier: FeeAmount.LOWEST,
   },
   {
+    gid: 228,
+    pairName: 'WETH-ETH+',
+    address: '0xdAA80a051E22A7f7b0cfC33Aa29572fbDE65183E',
+    chainId: ChainId.ARBITRUM_ONE,
+    type: GaugeType.V3,
+    token0Address: arbitrumTokens.weth.address,
+    token1Address: arbitrumTokens.ethplus.address,
+    feeTier: FeeAmount.LOWEST,
+  },
+  {
     gid: 229,
     pairName: 'INSP-BNB',
     address: '0x06aC8EE32BCdcE6bF2eA82D9Bfb932a84D45BFcb',
