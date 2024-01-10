4e0ce397a00bdb9fbe095290383f681b2df0fc30
memoyil <2213635+memoyil@users.noreply.github.com>
2024-01-10 17:22:53 +0100

    fix: Ios

--- apps/web/src/views/Home/hooks/useIsIOS.ts
+++ apps/web/src/views/Home/hooks/useIsIOS.ts
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import { useIsomorphicEffect } from '@pancakeswap/uikit'
 
 export function checkIsIOS() {
-  const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent)
-  return (
-    ['iPad Simulator', 'iPhone Simulator', 'iPod Simulator', 'iPad', 'iPhone', 'iPod'].includes(navigator.platform) ||
-    // iPad on iOS 13 detection
-    (navigator.userAgent.includes('Mac') && 'ontouchend' in document) ||
-    isSafari
-  )
+  if (typeof window !== 'undefined') {
+    const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent)
+    return (
+      ['iPad Simulator', 'iPhone Simulator', 'iPod Simulator', 'iPad', 'iPhone', 'iPod'].includes(navigator.platform) ||
+      // iPad on iOS 13 detection
+      (navigator.userAgent.includes('Mac') && 'ontouchend' in document) ||
+      isSafari
+    )
+  }
+  return false
 }
 
 export const useIsIOS = () => {
