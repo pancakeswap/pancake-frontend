920860bdb934badccbe3c2090d70dcfb6934fe2f
chefmomota <momota@pancakeswap.com>
2024-01-15 14:37:53 +0800

    feat: Check when connected wallet

--- apps/aptos/components/Farms/hooks/useCheckIsUserIpPass.ts
+++ apps/aptos/components/Farms/hooks/useCheckIsUserIpPass.ts
@@ -1,16 +1,24 @@
-import { useQuery } from '@pancakeswap/awgmi'
+import { useAccount, useQuery } from '@pancakeswap/awgmi'
 import { CHECK_USER_IP_API } from 'config/index'
 
 export const useCheckIsUserIpPass = (): boolean => {
-  const { data } = useQuery(['checkIsUserIpPass'], async () => {
-    try {
-      const response = await fetch(CHECK_USER_IP_API)
-      const responseData = await response.json()
-      return responseData.result // true = pass, false = US or vpn
-    } catch (error) {
-      console.error('Fetch Check User IP Error: ', error)
-      return true
-    }
-  })
+  const { account } = useAccount()
+
+  const { data } = useQuery(
+    ['checkIsUserIpPass', account],
+    async () => {
+      try {
+        const response = await fetch(CHECK_USER_IP_API)
+        const responseData = await response.json()
+        return responseData.result // true = pass, false = US or vpn
+      } catch (error) {
+        console.error('Fetch Check User IP Error: ', error)
+        return true
+      }
+    },
+    {
+      enabled: Boolean(account?.address),
+    },
+  )
   return data
 }
