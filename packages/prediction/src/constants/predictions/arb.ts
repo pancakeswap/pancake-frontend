805890de6429d3f0573e03bbe65655bc34a0377b
chefmomota <momota@pancakeswap.com>
2024-01-16 16:37:40 +0800

    feat: Hide ETH

--- packages/prediction/src/constants/predictions/arb.ts
+++ packages/prediction/src/constants/predictions/arb.ts
@@ -1,21 +1,20 @@
 import { ChainId } from '@pancakeswap/chains'
-import { Native } from '@pancakeswap/sdk'
 import { arbitrumTokens } from '@pancakeswap/tokens'
-import { chainlinkOracleETH, chainlinkOracleWBTC } from '../../chainlinkOracleContract'
-import { GRAPH_API_PREDICTION_ETH, GRAPH_API_PREDICTION_WBTC } from '../../endpoints'
-import { predictionsETH, predictionsWBTC } from '../../predictionContract'
+import { chainlinkOracleWBTC } from '../../chainlinkOracleContract'
+import { GRAPH_API_PREDICTION_WBTC } from '../../endpoints'
+import { predictionsWBTC } from '../../predictionContract'
 import { PredictionConfig, PredictionSupportedSymbol } from '../../type'
 
 export const predictions: Record<string, PredictionConfig> = {
-  [PredictionSupportedSymbol.ETH]: {
-    isNativeToken: true,
-    address: predictionsETH[ChainId.ARBITRUM_ONE],
-    api: GRAPH_API_PREDICTION_ETH[ChainId.ARBITRUM_ONE],
-    chainlinkOracleAddress: chainlinkOracleETH[ChainId.ARBITRUM_ONE],
-    displayedDecimals: 4,
-    token: Native.onChain(ChainId.ARBITRUM_ONE),
-    tokenBackgroundColor: '#647ceb',
-  },
+  // [PredictionSupportedSymbol.ETH]: {
+  //   isNativeToken: true,
+  //   address: predictionsETH[ChainId.ARBITRUM_ONE],
+  //   api: GRAPH_API_PREDICTION_ETH[ChainId.ARBITRUM_ONE],
+  //   chainlinkOracleAddress: chainlinkOracleETH[ChainId.ARBITRUM_ONE],
+  //   displayedDecimals: 4,
+  //   token: Native.onChain(ChainId.ARBITRUM_ONE),
+  //   tokenBackgroundColor: '#647ceb',
+  // },
   [PredictionSupportedSymbol.WBTC]: {
     isNativeToken: false,
     address: predictionsWBTC[ChainId.ARBITRUM_ONE],
