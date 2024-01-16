a12388add1149316cdbe3781566adc4029c4c524
BingBong <bingbong@pancakseswap.com>
2024-01-16 03:42:02 +0000

    fix: remove needles api endpoint.

--- apps/web/src/views/BuyCrypto/hooks/useProviderAvailability.ts
+++ apps/web/src/views/BuyCrypto/hooks/useProviderAvailability.ts
@@ -1,8 +1,9 @@
+import { ONRAMP_API_BASE_URL } from 'config/constants/endpoints'
 import { ONRAMP_PROVIDERS } from '../constants'
 
 export async function fetchProviderAvailabilities(): Promise<{ [provider in keyof typeof ONRAMP_PROVIDERS]: boolean }> {
   try {
-    const response = await fetch(`/api/onramp/provider-availability`, {
+    const response = await fetch(`${ONRAMP_API_BASE_URL}/fetch-provider-availability-get`, {
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
