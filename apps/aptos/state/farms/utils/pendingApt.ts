9bbd36feee8d18eba1cf838d3d5efe57d3d955f2
chefmomota <momota@pancakeswap.com>
2024-01-17 19:44:20 +0800

    feat: Update APT APR

--- apps/aptos/state/farms/utils/pendingApt.ts
+++ apps/aptos/state/farms/utils/pendingApt.ts
@@ -1,4 +1,6 @@
+import { BIG_TEN } from '@pancakeswap/utils/bigNumber'
 import BigNumber from 'bignumber.js'
+import { FARM_DEFAULT_DECIMALS } from 'components/Farms/constants'
 import { SECONDS_IN_YEAR } from 'config'
 import { TOTAL_CAKE_RATE_PRECISION } from './pendingCake'
 
@@ -10,6 +12,6 @@ export const calcPendingRewardApt = (
 ) => {
   const aptPerSecond = new BigNumber(cakePerSecond).times(aptIncentiveInfo).div(TOTAL_CAKE_RATE_PRECISION)
   const aptRewardOneYearUSD = new BigNumber(aptPerSecond).times(aptPrice).times(SECONDS_IN_YEAR)
-  const aptApr = aptRewardOneYearUSD.div(tvlUSD)
-  return aptApr.toNumber()
+  const aptApr = aptRewardOneYearUSD.div(tvlUSD).times(100)
+  return aptApr.div(BIG_TEN.pow(FARM_DEFAULT_DECIMALS)).toNumber()
 }
