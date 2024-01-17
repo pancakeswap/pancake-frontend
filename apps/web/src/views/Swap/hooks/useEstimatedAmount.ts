b421bf7a85c4250bf43731bc817a0470e3e3f9e8
memoyil <2213635+memoyil@users.noreply.github.com>
2024-01-17 16:11:40 +0100

    fix: Price when removing stable pair

--- apps/web/src/views/Swap/hooks/useEstimatedAmount.ts
+++ apps/web/src/views/Swap/hooks/useEstimatedAmount.ts
@@ -12,7 +12,7 @@ export function useEstimatedAmount({ estimatedCurrency, stableSwapConfig, quotie
 
       const args = isToken0 ? [1, 0, deferQuotient] : [0, 1, deferQuotient]
 
-      const estimatedAmount = await stableSwapContract.get_dy(...args)
+      const estimatedAmount = await stableSwapContract.read.get_dy(args)
 
       return CurrencyAmount.fromRawAmount(estimatedCurrency, estimatedAmount)
     },
