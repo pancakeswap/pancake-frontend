ad4027a57f73cc7aa4071fdc4e47b91061ae46ad
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2024-01-23 11:37:36 +0800

    chore(seo): Update metadata

--- apps/web/next-seo.config.ts
+++ apps/web/next-seo.config.ts
@@ -3,17 +3,15 @@ import { DefaultSeoProps } from 'next-seo'
 export const SEO: DefaultSeoProps = {
   titleTemplate: '%s | PancakeSwap',
   defaultTitle: 'PancakeSwap',
-  description:
-    'Cheaper and faster than Uniswap? Discover PancakeSwap, the leading DEX on BNB Smart Chain (BSC) with the best farms in DeFi and a lottery for CAKE.',
+  description: 'Trade, earn, and own crypto on the all-in-one multichain DEX',
   twitter: {
     cardType: 'summary_large_image',
     handle: '@PancakeSwap',
     site: '@PancakeSwap',
   },
   openGraph: {
-    title: '🥞 PancakeSwap - A next evolution DeFi exchange on BNB Smart Chain (BSC)',
-    description:
-      'The most popular AMM on BSC by user count! Earn CAKE through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by PancakeSwap), NFTs, and more, on a platform you can trust.',
+    title: "🥞 PancakeSwap - Everyone's Favorite DEX",
+    description: 'Trade, earn, and own crypto on the all-in-one multichain DEX',
     images: [{ url: 'https://assets.pancakeswap.finance/web/og/v2/hero.jpg' }],
   },
 }
