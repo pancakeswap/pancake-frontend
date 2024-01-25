cdf59e98532e59449134dd7a0194d8b874e86978
chefmomota <momota@pancakeswap.com>
2024-01-25 22:06:25 +0800

    feat: Add VeCakeAddCakeOrWeeksModal

---
+++ apps/web/src/views/TradingReward/components/YourTradingReward/VeCake/VeCakeAddCakeOrWeeksModal.tsx
@@ -0,0 +1,60 @@
+import { useTranslation } from '@pancakeswap/localization'
+import { ArrowUpDownIcon, Flex, InjectedModalProps, Modal, Text } from '@pancakeswap/uikit'
+import React, { useEffect, useState } from 'react'
+import { styled } from 'styled-components'
+import { LockCakeForm } from 'views/CakeStaking/components/LockCakeForm'
+import { LockWeeksForm } from 'views/CakeStaking/components/LockWeeksForm'
+
+const StyledSwitchTextContainer = styled(Flex)`
+  position: absolute;
+  top: 24px;
+  right: 24px;
+  cursor: pointer;
+  z-index: 1;
+`
+
+export enum VeCakeModalView {
+  CAKE_FORM_VIEW = 'CAKE_FORM_VIEW',
+  WEEKS_FORM_VIEW = 'WEEKS_FORM_VIEW',
+}
+
+interface VeCakeAddCakeOrWeeksModalProps extends InjectedModalProps {
+  viewMode?: VeCakeModalView
+}
+
+export const VeCakeAddCakeOrWeeksModal: React.FC<React.PropsWithChildren<VeCakeAddCakeOrWeeksModalProps>> = ({
+  viewMode,
+  onDismiss,
+}) => {
+  const { t } = useTranslation()
+  const [modalViewMode, setModalViewMode] = useState(VeCakeModalView.CAKE_FORM_VIEW)
+
+  useEffect(() => {
+    if (viewMode) {
+      setModalViewMode(viewMode)
+    }
+  }, [])
+
+  const toggleViewMode = () => {
+    const mode =
+      modalViewMode === VeCakeModalView.CAKE_FORM_VIEW
+        ? VeCakeModalView.WEEKS_FORM_VIEW
+        : VeCakeModalView.CAKE_FORM_VIEW
+    setModalViewMode(mode)
+  }
+
+  return (
+    <Modal title="Increase your veCAKE" headerBorderColor="transparent" maxWidth={500} onDismiss={onDismiss}>
+      <StyledSwitchTextContainer onClick={toggleViewMode}>
+        <ArrowUpDownIcon mr="4px" color="primary" style={{ rotate: '90deg' }} />
+        <Text bold color="primary">
+          {modalViewMode === VeCakeModalView.CAKE_FORM_VIEW ? t('Extend Lock Instead') : t('Add CAKE Instead')}
+        </Text>
+      </StyledSwitchTextContainer>
+      <Flex position="relative">
+        {modalViewMode === VeCakeModalView.CAKE_FORM_VIEW ? <LockCakeForm /> : <LockWeeksForm />}
+      </Flex>
+      {/* <PreviewOfVeCakeSnapShotTime /> */}
+    </Modal>
+  )
+}
