0a1efc5ecee3d3d4d6580f1fddc143b404313ef9
chefmomota <momota@pancakeswap.com>
2024-01-26 18:22:43 +0800

    feat: List opBNB Farm

--- packages/farms/constants/opBNB.ts
+++ packages/farms/constants/opBNB.ts
@@ -5,6 +5,20 @@ import { defineFarmV3Configs } from '../src/defineFarmV3Configs'
 
 export const farmsV3 = defineFarmV3Configs([
   {
+    pid: 3,
+    lpAddress: '0xFf00F4E09820dbbe8582F771800732DaE7F002bD',
+    token0: opBnbTokens.wbnb,
+    token1: opBnbTokens.eth,
+    feeAmount: FeeAmount.MEDIUM,
+  },
+  {
+    pid: 2,
+    lpAddress: '0xD9004241D34392e9Ae0C84d5aCDF76941a27D8D1',
+    token0: opBnbTokens.fdusd,
+    token1: opBnbTokens.usdt,
+    feeAmount: FeeAmount.LOWEST,
+  },
+  {
     pid: 1,
     lpAddress: '0xc4f981189558682F15F60513158B699354B30204',
     token0: opBnbTokens.wbnb,
--- packages/tokens/src/opBNB.ts
+++ packages/tokens/src/opBNB.ts
@@ -15,4 +15,20 @@ export const opBnbTokens = {
     'ApolloX LP',
     'https://www.apollox.finance/en',
   ),
+  eth: new ERC20Token(
+    ChainId.OPBNB,
+    '0xE7798f023fC62146e8Aa1b36Da45fb70855a77Ea',
+    18,
+    'ETH',
+    'Ethereum',
+    'https://opbnb.bnbchain.org/en',
+  ),
+  fdusd: new ERC20Token(
+    ChainId.OPBNB,
+    '0x50c5725949A6F0c72E6C4a641F24049A917DB0Cb',
+    18,
+    'FDUSD',
+    'First Digital USD',
+    'https://firstdigitallabs.com/',
+  ),
 }
