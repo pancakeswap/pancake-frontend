5661e5edf6250cf72957416162765165709ae76a
Chef Jerry <jerry@pancakeswap.com>
2024-01-31 16:27:00 +0800

    feat: remove binance extension wallet
    binance extension wallet is not maintained anymore

--- packages/wagmi/tsup.config.ts
+++ packages/wagmi/tsup.config.ts
@@ -4,7 +4,6 @@ export default defineConfig({
   entry: {
     index: 'src/index.ts',
     'connectors/miniProgram': 'connectors/miniProgram/index.ts',
-    'connectors/binanceWallet': 'connectors/binanceWallet/index.ts',
     'connectors/blocto': 'connectors/blocto/index.ts',
     'connectors/trustWallet': 'connectors/trustWallet/index.ts',
   },
