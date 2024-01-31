8d8ecb73ea7e5fa751bd5dfba285e99565cfa02f
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2024-01-31 12:01:10 +0800

    fix: PM zksync pricing issue

--- apps/web/src/views/PositionManagers/hooks/useEarningTokenPriceInfo.ts
+++ apps/web/src/views/PositionManagers/hooks/useEarningTokenPriceInfo.ts
@@ -1,19 +1,25 @@
 import { Currency } from '@pancakeswap/sdk'
 import { getBalanceAmount } from '@pancakeswap/utils/formatBalance'
 import BigNumber from 'bignumber.js'
+import { useCakePrice } from 'hooks/useCakePrice'
 import { useCurrencyUsdPrice } from 'hooks/useCurrencyUsdPrice'
 import { useMemo } from 'react'
 
 export const useEarningTokenPriceInfo = (earningToken: Currency, pendingReward: bigint | undefined) => {
   const { data: earningTokenPrice } = useCurrencyUsdPrice(earningToken ?? undefined)
+  const cakePrice = useCakePrice()?.toNumber()
+
   const earningsBalance = useMemo(
     () => getBalanceAmount(new BigNumber(pendingReward?.toString() ?? 0), earningToken.decimals).toNumber(),
     [pendingReward, earningToken],
   )
 
   const earningUsdValue = useMemo(
-    () => new BigNumber(earningsBalance).times(earningTokenPrice ?? 0).toNumber(),
-    [earningsBalance, earningTokenPrice],
+    () =>
+      new BigNumber(earningsBalance)
+        .times((earningToken.symbol === 'CAKE' ? cakePrice : earningTokenPrice) ?? 0)
+        .toNumber(),
+    [earningsBalance, earningToken.symbol, cakePrice, earningTokenPrice],
   )
-  return { earningUsdValue, earningTokenPrice, earningsBalance }
+  return { earningUsdValue, earningTokenPrice: earningTokenPrice ?? cakePrice, earningsBalance }
 }
