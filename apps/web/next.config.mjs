804833caa613697c1fe565be4a984ee513d95bab
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2024-02-01 13:38:06 +0800

    Revert "perf: Optimize loading barrel packages (#8942)"
    This reverts commit 6dd0eb9da77904219b7e2e1e20761a8a7df207e8.

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -53,7 +53,6 @@ const config = {
     outputFileTracingExcludes: {
       '*': [],
     },
-    optimizePackageImports: ['@pancakeswap/widgets-internal', '@pancakeswap/uikit'],
   },
   transpilePackages: [
     '@pancakeswap/farms',
