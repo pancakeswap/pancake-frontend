edcc00796114e63392118502c1ada20e9361e66a
Chef Jerry <jerry@pancakeswap.com>
2024-02-09 13:33:24 +0800

    feat: update address

--- packages/permit2-sdk/src/constants.ts
+++ packages/permit2-sdk/src/constants.ts
@@ -2,10 +2,10 @@ import { ChainId } from '@pancakeswap/chains'
 import { Address } from 'viem'
 
 // @fixme convert to ChainId after all chains are updated
-// const PERMIT2_ADDRESSES: Record<ChainId, Address> = {
-const PERMIT2_ADDRESSES: Record<number, Address> = {
+const PERMIT2_ADDRESSES: Record<ChainId, Address> = {
   [ChainId.ETHEREUM]: '0x31c2F6fcFf4F8759b3Bd5Bf0e1084A055615c768',
   [ChainId.GOERLI]: '0x31c2F6fcFf4F8759b3Bd5Bf0e1084A055615c768',
+  [ChainId.SEPOLIA]: '0x31c2F6fcFf4F8759b3Bd5Bf0e1084A055615c768',
 
   [ChainId.BSC]: '0x31c2F6fcFf4F8759b3Bd5Bf0e1084A055615c768',
   [ChainId.BSC_TESTNET]: '0x31c2F6fcFf4F8759b3Bd5Bf0e1084A055615c768',
@@ -15,9 +15,11 @@ const PERMIT2_ADDRESSES: Record<number, Address> = {
 
   [ChainId.ARBITRUM_ONE]: '0x31c2F6fcFf4F8759b3Bd5Bf0e1084A055615c768',
   [ChainId.ARBITRUM_GOERLI]: '0x31c2F6fcFf4F8759b3Bd5Bf0e1084A055615c768',
+  [ChainId.ARBITRUM_SEPOLIA]: '0x31c2F6fcFf4F8759b3Bd5Bf0e1084A055615c768',
 
   [ChainId.BASE]: '0x31c2F6fcFf4F8759b3Bd5Bf0e1084A055615c768',
   [ChainId.BASE_TESTNET]: '0x31c2F6fcFf4F8759b3Bd5Bf0e1084A055615c768',
+  [ChainId.BASE_SEPOLIA]: '0x31c2F6fcFf4F8759b3Bd5Bf0e1084A055615c768',
 
   [ChainId.POLYGON_ZKEVM]: '0x31c2F6fcFf4F8759b3Bd5Bf0e1084A055615c768',
   [ChainId.POLYGON_ZKEVM_TESTNET]: '0x31c2F6fcFf4F8759b3Bd5Bf0e1084A055615c768',
