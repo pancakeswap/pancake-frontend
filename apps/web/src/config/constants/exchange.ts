3700d88d5395028c828904522620e485819ac6fb
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2024-02-09 18:11:58 +0800

    fix: Token list include path (#9048)
    <!--
    Before opening a pull request, please read the [contributing
    guidelines](https://github.com/pancakeswap/pancake-frontend/blob/develop/CONTRIBUTING.md)
    first
    -->
    
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    This PR adds new paths to the `EXCHANGE_PAGE_PATHS` constant in three
    files and reorders imports in two files.
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your
    question}`
    
    <!-- end pr-codex -->

--- apps/web/src/config/constants/exchange.ts
+++ apps/web/src/config/constants/exchange.ts
@@ -172,4 +172,4 @@ export const GENERIC_GAS_LIMIT_ORDER_EXECUTION = 500000n
 
 export const LIMIT_ORDERS_DOCS_URL = 'https://docs.pancakeswap.finance/products/pancakeswap-exchange/limit-orders'
 
-export const EXCHANGE_PAGE_PATHS = ['/swap', '/limit-orders', 'liquidity', '/add', '/find', '/remove']
+export const EXCHANGE_PAGE_PATHS = ['/swap', '/limit-orders', 'liquidity', '/add', '/find', '/remove', '/stable', '/v2']
