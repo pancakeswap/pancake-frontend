7e79e08973ce8f83e543576d425897190552c152
chefmomota <momota@pancakeswap.com>
2024-02-15 15:28:54 +0800

    feat: Hide Arbitrum predition

--- packages/prediction/src/constants/supportedChains.ts
+++ packages/prediction/src/constants/supportedChains.ts
@@ -1,8 +1,8 @@
 import { ChainId } from '@pancakeswap/chains'
-import { arbitrum, bsc, zkSync } from 'wagmi/chains'
+import { bsc, zkSync } from 'wagmi/chains'
 
-export const SUPPORTED_CHAIN_IDS = [ChainId.BSC, ChainId.ZKSYNC, ChainId.ARBITRUM_ONE] as const
+export const SUPPORTED_CHAIN_IDS = [ChainId.BSC, ChainId.ZKSYNC] as const
 
 export type SupportedChainId = (typeof SUPPORTED_CHAIN_IDS)[number]
 
-export const targetChains = [bsc, zkSync, arbitrum]
+export const targetChains = [bsc, zkSync]
