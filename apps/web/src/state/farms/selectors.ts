f84329c43a4d43594b433e37eb8dc8d666d70857
chefilip <philip@pancakeswap.com>
2024-02-22 09:14:31 +0700

    chore: Clean up ts error 2

--- apps/web/src/state/farms/selectors.ts
+++ apps/web/src/state/farms/selectors.ts
@@ -10,11 +10,11 @@ const selectFarmByKey = (key: string, value: string | number) => (state: State) 
   state.farms.data.find((f) => f[key] === value)
 
 export const makeFarmFromPidSelector = (pid: number) =>
-  createSelector([selectFarmByKey('pid', pid)], (farm) => deserializeFarm(farm))
+  createSelector([selectFarmByKey('pid', pid)], (farm) => farm && deserializeFarm(farm))
 
 export const makeBusdPriceFromPidSelector = (pid: number) =>
   createSelector([selectFarmByKey('pid', pid)], (farm) => {
-    return farm && new BigNumber(farm.tokenPriceBusd)
+    return farm && new BigNumber(farm.tokenPriceBusd || '0')
   })
 
 export const makeUserFarmFromPidSelector = (pid: number) =>
@@ -31,11 +31,11 @@ export const makeUserFarmFromPidSelector = (pid: number) =>
 
 export const priceCakeFromPidSelector = createSelector([selectCakeFarm], (cakeBnbFarm) => {
   const cakePriceBusdAsString = cakeBnbFarm?.tokenPriceBusd
-  return new BigNumber(cakePriceBusdAsString)
+  return new BigNumber(cakePriceBusdAsString || '0')
 })
 
 export const farmFromLpSymbolSelector = (lpSymbol: string) =>
-  createSelector([selectFarmByKey('lpSymbol', lpSymbol)], (farm) => deserializeFarm(farm))
+  createSelector([selectFarmByKey('lpSymbol', lpSymbol)], (farm) => farm && deserializeFarm(farm))
 
 export const makeLpTokenPriceFromLpSymbolSelector = (lpSymbol: string) =>
   createSelector([selectFarmByKey('lpSymbol', lpSymbol)], (farm) => {
@@ -45,7 +45,7 @@ export const makeLpTokenPriceFromLpSymbolSelector = (lpSymbol: string) =>
       const lpTotalSupply = farm.lpTotalSupply ? new BigNumber(farm.lpTotalSupply) : BIG_ZERO
 
       if (lpTotalSupply.gt(0) && lpTotalInQuoteToken.gt(0)) {
-        const farmTokenPriceInUsd = new BigNumber(farm.tokenPriceBusd)
+        const farmTokenPriceInUsd = new BigNumber(farm.tokenPriceBusd || '0')
         const tokenAmountTotal = farm.tokenAmountTotal ? new BigNumber(farm.tokenAmountTotal) : BIG_ZERO
         // Total value of base token in LP
         const valueOfBaseTokenInFarm = farmTokenPriceInUsd.times(tokenAmountTotal)
--- packages/farms/src/v2/deserializeFarmUserData.ts
+++ packages/farms/src/v2/deserializeFarmUserData.ts
@@ -1,13 +1,13 @@
-import BigNumber from 'bignumber.js'
 import { BIG_ZERO } from '@pancakeswap/utils/bigNumber'
-import { SerializedFarm, DeserializedFarmUserData } from '../types'
+import BigNumber from 'bignumber.js'
+import { DeserializedFarmUserData, SerializedFarm } from '../types'
 
-export const deserializeFarmUserData = (farm: SerializedFarm): DeserializedFarmUserData => {
+export const deserializeFarmUserData = (farm?: SerializedFarm): DeserializedFarmUserData => {
   return {
-    allowance: farm.userData ? new BigNumber(farm.userData.allowance) : BIG_ZERO,
-    tokenBalance: farm.userData ? new BigNumber(farm.userData.tokenBalance) : BIG_ZERO,
-    stakedBalance: farm.userData ? new BigNumber(farm.userData.stakedBalance) : BIG_ZERO,
-    earnings: farm.userData ? new BigNumber(farm.userData.earnings) : BIG_ZERO,
+    allowance: farm?.userData ? new BigNumber(farm.userData.allowance) : BIG_ZERO,
+    tokenBalance: farm?.userData ? new BigNumber(farm.userData.tokenBalance) : BIG_ZERO,
+    stakedBalance: farm?.userData ? new BigNumber(farm.userData.stakedBalance) : BIG_ZERO,
+    earnings: farm?.userData ? new BigNumber(farm.userData.earnings) : BIG_ZERO,
     proxy: {
       allowance: farm?.userData?.proxy ? new BigNumber(farm?.userData?.proxy.allowance) : BIG_ZERO,
       tokenBalance: farm?.userData?.proxy ? new BigNumber(farm?.userData?.proxy.tokenBalance) : BIG_ZERO,
