8375102478b80b389d341a4e8e29da98919e246d
Chef Jerry <jerry@pancakeswap.com>
2024-03-05 21:42:05 +0800

    feat: poc

--- apps/web/src/views/Swap/V3Swap/hooks/useSendSwapTransaction.ts
+++ apps/web/src/views/Swap/V3Swap/hooks/useSendSwapTransaction.ts
@@ -10,17 +10,81 @@ import { useMemo } from 'react'
 import { useSwapState } from 'state/swap/hooks'
 import { useTransactionAdder } from 'state/transactions/hooks'
 import { calculateGasMargin, safeGetAddress } from 'utils'
+import { logger } from 'utils/datadog'
 import { basisPointsToPercent } from 'utils/exchange'
 import { logSwap, logTx } from 'utils/log'
 import { isUserRejected } from 'utils/sentry'
 import { transactionErrorToUserReadableMessage } from 'utils/transactionErrorToUserReadableMessage'
-import { Address, Hex, TransactionExecutionError, UserRejectedRequestError, hexToBigInt } from 'viem'
-import { useSendTransaction } from 'wagmi'
+import { viemClients } from 'utils/viem'
+import { Address, Hex, TransactionExecutionError, UserRejectedRequestError, hexToBigInt, stringify } from 'viem'
+import { useSendTransaction, useWalletClient } from 'wagmi'
 import { SendTransactionResult } from 'wagmi/actions'
 
-import { logger } from 'utils/datadog'
-import { viemClients } from 'utils/viem'
+import { zkSync } from 'viem/chains'
 import { isZero } from '../utils/isZero'
+
+const getEip712Domain = (transaction) => {
+  const message = transactionToMessage(transaction as any)
+
+  return {
+    domain: {
+      name: 'zkSync',
+      version: '2',
+      chainId: transaction.chainId,
+    },
+    types: {
+      Transaction: [
+        { name: 'txType', type: 'uint256' },
+        { name: 'from', type: 'uint256' },
+        { name: 'to', type: 'uint256' },
+        { name: 'gasLimit', type: 'uint256' },
+        { name: 'gasPerPubdataByteLimit', type: 'uint256' },
+        { name: 'maxFeePerGas', type: 'uint256' },
+        { name: 'maxPriorityFeePerGas', type: 'uint256' },
+        { name: 'paymaster', type: 'uint256' },
+        { name: 'nonce', type: 'uint256' },
+        { name: 'value', type: 'uint256' },
+        { name: 'data', type: 'bytes' },
+        { name: 'factoryDeps', type: 'bytes32[]' },
+        { name: 'paymasterInput', type: 'bytes' },
+      ],
+    },
+    primaryType: 'Transaction',
+    message,
+  }
+}
+function transactionToMessage(transaction: any): any {
+  const {
+    gas,
+    nonce,
+    to,
+    from,
+    value,
+    maxFeePerGas,
+    maxPriorityFeePerGas,
+    factoryDeps,
+    paymaster,
+    paymasterInput,
+    gasPerPubdata,
+    data,
+  } = transaction
+
+  return {
+    txType: 113n,
+    from: BigInt(from),
+    to: to ? BigInt(to) : 0n,
+    gasLimit: gas ?? 0n,
+    gasPerPubdataByteLimit: gasPerPubdata ?? 0n,
+    maxFeePerGas: maxFeePerGas ?? 0n,
+    maxPriorityFeePerGas: maxPriorityFeePerGas ?? 0n,
+    paymaster: paymaster ? BigInt(paymaster) : 0n,
+    nonce: nonce ? BigInt(nonce) : 0n,
+    value: value ?? 0n,
+    data: data || '0x0',
+    factoryDeps: factoryDeps ?? [],
+    paymasterInput: paymasterInput || '0x0',
+  }
+}
 
 interface SwapCall {
   address: Address
@@ -55,9 +119,10 @@ export default function useSendSwapTransaction(
   trade?: SmartRouterTrade<TradeType> | null, // trade to execute, required
   swapCalls: SwapCall[] | WallchainSwapCall[] = [],
   type: 'V3SmartSwap' | 'UniversalRouter' = 'V3SmartSwap',
-): { callback: null | (() => Promise<SendTransactionResult>) } {
+): { callback: null | (() => Promise<SendTransactionResult | undefined>) } {
   const { t } = useTranslation()
   const addTransaction = useTransactionAdder()
+  const { data: walletClient } = useWalletClient()
   const { sendTransactionAsync } = useSendTransaction()
   const publicClient = viemClients[chainId as ChainId]
   const [allowedSlippage] = useUserSlippage() || [INITIAL_ALLOWED_SLIPPAGE]
@@ -69,7 +134,7 @@ export default function useSendSwapTransaction(
       return { callback: null }
     }
     return {
-      callback: async function onSwap(): Promise<SendTransactionResult> {
+      callback: async function onSwap(): Promise<SendTransactionResult | undefined> {
         const estimatedCalls: SwapCallEstimate[] = await Promise.all(
           swapCalls.map((call) => {
             const { address, calldata, value } = call
@@ -140,14 +205,89 @@ export default function useSendSwapTransaction(
               : undefined
         }
 
-        return sendTransactionAsync({
-          account,
-          chainId,
-          to: call.address,
-          data: call.calldata,
-          value: call.value && !isZero(call.value) ? hexToBigInt(call.value) : 0n,
-          gas: call.gas,
-        })
+        let sendTxResult: Promise<SendTransactionResult> = Promise.reject()
+
+        // zkSync paymaster
+        if (chainId === zkSync.id && trade.outputAmount.currency.isToken) {
+          const resp = await fetch(`https://paymaster.zyfi.org/api/v1/erc20_paymaster`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: stringify({
+              feeTokenAddress: trade.outputAmount.currency.address,
+              gasLimit: call.gas,
+              txData: {
+                from: account,
+                to: call.address,
+                value: call.value,
+                data: call.calldata,
+              },
+            }),
+          })
+          if (resp.ok) {
+            const rawTx = await resp.json()
+            console.debug('debug rawTx', rawTx)
+            const { customData } = rawTx.txData
+            const newTx = {
+              // type: 'eip712',
+              account,
+              to: call.address,
+              value: call.value && !isZero(call.value) ? hexToBigInt(call.value) : 0n,
+              chainId,
+              gas: BigInt(rawTx.gasLimit),
+              maxFeePerGas: BigInt(rawTx.txData.maxFeePerGas),
+              maxPriorityFeePerGas: BigInt(0),
+              data: call.calldata,
+              gasPerPubdata: BigInt(customData.gasPerPubdata),
+              paymaster: customData.paymasterParams.paymaster,
+              paymasterInput: customData.paymasterParams.paymasterInput,
+            }
+
+            if (walletClient) {
+              // sendTxResult = sendTransactionAsync(newTx as any)
+              const txRequest = await walletClient.prepareTransactionRequest(newTx as any)
+              console.debug('debug txRequest', txRequest)
+
+              const eip712Domain = getEip712Domain({
+                ...txRequest,
+                chainId,
+                from: txRequest.account,
+                type: 'eip712',
+              })
+              const customSignature = await walletClient.signTypedData({
+                ...eip712Domain,
+                account: txRequest.account,
+              } as any)
+              console.debug('debug customSig', customSignature)
+              const serializedTransaction = zkSync.serializers!.transaction!({
+                ...txRequest,
+                chainId,
+                customSignature,
+                type: 'eip712',
+              } as any)
+
+              console.debug('debug serializedTransaction', serializedTransaction)
+              const p = await walletClient.sendRawTransaction({ serializedTransaction }).then((hash) => {
+                return {
+                  hash,
+                } as SendTransactionResult
+              })
+              sendTxResult = Promise.resolve(p)
+            }
+          }
+        } else {
+          sendTxResult = sendTransactionAsync({
+            account,
+            chainId,
+            to: call.address,
+            data: call.calldata,
+            value: call.value && !isZero(call.value) ? hexToBigInt(call.value) : 0n,
+            gas: call.gas,
+          })
+        }
+
+        return sendTxResult
           .then((response) => {
             const inputSymbol = trade.inputAmount.currency.symbol
             const outputSymbol = trade.outputAmount.currency.symbol
@@ -208,6 +348,7 @@ export default function useSendSwapTransaction(
             return response
           })
           .catch((error) => {
+            console.error('Swap failed', error)
             // if the user rejected the tx, pass this along
             if (isUserRejected(error)) {
               throw new TransactionRejectedError(t('Transaction rejected'))
@@ -240,6 +381,7 @@ export default function useSendSwapTransaction(
     publicClient,
     swapCalls,
     t,
+    walletClient,
     allowedSlippage,
     recipientAddress,
     recipient,
