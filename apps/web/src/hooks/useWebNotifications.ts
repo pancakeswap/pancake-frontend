ae342cfd1bbdc71b658a752d512af60d794f52a9
BingBong <bingbong@pancakseswap.com>
2024-03-07 13:58:23 +0000

    chore: keep feature flag in

--- apps/web/src/hooks/useWebNotifications.ts
+++ apps/web/src/hooks/useWebNotifications.ts
@@ -1,13 +1,11 @@
+import { EXPERIMENTAL_FEATURES } from 'config/experimentalFeatures'
+import { useExperimentalFeatureEnabled } from 'hooks/useExperimentalFeatureEnabled'
 import { useAllowNotifications } from 'state/notifications/hooks'
 
 export const useWebNotifications = () => {
   const [allowNotifications] = useAllowNotifications()
-
-  // uncomment if need to add web3 notis back to feature flag
-  // const featureEnabled = useExperimentalFeatureEnabled(EXPERIMENTAL_FEATURES.WebNotifications)
-  // const enabled = Boolean(allowNotifications ?? featureEnabled)
-
-  const enabled = Boolean(allowNotifications)
+  const featureEnabled = useExperimentalFeatureEnabled(EXPERIMENTAL_FEATURES.WebNotifications)
+  const enabled = Boolean(allowNotifications ?? featureEnabled)
 
   return { enabled }
 }
