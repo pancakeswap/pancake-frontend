a87d7f951830b2b335e10ed8794209bcfbd79edf
Woody <woodywu810@gmail.com>
2024-03-07 23:12:46 +0800

    fix

--- apps/blog/components/Article/CategoriesSelector.tsx
+++ apps/blog/components/Article/CategoriesSelector.tsx
@@ -19,7 +19,7 @@ const CategoriesSelector = ({ selected, categoriesData, childMargin, setSelected
   }, [categoriesData, t])
 
   return (
-    <Flex flexDirection={['row', 'row', 'row', 'row', 'row', 'row', 'column']}>
+    <Flex flexDirection={['row', 'row', 'row', 'row', 'row', 'column', 'column']}>
       {allCategories?.map((category) => (
         <Button
           key={category.id}
