711bf12b97687b3bc25f9f7863879a2d4693a226
BingBong <bingbong@pancakseswap.com>
2024-03-12 13:29:19 +0000

    chore: allow all chains on buy crypto

--- apps/web/src/pages/buy-crypto/index.tsx
+++ apps/web/src/pages/buy-crypto/index.tsx
@@ -1,7 +1,10 @@
+import { CHAIN_IDS } from 'utils/wagmi'
 import BuyCrypto from 'views/BuyCrypto'
 
 const BuyCryptoPage = () => {
   return <BuyCrypto />
 }
+
+BuyCryptoPage.chains = CHAIN_IDS
 
 export default BuyCryptoPage
