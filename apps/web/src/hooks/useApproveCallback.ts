2870edce2d7e35d0f6ded8175e7b41e9ca08f875
Chef Jerry <jerry@pancakeswap.com>
2024-03-13 23:40:41 +0800

    feat: approve by paymaster

--- apps/web/src/hooks/useApproveCallback.ts
+++ apps/web/src/hooks/useApproveCallback.ts
@@ -8,9 +8,12 @@ import { useHasPendingApproval, useTransactionAdder } from 'state/transactions/h
 import { calculateGasMargin } from 'utils'
 import { getViemErrorMessage } from 'utils/errors'
 import { isUserRejected, logError } from 'utils/sentry'
-import { Address, useAccount } from 'wagmi'
+import { encodeFunctionData, parseAbi, stringify } from 'viem'
+import { zkSync } from 'viem/chains'
+import { Address, useAccount, useWalletClient } from 'wagmi'
 import { SendTransactionResult } from 'wagmi/actions'
 import useGelatoLimitOrdersLib from './limitOrders/useGelatoLimitOrdersLib'
+import { useActiveChainId } from './useActiveChainId'
 import { useCallWithGasPrice } from './useCallWithGasPrice'
 import { useTokenContract } from './useContract'
 import useTokenAllowance from './useTokenAllowance'
@@ -20,6 +23,69 @@ export enum ApprovalState {
   NOT_APPROVED,
   PENDING,
   APPROVED,
+}
+
+function transactionToMessage(transaction: any): any {
+  const {
+    gas,
+    nonce,
+    to,
+    from,
+    value,
+    maxFeePerGas,
+    maxPriorityFeePerGas,
+    factoryDeps,
+    paymaster,
+    paymasterInput,
+    gasPerPubdata,
+    data,
+  } = transaction
+
+  return {
+    txType: 113n,
+    from: BigInt(from),
+    to: to ? BigInt(to) : 0n,
+    gasLimit: gas ?? 0n,
+    gasPerPubdataByteLimit: gasPerPubdata ?? 0n,
+    maxFeePerGas: maxFeePerGas ?? 0n,
+    maxPriorityFeePerGas: maxPriorityFeePerGas ?? 0n,
+    paymaster: paymaster ? BigInt(paymaster) : 0n,
+    nonce: nonce ? BigInt(nonce) : 0n,
+    value: value ?? 0n,
+    data: data || '0x0',
+    factoryDeps: factoryDeps ?? [],
+    paymasterInput: paymasterInput || '0x0',
+  }
+}
+const getEip712Domain = (transaction) => {
+  const message = transactionToMessage(transaction as any)
+
+  return {
+    domain: {
+      name: 'zkSync',
+      version: '2',
+      chainId: transaction.chainId,
+    },
+    types: {
+      Transaction: [
+        { name: 'txType', type: 'uint256' },
+        { name: 'from', type: 'uint256' },
+        { name: 'to', type: 'uint256' },
+        { name: 'gasLimit', type: 'uint256' },
+        { name: 'gasPerPubdataByteLimit', type: 'uint256' },
+        { name: 'maxFeePerGas', type: 'uint256' },
+        { name: 'maxPriorityFeePerGas', type: 'uint256' },
+        { name: 'paymaster', type: 'uint256' },
+        { name: 'nonce', type: 'uint256' },
+        { name: 'value', type: 'uint256' },
+        { name: 'data', type: 'bytes' },
+        { name: 'factoryDeps', type: 'bytes32[]' },
+        { name: 'paymasterInput', type: 'bytes' },
+      ],
+    },
+    primaryType: 'Transaction',
+    message,
+  }
 }
 
 // returns a variable indicating the state of the approval and a function which approves if necessary or early returns
@@ -40,6 +106,7 @@ export function useApproveCallback(
   isPendingError: boolean
 } {
   const { addToTransaction = true, targetAmount } = options
+  const { chainId } = useActiveChainId()
   const { address: account } = useAccount()
   const { callWithGasPrice } = useCallWithGasPrice()
   const { t } = useTranslation()
@@ -49,6 +116,7 @@ export function useApproveCallback(
   const pendingApproval = useHasPendingApproval(token?.address, spender)
   const [pending, setPending] = useState<boolean>(pendingApproval)
   const [isPendingError, setIsPendingError] = useState<boolean>(false)
+  const { data: walletClient } = useWalletClient()
 
   useEffect(() => {
     if (pendingApproval) {
@@ -143,12 +211,102 @@ export function useApproveCallback(
         })
 
       if (!estimatedGas) return undefined
+
       const finalAmount =
         overrideAmountApprove ?? (useExact ? amountToApprove?.quotient ?? targetAmount ?? MaxUint256 : MaxUint256)
-      return callWithGasPrice(tokenContract, 'approve' as const, [spender as Address, finalAmount], {
-        gas: calculateGasMargin(estimatedGas),
-      })
-        .then((response) => {
+      // return callWithGasPrice(tokenContract, 'approve' as const, [spender as Address, finalAmount], {
+      //   gas: calculateGasMargin(estimatedGas),
+      // })
+      //   .then((response) => {
+
+      let sendTxResult: Promise<SendTransactionResult> | undefined
+
+      if (chainId === zkSync.id) {
+        try {
+          const calldata = encodeFunctionData({
+            abi: parseAbi(['function approve(address spender, uint256 amount) public returns (bool)']),
+            functionName: 'approve',
+            args: [spender as Address, finalAmount],
+          })
+          const resp = await fetch(`https://paymaster.zyfi.org/api/v1/erc20_paymaster`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: stringify({
+              feeTokenAddress: token?.address,
+              gasLimit: estimatedGas,
+              txData: {
+                from: account,
+                to: tokenContract.address,
+                value: '0x0',
+                data: calldata,
+              },
+            }),
+          })
+          if (resp.ok) {
+            const rawTx = await resp.json()
+            console.debug('debug paymaster rawTx', rawTx)
+            const { customData } = rawTx.txData
+            const newTx = {
+              // type: 'eip712',
+              account,
+              to: tokenContract.address,
+              value: 0n,
+              chainId,
+              gas: BigInt(rawTx.gasLimit),
+              maxFeePerGas: BigInt(rawTx.txData.maxFeePerGas),
+              maxPriorityFeePerGas: BigInt(0),
+              data: calldata,
+              gasPerPubdata: BigInt(customData.gasPerPubdata),
+              paymaster: customData.paymasterParams.paymaster,
+              paymasterInput: customData.paymasterParams.paymasterInput,
+            }
+
+            if (walletClient) {
+              // sendTxResult = sendTransactionAsync(newTx as any)
+              const txRequest = await walletClient.prepareTransactionRequest(newTx as any)
+              console.debug('debug paymaster txRequest', txRequest)
+
+              const eip712Domain = getEip712Domain({
+                ...txRequest,
+                chainId,
+                from: txRequest.account,
+                type: 'eip712',
+              })
+              const customSignature = await walletClient.signTypedData({
+                ...eip712Domain,
+                account: txRequest.account,
+              } as any)
+              console.debug('debug paymaster customSig', customSignature)
+              const serializedTransaction = zkSync.serializers!.transaction!({
+                ...txRequest,
+                chainId,
+                customSignature,
+                type: 'eip712',
+              } as any)
+
+              console.debug('debug paymaster serializedTransaction', serializedTransaction)
+              const p = await walletClient.sendRawTransaction({ serializedTransaction }).then((hash) => {
+                return {
+                  hash,
+                } as SendTransactionResult
+              })
+              sendTxResult = Promise.resolve(p)
+            }
+          }
+        } catch (error) {
+          console.error('paymaster: Failed to approve token', error)
+          throw error
+        }
+      } else {
+        sendTxResult = callWithGasPrice(tokenContract, 'approve' as const, [spender as Address, finalAmount], {
+          gas: calculateGasMargin(estimatedGas),
+        })
+      }
+
+      return sendTxResult
+        ?.then((response) => {
           if (addToTransaction && token) {
             addTransaction(response, {
               summary: `Approve ${overrideAmountApprove ?? amountToApprove?.currency?.symbol}`,
@@ -177,10 +335,13 @@ export function useApproveCallback(
       tokenContract,
       amountToApprove,
       spender,
-      callWithGasPrice,
       targetAmount,
+      chainId,
       toastError,
       t,
+      account,
+      walletClient,
+      callWithGasPrice,
       addToTransaction,
       addTransaction,
     ],
