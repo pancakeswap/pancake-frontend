2e5814bcedbc2cb9d8c462143ee598f19dec552d
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2024-03-14 12:29:58 +0800

    refactor: Update endpoint URL for wallet API integration.

--- packages/smart-router/evm/v3-router/providers/getCommonTokenPrices.ts
+++ packages/smart-router/evm/v3-router/providers/getCommonTokenPrices.ts
@@ -152,7 +152,7 @@ export const getCommonTokenPricesByLlma = createCommonTokenPriceProvider<BySubgr
 
 export const getCommonTokenPricesByWalletApi = createCommonTokenPriceProvider<BySubgraphEssentials>(
   createGetTokenPriceFromLlmaWithCache({
-    endpoint: 'https://wallet-api.pancakeswap.com/v0/prices',
+    endpoint: 'https://wallet-api.pancakeswap.com/v1/prices',
   }),
 )
 
