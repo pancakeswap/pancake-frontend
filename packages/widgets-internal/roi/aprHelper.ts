334eba5b56dff2212f1ca62d85f26790ebbad781
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2024-03-15 17:44:29 +0800

    chore: fix the calculation

--- packages/widgets-internal/roi/aprHelper.ts
+++ packages/widgets-internal/roi/aprHelper.ts
@@ -1,10 +1,10 @@
 import { Percent } from "@pancakeswap/sdk";
 import { formatFraction } from "@pancakeswap/utils/formatFractions";
 
-export function getAccrued(principal: number, apr: Percent, timesCompounded?: number, stakeFor = 1) {
+export function getAccrued(principal: number, apr: Percent, timesCompounded = 0, stakeFor = 1) {
   const aprAsDecimal = parseFloat(formatFraction(apr.asFraction, 6) || "0");
   const daysAsDecimalOfYear = stakeFor / 365;
-  if (timesCompounded && timesCompounded >= stakeFor) {
+  if (timesCompounded !== 0) {
     return principal * (1 + aprAsDecimal / timesCompounded) ** (timesCompounded * daysAsDecimalOfYear);
   }
   return principal * aprAsDecimal * daysAsDecimalOfYear; // simple calc when not compounding
