c69abec0f84b11ffc373ebd24387b9e8a8ed1889
chefmomota <momota@pancakeswap.com>
2024-03-18 13:52:51 +0800

    feat: Turn on Arb Prediction

--- packages/prediction/src/galetoOracleContract.ts
+++ packages/prediction/src/galetoOracleContract.ts
@@ -6,5 +6,5 @@ import { ContractAddresses } from './type'
 export const galetoOracleETH: Record<string, Address> = {
   [ChainId.BSC]: '0x',
   [ChainId.ZKSYNC]: '0xff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace',
-  // [ChainId.ARBITRUM_ONE]: '0x',
+  [ChainId.ARBITRUM_ONE]: '0x',
 } as const satisfies ContractAddresses<SupportedChainId>
--- packages/prediction/src/predictionContract.ts
+++ packages/prediction/src/predictionContract.ts
@@ -6,23 +6,23 @@ import { ContractAddresses } from './type'
 export const predictionsBNB: Record<string, Address> = {
   [ChainId.BSC]: '0x18B2A687610328590Bc8F2e5fEdDe3b582A49cdA',
   [ChainId.ZKSYNC]: '0x',
-  // [ChainId.ARBITRUM_ONE]: '0x',
+  [ChainId.ARBITRUM_ONE]: '0x',
 } as const satisfies ContractAddresses<SupportedChainId>
 
 export const predictionsCAKE: Record<string, Address> = {
   [ChainId.BSC]: '0x0E3A8078EDD2021dadcdE733C6b4a86E51EE8f07',
   [ChainId.ZKSYNC]: '0x',
-  // [ChainId.ARBITRUM_ONE]: '0x',
+  [ChainId.ARBITRUM_ONE]: '0x',
 } as const satisfies ContractAddresses<SupportedChainId>
 
 export const predictionsETH: Record<string, Address> = {
   [ChainId.BSC]: '0x',
   [ChainId.ZKSYNC]: '0x43c7771DEB958A2e3198ED98772056ba70DaA84c',
-  // [ChainId.ARBITRUM_ONE]: '0xF2F90E718a3BFaCb430c1818cB962f05A2631998',
+  [ChainId.ARBITRUM_ONE]: '0xF2F90E718a3BFaCb430c1818cB962f05A2631998',
 } as const satisfies ContractAddresses<SupportedChainId>
 
 export const predictionsWBTC: Record<string, Address> = {
   [ChainId.BSC]: '0x',
   [ChainId.ZKSYNC]: '0x',
-  // [ChainId.ARBITRUM_ONE]: '0x870CBfD72970E6ad146310Dd0EC546Db1Cbbe6F8',
+  [ChainId.ARBITRUM_ONE]: '0x870CBfD72970E6ad146310Dd0EC546Db1Cbbe6F8',
 } as const satisfies ContractAddresses<SupportedChainId>
