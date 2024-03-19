329985ea83d686114248ca37d7373d78b94187dc
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2024-03-19 19:56:54 +0800

    chore: list new bCakeWrapper PM vault on arb (#9362)
    <!--
    Before opening a pull request, please read the [contributing
    guidelines](https://github.com/pancakeswap/pancake-frontend/blob/develop/CONTRIBUTING.md)
    first
    -->
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    The focus of this PR is to update components in the PositionManagers
    feature, introducing a new `rewardToken` prop and adapting the UI to
    display different tokens as rewards.
    
    ### Detailed summary
    - Added `rewardToken` prop to components in PositionManagers
    - Updated UI to display different reward tokens
    - Added new vault configurations for DEFIEDGE in arbitrumTokens
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your
    question}`
    
    <!-- end pr-codex -->

--- apps/web/src/views/PositionManagers/hooks/useApr.ts
+++ apps/web/src/views/PositionManagers/hooks/useApr.ts
@@ -4,7 +4,7 @@ import { BIG_ZERO } from '@pancakeswap/utils/bigNumber'
 import { getBalanceAmount } from '@pancakeswap/utils/formatBalance'
 import { YEAR_IN_SECONDS } from '@pancakeswap/utils/getTimePeriods'
 import BigNumber from 'bignumber.js'
-import { useCakePrice } from 'hooks/useCakePrice'
+import { useCurrencyUsdPrice } from 'hooks/useCurrencyUsdPrice'
 import { useMemo } from 'react'
 import { useTotalStakedInUsd } from 'views/PositionManagers/hooks/useTotalStakedInUsd'
 
@@ -48,7 +48,7 @@ export const useApr = ({
   rewardStartTime,
   farmRewardAmount,
 }: AprProps): AprResult => {
-  const cakePriceBusd = useCakePrice()
+  const { data: rewardUsdPrice } = useCurrencyUsdPrice(earningToken ?? undefined)
 
   const isInCakeRewardDateRange = useMemo(
     () => Date.now() / 1000 < rewardEndTime && Date.now() / 1000 >= rewardStartTime,
@@ -71,7 +71,7 @@ export const useApr = ({
     const totalToken1Usd = getBalanceAmount(new BigNumber(avgToken1Amount), currencyB.decimals).times(
       token1PriceUSD ?? 0,
     )
-    const cakeRewardUsd = getBalanceAmount(new BigNumber(farmRewardAmount ?? 0), 18).times(cakePriceBusd)
+    const cakeRewardUsd = getBalanceAmount(new BigNumber(farmRewardAmount ?? 0), 18).times(rewardUsdPrice ?? 0)
 
     const totalAvgStakedInUsd = totalToken0Usd.plus(totalToken1Usd).plus(cakeRewardUsd)
 
@@ -85,7 +85,7 @@ export const useApr = ({
     token1PriceUSD,
     totalStakedInUsd,
     farmRewardAmount,
-    cakePriceBusd,
+    rewardUsdPrice,
   ])
 
   const cakeYieldApr = useMemo(() => {
@@ -95,10 +95,10 @@ export const useApr = ({
 
     return getBalanceAmount(new BigNumber(rewardPerSecond), earningToken.decimals)
       .times(YEAR_IN_SECONDS)
-      .times(cakePriceBusd)
+      .times(rewardUsdPrice ?? 0)
       .div(totalStakedInUsd)
       .times(100)
-  }, [isInCakeRewardDateRange, earningToken, rewardPerSecond, cakePriceBusd, totalStakedInUsd])
+  }, [isInCakeRewardDateRange, earningToken, rewardPerSecond, rewardUsdPrice, totalStakedInUsd])
 
   const totalApr = useMemo(() => cakeYieldApr.plus(totalLpApr), [cakeYieldApr, totalLpApr])
 
