3fb4adb74608fd845480d143bdf43073c6c5e355
chefilip <philip@pancakeswap.com>
2024-03-21 19:15:10 +0700

    chore: Update connector

--- apps/web/src/utils/wagmi.ts
+++ apps/web/src/utils/wagmi.ts
@@ -3,56 +3,36 @@ import { CyberWalletConnector, isCyberWallet } from '@cyberlab/cyber-app-sdk'
 import { ChainId } from '@pancakeswap/chains'
 import { BloctoConnector } from '@pancakeswap/wagmi/connectors/blocto'
 import { TrustWalletConnector } from '@pancakeswap/wagmi/connectors/trustWallet'
+import { CHAINS as chains } from 'config/chains'
+import { PUBLIC_NODES } from 'config/nodes'
 import memoize from 'lodash/memoize'
-import { createConfig, createStorage } from 'wagmi'
-import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
-import { InjectedConnector } from 'wagmi/connectors/injected'
-// import { LedgerConnector } from 'wagmi/connectors/ledger'
-import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
-import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
-import { chains, publicClient } from './client'
+import { Transport } from 'viem'
+import { createConfig, createStorage, http } from 'wagmi'
+import { mainnet } from 'wagmi/chains'
+import { coinbaseWallet, injected, walletConnect } from 'wagmi/connectors'
 
-export { chains, publicClient }
-
-export const injectedConnector = new InjectedConnector({
-  chains,
-  options: {
-    shimDisconnect: false,
-  },
+export const injectedConnector = injected({
+  shimDisconnect: false,
 })
 
-export const coinbaseConnector = new CoinbaseWalletConnector({
-  chains,
-  options: {
-    appName: 'PancakeSwap',
-    appLogoUrl: 'https://pancakeswap.com/logo.png',
-  },
+export const coinbaseConnector = coinbaseWallet({
+  appName: 'PancakeSwap',
+  appLogoUrl: 'https://pancakeswap.com/logo.png',
 })
 
-export const walletConnectConnector = new WalletConnectConnector({
-  chains,
-  options: {
-    // ignore the error in test environment
-    // Error: To use QR modal, please install @walletconnect/modal package
-    showQrModal: process.env.NODE_ENV !== 'test',
-    projectId: 'e542ff314e26ff34de2d4fba98db70bb',
-  },
+export const walletConnectConnector = walletConnect({
+  // ignore the error in test environment
+  // Error: To use QR modal, please install @walletconnect/modal package
+  showQrModal: process.env.NODE_ENV !== 'test',
+  projectId: 'e542ff314e26ff34de2d4fba98db70bb',
 })
 
-export const walletConnectNoQrCodeConnector = new WalletConnectConnector({
-  chains,
-  options: {
-    showQrModal: false,
-    projectId: 'e542ff314e26ff34de2d4fba98db70bb',
-  },
+export const walletConnectNoQrCodeConnector = walletConnect({
+  showQrModal: false,
+  projectId: 'e542ff314e26ff34de2d4fba98db70bb',
 })
 
-export const metaMaskConnector = new MetaMaskConnector({
-  chains,
-  options: {
-    shimDisconnect: false,
-  },
-})
+export const metaMaskConnector = injected({ target: 'metaMask', shimDisconnect: false })
 
 const bloctoConnector = new BloctoConnector({
   chains,
@@ -61,13 +41,6 @@ const bloctoConnector = new BloctoConnector({
     appId: 'e2f2f0cd-3ceb-4dec-b293-bb555f2ed5af',
   },
 })
-
-// const ledgerConnector = new LedgerConnector({
-//   chains,
-//   options: {
-//     projectId: 'e542ff314e26ff34de2d4fba98db70bb',
-//   },
-// })
 
 export const trustWalletConnector = new TrustWalletConnector({
   chains,
@@ -97,17 +70,48 @@ export const binanceWeb3WalletConnector = new BinanceW3WConnector({
 
 export const noopStorage = {
   getItem: (_key: any) => '',
-  setItem: (_key: any, _value: any) => null,
-  removeItem: (_key: any) => null,
+  setItem: (_key: any, _value: any) => {},
+  removeItem: (_key: any) => {},
 }
 
+const transports: Record<number, Transport> = chains.reduce((ts, chain) => {
+  let httpStrings: string[] = []
+
+  if (process.env.NODE_ENV === 'test' && chain.id === mainnet.id) {
+    httpStrings = ['https://ethereum.publicnode.com']
+  }
+
+  httpStrings = PUBLIC_NODES[chain.id] ? PUBLIC_NODES[chain.id] : undefined
+
+  if (ts) {
+    return {
+      ...ts,
+      [chain.id]: httpStrings.map((t) => http(t)),
+    }
+  }
+
+  return {
+    [chain.id]: httpStrings.map((t) => http(t)),
+  }
+}, {})
+
 export const wagmiConfig = createConfig({
+  chains,
+
   storage: createStorage({
     storage: typeof window !== 'undefined' ? window.localStorage : noopStorage,
     key: 'wagmi_v1.1',
   }),
-  autoConnect: false,
-  publicClient,
+  syncConnectedChain: false,
+  transports,
+  batch: {
+    multicall: {
+      batchSize: 1024 * 200,
+      wait: 16,
+    },
+  },
+  pollingInterval: 6_000,
+
   connectors: [
     metaMaskConnector,
     injectedConnector,
