5e1468213c85f6dd76dba5fe8a43c94f67b77522
wmroar <wmroar@gmail.com>
2024-03-25 22:53:42 +0800

    Create shadowRealm.ts to list a new game
    shadow realm

---
+++ packages/games/src/constants/games/shadowRealm.ts
@@ -0,0 +1,74 @@
+import { GameType, GenreType, PostersItemDataType, PostersLayout, TrendingTagType } from '../../types'
+
+export const shadowRealm: GameType = {
+  id: 'shadow-realm',
+  projectName: 'ChainzStudios',
+  title: 'Shadow Realm',
+  subTitle: 'Put your CAKE and Pancake NFTS to the test in this 3D Action-RPG',
+  description: `Join "Shadow Realms, Chainz Studios" RPG on PancakeSwap. Battle the Zerg, explore Dragon Cry, and become the ultimate Awakener. Your adventure starts now.`,
+  publishDate: 1711612800,
+  genre: GenreType.MMORPG,
+  trendingTags: [
+    TrendingTagType.MMORPG,
+    TrendingTagType.Strategy,
+    TrendingTagType.PancakeSquadNFT,
+    TrendingTagType.PancakeBunniesNFT,
+    TrendingTagType.Multiplayer,
+    TrendingTagType.CakeToken,
+  ],
+  headerImage: 'https://shadow-realm.chainzstudios.com/overview/header.jpg',
+  headerIconImage: {
+    desktop: 'https://shadow-realm.chainzstudios.com/overview/headerIcon.png',
+    mobile: 'https://shadow-realm.chainzstudios.com/overview/headerIcon.png',
+  },
+  projectLogo: {
+    lightTheme: 'https://shadow-realm.chainzstudios.com/overview/logo.png',
+    darkTheme: 'https://shadow-realm.chainzstudios.com/overview/logo.png',
+  },
+  projectCircleLogo: {
+    lightTheme: 'https://shadow-realm.chainzstudios.com/overview/logo-circle.png',
+    darkTheme: 'https://shadow-realm.chainzstudios.com/overview/logo-circle.png',
+  },
+  gameLink: {
+    playNowLink: 'https://www.chainzstudios.com/shadow-realm/',
+  },
+  posters: {
+    layout: PostersLayout.Horizontal,
+    items: [
+      {
+        type: PostersItemDataType.Video,
+        video: 'https://shadow-realm.chainzstudios.com/overview/shadow_realms.mp4',
+        image: 'https://shadow-realm.chainzstudios.com/overview/vd.png',
+      },
+      {
+        type: PostersItemDataType.Image,
+        image: 'https://shadow-realm.chainzstudios.com/overview/1.png',
+      },
+      {
+        type: PostersItemDataType.Image,
+        image: 'https://shadow-realm.chainzstudios.com/overview/2.png',
+      },
+      {
+        type: PostersItemDataType.Image,
+        image: 'https://shadow-realm.chainzstudios.com/overview/3.png',
+      },
+      {
+        type: PostersItemDataType.Image,
+        image: 'https://shadow-realm.chainzstudios.com/overview/4.jpg',
+      },
+      {
+        type: PostersItemDataType.Image,
+        image: 'https://shadow-realm.chainzstudios.com/overview/5.jpg',
+      },
+      {
+        type: PostersItemDataType.Image,
+        image: 'https://shadow-realm.chainzstudios.com/overview/6.jpg',
+      },
+    ],
+  },
+  playlist: [],
+  socialMedia: {
+    telegram: 'https://t.me/PancakeSwap/',
+    discord: 'https://discord.gg/pancakeswap',
+  },
+}
