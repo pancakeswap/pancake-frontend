dadadb77eccd4a7a06437e0cfb006a6b9986be98
wmroar <wmroar@gmail.com>
2024-03-25 22:52:33 +0800

    Update index.ts
    to list a new game

--- packages/games/src/constants/games/index.ts
+++ packages/games/src/constants/games/index.ts
@@ -4,9 +4,10 @@ import { GameType } from '../../types'
 import { binaryX } from './binaryX'
 import { nemesisDownfall } from './nemesisDownfall'
 import { pancakeProtectors } from './pancakeProtectors'
+import { shadowRealm } from './shadowRealm'
 
-export const TOP_GAMES_LIST: GameType[] = [pancakeProtectors]
+export const TOP_GAMES_LIST: GameType[] = [shadowRealm]
 
-export const DEFAULT_GAMES_LIST: GameType[] = [nemesisDownfall, binaryX]
+export const DEFAULT_GAMES_LIST: GameType[] = [nemesisDownfall, binaryX, pancakeProtectors]
 
 export const GAMES_LIST = [...TOP_GAMES_LIST, ...shuffle(DEFAULT_GAMES_LIST)]
