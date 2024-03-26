bfdc9154e048b9584117dbf828536967f53a46c3
wmroar <wmroar@gmail.com>
2024-03-26 10:40:39 +0800

    Update index.ts change game position

--- packages/games/src/constants/games/index.ts
+++ packages/games/src/constants/games/index.ts
@@ -6,8 +6,8 @@ import { nemesisDownfall } from './nemesisDownfall'
 import { pancakeProtectors } from './pancakeProtectors'
 import { shadowRealm } from './shadowRealm'
 
-export const TOP_GAMES_LIST: GameType[] = [shadowRealm]
+export const TOP_GAMES_LIST: GameType[] = [pancakeProtectors]
 
-export const DEFAULT_GAMES_LIST: GameType[] = [nemesisDownfall, binaryX, pancakeProtectors]
+export const DEFAULT_GAMES_LIST: GameType[] = [nemesisDownfall, binaryX, shadowRealm]
 
 export const GAMES_LIST = [...TOP_GAMES_LIST, ...shuffle(DEFAULT_GAMES_LIST)]
