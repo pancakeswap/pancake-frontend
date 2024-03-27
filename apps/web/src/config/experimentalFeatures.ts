76bc38575ca7d469225de3da0481846a75db2a79
Chef Jerry <jerry@pancakeswap.com>
2024-03-27 11:24:44 +0800

    feat: open 50%

--- apps/web/src/config/experimentalFeatures.ts
+++ apps/web/src/config/experimentalFeatures.ts
@@ -31,7 +31,7 @@ export const EXPERIMENTAL_FEATURE_CONFIGS: ExperimentalFeatureConfigs = [
   },
   {
     feature: EXPERIMENTAL_FEATURES.UniversalRouter,
-    percentage: 0.2,
+    percentage: 0.5,
     whitelist: [],
   },
 ]
