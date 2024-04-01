e07e73d86684fa1e1a8ec3d65ecc3d4c6a1e0fa3
chefjackson <116779127+chefjackson@users.noreply.github.com>
2024-04-01 15:11:46 +0800

    chore: fast routing opt in 50%

--- apps/web/src/config/experimentalFeatures.ts
+++ apps/web/src/config/experimentalFeatures.ts
@@ -27,7 +27,7 @@ export const EXPERIMENTAL_FEATURE_CONFIGS: ExperimentalFeatureConfigs = [
   },
   {
     feature: EXPERIMENTAL_FEATURES.SpeedQuote,
-    percentage: 0.3,
+    percentage: 0.5,
     whitelist: [],
   },
   {
