8cdbb9eedeef6b196c7acb773b29fe868113cf63
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2024-04-02 21:41:26 +0800

    chore: fix mobile UI

--- packages/widgets-internal/farm/components/FarmTable/Actions/StakeComponent.tsx
+++ packages/widgets-internal/farm/components/FarmTable/Actions/StakeComponent.tsx
@@ -1,5 +1,5 @@
 import { useTranslation } from "@pancakeswap/localization";
-import { Button, Text } from "@pancakeswap/uikit";
+import { Button, Text, useMatchBreakpoints } from "@pancakeswap/uikit";
 import { ActionContent, ActionTitles, StyledActionContainer } from "./styles";
 
 export interface StakeComponentProps {
@@ -16,11 +16,23 @@ const StakeComponent: React.FunctionComponent<React.PropsWithChildren<StakeCompo
   bCakeInfoSlot,
 }) => {
   const { t } = useTranslation();
-
+  const { isMobile } = useMatchBreakpoints();
   return (
-    <StyledActionContainer style={bCakeInfoSlot ? { display: "flex", gap: 16, alignItems: "center" } : undefined}>
-      {!bCakeInfoSlot && (
-        <ActionTitles>
+    <StyledActionContainer
+      style={
+        bCakeInfoSlot
+          ? {
+              display: "flex",
+              gap: 16,
+              alignItems: "center",
+              flexDirection: isMobile ? "column" : "row",
+              minHeight: isMobile ? "auto" : undefined,
+            }
+          : undefined
+      }
+    >
+      {(!bCakeInfoSlot || isMobile) && (
+        <ActionTitles style={bCakeInfoSlot && isMobile ? { alignItems: "flex-start", width: "100%" } : undefined}>
           <Text bold textTransform="uppercase" color="textSubtle" fontSize="12px" pr="4px">
             {t("Stake")}
           </Text>
@@ -29,7 +41,7 @@ const StakeComponent: React.FunctionComponent<React.PropsWithChildren<StakeCompo
           </Text>
         </ActionTitles>
       )}
-      <ActionContent style={bCakeInfoSlot ? { flexGrow: 1 } : undefined}>
+      <ActionContent style={bCakeInfoSlot ? { flexGrow: 1, width: isMobile ? "100%" : undefined } : undefined}>
         <Button width="100%" onClick={onPresentDeposit} variant="secondary" disabled={isStakeReady}>
           {t("Stake LP")}
         </Button>
