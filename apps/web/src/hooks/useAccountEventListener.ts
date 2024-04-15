7377e9b7c45f00e7c9c36c5fb986469e9f38a1e6
Chef Jerry <jerry@pancakeswap.com>
2024-04-15 16:00:49 +0800

    refactor: use emitter listener

--- apps/web/src/hooks/useAccountEventListener.ts
+++ apps/web/src/hooks/useAccountEventListener.ts
@@ -1,10 +1,33 @@
+import { useCallback, useEffect } from 'react'
 import { useAccount, useAccountEffect } from 'wagmi'
 import { useAppDispatch } from '../state'
 import { clearUserStates } from '../utils/clearUserStates'
+import { useSwitchNetworkLocal } from './useSwitchNetwork'
+
+export const useChainIdListener = () => {
+  const switchNetworkCallback = useSwitchNetworkLocal()
+  const onChainChanged = useCallback(
+    ({ chainId }: { chainId?: number }) => {
+      if (chainId === undefined) return
+      switchNetworkCallback(chainId)
+    },
+    [switchNetworkCallback],
+  )
+  const { connector } = useAccount()
+
+  useEffect(() => {
+    connector?.emitter?.on('change', onChainChanged)
+
+    return () => {
+      connector?.emitter?.off('change', onChainChanged)
+    }
+  })
+}
 
 export const useAccountEventListener = () => {
   const dispatch = useAppDispatch()
   const { chainId } = useAccount()
+  useChainIdListener()
 
   useAccountEffect({
     onDisconnect() {
