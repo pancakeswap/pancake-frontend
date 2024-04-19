ba0c7e007ff6fec1fad9a50183344bb0a3dc023a
Chef Jerry <jerry@pancakeswap.com>
2024-04-19 14:45:55 +0800

    chore: add cl pool

---
+++ packages/v4-sdk/src/functions/clamm/index.ts
@@ -0,0 +1,37 @@
+import { Currency, CurrencyAmount } from '@pancakeswap/swap-sdk-core'
+import invariant from 'tiny-invariant'
+
+export type BinPool = {
+  activeId: bigint
+  swapFee: bigint
+  reserveOfBin: Record<
+    string,
+    {
+      reserve0: bigint
+      reserve1: bigint
+    }
+  >
+}
+
+export const swap = ({
+  pool,
+  zeroForOne,
+  binStep,
+  amountIn,
+}: {
+  pool: BinPool
+  zeroForOne: boolean
+  binStep: bigint
+  amountIn: CurrencyAmount<Currency>
+}) => {
+  invariant(amountIn.greaterThan(0), 'AMOUNT_IN')
+
+  while (true) {
+    const { reserve0, reserve1 } = pool.reserveOfBin[pool.activeId.toString()]
+    const reserve = zeroForOne ? reserve0 : reserve1
+    if (reserve !== 0n) {
+      const amountOut = zeroForOne ? reserve1 : reserve0
+      return amountOut
+    }
+  }
+}
