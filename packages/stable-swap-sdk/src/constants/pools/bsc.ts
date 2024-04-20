b4a1d82892806597e93247e1fcdf24609c2fc6ea
chefjackson <116779127+chefjackson@users.noreply.github.com>
2024-04-20 13:40:43 +0800

    chore: List new stable pair (#9642)
    <!--
    Before opening a pull request, please read the [contributing
    guidelines](https://github.com/pancakeswap/pancake-frontend/blob/develop/CONTRIBUTING.md)
    first
    -->
    
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    This PR adds a new stable pair for wBETH and mwBETH on BSC chain.
    
    ### Detailed summary
    - Added mwBETH token to BSC tokens constants
    - Added mwBETH-wBETH LP pool to BSC pools constants
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your
    question}`
    
    <!-- end pr-codex -->

--- packages/stable-swap-sdk/src/constants/pools/bsc.ts
+++ packages/stable-swap-sdk/src/constants/pools/bsc.ts
@@ -123,4 +123,14 @@ export const pools: StableSwapPool[] = [
     stableLpFee: 0.0025,
     stableLpFeeRateOfTotalFee: 0.5,
   },
+  {
+    lpSymbol: 'mwBETH-wBETH LP',
+    lpAddress: '0x4cBEa76B4A1c42C356B4c52B0314A98313fFE9df',
+    token: bscTokens.mwbeth,
+    quoteToken: bscTokens.wbeth,
+    stableSwapAddress: '0xfF5Ce4846A3708EA9befa6c3Ab145e63f65DC045',
+    infoStableSwapAddress: '0x150c8AbEB487137acCC541925408e73b92F39A50',
+    stableLpFee: 0.0002,
+    stableLpFeeRateOfTotalFee: 0.5,
+  },
 ]
