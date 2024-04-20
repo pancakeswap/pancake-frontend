2c9922d79d9bda3038d37b7076e6add6a05c7677
memoyil <2213635+memoyil@users.noreply.github.com>
2024-04-20 12:58:38 +0200

    feat: Add multi chain tvl tx info

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,12 +1,15 @@
 import { dehydrate, QueryClient } from '@tanstack/react-query'
 import dayjs from 'dayjs'
-import { gql } from 'graphql-request'
+import { gql, GraphQLClient } from 'graphql-request'
 import { GetStaticProps } from 'next'
 import { getCakeVaultAddress } from 'utils/addressHelpers'
 import { getCakeContract } from 'utils/contractHelpers'
 import { getBlocksFromTimestamps } from 'utils/getBlocksFromTimestamps'
-import { bitQueryServerClient, infoServerClient } from 'utils/graphql'
+import { bitQueryServerClient } from 'utils/graphql'
 import { formatEther } from 'viem'
+import { V2_SUBGRAPH_URLS, V3_SUBGRAPH_URLS } from 'config/constants/endpoints'
+import { ChainId } from '@pancakeswap/chains'
+import addresses from 'config/constants/contracts'
 import Home from '../views/Home'
 
 const IndexPage = () => {
@@ -22,10 +25,18 @@ const tvl = 6082955532.115718
 export const getStaticProps: GetStaticProps = async () => {
   const queryClient = new QueryClient()
 
-  const totalTxQuery = gql`
+  const totalTxV2Query = gql`
     query TotalTransactions($block: Block_height) {
-      pancakeFactory(block: $block) {
+      pancakeFactories(block: $block) {
         totalTransactions
+      }
+    }
+  `
+
+  const totalTxV3Query = gql`
+    query TotalTransactions($block: Block_height) {
+      factories(block: $block) {
+        txCount
       }
     }
   `
@@ -38,6 +49,32 @@ export const getStaticProps: GetStaticProps = async () => {
     tvl,
   }
 
+  const v3ProdClients = Object.entries(V3_SUBGRAPH_URLS)
+    .filter(([string, clientUrl]) => {
+      return Boolean(!ChainId[string].toLowerCase().includes('test') && clientUrl)
+    })
+    .map(([, clientUrl]) => {
+      return new GraphQLClient(clientUrl!, {
+        timeout: 5000,
+        headers: {
+          origin: 'https://pancakeswap.finance',
+        },
+      })
+    })
+
+  const v2ProdClients = Object.entries(V2_SUBGRAPH_URLS)
+    .filter(([string, clientUrl]) => {
+      return Boolean(!ChainId[string].toLowerCase().includes('test') && clientUrl)
+    })
+    .map(([, clientUrl]) => {
+      return new GraphQLClient(clientUrl!, {
+        timeout: 5000,
+        headers: {
+          origin: 'https://pancakeswap.finance',
+        },
+      })
+    })
+
   try {
     const [days30AgoBlock] = await getBlocksFromTimestamps([days30Ago.unix()])
 
@@ -45,20 +82,123 @@ export const getStaticProps: GetStaticProps = async () => {
       throw new Error('No block found for 30 days ago')
     }
 
-    const totalTx = await infoServerClient.request<any>(totalTxQuery)
-    const totalTx30DaysAgo = await infoServerClient.request<any>(totalTxQuery, {
-      block: {
-        number: days30AgoBlock.number,
-      },
-    })
+    const v2TotalTxResults: any[] = (
+      await Promise.all(
+        v2ProdClients.map(async (client) => {
+          let result
+          try {
+            result = await client.request<any>(totalTxV2Query)
+          } catch (error) {
+            if (process.env.NODE_ENV === 'production') {
+              console.error('Error when fetching tvl stats', error)
+            }
+          }
+          return result
+        }),
+      )
+    ).filter(Boolean)
 
-    if (
-      totalTx?.pancakeFactory?.totalTransactions &&
-      totalTx30DaysAgo?.pancakeFactory?.totalTransactions &&
-      parseInt(totalTx.pancakeFactory.totalTransactions) > parseInt(totalTx30DaysAgo.pancakeFactory.totalTransactions)
-    ) {
-      results.totalTx30Days =
-        parseInt(totalTx.pancakeFactory.totalTransactions) - parseInt(totalTx30DaysAgo.pancakeFactory.totalTransactions)
+    const v2TotalTx30DaysAgoResults: any[] = (
+      await Promise.all(
+        v2ProdClients.map(async (client) => {
+          let result
+          try {
+            result = await client.request<any>(totalTxV2Query, {
+              block: {
+                number: days30AgoBlock.number,
+              },
+            })
+          } catch (error) {
+            if (process.env.NODE_ENV === 'production') {
+              console.error('Error when fetching tvl stats', error)
+            }
+          }
+          return result
+        }),
+      )
+    ).filter(Boolean)
+
+    const v2TotalTx = v2TotalTxResults
+      .map((factories) => {
+        return factories.pancakeFactories?.[0]
+      })
+      .filter(Boolean)
+      .map((transactions) => {
+        return transactions.totalTransactions
+      })
+      .reduce((acc, v2Tx) => acc + parseFloat(v2Tx), 0)
+
+    const v2Total30DaysAgoTx = v2TotalTx30DaysAgoResults
+      .map((factories) => {
+        return factories.pancakeFactories?.[0]
+      })
+      .filter(Boolean)
+      .map((transactions) => {
+        return transactions.totalTransactions
+      })
+      .reduce((acc, v2Tx30Ago) => acc + parseFloat(v2Tx30Ago), 0)
+
+    const v3TotalTxResults: any[] = (
+      await Promise.all(
+        v3ProdClients.map(async (client) => {
+          let result
+          try {
+            result = await client.request<any>(totalTxV3Query)
+          } catch (error) {
+            if (process.env.NODE_ENV === 'production') {
+              console.error('Error when fetching tvl stats', error)
+            }
+          }
+          return result
+        }),
+      )
+    ).filter(Boolean)
+
+    const v3TotalTx30DaysAgoResults: any[] = (
+      await Promise.all(
+        v3ProdClients.map(async (client) => {
+          let result
+          try {
+            result = await client.request<any>(totalTxV3Query, {
+              block: {
+                number: days30AgoBlock.number,
+              },
+            })
+          } catch (error) {
+            if (process.env.NODE_ENV === 'production') {
+              console.error('Error when fetching tvl stats', error)
+            }
+          }
+          return result
+        }),
+      )
+    ).filter(Boolean)
+
+    const v3TotalTx = v3TotalTxResults
+      .map((factories) => {
+        return factories.factories?.[0]
+      })
+      .filter(Boolean)
+      .map((transactions) => {
+        return transactions.txCount
+      })
+      .reduce((acc, v3Tx) => acc + parseFloat(v3Tx), 0)
+
+    const v3Total30DaysAgoTx = v3TotalTx30DaysAgoResults
+      .map((factories) => {
+        return factories.factories?.[0]
+      })
+      .filter(Boolean)
+      .map((transactions) => {
+        return transactions.txCount
+      })
+      .reduce((acc, v3Tx30DaysAgo) => acc + parseFloat(v3Tx30DaysAgo), 0)
+
+    const totalTx = parseInt(v3TotalTx + v2TotalTx)
+    const totalTx30DaysAgo = parseInt(v3Total30DaysAgoTx + v2Total30DaysAgoTx)
+
+    if (totalTx && totalTx30DaysAgo && totalTx > totalTx30DaysAgo) {
+      results.totalTx30Days = totalTx - totalTx30DaysAgo
     }
   } catch (error) {
     if (process.env.NODE_ENV === 'production') {
@@ -93,19 +233,80 @@ export const getStaticProps: GetStaticProps = async () => {
   }
 
   try {
-    const result = await infoServerClient.request<any>(gql`
-      query tvl {
-        pancakeFactories(first: 1) {
-          totalLiquidityUSD
-        }
-      }
-    `)
+    const v3TvlResults: any[] = (
+      await Promise.all(
+        v3ProdClients.map(async (client) => {
+          let result
+          try {
+            result = await client.request<any>(gql`
+              query tvl {
+                factories(first: 1) {
+                  totalValueLockedUSD
+                }
+              }
+            `)
+          } catch (error) {
+            if (process.env.NODE_ENV === 'production') {
+              console.error('Error when fetching tvl stats', error)
+            }
+          }
+          return result
+        }),
+      )
+    ).filter(Boolean)
+
+    const v2TvlResults: any[] = (
+      await Promise.all(
+        v2ProdClients.map(async (client) => {
+          let result
+          try {
+            result = await client.request<any>(gql`
+              query tvl {
+                pancakeFactories(first: 1) {
+                  totalLiquidityUSD
+                }
+              }
+            `)
+          } catch (error) {
+            if (process.env.NODE_ENV === 'production') {
+              console.error('Error when fetching tvl stats', error)
+            }
+          }
+          return result
+        }),
+      )
+    ).filter(Boolean)
+
+    const v3Tvl = v3TvlResults
+      .map((factories) => {
+        return factories.factories?.[0]
+      })
+      .filter(Boolean)
+      .map((valueLocked) => {
+        return valueLocked.totalValueLockedUSD
+      })
+      .reduce((acc, v3TvlString) => acc + parseFloat(v3TvlString), 0)
+
+    const v2Tvl = v2TvlResults
+      .map((factories) => {
+        return factories.pancakeFactories?.[0]
+      })
+      .filter(Boolean)
+      .map((valueLocked) => {
+        return valueLocked.totalLiquidityUSD
+      })
+      .reduce((acc, v2TvlString) => acc + parseFloat(v2TvlString), 0)
+
     const cake = await (await fetch('https://farms-api.pancakeswap.com/price/cake')).json()
-    const { totalLiquidityUSD } = result.pancakeFactories[0]
     const cakeVaultV2 = getCakeVaultAddress()
     const cakeContract = getCakeContract()
     const totalCakeInVault = await cakeContract.read.balanceOf([cakeVaultV2])
-    results.tvl = parseFloat(formatEther(totalCakeInVault)) * cake.price + parseFloat(totalLiquidityUSD)
+    const totalCakeInVE = await cakeContract.read.balanceOf([addresses.veCake[ChainId.BSC]])
+    results.tvl =
+      parseFloat(formatEther(totalCakeInVault)) * cake.price +
+      parseFloat(formatEther(totalCakeInVE)) * cake.price +
+      v3Tvl +
+      v2Tvl
   } catch (error) {
     if (process.env.NODE_ENV === 'production') {
       console.error('Error when fetching tvl stats', error)
