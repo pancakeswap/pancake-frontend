ed602d285f3961eb66be6ddff2ddc3a9277416f8
memoyil <2213635+memoyil@users.noreply.github.com>
2024-04-23 20:29:27 +0200

    fix: Use api

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -9,7 +9,12 @@ const IndexPage = () => {
 export const getStaticProps: GetStaticProps = async () => {
   const queryClient = new QueryClient()
   try {
-    const results = await (await fetch('api/info')).json()
+    let results
+    if (process.env.NODE_ENV === 'production') {
+      results = await (await fetch('https://pancakeswap.finance/api/info')).json()
+    } else {
+      results = await (await fetch('http://localhost:3000/api/info')).json()
+    }
     queryClient.setQueryData(['totalTx30Days'], results.totalTx30Days)
     queryClient.setQueryData(['tvl'], results.tvl)
     queryClient.setQueryData(['addressCount30Days'], results.addressCount30Days)
