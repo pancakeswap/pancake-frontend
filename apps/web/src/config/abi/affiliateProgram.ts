a82ebe0bab26f7736a7cad24a5cd63bcb6ff22b0
ChefMomota <98292246+ChefMomota@users.noreply.github.com>
2024-04-24 23:05:17 +0800

    fix: Affliate Program (#9669)
    <!--
    Before opening a pull request, please read the [contributing
    guidelines](https://github.com/pancakeswap/pancake-frontend/blob/develop/CONTRIBUTING.md)
    first
    -->
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    This PR adds `chainId` and `signer` parameters to `contractHelpers.ts`,
    implements an `useAddressListener` hook, and updates ABI definitions in
    `affiliateProgram.ts`.
    
    ### Detailed summary
    - Added `chainId` and `signer` parameters in `contractHelpers.ts`
    - Implemented `useAddressListener` hook in `useAccountEventListener.ts`
    - Updated ABI definitions in `affiliateProgram.ts`
    
    > The following files were skipped due to too many changes:
    `apps/web/src/config/abi/affiliateProgram.ts`
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your
    question}`
    
    <!-- end pr-codex -->

--- apps/web/src/config/abi/affiliateProgram.ts
+++ apps/web/src/config/abi/affiliateProgram.ts
@@ -1,26 +1,10 @@
 export const affiliateProgramABI = [
   {
     inputs: [
-      {
-        internalType: 'address',
-        name: '_admin',
-        type: 'address',
-      },
-      {
-        internalType: 'address',
-        name: '_signer',
-        type: 'address',
-      },
-      {
-        internalType: 'address',
-        name: '_cake',
-        type: 'address',
-      },
-      {
-        internalType: 'uint256',
-        name: '_dailyLimit',
-        type: 'uint256',
-      },
+      { internalType: 'address', name: '_admin', type: 'address' },
+      { internalType: 'address', name: '_signer', type: 'address' },
+      { internalType: 'address', name: '_cake', type: 'address' },
+      { internalType: 'uint256', name: '_dailyLimit', type: 'uint256' },
     ],
     stateMutability: 'nonpayable',
     type: 'constructor',
@@ -28,68 +12,26 @@ export const affiliateProgramABI = [
   {
     anonymous: false,
     inputs: [
-      {
-        indexed: false,
-        internalType: 'uint256',
-        name: 'nonce',
-        type: 'uint256',
-      },
-      {
-        indexed: false,
-        internalType: 'address',
-        name: 'claimer',
-        type: 'address',
-      },
-      {
-        indexed: false,
-        internalType: 'enum AffiliateProgram.ClaimType',
-        name: 'claimType',
-        type: 'uint8',
-      },
-      {
-        indexed: false,
-        internalType: 'uint256',
-        name: 'amount',
-        type: 'uint256',
-      },
+      { indexed: false, internalType: 'uint256', name: 'nonce', type: 'uint256' },
+      { indexed: false, internalType: 'address', name: 'claimer', type: 'address' },
+      { indexed: false, internalType: 'enum AffiliateProgram.ClaimType', name: 'claimType', type: 'uint8' },
+      { indexed: false, internalType: 'uint256', name: 'amount', type: 'uint256' },
     ],
     name: 'Claim',
     type: 'event',
   },
   {
     anonymous: false,
-    inputs: [
-      {
-        indexed: false,
-        internalType: 'uint256',
-        name: 'timestamp',
-        type: 'uint256',
-      },
-    ],
+    inputs: [{ indexed: false, internalType: 'uint256', name: 'timestamp', type: 'uint256' }],
     name: 'ForceResetLimit',
     type: 'event',
   },
   {
     anonymous: false,
     inputs: [
-      {
-        indexed: false,
-        internalType: 'address',
-        name: 'user',
-        type: 'address',
-      },
-      {
-        indexed: false,
-        internalType: 'enum AffiliateProgram.ClaimType',
-        name: 'claimType',
-        type: 'uint8',
-      },
-      {
-        indexed: false,
-        internalType: 'uint256',
-        name: 'nonce',
-        type: 'uint256',
-      },
+      { indexed: false, internalType: 'address', name: 'user', type: 'address' },
+      { indexed: false, internalType: 'enum AffiliateProgram.ClaimType', name: 'claimType', type: 'uint8' },
+      { indexed: false, internalType: 'uint256', name: 'nonce', type: 'uint256' },
     ],
     name: 'IncrementNonce',
     type: 'event',
@@ -97,84 +39,40 @@ export const affiliateProgramABI = [
   {
     anonymous: false,
     inputs: [
-      {
-        indexed: true,
-        internalType: 'address',
-        name: 'previousOwner',
-        type: 'address',
-      },
-      {
-        indexed: true,
-        internalType: 'address',
-        name: 'newOwner',
-        type: 'address',
-      },
+      { indexed: true, internalType: 'address', name: 'previousOwner', type: 'address' },
+      { indexed: true, internalType: 'address', name: 'newOwner', type: 'address' },
     ],
     name: 'OwnershipTransferred',
     type: 'event',
   },
   {
     anonymous: false,
-    inputs: [
-      {
-        indexed: false,
-        internalType: 'address',
-        name: 'account',
-        type: 'address',
-      },
-    ],
+    inputs: [{ indexed: false, internalType: 'address', name: 'account', type: 'address' }],
     name: 'Paused',
     type: 'event',
   },
   {
     anonymous: false,
-    inputs: [
-      {
-        indexed: false,
-        internalType: 'uint256',
-        name: 'dailyLimit',
-        type: 'uint256',
-      },
-    ],
+    inputs: [{ indexed: false, internalType: 'uint256', name: 'dailyLimit', type: 'uint256' }],
     name: 'SetDailyLimit',
     type: 'event',
   },
   {
     anonymous: false,
-    inputs: [
-      {
-        indexed: false,
-        internalType: 'address',
-        name: 'signer',
-        type: 'address',
-      },
-    ],
+    inputs: [{ indexed: false, internalType: 'address', name: 'signer', type: 'address' }],
     name: 'SetSigner',
     type: 'event',
   },
   {
     anonymous: false,
-    inputs: [
-      {
-        indexed: false,
-        internalType: 'address',
-        name: 'account',
-        type: 'address',
-      },
-    ],
+    inputs: [{ indexed: false, internalType: 'address', name: 'account', type: 'address' }],
     name: 'Unpaused',
     type: 'event',
   },
   {
     inputs: [],
     name: 'approvalSigner',
-    outputs: [
-      {
-        internalType: 'address',
-        name: '',
-        type: 'address',
-      },
-    ],
+    outputs: [{ internalType: 'address', name: '', type: 'address' }],
     stateMutability: 'view',
     type: 'function',
   },
@@ -182,41 +80,17 @@ export const affiliateProgramABI = [
     inputs: [
       {
         components: [
-          {
-            internalType: 'uint256',
-            name: 'nonce',
-            type: 'uint256',
-          },
-          {
-            internalType: 'address',
-            name: 'claimer',
-            type: 'address',
-          },
-          {
-            internalType: 'enum AffiliateProgram.ClaimType',
-            name: 'claimType',
-            type: 'uint8',
-          },
-          {
-            internalType: 'uint256',
-            name: 'totalAmount',
-            type: 'uint256',
-          },
-          {
-            internalType: 'uint256',
-            name: 'expiryTimestamp',
-            type: 'uint256',
-          },
+          { internalType: 'uint256', name: 'nonce', type: 'uint256' },
+          { internalType: 'address', name: 'claimer', type: 'address' },
+          { internalType: 'enum AffiliateProgram.ClaimType', name: 'claimType', type: 'uint8' },
+          { internalType: 'uint256', name: 'totalAmount', type: 'uint256' },
+          { internalType: 'uint256', name: 'expiryTimestamp', type: 'uint256' },
         ],
         internalType: 'struct AffiliateProgram.ClaimRequest',
         name: '_request',
         type: 'tuple',
       },
-      {
-        internalType: 'bytes',
-        name: '_signature',
-        type: 'bytes',
-      },
+      { internalType: 'bytes', name: '_signature', type: 'bytes' },
     ],
     name: 'claim',
     outputs: [],
@@ -224,105 +98,28 @@ export const affiliateProgramABI = [
     type: 'function',
   },
   {
-    inputs: [
-      {
-        components: [
-          {
-            internalType: 'uint256',
-            name: 'nonce',
-            type: 'uint256',
-          },
-          {
-            internalType: 'address',
-            name: 'claimer',
-            type: 'address',
-          },
-          {
-            internalType: 'enum AffiliateProgram.ClaimType',
-            name: 'claimType',
-            type: 'uint8',
-          },
-          {
-            internalType: 'uint256',
-            name: 'totalAmount',
-            type: 'uint256',
-          },
-          {
-            internalType: 'uint256',
-            name: 'expiryTimestamp',
-            type: 'uint256',
-          },
-        ],
-        internalType: 'struct AffiliateProgram.ClaimRequest[]',
-        name: '_request',
-        type: 'tuple[]',
-      },
-      {
-        internalType: 'bytes[]',
-        name: '_signature',
-        type: 'bytes[]',
-      },
-    ],
-    name: 'claimMulti',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
     inputs: [],
     name: 'dailyClaimLimit',
-    outputs: [
-      {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256',
-      },
-    ],
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
     stateMutability: 'view',
     type: 'function',
   },
   {
     inputs: [],
     name: 'dailyClaimedAmount',
-    outputs: [
-      {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256',
-      },
-    ],
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
     stateMutability: 'view',
     type: 'function',
   },
+  { inputs: [], name: 'forceResetDailyLimit', outputs: [], stateMutability: 'nonpayable', type: 'function' },
   {
-    inputs: [],
-    name: 'forceResetDailyLimit',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: '_user',
-        type: 'address',
-      },
-    ],
+    inputs: [{ internalType: 'address', name: '_user', type: 'address' }],
     name: 'getAffiliateInfo',
     outputs: [
       {
         components: [
-          {
-            internalType: 'uint256',
-            name: 'nonce',
-            type: 'uint256',
-          },
-          {
-            internalType: 'uint256',
-            name: 'totalClaimedAmount',
-            type: 'uint256',
-          },
+          { internalType: 'uint256', name: 'nonce', type: 'uint256' },
+          { internalType: 'uint256', name: 'totalClaimedAmount', type: 'uint256' },
         ],
         internalType: 'struct AffiliateProgram.UserInfo',
         name: '',
@@ -333,27 +130,13 @@ export const affiliateProgramABI = [
     type: 'function',
   },
   {
-    inputs: [
-      {
-        internalType: 'address',
-        name: '_user',
-        type: 'address',
-      },
-    ],
+    inputs: [{ internalType: 'address', name: '_user', type: 'address' }],
     name: 'getUserInfo',
     outputs: [
       {
         components: [
-          {
-            internalType: 'uint256',
-            name: 'nonce',
-            type: 'uint256',
-          },
-          {
-            internalType: 'uint256',
-            name: 'totalClaimedAmount',
-            type: 'uint256',
-          },
+          { internalType: 'uint256', name: 'nonce', type: 'uint256' },
+          { internalType: 'uint256', name: 'totalClaimedAmount', type: 'uint256' },
         ],
         internalType: 'struct AffiliateProgram.UserInfo',
         name: '',
@@ -365,16 +148,8 @@ export const affiliateProgramABI = [
   },
   {
     inputs: [
-      {
-        internalType: 'address',
-        name: '_user',
-        type: 'address',
-      },
-      {
-        internalType: 'enum AffiliateProgram.ClaimType',
-        name: '_claimType',
-        type: 'uint8',
-      },
+      { internalType: 'address', name: '_user', type: 'address' },
+      { internalType: 'enum AffiliateProgram.ClaimType', name: '_claimType', type: 'uint8' },
     ],
     name: 'incrementNonce',
     outputs: [],
@@ -384,90 +159,42 @@ export const affiliateProgramABI = [
   {
     inputs: [],
     name: 'lastLimitResetTimestamp',
-    outputs: [
-      {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256',
-      },
-    ],
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
     stateMutability: 'view',
     type: 'function',
   },
   {
     inputs: [],
     name: 'owner',
-    outputs: [
-      {
-        internalType: 'address',
-        name: '',
-        type: 'address',
-      },
-    ],
+    outputs: [{ internalType: 'address', name: '', type: 'address' }],
     stateMutability: 'view',
     type: 'function',
   },
-  {
-    inputs: [],
-    name: 'pauseClaim',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
+  { inputs: [], name: 'pauseClaim', outputs: [], stateMutability: 'nonpayable', type: 'function' },
   {
     inputs: [],
     name: 'paused',
-    outputs: [
-      {
-        internalType: 'bool',
-        name: '',
-        type: 'bool',
-      },
-    ],
+    outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
     stateMutability: 'view',
     type: 'function',
   },
+  { inputs: [], name: 'renounceOwnership', outputs: [], stateMutability: 'nonpayable', type: 'function' },
   {
-    inputs: [],
-    name: 'renounceOwnership',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'uint256',
-        name: '_dailyLimit',
-        type: 'uint256',
-      },
-    ],
+    inputs: [{ internalType: 'uint256', name: '_dailyLimit', type: 'uint256' }],
     name: 'setDailyLimit',
     outputs: [],
     stateMutability: 'nonpayable',
     type: 'function',
   },
   {
-    inputs: [
-      {
-        internalType: 'address',
-        name: '_signer',
-        type: 'address',
-      },
-    ],
+    inputs: [{ internalType: 'address', name: '_signer', type: 'address' }],
     name: 'setSigner',
     outputs: [],
     stateMutability: 'nonpayable',
     type: 'function',
   },
   {
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'newOwner',
-        type: 'address',
-      },
-    ],
+    inputs: [{ internalType: 'address', name: 'newOwner', type: 'address' }],
     name: 'transferOwnership',
     outputs: [],
     stateMutability: 'nonpayable',
@@ -477,61 +204,25 @@ export const affiliateProgramABI = [
     inputs: [
       {
         components: [
-          {
-            internalType: 'uint256',
-            name: 'nonce',
-            type: 'uint256',
-          },
-          {
-            internalType: 'address',
-            name: 'claimer',
-            type: 'address',
-          },
-          {
-            internalType: 'enum AffiliateProgram.ClaimType',
-            name: 'claimType',
-            type: 'uint8',
-          },
-          {
-            internalType: 'uint256',
-            name: 'totalAmount',
-            type: 'uint256',
-          },
-          {
-            internalType: 'uint256',
-            name: 'expiryTimestamp',
-            type: 'uint256',
-          },
+          { internalType: 'uint256', name: 'nonce', type: 'uint256' },
+          { internalType: 'address', name: 'claimer', type: 'address' },
+          { internalType: 'enum AffiliateProgram.ClaimType', name: 'claimType', type: 'uint8' },
+          { internalType: 'uint256', name: 'totalAmount', type: 'uint256' },
+          { internalType: 'uint256', name: 'expiryTimestamp', type: 'uint256' },
         ],
         internalType: 'struct AffiliateProgram.ClaimRequest',
         name: '_request',
         type: 'tuple',
       },
-      {
-        internalType: 'bytes',
-        name: '_signature',
-        type: 'bytes',
-      },
+      { internalType: 'bytes', name: '_signature', type: 'bytes' },
     ],
     name: 'verifySignature',
-    outputs: [
-      {
-        internalType: 'bool',
-        name: '',
-        type: 'bool',
-      },
-    ],
+    outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
     stateMutability: 'view',
     type: 'function',
   },
   {
-    inputs: [
-      {
-        internalType: 'address',
-        name: '_token',
-        type: 'address',
-      },
-    ],
+    inputs: [{ internalType: 'address', name: '_token', type: 'address' }],
     name: 'withdraw',
     outputs: [],
     stateMutability: 'nonpayable',
--- apps/web/src/hooks/useAccountEventListener.ts
+++ apps/web/src/hooks/useAccountEventListener.ts
@@ -1,4 +1,5 @@
 import { useCallback, useEffect } from 'react'
+import { Address } from 'viem'
 import { useAccount, useAccountEffect } from 'wagmi'
 import { useAppDispatch } from '../state'
 import { clearUserStates } from '../utils/clearUserStates'
@@ -24,10 +25,33 @@ export const useChainIdListener = () => {
   })
 }
 
+const useAddressListener = () => {
+  const { connector, chainId } = useAccount()
+  const dispatch = useAppDispatch()
+
+  const onAddressChanged = useCallback(
+    ({ accounts }: { accounts?: readonly Address[] | undefined }) => {
+      if (accounts && accounts?.length > 0) {
+        clearUserStates(dispatch, { chainId })
+      }
+    },
+    [chainId, dispatch],
+  )
+
+  useEffect(() => {
+    connector?.emitter?.on('change', onAddressChanged)
+
+    return () => {
+      connector?.emitter?.off('change', onAddressChanged)
+    }
+  })
+}
+
 export const useAccountEventListener = () => {
   const dispatch = useAppDispatch()
   const { chainId } = useAccount()
   useChainIdListener()
+  useAddressListener()
 
   useAccountEffect({
     onDisconnect() {
