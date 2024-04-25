12225f10195c30a11c5278bcf8bd9ebe31563c35
chefmomota <momota@pancakeswap.com>
2024-04-25 16:43:18 +0800

    style: Update StyledButtonMenu

--- apps/gamification/views/DashboardQuests/index.tsx
+++ apps/gamification/views/DashboardQuests/index.tsx
@@ -30,6 +30,25 @@ const IconButton = styled(Button)`
   }
 `
 
+const StyledButtonMenu = styled(ButtonMenu)`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+
+  > button {
+    width: calc(50% - 4px);
+  }
+
+  ${({ theme }) => theme.mediaQueries.sm} {
+    width: fit-content;
+
+    > button {
+      width: fit-content;
+    }
+  }
+`
+
 export const DashboardQuests = () => {
   const { t } = useTranslation()
   const { isTablet } = useMatchBreakpoints()
@@ -43,7 +62,7 @@ export const DashboardQuests = () => {
   return (
     <Flex flexDirection="column">
       <Container>
-        <Flex flexDirection={['column', 'column', 'column', 'column', 'row']} maxWidth={['1200px']} margin="auto">
+        <Flex maxWidth={['1200px']} margin="auto" flexDirection={['column', 'column', 'column', 'column', 'row']}>
           <Flex>
             <Text fontSize={['36px']} bold>
               {t('Guests')}
@@ -51,10 +70,10 @@ export const DashboardQuests = () => {
             <IconButton scale="sm" endIcon={<AddIcon color="white" />} />
           </Flex>
           <Flex width="100%" alignItems={['flex-start', 'flex-start', 'flex-start', 'flex-start', 'center']}>
-            <ButtonMenu
+            <StyledButtonMenu
               scale="sm"
-              m={['0', '0', '0 auto 0 0', '0 auto 0 0', 'auto']}
               variant="subtle"
+              m={['0', '0', '0 auto 0 0', '0 auto 0 0', 'auto']}
               activeIndex={statusButtonIndex}
               onItemClick={onStatusButtonChange}
             >
@@ -62,7 +81,7 @@ export const DashboardQuests = () => {
               <ButtonMenuItem>{t('Scheduled')}</ButtonMenuItem>
               <ButtonMenuItem>{t('Finished')}</ButtonMenuItem>
               <ButtonMenuItem>{t('Drafted')}</ButtonMenuItem>
-            </ButtonMenu>
+            </StyledButtonMenu>
             <Button display={['none', 'none', 'flex']} scale="sm" endIcon={<AddIcon color="white" />}>
               {isTablet ? t('Create') : t('Create a quest')}
             </Button>
