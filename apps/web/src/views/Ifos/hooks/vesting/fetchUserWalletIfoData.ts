f0726ab842188c772f8d3278bf70b2c1b94e364f
chefjackson <116779127+chefjackson@users.noreply.github.com>
2024-04-26 13:34:35 +0800

    fix: unable to see vesting period

--- apps/web/src/views/Ifos/hooks/vesting/fetchUserWalletIfoData.ts
+++ apps/web/src/views/Ifos/hooks/vesting/fetchUserWalletIfoData.ts
@@ -1,4 +1,10 @@
-import { Ifo, UserVestingData, VestingCharacteristics, fetchUserVestingData } from '@pancakeswap/ifos'
+import {
+  Ifo,
+  UserVestingData,
+  VestingCharacteristics,
+  fetchUserVestingData,
+  fetchUserVestingDataV8,
+} from '@pancakeswap/ifos'
 import { Address } from 'viem'
 
 import { getViemClients } from 'utils/viem'
@@ -11,8 +17,9 @@ export interface VestingData {
 }
 
 export const fetchUserWalletIfoData = async (ifo: Ifo, account?: Address): Promise<VestingData> => {
-  const { address, chainId } = ifo
-  const userVestingData = await fetchUserVestingData({
+  const { address, chainId, version } = ifo
+  const fetchUserData = version >= 8 ? fetchUserVestingDataV8 : fetchUserVestingData
+  const userVestingData = await fetchUserData({
     ifoAddress: address,
     chainId,
     account,
