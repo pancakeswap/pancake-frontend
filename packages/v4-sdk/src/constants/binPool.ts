19709649b5475d7e58a86e2869bc9eaba1e3571c
Chef Jerry <jerry@pancakeswap.com>
2024-04-29 15:07:19 +0800

    chore: sort import

--- packages/v4-sdk/src/constants/binPool.ts
+++ packages/v4-sdk/src/constants/binPool.ts
@@ -1,5 +1,6 @@
 export const SCALE_OFFSET = 128n
 
+// eslint-disable-next-line no-bitwise
 export const SCALE = 1n << SCALE_OFFSET
 
 export const PRECISION = BigInt(1e18)
@@ -8,4 +9,5 @@ export const SQUARED_PRECISION = BigInt(1e36)
 
 export const BASIS_POINT_MAX = 10000n
 
+// eslint-disable-next-line no-bitwise
 export const REAL_ID_SHIFT = 1n << 23n
