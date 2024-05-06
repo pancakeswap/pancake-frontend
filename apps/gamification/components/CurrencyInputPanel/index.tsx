7d0f28c7ab67ec94856ec2db3297bac12f1a92de
chefmomota <momota@pancakeswap.com>
2024-05-06 12:40:55 +0800

    feat: Add CurrencyInputPanel

---
+++ apps/gamification/components/CurrencyInputPanel/index.tsx
@@ -0,0 +1,244 @@
+import { useTranslation } from '@pancakeswap/localization'
+import { Currency, Token } from '@pancakeswap/sdk'
+import { ArrowDropDownIcon, Button, Flex, Skeleton, Text } from '@pancakeswap/uikit'
+import { getFullDisplayBalance } from '@pancakeswap/utils/formatBalance'
+import { CurrencyLogo, Swap as SwapUI } from '@pancakeswap/widgets-internal'
+import { BigNumber } from 'bignumber.js'
+import { memo, useCallback, useMemo } from 'react'
+import { styled } from 'styled-components'
+
+import useTokenBalance from 'hooks/useTokenBalance'
+import { useAccount } from 'wagmi'
+// import CurrencySearchModal from '../SearchModal/CurrencySearchModal'
+
+const InputRow = styled.div<{ selected: boolean }>`
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  justify-content: flex-end;
+  padding: ${({ selected }) => (selected ? '0.75rem 0.5rem 0.75rem 1rem' : '0.75rem 0.75rem 0.75rem 1rem')};
+`
+const CurrencySelectButton = styled(Button).attrs({ variant: 'text', scale: 'sm' })`
+  padding: 0px;
+`
+
+interface CurrencyInputPanelProps {
+  value: string | undefined
+  onUserInput: (value: string) => void
+  onInputBlur?: () => void
+  onPercentInput?: (percent: number) => void
+  onMax?: () => void
+  showQuickInputButton?: boolean
+  showMaxButton: boolean
+  maxAmount?: BigNumber
+  lpPercent?: string
+  label?: string
+  onCurrencySelect?: (currency: Currency) => void
+  currency?: Currency | null
+  disableCurrencySelect?: boolean
+  hideBalance?: boolean
+  otherCurrency?: Currency | null
+  id: string
+  showCommonBases?: boolean
+  commonBasesType?: string
+  showSearchInput?: boolean
+  beforeButton?: React.ReactNode
+  disabled?: boolean
+  error?: boolean | string
+  tokensToShow?: Token[]
+  currencyLoading?: boolean
+  inputLoading?: boolean
+  title?: React.ReactNode
+  hideBalanceComp?: boolean
+}
+const CurrencyInputPanel = memo(function CurrencyInputPanel({
+  value,
+  onUserInput,
+  onInputBlur,
+  onPercentInput,
+  onMax,
+  showQuickInputButton = false,
+  showMaxButton,
+  maxAmount,
+  lpPercent,
+  label,
+  onCurrencySelect,
+  currency,
+  disableCurrencySelect = false,
+  hideBalance = false,
+  beforeButton,
+  otherCurrency,
+  id,
+  showCommonBases,
+  commonBasesType,
+  showSearchInput,
+  disabled,
+  error,
+  tokensToShow,
+  currencyLoading,
+  inputLoading,
+  title,
+  hideBalanceComp,
+}: CurrencyInputPanelProps) {
+  const { address: account } = useAccount()
+  const { balance: selectedCurrencyBalance } = useTokenBalance(currency?.address)
+
+  const { t } = useTranslation()
+
+  const mode = id
+
+  // const [onPresentCurrencyModal] = useModal(
+  //   <CurrencySearchModal
+  //     onCurrencySelect={onCurrencySelect}
+  //     selectedCurrency={currency}
+  //     otherSelectedCurrency={otherCurrency}
+  //     showCommonBases={showCommonBases}
+  //     commonBasesType={commonBasesType}
+  //     showSearchInput={showSearchInput}
+  //     tokensToShow={tokensToShow}
+  //     mode={mode}
+  //   />,
+  // )
+
+  const percentAmount: any = useMemo(
+    () => ({
+      25: maxAmount ? maxAmount.multipliedBy(new BigNumber(25).div(100)).toString() : undefined,
+      50: maxAmount ? maxAmount.multipliedBy(new BigNumber(50).div(100)).toString() : undefined,
+      75: maxAmount ? maxAmount.multipliedBy(new BigNumber(75).div(100)).toString() : undefined,
+    }),
+    [maxAmount],
+  )
+
+  const handleUserInput = useCallback(
+    (val: string) => {
+      onUserInput(val)
+    },
+    [onUserInput],
+  )
+
+  const onCurrencySelectClick = useCallback(() => {
+    if (!disableCurrencySelect) {
+      // onPresentCurrencyModal()
+      console.log('Clicked me')
+    }
+  }, [disableCurrencySelect])
+
+  const isAtPercentMax = (maxAmount && value === maxAmount.toString()) || (lpPercent && lpPercent === '100')
+
+  const balance =
+    !hideBalance && !!currency ? getFullDisplayBalance(selectedCurrencyBalance, currency.decimals, 6) : undefined
+
+  return (
+    <SwapUI.CurrencyInputPanel
+      id={id}
+      disabled={disabled}
+      error={error as boolean}
+      value={value}
+      onInputBlur={onInputBlur}
+      onUserInput={handleUserInput}
+      loading={inputLoading}
+      top={
+        <>
+          {title}
+          <Flex alignItems="center">
+            {beforeButton}
+            <CurrencySelectButton
+              className="open-currency-select-button"
+              data-dd-action-name="Select currency"
+              selected={!!currency}
+              onClick={onCurrencySelectClick}
+            >
+              <Flex alignItems="center" justifyContent="space-between">
+                {currency ? (
+                  <CurrencyLogo currency={currency} size="24px" style={{ marginRight: '8px' }} />
+                ) : currencyLoading ? (
+                  <Skeleton width="24px" height="24px" variant="circle" />
+                ) : null}
+                {currencyLoading ? null : (
+                  <Text id="pair" bold>
+                    {(currency && currency.symbol && currency.symbol.length > 10
+                      ? `${currency.symbol.slice(0, 4)}...${currency.symbol.slice(
+                          currency.symbol.length - 5,
+                          currency.symbol.length,
+                        )}`
+                      : currency?.symbol) || t('Select a currency')}
+                  </Text>
+                )}
+                {!currencyLoading && !disableCurrencySelect && <ArrowDropDownIcon />}
+              </Flex>
+            </CurrencySelectButton>
+          </Flex>
+
+          {account && !hideBalanceComp && (
+            <Text
+              data-dd-action-name="Token balance"
+              onClick={!disabled ? onMax : undefined}
+              color="textSubtle"
+              fontSize="12px"
+              ellipsis
+              title={!hideBalance && !!currency ? t('Balance: %balance%', { balance: balance ?? t('Loading') }) : ' -'}
+              style={{ display: 'inline', cursor: 'pointer' }}
+            >
+              {!hideBalance && !!currency
+                ? (balance?.replace('.', '')?.length || 0) > 12
+                  ? balance
+                  : t('Balance: %balance%', { balance: balance ?? t('Loading') })
+                : ' -'}
+            </Text>
+          )}
+        </>
+      }
+      bottom={
+        <>
+          <InputRow selected={disableCurrencySelect}>
+            {account && currency && selectedCurrencyBalance?.gt(0) && !disabled && label !== 'To' && (
+              <Flex alignItems="right" justifyContent="right">
+                {maxAmount?.gt(0) &&
+                  showQuickInputButton &&
+                  onPercentInput &&
+                  [25, 50, 75].map((percent) => {
+                    const isAtCurrentPercent =
+                      (maxAmount && value !== '0' && value === percentAmount[percent]) ||
+                      (lpPercent && lpPercent === percent.toString())
+
+                    return (
+                      <Button
+                        key={`btn_quickCurrency${percent}`}
+                        data-dd-action-name={`Balance percent ${percent}`}
+                        onClick={() => {
+                          onPercentInput(percent)
+                        }}
+                        scale="xs"
+                        mr="5px"
+                        variant={isAtCurrentPercent ? 'primary' : 'secondary'}
+                        style={{ textTransform: 'uppercase' }}
+                      >
+                        {percent}%
+                      </Button>
+                    )
+                  })}
+                {maxAmount?.gt(0) && showMaxButton && (
+                  <Button
+                    data-dd-action-name="Balance percent max"
+                    onClick={(e) => {
+                      e.stopPropagation()
+                      e.preventDefault()
+                      onMax?.()
+                    }}
+                    scale="xs"
+                    variant={isAtPercentMax ? 'primary' : 'secondary'}
+                    style={{ textTransform: 'uppercase' }}
+                  >
+                    {t('Max')}
+                  </Button>
+                )}
+              </Flex>
+            )}
+          </InputRow>
+        </>
+      }
+    />
+  )
+})
+
+export default CurrencyInputPanel
--- apps/gamification/views/DashboardQuestEdit/components/Reward/AddRewardModal.tsx
+++ apps/gamification/views/DashboardQuestEdit/components/Reward/AddRewardModal.tsx
@@ -1,11 +1,39 @@
 import { useTranslation } from '@pancakeswap/localization'
-import { Button, Flex, InjectedModalProps, Modal } from '@pancakeswap/uikit'
-// import { TokenImage } from 'components/TokenImage'
+import { Currency } from '@pancakeswap/sdk'
+import { CAKE } from '@pancakeswap/tokens'
+import { Box, Button, Flex, InjectedModalProps, Modal } from '@pancakeswap/uikit'
+import { getFullDisplayBalance } from '@pancakeswap/utils/formatBalance'
+import { BigNumber } from 'bignumber.js'
+import CurrencyInputPanel from 'components/CurrencyInputPanel'
+import useActiveWeb3React from 'hooks/useActiveWeb3React'
+import useTokenBalance from 'hooks/useTokenBalance'
+import { useCallback, useState } from 'react'
 
 interface AddRewardModalProps extends InjectedModalProps {}
 
 export const AddRewardModal: React.FC<React.PropsWithChildren<AddRewardModalProps>> = ({ onDismiss }) => {
   const { t } = useTranslation()
+  const { account, chainId } = useActiveWeb3React()
+  const defaultInputCurrency = (CAKE as any)?.[chainId]
+  const [inputCurrency, setInputCurrency] = useState<Currency>(defaultInputCurrency)
+  const [stakeAmount, setStakeAmount] = useState('')
+  const { balance: currencyBalance } = useTokenBalance(inputCurrency?.address)
+
+  const handleMaxInput = useCallback(() => {
+    const newBalanceAmount = getFullDisplayBalance(currencyBalance, inputCurrency.decimals).toString()
+    setStakeAmount(newBalanceAmount)
+  }, [currencyBalance, inputCurrency])
+
+  const handlePercentButton = useCallback(
+    (percent: number) => {
+      const amount = currencyBalance.multipliedBy(new BigNumber(percent).div(100))
+      const newBalanceAmount = getFullDisplayBalance(amount, inputCurrency.decimals).toString()
+      setStakeAmount(newBalanceAmount)
+    },
+    [currencyBalance, inputCurrency.decimals],
+  )
+
+  const handleInputSelect = useCallback((newCurrency: Currency) => setInputCurrency(newCurrency), [])
 
   return (
     <Modal title={t('Add a reward')} onDismiss={onDismiss}>
@@ -15,8 +43,20 @@ export const AddRewardModal: React.FC<React.PropsWithChildren<AddRewardModalProp
         justifyContent="center"
         width={['100%', '100%', '100%', '430px']}
       >
-        {/* <TokenImage token={token} width={64} height={64} /> */}
-
+        <Box width="100%">
+          <CurrencyInputPanel
+            id="currency-input"
+            showMaxButton
+            showQuickInputButton
+            value={stakeAmount}
+            currency={inputCurrency}
+            maxAmount={currencyBalance}
+            onMax={handleMaxInput}
+            onUserInput={setStakeAmount}
+            onPercentInput={handlePercentButton}
+            onCurrencySelect={handleInputSelect}
+          />
+        </Box>
         <Button width="100%" mt="24px">
           {t('Continue')}
         </Button>
