15dc73bab9d99cb2346178abbea0cded8adb18b7
memoyil <2213635+memoyil@users.noreply.github.com>
2024-05-07 09:53:08 +0200

    chore: Remove transferFrom from nft position

--- packages/v3-sdk/src/abi/NonfungiblePositionManager.ts
+++ packages/v3-sdk/src/abi/NonfungiblePositionManager.ts
@@ -482,17 +482,6 @@ export const nonfungiblePositionManagerABI = [
   },
   {
     inputs: [
-      { internalType: 'address', name: 'from', type: 'address' },
-      { internalType: 'address', name: 'to', type: 'address' },
-      { internalType: 'uint256', name: 'tokenId', type: 'uint256' },
-    ],
-    name: 'transferFrom',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    inputs: [
       { internalType: 'uint256', name: 'amountMinimum', type: 'uint256' },
       { internalType: 'address', name: 'recipient', type: 'address' },
     ],
