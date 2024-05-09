16dde1e482fba1eafae8b6451fb4a9da3077efe0
chefmomota <momota@pancakeswap.com>
2024-05-09 15:46:47 +0800

    feat: List SolvBtcb-BTCB farm

--- packages/farms/constants/bsc.ts
+++ packages/farms/constants/bsc.ts
@@ -76,6 +76,13 @@ export const farmsV3 = defineFarmV3Configs([
   // new lps should follow after the top fixed lps
   // latest first
   {
+    pid: 164,
+    token0: bscTokens.solvbtc,
+    token1: bscTokens.btcb,
+    lpAddress: '0x4aae823a6a0b376De6A78e74eCC5b079d38cBCf7',
+    feeAmount: FeeAmount.LOW,
+  },
+  {
     pid: 163,
     token0: bscTokens.weEth,
     token1: bscTokens.eth,
