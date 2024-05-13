05b97eca2fc5dbd457dacfb6a9c4288417303f7a
chefmomota <momota@pancakeswap.com>
2024-05-13 16:20:38 +0800

    feat: Update SocialTask

--- apps/gamification/views/DashboardQuestEdit/components/Tasks/SocialTask.tsx
+++ apps/gamification/views/DashboardQuestEdit/components/Tasks/SocialTask.tsx
@@ -2,8 +2,8 @@ import { useTranslation } from '@pancakeswap/localization'
 import {
   Box,
   DeleteOutlineIcon,
+  ErrorFillIcon,
   Flex,
-  Input,
   InputGroup,
   OpenNewIcon,
   Text,
@@ -11,17 +11,14 @@ import {
   useModal,
   useTooltip,
 } from '@pancakeswap/uikit'
-import { useState } from 'react'
-import { styled } from 'styled-components'
+import { useMemo } from 'react'
+import { InputErrorText, StyledInput } from 'views/DashboardQuestEdit/components/InputStyle'
 import { ConfirmDeleteModal } from 'views/DashboardQuestEdit/components/Tasks/ConfirmDeleteModal'
 import { TaskSocialConfig } from 'views/DashboardQuestEdit/context/types'
 import { useQuestEdit } from 'views/DashboardQuestEdit/context/useQuestEdit'
 import { useTaskInfo } from 'views/DashboardQuestEdit/hooks/useTaskInfo'
 import { TaskType } from 'views/DashboardQuestEdit/type'
-
-const StyledInput = styled(Input)`
-  height: 32px;
-`
+import { validateUrl } from 'views/DashboardQuestEdit/utils/validateTask'
 
 interface SocialTaskProps {
   task: TaskSocialConfig
@@ -30,7 +27,6 @@ interface SocialTaskProps {
 export const SocialTask: React.FC<SocialTaskProps> = ({ task }) => {
   const { t } = useTranslation()
   const { isMobile } = useMatchBreakpoints()
-  const [urlLink, setUrlLink] = useState('https://google.com')
   const { tasks, onTasksChange, deleteTask } = useQuestEdit()
 
   const [onPresentDeleteModal] = useModal(<ConfirmDeleteModal handleDelete={() => deleteTask(task.sid)} />)
@@ -43,56 +39,74 @@ export const SocialTask: React.FC<SocialTaskProps> = ({ task }) => {
   })
 
   const onclickOpenNewIcon = () => {
-    window.open(urlLink, '_blank', 'noopener noreferrer')
+    window.open(task.socialLink, '_blank', 'noopener noreferrer')
   }
 
+  const handleUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const forkTasks = Object.assign(tasks)
+    const indexToUpdate = forkTasks.findIndex((i: TaskSocialConfig) => i.sid === task.sid)
+    forkTasks[indexToUpdate].socialLink = e.target.value
+
+    onTasksChange([...forkTasks])
+  }
+
+  const isUrlError = useMemo(() => validateUrl(task.socialLink), [task?.socialLink])
+
   return (
-    <Flex flexDirection={['column', 'column', 'row']}>
-      <Flex>
-        <Flex mr="8px" alignSelf="center">
-          {taskIcon(social)}
+    <Flex flexDirection="column">
+      <Flex flexDirection={['column', 'column', 'row']}>
+        <Flex>
+          <Flex mr="8px" alignSelf="center">
+            {taskIcon(social)}
+          </Flex>
+          <Text style={{ alignSelf: 'center' }} bold>
+            {taskNaming(social)}
+          </Text>
+          {isMobile && (
+            <DeleteOutlineIcon
+              ml="auto"
+              width="20px"
+              height="20px"
+              color="primary"
+              style={{ cursor: 'pointer' }}
+              onClick={onPresentDeleteModal}
+            />
+          )}
         </Flex>
-        <Text style={{ alignSelf: 'center' }} bold>
-          {taskNaming(social)}
-        </Text>
-        {isMobile && (
-          <DeleteOutlineIcon
-            ml="auto"
-            width="20px"
-            height="20px"
-            color="primary"
-            style={{ cursor: 'pointer' }}
-            onClick={onPresentDeleteModal}
-          />
-        )}
+        <Flex width={['100%', '100%', 'fit-content']} m={['8px 0 0 0', '8px 0 0 0', '0 0 0 auto']} alignSelf="center">
+          <InputGroup
+            endIcon={
+              isUrlError ? (
+                <ErrorFillIcon color="failure" width={16} height={16} />
+              ) : (
+                <Box ref={targetRef} onClick={onclickOpenNewIcon}>
+                  <OpenNewIcon style={{ cursor: 'pointer' }} color="primary" width="20px" />
+                  {tooltipVisible && tooltip}
+                </Box>
+              )
+            }
+          >
+            <StyledInput
+              value={task.socialLink}
+              isError={isUrlError}
+              style={{ borderRadius: '24px' }}
+              placeholder={taskInputPlaceholder(social)}
+              onChange={handleUrlChange}
+            />
+          </InputGroup>
+          {!isMobile && (
+            <DeleteOutlineIcon
+              ml="8px"
+              width="20px"
+              height="20px"
+              color="primary"
+              style={{ cursor: 'pointer' }}
+              onClick={onPresentDeleteModal}
+            />
+          )}
+        </Flex>
       </Flex>
-      <Flex width={['100%', '100%', 'fit-content']} m={['8px 0 0 0', '8px 0 0 0', '0 0 0 auto']} alignSelf="center">
-        <InputGroup
-          endIcon={
-            <Box ref={targetRef} onClick={onclickOpenNewIcon}>
-              <OpenNewIcon style={{ cursor: 'pointer' }} color="primary" width="20px" />
-              {tooltipVisible && tooltip}
-            </Box>
-          }
-        >
-          <StyledInput
-            value={urlLink}
-            style={{ borderRadius: '24px' }}
-            placeholder={taskInputPlaceholder(social)}
-            onChange={(e) => setUrlLink(e.target.value)}
-          />
-        </InputGroup>
-        {!isMobile && (
-          <DeleteOutlineIcon
-            ml="8px"
-            width="20px"
-            height="20px"
-            color="primary"
-            style={{ cursor: 'pointer' }}
-            onClick={onPresentDeleteModal}
-          />
-        )}
-      </Flex>
+      {isUrlError && <InputErrorText errorText={t('Enter a valid website URL')} />}
     </Flex>
   )
 }
--- apps/gamification/views/DashboardQuestEdit/utils/validateTask.ts
+++ apps/gamification/views/DashboardQuestEdit/utils/validateTask.ts
@@ -8,3 +8,10 @@ export const validateLpAddress = (lpAddress: string) => {
   const regex = /^(0x[a-fA-F0-9]{40})$/
   return !regex.test(lpAddress)
 }
+
+export const validateUrl = (url: string) => {
+  const pattern =
+    /(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?\/[a-zA-Z0-9]{2,}|((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?)|(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})?/g
+
+  return !pattern.test(url)
+}
