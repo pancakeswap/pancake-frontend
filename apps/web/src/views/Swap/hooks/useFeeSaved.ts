0c27b4cad12a7e8ff54b8bd5db1984de5aaaf00a
Chef Jerry <144641937+ChefJerry@users.noreply.github.com>
2024-05-14 12:50:34 +0800

    fix: remove undefined key of stablecoin (#9805)
    <!--
    Before opening a pull request, please read the [contributing
    guidelines](https://github.com/pancakeswap/pancake-frontend/blob/develop/CONTRIBUTING.md)
    first
    -->
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    This PR updates the `MAJOR_DEFINED_STABLE_COINS` object structure and
    refactors the `useFeeSaved` hook to handle stable coins more
    efficiently.
    
    ### Detailed summary
    - Restructured `MAJOR_DEFINED_STABLE_COINS` object with a chain-specific
    array
    - Updated `useFeeSaved` to handle stable coins based on the chain ID
    - Improved handling of zero fee scenarios
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your
    question}`
    
    <!-- end pr-codex -->

--- apps/web/src/views/Swap/hooks/useFeeSaved.ts
+++ apps/web/src/views/Swap/hooks/useFeeSaved.ts
@@ -1,27 +1,18 @@
-import { ChainId, Currency, CurrencyAmount } from '@pancakeswap/sdk'
+import { ChainId, Currency, CurrencyAmount, ERC20Token } from '@pancakeswap/sdk'
 import { DAI, USDC, USDT } from '@pancakeswap/tokens'
 import { parseNumberToFraction } from '@pancakeswap/utils/formatFractions'
 import { BigNumber } from 'bignumber.js'
 import { useCurrencyUsdPrice } from 'hooks/useCurrencyUsdPrice'
 import { useMemo } from 'react'
 
-const MAJOR_DEFINED_STABLE_COINS = {
+const MAJOR_DEFINED_STABLE_COINS: {
+  [chainId in ChainId]?: Array<ERC20Token | undefined>
+} = {
   [ChainId.ETHEREUM]: [USDC[ChainId.ETHEREUM], DAI[ChainId.ETHEREUM], USDT[ChainId.ETHEREUM]],
   [ChainId.ARBITRUM_ONE]: [USDC[ChainId.ARBITRUM_ONE], DAI[ChainId.ARBITRUM_ONE], USDT[ChainId.ARBITRUM_ONE]],
-  [ChainId.ARBITRUM_GOERLI]: [
-    USDC[ChainId.ARBITRUM_GOERLI],
-    DAI[ChainId.ARBITRUM_GOERLI],
-    USDT[ChainId.ARBITRUM_GOERLI],
-  ],
   [ChainId.POLYGON_ZKEVM]: [USDC[ChainId.POLYGON_ZKEVM], DAI[ChainId.POLYGON_ZKEVM], USDT[ChainId.POLYGON_ZKEVM]],
-  [ChainId.POLYGON_ZKEVM_TESTNET]: [
-    USDC[ChainId.POLYGON_ZKEVM_TESTNET],
-    DAI[ChainId.POLYGON_ZKEVM_TESTNET],
-    USDT[ChainId.POLYGON_ZKEVM_TESTNET],
-  ],
   [ChainId.BSC]: [USDC[ChainId.BSC], DAI[ChainId.BSC], USDT[ChainId.BSC]],
-  [ChainId.BASE]: [USDC[ChainId.BASE], DAI[ChainId.BASE], USDT[ChainId.BASE]],
-  [ChainId.SEPOLIA]: [USDC[ChainId.SEPOLIA], DAI[ChainId.SEPOLIA], USDT[ChainId.SEPOLIA]],
+  [ChainId.BASE]: [USDC[ChainId.BASE], DAI[ChainId.BASE]],
 }
 
 export const useFeeSaved = (inputAmount?: CurrencyAmount<Currency>, outputAmount?: CurrencyAmount<Currency>) => {
@@ -30,9 +21,11 @@ export const useFeeSaved = (inputAmount?: CurrencyAmount<Currency>, outputAmount
 
     const { chainId } = inputAmount.currency
 
-    const majorDefinedStableCoins = MAJOR_DEFINED_STABLE_COINS[chainId] ?? []
+    const majorDefinedStableCoins = MAJOR_DEFINED_STABLE_COINS[chainId as ChainId]
+    if (!majorDefinedStableCoins) return undefined
+
     const zeroFee = [inputAmount.currency, outputAmount.currency].every((currency) =>
-      majorDefinedStableCoins.some((coin) => coin.equals(currency)),
+      majorDefinedStableCoins.some((coin) => coin?.equals(currency)),
     )
 
     if (zeroFee) return undefined
