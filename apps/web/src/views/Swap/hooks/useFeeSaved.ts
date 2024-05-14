a8610f62d704df206eeee64f4eefcd3eab8d9251
Chef Jerry <jerry@pancakeswap.com>
2024-05-14 12:08:55 +0800

    fix: remove undefined key of stablecoin

--- apps/web/src/views/Swap/hooks/useFeeSaved.ts
+++ apps/web/src/views/Swap/hooks/useFeeSaved.ts
@@ -8,20 +8,9 @@ import { useMemo } from 'react'
 const MAJOR_DEFINED_STABLE_COINS = {
   [ChainId.ETHEREUM]: [USDC[ChainId.ETHEREUM], DAI[ChainId.ETHEREUM], USDT[ChainId.ETHEREUM]],
   [ChainId.ARBITRUM_ONE]: [USDC[ChainId.ARBITRUM_ONE], DAI[ChainId.ARBITRUM_ONE], USDT[ChainId.ARBITRUM_ONE]],
-  [ChainId.ARBITRUM_GOERLI]: [
-    USDC[ChainId.ARBITRUM_GOERLI],
-    DAI[ChainId.ARBITRUM_GOERLI],
-    USDT[ChainId.ARBITRUM_GOERLI],
-  ],
   [ChainId.POLYGON_ZKEVM]: [USDC[ChainId.POLYGON_ZKEVM], DAI[ChainId.POLYGON_ZKEVM], USDT[ChainId.POLYGON_ZKEVM]],
-  [ChainId.POLYGON_ZKEVM_TESTNET]: [
-    USDC[ChainId.POLYGON_ZKEVM_TESTNET],
-    DAI[ChainId.POLYGON_ZKEVM_TESTNET],
-    USDT[ChainId.POLYGON_ZKEVM_TESTNET],
-  ],
   [ChainId.BSC]: [USDC[ChainId.BSC], DAI[ChainId.BSC], USDT[ChainId.BSC]],
-  [ChainId.BASE]: [USDC[ChainId.BASE], DAI[ChainId.BASE], USDT[ChainId.BASE]],
-  [ChainId.SEPOLIA]: [USDC[ChainId.SEPOLIA], DAI[ChainId.SEPOLIA], USDT[ChainId.SEPOLIA]],
+  [ChainId.BASE]: [USDC[ChainId.BASE], DAI[ChainId.BASE]],
 }
 
 export const useFeeSaved = (inputAmount?: CurrencyAmount<Currency>, outputAmount?: CurrencyAmount<Currency>) => {
@@ -32,7 +21,7 @@ export const useFeeSaved = (inputAmount?: CurrencyAmount<Currency>, outputAmount
 
     const majorDefinedStableCoins = MAJOR_DEFINED_STABLE_COINS[chainId] ?? []
     const zeroFee = [inputAmount.currency, outputAmount.currency].every((currency) =>
-      majorDefinedStableCoins.some((coin) => coin.equals(currency)),
+      majorDefinedStableCoins.some((coin) => coin?.equals(currency)),
     )
 
     if (zeroFee) return undefined
