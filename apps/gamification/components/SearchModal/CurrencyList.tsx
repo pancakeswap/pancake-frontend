f365dbb4f0ce14436e15287ae7d35979c81c5bbc
chefmomota <momota@pancakeswap.com>
2024-05-15 12:27:42 +0800

    fix: Build

--- apps/gamification/components/SearchModal/CurrencyList.tsx
+++ apps/gamification/components/SearchModal/CurrencyList.tsx
@@ -39,7 +39,7 @@ export const CurrencyList: React.FC<CurrencyListProps> = ({
           currency?.chainId === selectedCurrency?.chainId &&
           currency.symbol === selectedCurrency?.symbol
 
-        let newToken = null
+        let newToken
         if (isNative) {
           newToken = NATIVE[currency.chainId as ChainId]
         } else {
