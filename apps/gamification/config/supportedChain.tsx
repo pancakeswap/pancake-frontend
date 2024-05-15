d1eb0aa56d8d78ff86e337a9f9d91763d4c8daad
chefmomota <momota@pancakeswap.com>
2024-05-15 12:53:40 +0800

    feat: Update

--- apps/gamification/config/supportedChain.tsx
+++ apps/gamification/config/supportedChain.tsx
@@ -1,12 +1,12 @@
-import { ChainId, Currency } from '@pancakeswap/sdk'
+import { ChainId } from '@pancakeswap/chains'
 import { arbitrumTokens, baseTokens, bscTokens, zksyncTokens } from '@pancakeswap/tokens'
 import { arbitrum, base, bsc, zkSync } from 'wagmi/chains'
 
 export const targetChains = [bsc, zkSync, arbitrum, base]
 
-export const TOKEN_LIST = {
+export const TOKEN_LIST: any = {
   [ChainId.BSC]: bscTokens,
   [ChainId.ZKSYNC]: zksyncTokens,
   [ChainId.ARBITRUM_ONE]: arbitrumTokens,
   [ChainId.BASE]: baseTokens,
-} as { [key: string]: Currency }
+}
