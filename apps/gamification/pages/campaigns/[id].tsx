4bf645d16a730a4189d1bdd8dd20c02a071aa3ca
chefmomota <momota@pancakeswap.com>
2024-05-15 14:08:42 +0800

    feat: Remove mobile claim component

--- apps/gamification/pages/campaigns/[id].tsx
+++ apps/gamification/pages/campaigns/[id].tsx
@@ -1,10 +1,7 @@
-import { MobileClaimButton } from 'views/Campaign/components/MobileClaimButton'
 import { Campaign } from 'views/Campaign/index'
 
 const CampaignsPage = () => {
   return <Campaign />
 }
-
-CampaignsPage.CustomComponent = <MobileClaimButton />
 
 export default CampaignsPage
--- apps/gamification/pages/quests/[id].tsx
+++ apps/gamification/pages/quests/[id].tsx
@@ -1,10 +1,7 @@
-import { MobileClaimButton } from 'views/Quest/components/MobileClaimButton'
 import { Quest } from 'views/Quest/index'
 
 const QuestPage = () => {
   return <Quest />
 }
-
-QuestPage.CustomComponent = <MobileClaimButton />
 
 export default QuestPage
