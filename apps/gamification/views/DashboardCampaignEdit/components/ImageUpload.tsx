d75f4f28c860e21c2c91cf169ee08660516e7604
chefmomota <momota@pancakeswap.com>
2024-05-16 10:57:20 +0800

    feat: Upload image add accept type and max size

--- apps/gamification/views/DashboardCampaignEdit/components/ImageUpload.tsx
+++ apps/gamification/views/DashboardCampaignEdit/components/ImageUpload.tsx
@@ -24,6 +24,9 @@ const EmptyContainer = styled(Flex)`
   }
 `
 
+const ACCEPT_TYPE = ['jpeg', 'jpg', 'png']
+const MAX_FILE_SIZE = 2000000 // 2000000 Bytes = 2 MB
+
 export const ImageUpload = () => {
   const { t } = useTranslation()
   const [images, setImages] = useState<ImageListType>([])
@@ -34,49 +37,71 @@ export const ImageUpload = () => {
 
   return (
     <Box>
-      <ImageUploading value={images} onChange={onChange} dataURLKey="data_url">
-        {({ imageList, onImageUpload, onImageUpdate }) => {
-          return imageList[0] ? (
-            <>
-              <Flex justifyContent="space-between" mb="8px">
-                <Text color="textSubtle" style={{ alignSelf: 'center' }}>
-                  {images?.[0]?.file?.name}
-                </Text>
-                <Button
-                  paddingRight={0}
-                  variant="text"
-                  endIcon={
-                    <ArrowFirstIcon style={{ transform: 'rotate(-90deg)' }} color="primary" width={20} height={20} />
-                  }
-                  onClick={() => onImageUpdate(0)}
-                >
-                  {t('Upload New')}
-                </Button>
-              </Flex>
-              <Box overflow="hidden" borderRadius="16px" height={['162px', '261px', '261px', '261px', '378px']}>
-                <StyledThumbnail
-                  className="thumbnail"
-                  style={{
-                    backgroundImage: `url(${images?.[0]?.data_url})`,
-                  }}
-                />
-              </Box>
-            </>
-          ) : (
-            <EmptyContainer>
-              <Button
-                variant="text"
-                endIcon={
-                  <ArrowFirstIcon style={{ transform: 'rotate(-90deg)' }} color="primary" width={20} height={20} />
-                }
-                onClick={onImageUpload}
-              >
-                {t('Upload New')}
-              </Button>
-              <Text style={{ alignSelf: 'center' }} color="textSubtle">
-                {t('JPEG or PNG smaller than 2mb')}
+      <ImageUploading
+        value={images}
+        onChange={onChange}
+        dataURLKey="data_url"
+        acceptType={ACCEPT_TYPE}
+        maxFileSize={MAX_FILE_SIZE}
+      >
+        {({ imageList, errors, onImageUpload, onImageUpdate }) => {
+          return (
+            <Flex flexDirection="column">
+              {imageList[0] ? (
+                <>
+                  <Box>
+                    <Flex justifyContent="space-between" mb="8px">
+                      <Text ellipsis color="textSubtle" style={{ alignSelf: 'center' }}>
+                        {images?.[0]?.file?.name}
+                      </Text>
+                      <Button
+                        minWidth="145px"
+                        paddingRight={0}
+                        variant="text"
+                        endIcon={
+                          <ArrowFirstIcon
+                            style={{ transform: 'rotate(-90deg)' }}
+                            color="primary"
+                            width={20}
+                            height={20}
+                          />
+                        }
+                        onClick={() => onImageUpdate(0)}
+                      >
+                        {t('Upload New')}
+                      </Button>
+                    </Flex>
+                    <Box overflow="hidden" borderRadius="16px" height={['162px', '261px', '261px', '261px', '378px']}>
+                      <StyledThumbnail
+                        className="thumbnail"
+                        style={{
+                          backgroundImage: `url(${images?.[0]?.data_url})`,
+                        }}
+                      />
+                    </Box>
+                  </Box>
+                </>
+              ) : (
+                <EmptyContainer>
+                  <Button
+                    variant="text"
+                    endIcon={
+                      <ArrowFirstIcon style={{ transform: 'rotate(-90deg)' }} color="primary" width={20} height={20} />
+                    }
+                    onClick={onImageUpload}
+                  >
+                    {t('Upload New')}
+                  </Button>
+                  <Text style={{ alignSelf: 'center' }} color="textSubtle">
+                    {t('JPEG or PNG smaller than 2mb')}
+                  </Text>
+                </EmptyContainer>
+              )}
+              <Text color="failure" mt="4px">
+                {errors?.maxFileSize && <span>{t('Maximum upload file size 2 MB')}</span>}
+                {errors?.acceptType && <span>{t('File type no supported. We accept JPG, JPEG, PNG.')}</span>}
               </Text>
-            </EmptyContainer>
+            </Flex>
           )
         }}
       </ImageUploading>
