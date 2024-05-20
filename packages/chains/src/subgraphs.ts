965d0580083abc3a4b7812bc614aec7ea0dcd3b5
chefjackson <116779127+chefjackson@users.noreply.github.com>
2024-05-20 16:47:50 +0800

    chore: migrate polygon zkevm v3 subgraph

--- packages/chains/src/subgraphs.ts
+++ packages/chains/src/subgraphs.ts
@@ -28,7 +28,7 @@ export function getV3Subgraphs({ noderealApiKey }: SubgraphParams) {
     [ChainId.BSC_TESTNET]: 'https://api.thegraph.com/subgraphs/name/pancakeswap/exchange-v3-chapel',
     [ChainId.ARBITRUM_ONE]: 'https://api.thegraph.com/subgraphs/name/pancakeswap/exchange-v3-arb',
     [ChainId.ARBITRUM_GOERLI]: 'https://api.thegraph.com/subgraphs/name/chef-jojo/exhange-v3-arb-goerli',
-    [ChainId.POLYGON_ZKEVM]: 'https://api.studio.thegraph.com/query/45376/exchange-v3-polygon-zkevm/version/latest',
+    [ChainId.POLYGON_ZKEVM]: 'https://thegraph.pancakeswap.com/exchange-v3-polygon-zkevm',
     [ChainId.POLYGON_ZKEVM_TESTNET]: null,
     [ChainId.ZKSYNC]: 'https://api.studio.thegraph.com/query/45376/exchange-v3-zksync/version/latest',
     [ChainId.ZKSYNC_TESTNET]: 'https://api.studio.thegraph.com/query/45376/exchange-v3-zksync-testnet/version/latest',
