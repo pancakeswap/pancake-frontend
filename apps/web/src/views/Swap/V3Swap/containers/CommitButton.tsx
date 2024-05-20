e26f98b97720c3b19083acc2393e37fd81602380
Chef Jerry <jerry@pancakeswap.com>
2024-05-20 18:16:05 +0800

    refactor: default universal-router

--- apps/web/src/views/Swap/V3Swap/containers/CommitButton.tsx
+++ apps/web/src/views/Swap/V3Swap/containers/CommitButton.tsx
@@ -1,9 +1,7 @@
 import { SmartRouterTrade, V4Router } from '@pancakeswap/smart-router'
 import { TradeType } from '@pancakeswap/swap-sdk-core'
 import { MMCommitTrade } from '../types'
-import { MMCommitButton } from './MMCommitButton'
 import { MMCommitButtonV2 } from './MMCommitButtonV2'
-import { SwapCommitButton } from './SwapCommitButton'
 import { SwapCommitButtonV2 } from './SwapCommitButtonV2'
 
 type Trade = SmartRouterTrade<TradeType> | V4Router.V4TradeWithoutGraph<TradeType>
@@ -14,11 +12,9 @@ export type CommitButtonProps = {
   tradeLoaded: boolean
   beforeCommit?: () => void
   afterCommit?: () => void
-  useUniversalRouter?: boolean
 }
 
 export const CommitButton: React.FC<CommitButtonProps> = ({
-  useUniversalRouter,
   trade,
   tradeError,
   tradeLoaded,
@@ -27,15 +23,11 @@ export const CommitButton: React.FC<CommitButtonProps> = ({
 }) => {
   if (trade && 'isMMBetter' in trade && trade?.isMMBetter) {
     const currentTrade = trade
-    return useUniversalRouter ? (
-      <MMCommitButtonV2 {...currentTrade} beforeCommit={beforeCommit} afterCommit={afterCommit} />
-    ) : (
-      <MMCommitButton {...currentTrade} />
-    )
+    return <MMCommitButtonV2 {...currentTrade} beforeCommit={beforeCommit} afterCommit={afterCommit} />
   }
 
   const currentTrade = trade as SmartRouterTrade<TradeType>
-  return useUniversalRouter ? (
+  return (
     <SwapCommitButtonV2
       trade={currentTrade}
       tradeError={tradeError}
@@ -43,7 +35,5 @@ export const CommitButton: React.FC<CommitButtonProps> = ({
       beforeCommit={beforeCommit}
       afterCommit={afterCommit}
     />
-  ) : (
-    <SwapCommitButton trade={currentTrade} tradeError={tradeError} tradeLoading={!tradeLoaded} />
   )
 }
