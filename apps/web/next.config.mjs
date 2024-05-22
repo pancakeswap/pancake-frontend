dd72ca16f09dd0c52764afde4df8a884592e58d3
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2024-05-22 15:47:27 +0800

    chore: next config

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,12 +1,12 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import BundleAnalyzer from '@next/bundle-analyzer'
 import { withWebSecurityHeaders } from '@pancakeswap/next-config/withWebSecurityHeaders'
-import smartRouterPkgs from '@pancakeswap/smart-router/package.json' with { type: 'json' }
+import smartRouterPkgs from '@pancakeswap/smart-router/package.json' assert { type: 'json' }
 import { withSentryConfig } from '@sentry/nextjs'
 import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin'
+import vercelToolbarPlugin from '@vercel/toolbar/plugins/next'
 import path from 'path'
 import { fileURLToPath } from 'url'
-import vercelToolbarPlugin from '@vercel/toolbar/plugins/next'
 
 const withVercelToolbar = vercelToolbarPlugin()
 
