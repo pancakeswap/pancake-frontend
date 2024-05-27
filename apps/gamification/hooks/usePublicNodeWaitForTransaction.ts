134d1e57d1d3562595335155d8539df5117eb71e
chefmomota <momota@pancakeswap.com>
2024-05-27 16:04:37 +0800

    feat: Done Profile Create

---
+++ apps/gamification/hooks/usePublicNodeWaitForTransaction.ts
@@ -0,0 +1,88 @@
+import { ChainId } from '@pancakeswap/chains'
+import { BSC_BLOCK_TIME } from 'config'
+import { CHAINS } from 'config/chains'
+import { AVERAGE_CHAIN_BLOCK_TIMES } from 'config/constants/averageChainBlockTimes'
+import first from 'lodash/first'
+import { useCallback } from 'react'
+import { RetryableError, retry } from 'utils/retry'
+import {
+  BlockNotFoundError,
+  GetTransactionReceiptParameters,
+  PublicClient,
+  TransactionNotFoundError,
+  TransactionReceipt,
+  TransactionReceiptNotFoundError,
+  WaitForTransactionReceiptTimeoutError,
+  createPublicClient,
+  http,
+} from 'viem'
+import { usePublicClient } from 'wagmi'
+import { useActiveChainId } from './useActiveChainId'
+
+export const viemClientsPublicNodes = CHAINS.reduce((prev, cur) => {
+  return {
+    ...prev,
+    [cur.id]: createPublicClient({
+      chain: cur,
+      transport: http(first(cur.rpcUrls.default.http), {
+        timeout: 15_000,
+      }),
+      batch: {
+        multicall: {
+          batchSize: 1024 * 200,
+          wait: 16,
+        },
+      },
+      pollingInterval: 6_000,
+    }),
+  }
+}, {} as Record<ChainId, PublicClient>)
+
+export type PublicNodeWaitForTransactionParams = GetTransactionReceiptParameters & {
+  chainId?: number
+}
+
+export function usePublicNodeWaitForTransaction() {
+  const { chainId } = useActiveChainId()
+  const provider = usePublicClient({ chainId })
+
+  const waitForTransaction_ = useCallback(
+    async (opts: PublicNodeWaitForTransactionParams): Promise<TransactionReceipt> => {
+      const getTransaction = async () => {
+        try {
+          const selectedChain: ChainId = opts?.chainId ?? chainId
+          // our custom node might be late to sync up
+          if (selectedChain && viemClientsPublicNodes[selectedChain]) {
+            return await viemClientsPublicNodes[selectedChain].getTransactionReceipt({ hash: opts.hash })
+          }
+
+          if (!provider) return undefined
+
+          return await provider.getTransactionReceipt({ hash: opts.hash })
+        } catch (error) {
+          if (error instanceof TransactionNotFoundError) {
+            throw new RetryableError(`Transaction not found: ${opts.hash}`)
+          } else if (error instanceof TransactionReceiptNotFoundError) {
+            throw new RetryableError(`Transaction receipt not found: ${opts.hash}`)
+          } else if (error instanceof BlockNotFoundError) {
+            throw new RetryableError(`Block not found for transaction: ${opts.hash}`)
+          } else if (error instanceof WaitForTransactionReceiptTimeoutError) {
+            throw new RetryableError(`Timeout reached when fetching transaction receipt: ${opts.hash}`)
+          }
+          throw error
+        }
+      }
+      return retry(getTransaction, {
+        n: 10,
+        minWait: 5000,
+        maxWait: 10000,
+        delay: (chainId ? AVERAGE_CHAIN_BLOCK_TIMES[chainId as ChainId] : BSC_BLOCK_TIME) * 1000 + 1000,
+      }).promise as Promise<TransactionReceipt>
+    },
+    [chainId, provider],
+  )
+
+  return {
+    waitForTransaction: waitForTransaction_,
+  }
+}
---
+++ apps/gamification/utils/retry.ts
@@ -0,0 +1,80 @@
+/* eslint-disable */
+
+export function wait(ms: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+function waitRandom(min: number, max: number): Promise<void> {
+  return wait(min + Math.round(Math.random() * Math.max(0, max - min)))
+}
+
+/**
+ * This error is thrown if the function is cancelled before completing
+ */
+export class CancelledError extends Error {
+  constructor() {
+    super('Cancelled')
+  }
+}
+
+/**
+ * Throw this error if the function should retry
+ */
+export class RetryableError extends Error {}
+
+/**
+ * Retries the function that returns the promise until the promise successfully resolves up to n retries
+ * @param fn function to retry
+ * @param n how many times to retry
+ * @param minWait min wait between retries in ms
+ * @param maxWait max wait between retries in ms
+ * @param delay the function should be run after some time without running the function
+ */
+export function retry<T>(
+  fn: () => Promise<T>,
+  { n, minWait, maxWait, delay }: { n: number; minWait: number; maxWait: number; delay?: number },
+): { promise: Promise<T>; cancel: () => void } {
+  let completed = false
+  let firstRun = true
+  let rejectCancelled: (error: Error) => void
+  const promise = new Promise<T>(async (resolve, reject) => {
+    rejectCancelled = reject
+    while (true) {
+      if (delay && firstRun) {
+        await wait(delay)
+        firstRun = false
+      }
+      let result: T
+      try {
+        result = await fn()
+        if (!completed) {
+          resolve(result)
+          completed = true
+        }
+        break
+      } catch (error) {
+        if (completed) {
+          break
+        }
+        if (n <= 0 || !(error instanceof RetryableError)) {
+          console.error(error)
+          reject(error)
+          completed = true
+          break
+        }
+        n--
+      }
+      await waitRandom(minWait, maxWait)
+    }
+  })
+  return {
+    promise,
+    cancel: () => {
+      if (completed) return
+      completed = true
+      rejectCancelled(new CancelledError())
+    },
+  }
+}
+
+/* eslint-enable */
