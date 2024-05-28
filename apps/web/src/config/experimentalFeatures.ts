b92483504abe5d190b726ca4ffed82c218626d56
chefjackson <116779127+chefjackson@users.noreply.github.com>
2024-05-28 20:35:22 +0800

    chore: use deprecated router by default

--- apps/web/src/config/experimentalFeatures.ts
+++ apps/web/src/config/experimentalFeatures.ts
@@ -32,7 +32,7 @@ export const EXPERIMENTAL_FEATURE_CONFIGS: ExperimentalFeatureConfigs = [
   },
   {
     feature: EXPERIMENTAL_FEATURES.UniversalRouter,
-    percentage: 1,
+    percentage: 0,
     whitelist: [],
   },
   {
