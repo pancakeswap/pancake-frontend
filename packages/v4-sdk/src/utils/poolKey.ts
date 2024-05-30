7c6c0c9080571ab8c24eb4590d153d23d0c5abe6
Chef Jerry <jerry@pancakeswap.com>
2024-05-31 04:19:06 +0800

    feat: poolkey

---
+++ packages/v4-sdk/src/utils/poolKey.ts
@@ -0,0 +1,36 @@
+import { Address, zeroAddress } from 'viem'
+import { Bytes32 } from '../types'
+import { BinPoolParameter, CLPoolParameter, encodePoolParameters } from './encodePoolParameters'
+
+export type PoolType = 'CL' | 'Bin'
+
+/**
+ * PoolKey is a unique identifier for a pool
+ *
+ * @see PoolKey
+ */
+export type PoolKey<T extends PoolType = never> = {
+  currency0: Address
+  currency1: Address
+  hooks?: Address
+  poolManager: Address
+  fee: number
+  parameters: T extends 'CL' ? CLPoolParameter : T extends 'Bin' ? BinPoolParameter : CLPoolParameter | BinPoolParameter
+}
+
+export type PoolKeyStruct = {
+  currency0: Address
+  currency1: Address
+  hooks: Address
+  poolManager: Address
+  fee: number
+  parameters: Bytes32
+}
+
+export const parsePoolKey = (poolKey: PoolKey): PoolKeyStruct => {
+  return {
+    ...poolKey,
+    hooks: poolKey.hooks ?? zeroAddress,
+    parameters: encodePoolParameters(poolKey.parameters),
+  }
+}
