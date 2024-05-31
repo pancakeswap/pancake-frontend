33d67a2763f581797e564e21ff6d399e060a1175
chefjackson <116779127+chefjackson@users.noreply.github.com>
2024-05-31 12:27:30 +0800

    chore: eth ifo

--- apps/web/src/views/TradingCompetition/components/YourScore/AchievementPoints.tsx
+++ apps/web/src/views/TradingCompetition/components/YourScore/AchievementPoints.tsx
@@ -1,6 +1,8 @@
 import { useTranslation } from '@pancakeswap/localization'
 import { Flex, Image, Text } from '@pancakeswap/uikit'
 
+import { ASSET_CDN } from 'config/constants/endpoints'
+
 interface AchievementPointsProps {
   achievement?: {
     image?: string
@@ -16,7 +18,9 @@ const AchievementPoints: React.FC<React.PropsWithChildren<AchievementPointsProps
 
   return (
     <Flex alignItems="center" flexWrap="wrap" justifyContent="center" width="100%">
-      {achievement?.image && <Image src={`/images/achievements/${achievement?.image}`} width={25} height={25} />}
+      {achievement?.image && (
+        <Image src={`${ASSET_CDN}/web/achievements/${achievement?.image}`} width={25} height={25} />
+      )}
       <Text fontSize="12px" color="textSubtle" textTransform="lowercase">
         + {userPointReward} {t('Points')}
       </Text>
