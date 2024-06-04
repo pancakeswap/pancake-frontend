7d589c9e15ffd0e5036727d1e84bb0bb61458893
Chef Jerry <jerry@pancakeswap.com>
2024-06-04 14:18:22 +0800

    feat: retry safe hash with status

--- apps/web/src/hooks/useSafeTxHashTransformer.ts
+++ apps/web/src/hooks/useSafeTxHashTransformer.ts
@@ -1,14 +1,21 @@
 import { ChainId } from '@pancakeswap/chains'
 import {
   DetailedExecutionInfoType,
+  TransactionStatus,
   type MultisigExecutionDetails,
   type TransactionDetails,
 } from '@safe-global/safe-gateway-typescript-sdk'
 import { AVERAGE_CHAIN_BLOCK_TIMES } from 'config/constants/averageChainBlockTimes'
 import { useCallback, useMemo } from 'react'
-import { wait } from 'state/multicall/retry'
+import { RetryableError, retry } from 'state/multicall/retry'
 import { Hash } from 'viem'
 import { useAccount } from 'wagmi'
+
+class SafeTxHashNotAvailableError extends Error {
+  constructor() {
+    super('Safe transaction hash is not available')
+  }
+}
 
 export const useSafeTxHashTransformer = () => {
   const { connector, chainId } = useAccount()
@@ -16,7 +23,7 @@ export const useSafeTxHashTransformer = () => {
   const confirmationSeconds = chainId ? AVERAGE_CHAIN_BLOCK_TIMES[chainId] : AVERAGE_CHAIN_BLOCK_TIMES[ChainId.BSC]
 
   return useCallback(
-    async <T = Hash | TransactionDetails>(safeTxHash: T, confirmationsRequired = 1): Promise<Hash> => {
+    async <T = Hash | TransactionDetails>(safeTxHash: T): Promise<Hash> => {
       console.debug('debug call useSafeTxHashTransformer', safeTxHash)
       if (!isGnosisSafe) return safeTxHash as Hash
       let hash = safeTxHash as Hash
@@ -35,24 +42,47 @@ export const useSafeTxHashTransformer = () => {
           return safeTxHash as Hash
         }
 
-        const provider: any = await connector!.getProvider()
-        await wait(confirmationSeconds * confirmationsRequired * 1000)
-        const resp = (await provider.sdk.txs.getBySafeTxHash(hash)) as TransactionDetails
+        const getTxHash = async (): Promise<Hash> => {
+          try {
+            const provider: any = await connector!.getProvider()
+            const resp = (await provider.sdk.txs.getBySafeTxHash(hash)) as TransactionDetails
 
-        if (resp.txHash) return resp.txHash as Hash
+            if (resp.txHash) return resp.txHash as Hash
 
-        if (resp.detailedExecutionInfo && resp.detailedExecutionInfo.type === DetailedExecutionInfoType.MULTISIG) {
-          console.debug('debug detailedExecutionInfo', resp.detailedExecutionInfo)
-          return resp.detailedExecutionInfo.safeTxHash as Hash
+            if (resp.detailedExecutionInfo && resp.detailedExecutionInfo.type === DetailedExecutionInfoType.MULTISIG) {
+              console.debug('debug detailedExecutionInfo', resp.detailedExecutionInfo)
+              return resp.detailedExecutionInfo.safeTxHash as Hash
+            }
+
+            console.debug('debug txStatus', resp.txStatus)
+            if (
+              resp.txStatus === TransactionStatus.AWAITING_CONFIRMATIONS ||
+              resp.txStatus === TransactionStatus.AWAITING_EXECUTION
+            ) {
+              throw new SafeTxHashNotAvailableError()
+            }
+            return (resp.txHash as Hash) ?? hash
+          } catch (error) {
+            console.error('Failed to get transaction hash from Safe SDK', error)
+            if (error instanceof SafeTxHashNotAvailableError) {
+              throw new RetryableError(error.message)
+            }
+            throw error
+          }
         }
 
-        return resp.txHash as Hash
+        return retry(getTxHash, {
+          n: 10,
+          minWait: 5000,
+          maxWait: 10000,
+          delay: confirmationSeconds * 1000 + 1000,
+        }).promise as Promise<Hash>
       } catch (error) {
         console.error('Failed to get transaction hash from Safe SDK', error)
       }
 
       return safeTxHash as Hash
     },
-    [connector, isGnosisSafe],
+    [confirmationSeconds, connector, isGnosisSafe],
   )
 }
