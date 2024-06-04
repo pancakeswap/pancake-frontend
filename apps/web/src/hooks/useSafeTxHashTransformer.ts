2b85acf4d133bb1477ac7364f9e3f12c3e913085
Chef Jerry <jerry@pancakeswap.com>
2024-06-04 23:41:34 +0800

    fix: remove pkg

--- apps/web/src/hooks/useSafeTxHashTransformer.ts
+++ apps/web/src/hooks/useSafeTxHashTransformer.ts
@@ -1,15 +1,22 @@
 import { ChainId } from '@pancakeswap/chains'
-import {
-  DetailedExecutionInfoType,
-  TransactionStatus,
-  type MultisigExecutionDetails,
-  type TransactionDetails,
-} from '@safe-global/safe-gateway-typescript-sdk'
 import { AVERAGE_CHAIN_BLOCK_TIMES } from 'config/constants/averageChainBlockTimes'
 import { useCallback, useMemo } from 'react'
 import { RetryableError, retry } from 'state/multicall/retry'
 import { Hash } from 'viem'
 import { useAccount } from 'wagmi'
+
+enum TransactionStatus {
+  AWAITING_CONFIRMATIONS = 'AWAITING_CONFIRMATIONS',
+  AWAITING_EXECUTION = 'AWAITING_EXECUTION',
+  CANCELLED = 'CANCELLED',
+  FAILED = 'FAILED',
+  SUCCESS = 'SUCCESS',
+}
+
+type TransactionDetails = {
+  txStatus: TransactionStatus
+  txHash?: string
+}
 
 class SafeTxHashNotAvailableError extends Error {
   constructor() {
@@ -26,18 +33,9 @@ export const useSafeTxHashTransformer = () => {
     async <T = Hash | TransactionDetails>(safeTxHash: T): Promise<Hash> => {
       console.debug('debug call useSafeTxHashTransformer', safeTxHash)
       if (!isGnosisSafe) return safeTxHash as Hash
-      let hash = safeTxHash as Hash
+      const hash = safeTxHash as Hash
 
       try {
-        if (typeof safeTxHash !== 'string') {
-          console.debug('debug safeTxHash is resp', safeTxHash)
-          const detailedExecutionInfo = (safeTxHash as TransactionDetails)
-            ?.detailedExecutionInfo as MultisigExecutionDetails
-
-          // eslint-disable-next-line no-param-reassign
-          hash = detailedExecutionInfo?.safeTxHash as Hash
-        }
-
         if (!hash) {
           return safeTxHash as Hash
         }
@@ -55,15 +53,6 @@ export const useSafeTxHashTransformer = () => {
             }
 
             if (resp.txHash) return resp.txHash as Hash
-
-            if (
-              resp.detailedExecutionInfo &&
-              resp.detailedExecutionInfo.type === DetailedExecutionInfoType.MULTISIG &&
-              resp.detailedExecutionInfo.safeTxHash
-            ) {
-              console.debug('debug detailedExecutionInfo', resp.detailedExecutionInfo)
-              return resp.detailedExecutionInfo.safeTxHash as Hash
-            }
 
             console.debug('debug txStatus', resp.txStatus)
             return (resp.txHash as Hash) ?? hash
