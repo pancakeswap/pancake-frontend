eaca70716f9d896e616baca7594dd150c0db12e2
chefmomota <momota@pancakeswap.com>
2024-06-05 14:56:52 +0800

    feat: Update useConnectTwitter type

--- apps/gamification/views/Profile/hooks/settingsModal/useConnectTwitter.tsx
+++ apps/gamification/views/Profile/hooks/settingsModal/useConnectTwitter.tsx
@@ -6,6 +6,14 @@ declare global {
   }
 }
 
+interface TwitterResponse {
+  auth_date: number
+  first_name: string
+  hash: string
+  id: number
+  username: string
+}
+
 // const YOUR_BOT_TOKEN = get from telegram
 // https://api.telegram.org/botYOUR_BOT_TOKEN/getMe
 const YOUR_BOT_ID = '7140457343'
@@ -30,8 +38,8 @@ export const useConnectTwitter = () => {
         bot_id: YOUR_BOT_ID, // Replace with your bot's ID
         request_access: true,
       },
-      (user) => {
-        console.log(user)
+      (user: TwitterResponse) => {
+        console.log(user.id)
         // Handle the authenticated user information
         // You can send it to your backend server
       },
