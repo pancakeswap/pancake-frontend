a090c3d858e544c12cc4187a21fbf22d645522ea
chefmomota <momota@pancakeswap.com>
2024-06-07 15:38:43 +0800

    fix: Conflict

--- apps/gamification/config/constants/types.ts
+++ apps/gamification/config/constants/types.ts
@@ -1,3 +1,6 @@
+import { Currency, CurrencyAmount, Percent, Price, TradeType } from '@pancakeswap/sdk'
+import { LegacyTradeWithStableSwap as TradeWithStableSwap } from '@pancakeswap/smart-router/legacy-router'
+
 export const FetchStatus = {
   Idle: 'idle',
   Fetching: 'pending',
@@ -34,4 +37,29 @@ export interface Achievement {
   description?: TranslatableText
   badge: string
   points: number
+}
+
+export interface StableTrade {
+  tradeType: TradeType
+  inputAmount: CurrencyAmount<Currency>
+  outputAmount: CurrencyAmount<Currency>
+  executionPrice: Price<Currency, Currency>
+  priceImpact: null
+  maximumAmountIn: (slippaged: Percent) => CurrencyAmount<Currency>
+  minimumAmountOut: (slippaged: Percent) => CurrencyAmount<Currency>
+}
+
+export type ITrade =
+  | Trade<Currency, Currency, TradeType>
+  | StableTrade
+  | TradeWithStableSwap<Currency, Currency, TradeType>
+  | undefined
+
+export type V2TradeAndStableSwap = Trade<Currency, Currency, TradeType> | StableTrade | undefined
+
+export const isStableSwap = (trade: ITrade): trade is StableTrade => {
+  return (
+    Boolean((trade as StableTrade)?.maximumAmountIn) &&
+    !(trade as Trade<Currency, Currency, TradeType> | TradeWithStableSwap<Currency, Currency, TradeType>)?.route
+  )
 }
--- apps/gamification/utils/exchange.ts
+++ apps/gamification/utils/exchange.ts
@@ -1,8 +1,13 @@
+import { Percent } from '@pancakeswap/sdk'
 import { pancakeRouter02ABI } from 'config/abi/IPancakeRouter02'
-import { V2_ROUTER_ADDRESS } from 'config/constants/exchange'
-
+import { BIPS_BASE, V2_ROUTER_ADDRESS } from 'config/constants/exchange'
 import { useActiveChainId } from 'hooks/useActiveChainId'
 import { useContract } from 'hooks/useContract'
+
+// converts a basis points value to a sdk percent
+export function basisPointsToPercent(num: number): Percent {
+  return new Percent(BigInt(num), BIPS_BASE)
+}
 
 export function useRouterContract() {
   const { chainId } = useActiveChainId()
