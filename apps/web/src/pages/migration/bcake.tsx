1033efe25404169211a84fba2383e2bacbc13e22
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2024-06-08 00:04:18 +0800

    chore: whitelist ETH for  migration page

--- apps/web/src/pages/migration/bcake.tsx
+++ apps/web/src/pages/migration/bcake.tsx
@@ -1,7 +1,7 @@
 import { ChainId } from '@pancakeswap/sdk'
 import Migration from 'views/Migration/bCake'
 
-const V2_SS_PM_SUPPORTED_CHAINS = [ChainId.BSC]
+const V2_SS_PM_SUPPORTED_CHAINS = [ChainId.BSC, ChainId.ETHEREUM]
 const MigrationPage = () => <Migration />
 
 MigrationPage.chains = V2_SS_PM_SUPPORTED_CHAINS
