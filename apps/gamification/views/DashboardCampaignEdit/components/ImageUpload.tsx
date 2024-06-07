567ba94b98532da5b6ad5203d4eee0f6a6eb2689
chefmomota <momota@pancakeswap.com>
2024-06-07 15:38:10 +0800

    fix: Conflict

--- apps/gamification/views/DashboardCampaignEdit/components/ImageUpload.tsx
+++ apps/gamification/views/DashboardCampaignEdit/components/ImageUpload.tsx
@@ -1,5 +1,7 @@
 import { useTranslation } from '@pancakeswap/localization'
 import { ArrowFirstIcon, Box, Button, Flex, Text } from '@pancakeswap/uikit'
+import { useState } from 'react'
+import ImageUploading, { ImageListType } from 'react-images-uploading'
 import { styled } from 'styled-components'
 
 const StyledThumbnail = styled('div')`
@@ -24,48 +26,60 @@ const EmptyContainer = styled(Flex)`
 
 export const ImageUpload = () => {
   const { t } = useTranslation()
-  const hasImage = false
+  const [images, setImages] = useState<ImageListType>([])
+
+  const onChange = (imageList: ImageListType) => {
+    setImages(imageList)
+  }
 
   return (
     <Box>
-      {hasImage ? (
-        <>
-          <Flex justifyContent="space-between" mb="8px">
-            <Text color="textSubtle" style={{ alignSelf: 'center' }}>
-              123.png
-            </Text>
-            <Button
-              paddingRight={0}
-              variant="text"
-              endIcon={
-                <ArrowFirstIcon style={{ transform: 'rotate(-90deg)' }} color="primary" width={20} height={20} />
-              }
-            >
-              {t('Upload New')}
-            </Button>
-          </Flex>
-          <Box overflow="hidden" borderRadius="16px" height={['162px', '261px', '261px', '261px', '378px']}>
-            <StyledThumbnail
-              className="thumbnail"
-              style={{
-                backgroundImage: `url('https://sgp1.digitaloceanspaces.com/strapi.space/ef14df0bf361ee3d0a0b604a54f0decb.jpg')`,
-              }}
-            />
-          </Box>
-        </>
-      ) : (
-        <EmptyContainer>
-          <Button
-            variant="text"
-            endIcon={<ArrowFirstIcon style={{ transform: 'rotate(-90deg)' }} color="primary" width={20} height={20} />}
-          >
-            {t('Upload New')}
-          </Button>
-          <Text style={{ alignSelf: 'center' }} color="textSubtle">
-            {t('JPEG or PNG smaller than 2mb')}
-          </Text>
-        </EmptyContainer>
-      )}
+      <ImageUploading value={images} onChange={onChange} dataURLKey="data_url">
+        {({ imageList, onImageUpload, onImageUpdate }) => {
+          return imageList[0] ? (
+            <>
+              <Flex justifyContent="space-between" mb="8px">
+                <Text color="textSubtle" style={{ alignSelf: 'center' }}>
+                  {images?.[0]?.file?.name}
+                </Text>
+                <Button
+                  paddingRight={0}
+                  variant="text"
+                  endIcon={
+                    <ArrowFirstIcon style={{ transform: 'rotate(-90deg)' }} color="primary" width={20} height={20} />
+                  }
+                  onClick={() => onImageUpdate(0)}
+                >
+                  {t('Upload New')}
+                </Button>
+              </Flex>
+              <Box overflow="hidden" borderRadius="16px" height={['162px', '261px', '261px', '261px', '378px']}>
+                <StyledThumbnail
+                  className="thumbnail"
+                  style={{
+                    backgroundImage: `url(${images?.[0]?.data_url})`,
+                  }}
+                />
+              </Box>
+            </>
+          ) : (
+            <EmptyContainer>
+              <Button
+                variant="text"
+                endIcon={
+                  <ArrowFirstIcon style={{ transform: 'rotate(-90deg)' }} color="primary" width={20} height={20} />
+                }
+                onClick={onImageUpload}
+              >
+                {t('Upload New')}
+              </Button>
+              <Text style={{ alignSelf: 'center' }} color="textSubtle">
+                {t('JPEG or PNG smaller than 2mb')}
+              </Text>
+            </EmptyContainer>
+          )
+        }}
+      </ImageUploading>
     </Box>
   )
 }
