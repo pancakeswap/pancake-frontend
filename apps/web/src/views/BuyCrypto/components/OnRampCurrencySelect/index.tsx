73c5a979987f51e22c8b14a51ea79c2edf4cb064
chefBingbong <133646395+ChefBingbong@users.noreply.github.com>
2024-06-12 14:19:39 +0100

    fix: show skeleton when new quote is fetching (#9891)
    <!--
    Before opening a pull request, please read the [contributing
    guidelines](https://github.com/pancakeswap/pancake-frontend/blob/develop/CONTRIBUTING.md)
    first
    -->
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    The focus of this PR is to refactor the `BuyCryptoForm` component and
    improve loading states in the `BuyCryptoSelector` component.
    
    ### Detailed summary
    - Refactored `useOnRampQuotes` hook to use `isLoading` instead of
    `isFetching`
    - Updated loading states in `BuyCryptoForm` component
    - Updated loading states and components in `BuyCryptoSelector` component
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your
    question}`
    
    <!-- end pr-codex -->

--- apps/web/src/views/BuyCrypto/components/OnRampCurrencySelect/index.tsx
+++ apps/web/src/views/BuyCrypto/components/OnRampCurrencySelect/index.tsx
@@ -1,26 +1,25 @@
 import { useTranslation } from '@pancakeswap/localization'
 import { Currency, Token } from '@pancakeswap/sdk'
-import { ArrowDropDownIcon, Box, BoxProps, Flex, Skeleton, Text, useModal } from '@pancakeswap/uikit'
-import { NumericalInput } from '@pancakeswap/widgets-internal'
+import { ArrowDropDownIcon, Box, BoxProps, Flex, SkeletonText, Text, useModal } from '@pancakeswap/uikit'
+import { NumberDisplay, NumericalInput } from '@pancakeswap/widgets-internal'
 import OnRampCurrencySearchModal, { CurrencySearchModalProps } from 'components/SearchModal/OnRampCurrencyModal'
-import { ReactNode } from 'react'
 import { fiatCurrencyMap, getNetworkDisplay, onRampCurrencies } from 'views/BuyCrypto/constants'
-import { DropDownContainer, OptionSelectButton, StyledCircle } from 'views/BuyCrypto/styles'
+import { DropDownContainer, OptionSelectButton } from 'views/BuyCrypto/styles'
 import { OnRampUnit } from 'views/BuyCrypto/types'
 import { OnRampCurrencyLogo } from '../OnRampProviderLogo/OnRampProviderLogo'
 
 interface BuyCryptoSelectorProps extends Omit<CurrencySearchModalProps, 'mode'>, BoxProps {
   id: 'onramp-fiat' | 'onramp-crypto'
+  currencyLoading: boolean
+  inputLoading?: boolean
   value: string
   onUserInput?: (value: string) => void
   disableCurrencySelect?: boolean
   error?: boolean
   errorText?: string
   onInputBlur?: () => void
-  disabled?: boolean
-  loading?: boolean
-  topElement?: ReactNode
-  bottomElement?: ReactNode
+  disableInput?: boolean
+  unit: OnRampUnit
 }
 
 const ButtonAsset = ({
@@ -59,19 +58,6 @@ const ButtonAsset = ({
   )
 }
 
-interface BuyCryptoSelectorProps extends Omit<CurrencySearchModalProps, 'mode'>, BoxProps {
-  id: 'onramp-fiat' | 'onramp-crypto'
-  currencyLoading: boolean
-  value: string
-  onUserInput?: (value: string) => void
-  disableCurrencySelect?: boolean
-  error?: boolean
-  errorText?: string
-  onInputBlur?: () => void
-  disableInput?: boolean
-  unit: OnRampUnit
-}
-
 export const BuyCryptoSelector = ({
   onCurrencySelect,
   onUserInput,
@@ -80,6 +66,7 @@ export const BuyCryptoSelector = ({
   otherSelectedCurrency,
   id,
   currencyLoading,
+  inputLoading = false,
   error,
   value,
   disableInput = false,
@@ -99,34 +86,33 @@ export const BuyCryptoSelector = ({
   )
 
   return (
-    <Box width="100%" {...props}>
-      <DropDownContainer error={error as any}>
-        {!disableInput && <StyledCircle />}
-        <NumericalInput
-          error={error}
-          disabled={disableInput || !selectedCurrency}
-          loading={!selectedCurrency}
-          className="token-amount-input"
-          value={value}
-          onBlur={onInputBlur}
-          onUserInput={(val) => {
-            onUserInput?.(val)
-          }}
-          align="left"
-        />
+    <Box width="100%" {...props} position="relative">
+      <DropDownContainer error={Boolean(error)}>
+        {!disableInput ? (
+          <NumericalInput
+            error={error}
+            align="left"
+            loading={!selectedCurrency}
+            className="token-amount-input"
+            value={inputLoading ? '' : value}
+            onBlur={onInputBlur}
+            onUserInput={(val) => {
+              onUserInput?.(val)
+            }}
+          />
+        ) : (
+          <SkeletonText initialHeight={25} initialWidth={100} loading={inputLoading}>
+            <NumberDisplay value={value} />
+          </SkeletonText>
+        )}
+
         <OptionSelectButton
           className="open-currency-select-button"
           selected={!!selectedCurrency}
           onClick={onPresentCurrencyModal}
         >
-          {selectedCurrency ? (
-            <ButtonAsset id={id} selectedCurrency={selectedCurrency as Currency} currencyLoading={currencyLoading} />
-          ) : (
-            <Flex>
-              <Skeleton width="105px" height="26px" variant="round" isDark />
-            </Flex>
-          )}
-          {selectedCurrency && <ArrowDropDownIcon color="primary" />}
+          <ButtonAsset id={id} selectedCurrency={selectedCurrency as Currency} currencyLoading={currencyLoading} />
+          <ArrowDropDownIcon color="primary" />
         </OptionSelectButton>
       </DropDownContainer>
     </Box>
