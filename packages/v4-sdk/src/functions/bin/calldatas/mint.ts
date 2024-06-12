15820c513ca78adb04de420fa6dad002549d38b7
Chef Jerry <jerry@pancakeswap.com>
2024-06-12 17:28:46 +0800

    feat(calldata): bin mint

---
+++ packages/v4-sdk/src/functions/bin/calldatas/mint.ts
@@ -0,0 +1,43 @@
+import invariant from 'tiny-invariant'
+import { Address, encodeFunctionData, Hex, toHex } from 'viem'
+import { BinPoolManager } from '../../../abis/BinPoolManager'
+import { parsePoolKey, PoolKey } from '../../../utils'
+import { encodeLiquidityConfig, LiquidityConfig } from '../liquidityConfigs'
+
+export type BinPoolMintParams = {
+  /**
+   * nft minted to
+   */
+  to: Address
+  liquidityConfigs: LiquidityConfig[]
+  // [amountX, amountY]
+  amountIn: [bigint, bigint]
+  binStep: bigint
+  salt: string | number
+}
+
+export const binPoolMintCalldata = <THookData extends Hex>(
+  key: PoolKey,
+  params: BinPoolMintParams,
+  hookData: THookData
+) => {
+  invariant(params.liquidityConfigs.length > 0, 'LIQUIDITY_CONFIGS_EMPTY')
+
+  const [X, Y] = params.amountIn
+
+  return encodeFunctionData({
+    abi: BinPoolManager,
+    functionName: 'mint',
+    args: [
+      parsePoolKey(key),
+      {
+        ...params,
+        // eslint-disable-next-line no-bitwise
+        amountIn: toHex((Y << 128n) | X, { size: 32 }),
+        salt: toHex(params.salt, { size: 32 }),
+        liquidityConfigs: params.liquidityConfigs.map((config) => encodeLiquidityConfig(config)),
+      },
+      hookData,
+    ],
+  })
+}
