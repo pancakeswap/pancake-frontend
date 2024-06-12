fa96c2a31e90cb3ee6d4c1953716e4dfdb927517
Akbar Karshiev <akbar.karshiev@gcore.lu>
2024-06-12 19:32:42 +0500

    header and footer links clean up
    empty items list fix
    
    farm navigation item enable

--- packages/uikit/src/components/Footer/config.tsx
+++ packages/uikit/src/components/Footer/config.tsx
@@ -1,6 +1,6 @@
 import { Language } from "../LangSelector/types";
+import { DiscordIcon, TwitterIcon } from "../Svg";
 import { FooterLinkType } from "./types";
-import { TwitterIcon, TelegramIcon, RedditIcon, InstagramIcon, GithubIcon, DiscordIcon, YoutubeIcon } from "../Svg";
 
 export const footerLinks: FooterLinkType[] = [
   {
@@ -80,102 +80,14 @@ export const socials = [
   {
     label: "Twitter",
     icon: TwitterIcon,
-    href: "https://twitter.com/pancakeswap",
-  },
-  {
-    label: "Telegram",
-    icon: TelegramIcon,
-    items: [
-      {
-        label: "English",
-        href: "https://t.me/pancakeswap",
-      },
-      {
-        label: "Bahasa Indonesia",
-        href: "https://t.me/pancakeswapIndonesia",
-      },
-      {
-        label: "中文",
-        href: "https://t.me/pancakeswap_CN",
-      },
-      {
-        label: "Tiếng Việt",
-        href: "https://t.me/PancakeSwapVN",
-      },
-      {
-        label: "Italiano",
-        href: "https://t.me/pancakeswap_Ita",
-      },
-      {
-        label: "русский",
-        href: "https://t.me/pancakeswap_ru",
-      },
-      {
-        label: "Türkiye",
-        href: "https://t.me/pancakeswapturkiye",
-      },
-      {
-        label: "Português",
-        href: "https://t.me/pancakeswapPortuguese",
-      },
-      {
-        label: "Español",
-        href: "https://t.me/pancakeswapES",
-      },
-      {
-        label: "日本語",
-        href: "https://t.me/pancakeswapJP",
-      },
-      {
-        label: "Français",
-        href: "https://t.me/pancakeswapFR",
-      },
-      {
-        label: "Deutsch",
-        href: "https://t.me/pancakeswap_DE",
-      },
-      {
-        label: "Filipino",
-        href: "https://t.me/pancakeswap_PH",
-      },
-      {
-        label: "ქართული ენა",
-        href: "https://t.me/pancakeswapGeorgia",
-      },
-      {
-        label: "हिन्दी",
-        href: "https://t.me/pancakeswap_INDIA",
-      },
-      {
-        label: "Announcements",
-        href: "https://t.me/PancakeSwapAnn",
-      },
-    ],
-  },
-  {
-    label: "Reddit",
-    icon: RedditIcon,
-    href: "https://reddit.com/r/pancakeswap",
-  },
-  {
-    label: "Instagram",
-    icon: InstagramIcon,
-    href: "https://instagram.com/pancakeswap_official",
-  },
-  {
-    label: "Github",
-    icon: GithubIcon,
-    href: "https://github.com/pancakeswap/",
+    href: "https://twitter.com/fraxcakeswap",
+    items: [],
   },
   {
     label: "Discord",
     icon: DiscordIcon,
-    href: "https://discord.gg/pancakeswap",
-  },
-  {
-    label: "Youtube",
-    icon: YoutubeIcon,
-    href: "https://www.youtube.com/@pancakeswap_official",
+    href: "https://discord.gg/6dvzXEhHBp",
+    items: [],
   },
 ];
 
