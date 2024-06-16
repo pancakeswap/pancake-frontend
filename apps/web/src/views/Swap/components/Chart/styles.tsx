b916ef2539f8e1fe57cd96f6915a7c3be6847523
Akbar Karshiev <akbar.karshiev@gcore.lu>
2024-06-16 22:15:35 +0500

    chart background fix

--- apps/web/src/views/Swap/components/Chart/styles.tsx
+++ apps/web/src/views/Swap/components/Chart/styles.tsx
@@ -12,7 +12,7 @@ export const StyledPriceChart = styled(Box)<{
   padding-top: 36px;
   ${({ theme }) => theme.mediaQueries.sm} {
     padding-top: 8px;
-    background: ${({ $isDark }) => ($isDark ? 'rgba(39, 38, 44, 0.5)' : 'rgba(255, 255, 255, 0.5)')};
+    background: ${({ $isDark }) => ($isDark ? 'rgba(255, 255, 255, 0.5)' : 'rgba(255, 255, 255, 0.5)')};
     border: ${({ theme }) => `1px solid ${theme.colors.cardBorder}`};
     border-radius: ${({ $isExpanded }) => ($isExpanded ? '0' : '16px')};
     width: ${({ $isExpanded, $isFullWidthContainer }) => ($isFullWidthContainer || $isExpanded ? '100%' : '50%')};
