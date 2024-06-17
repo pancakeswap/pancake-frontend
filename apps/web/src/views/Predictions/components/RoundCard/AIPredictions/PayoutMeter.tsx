157855dbd0ba2cd2de59b4dfe3253cfc3dee0c94
Chef Penguin <penguin@pancakeswap.com>
2024-06-17 12:22:32 +0530

    chore: remove unused svgs

--- apps/web/src/views/Predictions/components/RoundCard/AIPredictions/PayoutMeter.tsx
+++ apps/web/src/views/Predictions/components/RoundCard/AIPredictions/PayoutMeter.tsx
@@ -10,9 +10,6 @@ interface PayoutMeterProps extends BoxProps {
 const AnimatedMeter = (props: BoxProps) => {
   return (
     <Box position="relative" {...props}>
-      {/* <img src="/images/predictions-temp/meter.svg" alt="Payout Meter" width={100} /> */}
-      {/* <MeterPointerImage src="/images/predictions-temp/meter-pointer.svg" alt="Payout Pointer" width={12} /> */}
-
       <Lottie animationData={MeterJSON} style={{ width: '110px' }} />
     </Box>
   )
