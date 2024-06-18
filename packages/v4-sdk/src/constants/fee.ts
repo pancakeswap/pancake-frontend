c3b97cdc3ebf567b992cec75c5ecd4ccc566b865
Chef Jerry <jerry@pancakeswap.com>
2024-06-18 16:17:45 +0800

    feat: add DYNAMIC_FEE_FLAG

--- packages/v4-sdk/src/constants/fee.ts
+++ packages/v4-sdk/src/constants/fee.ts
@@ -16,3 +16,9 @@ export const TEN_PERCENT_FEE = 100_000n
  * Max protocol fee is 0.1% (1000 pips)
  */
 export const MAX_PROTOCOL_FEE = 1_000n
+
+/**
+ * a dynamic fee pool must have exactly same value for fee field
+ *
+ */
+export const DYNAMIC_FEE_FLAG = 0x800000
