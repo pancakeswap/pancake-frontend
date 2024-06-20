a7bc1d410ecd569961de201f6060523b863d26f8
chefmomota <momota@pancakeswap.com>
2024-06-20 21:05:14 +0800

    fix: Context problem

--- apps/gamification/views/DashboardQuestEdit/index.tsx
+++ apps/gamification/views/DashboardQuestEdit/index.tsx
@@ -1,11 +1,14 @@
 import { useTranslation } from '@pancakeswap/localization'
 import { Flex, useMatchBreakpoints } from '@pancakeswap/uikit'
+import { useEffect, useState } from 'react'
 import { styled } from 'styled-components'
 import { EditTemplate } from 'views/DashboardQuestEdit/components/EditTemplate'
 import { Reward } from 'views/DashboardQuestEdit/components/Reward'
 import { SubmitAction } from 'views/DashboardQuestEdit/components/SubmitAction'
 import { Tasks } from 'views/DashboardQuestEdit/components/Tasks'
 import { useQuestEdit } from 'views/DashboardQuestEdit/context/useQuestEdit'
+import { useGetSingleQuestData } from 'views/DashboardQuestEdit/hooks/useGetSingleQuestData'
+import { convertDateAndTime } from 'views/DashboardQuestEdit/utils/combineDateAndTime'
 
 const DashboardQuestEditContainer = styled(Flex)`
   padding: 16px;
@@ -17,10 +20,42 @@ const DashboardQuestEditContainer = styled(Flex)`
   }
 `
 
-export const DashboardQuestEdit = () => {
+export const DashboardQuestEdit = ({ questId }: { questId?: string }) => {
   const { t } = useTranslation()
+  const [showPage, setShowPage] = useState(false)
   const { isDesktop } = useMatchBreakpoints()
-  const { state, updateValue } = useQuestEdit()
+  const { state, updateValue, onTasksChange, updateAllState } = useQuestEdit()
+
+  const { questData, isFetched } = useGetSingleQuestData(questId ?? '')
+
+  useEffect(() => {
+    if (!showPage && isFetched && questData) {
+      const { orgId, chainId, completionStatus, title, description, reward, startDateTime, endDateTime } = questData
+      const startDateConvert = startDateTime ? new Date(convertDateAndTime(startDateTime)) : null
+      const endDateConvert = startDateTime ? new Date(convertDateAndTime(endDateTime)) : null
+
+      updateAllState({
+        chainId,
+        orgId,
+        completionStatus,
+        title,
+        description,
+        startDate: startDateConvert,
+        startTime: startDateConvert,
+        endDate: endDateConvert,
+        endTime: endDateConvert,
+        reward,
+        startDateTime,
+        endDateTime,
+      })
+      onTasksChange(questData.task)
+      setShowPage(false)
+    }
+  }, [isFetched, showPage, questData, onTasksChange, updateAllState])
+
+  if (questId && !showPage) {
+    return null
+  }
 
   return (
     <DashboardQuestEditContainer>
