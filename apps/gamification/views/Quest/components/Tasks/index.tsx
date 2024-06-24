11cd1952f2abe251c0ae3e4357fb56ca1a4dd381
chefmomota <momota@pancakeswap.com>
2024-06-24 13:44:17 +0800

    feat: handleLinkUserToQuest

--- apps/gamification/views/Quest/components/Tasks/index.tsx
+++ apps/gamification/views/Quest/components/Tasks/index.tsx
@@ -1,8 +1,11 @@
 import { useTranslation } from '@pancakeswap/localization'
-import { Box, Flex, FlexGap, Tag, Text } from '@pancakeswap/uikit'
+import { Box, Button, Flex, FlexGap, Tag, Text } from '@pancakeswap/uikit'
+import { GAMIFICATION_PUBLIC_API } from 'config/constants/endpoints'
 import { useMemo } from 'react'
+import { OptionIcon } from 'views/DashboardQuestEdit/components/Tasks/OptionIcon'
 import { useUserSocialHub } from 'views/Profile/hooks/settingsModal/useUserSocialHub'
 import { Task } from 'views/Quest/components/Tasks/Task'
+import { useAccount } from 'wagmi'
 // import { useVerifyTaskStatus } from 'views/Quest/hooks/useVerifyTaskStatus'
 
 interface TasksProps {
@@ -11,12 +14,29 @@ interface TasksProps {
 
 export const Tasks: React.FC<TasksProps> = ({ questId }) => {
   const { t } = useTranslation()
-  const { userInfo } = useUserSocialHub()
+  const { address: account } = useAccount()
+  const { userInfo, refresh } = useUserSocialHub()
   // const { taskStatus } = useVerifyTaskStatus(questId)
   const hasIdRegister = useMemo(
     () => userInfo.questIds?.map((i) => i.toLowerCase())?.includes(questId.toLowerCase()),
     [questId, userInfo.questIds],
   )
+
+  // TODO
+  // 1) If not hasIdRegister should call userLinkUserToQuest
+  // 2) If status finished no need to call action
+  // 3) If account not yet connect social need direct to /profile let user connect it.
+
+  const handleLinkUserToQuest = async () => {
+    try {
+      const response = await fetch(`${GAMIFICATION_PUBLIC_API}/userInfo/v1/linkUserToQuest/${account}/${questId}`)
+      if (response.ok) {
+        refresh()
+      }
+    } catch (error) {
+      console.error(`Submit link user to quest error: ${error}`)
+    }
+  }
 
   return (
     <Box mb="32px">
@@ -39,6 +59,26 @@ export const Tasks: React.FC<TasksProps> = ({ questId }) => {
       <FlexGap flexDirection="column" gap="12px">
         <Task />
       </FlexGap>
+      <Box>
+        <Text bold as="span">
+          {t('Tasks marked with the')}
+        </Text>
+        <OptionIcon />
+        <Text bold as="span">
+          {t('badge are optional.')}
+        </Text>
+        <Text bold as="span">
+          {t('But your chances of winning will be increased if you complete all the tasks!')}
+        </Text>
+      </Box>
+      {!hasIdRegister && (
+        <Flex>
+          <Text bold fontSize="12px" color="textSubtle">
+            {t('Start the quest to get access to the tasks')}
+          </Text>
+          <Button onClick={handleLinkUserToQuest}>{t('Start the Quest')}</Button>
+        </Flex>
+      )}
     </Box>
   )
 }
