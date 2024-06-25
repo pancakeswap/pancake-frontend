53b8245d18b105f64a95fa79890528884e861890
chefmomota <momota@pancakeswap.com>
2024-06-25 15:09:08 +0800

    feat: Update validateUrl

--- apps/gamification/views/DashboardQuestEdit/utils/validateFormat.ts
+++ apps/gamification/views/DashboardQuestEdit/utils/validateFormat.ts
@@ -10,9 +10,7 @@ export const validateLpAddress = (lpAddress: string) => {
 }
 
 export const validateUrl = (url: string) => {
-  const pattern =
-    /(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?\/[a-zA-Z0-9]{2,}|((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?)|(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})?/g
-
+  const pattern = /^(https?:\/\/)?(www\.)?[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)+([\\/?].*)?$/
   return !pattern.test(url)
 }
 
