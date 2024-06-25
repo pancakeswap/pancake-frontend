00c861fd1fc6eceafd8a23eb305aa18c730960fc
memoyil <2213635+memoyil@users.noreply.github.com>
2024-06-25 14:23:17 +0200

    fix: Build

--- apps/web/src/views/AffiliatesProgram/hooks/useShowWarningMessage.tsx
+++ apps/web/src/views/AffiliatesProgram/hooks/useShowWarningMessage.tsx
@@ -1,13 +1,14 @@
 import { V3_SUBGRAPH_URLS } from 'config/constants/endpoints'
 import { useActiveChainId } from 'hooks/useActiveChainId'
-import useSubgraphHealth, { Status } from 'hooks/useSubgraphHealth'
+import useSubgraphHealth from 'hooks/useSubgraphHealth'
+import { ApiStatus } from 'hooks/types'
 
 const useShowWarningMessage = () => {
   const { chainId } = useActiveChainId()
   const subgraph = chainId ? V3_SUBGRAPH_URLS[chainId] || '' : ''
   const { status } = useSubgraphHealth({ chainId, subgraph })
 
-  return status === Status.DOWN
+  return status === ApiStatus.DOWN
 }
 
 export default useShowWarningMessage
