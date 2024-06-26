990adbbe5f851eecdf92b149170cd2bf1b7e9fed
ChefEric <173023571+chef-eric@users.noreply.github.com>
2024-06-26 15:28:11 +0800

    feat: add perpetual banner (#10083)
    <!--
    Before opening a pull request, please read the [contributing
    guidelines](https://github.com/pancakeswap/pancake-frontend/blob/develop/CONTRIBUTING.md)
    first
    -->
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    The focus of this PR is to update banner components and translations,
    add a new PerpetualSeasonalBanner, and refactor useMultipleBannerConfig.
    
    ### Detailed summary
    - Updated `BannerTitle` component to handle additional variant types
    - Added `Trade and Win` banner with translations and links
    - Introduced `PerpetualSeason` and `PerpetualSeasonalBanner` components
    - Refactored `useMultipleBannerConfig` to include
    `PerpetualSeasonalBanner` and remove `PerpetualBanner` usage
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your
    question}`
    
    <!-- end pr-codex -->
    
    ---------
    
    Co-authored-by: chefjackson <116779127+chefjackson@users.noreply.github.com>

--- apps/web/src/views/Home/components/Banners/hooks/useMultipleBannerConfig.tsx
+++ apps/web/src/views/Home/components/Banners/hooks/useMultipleBannerConfig.tsx
@@ -4,18 +4,16 @@ import CompetitionBanner from '../CompetitionBanner'
 import { FeeRefundBanner } from '../FeeRefundBanner'
 import { GalxeTraverseBanner } from '../GalxeTraverseBanner'
 import GameBanner from '../GameBanner'
-import { ListaIFOBanner } from '../ListaIFOBanner'
 import { MultiChainBanner } from '../MultichainBanner'
 import { NemesisDownfallBanner } from '../NemesisDownfallBanner'
 import { OptionsBanner } from '../OptionsBanner'
 import { PaymasterBanner } from '../PaymasterBanner'
-import PerpetualBanner from '../PerpetualBanner'
+import { PerpetualSeasonalBanner } from '../PerpetualSeasonalBanner'
 import UserBanner from '../UserBanner'
 import { V4InfoBanner } from '../V4InfoBanner'
 import { VeCakeBanner } from '../VeCakeBanner'
 import WebNotificationBanner from '../WebNotificationBanner'
 import useIsRenderCompetitionBanner from './useIsRenderCompetitionBanner'
-import useIsRenderIfoBanner from './useIsRenderIFOBanner'
 import useIsRenderUserBanner from './useIsRenderUserBanner'
 
 interface IBannerConfig {
@@ -39,7 +37,6 @@ interface IBannerConfig {
 export const useMultipleBannerConfig = () => {
   const isRenderCompetitionBanner = useIsRenderCompetitionBanner()
   const isRenderUserBanner = useIsRenderUserBanner()
-  const shouldRenderIfoBanner = useIsRenderIfoBanner()
 
   return useMemo(() => {
     const NO_SHUFFLE_BANNERS: IBannerConfig[] = [
@@ -47,7 +44,10 @@ export const useMultipleBannerConfig = () => {
         shouldRender: isRenderUserBanner.shouldRender && !isRenderUserBanner.isEarningsBusdZero,
         banner: <UserBanner />,
       },
-      { shouldRender: shouldRenderIfoBanner, banner: <ListaIFOBanner /> },
+      {
+        shouldRender: true,
+        banner: <PerpetualSeasonalBanner />,
+      },
       {
         shouldRender: true,
         banner: <PaymasterBanner />,
@@ -83,10 +83,6 @@ export const useMultipleBannerConfig = () => {
         shouldRender: isRenderCompetitionBanner,
         banner: <CompetitionBanner />,
       },
-      {
-        shouldRender: true,
-        banner: <PerpetualBanner />,
-      },
     ]
     return [
       ...NO_SHUFFLE_BANNERS,
@@ -99,10 +95,5 @@ export const useMultipleBannerConfig = () => {
     ]
       .filter((bannerConfig: IBannerConfig) => bannerConfig.shouldRender)
       .map((bannerConfig: IBannerConfig) => bannerConfig.banner)
-  }, [
-    shouldRenderIfoBanner,
-    isRenderCompetitionBanner,
-    isRenderUserBanner.isEarningsBusdZero,
-    isRenderUserBanner.shouldRender,
-  ])
+  }, [isRenderCompetitionBanner, isRenderUserBanner.isEarningsBusdZero, isRenderUserBanner.shouldRender])
 }
