6204eee3ba03be75595b7efb2babb82a5d8a0593
chefmomota <momota@pancakeswap.com>
2024-06-26 16:38:50 +0800

    feat: Update quest info task

--- apps/gamification/views/Quest/components/Tasks/Task.tsx
+++ apps/gamification/views/Quest/components/Tasks/Task.tsx
@@ -5,72 +5,57 @@ import {
   Card,
   CheckmarkCircleFillIcon,
   ChevronDownIcon,
-  ErrorFillIcon,
   Flex,
   FlexGap,
-  Loading,
-  RefreshIcon,
+  OpenNewIcon,
   Text,
-  TwitterIcon,
 } from '@pancakeswap/uikit'
-import { styled } from 'styled-components'
-
-const VerifyButton = styled(Button)<{ $verified?: boolean }>`
-  background-color: ${({ theme, $verified }) => ($verified ? theme.colors.success : theme.colors.secondary)};
-`
-
-const ErrorButton = styled(Button)<{ $isError?: boolean }>`
-  background-color: ${({ theme, $isError }) => ($isError ? theme.colors.failure : theme.colors.warning)};
-`
+import { useTaskInfo } from 'views/DashboardQuestEdit/hooks/useTaskInfo'
+import { TaskType } from 'views/DashboardQuestEdit/type'
 
 export const Task = () => {
   const { t } = useTranslation()
   const isVerified = true
+  const taskType = TaskType.TELEGRAM_JOIN_GROUP
+  const { taskIcon, taskNaming } = useTaskInfo(false, 22)
 
   return (
     <Card>
       <Flex flexDirection="column" padding="16px">
         <Flex>
           <Flex mr="auto">
-            <TwitterIcon color="textSubtle" width={20} height={20} />
+            {taskIcon(taskType)}
             <Text ml="16px" bold>
-              123
+              {taskNaming(taskType)}
             </Text>
           </Flex>
           <Flex alignSelf="center">
-            <ChevronDownIcon color="primary" width={20} height={20} />
-            <CheckmarkCircleFillIcon color="success" />
-            <Loading margin="auto" width={16} height={16} color="secondary" />
-            <ErrorFillIcon color="warning" />
-            <ErrorFillIcon color="failure" />
+            {isVerified ? (
+              <CheckmarkCircleFillIcon color="success" />
+            ) : (
+              <ChevronDownIcon color="primary" width={20} height={20} />
+            )}
           </Flex>
         </Flex>
         <Box>
           <Text bold m="8px 0 16px 0">
-            Please connect your Social Name account to continue
+            description
           </Text>
           <FlexGap gap="8px">
-            <Button width="100%" scale="sm">
-              Proceed to connect
-            </Button>
-            <VerifyButton
-              scale="sm"
-              width="100%"
-              $verified={isVerified}
-              endIcon={
-                isVerified ? <CheckmarkCircleFillIcon color="invertedContrast" /> : <Loading width={16} height={16} />
-              }
-            >
-              {isVerified ? t('Verified ') : t('Verify')}
-            </VerifyButton>
-            <ErrorButton
-              width="100%"
-              scale="sm"
-              $isError
-              endIcon={<RefreshIcon style={{ transform: 'scaleX(-1) rotate(0deg)' }} color="invertedContrast" />}
-            >
-              {t('Retry')}
-            </ErrorButton>
+            {!isVerified ? (
+              <Button width="100%" scale="sm" endIcon={<OpenNewIcon color="invertedContrast" />}>
+                {t('Proceed to connect')}
+              </Button>
+            ) : (
+              <Button
+                variant="success"
+                scale="sm"
+                width="100%"
+                endIcon={<CheckmarkCircleFillIcon color="invertedContrast" />}
+              >
+                {t('Completed')}
+              </Button>
+            )}
           </FlexGap>
         </Box>
       </Flex>
