2af846bd9e7e635c60844201c626c7fdad8bd9c3
chefmomota <momota@pancakeswap.com>
2024-06-28 11:19:56 +0800

    fix: Dashboard page cache problem

--- apps/gamification/views/DashboardQuests/index.tsx
+++ apps/gamification/views/DashboardQuests/index.tsx
@@ -1,14 +1,18 @@
 import { useTranslation } from '@pancakeswap/localization'
+import { useQueryClient } from '@tanstack/react-query'
 import { options } from 'components/MultiSelectorUI'
-import { useMemo, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { CompletionStatusIndex } from 'views/DashboardQuestEdit/type'
 import { RecordTemplate } from 'views/DashboardQuests/components/RecordTemplate'
 import { Records } from 'views/DashboardQuests/components/Records'
 import { useFetchAllQuests } from 'views/DashboardQuests/hooks/useFetchAllQuests'
 import { convertIndexToStatus } from 'views/DashboardQuests/utils/convertIndexToStatus'
+import { useAccount } from 'wagmi'
 
 export const DashboardQuests = () => {
   const { t } = useTranslation()
+  const { address: account } = useAccount()
+  const queryClient = useQueryClient()
   const [statusButtonIndex, setStatusButtonIndex] = useState(CompletionStatusIndex.ONGOING)
   const [pickMultiSelect, setPickMultiSelect] = useState<Array<number>>([])
 
@@ -21,6 +25,19 @@ export const DashboardQuests = () => {
       })
       .filter((value): value is number => value !== null)
   }, [pickMultiSelect])
+
+  useEffect(() => {
+    return () => {
+      queryClient.invalidateQueries({
+        queryKey: [
+          'fetch-all-quest-dashboard-data',
+          account,
+          chainsValuePicked,
+          convertIndexToStatus(statusButtonIndex),
+        ],
+      })
+    }
+  }, [])
 
   const { questsData, isFetching } = useFetchAllQuests({
     chainIdList: chainsValuePicked,
