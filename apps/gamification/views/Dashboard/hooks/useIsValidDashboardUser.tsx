f1777378f72f95b0a2a1d13321c7a28d371ed582
chefmomota <momota@pancakeswap.com>
2024-07-02 17:47:21 +0800

    feat: Add more white list address

--- apps/gamification/views/Dashboard/hooks/useIsValidDashboardUser.tsx
+++ apps/gamification/views/Dashboard/hooks/useIsValidDashboardUser.tsx
@@ -10,6 +10,8 @@ const WHITE_LIST_WALLET_ADDRESS = [
   '0xC9E86aB663c713B3Fb874c073176670acb967ad7', // Chef Fran
   '0x621B94f09fdD36BDe89eDca8193bbB7865bb950d', // Chef Brownie
   '0x586ca92c4AB530f9F9b686aD754e1274702C037f', // Chef Brie
+  '0x273B863F7234804DEE624726A69EC228eb01E3d2', // Chef Cannoli
+  '0xee867c4d0D4A530D7551B57Ef097d1D7ceDA5879', // Chef Honney
 ].map((i) => i.toLowerCase())
 
 export const useIsValidDashboardUser = () => {
