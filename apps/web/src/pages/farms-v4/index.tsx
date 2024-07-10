f068425e784591d0e48cd1f055238ffed5601dbe
Chef Jerry <jerry@pancakeswap.com>
2024-07-10 15:40:28 +0800

    chore: remove flag

--- apps/web/src/pages/farms-v4/index.tsx
+++ apps/web/src/pages/farms-v4/index.tsx
@@ -1,22 +1,7 @@
 import { SUPPORT_FARMS } from 'config/constants/supportChains'
-import { EXPERIMENTAL_FEATURES } from 'config/experimentalFeatures'
-import { useExperimentalFeatureEnabled } from 'hooks/useExperimentalFeatureEnabled'
-import { useEffect } from 'react'
 import { FarmsV4 } from 'views/Farms/FarmsV4'
 
 const FarmsPage = () => {
-  const enabled = useExperimentalFeatureEnabled(EXPERIMENTAL_FEATURES.V4Farm)
-  useEffect(() => {
-    console.info('enabled', enabled)
-    if (!enabled) {
-      window.location.replace('/farms')
-    }
-  }, [enabled])
-
-  if (!enabled) {
-    return null
-  }
-
   return FarmsV4
 }
 
