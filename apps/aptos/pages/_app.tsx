f6fc806c1f351c321e75b89feb87991ded82bc02
chefjackson <116779127+chefjackson@users.noreply.github.com>
2024-07-11 10:05:54 +0800

    chore: install gtm on aptos

--- apps/aptos/pages/_app.tsx
+++ apps/aptos/pages/_app.tsx
@@ -52,6 +52,21 @@ function MyApp(props: AppProps) {
         />
         <meta name="theme-color" content="#1FC7D4" />
       </Head>
+      {process.env.NEXT_PUBLIC_GTM_ID ? (
+        <Script
+          strategy="afterInteractive"
+          id="google-tag"
+          dangerouslySetInnerHTML={{
+            __html: `
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
+`,
+          }}
+        />
+      ) : null}
       <DefaultSeo {...SEO} />
       <Providers>
         <GlobalHooks />
@@ -60,28 +75,17 @@ function MyApp(props: AppProps) {
         <Updaters />
         <App {...props} />
       </Providers>
-      {process.env.NEXT_PUBLIC_GA_TRACKING_ID && (
-        <Script
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
-        />
-      )}
-      {process.env.NEXT_PUBLIC_GA_TRACKING_ID && (
-        <Script
-          id="gtag-init"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-          }}
-        />
-      )}
+      {process.env.NEXT_PUBLIC_GTM_ID ? (
+        <noscript>
+          <iframe
+            title="gtm"
+            src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+      ) : null}
     </>
   )
 }
