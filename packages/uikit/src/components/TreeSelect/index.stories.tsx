7fa9c66774906fa18769d15f1dfe14f93da34a5b
ChefEric <173023571+chef-eric@users.noreply.github.com>
2024-07-15 10:48:08 +0800

    feat: initial universal farm list (#10185)
    <!--
    Before opening a pull request, please read the [contributing
    guidelines](https://github.com/pancakeswap/pancake-frontend/blob/develop/CONTRIBUTING.md)
    first
    -->
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    This PR focuses on restructuring and exporting components in the
    `widgets-internal` package.
    
    ### Detailed summary
    - Moved and exported `PoolTypeMenu` to `components` directory
    - Added exports for `TokenOverview`, `PoolTagFilter`, and `PoolTypeMenu`
    - Updated component imports in various files
    
    > The following files were skipped due to too many changes:
    `apps/web/src/views/Farms/v4/components/Pools.tsx`
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your
    question}`
    
    <!-- end pr-codex -->

--- packages/uikit/src/components/TreeSelect/index.stories.tsx
+++ packages/uikit/src/components/TreeSelect/index.stories.tsx
@@ -1,8 +1,4 @@
-import React, { useCallback, useState } from "react";
-import { styled } from "styled-components";
-import isEmpty from "lodash/isEmpty";
-import { useTheme } from "@pancakeswap/hooks";
-import type { TreeNode, TreeSelectChangeEvent, TreeSelectProps } from "./index";
+import React from "react";
 import { TreeSelect } from "./TreeSelect";
 import { Box } from "../Box";
 
@@ -64,69 +60,6 @@ const mockData = [
     ],
   },
 ];
-const Container = styled(Box)<{ isShow: boolean }>`
-  ${({ isShow, theme }) =>
-    isShow &&
-    `
-  && .p-treeselect {
-     border: 1px solid ${theme.colors.secondary};
-     border-bottom: 1px solid ${theme.colors.inputSecondary};
-     box-shadow: -2px -2px 2px 2px #7645D933, 2px -2px 2px 2px #7645D933;
-     border-bottom-left-radius: 0;
-     border-bottom-right-radius: 0;
-  }
-  && .p-treeselect-panel {
-    border: 1px solid ${theme.colors.secondary};
-    box-shadow: 2px 2px 2px 2px #7645D933, -2px 2px 2px 2px #7645D933;
-    border-top-left-radius: 0;
-    border-top-right-radius: 0;
-    border-top: none;
-  }
- `}
-  .p-treeselect,
- .p-treeselect-panel {
-    transition: box-shadow 0.1s, border 0.1s, border-radius 0.1s;
-  }
-  && .p-treeselect-panel {
-    margin: -1px 0 0 -1px;
-  }
-`;
-export interface IPoolsTypeFilterProps {
-  data?: TreeNode[];
-}
-
-export const PoolsTypeFilter: React.FC<IPoolsTypeFilterProps> = () => {
-  const [isShow, setIsShow] = useState(false);
-  const [selectedNodeKey, setSelectedNodeKey] = useState<TreeSelectProps["value"]>(null);
-  const { theme } = useTheme();
-
-  const handleTreeSelectChange = useCallback((e: TreeSelectChangeEvent) => {
-    // To prevent the user from unchecking all items, we need at least one poolType to filter.
-    if (isEmpty(e.value)) {
-      e.preventDefault();
-      return;
-    }
-    setSelectedNodeKey(e.value);
-  }, []);
-
-  return (
-    <Container margin="30px" width="272px" isShow={isShow}>
-      <TreeSelect
-        data={mockData}
-        style={{
-          backgroundColor: theme.colors.input,
-        }}
-        panelStyle={{
-          backgroundColor: theme.colors.input,
-        }}
-        value={selectedNodeKey}
-        onShow={() => setIsShow(true)}
-        onHide={() => setIsShow(false)}
-        onChange={handleTreeSelectChange}
-      />
-    </Container>
-  );
-};
 
 export const Default: React.FC<React.PropsWithChildren> = () => {
   return (
