7eaad4100a8a9e6331fd166b02975e831eff3503
memoyil <2213635+memoyil@users.noreply.github.com>
2024-07-15 22:41:55 +0200

    fix: Build

--- packages/gauges/src/getAllGauges.ts
+++ packages/gauges/src/getAllGauges.ts
@@ -4,7 +4,7 @@ import { CONFIG_TESTNET } from './constants/config/testnet'
 import { fetchAllGauges } from './fetchAllGauges'
 import { fetchAllKilledGauges } from './fetchAllKilledGauges'
 import { fetchAllGaugesVoting } from './fetchGaugeVoting'
-import { Gauge, GaugeConfig, GaugeInfoConfig } from './types'
+import { Gauge, GaugeConfig, GaugeInfo, GaugeInfoConfig } from './types'
 
 export type getAllGaugesOptions = {
   testnet?: boolean
