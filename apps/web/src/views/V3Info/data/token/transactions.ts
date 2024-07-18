a23c9392e5ed395cfba2887c0cc28e4e3954840f
Philip <97418926+chefilip@users.noreply.github.com>
2024-07-18 11:30:19 -0400

    chore(patch-4): consolidate transformPoolData (#10223)
    <!--
    Before opening a pull request, please read the [contributing
    guidelines](https://github.com/pancakeswap/pancake-frontend/blob/develop/CONTRIBUTING.md)
    first
    -->
    
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    The focus of this PR is to refactor transaction data handling in the
    V3Info module.
    
    ### Detailed summary
    - Added `ApiTransaction` type for transaction data structure
    - Refactored transaction data transformation functions
    - Replaced manual mapping with `transformTransaction` function
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your
    question}`
    
    <!-- end pr-codex -->
    
    Co-authored-by: chefilip <philip@pancakeswap.com>

--- apps/web/src/views/V3Info/data/token/transactions.ts
+++ apps/web/src/views/V3Info/data/token/transactions.ts
@@ -1,7 +1,7 @@
 import { explorerApiClient } from 'state/info/api/client'
-import dayjs from 'dayjs'
 import type { components } from 'state/info/api/schema'
-import { Transaction, TransactionType } from '../../types'
+import { transformTransaction } from 'views/V3Info/utils'
+import { Transaction } from '../../types'
 
 export async function fetchTokenTransactions(
   address: string,
@@ -24,24 +24,7 @@ export async function fetchTokenTransactions(
       .then((res) => res.data)
 
     return {
-      data: data?.map((m) => {
-        return {
-          type:
-            m.type === 'mint' ? TransactionType.MINT : m.type === 'burn' ? TransactionType.BURN : TransactionType.SWAP,
-          hash: m.transactionHash,
-          timestamp: dayjs(m.timestamp as string)
-            .unix()
-            .toString(),
-          sender: m.origin ?? '',
-          token0Symbol: m.token0.symbol,
-          token1Symbol: m.token1.symbol,
-          token0Address: m.token0.id,
-          token1Address: m.token1.id,
-          amountUSD: parseFloat(m.amountUSD),
-          amountToken0: parseFloat(m.amount0),
-          amountToken1: parseFloat(m.amount1),
-        }
-      }),
+      data: data?.map(transformTransaction),
       error: false,
       loading: false,
     }
