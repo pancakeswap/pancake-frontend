63cc04b813c8add93a7434f600c167cced37b872
BingBong <bingbong@pancakeswap.com>
2024-07-21 14:14:10 +0100

    use right color plus cleanup

--- apps/web/src/views/Home/components/Banners/PancakeSwapAthleticsBanner.tsx
+++ apps/web/src/views/Home/components/Banners/PancakeSwapAthleticsBanner.tsx
@@ -1,5 +1,5 @@
 import { useTranslation } from '@pancakeswap/localization'
-import { Flex, useMatchBreakpoints } from '@pancakeswap/uikit'
+import { Box, Flex, useMatchBreakpoints } from '@pancakeswap/uikit'
 import {
   BackgroundGraphic,
   BannerActionContainer,
@@ -19,17 +19,35 @@ import styled from 'styled-components'
 
 type IActions = { href: string; text: string; icon?: 'arrowForward' | 'openNew' } & Partial<CSSStyleDeclaration>
 
+enum CustomBreakPoints {
+  sm = 'sm',
+  md = 'md',
+  lg = 'lg',
+}
 const bg = 'radial-gradient(70.49% 124.11% at 44.31% -24.11%, #FEC375 0%, #F68129 18.86%, #EC6357 66.24%, #D8464F 100%)'
 
 const StyledImage = styled.img<{ hidden?: boolean }>`
   display: ${({ hidden }) => (hidden ? 'none' : 'block')};
   width: 45px;
+  margin: 0 4px;
 `
 
 const bgDesktop = `https://new-assets-for-athletic-bann.assets-agx.pages.dev/web/banners/athletics/athletics-bunnies-lg.png`
 const bgMobile = `https://new-assets-for-athletic-bann.assets-agx.pages.dev/web/banners/athletics/athletics-bunnies-md.png`
 const rings = `https://new-assets-for-athletic-bann.assets-agx.pages.dev/web/banners/athletics/athletics-top-logo.png`
 const ringsAlt = `https://new-assets-for-athletic-bann.assets-agx.pages.dev/web/banners/athletics/athletics-rings.png`
+
+const Titles: { [bp in CustomBreakPoints]: string } = {
+  sm: 'PancakeSwap Athletic Games:',
+  md: 'PancakeSwap Games: Get Usdt, Merch, and NFTs',
+  lg: 'PancakeSwap Athletic Games: Get Usdt, Merch, special edition NFTs',
+}
+
+const SubTitles: { [bp in CustomBreakPoints]: string } = {
+  sm: 'Get NFTs, merch, and USDT ',
+  md: 'Complete tasks for NFTs, merch, and USDT Prises!',
+  lg: 'Complete tasks for special edition NFTs, merch, and USDT Prises!',
+}
 
 const learnMoreLink = ''
 const joinNowLink = ''
@@ -47,7 +65,7 @@ const bgXsVariant: GraphicDetail = {
 }
 
 const titleVariant = {
-  color: '#7645D9',
+  color: '#280D5F',
   strokeColor: '#ffffff',
   strokeSize: 2,
   fontSize: 28,
@@ -61,22 +79,23 @@ export const AthleticsBanner = () => {
   const { isMobile } = useMatchBreakpoints()
 
   const titleText = useMemo(() => {
-    if (width < 800) return t('PancakeSwap Athletic Games:')
-    if (width < 1040) return t('PancakeSwap Games: Get Usdt, Merch, and NFTs')
-    return t('PancakeSwap Athletic Games: Get Usdt, Merch, special edition NFTs')
-  }, [t, width])
+    if (width < 800) return Titles.sm
+    if (width < 1040) return Titles.md
+    return Titles.lg
+  }, [width])
 
   const subTitleText = useMemo(() => {
-    if (width < 400) return ''
-    if (width < 575) return t('Get NFTs, merch, and USDT ')
-    if (width < 1040) return t('Complete tasks for NFTs, merch, and USDT Prises!')
-    return t('Complete tasks for special edition NFTs, merch, and USDT Prises!')
-  }, [t, width])
+    if (width < 800) return SubTitles.sm
+    if (width < 1040) return SubTitles.md
+    return SubTitles.lg
+  }, [width])
 
   const Action = ({ href, icon, text, ...props }: IActions) => (
-    <LinkExternalAction href={href} externalIcon={icon} color={props.color} display={props.display}>
-      {t(text)}
-    </LinkExternalAction>
+    <Box display={props.display}>
+      <LinkExternalAction href={href} externalIcon={icon} color={props.color}>
+        {t(text)}
+      </LinkExternalAction>
+    </Box>
   )
 
   return (
@@ -85,18 +104,18 @@ export const AthleticsBanner = () => {
         badges={
           <Flex alignItems="center" justifyContent="center">
             <PancakeSwapBadge compact={width < 450} />
-            <StyledImage src={rings} alt="rings" />
             <StyledImage src={ringsAlt} alt="rings-alt" hidden={isMobile} />
+            <StyledImage src={rings} alt="rings" />
           </Flex>
         }
         actions={
           <BannerActionContainer>
-            <Action href="" icon="arrowForward" display="flex" text="Join Now" />
-            <Action href="" icon="openNew" display="flex" text="Learn More" />
+            <Action href="" icon="arrowForward" display="flex" text="Join Now" color="#280D5F" />
+            <Action href="" icon="openNew" display={isMobile ? 'none' : 'flex'} text="Learn More" color="#280D5F" />
           </BannerActionContainer>
         }
-        desc={<BannerDesc style={{ whiteSpace: 'break-spaces' }}>{subTitleText}</BannerDesc>}
-        title={<BannerTitle variant={titleVariant}> {titleText}</BannerTitle>}
+        desc={<BannerDesc style={{ whiteSpace: 'break-spaces' }}>{t(subTitleText)}</BannerDesc>}
+        title={<BannerTitle variant={titleVariant}> {t(titleText)}</BannerTitle>}
       />
       <BannerGraphics>
         <BackgroundGraphic src={bgDesktop} width={444} height={224} sm={bgSmVariant} xs={bgXsVariant} />
