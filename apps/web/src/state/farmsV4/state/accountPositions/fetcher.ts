9dc2d6c0fc7eb87246e39155cf158e58c8e93fa0
Chef Jerry <jerry@pancakeswap.com>
2024-07-25 19:37:16 +0800

    feat: v2/ss staked & rewards

--- apps/web/src/state/farmsV4/state/accountPositions/fetcher.ts
+++ apps/web/src/state/farmsV4/state/accountPositions/fetcher.ts
@@ -1,11 +1,15 @@
 import { ChainId } from '@pancakeswap/chains'
-import { PositionDetails } from '@pancakeswap/farms'
+import { BCakeWrapperFarmConfig, PositionDetails } from '@pancakeswap/farms'
 import { masterChefV3ABI, NFT_POSITION_MANAGER_ADDRESSES, nonfungiblePositionManagerABI } from '@pancakeswap/v3-sdk'
-import { getMasterChefV3Address } from 'utils/addressHelpers'
+import BigNumber from 'bignumber.js'
+import { masterChefV2ABI } from 'config/abi/masterchefV2'
+import { v2BCakeWrapperABI } from 'config/abi/v2BCakeWrapper'
+import { getCrossFarmingVaultAddress, getMasterChefV2Address, getMasterChefV3Address } from 'utils/addressHelpers'
 import { getMasterChefV3Contract } from 'utils/contractHelpers'
 import { publicClient } from 'utils/viem'
 import { Address, encodeFunctionData, Hex } from 'viem'
 import { decodeFunctionResult } from 'viem/_types/utils/abi/decodeFunctionResult'
+import { StablePoolInfo, V2PoolInfo } from '../type'
 
 export const getAccountV3TokenIdsInContract = async (
   chainId: number,
@@ -153,5 +157,101 @@ export const getAccountV3FarmingPendingCakeReward = async (
       functionName: isZkSync ? 'pendingCake' : 'harvest',
       data: res,
     })
+  })
+}
+
+export const getAccountV2FarmingStakedBalances = async (
+  chainId: number,
+  account: Address,
+  pools: Array<V2PoolInfo | StablePoolInfo>,
+) => {
+  const masterChefV2Address =
+    chainId === ChainId.BSC ? getMasterChefV2Address(chainId) : getCrossFarmingVaultAddress(chainId)
+  if (!account || !chainId || pools.length === 0 || !masterChefV2Address) return []
+
+  const validPools = pools.filter(
+    (pool) => ['v2', 'stable'].includes(pool.protocol) && pool.pid && pool.chainId === chainId,
+  )
+  const client = publicClient({ chainId })
+
+  const balanceCalls = validPools.map((pool) => {
+    return {
+      abi: masterChefV2ABI,
+      address: masterChefV2Address,
+      functionName: 'userInfo',
+      args: [BigInt(pool.pid!), account] as const,
+    } as const
+  })
+
+  const balances = await client.multicall({
+    contracts: balanceCalls,
+    allowFailure: false,
+  })
+
+  return balances.map((balance) => {
+    return new BigNumber(balance[0].toString()).toString()
+  })
+}
+
+export const getAccountV2FarmingPendingCakeReward = async (
+  chainId: number,
+  account: Address,
+  pools: Array<V2PoolInfo | StablePoolInfo>,
+) => {
+  const masterChefV2Address =
+    chainId === ChainId.BSC ? getMasterChefV2Address(chainId) : getCrossFarmingVaultAddress(chainId)
+  if (!account || !chainId || pools.length === 0 || !masterChefV2Address) return []
+
+  const validPools = pools.filter(
+    (pool) => ['v2', 'stable'].includes(pool.protocol) && pool.pid && pool.chainId === chainId,
+  )
+  const client = publicClient({ chainId })
+
+  const earningCalls = validPools.map((pool) => {
+    return {
+      abi: masterChefV2ABI,
+      address: masterChefV2Address,
+      functionName: 'pendingCake',
+      args: [BigInt(pool.pid!), account] as const,
+    } as const
+  })
+
+  const earnings = await client.multicall({
+    contracts: earningCalls,
+    allowFailure: false,
+  })
+
+  return earnings.map((earning) => {
+    return new BigNumber(earning.toString()).toString()
+  })
+}
+
+export const getAccountV2FarmingBCakeWrapperEarning = async (
+  chainId: number,
+  account: Address,
+  bCakeWrapperConfig: BCakeWrapperFarmConfig[],
+) => {
+  const client = publicClient({ chainId })
+
+  if (!account || !chainId || bCakeWrapperConfig.length === 0) return []
+
+  const validConfig = bCakeWrapperConfig.filter((pool) => pool.bCakeWrapperAddress && pool.chainId === chainId)
+
+  const earningCalls = validConfig.map((pool) => {
+    return {
+      abi: v2BCakeWrapperABI,
+      address: pool.bCakeWrapperAddress,
+      functionName: 'pendingReward',
+      args: [account] as const,
+    } as const
+  })
+
+  const earnings = await client.multicall({
+    contracts: earningCalls,
+    allowFailure: false,
+  })
+
+  return earnings.map((earning) => {
+    return new BigNumber(earning.toString()).toString()
   })
 }
