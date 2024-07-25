1ee24de2cc935727404a2a16f9c36bc59ebbea95
Chef Jerry <jerry@pancakeswap.com>
2024-07-25 19:45:36 +0800

    feat: position state type

---
+++ apps/web/src/state/farmsV4/state/accountPositions/atom.ts
@@ -0,0 +1,30 @@
+import { PositionDetails } from '@pancakeswap/farms'
+import { atom } from 'jotai'
+import { ChainIdAddressKey } from '../type'
+
+export type AccountPositionMap = {
+  [poolKey: ChainIdAddressKey]: PositionInfo
+}
+
+export const userPositionsAtom = atom<Record<ChainIdAddressKey, AccountPositionMap>>({})
+
+export const cakePendingRewardsAtom = atom<Record<ChainIdAddressKey, number>>({})
+
+type PositionInfo = {
+  chainId: number
+  lpAddress: string
+  info: V2PositionInfo | V3PositionInfo
+}
+
+type V2PositionInfo = {
+  pendingCakeReward: bigint
+  balance: bigint
+}
+
+type V3FarmingPosition = PositionDetails & {
+  pendingCakeReward: bigint
+}
+
+type V3NonFarmingPosition = PositionDetails
+
+type V3PositionInfo = V3FarmingPosition | V3NonFarmingPosition
