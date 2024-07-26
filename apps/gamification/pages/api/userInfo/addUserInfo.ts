1b019f0e7e06aecd36823a3eb4acd586c49b05c0
chefmomota <momota@pancakeswap.com>
2024-07-26 12:13:12 +0800

    feat: Update

--- apps/gamification/pages/api/userInfo/addUserInfo.ts
+++ apps/gamification/pages/api/userInfo/addUserInfo.ts
@@ -16,7 +16,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   const result = await response.json()
   if (!response.ok) {
-    return res.status(400).json(result)
+    console.log('addUserInfo result: ', result)
+    return res.status(400).json(response)
+    // return res.status(400).json(result)
   }
 
   return res.status(200).json(response)
