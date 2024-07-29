3d81b4703433e042bded7eed2109dd3bb3abfdb9
chefjackson <116779127+chefjackson@users.noreply.github.com>
2024-07-29 13:02:16 +0800

    chore: use middleware for siwe

---
+++ apps/gamification/middlewares/withSiwe.ts
@@ -0,0 +1,44 @@
+import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next'
+import { getViemClients } from 'utils/viem.server'
+import { parseSiweMessage, verifySiweMessage } from 'viem/siwe'
+
+type ExtendedReq = NextApiRequest & {
+  siwe: ReturnType<typeof parseSiweMessage>
+}
+
+export type ExtendedApiHandler = (req: ExtendedReq, res: NextApiResponse) => ReturnType<NextApiHandler>
+
+export function withSiweAuth(handler: NextApiHandler | ExtendedApiHandler): ExtendedApiHandler {
+  return async (req, res) => {
+    const unauthorized = () => res.status(401).json({ message: 'Unauthorized' })
+    const encodedMessage = req.headers['x-g-siwe-message']
+    const signature = req.headers['x-g-siwe-signature']
+    const chainId = req.headers['x-g-siwe-chain-id']
+    if (
+      !encodedMessage ||
+      !signature ||
+      typeof encodedMessage !== 'string' ||
+      typeof signature !== 'string' ||
+      !chainId
+    ) {
+      return unauthorized()
+    }
+    const message = decodeURIComponent(encodedMessage)
+    const siweMessage = parseSiweMessage(message)
+    const { address } = siweMessage
+    if (!address) {
+      return unauthorized()
+    }
+    const client = getViemClients({ chainId: Number(chainId) })
+    const validSignature = await verifySiweMessage(client, {
+      message,
+      signature: signature as `0x{string}`,
+    })
+    if (!validSignature) {
+      return unauthorized()
+    }
+    // eslint-disable-next-line no-param-reassign
+    req.siwe = siweMessage
+    return handler(req, res)
+  }
+}
--- apps/gamification/pages/api/dashboard/all-quests-info.ts
+++ apps/gamification/pages/api/dashboard/all-quests-info.ts
@@ -1,10 +1,8 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
 import qs from 'qs'
-import { parseSiweMessage, verifySiweMessage } from 'viem/siwe'
 import { z } from 'zod'
 
-import { getViemClients } from 'utils/viem.server'
-import { DASHBOARD_ALLOW_LIST } from 'config/constants/dashboardAllowList'
+import { withSiweAuth } from 'middlewares/withSiwe'
+import { withDashboardAllowlistAuth } from 'middlewares/withDashboardAllowlistAuth'
 
 const zAddress = z.string().regex(/^0x[a-fA-F0-9]{40}$/)
 const zChainIdOptional = z
@@ -30,63 +28,40 @@ const zQuery = z.object({
   pageSize: z.coerce.number().max(100).nullable(),
 })
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (!process.env.GAMIFICATION_DASHBOARD_API || !req.query || req.method !== 'GET') {
-    return res.status(400).json({ message: 'API URL Empty / Method wrong' })
-  }
+const handler = withSiweAuth(
+  withDashboardAllowlistAuth(async (req, res) => {
+    if (!process.env.GAMIFICATION_DASHBOARD_API || !req.query || req.method !== 'GET') {
+      return res.status(400).json({ message: 'API URL Empty / Method wrong' })
+    }
 
-  const queryString = qs.stringify(req.query, { arrayFormat: 'comma' })
-  const queryParsed = qs.parse(queryString)
-  const parsed = zQuery.safeParse(queryParsed)
+    const queryString = qs.stringify(req.query, { arrayFormat: 'comma' })
+    const queryParsed = qs.parse(queryString)
+    const parsed = zQuery.safeParse(queryParsed)
 
-  if (parsed.success === false) {
-    return res.status(400).json({ message: 'Invalid query', reason: parsed.error })
-  }
+    if (parsed.success === false) {
+      return res.status(400).json({ message: 'Invalid query', reason: parsed.error })
+    }
 
-  const unauthorized = () => res.status(401).json({ message: 'Unauthorized' })
-  const encodedMessage = req.headers['x-g-siwe-message']
-  const signature = req.headers['x-g-siwe-signature']
-  const chainId = req.headers['x-g-siwe-chain-id']
-  if (
-    !encodedMessage ||
-    !signature ||
-    typeof encodedMessage !== 'string' ||
-    typeof signature !== 'string' ||
-    !chainId
-  ) {
-    return unauthorized()
-  }
-  const message = decodeURIComponent(encodedMessage)
-  const { address } = parseSiweMessage(message)
-  if (!address || !DASHBOARD_ALLOW_LIST.includes(address)) {
-    return unauthorized()
-  }
-  const client = getViemClients({ chainId: Number(chainId) })
-  const validSignature = await verifySiweMessage(client, {
-    message,
-    signature: signature as `0x{string}`,
-  })
-  if (!validSignature) {
-    return unauthorized()
-  }
-
-  const response = await fetch(
-    `${process.env.GAMIFICATION_DASHBOARD_API}/quests/org/${queryParsed.address}?${queryString}`,
-    {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-        'x-secure-token': process.env.DASHBOARD_TOKEN as string,
+    const response = await fetch(
+      `${process.env.GAMIFICATION_DASHBOARD_API}/quests/org/${queryParsed.address}?${queryString}`,
+      {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          'x-secure-token': process.env.DASHBOARD_TOKEN as string,
+        },
       },
-    },
-  )
+    )
 
-  if (!response.ok) {
-    return res.status(400).json({ message: 'An error occurred please try again' })
-  }
+    if (!response.ok) {
+      return res.status(400).json({ message: 'An error occurred please try again' })
+    }
 
-  const data = await response.json()
+    const data = await response.json()
 
-  return res.status(200).json(data)
-}
+    return res.status(200).json(data)
+  }),
+)
+
+export default handler
