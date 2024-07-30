435f77d2a1eaebe4447e980e403c2f6950f68925
ChefMomota <98292246+ChefMomota@users.noreply.github.com>
2024-07-30 18:53:15 +0800

    fix: Karat Galaxy eslint (#10289)
    <!--
    Before opening a pull request, please read the [contributing
    guidelines](https://github.com/pancakeswap/pancake-frontend/blob/develop/CONTRIBUTING.md)
    first
    -->
    
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    The focus of this PR is to update the `karatGalaxy` game in the Karat
    project with a new genre and image links.
    
    ### Detailed summary
    - Added `genre` as `GenreType.FPS`
    - Updated image links for `headerIconImage`, `projectLogo`, and
    `projectCircleLogo`
    - Modified the `posters` structure with new image and video links
    - Updated `socialMedia` links for Telegram and Discord
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your
    question}`
    
    <!-- end pr-codex -->

--- packages/games/src/constants/games/karatGalaxy.ts
+++ packages/games/src/constants/games/karatGalaxy.ts
@@ -1,12 +1,13 @@
 import { GameType, GenreType, PostersItemDataType, PostersLayout, TrendingTagType } from '../../types'
 
-export const karatGalaxy: GameType = { 
-  id: 'karat-galaxy', 
-  projectName: 'Karat', 
-  title: 'Karat Galaxy', 
-  subTitle: 'Embark on a Cosmic Karat Quest: Shoot, mine, win!', 
-  description: 'Join an interstellar adventure where mining, NFTs, DeFi, and fun unite.  Power up with KARAT and turn CAKE tokens into gold coins, unlocking exciting in-game adventures. Enjoy exclusive bonuses with Pancake Squad and Bunny NFTs. Battle, mine, win, and conquer the cosmos.', 
-  publishDate: 1698044972, // Timestamp in seconds 
+export const karatGalaxy: GameType = {
+  id: 'karat-galaxy',
+  projectName: 'Karat',
+  title: 'Karat Galaxy',
+  subTitle: 'Embark on a Cosmic Karat Quest: Shoot, mine, win!',
+  description:
+    'Join an interstellar adventure where mining, NFTs, DeFi, and fun unite.  Power up with KARAT and turn CAKE tokens into gold coins, unlocking exciting in-game adventures. Enjoy exclusive bonuses with Pancake Squad and Bunny NFTs. Battle, mine, win, and conquer the cosmos.',
+  publishDate: 1698044972, // Timestamp in seconds
   genre: GenreType.FPS,
   trendingTags: [
     TrendingTagType.FPS,
@@ -16,39 +17,29 @@ export const karatGalaxy: GameType = {
     TrendingTagType.Multiplayer,
     TrendingTagType.CakeToken,
   ],
- // 1600px x 224px  
- headerImage: 'https://statics.karatgalaxy.io/pancakeswap-game-landing/header.jpg',
- headerIconImage: { 
-  // 716px × 290px 
+  headerImage: 'https://statics.karatgalaxy.io/pancakeswap-game-landing/header.jpg',
+  headerIconImage: {
     desktop: 'https://statics.karatgalaxy.io/pancakeswap-game-landing/header-icon.png',
-  // 215px × 174px 
-    mobile: 'https://statics.karatgalaxy.io/pancakeswap-game-landing/header-icon.png'
-  }, 
-  projectLogo: { 
-  // 153px × 55px 
-    lightTheme: 'https://statics.karatgalaxy.io/pancakeswap-game-landing/logo.svg', 
-  // 153px × 55px 
-    darkTheme: 'https://statics.karatgalaxy.io/pancakeswap-game-landing/logo.svg', 
-  }, 
-  projectCircleLogo: { 
-  // 120px × 120px 
+    mobile: 'https://statics.karatgalaxy.io/pancakeswap-game-landing/header-icon.png',
+  },
+  projectLogo: {
+    lightTheme: 'https://statics.karatgalaxy.io/pancakeswap-game-landing/logo.svg',
+    darkTheme: 'https://statics.karatgalaxy.io/pancakeswap-game-landing/logo.svg',
+  },
+  projectCircleLogo: {
     lightTheme: 'https://statics.karatgalaxy.io/pancakeswap-game-landing/circle-logo.png',
-    // 120px × 120px 
-	darkTheme: 'https://statics.karatgalaxy.io/pancakeswap-game-landing/circle-logo.png',
-  }, 
+    darkTheme: 'https://statics.karatgalaxy.io/pancakeswap-game-landing/circle-logo.png',
+  },
   gameLink: {
     playNowLink: 'https://statics.karatgalaxy.io/',
   },
-  posters: { 
-    layout: PostersLayout.Horizontal, 
-  // Layout horizontal ->  Recommend image size 1200px × 674px 
-  // Layout vertical -> Recommend image size 760px x 1360px 
-  // Video format mp4 / webm 
-    items: [ 
-      { 
-        type: PostersItemDataType.Video, 
+  posters: {
+    layout: PostersLayout.Horizontal,
+    items: [
+      {
+        type: PostersItemDataType.Video,
         image: 'https://statics.karatgalaxy.io/pancakeswap-game-landing/movie_v1.png',
-        video: 'https://statics.karatgalaxy.io/pancakeswap-game-landing/movie.mp4', // format mp4 / webm 
+        video: 'https://statics.karatgalaxy.io/pancakeswap-game-landing/movie.mp4',
       },
       {
         type: PostersItemDataType.Image,
@@ -74,14 +65,11 @@ export const karatGalaxy: GameType = {
         type: PostersItemDataType.Image,
         image: 'https://statics.karatgalaxy.io/pancakeswap-game-landing/6.png',
       },
-    ], 
-  }, 
-  playlist: [], 
-  socialMedia: { 
+    ],
+  },
+  playlist: [],
+  socialMedia: {
     telegram: 'https://t.me/PancakeSwap/3466527',
-    discord: 'https://discord.gg/UtgWsTqu', 
-  }, 
+    discord: 'https://discord.gg/UtgWsTqu',
+  },
 }
-
-
-
