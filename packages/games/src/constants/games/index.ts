a236a562b7733b0ef2794082731faa34c0228cf5
fcwrsmall <68272192+fcwrsmall@users.noreply.github.com>
2024-07-30 15:19:51 +0800

    feat: List new game karat galaxy (#10221)
    karat galaxy
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    The focus of this PR is to add a new game called `Karat Galaxy` to the
    list of top games and update the default games list in the games
    constants.
    
    ### Detailed summary
    - Added `karatGalaxy` game details to `constants/games/karatGalaxy.ts`
    - Updated `TOP_GAMES_LIST` to include `karatGalaxy`
    - Updated `DEFAULT_GAMES_LIST` to include `pancakeProtectors,
    shadowRealm, nemesisDownfall, binaryX`
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your
    question}`
    
    <!-- end pr-codex -->
    
    ---------
    
    Co-authored-by: git <git@gmail.com>

--- packages/games/src/constants/games/index.ts
+++ packages/games/src/constants/games/index.ts
@@ -5,9 +5,10 @@ import { binaryX } from './binaryX'
 import { nemesisDownfall } from './nemesisDownfall'
 import { pancakeProtectors } from './pancakeProtectors'
 import { shadowRealm } from './shadowRealm'
+import { karatGalaxy } from './karatGalaxy'
 
-export const TOP_GAMES_LIST: GameType[] = [pancakeProtectors]
+export const TOP_GAMES_LIST: GameType[] = [karatGalaxy]
 
-export const DEFAULT_GAMES_LIST: GameType[] = [shadowRealm, nemesisDownfall, binaryX]
+export const DEFAULT_GAMES_LIST: GameType[] = [pancakeProtectors, shadowRealm, nemesisDownfall, binaryX]
 
 export const GAMES_LIST = [...TOP_GAMES_LIST, ...shuffle(DEFAULT_GAMES_LIST)]
