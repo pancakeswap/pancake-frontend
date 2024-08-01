04cd25d29a7b1ad9990372725c24780d0c499a2b
chefmomota <momota@pancakeswap.com>
2024-08-01 15:52:55 +0800

    feat: Hide error message

--- apps/gamification/views/Profile/components/ProfileHeader.tsx
+++ apps/gamification/views/Profile/components/ProfileHeader.tsx
@@ -144,12 +144,11 @@ const ProfileHeader: React.FC<React.PropsWithChildren<HeaderProps>> = ({
             isFetchingRef.current = false
           }
         } catch (error) {
-          toastError(
-            `Failed to connect ${social}: ${
-              error instanceof Error && error?.message ? error.message : JSON.stringify(error)
-            }`,
-          )
-          console.error(`Connect ${social} error: `, error)
+          const errorMessage = error instanceof Error && error?.message ? error.message : JSON.stringify(error)
+          if (errorMessage !== 'connection.connector.getChainId is not a function') {
+            toastError(`Failed to connect ${social}: ${errorMessage}`)
+            console.error(`Connect ${social} error: `, error)
+          }
         } finally {
           setTimeout(() => setIsFetchingApi(false), 1000)
         }
