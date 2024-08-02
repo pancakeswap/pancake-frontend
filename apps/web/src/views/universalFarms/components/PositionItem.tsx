f56d2d73f73ef388ca1f0e3a259f062f40466c42
ChefEric <173023571+chef-eric@users.noreply.github.com>
2024-08-02 18:24:55 +0800

    fix: correct usd price of position & refactor code by diff protocol (#10305)
    <!--
    Before opening a pull request, please read the [contributing
    guidelines](https://github.com/pancakeswap/pancake-frontend/blob/develop/CONTRIBUTING.md)
    first
    -->
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    This PR introduces changes related to position status handling and UI
    enhancements in the Universal Farms feature.
    
    ### Detailed summary
    - Added a new `CAKE earned` translation key
    - Updated `hooks.ts` to remove unused variables and adjust object
    destructuring
    - Added a new `StakeModal` component with styled elements and
    functionality
    - Updated `PositionPage.tsx` to handle V3 position status and filter
    positions based on status
    
    > The following files were skipped due to too many changes:
    `apps/web/src/views/universalFarms/PositionPage.tsx`,
    `apps/web/src/views/universalFarms/components/PositionItem.tsx`
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your
    question}`
    
    <!-- end pr-codex -->

--- apps/web/src/views/universalFarms/components/PositionItem.tsx
+++ apps/web/src/views/universalFarms/components/PositionItem.tsx
@@ -1,9 +1,10 @@
+import BigNumber from 'bignumber.js'
+import { Button, Column, FeeTier, Flex, Row, Skeleton, SortArrow, Tag, Text, useModalV2 } from '@pancakeswap/uikit'
 import { Protocol } from '@pancakeswap/farms'
-import { useTheme } from '@pancakeswap/hooks'
+import { ERC20Token } from '@pancakeswap/sdk'
 import { useTranslation } from '@pancakeswap/localization'
 import { Currency, CurrencyAmount, Price, Token } from '@pancakeswap/swap-sdk-core'
 import { unwrappedToken } from '@pancakeswap/tokens'
-import { Column, FeeTier, Flex, Row, Skeleton, SortArrow, Tag, Text } from '@pancakeswap/uikit'
 import { Bound, FiatNumberDisplay } from '@pancakeswap/widgets-internal'
 import { RangeTag } from 'components/RangeTag'
 import { TokenPairImage } from 'components/TokenImage'
@@ -14,7 +15,15 @@ import { useExtraV3PositionInfo } from 'state/farmsV4/hooks'
 import type { PositionDetail, StableLPDetail, V2LPDetail } from 'state/farmsV4/state/accountPositions/type'
 import { type PoolInfo } from 'state/farmsV4/state/type'
 import styled from 'styled-components'
+import { useTheme } from '@pancakeswap/hooks'
+import { useCurrencyUsdPrice } from 'hooks/useCurrencyUsdPrice'
+import useFarmV3Actions from 'views/Farms/hooks/v3/useFarmV3Actions'
+import { logGTMClickStakeFarmEvent } from 'utils/customGTMEventTracking'
+import { formatBigInt } from '@pancakeswap/utils/formatBalance'
+import { useCakePrice } from 'hooks/useCakePrice'
+import { useStakedPositionsByUser } from 'state/farmsV3/hooks'
 import { PoolApyButton } from './PoolApyButton'
+import { StakeModal } from './StakeModal'
 
 const Container = styled(Flex)`
   min-width: 576px;
@@ -30,11 +39,11 @@ const Container = styled(Flex)`
 `
 
 const DetailsContainer = styled(Flex)`
-  flex-direction: column;
+  flex-direction: row;
+  justify-content: space-between;
+  flex: 1;
   color: ${({ theme }) => theme.colors.textSubtle};
 `
-
-const ActionContainer = styled(Flex)``
 
 const TagCell = styled(Flex)`
   position: absolute;
@@ -44,8 +53,8 @@ const TagCell = styled(Flex)`
 `
 
 interface IPriceRangeProps {
-  currencyBase: Currency
-  currencyQuote: Currency
+  currency0: Currency
+  currency1: Currency
   priceUpper?: Price<Token, Token>
   priceLower?: Price<Token, Token>
   tickAtLimit: {
@@ -54,7 +63,32 @@ interface IPriceRangeProps {
   }
 }
 
-const PriceRange = memo(({ currencyBase, currencyQuote, priceLower, priceUpper, tickAtLimit }: IPriceRangeProps) => {
+const useTotalPriceUSD = ({
+  currency0,
+  currency1,
+  amount0,
+  amount1,
+}: {
+  currency0: Currency | null | undefined
+  currency1: Currency | null | undefined
+  amount0?: CurrencyAmount<ERC20Token>
+  amount1?: CurrencyAmount<ERC20Token>
+}) => {
+  const { data: currency0PriceFromApi } = useCurrencyUsdPrice(currency0, {
+    enabled: !!currency0,
+  })
+  const { data: currency1PriceFromApi } = useCurrencyUsdPrice(currency1, {
+    enabled: !!currency1,
+  })
+  return useMemo(() => {
+    return (
+      Number(currency0PriceFromApi) * Number(amount0 ? amount0.toExact() : 0) +
+      Number(currency1PriceFromApi) * Number(amount1 ? amount1.toExact() : 0)
+    )
+  }, [amount0, amount1, currency0PriceFromApi, currency1PriceFromApi])
+}
+
+const PriceRange = memo(({ currency1, currency0, priceLower, priceUpper, tickAtLimit }: IPriceRangeProps) => {
   const [priceBaseInvert, setPriceBaseInvert] = useState(false)
   const {
     t,
@@ -89,8 +123,8 @@ const PriceRange = memo(({ currencyBase, currencyQuote, priceLower, priceUpper, 
         ),
       })}{' '}
       {t('of %assetA% per %assetB%', {
-        assetA: priceBaseInvert ? currencyBase.symbol : currencyQuote.symbol,
-        assetB: priceBaseInvert ? currencyQuote.symbol : currencyBase.symbol,
+        assetA: priceBaseInvert ? currency1.symbol : currency0.symbol,
+        assetB: priceBaseInvert ? currency0.symbol : currency1.symbol,
       })}
       <SortArrow
         color="textSubtle"
@@ -102,27 +136,24 @@ const PriceRange = memo(({ currencyBase, currencyQuote, priceLower, priceUpper, 
   ) : null
 })
 
-export const PositionV2Item = memo(({ data, pool }: { data: V2LPDetail; pool?: PoolInfo }) => {
+export const PositionV2Item = memo(({ data }: { data: V2LPDetail; pool?: PoolInfo }) => {
   const { pair, deposited0, deposited1 } = data
 
   const unwrappedToken0 = unwrappedToken(pair.token0)
   const unwrappedToken1 = unwrappedToken(pair.token1)
-
-  const totalPriceUSD = useMemo(() => {
-    if (!pool?.token0Price) {
-      return 0
-    }
-    return (
-      Number(pool.token0Price) * Number(deposited1.toExact()) +
-      Number(pool.token1Price) * Number(pool.token0Price) * Number(deposited0.toExact())
-    )
-  }, [pool?.token0Price, pool?.token1Price, deposited0, deposited1])
+  const totalPriceUSD = useTotalPriceUSD({
+    currency0: unwrappedToken0,
+    currency1: unwrappedToken1,
+    amount0: deposited0,
+    amount1: deposited1,
+  })
 
   return (
     <PositionItemDetail
+      chainId={data.pair.chainId}
       totalPriceUSD={totalPriceUSD}
-      currencyBase={unwrappedToken1}
-      currencyQuote={unwrappedToken0}
+      currency0={unwrappedToken0}
+      currency1={unwrappedToken1}
       removed={false}
       outOfRange={false}
       protocol={data.protocol}
@@ -134,24 +165,22 @@ export const PositionV2Item = memo(({ data, pool }: { data: V2LPDetail; pool?: P
   )
 })
 
-export const PositionStableItem = memo(({ data, pool }: { data: StableLPDetail; pool?: PoolInfo }) => {
+export const PositionStableItem = memo(({ data }: { data: StableLPDetail; pool?: PoolInfo }) => {
   const { pair, deposited0, deposited1 } = data
 
-  const totalPriceUSD = useMemo(() => {
-    if (!pool?.token0Price) {
-      return 0
-    }
-    return (
-      Number(pool.token0Price) * Number(deposited1.toExact()) +
-      Number(pool.token1Price) * Number(pool.token0Price) * Number(deposited0.toExact())
-    )
-  }, [pool?.token0Price, pool?.token1Price, deposited0, deposited1])
+  const totalPriceUSD = useTotalPriceUSD({
+    currency0: pair.token0,
+    currency1: pair.token1,
+    amount0: deposited0,
+    amount1: deposited1,
+  })
 
   return (
     <PositionItemDetail
+      chainId={data.pair.token1.chainId}
       totalPriceUSD={totalPriceUSD}
-      currencyBase={pair.token1}
-      currencyQuote={pair.token0}
+      currency0={pair.token0}
+      currency1={pair.token1}
       removed={false}
       outOfRange={false}
       protocol={data.protocol}
@@ -166,24 +195,21 @@ interface IPositionV3ItemProps {
   pool: PoolInfo | undefined
 }
 export const PositionV3Item = memo(({ data, pool }: IPositionV3ItemProps) => {
-  const { currencyBase, currencyQuote, removed, outOfRange, priceUpper, priceLower, tickAtLimit, position } =
+  const { currency0, currency1, removed, outOfRange, priceUpper, priceLower, tickAtLimit, position } =
     useExtraV3PositionInfo(data)
 
-  const totalPriceUSD = useMemo(() => {
-    if (!position || !pool) {
-      return 0
-    }
-    return (
-      Number(pool.token0Price) * Number(position.amount1.toExact()) +
-      Number(pool.token1Price) * Number(pool.token0Price) * Number(position.amount0.toExact())
-    )
-  }, [position, pool])
+  const totalPriceUSD = useTotalPriceUSD({
+    currency0,
+    currency1,
+    amount0: position?.amount0,
+    amount1: position?.amount1,
+  })
 
   const desc =
-    currencyBase && currencyQuote ? (
+    currency1 && currency0 ? (
       <PriceRange
-        currencyBase={currencyBase}
-        currencyQuote={currencyQuote}
+        currency0={currency0}
+        currency1={currency1}
         priceLower={priceLower}
         priceUpper={priceUpper}
         tickAtLimit={tickAtLimit}
@@ -192,14 +218,15 @@ export const PositionV3Item = memo(({ data, pool }: IPositionV3ItemProps) => {
 
   return (
     <PositionItemDetail
+      chainId={data.chainId}
       link={`/liquidity/${data.tokenId}`}
       pool={pool}
       totalPriceUSD={totalPriceUSD}
       amount0={position?.amount0}
       amount1={position?.amount1}
       desc={desc}
-      currencyBase={currencyBase}
-      currencyQuote={currencyQuote}
+      currency0={currency0}
+      currency1={currency1}
       removed={removed}
       outOfRange={outOfRange}
       fee={data.fee}
@@ -210,9 +237,31 @@ export const PositionV3Item = memo(({ data, pool }: IPositionV3ItemProps) => {
   )
 })
 
+const DetailInfoTitle = styled.div`
+  display: flex;
+  gap: 8px;
+  font-size: 16px;
+  font-weight: 600;
+`
+
+const DetailInfoDesc = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  font-size: 12px;
+  font-weight: 400;
+`
+
+const DetailInfoLabel = styled(Text)`
+  color: ${({ theme }) => theme.colors.textSubtle};
+  font-weight: 600;
+  font-size: 12px;
+`
+
 interface IPositionItemDetailProps {
-  currencyBase?: Currency
-  currencyQuote?: Currency
+  chainId: number
+  currency0?: Currency
+  currency1?: Currency
   removed: boolean
   outOfRange: boolean
   desc?: React.ReactNode
@@ -241,8 +290,8 @@ export const PositionItemSkeleton = () => {
 
 export const PositionItemDetail = ({
   link,
-  currencyBase,
-  currencyQuote,
+  currency0,
+  currency1,
   removed,
   outOfRange,
   desc,
@@ -257,53 +306,124 @@ export const PositionItemDetail = ({
 }: IPositionItemDetailProps) => {
   const { t } = useTranslation()
   const { theme } = useTheme()
+  const { onStake, onHarvest, attemptingTxn } = useFarmV3Actions({
+    tokenId: tokenId?.toString() ?? '',
+    onDone: () => {},
+  })
+  const { isOpen, onDismiss, onOpen } = useModalV2()
 
-  if (!(currencyBase && currencyQuote)) {
+  const cakePrice = useCakePrice()
+  const stackedTokenId = useMemo(() => (tokenId ? [tokenId] : []), [tokenId])
+  const {
+    tokenIdResults: [pendingCake],
+  } = useStakedPositionsByUser(stackedTokenId)
+  const earningsAmount = useMemo(() => +formatBigInt(pendingCake || 0n, 4), [pendingCake])
+  const earningsBusd = useMemo(() => {
+    return new BigNumber(earningsAmount).times(cakePrice.toString()).toNumber()
+  }, [cakePrice, earningsAmount])
+
+  const handleStakeAndCheckInactive = useCallback(
+    async (e: React.MouseEvent) => {
+      e.preventDefault()
+      logGTMClickStakeFarmEvent()
+      if (outOfRange && !isStaked) {
+        onOpen()
+      } else {
+        await onStake()
+      }
+    },
+    [isStaked, onStake, outOfRange, onOpen],
+  )
+
+  const handleStake = useCallback(
+    async (e: React.MouseEvent) => {
+      e.preventDefault()
+      logGTMClickStakeFarmEvent()
+      await onStake()
+    },
+    [onStake],
+  )
+
+  const handleHarvest = useCallback(
+    async (e: React.MouseEvent) => {
+      e.preventDefault()
+      onHarvest()
+    },
+    [onHarvest],
+  )
+
+  if (!(currency0 && currency1)) {
     return <PositionItemSkeleton />
   }
 
+  const detailInfo = (
+    <>
+      <DetailInfoTitle>
+        <Text bold>{`${currency0.symbol} / ${currency1.symbol} LP`}</Text>
+        {tokenId ? <Text color="textSubtle">(#{tokenId.toString()})</Text> : null}
+        <FeeTier type={protocol} fee={fee} />
+        <TagCell>
+          {isStaked && (
+            <Tag variant="primary60" mr="8px">
+              {t('Farming')}
+            </Tag>
+          )}
+          {protocol === Protocol.V3 && <RangeTag lowContrast removed={removed} outOfRange={outOfRange} />}
+        </TagCell>
+      </DetailInfoTitle>
+      <DetailInfoDesc>
+        <Row>{desc}</Row>
+        <Row gap="sm">
+          <FiatNumberDisplay
+            prefix="~"
+            value={totalPriceUSD}
+            style={{ color: theme.colors.textSubtle, fontSize: '12px' }}
+            showFullDigitsTooltip={false}
+          />
+          ({amount0 ? amount0.toFixed(6) : 0} {currency0?.symbol ?? '-'} / {amount1 ? amount1.toFixed(6) : 0}{' '}
+          {currency1?.symbol ?? '-'})
+        </Row>
+        <Row gap="8px">
+          <DetailInfoLabel>APR: </DetailInfoLabel>
+          {pool ? <PoolApyButton pool={pool} /> : <Skeleton width={60} />}
+        </Row>
+        {earningsAmount > 0 && (
+          <Row gap="8px">
+            <DetailInfoLabel>
+              {t('CAKE earned')}: {earningsAmount} (~${earningsBusd})
+            </DetailInfoLabel>
+          </Row>
+        )}
+      </DetailInfoDesc>
+    </>
+  )
   const content = (
     <Container>
       <TokenPairImage
         width={48}
         height={48}
         variant="inverted"
-        primaryToken={currencyQuote}
-        secondaryToken={currencyBase.wrapped}
+        primaryToken={currency0}
+        secondaryToken={currency1.wrapped}
       />
       <DetailsContainer>
-        <Row gap="8px">
-          <Text bold>{`${currencyQuote.symbol} / ${currencyBase.symbol} LP`}</Text>
-          {tokenId ? <Text color="textSubtle">(#{tokenId.toString()})</Text> : null}
-          <FeeTier type={protocol} fee={fee} />
-          <TagCell>
-            {isStaked && (
-              <Tag variant="primary60" mr="8px">
-                {t('Farming')}
-              </Tag>
-            )}
-            {protocol === Protocol.V3 && <RangeTag lowContrast removed={removed} outOfRange={outOfRange} />}
-          </TagCell>
-        </Row>
-        <Row>{desc}</Row>
-        <Row gap="sm">
-          <FiatNumberDisplay
-            prefix="~"
-            value={totalPriceUSD}
-            style={{ color: theme.colors.textSubtle }}
-            showFullDigitsTooltip={false}
-          />
-          ({amount0 ? amount0.toFixed(6) : 0} {currencyQuote?.symbol ?? '-'} / {amount1 ? amount1.toFixed(6) : 0}{' '}
-          {currencyBase?.symbol ?? '-'})
-        </Row>
-
-        {pool ? (
-          <Row>
-            <Column style={{ fontWeight: 600 }}>APR: </Column>
-            <PoolApyButton pool={pool} />
-          </Row>
-        ) : null}
+        <Column gap="8px">{detailInfo}</Column>
+        <Column justifyContent="flex-end">
+          {!isStaked && !removed ? (
+            <Button scale="md" width={['100px']} style={{ alignSelf: 'center' }} onClick={handleStakeAndCheckInactive}>
+              {t('Stake')}
+            </Button>
+          ) : null}
+          {isStaked && !removed ? (
+            <Button width={['100px']} scale="md" disabled={attemptingTxn} onClick={handleHarvest}>
+              {attemptingTxn ? t('Harvesting') : t('Harvest')}
+            </Button>
+          ) : null}
+        </Column>
       </DetailsContainer>
+      <StakeModal isOpen={isOpen} staking={outOfRange && !isStaked} onStake={handleStake} onDismiss={onDismiss}>
+        {detailInfo}
+      </StakeModal>
     </Container>
   )
 
