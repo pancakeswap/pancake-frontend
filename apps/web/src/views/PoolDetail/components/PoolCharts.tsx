0d9e87b1b303c1f498605185102bf821adf7771f
Chef Jerry <jerry@pancakeswap.com>
2024-08-05 23:50:50 +0800

    feat: TVL Chart

--- apps/web/src/views/PoolDetail/components/PoolCharts.tsx
+++ apps/web/src/views/PoolDetail/components/PoolCharts.tsx
@@ -5,6 +5,7 @@ import { PoolInfo } from 'state/farmsV4/state/type'
 import styled from 'styled-components'
 import { useRouterQuery } from '../hooks/useRouterQuery'
 import { ChartLiquidity } from './ChartLiquidity'
+import { ChartTVL } from './ChartTVL'
 import { ChartVolume } from './ChartVolume'
 
 enum PoolChart {
@@ -22,6 +23,10 @@ const StyledTabMenuContainer = styled(Container)`
   margin-left: -16px;
 `
 
+const CustomTab = styled(Tab)<{ $enable?: boolean }>`
+  ${({ $enable }) => !$enable && 'display: none;'}
+`
+
 type PoolChartsProps = {
   poolInfo?: PoolInfo | null
 }
@@ -29,6 +34,7 @@ export const PoolCharts: React.FC<PoolChartsProps> = ({ poolInfo }) => {
   const { t } = useTranslation()
   const { id } = useRouterQuery()
   const isV4 = useMemo(() => poolInfo?.protocol === 'v4bin', [poolInfo])
+  const isV3 = useMemo(() => poolInfo?.protocol === 'v3', [poolInfo])
   const [chart, setChart] = useState<PoolChart>(PoolChart.Volume)
   return (
     <Column>
@@ -40,24 +46,20 @@ export const PoolCharts: React.FC<PoolChartsProps> = ({ poolInfo }) => {
           <Tab key={PoolChart.Liquidity}>
             <Box px="8px">{t('Liquidity')}</Box>
           </Tab>
-          <Tab key={PoolChart.Fees}>
+
+          <CustomTab key={PoolChart.Fees} $enable={isV3}>
             <Box px="8px">{t('Fees')}</Box>
-          </Tab>
-          <Tab key={PoolChart.TVL}>
+          </CustomTab>
+          <CustomTab key={PoolChart.TVL} $enable={isV3}>
             <Box px="8px">{t('TVL')}</Box>
-          </Tab>
-          <>
-            {isV4 ? (
-              <Tab key={PoolChart.Donation}>
-                <Box px="8px">{t('Donation')}</Box>
-              </Tab>
-            ) : null}
-            {isV4 ? (
-              <Tab key={PoolChart.DynamicFee}>
-                <Box px="8px">{t('Dynamic Fee')}</Box>
-              </Tab>
-            ) : null}
-          </>
+          </CustomTab>
+
+          <CustomTab key={PoolChart.Donation} $enable={isV4}>
+            <Box px="8px">{t('Donation')}</Box>
+          </CustomTab>
+          <CustomTab key={PoolChart.DynamicFee} $enable={isV4}>
+            <Box px="8px">{t('Dynamic Fee')}</Box>
+          </CustomTab>
         </TabMenu>
       </StyledTabMenuContainer>
       <Card>
@@ -65,7 +67,7 @@ export const PoolCharts: React.FC<PoolChartsProps> = ({ poolInfo }) => {
           {chart === PoolChart.Volume ? <ChartVolume address={id} poolInfo={poolInfo} /> : null}
           {chart === PoolChart.Liquidity ? <ChartLiquidity address={id} poolInfo={poolInfo} /> : null}
           {chart === PoolChart.Fees ? <div>fee</div> : null}
-          {chart === PoolChart.TVL ? <div>TVL</div> : null}
+          {chart === PoolChart.TVL ? <ChartTVL address={id} poolInfo={poolInfo} /> : null}
         </CardBody>
       </Card>
     </Column>
