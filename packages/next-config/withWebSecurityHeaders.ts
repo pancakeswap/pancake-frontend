29e776b3a5c66321136259a3ef8d2b56a4e216dd
chefjackson <116779127+chefjackson@users.noreply.github.com>
2024-08-06 12:18:36 +0800

    fix: TG connect (#10332)
    <!--
    Before opening a pull request, please read the [contributing
    guidelines](https://github.com/pancakeswap/pancake-frontend/blob/develop/CONTRIBUTING.md)
    first
    -->
    
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    The focus of this PR is to update headers handling in Next.js
    configuration and fix a window location redirect issue.
    
    ### Detailed summary
    - Added header handling for Cross-Origin-Opener-Policy
    - Updated handling of headers in Next.js configuration
    - Fixed window location redirect issue in a TypeScript file
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your
    question}`
    
    <!-- end pr-codex -->

--- packages/next-config/withWebSecurityHeaders.ts
+++ packages/next-config/withWebSecurityHeaders.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from 'next'
 
 type Headers = Awaited<ReturnType<NonNullable<NextConfig['headers']>>>
 
+type Header = Headers[0]
+
+type HeaderArray = Headers[0]['headers']
+
 // function createCSP() {
 //   const IFRAME_WHITE_LIST = ['https://*.safe.global']
 //
@@ -43,10 +47,49 @@ export function withWebSecurityHeaders(config: NextConfig): NextConfig {
     ]
 
     if (typeof originalHeaders !== 'function') {
-      return [...headers, ...originalHeaders]
+      return mergeHeaders(headers, originalHeaders)
     }
     const customHeaders = await originalHeaders()
-    return [...headers, ...customHeaders]
+    return mergeHeaders(headers, customHeaders)
   }
   return config
+}
+
+function mergeHeaders(a: Headers, b: Headers): Headers {
+  const headerMap = new Map<string, Header>()
+
+  a.forEach((header) => {
+    headerMap.set(header.source, { ...header })
+  })
+
+  b.forEach((header) => {
+    const existingHeader = headerMap.get(header.source)
+
+    if (existingHeader) {
+      const mergedHeaders = mergeHeadersArray(existingHeader.headers, header.headers)
+      headerMap.set(header.source, {
+        ...existingHeader,
+        ...header,
+        headers: mergedHeaders,
+      })
+    } else {
+      headerMap.set(header.source, { ...header })
+    }
+  })
+
+  return Array.from(headerMap.values())
+}
+
+function mergeHeadersArray(aHeaders: HeaderArray, bHeaders: HeaderArray): HeaderArray {
+  const headerMap = new Map<string, string>()
+
+  aHeaders.forEach((header) => {
+    headerMap.set(header.key, header.value)
+  })
+
+  bHeaders.forEach((header) => {
+    headerMap.set(header.key, header.value)
+  })
+
+  return Array.from(headerMap.entries()).map(([key, value]) => ({ key, value }))
 }
