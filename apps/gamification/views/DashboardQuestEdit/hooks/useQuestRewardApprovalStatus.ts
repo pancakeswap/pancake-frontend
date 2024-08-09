77cebc7eb77f376107cfe2ed00fef11807991c81
memoyil <2213635+memoyil@users.noreply.github.com>
2024-08-09 18:45:11 +0200

    feat: Port trigger block data after trx to gamifications

--- apps/gamification/views/DashboardQuestEdit/hooks/useQuestRewardApprovalStatus.ts
+++ apps/gamification/views/DashboardQuestEdit/hooks/useQuestRewardApprovalStatus.ts
@@ -1,9 +1,10 @@
 import { ChainId } from '@pancakeswap/sdk'
 import { BIG_ZERO } from '@pancakeswap/utils/bigNumber'
+import { useReadContract } from '@pancakeswap/wagmi'
 import BigNumber from 'bignumber.js'
 import { getQuestRewardAddress } from 'utils/addressHelpers'
 import { Address, erc20Abi } from 'viem'
-import { useAccount, useReadContract } from 'wagmi'
+import { useAccount } from 'wagmi'
 
 export const useQuestRewardApprovalStatus = (tokenAddress: Address, chainId: ChainId) => {
   const { address: account } = useAccount()
