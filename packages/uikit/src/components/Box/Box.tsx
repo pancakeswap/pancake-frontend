1a2b90ecbbd57f74be314c8e78cdbc1d82738081
BingBong <bingbong@pancakeswap.com>
2024-08-14 00:22:49 +0100

    chore use lazy

--- packages/uikit/src/components/Box/Box.tsx
+++ packages/uikit/src/components/Box/Box.tsx
@@ -1,10 +1,10 @@
-import { motion, Variants } from "framer-motion";
+import { m as motion, Variants } from "framer-motion";
 import shouldForwardProp from "@styled-system/should-forward-prop";
 import { styled } from "styled-components";
 import { background, border, layout, position, space, color } from "styled-system";
 import { BoxProps } from "./types";
 
-export { AnimatePresence, type LazyMotion, domAnimation } from "framer-motion";
+export { type AnimatePresence, LazyMotion, domAnimation } from "framer-motion";
 export type MotionVariants = Variants;
 
 export const MotionBox = styled(motion.div).withConfig({})<BoxProps>`
--- packages/uikit/src/components/Box/index.tsx
+++ packages/uikit/src/components/Box/index.tsx
@@ -1,4 +1,4 @@
-export { AnimatePresence, default as Box, MotionBox } from "./Box";
+export { default as Box, LazyMotion, MotionBox } from "./Box";
 export { default as Flex, MotionFlex } from "./Flex";
 export { default as Grid, MotionGrid } from "./Grid";
 export type { BoxProps, FlexProps, GridProps } from "./types";
