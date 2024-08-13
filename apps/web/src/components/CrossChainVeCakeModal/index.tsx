0511fcfdc24d1598327362e255dabcf4a8d13652
memoyil <2213635+memoyil@users.noreply.github.com>
2024-08-13 10:10:54 +0200

    fix: Build

--- apps/web/src/components/CrossChainVeCakeModal/index.tsx
+++ apps/web/src/components/CrossChainVeCakeModal/index.tsx
@@ -192,7 +192,7 @@ export const CrossChainVeCakeModal: React.FC<{
       if (!account || !veCakeSenderV2Contract || !chainId || !isInitialized) return
       let syncFee = BigInt(
         new BigNumber(CROSS_CHAIN_CONFIG[chainId].layerZeroFee.toString())
-          .times(CROSS_CHAIN_CONFIG[chainId].layerZeroDeeBufferTimes ?? 1.1)
+          .times(CROSS_CHAIN_CONFIG[chainId].layerZeroFeeBufferTimes ?? 1.1)
           .toNumber()
           .toFixed(0),
       )
@@ -205,7 +205,7 @@ export const CrossChainVeCakeModal: React.FC<{
         if (feeData.nativeFee !== 0n) {
           syncFee = BigInt(
             new BigNumber(feeData.nativeFee.toString())
-              .times(CROSS_CHAIN_CONFIG[chainId].layerZeroDeeBufferTimes ?? 1.1)
+              .times(CROSS_CHAIN_CONFIG[chainId].layerZeroFeeBufferTimes ?? 1.1)
               .toNumber()
               .toFixed(0),
           )
