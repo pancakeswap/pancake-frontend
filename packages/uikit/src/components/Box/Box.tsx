5e8d27ec74fe7893f3e794f43af14b2bbf55aedb
BingBong <bingbong@pancakeswap.com>
2024-08-13 19:00:00 +0100

    chore: distinguish between AnimatePresence type and component export

--- packages/uikit/src/components/Box/Box.tsx
+++ packages/uikit/src/components/Box/Box.tsx
@@ -1,10 +1,10 @@
-import { m as motion, Variants } from "framer-motion";
 import shouldForwardProp from "@styled-system/should-forward-prop";
+import { m as motion, Variants } from "framer-motion";
 import { styled } from "styled-components";
-import { background, border, layout, position, space, color } from "styled-system";
+import { background, border, color, layout, position, space } from "styled-system";
 import { BoxProps } from "./types";
 
-export { type AnimatePresence, type LazyMotion, domAnimation } from "framer-motion";
+export { domAnimation, type AnimatePresence as AnimatePresenceType, type LazyMotion } from "framer-motion";
 export type MotionVariants = Variants;
 
 export const MotionBox = styled(motion.div).withConfig({})<BoxProps>`
