4c64a46052a5ab32bff60898b4b10cdcc195df10
ChefEric <173023571+chef-eric@users.noreply.github.com>
2024-08-14 19:08:50 +0800

    fix: resolve cannot stake v2/ss pool when cross chain

--- apps/web/src/hooks/useTokenAllowance.ts
+++ apps/web/src/hooks/useTokenAllowance.ts
@@ -16,7 +16,28 @@ function useTokenAllowance(
   refetch: () => Promise<QueryObserverResult<bigint>>
 } {
   const { chainId } = useActiveChainId()
+  return useTokenAllowanceByChainId({
+    chainId,
+    token,
+    owner,
+    spender,
+  })
+}
 
+export function useTokenAllowanceByChainId({
+  chainId,
+  token,
+  owner,
+  spender,
+}: {
+  chainId: number
+  token?: Token
+  owner?: string
+  spender?: string
+}): {
+  allowance: CurrencyAmount<Token> | undefined
+  refetch: () => Promise<QueryObserverResult<bigint>>
+} {
   const inputs = useMemo(() => [owner, spender] as [`0x${string}`, `0x${string}`], [owner, spender])
 
   const { data: allowance, refetch } = useQuery({
