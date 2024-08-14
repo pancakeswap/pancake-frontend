1a24f7ac6521f8ab1549856c02ec3fba331b6fcd
Chef Jerry <jerry@pancakeswap.com>
2024-08-14 14:12:02 +0800

    fix: apr fetch

--- apps/web/src/views/PoolDetail/PoolDetail.tsx
+++ apps/web/src/views/PoolDetail/PoolDetail.tsx
@@ -1,12 +1,9 @@
 import { AutoColumn } from '@pancakeswap/uikit'
 import Page from 'components/Layout/Page'
-import { usePoolAprUpdater } from 'state/farmsV4/hooks'
 import { BreadcrumbNav } from './components/BreadcrumbNav'
 import { PoolInfo } from './components/PoolInfo'
 
 export const PoolDetail: React.FC = () => {
-  usePoolAprUpdater()
-
   return (
     <Page>
       <AutoColumn gap="48px">
