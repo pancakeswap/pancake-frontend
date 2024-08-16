dbd347443089c5385657dbb44771f0c48452faa4
chefjackson <116779127+chefjackson@users.noreply.github.com>
2024-08-16 15:26:37 +0800

    fix: unexpected test scope

--- packages/routing-sdk/vitest.config.ts
+++ packages/routing-sdk/vitest.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig, configDefaults } from 'vitest/config'
 
 export default defineConfig({
   test: {
     environment: 'node',
+    exclude: [...configDefaults.exclude, 'addons/**'],
   },
 })
