095e75970b1ac19113e1e018ae707722119b07d0
Chef Jerry <144641937+ChefJerry@users.noreply.github.com>
2024-08-19 20:37:10 +0800

    fix: apply apr to liquidity detail (#10449)
    <!--
    Before opening a pull request, please read the [contributing
    guidelines](https://github.com/pancakeswap/pancake-frontend/blob/develop/CONTRIBUTING.md)
    first
    -->
    
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    This PR introduces new features and updates existing components related
    to APR calculation in the farmsV4 state.
    
    ### Detailed summary
    - Added `useAccountV3Position` hook for fetching account positions
    - Updated `usePoolApr` to accept a nullable `ChainIdAddressKey`
    - Replaced `AprCalculator` with `AprCalculatorV2` in multiple files
    - Added `AprCalculatorV2` component for APR calculation with different
    variations
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your
    question}`
    
    <!-- end pr-codex -->

---
+++ apps/web/src/state/farmsV4/state/accountPositions/hooks/useAccountV3Position.ts
@@ -0,0 +1,17 @@
+import { useQuery } from '@tanstack/react-query'
+import { SLOW_INTERVAL } from 'config/constants'
+import { readPositions } from '../fetcher/v3'
+
+export const useAccountV3Position = (chainId: number, tokenId: bigint) => {
+  return useQuery({
+    queryKey: ['accountV3Position', chainId, tokenId.toString()],
+    queryFn: () => readPositions(chainId, [tokenId]),
+    select: (data) => data[0],
+    enabled: !!tokenId && !!chainId,
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
+    refetchInterval: SLOW_INTERVAL,
+    staleTime: SLOW_INTERVAL,
+  })
+}
