2a55e75a50797bd145a30b9a9e0c21b22b50baf3
BingBong <bingbong@pancakeswap.com>
2024-08-20 10:31:06 +0100

    chore: re-use web notification banner

--- apps/web/src/views/Home/components/Banners/hooks/useMultipleBannerConfig.tsx
+++ apps/web/src/views/Home/components/Banners/hooks/useMultipleBannerConfig.tsx
@@ -15,6 +15,7 @@ import { QuestBanner } from '../QuestBanner'
 import UserBanner from '../UserBanner'
 import { V4InfoBanner } from '../V4InfoBanner'
 import { VeCakeBanner } from '../VeCakeBanner'
+import WebNotificationBanner from '../WebNotificationBanner'
 import { ZksyncAirDropBanner } from '../ZksyncAirdropBanner'
 import useIsRenderCompetitionBanner from './useIsRenderCompetitionBanner'
 import useIsRenderUserBanner from './useIsRenderUserBanner'
@@ -46,6 +47,10 @@ export const useMultipleBannerConfig = () => {
       {
         shouldRender: isRenderUserBanner.shouldRender && !isRenderUserBanner.isEarningsBusdZero,
         banner: <UserBanner />,
+      },
+      {
+        shouldRender: true,
+        banner: <WebNotificationBanner />,
       },
       {
         shouldRender: true,
