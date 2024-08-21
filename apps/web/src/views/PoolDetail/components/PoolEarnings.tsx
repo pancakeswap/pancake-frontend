6f5ac38c1e4a3a30165afc54d8d3e158ba1eaa6a
Chef Jerry <144641937+ChefJerry@users.noreply.github.com>
2024-08-22 01:39:34 +0800

    fix: v3 total farms (#10494)
    <!--
    Before opening a pull request, please read the [contributing
    guidelines](https://github.com/pancakeswap/pancake-frontend/blob/develop/CONTRIBUTING.md)
    first
    -->
    
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    The focus of this PR is to enhance the calculation and display of
    earnings for Cake rewards in different pools.
    
    ### Detailed summary
    - Added `isLoading` to `useCakeEarning` hooks for better loading state
    handling
    - Improved calculation of `earningsAmount` in `useV3CakeEarning`
    - Updated `PoolEarnings` component to display `earningsAmount` and
    loading state
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your
    question}`
    
    <!-- end pr-codex -->

--- apps/web/src/views/PoolDetail/components/PoolEarnings.tsx
+++ apps/web/src/views/PoolDetail/components/PoolEarnings.tsx
@@ -1,6 +1,6 @@
 import { Protocol } from '@pancakeswap/farms'
 import { useTranslation } from '@pancakeswap/localization'
-import { AutoColumn, Text } from '@pancakeswap/uikit'
+import { AutoColumn, Skeleton, Text } from '@pancakeswap/uikit'
 import { formatNumber } from '@pancakeswap/utils/formatBalance'
 import useAccountActiveChain from 'hooks/useAccountActiveChain'
 import { useMemo } from 'react'
@@ -11,37 +11,54 @@ import { useV2CakeEarning, useV3CakeEarning } from 'views/universalFarms/hooks/u
 
 type PoolEarningsProps = {
   earningsBusd: number
+  earningsAmount: number
+  loading?: boolean
 }
 
-const PoolEarnings: React.FC<PoolEarningsProps> = ({ earningsBusd }) => {
+const PoolEarnings: React.FC<PoolEarningsProps> = ({ earningsBusd, earningsAmount, loading }) => {
   const { t } = useTranslation()
+  if (loading) {
+    return (
+      <AutoColumn gap="sm">
+        <Skeleton height={24} width={188} />
+        <Skeleton height={36} width={100} />
+      </AutoColumn>
+    )
+  }
   return (
     <AutoColumn>
       <Text color="secondary" fontWeight={600} textTransform="uppercase">
         {t('total farming earning')}
       </Text>
       <Text as="h3" fontWeight={600} fontSize={24}>
-        ${formatNumber(earningsBusd)}
+        ${formatNumber(earningsBusd, 0, 4)}
+      </Text>
+      <Text color="secondary" fontSize={12}>
+        {formatNumber(earningsAmount)} CAKE
       </Text>
     </AutoColumn>
   )
 }
 
 export const V2PoolEarnings: React.FC<{ pool: PoolInfo | null | undefined }> = ({ pool }) => {
-  const { earningsBusd } = useV2CakeEarning(pool)
+  const { earningsBusd, earningsAmount, isLoading } = useV2CakeEarning(pool)
 
-  return <PoolEarnings earningsBusd={earningsBusd} />
+  return <PoolEarnings earningsAmount={earningsAmount} earningsBusd={earningsBusd} loading={isLoading} />
 }
 
 export const V3PoolEarnings: React.FC<{ pool: PoolInfo | null | undefined }> = ({ pool }) => {
   const chainId = useChainIdByQuery()
   const { account } = useAccountActiveChain()
-  const { data } = useAccountPositionDetailByPool<Protocol.V3>(chainId, account, pool ?? undefined)
+  const { data, isLoading } = useAccountPositionDetailByPool<Protocol.V3>(
+    pool?.chainId ?? chainId,
+    account,
+    pool ?? undefined,
+  )
   const tokenIds = useMemo(() => {
     if (!data) return []
     return data.filter((item) => item.isStaked).map((item) => item.tokenId)
   }, [data])
-  const { earningsBusd } = useV3CakeEarning(tokenIds, chainId)
+  const { earningsBusd, earningsAmount } = useV3CakeEarning(tokenIds, pool?.chainId ?? chainId)
 
-  return <PoolEarnings earningsBusd={earningsBusd} />
+  return <PoolEarnings earningsAmount={earningsAmount} earningsBusd={earningsBusd} loading={isLoading} />
 }
