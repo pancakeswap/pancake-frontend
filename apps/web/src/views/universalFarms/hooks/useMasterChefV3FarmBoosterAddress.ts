c3bc7875fc515d4f67ef92296c889fe9d98180dd
Chef Jerry <144641937+ChefJerry@users.noreply.github.com>
2024-08-23 02:21:29 +0800

    feat: add unstaked farm apr (#10501)
    <!--
    Before opening a pull request, please read the [contributing
    guidelines](https://github.com/pancakeswap/pancake-frontend/blob/develop/CONTRIBUTING.md)
    first
    -->
    
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    This PR introduces new hooks for fetching user multiplier and farm
    booster addresses, and updates position APR calculations in universal
    farms view.
    
    ### Detailed summary
    - Added `useMasterChefV3FarmBoosterAddress` hook
    - Added `useUserMultiplier` hook
    - Updated position APR calculations in `useV3PositionApr`
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your
    question}`
    
    <!-- end pr-codex -->

---
+++ apps/web/src/views/universalFarms/hooks/useMasterChefV3FarmBoosterAddress.ts
@@ -0,0 +1,18 @@
+import { useQuery } from '@tanstack/react-query'
+import { getMasterChefV3Contract } from 'utils/contractHelpers'
+
+export const useMasterChefV3FarmBoosterAddress = (chainId: number) => {
+  const masterChefV3 = getMasterChefV3Contract(undefined, chainId)
+  return useQuery({
+    queryKey: ['masterChefV3', 'farmBooster', chainId],
+    queryFn: async () => {
+      if (!masterChefV3) return 0
+      return masterChefV3.read.FARM_BOOSTER()
+    },
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+    refetchOnReconnect: false,
+    refetchInterval: false,
+    staleTime: Infinity,
+  })
+}
