206fc8dda214f23f4a59291f094aca84d80bb055
chefjackson <116779127+chefjackson@users.noreply.github.com>
2024-08-24 11:44:28 +0800

    feat: ETH stable swap (#10517)
    <!--
    Before opening a pull request, please read the [contributing
    guidelines](https://github.com/pancakeswap/pancake-frontend/blob/develop/CONTRIBUTING.md)
    first
    -->
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    This PR introduces stable swap functionality on the Ethereum mainnet.
    
    ### Detailed summary
    - Added a new stable swap pool for WBTC-mBTC LP on Ethereum mainnet
    - Updated supported chain IDs to include Ethereum
    - Added Ethereum pools to the stable pool map
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your
    question}`
    
    <!-- end pr-codex -->

---
+++ packages/stable-swap-sdk/src/constants/pools/eth.ts
@@ -0,0 +1,16 @@
+import { ethereumTokens } from '@pancakeswap/tokens'
+
+import { StableSwapPool } from '../../types'
+
+export const pools: StableSwapPool[] = [
+  {
+    lpSymbol: 'WBTC-mBTC LP',
+    lpAddress: '0xaB9dEBAED21270832cCdf54a7461A8E4B133B57A',
+    token: ethereumTokens.wbtc,
+    quoteToken: ethereumTokens.mBtc,
+    stableSwapAddress: '0x95A3832889B2c3455077991b834efA2d4fA3A945',
+    infoStableSwapAddress: '0xCcd9fea6bBF1910F4c188A5BBC13D98Ea4F5f9F9',
+    stableLpFee: 0.0002,
+    stableLpFeeRateOfTotalFee: 0.5,
+  },
+]
--- packages/stable-swap-sdk/src/constants/pools/pools.ts
+++ packages/stable-swap-sdk/src/constants/pools/pools.ts
@@ -4,6 +4,7 @@ import { StableSwapPool } from '../../types'
 import { pools as bscPools } from './bsc'
 import { pools as arbPools } from './arb'
 import { pools as bscTestnetPools } from './bscTestnet'
+import { pools as ethPools } from './eth'
 
 export type StableSwapPoolMap<TChainId extends number> = {
   [chainId in TChainId]: StableSwapPool[]
@@ -16,7 +17,12 @@ export const isStableSwapSupported = (chainId: number | undefined): chainId is S
   return STABLE_SUPPORTED_CHAIN_IDS.includes(chainId)
 }
 
-export const STABLE_SUPPORTED_CHAIN_IDS = [ChainId.BSC, ChainId.BSC_TESTNET, ChainId.ARBITRUM_ONE] as const
+export const STABLE_SUPPORTED_CHAIN_IDS = [
+  ChainId.BSC,
+  ChainId.BSC_TESTNET,
+  ChainId.ARBITRUM_ONE,
+  ChainId.ETHEREUM,
+] as const
 
 export type StableSupportedChainId = (typeof STABLE_SUPPORTED_CHAIN_IDS)[number]
 
@@ -24,4 +30,5 @@ export const STABLE_POOL_MAP = {
   [ChainId.BSC]: bscPools,
   [ChainId.BSC_TESTNET]: bscTestnetPools,
   [ChainId.ARBITRUM_ONE]: arbPools,
+  [ChainId.ETHEREUM]: ethPools,
 } satisfies StableSwapPoolMap<StableSupportedChainId>
