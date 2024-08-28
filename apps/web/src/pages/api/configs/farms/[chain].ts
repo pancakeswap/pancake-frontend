58a56166537024912c05465643f33ca2e8ecf395
Chef Jerry <jerry@pancakeswap.com>
2024-08-28 14:45:27 +0800

    feat: long cache control

--- apps/web/src/pages/api/configs/farms/[chain].ts
+++ apps/web/src/pages/api/configs/farms/[chain].ts
@@ -21,7 +21,7 @@ const handler: NextApiHandler = async (req, res) => {
   }
 
   try {
-    res.setHeader('Cache-Control', 's-maxage=60, max-age=30, stale-while-revalidate=300')
+    res.setHeader('Cache-Control', `max-age=10800, s-maxage=31536000`)
 
     return res.status(200).json({
       data: JSON.parse(JSON.stringify(UNIVERSAL_FARMS.filter((farm) => farm.chainId === chainId))),
