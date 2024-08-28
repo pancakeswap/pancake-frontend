8dacf1d90bb485ba5838ee9204e6eae0360145f8
Chef Jerry <jerry@pancakeswap.com>
2024-08-28 15:10:51 +0800

    test: update snapshot

--- packages/farms/index.test.ts
+++ packages/farms/index.test.ts
@@ -4,13 +4,13 @@ import * as namedExports from './index'
 test('exports', () => {
   expect(Object.keys(namedExports)).toMatchInlineSnapshot(`
     [
-      "UNIVERSAL_BCAKEWRAPPER_FARMS",
-      "UNIVERSAL_FARMS",
-      "UNIVERSAL_FARMS_WITH_TESTNET",
       "getFarmApr",
       "getPositionFarmApr",
       "getPositionFarmAprFactor",
       "FARM_AUCTION_HOSTING_IN_SECONDS",
+      "UNIVERSAL_BCAKEWRAPPER_FARMS",
+      "UNIVERSAL_FARMS",
+      "UNIVERSAL_FARMS_WITH_TESTNET",
       "isStableFarm",
       "Protocol",
       "isActiveV3Farm",
