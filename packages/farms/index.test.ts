ca24201ec475bae082bf457c8e64523c2011c766
Chef Jerry <jerry@pancakeswap.com>
2024-08-29 15:09:04 +0800

    feat: format from legacy config to universal

--- packages/farms/index.test.ts
+++ packages/farms/index.test.ts
@@ -14,6 +14,7 @@ test('exports', () => {
       "isStableFarm",
       "Protocol",
       "isActiveV3Farm",
+      "formatUniversalFarmToSerializedFarm",
       "deserializeFarm",
       "deserializeFarmUserData",
       "deserializeFarmBCakeUserData",
--- packages/farms/src/utils.ts
+++ packages/farms/src/utils.ts
@@ -1,5 +1,90 @@
-import { FarmV3Data } from './types'
+import { LegacyRouter } from '@pancakeswap/smart-router/legacy-router'
+import { isAddressEqual } from 'viem'
+import { UNIVERSAL_BCAKEWRAPPER_FARMS } from './farms'
+import {
+  ComputedFarmConfigV3,
+  FarmV3Data,
+  SerializedClassicFarmConfig,
+  SerializedFarmConfig,
+  SerializedStableFarmConfig,
+  UniversalFarmConfig,
+  UniversalFarmConfigStableSwap,
+  UniversalFarmConfigV2,
+  UniversalFarmConfigV3,
+} from './types'
 
 export function isActiveV3Farm(farm: FarmV3Data, poolLength: number) {
   return farm.pid !== 0 && farm.multiplier !== '0X' && poolLength && poolLength >= farm.pid
+}
+
+export function formatUniversalFarmToSerializedFarm(farms: UniversalFarmConfig[]): SerializedFarmConfig[] {
+  return farms
+    .map((farm) => {
+      switch (farm.protocol) {
+        case 'stable':
+          return formatStableUniversalFarmToSerializedFarm(farm as UniversalFarmConfigStableSwap)
+        case 'v2':
+          return formatV2UniversalFarmToSerializedFarm(farm as UniversalFarmConfigV2)
+        case 'v3':
+          return formatV3UniversalFarmToSerializedFarm(farm as UniversalFarmConfigV3)
+        default:
+          return undefined
+      }
+    })
+    .filter((farm): farm is SerializedFarmConfig => farm !== undefined)
+}
+
+const formatStableUniversalFarmToSerializedFarm = (
+  farm: UniversalFarmConfigStableSwap,
+): SerializedStableFarmConfig | undefined => {
+  const { chainId, lpAddress, pid, token0, token1, stableSwapAddress } = farm
+  const stablePair = LegacyRouter.stableSwapPairsByChainId[chainId].find((pair) => {
+    return isAddressEqual(pair.stableSwapAddress, stableSwapAddress)
+  })
+  const bCakeConfig = UNIVERSAL_BCAKEWRAPPER_FARMS.find((config) => {
+    return chainId === config.chainId && isAddressEqual(config.lpAddress, lpAddress)
+  })
+
+  if (!stablePair) {
+    console.warn(`Could not find stable pair for farm with stableSwapAddress ${stableSwapAddress}`)
+    return undefined
+  }
+
+  return {
+    pid,
+    lpAddress,
+    lpSymbol: `${token0.symbol}-${token1.symbol} LP`,
+    token: token0,
+    quoteToken: token1,
+    stableSwapAddress,
+    stableLpFee: stablePair.stableLpFee,
+    stableLpFeeRateOfTotalFee: stablePair.stableLpFeeRateOfTotalFee,
+    infoStableSwapAddress: stablePair.infoStableSwapAddress,
+    bCakeWrapperAddress: bCakeConfig?.bCakeWrapperAddress,
+  }
+}
+
+const formatV2UniversalFarmToSerializedFarm = (farm: UniversalFarmConfigV2): SerializedClassicFarmConfig => {
+  const { pid, lpAddress, token0, token1 } = farm
+  return {
+    pid,
+    lpAddress,
+    lpSymbol: `${token0.symbol}-${token1.symbol} LP`,
+    token: token0,
+    quoteToken: token1,
+  }
+}
+
+const formatV3UniversalFarmToSerializedFarm = (farm: UniversalFarmConfigV3): ComputedFarmConfigV3 => {
+  const { pid, lpAddress, token0, token1, feeAmount } = farm
+  return {
+    pid,
+    lpAddress,
+    lpSymbol: `${token0.symbol}-${token1.symbol} LP`,
+    token0,
+    token1,
+    token: token0,
+    quoteToken: token1,
+    feeAmount,
+  }
 }
