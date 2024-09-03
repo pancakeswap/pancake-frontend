fae89e79dac23dd9ea9da55195202578ab5b616a
chefjackson <116779127+chefjackson@users.noreply.github.com>
2024-09-03 17:45:36 +0800

    chore: build error

--- apps/web/src/views/BuyCrypto/hooks/useDefaultAmount.ts
+++ apps/web/src/views/BuyCrypto/hooks/useDefaultAmount.ts
@@ -2,10 +2,10 @@ import { useQuery, type QueryFunction } from '@tanstack/react-query'
 import { WALLET_API } from 'config/constants/endpoints'
 import { useCallback, useEffect, useMemo } from 'react'
 import { Field } from 'state/buyCrypto/actions'
+import { createQueryKey } from 'utils/reactQuery'
 import { useBuyCryptoActionHandlers } from 'state/buyCrypto/hooks'
 import { isFiat } from '../constants'
 import { OnRampUnit } from '../types'
-import { createQueryKey } from 'utils/reactQuery'
 
 const getFiatUsdRateQueryKey = createQueryKey<'fia-usd-rate', [currencyCode: string]>('fia-usd-rate')
 type GetFiatUsdRateKey = ReturnType<typeof getFiatUsdRateQueryKey>
