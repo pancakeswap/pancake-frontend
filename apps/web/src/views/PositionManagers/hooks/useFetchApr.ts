83b0bcc80b1c0afe3e7810c964de65992dc81d6f
memoyil <2213635+memoyil@users.noreply.github.com>
2024-09-04 08:52:07 +0200

    fix: Crash in position manager fetch apr (#10610)
    <!--
    Before opening a pull request, please read the [contributing
    guidelines](https://github.com/pancakeswap/pancake-frontend/blob/develop/CONTRIBUTING.md)
    first
    -->
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    This PR improves error handling in `useFetchApr` hook by checking
    response status and throwing an error if not ok.
    
    ### Detailed summary
    - Added check for response status in `useFetchApr` hook
    - Throw error if response status is not ok
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your
    question}`
    
    <!-- end pr-codex -->

--- apps/web/src/views/PositionManagers/hooks/useFetchApr.ts
+++ apps/web/src/views/PositionManagers/hooks/useFetchApr.ts
@@ -103,8 +103,11 @@ export const useFetchApr = (timeWindows: number[]): AprData => {
           }),
         })
 
-        const result: AprDataInfo[] = await response.json()
-        return result
+        if (response.ok) {
+          const result = await response.json()
+          return Array.isArray(result) ? (result as AprDataInfo[]) : []
+        }
+        throw new Error(`Failed to fetch data: Response returned an error, Status: ${response.status}`)
       } catch (error) {
         console.error(`Fetch fetch APR API Error: ${error}`)
         return []
