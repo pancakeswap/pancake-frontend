20b619904f3df1db05dfcca81c6fb70b910faf15
Chef Jerry <144641937+ChefJerry@users.noreply.github.com>
2024-09-04 22:38:57 +0800

    Update apps/web/src/views/CakeStaking/components/LockCakeForm.tsx

--- apps/web/src/views/CakeStaking/components/LockCakeForm.tsx
+++ apps/web/src/views/CakeStaking/components/LockCakeForm.tsx
@@ -32,7 +32,7 @@ const CakeInput: React.FC<{
   const onInput = useCallback(
     (input: string) => {
       setPercent(null)
-      onUserInput(input === '.' || input === '。' ? '0.' : input)
+      onUserInput(input === '.' ? '0.' : input)
     },
     [onUserInput],
   )
