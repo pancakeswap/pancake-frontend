5adfd6a3d0f9fe6dc2070eb4759fb32aa5e5d0f9
chefmomota <momota@pancakeswap.com>
2024-09-04 18:34:09 +0800

    feat: Test token

--- apps/gamification/hooks/useDashboardSiwe.ts
+++ apps/gamification/hooks/useDashboardSiwe.ts
@@ -4,7 +4,7 @@ import { useAtom } from 'jotai'
 import { atomWithStorage, createJSONStorage, RESET } from 'jotai/utils'
 import { useCallback } from 'react'
 import { Address } from 'viem'
-import { createSiweMessage, generateSiweNonce, parseSiweMessage } from 'viem/siwe'
+import { createSiweMessage, generateSiweNonce } from 'viem/siwe'
 import { useAccount, useAccountEffect, useSignMessage } from 'wagmi'
 
 const ONE_DAY_IN_MS = 60 * 60 * 24 * 1000
@@ -59,17 +59,25 @@ export function useDashboardSiwe() {
       if (!chainId) {
         throw new Error(`Invalid chain ${chainId}`)
       }
-      if (siwe) {
-        const parsed = parseSiweMessage(siwe.message)
-        if (
-          parsed.address === currentAddress &&
-          parsed.domain === window.location.host &&
-          parsed.uri === window.location.origin &&
-          (parsed.expirationTime?.getTime() ?? 0) > Date.now()
-        ) {
-          return siwe
-        }
+      return {
+        message:
+          'localhost:3005 wants you to sign in with your Ethereum account:\n0x7544503807b4DF4fc3Db6AcF4f32Ce5E7eE8FdFb\n\n\nURI: http://localhost:3005\nVersion: 1\nChain ID: 56\nNonce: 6674c8c3d411f0d4093723ff85a68c6675adf74445cf94502d61e3a65d7a38f4e3be4b2d95ec5df29a9d16a081e2c11b\nIssued At: 2024-09-04T08:18:19.709Z\nExpiration Time: 2024-09-05T08:18:19.709Z',
+        signature:
+          '0x172c8ce4190962c1f7fdfe9ab395780a748a39fdb2c33d284e086af2187827cb604003399b28eac0d43a657b86ad0fcff3b4c43900719355cc8610e8630bd3411b',
+        jwtToken:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIweDc1NDQ1MDM4MDdiNERGNGZjM0RiNkFjRjRmMzJDZTVFN2VFOEZkRmIiLCJpYXQiOjE3MjU0Mzc5MDYsImV4cCI6MTcyNTUyNDMwNn0.IANrFnxK1d2DVof3zQtZNdcuyA8f_lzad2uIBe8mdBI',
       }
+      // if (siwe) {
+      //   const parsed = parseSiweMessage(siwe.message)
+      //   if (
+      //     parsed.address === currentAddress &&
+      //     parsed.domain === window.location.host &&
+      //     parsed.uri === window.location.origin &&
+      //     (parsed.expirationTime?.getTime() ?? 0) > Date.now()
+      //   ) {
+      //     return siwe
+      //   }
+      // }
 
       const message = createSiweMessage({
         address,
