9a73f638a06fd9e9e97fbd87d490f7f57f3cbfa1
Chef Penguin <penguin@pancakeswap.com>
2024-09-05 18:26:30 +0530

    chore: cross chain vecake card (wip)

--- apps/web/src/views/Ifos/components/IfoPoolVaultCard.tsx
+++ apps/web/src/views/Ifos/components/IfoPoolVaultCard.tsx
@@ -1,11 +1,13 @@
-import { useMemo } from 'react'
-import { Flex } from '@pancakeswap/uikit'
-import { isCakeVaultSupported } from '@pancakeswap/pools'
-import { Address } from 'viem'
 import { ChainId } from '@pancakeswap/chains'
+import { isCakeVaultSupported } from '@pancakeswap/pools'
+import { Flex } from '@pancakeswap/uikit'
+import { useMemo } from 'react'
+import { Address } from 'viem'
 
 import { useActiveChainId } from 'hooks/useActiveChainId'
 
+import { isCrossChainIfoSupportedOnly } from '@pancakeswap/ifos'
+import { CrossChainVeCakeCard } from './CrossChainVeCakeCard'
 import IfoVesting from './IfoVesting/index'
 import { VeCakeCard } from './VeCakeCard'
 
@@ -19,11 +21,19 @@ const IfoPoolVaultCard = ({ ifoBasicSaleType, ifoAddress }: Props) => {
   const { chainId } = useActiveChainId()
   const cakeVaultSupported = useMemo(() => isCakeVaultSupported(chainId), [chainId])
 
-  const vault = <VeCakeCard ifoAddress={ifoAddress} />
+  const vault = useMemo(
+    () =>
+      cakeVaultSupported ? (
+        <VeCakeCard ifoAddress={ifoAddress} />
+      ) : isCrossChainIfoSupportedOnly(chainId) ? (
+        <CrossChainVeCakeCard ifoAddress={ifoAddress} />
+      ) : null,
+    [chainId, cakeVaultSupported, ifoAddress],
+  )
 
   return (
     <Flex width="100%" maxWidth={400} alignItems="center" flexDirection="column">
-      {cakeVaultSupported ? vault : null}
+      {vault}
       <IfoVesting ifoBasicSaleType={ifoBasicSaleType} />
     </Flex>
   )
--- packages/ifos/src/constants/ifos/arb.ts
+++ packages/ifos/src/constants/ifos/arb.ts
@@ -1,3 +1,25 @@
+import { arbitrumTokens } from '@pancakeswap/tokens'
 import { BaseIfoConfig } from '../../types'
 
-export const ifos: BaseIfoConfig[] = []
+export const ifos: BaseIfoConfig[] = [
+  {
+    id: 'testCrossChainIfo',
+    version: 8,
+    address: '0x0000000000000000000000000000000000000000',
+    isActive: true,
+    name: 'Test Arbitrum IFO',
+    description: 'This is an IFO on Arbitrum to test cross-chain IFOs functionality on PancakeSwap.',
+    currency: arbitrumTokens.weth,
+    token: arbitrumTokens.arb,
+    articleUrl: 'https://pancakeswap.medium.com/',
+    campaignId: 'random-for-testing',
+    poolBasic: {
+      raiseAmount: '$10,000',
+    },
+    poolUnlimited: {
+      raiseAmount: '$90,000',
+      additionalClaimingFee: false,
+    },
+    tokenOfferingPrice: 0.1,
+  },
+]
