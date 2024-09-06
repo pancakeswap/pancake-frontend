40c5c8fa204ba638b71b1ddab90b2f4acff0c50a
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2024-09-06 17:40:39 +0800

    chore: add new swap view

---
+++ apps/web/src/views/SwapSimplify/V4Swap/index.tsx
@@ -0,0 +1,76 @@
+import { SmartRouter } from '@pancakeswap/smart-router/evm'
+import { Box } from '@pancakeswap/uikit'
+import { useMemo } from 'react'
+import { MMLiquidityWarning } from 'views/Swap/MMLinkPools/components/MMLiquidityWarning'
+import { shouldShowMMLiquidityError } from 'views/Swap/MMLinkPools/utils/exchange'
+import {
+  BuyCryptoLink,
+  FormHeader,
+  FormMain,
+  MMTradeDetail,
+  PricingAndSlippage,
+  TradeDetails,
+} from '../../Swap/V3Swap/containers'
+import { CommitButton } from '../../Swap/V3Swap/containers/CommitButton'
+import { useAllTypeBestTrade } from '../../Swap/V3Swap/hooks/useAllTypeBestTrade'
+import { useCheckInsufficientError } from '../../Swap/V3Swap/hooks/useCheckSufficient'
+
+export function V3SwapForm() {
+  const {
+    bestTrade,
+    ammTrade,
+    mmTrade,
+    isMMBetter,
+    tradeError,
+    tradeLoaded,
+    refreshTrade,
+    refreshDisabled,
+    pauseQuoting,
+    resumeQuoting,
+  } = useAllTypeBestTrade()
+
+  const ammPrice = useMemo(() => (ammTrade ? SmartRouter.getExecutionPrice(ammTrade) : undefined), [ammTrade])
+  const insufficientFundCurrency = useCheckInsufficientError(ammTrade)
+  const commitHooks = useMemo(() => {
+    return {
+      beforeCommit: pauseQuoting,
+      afterCommit: resumeQuoting,
+    }
+  }, [pauseQuoting, resumeQuoting])
+
+  return (
+    <>
+      <FormHeader onRefresh={refreshTrade} refreshDisabled={refreshDisabled} />
+      <FormMain
+        tradeLoading={isMMBetter ? false : !tradeLoaded}
+        pricingAndSlippage={
+          <PricingAndSlippage priceLoading={!tradeLoaded} price={ammPrice ?? undefined} showSlippage={!isMMBetter} />
+        }
+        inputAmount={bestTrade?.inputAmount}
+        outputAmount={bestTrade?.outputAmount}
+        swapCommitButton={
+          <CommitButton
+            trade={isMMBetter ? mmTrade : ammTrade}
+            tradeError={tradeError}
+            tradeLoaded={tradeLoaded}
+            {...commitHooks}
+          />
+        }
+      />
+
+      <BuyCryptoLink currency={insufficientFundCurrency} />
+
+      {isMMBetter ? (
+        <MMTradeDetail loaded={!mmTrade.mmOrderBookTrade.isLoading} mmTrade={mmTrade.mmTradeInfo} />
+      ) : (
+        <TradeDetails loaded={tradeLoaded} trade={ammTrade} />
+      )}
+      {(shouldShowMMLiquidityError(mmTrade?.mmOrderBookTrade?.inputError) || mmTrade?.mmRFQTrade?.error) &&
+        !ammTrade && (
+          <Box mt="5px">
+            <MMLiquidityWarning />
+          </Box>
+        )}
+    </>
+  )
+}
---
+++ apps/web/src/views/SwapSimplify/index.tsx
@@ -0,0 +1,135 @@
+import { Currency } from '@pancakeswap/sdk'
+import { BottomDrawer, Flex, useMatchBreakpoints } from '@pancakeswap/uikit'
+import { AppBody } from 'components/App'
+import { useRouter } from 'next/router'
+import { useContext, useEffect, useState } from 'react'
+
+import { useCurrency } from 'hooks/Tokens'
+import { useSwapHotTokenDisplay } from 'hooks/useSwapHotTokenDisplay'
+import { Field } from 'state/swap/actions'
+import { useSingleTokenSwapInfo, useSwapState } from 'state/swap/hooks'
+import Page from '../Page'
+import { SwapFeaturesContext } from '../Swap/SwapFeaturesContext'
+import { V3SwapForm } from '../Swap/V3Swap'
+import PriceChartContainer from '../Swap/components/Chart/PriceChartContainer'
+import { SwapSelection } from '../Swap/components/SwapSelection'
+import { StyledInputCurrencyWrapper, StyledSwapContainer } from '../Swap/styles'
+import { SwapType } from '../Swap/types'
+
+export default function V4Swap() {
+  const { query } = useRouter()
+  const { isDesktop } = useMatchBreakpoints()
+  const {
+    isChartExpanded,
+    isChartDisplayed,
+    setIsChartDisplayed,
+    setIsChartExpanded,
+    isChartSupported,
+    // isHotTokenSupported,
+  } = useContext(SwapFeaturesContext)
+  const [isSwapHotTokenDisplay, setIsSwapHotTokenDisplay] = useSwapHotTokenDisplay()
+  // const { t } = useTranslation()
+  const [firstTime, setFirstTime] = useState(true)
+
+  useEffect(() => {
+    if (firstTime && query.showTradingReward) {
+      setFirstTime(false)
+      setIsSwapHotTokenDisplay(true)
+
+      if (!isSwapHotTokenDisplay && isChartDisplayed) {
+        setIsChartDisplayed?.((currentIsChartDisplayed) => !currentIsChartDisplayed)
+      }
+    }
+  }, [firstTime, isChartDisplayed, isSwapHotTokenDisplay, query, setIsSwapHotTokenDisplay, setIsChartDisplayed])
+
+  // swap state & price data
+  const {
+    [Field.INPUT]: { currencyId: inputCurrencyId },
+    [Field.OUTPUT]: { currencyId: outputCurrencyId },
+  } = useSwapState()
+  const inputCurrency = useCurrency(inputCurrencyId)
+  const outputCurrency = useCurrency(outputCurrencyId)
+
+  const currencies: { [field in Field]?: Currency } = {
+    [Field.INPUT]: inputCurrency ?? undefined,
+    [Field.OUTPUT]: outputCurrency ?? undefined,
+  }
+
+  const singleTokenPrice = useSingleTokenSwapInfo(
+    inputCurrencyId,
+    inputCurrency,
+    outputCurrencyId,
+    outputCurrency,
+    isChartSupported,
+  )
+
+  return (
+    <Page removePadding={isChartExpanded} hideFooterOnDesktop={isChartExpanded}>
+      <Flex width={['328px', '100%']} height="100%" justifyContent="center" position="relative" alignItems="flex-start">
+        {isDesktop && isChartSupported && (
+          <PriceChartContainer
+            inputCurrencyId={inputCurrencyId}
+            inputCurrency={currencies[Field.INPUT]}
+            outputCurrencyId={outputCurrencyId}
+            outputCurrency={currencies[Field.OUTPUT]}
+            isChartExpanded={isChartExpanded}
+            setIsChartExpanded={setIsChartExpanded}
+            isChartDisplayed={isChartDisplayed}
+            currentSwapPrice={singleTokenPrice}
+          />
+        )}
+        {!isDesktop && isChartSupported && (
+          <BottomDrawer
+            content={
+              <PriceChartContainer
+                inputCurrencyId={inputCurrencyId}
+                inputCurrency={currencies[Field.INPUT]}
+                outputCurrencyId={outputCurrencyId}
+                outputCurrency={currencies[Field.OUTPUT]}
+                isChartExpanded={isChartExpanded}
+                setIsChartExpanded={setIsChartExpanded}
+                isChartDisplayed={isChartDisplayed}
+                currentSwapPrice={singleTokenPrice}
+                isFullWidthContainer
+                isMobile
+              />
+            }
+            isOpen={isChartDisplayed}
+            setIsOpen={(isOpen) => setIsChartDisplayed?.(isOpen)}
+          />
+        )}
+        {/* {isDesktop && isSwapHotTokenDisplay && isHotTokenSupported && (
+          <HotTokenList handleOutputSelect={handleOutputSelect} />
+        )} */}
+        {/* <ModalV2
+          isOpen={!isDesktop && isSwapHotTokenDisplay && isHotTokenSupported}
+          onDismiss={() => setIsSwapHotTokenDisplay(false)}
+        >
+          <Modal
+            style={{ padding: 0 }}
+            title={t('Top Token')}
+            onDismiss={() => setIsSwapHotTokenDisplay(false)}
+            bodyPadding="0px"
+          >
+            <HotTokenList
+              handleOutputSelect={(newCurrencyOutput: Currency) => {
+                handleOutputSelect(newCurrencyOutput)
+                setIsSwapHotTokenDisplay(false)
+              }}
+            />
+          </Modal>
+        </ModalV2> */}
+        <Flex flexDirection="column">
+          <StyledSwapContainer $isChartExpanded={isChartExpanded}>
+            <StyledInputCurrencyWrapper mt={isChartExpanded ? '24px' : '0'}>
+              <SwapSelection swapType={SwapType.MARKET} />
+              <AppBody>
+                <V3SwapForm />
+              </AppBody>
+            </StyledInputCurrencyWrapper>
+          </StyledSwapContainer>
+        </Flex>
+      </Flex>
+    </Page>
+  )
+}
