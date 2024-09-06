5eeb7cff99cad19f79ceba95eb8af2a7535c7d6e
memoyil <2213635+memoyil@users.noreply.github.com>
2024-09-06 18:21:24 +0200

    chore: Remove unused components from voting (#10258)
    <!--
    Before opening a pull request, please read the [contributing
    guidelines](https://github.com/pancakeswap/pancake-frontend/blob/develop/CONTRIBUTING.md)
    first
    -->
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    The focus of this PR is to refactor the voting strategies in the
    `Proposals` component and update dependencies.
    
    ### Detailed summary
    - Removed `SNAPSHOT_VERSION` constant
    - Refactored `veCakeBalanceStrategy`
    - Updated imports in `helpers.ts`
    - Removed unused functions in `strategies.ts`
    - Updated `generateMetaData` and `generatePayloadData` functions
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your
    question}`
    
    <!-- end pr-codex -->

--- apps/web/src/views/Voting/components/Proposals/index.tsx
+++ apps/web/src/views/Voting/components/Proposals/index.tsx
@@ -1,2 +1,1 @@
 export { default as Proposals } from './Proposals'
-export { default as ProposalRow } from './ProposalRow'
--- apps/web/src/views/Voting/helpers.ts
+++ apps/web/src/views/Voting/helpers.ts
@@ -1,15 +1,13 @@
 import { cakeVaultV2ABI } from '@pancakeswap/pools'
 import { bscTokens } from '@pancakeswap/tokens'
 import BigNumber from 'bignumber.js'
-import { SNAPSHOT_HUB_API } from 'config/constants/endpoints'
-import fromPairs from 'lodash/fromPairs'
 import groupBy from 'lodash/groupBy'
 import { Proposal, ProposalState, ProposalType, Vote } from 'state/types'
 import { getCakeVaultAddress } from 'utils/addressHelpers'
 import { Address, createPublicClient, http } from 'viem'
 import { bsc } from 'viem/chains'
 import { convertSharesToCake } from 'views/Pools/helpers'
-import { ADMINS, PANCAKE_SPACE, SNAPSHOT_VERSION } from './config'
+import { ADMINS, PANCAKE_SPACE } from './config'
 import { getScores } from './getScores'
 import * as strategies from './strategies'
 
@@ -37,60 +35,10 @@ export const filterProposalsByState = (proposals: Proposal[], state: ProposalSta
   return proposals.filter((proposal) => proposal.state === state)
 }
 
-export interface Message {
-  address: string
-  msg: string
-  sig: string
-}
-
 const STRATEGIES = [
   { name: 'cake', params: { symbol: 'CAKE', address: bscTokens.cake.address, decimals: 18, max: 300 } },
 ]
 const NETWORK = '56'
-
-/**
- * Generates metadata required by snapshot to validate payload
- */
-export const generateMetaData = () => {
-  return {
-    plugins: {},
-    network: 56,
-    strategies: STRATEGIES,
-  }
-}
-
-/**
- * Returns data that is required on all snapshot payloads
- */
-export const generatePayloadData = () => {
-  return {
-    version: SNAPSHOT_VERSION,
-    timestamp: (Date.now() / 1e3).toFixed(),
-    space: PANCAKE_SPACE,
-  }
-}
-
-/**
- * General function to send commands to the snapshot api
- */
-export const sendSnapshotData = async (message: Message) => {
-  const response = await fetch(SNAPSHOT_HUB_API, {
-    method: 'post',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(message),
-  })
-
-  if (!response.ok) {
-    const error = await response.json()
-    throw new Error(error?.error_description)
-  }
-
-  const data = await response.json()
-  return data
-}
 
 export const VOTING_POWER_BLOCK = {
   v0: 16300686n,
@@ -251,24 +199,4 @@ export const getTotalFromVotes = (votes: Vote[]) => {
     }, 0)
   }
   return 0
-}
-
-/**
- * Get voting power by a list of voters, only total
- */
-export async function getVotingPowerByCakeStrategy(voters: string[], blockNumber: number) {
-  const strategyResponse = await getScores(PANCAKE_SPACE, STRATEGIES, NETWORK, voters, blockNumber)
-
-  const result = fromPairs(
-    voters.map((voter) => {
-      const defaultTotal = strategyResponse.reduce(
-        (total: any, scoreList: any) => total + (scoreList[voter] ? scoreList[voter] : 0),
-        0,
-      )
-
-      return [voter, defaultTotal]
-    }),
-  )
-
-  return result
 }
