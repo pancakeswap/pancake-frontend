10e2f6c2cea0f5c21a59e1cd9a54799f7b2945fc
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2024-09-09 15:05:52 +0800

    chore: switch button

---
+++ apps/web/src/views/SwapSimplify/V4Swap/FlipButton.tsx
@@ -0,0 +1,50 @@
+import { AutoColumn, Button } from '@pancakeswap/uikit'
+import { Swap as SwapUI } from '@pancakeswap/widgets-internal'
+
+import { useTranslation } from '@pancakeswap/localization'
+import replaceBrowserHistory from '@pancakeswap/utils/replaceBrowserHistory'
+import { memo, useCallback } from 'react'
+
+import { useExpertMode } from '@pancakeswap/utils/user'
+import { AutoRow } from 'components/Layout/Row'
+import { Field } from 'state/swap/actions'
+import { useSwapState } from 'state/swap/hooks'
+import { useSwapActionHandlers } from 'state/swap/useSwapActionHandlers'
+
+import { useAllowRecipient } from '../../Swap/V3Swap/hooks'
+
+export const FlipButton = memo(function FlipButton() {
+  const { t } = useTranslation()
+  const [isExpertMode] = useExpertMode()
+  const { onSwitchTokens, onChangeRecipient } = useSwapActionHandlers()
+  const {
+    recipient,
+    [Field.INPUT]: { currencyId: inputCurrencyId },
+    [Field.OUTPUT]: { currencyId: outputCurrencyId },
+  } = useSwapState()
+  const allowRecipient = useAllowRecipient()
+
+  const onFlip = useCallback(() => {
+    onSwitchTokens()
+    replaceBrowserHistory('inputCurrency', outputCurrencyId)
+    replaceBrowserHistory('outputCurrency', inputCurrencyId)
+  }, [onSwitchTokens, inputCurrencyId, outputCurrencyId])
+
+  return (
+    <AutoColumn justify="space-between">
+      <AutoRow justify={isExpertMode ? 'space-between' : 'center'} style={{ padding: '0 1rem', marginTop: '1em' }}>
+        <SwapUI.SwitchButtonV2 onClick={onFlip} />
+        {allowRecipient && recipient === null ? (
+          <Button
+            variant="text"
+            id="add-recipient-button"
+            onClick={() => onChangeRecipient('')}
+            data-dd-action-name="Swap flip button"
+          >
+            {t('+ Add a send (optional)')}
+          </Button>
+        ) : null}
+      </AutoRow>
+    </AutoColumn>
+  )
+})
