5c14e7d813738db392ab9981509e6df2d78e203f
BingBong <bingbong@pancakeswap.com>
2024-09-10 13:24:51 +0100

    add zkevm pool

--- packages/pools/src/constants/pools/1101.ts
+++ packages/pools/src/constants/pools/1101.ts
@@ -1,6 +1,24 @@
-import { SerializedPool } from '../../types'
+import { polygonZkEvmTokens } from '@pancakeswap/tokens'
+import { getAddress } from 'viem'
+import { PoolCategory, SerializedPool } from '../../types'
 
-export const livePools: SerializedPool[] = []
+export const livePools: SerializedPool[] = [
+  {
+    sousId: 1,
+    stakingToken: polygonZkEvmTokens.cake,
+    earningToken: polygonZkEvmTokens.pol,
+    contractAddress: '0x3BF2521A44502eDC06efA069be66694ec4d3AB65',
+    poolCategory: PoolCategory.CORE,
+    tokenPerSecond: '0.0055',
+    version: 3,
+  },
+].map((p) => ({
+  ...p,
+  isFinished: true,
+  contractAddress: getAddress(p.contractAddress),
+  stakingToken: p.stakingToken.serialize,
+  earningToken: p.earningToken.serialize,
+}))
 
 // known finished pools
 export const finishedPools: SerializedPool[] = []
