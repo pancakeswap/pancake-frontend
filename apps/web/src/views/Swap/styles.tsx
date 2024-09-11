c0258befa725096ba81c082c2e9a854d2fd15d42
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2024-09-12 06:33:44 +0800

    chore: layout styles

--- apps/web/src/views/Swap/styles.tsx
+++ apps/web/src/views/Swap/styles.tsx
@@ -5,7 +5,6 @@ export const StyledSwapContainer = styled(Flex)<{ $isChartExpanded: boolean }>`
   flex-shrink: 0;
   height: fit-content;
   padding: 0 16px;
-
   ${({ theme }) => theme.mediaQueries.lg} {
     padding: 0 40px;
   }
