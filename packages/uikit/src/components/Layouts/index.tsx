216b70083df333e42e408cc8e7f57bcfebe9134f
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2024-09-12 05:13:43 +0800

    chore: details

--- packages/uikit/src/components/Layouts/index.tsx
+++ packages/uikit/src/components/Layouts/index.tsx
@@ -2,5 +2,5 @@ export { default as BaseLayout } from "./BaseLayout";
 export { default as CardsLayout } from "./CardsLayout";
 export { default as Container } from "./Container";
 export { default as FlexLayout } from "./Flex";
-export { default as FlexGap } from "./FlexGap";
+export { default as FlexGap, MotionFlexGap } from "./FlexGap";
 export type { FlexGapProps } from "./FlexGap";
