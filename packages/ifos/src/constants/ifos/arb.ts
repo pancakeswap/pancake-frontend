3ab3ebc0c8e63f883863d8920ea8589fc8011dd5
Chef Penguin <penguin@pancakeswap.com>
2024-09-13 17:39:10 +0530

    chore: add previous test ifo to finished tab

--- packages/ifos/src/constants/ifos/arb.ts
+++ packages/ifos/src/constants/ifos/arb.ts
@@ -25,4 +25,27 @@ export const ifos: BaseIfoConfig[] = [
     },
     tokenOfferingPrice: 0.1,
   },
+  {
+    id: 'testCrossChainIfo',
+    version: 8,
+
+    address: '0x6164B999597a6F30DA9aEF8A7F31D6dD7AE57e04', // Test IFO
+    plannedStartTime: 1726207200,
+
+    isActive: false,
+    name: 'Test Arbitrum IFO',
+    description: 'This is an IFO on Arbitrum to test cross-chain IFOs functionality on PancakeSwap.',
+    currency: arbitrumTokens.cake,
+    token: arbitrumTokens.usdc,
+    articleUrl: 'https://pancakeswap.medium.com/',
+    campaignId: 'random-for-testing',
+    poolBasic: {
+      raiseAmount: '$10,000',
+    },
+    poolUnlimited: {
+      raiseAmount: '$90,000',
+      additionalClaimingFee: false,
+    },
+    tokenOfferingPrice: 0.1,
+  },
 ]
