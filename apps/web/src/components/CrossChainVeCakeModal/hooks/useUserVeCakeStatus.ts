be19d5173b0be4580f0cc1620c981458c412f43b
Chef Penguin <penguin@pancakeswap.com>
2024-09-16 11:27:18 +0530

    chore: update profile sync vars in useUserVeCakeStatus

--- apps/web/src/components/CrossChainVeCakeModal/hooks/useUserVeCakeStatus.ts
+++ apps/web/src/components/CrossChainVeCakeModal/hooks/useUserVeCakeStatus.ts
@@ -2,7 +2,7 @@ import { ChainId } from '@pancakeswap/chains'
 import { useMultichainVeCakeWellSynced } from 'components/CrossChainVeCakeModal/hooks/useMultichainVeCakeWellSynced'
 import { useMemo } from 'react'
 import { Address } from 'viem/accounts'
-import { useProfileProxyUserStatus } from './useProfileProxyWellSynced'
+import { useProfileProxyWellSynced } from './useProfileProxyWellSynced'
 
 export const useUserVeCakeStatus = (account?: Address, targetChainId?: ChainId, targetTime?: number) => {
   const {
@@ -13,19 +13,14 @@ export const useUserVeCakeStatus = (account?: Address, targetChainId?: ChainId, 
     targetChainProxyBalance,
   } = useMultichainVeCakeWellSynced(targetChainId, targetTime)
 
-  // Important:
-  // From the perspective of the IFO, the profile needs to be synced AND be active.
-  // because if its inactive, then the user needs to re-sync to participate in the public sale
-  // This is NOT a requirement for Claiming after the IFO expires.
-  // (When IFO expires, the profile is made inactive automatically)
-  const { isProfileActive } = useProfileProxyUserStatus(account, targetChainId, Boolean(isVeCakeSynced))
+  const { isSynced: isProfileSynced } = useProfileProxyWellSynced(targetChainId)
 
-  const isSynced = useMemo(() => Boolean(isVeCakeSynced && isProfileActive), [isVeCakeSynced, isProfileActive])
+  const isSynced = useMemo(() => Boolean(isVeCakeSynced && isProfileSynced), [isVeCakeSynced, isProfileSynced])
 
   return {
     isSynced,
     isVeCakeSynced: Boolean(isVeCakeSynced),
-    isProfileActive: Boolean(isProfileActive),
+    isProfileSynced: Boolean(isProfileSynced),
     bscBalance,
     bscProxyBalance,
     targetChainBalance,
