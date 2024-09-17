750e4b9da9a7a271307dc666206b31e0ae90a8e7
Chef Penguin <penguin@pancakeswap.com>
2024-09-17 10:35:35 +0530

    chore: new ifo and iCAKE addresses

--- packages/ifos/src/constants/ifos/arb.ts
+++ packages/ifos/src/constants/ifos/arb.ts
@@ -3,6 +3,29 @@ import { BaseIfoConfig } from '../../types'
 
 export const ifos: BaseIfoConfig[] = [
   {
+    id: 'testCrossChainIfo3',
+    version: 8,
+
+    address: '0xb7AdE79F9c517C9b3cc45E3522eBc4dCe1C54e7D', // Test IFO
+    plannedStartTime: 1726552801,
+
+    isActive: true,
+    name: 'Test Arbitrum IFO',
+    description: 'This is an IFO on Arbitrum to test cross-chain IFOs functionality on PancakeSwap.',
+    currency: arbitrumTokens.cake,
+    token: arbitrumTokens.usdc,
+    articleUrl: 'https://pancakeswap.medium.com/',
+    campaignId: 'random-for-testing',
+    poolBasic: {
+      raiseAmount: '$10,000',
+    },
+    poolUnlimited: {
+      raiseAmount: '$90,000',
+      additionalClaimingFee: false,
+    },
+    tokenOfferingPrice: 0.1,
+  },
+  {
     id: 'testCrossChainIfo2',
     version: 8,
 
