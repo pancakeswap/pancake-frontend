7b96db8ab34f6e945acc2f585232683a965c5cd5
chefjackson <116779127+chefjackson@users.noreply.github.com>
2024-09-18 17:22:30 +0800

    chore: Add security header (#10694)
    <!--
    Before opening a pull request, please read the [contributing
    guidelines](https://github.com/pancakeswap/pancake-frontend/blob/develop/CONTRIBUTING.md)
    first
    -->
    
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    This PR adds a Content-Security-Policy header with frame-ancestors
    'self' value to improve security in `next.config.mjs`.
    
    ### Detailed summary
    - Added Content-Security-Policy header with frame-ancestors 'self'
    value.
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your
    question}`
    
    <!-- end pr-codex -->

--- apps/gamification/next.config.mjs
+++ apps/gamification/next.config.mjs
@@ -75,6 +75,10 @@ const nextConfig = {
             key: 'Cross-Origin-Opener-Policy',
             value: 'same-origin-allow-popups',
           },
+          {
+            key: 'Content-Security-Policy',
+            value: `frame-ancestors 'self'`,
+          },
         ],
       },
     ]
