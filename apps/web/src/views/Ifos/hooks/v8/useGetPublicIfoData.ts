1c4fa5c1735c92a79a3dced74c817e6eeb100146
Chef Penguin <penguin@pancakeswap.com>
2024-09-24 10:22:59 +0530

    chore: update timestamp

--- apps/web/src/views/Ifos/hooks/v8/useGetPublicIfoData.ts
+++ apps/web/src/views/Ifos/hooks/v8/useGetPublicIfoData.ts
@@ -202,7 +202,7 @@ const useGetPublicIfoData = (ifo: Ifo): PublicIfoData => {
       : 0
 
     // const now = Math.floor(Date.now() / 1000)
-    const now = 1727172960 // evm_setNextBlockTimestamp, evm_increaseTime
+    const now = 1773743400 // evm_setNextBlockTimestamp, evm_increaseTime
 
     const status = getStatusByTimestamp(now, startTime, endTime)
     const duration = endTime - startTime
