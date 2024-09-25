73a0eebe18dd81ce9a238540d04031bdfd66ba83
memoyil <2213635+memoyil@users.noreply.github.com>
2024-09-25 10:27:40 +0200

    fix: Add response ok check

--- packages/gauges/src/constants/config/prod.ts
+++ packages/gauges/src/constants/config/prod.ts
@@ -3,8 +3,11 @@ import { GaugeConfig } from '../../types'
 export const CONFIG_PROD = async (): Promise<GaugeConfig[]> => {
   try {
     const response = await fetch('https://cms-public-api-7ys4p.ondigitalocean.app/api/data/gauges')
-    const result = await response.json()
-    return result
+    if (response.ok) {
+      const result = await response.json()
+      return result
+    }
+    throw response
   } catch (error) {
     console.error('Failed to fetch gauges config: ', error)
     return []
