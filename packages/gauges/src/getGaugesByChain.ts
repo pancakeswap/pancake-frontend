9351d22ebc10dbc3b777e9b9ce676be905f054a8
memoyil <2213635+memoyil@users.noreply.github.com>
2024-09-25 10:31:51 +0200

    fix: Revert introducing GAUGES_CONFIG

--- packages/gauges/src/getGaugesByChain.ts
+++ packages/gauges/src/getGaugesByChain.ts
@@ -1,10 +1,12 @@
 import { ChainId } from '@pancakeswap/chains'
 import memoize from 'lodash/memoize'
 import { GaugeConfig } from './types'
-import { GAUGES_CONFIG } from './constants/config'
+import { CONFIG_PROD } from './constants/config/prod'
 
 export const getGaugesByChain = memoize(
-  async (chainId?: ChainId): Promise<GaugeConfig[]> =>
-    chainId ? (await GAUGES_CONFIG[chainId])?.filter((gauge) => gauge.chainId === chainId) : [],
+  async (chainId?: ChainId): Promise<GaugeConfig[]> => {
+    const result = await CONFIG_PROD()
+    return result.filter((gauge) => gauge.chainId === chainId)
+  },
   (chainId) => chainId,
 )
