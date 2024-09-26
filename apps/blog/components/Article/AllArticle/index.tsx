682a6a03cfeb256e13308cf1a46ac66cef028680
chefmomota <momota@pancakeswap.com>
2024-09-26 14:42:22 +0800

    fix: SSR issue

--- apps/blog/components/Article/AllArticle/index.tsx
+++ apps/blog/components/Article/AllArticle/index.tsx
@@ -14,6 +14,7 @@ import { useQuery } from '@tanstack/react-query'
 import ArticleSortSelect from 'components/Article/ArticleSortSelect'
 import CardArticle from 'components/Article/CardArticle'
 import CategoriesSelector from 'components/Article/CategoriesSelector'
+import NoSSR from 'components/NoSSR'
 import SkeletonArticle from 'components/SkeletonArticle'
 import useAllArticle from 'hooks/useAllArticle'
 import useLanguage from 'hooks/useLanguage'
@@ -136,98 +137,100 @@ const AllArticle = () => {
   }
 
   return (
-    <StyledArticleContainer id="all" ref={articlesWrapperEl}>
-      <Text
-        bold
-        color="secondary"
-        mb={['12px', '12px', '12px', '35px']}
-        pl={['16px']}
-        fontSize={['24px', '24px', '24px', '40px']}
-      >
-        {t('All articles')}
-      </Text>
-      <Flex p={['0', '0', '0', '0', '0', '0 16px', '0 16px']}>
-        {isDesktop && !isXl && (
-          <StyledTagContainer>
-            <CategoriesSelector
-              selected={selectedCategories}
-              categoriesData={categoriesData ?? []}
-              setSelected={setSelectCategoriesSelected}
-              childMargin="0 0 28px 0"
-            />
-          </StyledTagContainer>
-        )}
-        <Flex width={['100%', '100%', '100%', '100%', '100%', '845px', '907px']} flexDirection="column">
-          <Flex
-            mb={['18px', '18px', '18px', '24px']}
-            flexDirection={['column-reverse', 'column-reverse', 'column-reverse', 'row']}
-            alignItems={['flexStart', 'flexStart', 'flexStart', 'center']}
-            p={['0 16px', '0 16px', '0 16px', '0 16px', '0 16px', '0', '0']}
-          >
-            {languageItems.length > 0 && (
-              <Flex flexDirection={['column', 'row']}>
-                <Box width="100%">
-                  <ArticleSortSelect
-                    title={t('Languages')}
-                    value={languageOption}
-                    options={languageItems}
-                    setOption={handleSwitchLanguage}
-                  />
-                </Box>
-                <Box width="100%" m={['10px 0 0 0', '0 0 0 16px', '0 0 0 16px', '0 16px']}>
-                  <ArticleSortSelect title={t('Sort By')} options={sortByItems} setOption={setSortBy} />
-                </Box>
-              </Flex>
-            )}
-            <Box width="100%" m={['0 0 12px 0', '0 0 12px 0', '0 0 12px 0', '22px 0 0 0']}>
-              <InputGroup startIcon={<SearchIcon style={{ zIndex: 1 }} color="textSubtle" width="18px" />}>
-                <SearchInput placeholder="Search" initialValue={query} onChange={(e) => setQuery(e.target.value)} />
-              </InputGroup>
-            </Box>
-          </Flex>
-          {(isMobile || isTablet || isXl) && (
-            <StyledMobileTagContainer>
-              <Text fontSize="12px" textTransform="uppercase" color="textSubtle" fontWeight={600} mb="4px">
-                {t('Filter by')}
-              </Text>
-              <Flex overflowY="auto">
-                <CategoriesSelector
-                  selected={selectedCategories}
-                  categoriesData={categoriesData ?? []}
-                  setSelected={setSelectCategoriesSelected}
-                  childMargin="0 4px 4px 0"
-                />
-              </Flex>
-            </StyledMobileTagContainer>
+    <NoSSR>
+      <StyledArticleContainer id="all" ref={articlesWrapperEl}>
+        <Text
+          bold
+          color="secondary"
+          mb={['12px', '12px', '12px', '35px']}
+          pl={['16px']}
+          fontSize={['24px', '24px', '24px', '40px']}
+        >
+          {t('All articles')}
+        </Text>
+        <Flex p={['0', '0', '0', '0', '0', '0 16px', '0 16px']}>
+          {isDesktop && !isXl && (
+            <StyledTagContainer>
+              <CategoriesSelector
+                selected={selectedCategories}
+                categoriesData={categoriesData ?? []}
+                setSelected={setSelectCategoriesSelected}
+                childMargin="0 0 28px 0"
+              />
+            </StyledTagContainer>
           )}
-          {!isFetching && articles.length === 0 ? (
-            <Text bold fontSize={20} padding={['0 16px', '0 16px', '0 16px', '0 16px', '0 16px', '0', '0']}>
-              {t('No results found.')}
-            </Text>
-          ) : (
-            <StyledCard>
-              {isFetching && articles.length === 0 ? (
-                <SkeletonArticle />
-              ) : (
-                <>
-                  <Box>
-                    {articles.map((article) => (
-                      <CardArticle key={article.id} article={article} />
-                    ))}
+          <Flex width={['100%', '100%', '100%', '100%', '100%', '845px', '907px']} flexDirection="column">
+            <Flex
+              mb={['18px', '18px', '18px', '24px']}
+              flexDirection={['column-reverse', 'column-reverse', 'column-reverse', 'row']}
+              alignItems={['flexStart', 'flexStart', 'flexStart', 'center']}
+              p={['0 16px', '0 16px', '0 16px', '0 16px', '0 16px', '0', '0']}
+            >
+              {languageItems.length > 0 && (
+                <Flex flexDirection={['column', 'row']}>
+                  <Box width="100%">
+                    <ArticleSortSelect
+                      title={t('Languages')}
+                      value={languageOption}
+                      options={languageItems}
+                      setOption={handleSwitchLanguage}
+                    />
                   </Box>
-                  <PaginationButton
-                    showMaxPageText
-                    currentPage={articlesData.pagination.page}
-                    maxPage={articlesData.pagination.pageCount}
-                    setCurrentPage={handlePagination}
-                  />
-                </>
+                  <Box width="100%" m={['10px 0 0 0', '0 0 0 16px', '0 0 0 16px', '0 16px']}>
+                    <ArticleSortSelect title={t('Sort By')} options={sortByItems} setOption={setSortBy} />
+                  </Box>
+                </Flex>
               )}
-            </StyledCard>
-          )}
+              <Box width="100%" m={['0 0 12px 0', '0 0 12px 0', '0 0 12px 0', '22px 0 0 0']}>
+                <InputGroup startIcon={<SearchIcon style={{ zIndex: 1 }} color="textSubtle" width="18px" />}>
+                  <SearchInput placeholder="Search" initialValue={query} onChange={(e) => setQuery(e.target.value)} />
+                </InputGroup>
+              </Box>
+            </Flex>
+            {(isMobile || isTablet || isXl) && (
+              <StyledMobileTagContainer>
+                <Text fontSize="12px" textTransform="uppercase" color="textSubtle" fontWeight={600} mb="4px">
+                  {t('Filter by')}
+                </Text>
+                <Flex overflowY="auto">
+                  <CategoriesSelector
+                    selected={selectedCategories}
+                    categoriesData={categoriesData ?? []}
+                    setSelected={setSelectCategoriesSelected}
+                    childMargin="0 4px 4px 0"
+                  />
+                </Flex>
+              </StyledMobileTagContainer>
+            )}
+            {!isFetching && articles.length === 0 ? (
+              <Text bold fontSize={20} padding={['0 16px', '0 16px', '0 16px', '0 16px', '0 16px', '0', '0']}>
+                {t('No results found.')}
+              </Text>
+            ) : (
+              <StyledCard>
+                {isFetching && articles.length === 0 ? (
+                  <SkeletonArticle />
+                ) : (
+                  <>
+                    <Box>
+                      {articles.map((article) => (
+                        <CardArticle key={article.id} article={article} />
+                      ))}
+                    </Box>
+                    <PaginationButton
+                      showMaxPageText
+                      currentPage={articlesData.pagination.page}
+                      maxPage={articlesData.pagination.pageCount}
+                      setCurrentPage={handlePagination}
+                    />
+                  </>
+                )}
+              </StyledCard>
+            )}
+          </Flex>
         </Flex>
-      </Flex>
-    </StyledArticleContainer>
+      </StyledArticleContainer>
+    </NoSSR>
   )
 }
 
