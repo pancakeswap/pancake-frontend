373545dfa76aaec90d4dc19f1b200c04a9a9b937
memoyil <2213635+memoyil@users.noreply.github.com>
2024-09-26 13:51:44 +0200

    fix: Remove memoize

--- packages/gauges/src/getGaugesByChain.ts
+++ packages/gauges/src/getGaugesByChain.ts
@@ -1,12 +1,8 @@
 import { ChainId } from '@pancakeswap/chains'
-import memoize from 'lodash/memoize'
 import { GaugeConfig } from './types'
 import { CONFIG_PROD } from './constants/config/prod'
 
-export const getGaugesByChain = memoize(
-  async (chainId?: ChainId): Promise<GaugeConfig[]> => {
-    const result = await CONFIG_PROD()
-    return result.filter((gauge) => gauge.chainId === chainId)
-  },
-  (chainId) => chainId,
-)
+export const getGaugesByChain = async (chainId?: ChainId): Promise<GaugeConfig[]> => {
+  const result = await CONFIG_PROD()
+  return result.filter((gauge) => gauge.chainId === chainId)
+}
