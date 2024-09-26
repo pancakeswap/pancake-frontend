37869cef1bf8544ce74f0dbf042a70e64915610b
chefmomota <momota@pancakeswap.com>
2024-09-26 08:13:26 +0800

    fix: Conflict

--- packages/gauges/src/constants/config/index.ts
+++ packages/gauges/src/constants/config/index.ts
@@ -3,6 +3,6 @@ import { CONFIG_PROD } from './prod'
 import { CONFIG_TESTNET } from './testnet'
 
 export const GAUGES_CONFIG = {
-  [ChainId.BSC]: CONFIG_PROD,
+  [ChainId.BSC]: CONFIG_PROD(),
   [ChainId.BSC_TESTNET]: CONFIG_TESTNET,
 }
