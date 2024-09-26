04fcddafba922beb48662e306b4dec99f1339e7a
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2024-09-26 14:14:16 +0800

    chore: shows the risk details

--- packages/widgets-internal/swap-v2/SwapWidgetV2.tsx
+++ packages/widgets-internal/swap-v2/SwapWidgetV2.tsx
@@ -1,7 +1,7 @@
 import { AssetSettingButtonList } from "./AssetSettingButtonList";
 import { CurrencyInputPanelSimplify } from "./CurrencyInputPanelSimplify";
 import { NumericalInput } from "./NumericalInput";
-import { InputPanelWrapper, SwapFormWrapper } from "./SwapWrapper";
+import { InputPanelWrapper, SwapFormWrapper, SwapTabAndInputPanelWrapper } from "./SwapWrapper";
 import { SwitchButtonV2 } from "./SwitchButtonV2";
 import { WalletAssetDisplay } from "./WalletAssetDisplay";
 
@@ -11,6 +11,7 @@ export {
   InputPanelWrapper,
   NumericalInput,
   SwapFormWrapper,
+  SwapTabAndInputPanelWrapper,
   SwitchButtonV2,
   WalletAssetDisplay,
 };
