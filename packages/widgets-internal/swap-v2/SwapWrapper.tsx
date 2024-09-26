371d00bb80db4e81c26ee48d245d02db6d295dc0
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2024-09-26 14:14:16 +0800

    chore: shows the risk details

--- packages/widgets-internal/swap-v2/SwapWrapper.tsx
+++ packages/widgets-internal/swap-v2/SwapWrapper.tsx
@@ -2,6 +2,7 @@ import { Flex } from "@pancakeswap/uikit";
 import { styled } from "styled-components";
 
 export const SwapFormWrapper = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
   gap: 16px;
@@ -19,4 +20,17 @@ export const InputPanelWrapper = styled(Flex)`
   background-color: ${({ theme }) => theme.colors.backgroundAlt};
   border-radius: 24px;
   border: 1px solid ${({ theme }) => theme.colors.cardBorder};
+`;
+
+export const SwapTabAndInputPanelWrapper = styled(Flex)`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  will-change: transform;
+  transition: transform 0.2 ease;
+  transform: translateY(0px);
 `;
