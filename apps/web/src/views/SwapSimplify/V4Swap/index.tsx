0bc46a43dc7c16c60b586a49ae61e22e46389efd
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2024-09-26 14:14:16 +0800

    chore: shows the risk details

--- apps/web/src/views/SwapSimplify/V4Swap/index.tsx
+++ apps/web/src/views/SwapSimplify/V4Swap/index.tsx
@@ -1,6 +1,10 @@
 import { SmartRouter } from '@pancakeswap/smart-router/evm'
 import { SwapUIV2 } from '@pancakeswap/widgets-internal'
+import { RiskDetailsPanel, useShouldRiskPanelDisplay } from 'components/AccessRisk/SwapRevampRiskDisplay'
+import { useCurrency } from 'hooks/Tokens'
 import { useMemo } from 'react'
+import { Field } from 'state/swap/actions'
+import { useSwapState } from 'state/swap/hooks'
 import { SwapType } from '../../Swap/types'
 import { PricingAndSlippage } from '../../Swap/V3Swap/containers'
 import { CommitButton } from '../../Swap/V3Swap/containers/CommitButton'
@@ -31,26 +35,40 @@ export function V4SwapForm() {
       afterCommit: resumeQuoting,
     }
   }, [pauseQuoting, resumeQuoting])
+  const {
+    independentField,
+    typedValue,
+    [Field.INPUT]: { currencyId: inputCurrencyId },
+    [Field.OUTPUT]: { currencyId: outputCurrencyId },
+  } = useSwapState()
+
+  const inputCurrency = useCurrency(inputCurrencyId)
+  const outputCurrency = useCurrency(outputCurrencyId)
+
+  const shouldRiskPanelDisplay = useShouldRiskPanelDisplay(inputCurrency?.wrapped, outputCurrency?.wrapped)
 
   return (
     <SwapUIV2.SwapFormWrapper>
-      <SwapSelection swapType={SwapType.MARKET} />
-      <FormMain
-        tradeLoading={isMMBetter ? false : !tradeLoaded}
-        pricingAndSlippage={
-          <PricingAndSlippage priceLoading={!tradeLoaded} price={ammPrice ?? undefined} showSlippage={!isMMBetter} />
-        }
-        inputAmount={bestTrade?.inputAmount}
-        outputAmount={bestTrade?.outputAmount}
-        swapCommitButton={
-          <CommitButton
-            trade={isMMBetter ? mmTrade : ammTrade}
-            tradeError={tradeError}
-            tradeLoaded={tradeLoaded}
-            {...commitHooks}
-          />
-        }
-      />
+      <SwapUIV2.SwapTabAndInputPanelWrapper>
+        <SwapSelection swapType={SwapType.MARKET} />
+        <FormMain
+          tradeLoading={isMMBetter ? false : !tradeLoaded}
+          pricingAndSlippage={
+            <PricingAndSlippage priceLoading={!tradeLoaded} price={ammPrice ?? undefined} showSlippage={!isMMBetter} />
+          }
+          inputAmount={bestTrade?.inputAmount}
+          outputAmount={bestTrade?.outputAmount}
+          swapCommitButton={
+            <CommitButton
+              trade={isMMBetter ? mmTrade : ammTrade}
+              tradeError={tradeError}
+              tradeLoaded={tradeLoaded}
+              {...commitHooks}
+            />
+          }
+        />
+      </SwapUIV2.SwapTabAndInputPanelWrapper>
+      {shouldRiskPanelDisplay && <RiskDetailsPanel token0={inputCurrency?.wrapped} token1={outputCurrency?.wrapped} />}
     </SwapUIV2.SwapFormWrapper>
   )
 }
