e00d572bf5335d035378b523ddad74770bf2f765
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2024-09-27 18:34:20 +0800

    chore: trade details UI

--- apps/web/src/views/SwapSimplify/V4Swap/ButtonAndDetailsPanel.tsx
+++ apps/web/src/views/SwapSimplify/V4Swap/ButtonAndDetailsPanel.tsx
@@ -1,6 +1,9 @@
 import { styled } from 'styled-components'
 
 export const PanelWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
   width: 100%;
   padding: 16px;
   border-radius: 24px;
@@ -9,8 +12,14 @@ export const PanelWrapper = styled.div`
 `
 interface ButtonAndDetailsPanelProps {
   swapCommitButton: React.ReactNode
+  tradeDetails: React.ReactNode
 }
 
-export const ButtonAndDetailsPanel: React.FC<ButtonAndDetailsPanelProps> = ({ swapCommitButton }) => {
-  return <PanelWrapper>{swapCommitButton}</PanelWrapper>
+export const ButtonAndDetailsPanel: React.FC<ButtonAndDetailsPanelProps> = ({ swapCommitButton, tradeDetails }) => {
+  return (
+    <PanelWrapper>
+      {swapCommitButton}
+      {tradeDetails}
+    </PanelWrapper>
+  )
 }
