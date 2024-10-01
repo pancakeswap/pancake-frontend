f5b019cfed63b031c05eeb021c71cb2f1f703639
Chef Penguin <penguin@pancakeswap.com>
2024-10-01 18:07:59 +0530

    feat: expert mode tab, update motion logic for modal

--- apps/web/src/components/Menu/GlobalSettings/SettingsModalV2/SettingsTab.tsx
+++ apps/web/src/components/Menu/GlobalSettings/SettingsModalV2/SettingsTab.tsx
@@ -1,8 +1,9 @@
 import { ChainId } from '@pancakeswap/chains'
 import { useTranslation } from '@pancakeswap/localization'
-import { Flex, PreTitle } from '@pancakeswap/uikit'
+import { Flex, PreTitle, QuestionHelper, Text, Toggle } from '@pancakeswap/uikit'
 import { useActiveChainId } from 'hooks/useActiveChainId'
 import { memo } from 'react'
+import { useSwapActionHandlers } from 'state/swap/useSwapActionHandlers'
 import GasSettings from '../GasSettings'
 import { PrimaryOutlineButton } from '../styles'
 import TransactionSettings from '../TransactionSettings'
@@ -10,30 +11,66 @@ import { TabContent } from './TabContent'
 
 interface SettingsTabProps {
   onCustomizeRoutingClick?: () => void
+  showExpertModeAcknowledgement: boolean
+  setShowConfirmExpertModal: (show: boolean) => void
+  expertMode: boolean
+  setExpertMode: (expertMode: any) => void
 }
 
-export const SettingsTab = memo(({ onCustomizeRoutingClick }: SettingsTabProps) => {
-  const { t } = useTranslation()
-  const { chainId } = useActiveChainId()
+export const SettingsTab = memo(
+  ({
+    onCustomizeRoutingClick,
+    showExpertModeAcknowledgement,
+    setShowConfirmExpertModal,
+    expertMode,
+    setExpertMode,
+  }: SettingsTabProps) => {
+    const { t } = useTranslation()
+    const { chainId } = useActiveChainId()
+    const { onChangeRecipient } = useSwapActionHandlers()
 
-  return (
-    <TabContent>
-      <Flex flexDirection="column">
-        <PreTitle>{t('Swaps & Liquidity')}</PreTitle>
-        <Flex justifyContent="space-between" alignItems="center" mt="12px" mb="24px">
-          {chainId === ChainId.BSC && <GasSettings />}
+    const handleExpertModeToggle = () => {
+      if (expertMode || !showExpertModeAcknowledgement) {
+        onChangeRecipient(null)
+        setExpertMode((s) => !s)
+      } else {
+        setShowConfirmExpertModal(true)
+      }
+    }
+
+    return (
+      <TabContent>
+        <Flex flexDirection="column">
+          <PreTitle>{t('Swaps & Liquidity')}</PreTitle>
+          <Flex justifyContent="space-between" alignItems="center" mt="8px" mb="24px">
+            {chainId === ChainId.BSC && <GasSettings />}
+          </Flex>
+          <TransactionSettings />
+
+          <PreTitle>{t('Interface Settings')}</PreTitle>
+
+          <Flex justifyContent="space-between" alignItems="center" mt="4px" mb="24px">
+            <Flex alignItems="center">
+              <Text>{t('Expert Mode')}</Text>
+              <QuestionHelper
+                text={t('Bypasses confirmation modals and allows high slippage trades. Use at your own risk.')}
+                placement="top"
+                ml="4px"
+              />
+            </Flex>
+            <Toggle id="toggle-expert-mode-button" scale="md" checked={expertMode} onChange={handleExpertModeToggle} />
+          </Flex>
         </Flex>
-        <TransactionSettings />
-      </Flex>
-      <PrimaryOutlineButton
-        variant="text"
-        mt="auto"
-        $height="48px"
-        width="100%"
-        onClick={() => onCustomizeRoutingClick?.()}
-      >
-        {t('Customize Routing')}
-      </PrimaryOutlineButton>
-    </TabContent>
-  )
-})
+        <PrimaryOutlineButton
+          variant="text"
+          mt="auto"
+          $height="48px"
+          width="100%"
+          onClick={() => onCustomizeRoutingClick?.()}
+        >
+          {t('Customize Routing')}
+        </PrimaryOutlineButton>
+      </TabContent>
+    )
+  },
+)
--- apps/web/src/components/Menu/GlobalSettings/SettingsModalV2/index.tsx
+++ apps/web/src/components/Menu/GlobalSettings/SettingsModalV2/index.tsx
@@ -1,9 +1,11 @@
 import { useTranslation } from '@pancakeswap/localization'
-import { domAnimation, Heading, LazyAnimatePresence, MotionModal, Text } from '@pancakeswap/uikit'
+import { Heading, MotionModal, Text, useMatchBreakpoints } from '@pancakeswap/uikit'
+import { useExpertMode, useUserExpertModeAcknowledgement } from '@pancakeswap/utils/user'
 import { MotionTabs } from 'components/Motion/MotionTabs'
 import { useCallback, useState } from 'react'
 import { SettingsMode } from '../types'
 import { CustomizeRoutingTab } from './CustomizeRoutingTab'
+import { ExpertModeTab } from './ExpertModeTab'
 import { RecentTransactionsTab } from './RecentTransactionsTab'
 import { SettingsTab } from './SettingsTab'
 import { TabContent } from './TabContent'
@@ -15,7 +17,10 @@ interface SettingsModalV2Props {
 
 export const SettingsModalV2 = ({ onDismiss, mode }: SettingsModalV2Props) => {
   const { t } = useTranslation()
+  const { isMobile } = useMatchBreakpoints()
   const [activeTabIndex, setActiveTabIndex] = useState(0)
+  const [showExpertModeAcknowledgement, setShowExpertModeAcknowledgement] = useUserExpertModeAcknowledgement()
+  const [expertMode, setExpertMode] = useExpertMode()
 
   const onTabChange = useCallback(
     (index: number) => {
@@ -24,42 +29,80 @@ export const SettingsModalV2 = ({ onDismiss, mode }: SettingsModalV2Props) => {
     [setActiveTabIndex],
   )
 
+  const renderTabHeading = useCallback(() => {
+    switch (activeTabIndex) {
+      case 2:
+        return (
+          <TabContent type="to_right">
+            <Heading>{t('Customize Routing')}</Heading>
+          </TabContent>
+        )
+      case 3:
+        return (
+          <TabContent type="to_right">
+            <Heading>{t('Expert Mode')}</Heading>
+          </TabContent>
+        )
+
+      default:
+        return (
+          <MotionTabs activeIndex={activeTabIndex} onItemClick={onTabChange}>
+            <Text>{t('Settings')}</Text>
+            <Text>{t('Recent Transactions')}</Text>
+          </MotionTabs>
+        )
+    }
+  }, [activeTabIndex, t, onTabChange])
+
   const renderTab = useCallback(() => {
     switch (activeTabIndex) {
       case 0:
-        return <SettingsTab key="settings_tab" onCustomizeRoutingClick={() => setActiveTabIndex(2)} />
+        return (
+          <SettingsTab
+            key="settings_tab"
+            onCustomizeRoutingClick={() => setActiveTabIndex(2)}
+            setShowConfirmExpertModal={(show) => (show ? setActiveTabIndex(3) : setActiveTabIndex(0))}
+            showExpertModeAcknowledgement={showExpertModeAcknowledgement}
+            expertMode={expertMode}
+            setExpertMode={setExpertMode}
+          />
+        )
       case 1:
         return <RecentTransactionsTab key="recent_txns_tab" />
       case 2:
         return <CustomizeRoutingTab key="customize_routing_tab" />
+      case 3:
+        return (
+          <ExpertModeTab
+            key="expert_mode_tab"
+            setShowConfirmExpertModal={(show) => (show ? setActiveTabIndex(3) : setActiveTabIndex(0))}
+            toggleExpertMode={() => setExpertMode((s) => !s)}
+            setShowExpertModeAcknowledgement={setShowExpertModeAcknowledgement}
+          />
+        )
       default:
         return null
     }
-  }, [activeTabIndex, setActiveTabIndex])
+  }, [
+    expertMode,
+    activeTabIndex,
+    showExpertModeAcknowledgement,
+    setActiveTabIndex,
+    setExpertMode,
+    setShowExpertModeAcknowledgement,
+  ])
 
   return (
     <>
       <MotionModal
         minWidth="480px"
+        minHeight={isMobile ? '500px' : undefined}
         headerPadding="6px 24px 0"
-        title={
-          activeTabIndex === 2 ? (
-            <TabContent type="to_right">
-              <Heading>{t('Customize Routing')}</Heading>
-            </TabContent>
-          ) : (
-            <MotionTabs activeIndex={activeTabIndex} onItemClick={onTabChange}>
-              <Text>{t('Settings')}</Text>
-              <Text>{t('Recent Transactions')}</Text>
-            </MotionTabs>
-          )
-        }
+        title={renderTabHeading()}
         onDismiss={onDismiss}
-        onBack={activeTabIndex === 2 ? () => setActiveTabIndex(0) : undefined}
+        onBack={activeTabIndex === 2 || activeTabIndex === 3 ? () => setActiveTabIndex(0) : undefined}
       >
-        <LazyAnimatePresence features={domAnimation} mode="wait">
-          {renderTab()}
-        </LazyAnimatePresence>
+        {renderTab()}
       </MotionModal>
     </>
   )
