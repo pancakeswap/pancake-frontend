a164730135a850ad414fe04f1b389b590f03a9ec
chefmomota <momota@pancakeswap.com>
2024-10-09 15:53:40 +0800

    feat: Update

--- packages/pools/src/constants/config/endpoint.ts
+++ packages/pools/src/constants/config/endpoint.ts
@@ -1,3 +1,1 @@
-// export const POOLS_API = 'https://configs.pancakeswap.com/api/data/cached/syrup-pools'
-
-export const POOLS_API = 'https://cms-public-api-7ys4p.ondigitalocean.app/api/data/cached/syrup-pools'
+export const POOLS_API = 'https://configs.pancakeswap.com/api/data/cached/syrup-pools'
