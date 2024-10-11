388e072c1965af742947d9f6da0a0dc6ecf53ee9
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2024-10-11 17:36:48 +0800

    chore: fix useUserInsufficientBalance hook

--- apps/web/src/views/SwapSimplify/hooks/useUserInsufficientBalance.ts
+++ apps/web/src/views/SwapSimplify/hooks/useUserInsufficientBalance.ts
@@ -5,23 +5,26 @@ import { Field } from 'state/swap/actions'
 import { PriceOrder } from '@pancakeswap/price-api-sdk'
 import { useCurrencyBalances } from 'state/wallet/hooks'
 import { useAccount } from 'wagmi'
+import { useAllTypeBestTrade } from '../../Swap/V3Swap/hooks/useAllTypeBestTrade'
 import { useSlippageAdjustedAmounts } from '../../Swap/V3Swap/hooks/useSlippageAdjustedAmounts'
 import { useSwapCurrency } from '../../Swap/V3Swap/hooks/useSwapCurrency'
 
 export function useUserInsufficientBalance(order: PriceOrder | undefined): boolean {
   const [inputCurrency, outputCurrency] = useSwapCurrency()
+  const { tradeLoaded } = useAllTypeBestTrade()
   const { address: account } = useAccount()
   const relevantTokenBalances = useCurrencyBalances(account ?? undefined, [
     inputCurrency ?? undefined,
     outputCurrency ?? undefined,
   ])
+
   const slippageAdjustedAmounts = useSlippageAdjustedAmounts(order)
   const isInsufficientBalance = useMemo(() => {
     const currencyBalances = {
       [Field.INPUT]: relevantTokenBalances[0],
       [Field.OUTPUT]: relevantTokenBalances[1],
     }
-    if (!account) {
+    if (!account || !order || !tradeLoaded) {
       return false
     }
     const [balanceIn, amountIn] = [
@@ -32,7 +35,7 @@ export function useUserInsufficientBalance(order: PriceOrder | undefined): boole
       return true
     }
     return false
-  }, [account, relevantTokenBalances, slippageAdjustedAmounts])
+  }, [account, relevantTokenBalances, slippageAdjustedAmounts, order, tradeLoaded])
 
   return isInsufficientBalance
 }
