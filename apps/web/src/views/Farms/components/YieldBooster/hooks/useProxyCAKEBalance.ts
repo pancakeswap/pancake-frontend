36a7376d47a1bc4548a2448aff0f07d3ca5018b8
memoyil <2213635+memoyil@users.noreply.github.com>
2024-10-13 13:46:17 +0200

    chore: Remove select

--- apps/web/src/views/Farms/components/YieldBooster/hooks/useProxyCAKEBalance.ts
+++ apps/web/src/views/Farms/components/YieldBooster/hooks/useProxyCAKEBalance.ts
@@ -4,23 +4,18 @@ import useAccountActiveChain from 'hooks/useAccountActiveChain'
 import { getCakeContract } from 'utils/contractHelpers'
 import { useBCakeProxyContractAddress } from 'hooks/useBCakeProxyContractAddress'
 import { useReadContract } from '@pancakeswap/wagmi'
-import { useCallback } from 'react'
 
 const useProxyCAKEBalance = () => {
   const { account, chainId } = useAccountActiveChain()
   const { proxyAddress } = useBCakeProxyContractAddress(account, chainId)
   const cakeContract = getCakeContract()
 
-  const { data = 0, refetch } = useReadContract({
+  const { data, refetch } = useReadContract({
     chainId,
     address: cakeContract.address,
     abi: cakeContract.abi,
     query: {
       enabled: Boolean(account && proxyAddress),
-      select: useCallback(
-        (cakeBalance: bigint) => (cakeBalance ? getBalanceNumber(new BigNumber(cakeBalance.toString())) : 0),
-        [],
-      ),
     },
     functionName: 'balanceOf',
     args: proxyAddress && [proxyAddress],
@@ -28,7 +23,7 @@ const useProxyCAKEBalance = () => {
 
   return {
     refreshProxyCakeBalance: refetch,
-    proxyCakeBalance: data,
+    proxyCakeBalance: data ? getBalanceNumber(new BigNumber(data.toString())) : 0,
   }
 }
 
