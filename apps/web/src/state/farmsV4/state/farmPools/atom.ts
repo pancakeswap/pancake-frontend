23a0aa121a4b8342d4d2a96ce3bc48c919370dd7
memoyil <2213635+memoyil@users.noreply.github.com>
2024-10-17 19:09:00 +0200

    fix: Revert

--- apps/web/src/state/farmsV4/state/farmPools/atom.ts
+++ apps/web/src/state/farmsV4/state/farmPools/atom.ts
@@ -2,20 +2,3 @@ import { atom } from 'jotai'
 import { PoolInfo } from '../type'
 
 export const farmPoolsAtom = atom<PoolInfo[]>([])
-
-export const updateFarmPoolAtom = atom(null, (get, set, newPool: PoolInfo) => {
-  const pools = get(farmPoolsAtom)
-  const updatedPools = pools.map((pool) => {
-    const isMatchingPool = newPool.stableSwapAddress
-      ? pool.chainId === newPool.chainId &&
-        pool.lpAddress === newPool.lpAddress &&
-        pool.stableSwapAddress === newPool.stableSwapAddress
-      : pool.chainId === newPool.chainId && pool.lpAddress === newPool.lpAddress
-    if (isMatchingPool) {
-      return newPool
-    }
-    return pool
-  })
-
-  set(farmPoolsAtom, updatedPools)
-})
