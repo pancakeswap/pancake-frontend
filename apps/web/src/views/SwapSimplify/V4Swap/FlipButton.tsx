b5f16833d8fd0f823400fe78022a2262497a6e66
Chef Penguin <penguin@pancakeswap.com>
2024-10-23 01:45:24 +0530

    chore: add swap arrow animation (disabled currently)

--- apps/web/src/views/SwapSimplify/V4Swap/FlipButton.tsx
+++ apps/web/src/views/SwapSimplify/V4Swap/FlipButton.tsx
@@ -1,5 +1,4 @@
 import { AutoColumn, Button } from '@pancakeswap/uikit'
-import { SwapUIV2 } from '@pancakeswap/widgets-internal'
 
 import { useTranslation } from '@pancakeswap/localization'
 import replaceBrowserHistory from '@pancakeswap/utils/replaceBrowserHistory'
@@ -11,6 +10,7 @@ import { useSwapState } from 'state/swap/hooks'
 import { useSwapActionHandlers } from 'state/swap/useSwapActionHandlers'
 import { styled } from 'styled-components'
 
+import { SwapUIV2 } from '@pancakeswap/widgets-internal'
 import { useAllowRecipient } from '../../Swap/V3Swap/hooks'
 
 export const Line = styled.div`
@@ -23,6 +23,8 @@ export const Line = styled.div`
 `
 
 export const FlipButton = memo(function FlipButton() {
+  // const lottieRef = useRef<LottieRefCurrentProps | null>(null)
+
   const { onSwitchTokens } = useSwapActionHandlers()
   const {
     [Field.INPUT]: { currencyId: inputCurrencyId },
@@ -40,6 +42,17 @@ export const FlipButton = memo(function FlipButton() {
       <Line />
       <AutoRow justify="center" style={{ padding: '0 1rem', marginTop: '1em' }}>
         <SwapUIV2.SwitchButtonV2 onClick={onFlip} />
+
+        {/* <Lottie
+          lottieRef={lottieRef}
+          animationData={ArrowLottie}
+          style={{ height: '78px', cursor: 'pointer' }}
+          onClick={onFlip}
+          autoplay={false}
+          loop={false}
+          onMouseEnter={() => lottieRef.current?.playSegments([19, 32], true)}
+          onMouseLeave={() => lottieRef.current?.playSegments([52, 73], true)}
+        /> */}
       </AutoRow>
     </AutoColumn>
   )
