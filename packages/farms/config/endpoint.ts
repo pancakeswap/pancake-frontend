b745d4bd4338d191879602c33d0a9af27808aec3
chefmomota <momota@pancakeswap.com>
2024-10-24 14:57:54 +0800

    feat: Update prod endpoint

--- packages/farms/config/endpoint.ts
+++ packages/farms/config/endpoint.ts
@@ -1,3 +1,1 @@
-// export const FARMS_API = 'https://configs.pancakeswap.com/api/data/cached/farms'
-
-export const FARMS_API = 'https://cms-public-api-7ys4p.ondigitalocean.app/api/data/cached/farms'
+export const FARMS_API = 'https://configs.pancakeswap.com/api/data/cached/farms'
